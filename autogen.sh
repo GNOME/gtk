a11062b2d9716dffa537906a03ce0f57b71ef656
Shawn Amundson <amundson@src.gnome.org>
1998-01-31 17:43:56 +0000

    I got sick of typing in the same commands over and over, so borrowed autogen.sh from gnome cvs...  Also corrected some info in HACKING.
    -Shawn

---
+++ autogen.sh
@@ -0,0 +1,52 @@
+#!/bin/sh
+# Run this to generate all the initial makefiles, etc.
+
+DIE=0
+
+(autoconf --version) < /dev/null > /dev/null 2>&1 || {
+	echo
+	echo "You must have autoconf installed to compile GTK+."
+	echo "Download the appropriate package for your distribution,"
+	echo "or get the source tarball at ftp://ftp.gnu.org/pub/gnu/"
+	DIE=1
+}
+
+(libtool --version) < /dev/null > /dev/null 2>&1 || {
+	echo
+	echo "You must have libtool installed to compile GTK+."
+	echo "Get ftp://alpha.gnu.org/gnu/libtool-1.0h.tar.gz"
+	echo "(or a newer version if it is available)"
+	DIE=1
+}
+
+(automake --version) < /dev/null > /dev/null 2>&1 || {
+	echo
+	echo "You must have automake installed to compile GTK+."
+	echo "Get ftp://ftp.cygnus.com/pub/home/tromey/automake-1.2d.tar.gz"
+	echo "(or a newer version if it is available)"
+	DIE=1
+}
+
+if test "$DIE" -eq 1; then
+	exit 1
+fi
+
+(test -d gtk && test -d glib) || {
+	echo "You must run this script in the top-level GTK+ directory"
+	exit 1
+}
+
+if test -z "$*"; then
+	echo "I am going to run ./configure with no arguments - if you wish "
+        echo "to pass any to it, please specify them on the $0 command line."
+fi
+
+for i in glib .
+do 
+  echo processing $i
+  (cd $i; aclocal; automake; autoconf)
+done
+./configure "$@"
+
+echo 
+echo "Now type 'make' to compile GTK+."
