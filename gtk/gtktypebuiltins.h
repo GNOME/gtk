736bdbeaeba89cb4d1516095570f3e7cfdf4e2e8
Arturo Espinosa <unammx@src.gnome.org>
1998-08-14 22:20:47 +0000

    Regenerated gtktypebuiltins.h by hand.  The autostuff files really need to be changed to detect the presence of awk/perl and enable regeneration of the maintainer-only files inside the gtk subdirectory - Federico

--- gtk/gtktypebuiltins.h
+++ gtk/gtktypebuiltins.h
@@ -108,4 +108,4 @@ extern GtkType GTK_TYPE_GDK_WINDOW;
 extern GtkType GTK_TYPE_GDK_EVENT;
 extern GtkType GTK_TYPE_GDK_COLOR;
 
-#define	GTK_TYPE_NUM_BUILTINS	(106)
+#define	GTK_TYPE_NUM_BUILTINS	(107)
