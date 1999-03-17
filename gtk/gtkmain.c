9f513b1840a178a41f86e0fa26650d6b42f81bbb
Tim Janik <timj@gtk.org>
1999-03-17 04:18:47 +0000

    tell people that they don't really want to use the Gtk+ devel version
    Wed Mar 17 05:06:49 1999  Tim Janik  <timj@gtk.org>
    
            * gtk/gtkmain.c (gtk_init_check): tell people that they don't really
            want to use the Gtk+ devel version (which is true, and yes - even i am
            currently working with the 1.2.x branch). so everyone reading this, you
            probably want to issue
            cvs checkout -r glib-1-2 glib
            and
            cvs checkout -r gtk-1-2 gtk+
            as your next two comands.

--- gtk/gtkmain.c
+++ gtk/gtkmain.c
@@ -438,10 +438,16 @@ gtk_init_check (int	 *argc,
 	}
     }
   g_slist_free (gtk_modules);
-
+  
+  g_warning (""              "YOU ARE USING THE DEVEL BRANCH 1.3.x OF GTK+ WHICH IS CURRENTLY\n"
+	     "                UNDER HEAVY DEVELOPMENT AND FREQUENTLY INTRODUCES INSTABILITIES.\n"
+	     "                if you don't know why you are getting this, you probably want to\n"
+	     "                use the stable branch which can be retrived from\n"
+	     "                ftp://ftp.gtk.org/pub/gtk/v1.2/ or via CVS with\n"
+	     "                cvs checkout -r glib-1-2 glib; cvs checkout -r gtk-1-2 gtk+");
+  
   return TRUE;
 }
-
 void
 gtk_init (int *argc, char ***argv)
 {
