e5c14565255c190f53b667aacdd89b95c290cf1b
Arturo Espinosa <unammx@src.gnome.org>
1999-12-02 23:57:25 +0000

    Start of the new test program - Federico

---
+++ gdk-pixbuf/test-gdk-pixbuf.c
@@ -0,0 +1,33 @@
+/* GdkPixbuf library - test program
+ *
+ * Copyright (C) 1999 The Free Software Foundation
+ *
+ * Author: Federico Mena-Quintero <federico@gimp.org>
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 2 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA 02111-1307, USA.
+ */
+
+#include <config.h>
+#include <gnome.h>
+#include "gdk-pixbuf.h"
+#include "gdk-pixbuf-loader.h"
+#include "gnome-canvas-pixbuf.h"
+
+
+
+int
+main (int argc, char **argv)
+{
+}
