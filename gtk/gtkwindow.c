228a23e2ffde119cae3a390438971e0b3972c8ba
George Lebl <jirka@5z.com>
2000-09-03 06:20:22 +0000

    Add some documenting comments about GTK_CAN_DEFAULT, GTK_HAS_DEFAULT and
    Sun Sep 03 00:47:08 2000  George Lebl <jirka@5z.com>
    
    	* gtk/gtkwidget.h:  Add some documenting comments about
    	  GTK_CAN_DEFAULT, GTK_HAS_DEFAULT and GTK_RECEIVES_DEFAULT
    	  just as owen said on irc.
    
    	* gtk/gtkwindow.c (gtk_window_set_default):  Don't blindly
    	  set HAS_DEFAULT.  Only set it if there isn't another window
    	  holding it (a focus window which RECEIVES_DEFAULT) and only unset
    	  it if the widget is not the focused window with RECEIVES_DEFAULT.
    	  This fixes weird double default bugs in dialogs which has things
    	  call gtk_window_set_default at times.

--- gtk/gtkwindow.c
+++ gtk/gtkwindow.c
@@ -455,7 +455,9 @@ gtk_window_set_default (GtkWindow *window,
     {
       if (window->default_widget)
 	{
-	  GTK_WIDGET_UNSET_FLAGS (window->default_widget, GTK_HAS_DEFAULT);
+	  if (window->focus_widget != window->default_widget ||
+	      !GTK_WIDGET_RECEIVES_DEFAULT (window->default_widget))
+	    GTK_WIDGET_UNSET_FLAGS (window->default_widget, GTK_HAS_DEFAULT);
 	  gtk_widget_draw_default (window->default_widget);
 	}
 
@@ -463,7 +465,9 @@ gtk_window_set_default (GtkWindow *window,
 
       if (window->default_widget)
 	{
-	  GTK_WIDGET_SET_FLAGS (window->default_widget, GTK_HAS_DEFAULT);
+	  if (window->focus_widget == NULL ||
+	      !GTK_WIDGET_RECEIVES_DEFAULT (window->focus_widget))
+	    GTK_WIDGET_SET_FLAGS (window->default_widget, GTK_HAS_DEFAULT);
 	  gtk_widget_draw_default (window->default_widget);
 	}
     }
