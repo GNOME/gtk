99b14ff2c5a97bd468859a06402828d52a6fb3ee
Tor Lillqvist <tml@iki.fi>
2000-11-17 22:30:47 +0000

    Update.
    2000-11-18  Tor Lillqvist  <tml@iki.fi>
    
    	* gtk/gtk.def: Update.
    
    	* gtk/gtktexttypes.h: Mark variables for export/import from DLL.

--- gtk/gtktexttypes.h
+++ gtk/gtktexttypes.h
@@ -21,25 +21,35 @@ typedef struct _GtkTextMarkBody GtkTextMarkBody;
  * Declarations for variables shared among the text-related files:
  */
 
+#ifdef G_OS_WIN32
+#ifdef GTK_COMPILATION
+#define VARIABLE __declspec(dllexport)
+#else
+#define VARIABLE __declspec(dllimport) extern
+#endif
+#else
+#define VARIABLE extern
+#endif
+
 /* In gtktextbtree.c */
-extern GtkTextLineSegmentClass gtk_text_char_type;
-extern GtkTextLineSegmentClass gtk_text_toggle_on_type;
-extern GtkTextLineSegmentClass gtk_text_toggle_off_type;
+VARIABLE GtkTextLineSegmentClass gtk_text_char_type;
+VARIABLE GtkTextLineSegmentClass gtk_text_toggle_on_type;
+VARIABLE GtkTextLineSegmentClass gtk_text_toggle_off_type;
 
 /* In gtktextmark.c */
-extern GtkTextLineSegmentClass gtk_text_left_mark_type;
-extern GtkTextLineSegmentClass gtk_text_right_mark_type;
+VARIABLE GtkTextLineSegmentClass gtk_text_left_mark_type;
+VARIABLE GtkTextLineSegmentClass gtk_text_right_mark_type;
 
 /* In gtktextchild.c */
-extern GtkTextLineSegmentClass gtk_text_pixbuf_type;
-extern GtkTextLineSegmentClass gtk_text_child_type;
+VARIABLE GtkTextLineSegmentClass gtk_text_pixbuf_type;
+VARIABLE GtkTextLineSegmentClass gtk_text_child_type;
 
 /*
  * UTF 8 Stubs
  */
 
 #define GTK_TEXT_UNKNOWN_CHAR 0xFFFC
-extern const gchar gtk_text_unknown_char_utf8[];
+VARIABLE const gchar gtk_text_unknown_char_utf8[];
 
 gboolean gtk_text_byte_begins_utf8_char (const gchar *byte);
 
