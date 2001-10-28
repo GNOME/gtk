c4721d19d38faa4f1db0558dfaf0b783b3751fea
Tor Lillqvist <tml@iki.fi>
2001-10-28 00:54:12 +0000

    New file.
    2001-10-28  Tor Lillqvist  <tml@iki.fi>
    
    	* gtk+-zip.sh: New file.

---
+++ gtk+-zip.sh
@@ -0,0 +1,27 @@
+#!/bin/sh
+
+# Build zipfile for gtk+-1.3-win32-production on Win32: both runtime
+# and developer stuff
+
+ZIP=/g/tmp/gtk+-1.3.0-`date +%Y%m%d`.zip
+rm $ZIP
+cd /target
+
+zip -r $ZIP -@ <<EOF
+COPYING.LIB-2
+etc/gtk
+include/gtk
+include/gdk
+lib/libgdk-0.dll
+lib/libgdk.dll.a
+lib/gdk.lib
+lib/libgtk-0.dll
+lib/libgtk.dll.a
+lib/gtk.lib
+lib/gtk+
+lib/pkgconfig/gdk-1.3-win32-production.pc
+lib/pkgconfig/gtk+-1.3-win32-production.pc
+share/themes/Default/gtk
+EOF
+
+zip -r $ZIP lib/locale/*/LC_MESSAGES/gtk+.mo
