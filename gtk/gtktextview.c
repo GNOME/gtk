77b8bac91d20a3c9ac1803b62f014e3a157b3de0
Havoc Pennington <hp@pobox.com>
2002-01-01 17:44:46 +0000

    doc fix
    2002-01-01  Havoc Pennington  <hp@pobox.com>
    
    	* gtk/gtktextview.c (gtk_text_view_get_default_attributes): doc
    	fix
    
    2002-01-01  Havoc Pennington  <hp@pobox.com>
    
    	* gtk/tmpl/gtktexttag.sgml: docs updates, mention that invisible
    	property isn't implemented
    
    	* gtk/text_widget.sgml: small tweaks

--- gtk/gtktextview.c
+++ gtk/gtktextview.c
@@ -4823,7 +4823,7 @@ gtk_text_view_ensure_layout (GtkTextView *text_view)
  * Obtains a copy of the default text attributes. These are the
  * attributes used for text unless a tag overrides them.
  * You'd typically pass the default attributes in to
- * gtk_text_tag_get_attributes() in order to get the
+ * gtk_text_iter_get_attributes() in order to get the
  * attributes in effect at a given text position.
  *
  * The return value is a copy owned by the caller of this function,
