a7d3cece4721f3d4bbdde816fcb699f4ccb2ded5
Owen Taylor <otaylor@src.gnome.org>
2002-02-01 17:53:56 +0000

    ri Feb  1 12:51:20 2002  Owen Taylor  <otaylor@redhat.com>
            * gtk/gtknotebook.c (focus_child_in): Catch the
            cur_page == NULL (no children) case. (#70287,
            Paolo Maggi.)

--- gtk/gtknotebook.c
+++ gtk/gtknotebook.c
@@ -1866,7 +1866,8 @@ static gboolean
 focus_child_in (GtkNotebook     *notebook,
 		GtkDirectionType direction)
 {
-  return gtk_widget_child_focus (notebook->cur_page->child, direction);
+  if (notebook->cur_page)
+    return gtk_widget_child_focus (notebook->cur_page->child, direction);
 }
 
 /* Focus in the notebook can either be on the pages, or on
