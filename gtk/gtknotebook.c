30e9527d24c3e1fd75f7cd0a8ffea0c08e23e269
Alexey Malyshev <maa@sparc.spb.su>
2002-03-01 14:46:38 +0000

    gtk+/gtk/gtkitemfactory.c fixed a little type in docs for
    2002-03-01 Alexey Malyshev <maa@sparc.spb.su>
    
            * gtk+/gtk/gtkitemfactory.c
    	fixed a little type in docs for gtk_notebook_set_show_border()

--- gtk/gtknotebook.c
+++ gtk/gtknotebook.c
@@ -4250,7 +4250,7 @@ gtk_notebook_prev_page (GtkNotebook *notebook)
  * @show_border: %TRUE if a bevel should be drawn around the notebook.
  * 
  * Sets whether a bevel will be drawn around the notebook pages.
- * this is only has an effect when the tabs are not shown.
+ * This is only has an effect when the tabs are not shown.
  * See gtk_notebook_set_show_tabs().
  **/
 void
