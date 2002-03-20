67359a9c01adb1418621678975b496964e8e6843
Owen Taylor <otaylor@redhat.com>
2002-03-20 17:37:48 +0000

    Remove leftover notification of has-default. (#75508, Jacob Berkman.)
    Wed Mar 20 12:35:22 2002  Owen Taylor  <otaylor@redhat.com>
    
            * gtk/gtkwidget.c (gtk_widget_grab_default): Remove
            leftover notification of has-default. (#75508, Jacob
            Berkman.)

--- gtk/gtkwidget.c
+++ gtk/gtkwidget.c
@@ -3499,10 +3499,7 @@ gtk_widget_grab_default (GtkWidget *widget)
   window = gtk_widget_get_toplevel (widget);
   
   if (window && GTK_WIDGET_TOPLEVEL (window))
-    {
-       gtk_window_set_default (GTK_WINDOW (window), widget);
-       g_object_notify (G_OBJECT (widget), "has_default");
-    }
+    gtk_window_set_default (GTK_WINDOW (window), widget);
   else
     g_warning (G_STRLOC ": widget not within a GtkWindow");
 }
