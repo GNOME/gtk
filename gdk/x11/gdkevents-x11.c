45fe0becc71f7c43b63cfda2f0f8bb729b37f514
Matthias Clasen <matthiasc@src.gnome.org>
2002-06-06 21:01:34 +0000

    Set hardware_keycode for KeyRelease events. (#84305)
    	* gdk/x11/gdkevents-x11.c (gdk_event_translate): Set
    	hardware_keycode for KeyRelease events.  (#84305)

--- gdk/x11/gdkevents-x11.c
+++ gdk/x11/gdkevents-x11.c
@@ -665,6 +665,7 @@ gdk_event_translate (GdkEvent *event,
       charcount = XLookupString (&xevent->xkey, buf, 16,
 				 &keysym, &compose);
       event->key.keyval = keysym;      
+      event->key.hardware_keycode = xevent->xkey.keycode;
       
       GDK_NOTE (EVENTS, 
 		g_message ("key release:\t\twindow: %ld	 key: %12s  %d",
