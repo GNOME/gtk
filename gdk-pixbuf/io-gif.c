3f92a70e8b121856e1ec69ec1f33a269a0e30428
Owen Taylor <otaylor@redhat.com>
2002-11-06 20:26:27 +0000

    Fix trailing semicolon on macro, clean up a bit.
    Wed Nov  6 15:19:53 2002  Owen Taylor  <otaylor@redhat.com>
    
            * io-gif.c (CHECK_LZW_SP): Fix trailing semicolon on macro,
            clean up a bit.

--- gdk-pixbuf/io-gif.c
+++ gdk-pixbuf/io-gif.c
@@ -565,13 +565,16 @@ gif_lzw_clear_code (GifContext *context)
 	return 0;
 }
 
-#define CHECK_LZW_SP() if(((guchar *)context->lzw_sp) >= (((guchar *)context->lzw_stack) + sizeof(context->lzw_stack))) { \
-        g_set_error (context->error,                  \
-                     GDK_PIXBUF_ERROR,                \
-                     GDK_PIXBUF_ERROR_CORRUPT_IMAGE,  \
-                     _("Stack overflow"));            \
-        return -2;                                    \
-}
+#define CHECK_LZW_SP() G_STMT_START {                                           \
+        if ((guchar *)context->lzw_sp >=                                        \
+            (guchar *)context->lzw_stack + sizeof (context->lzw_stack)) {       \
+                 g_set_error (context->error,                                   \
+                             GDK_PIXBUF_ERROR,                                  \
+                             GDK_PIXBUF_ERROR_CORRUPT_IMAGE,                    \
+                             _("Stack overflow"));                              \
+                return -2;                                                      \
+        }                                                                       \
+} G_STMT_END
 
 static int
 lzw_read_byte (GifContext *context)
