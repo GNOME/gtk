21694abd64c6da84769e61d867847f594b8d53b9
Owen Taylor <otaylor@redhat.com>
2003-04-21 22:39:43 +0000

    Remove a couple of obsolete comments, fix another comment. (#109737,
    Mon Apr 21 18:35:48 2003  Owen Taylor  <otaylor@redhat.com>
    
            * gtk/gtkobject.h: Remove a couple of obsolete comments,
            fix another comment. (#109737, Britton Kerin)

--- gtk/gtkobject.h
+++ gtk/gtkobject.h
@@ -76,11 +76,6 @@ typedef enum
 typedef struct _GtkObjectClass	GtkObjectClass;
 
 
-/* The GtkObject structure is the base of the Gtk+ objects hierarchy,
- * it ``inherits'' from the GtkTypeObject by mirroring its fields,
- * which must always be kept in sync completely. The GtkObject defines
- * the few basic items that all derived classes contain.
- */
 struct _GtkObject
 {
   GObject parent_instance;
@@ -93,11 +88,6 @@ struct _GtkObject
   guint32 flags;
 };
 
-/* The GtkObjectClass is the base of the Gtk+ objects classes hierarchy,
- * it ``inherits'' from the GtkTypeClass by mirroring its fields, which
- * must always be kept in sync completely. The GtkObjectClass defines
- * the basic necessities for the object inheritance mechanism to work.
- */
 struct _GtkObjectClass
 {
   GObjectClass parent_class;
@@ -110,13 +100,13 @@ struct _GtkObjectClass
 		   GtkArg    *arg,
 		   guint      arg_id);
   
-  /* The function that will end an objects life time. In one way ore
-   *  another all three of them are defined for all objects. If an
-   *  object class overrides one of the methods in order to perform class
+  /* Default signal handler for the ::destroy signal, which is
+   *  invoked to request that references to the widget be dropped.
+   *  If an object class overrides destroy() in order to perform class
    *  specific destruction then it must still invoke its superclass'
    *  implementation of the method after it is finished with its
-   *  own cleanup. (See the destroy function for GtkWidget for
-   *  an example of how to do this).
+   *  own cleanup. (See gtk_widget_real_destroy() for an example of
+   *  how to do this).
    */
   void (*destroy)  (GtkObject *object);
 };
