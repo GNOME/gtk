accfa25c0349379f28aeb4b08b0204734fa779fd
Owen Taylor <otaylor@redhat.com>
2003-08-13 21:14:27 +0000

    Remove usage of menu_shell->ignore leave which was to deal with stray
    Wed Aug 13 17:01:49 2003  Owen Taylor  <otaylor@redhat.com>
    
            * gtk/gtkmenushell.[ch] (gtk_menu_shell_enter_notify):
            Remove usage of menu_shell->ignore leave which was
            to deal with stray events we no longer get because
            we do grabbing differently. Comment it as unused in
            the header. (#115837, Keith Bissett)

--- gtk/gtkmenushell.h
+++ gtk/gtkmenushell.h
@@ -62,7 +62,7 @@ struct _GtkMenuShell
   guint active : 1;
   guint have_grab : 1;
   guint have_xgrab : 1;
-  guint ignore_leave : 1;
+  guint ignore_leave : 1;	/* unused */
   guint menu_flag : 1;
   guint ignore_enter : 1;
 };
