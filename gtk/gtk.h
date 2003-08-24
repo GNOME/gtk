b374ae3fb4ff4a92a5ba0cb3f50a1ba9073875e1
Owen Taylor <otaylor@redhat.com>
2003-08-24 21:16:15 +0000

    Remove reference to gtkresizegrip.h.
    Sun Aug 24 17:14:44 2003  Owen Taylor  <otaylor@redhat.com>
    
            * gtk/gtk.h: Remove reference to gtkresizegrip.h.

--- gtk/gtk.h
+++ gtk/gtk.h
@@ -125,7 +125,6 @@
 #include <gtk/gtkradiotoolbutton.h>
 #include <gtk/gtkrange.h>
 #include <gtk/gtkrc.h>
-#include <gtk/gtkresizegrip.h>
 #include <gtk/gtkruler.h>
 #include <gtk/gtkscale.h>
 #include <gtk/gtkscrollbar.h>
