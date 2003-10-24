1e5c8e64f5f44adc288e16bc2aafbb06372e600f
Owen Taylor <otaylor@redhat.com>
2003-10-24 20:03:37 +0000

    Relicense to be dual AFL/LGPL (and thus also GPL) rather than AFL/GPL.
    Fri Oct 24 16:02:32 2003  Owen Taylor  <otaylor@redhat.com>
    
            * *.[ch]: Relicense to be dual AFL/LGPL (and thus also
            GPL) rather than AFL/GPL. Also update AFL version to 1.2.

--- gtk/xdgmime/xdgmimeint.h
+++ gtk/xdgmime/xdgmimeint.h
@@ -6,22 +6,23 @@
  * Copyright (C) 2003  Red Hat, Inc.
  * Copyright (C) 2003  Jonathan Blandford <jrb@alum.mit.edu>
  *
- * Licensed under the Academic Free License version 1.2
+ * Licensed under the Academic Free License version 2.0
+ * Or under the following terms:
  * 
- * This program is free software; you can redistribute it and/or modify
- * it under the terms of the GNU General Public License as published by
- * the Free Software Foundation; either version 2 of the License, or
- * (at your option) any later version.
+ * This library is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Lesser General Public
+ * License as published by the Free Software Foundation; either
+ * version 2 of the License, or (at your option) any later version.
  *
- * This program is distributed in the hope that it will be useful,
+ * This library is distributed in the hope that it will be useful,
  * but WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
- * GNU General Public License for more details.
- * 
- * You should have received a copy of the GNU General Public License
- * along with this program; if not, write to the Free Software
- * Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.	 See the GNU
+ * Lesser General Public License for more details.
  *
+ * You should have received a copy of the GNU Lesser General Public
+ * License along with this library; if not, write to the
+ * Free Software Foundation, Inc., 59 Temple Place - Suite 330,
+ * Boston, MA 02111-1307, USA.
  */
 
 #ifndef __XDG_MIME_INT_H__
