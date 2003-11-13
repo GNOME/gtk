99a9ad8ce9bc0d6c318c3051f796d04120bc91f6
Matthias Clasen <maclas@gmx.de>
2003-11-13 00:48:03 +0000

    Use GTK_DATADIR/{icons,pixmaps} instead of
    Thu Nov 13 01:46:32 2003  Matthias Clasen  <maclas@gmx.de>
    
    	* gtk/gtkicontheme.c (gtk_icon_theme_init): Use
    	GTK_DATADIR/{icons,pixmaps} instead of
    	GTK_DATA_PREFIX/{icons,pixmaps}.  (#123365, Marco Pesenti Gritti)

--- gtk/gtkicontheme.c
+++ gtk/gtkicontheme.c
@@ -532,8 +532,8 @@ gtk_icon_theme_init (GtkIconTheme *icon_theme)
   priv->search_path[0] = g_build_filename (g_get_home_dir (),
 					   ".icons",
 					   NULL);
-  priv->search_path[1] = g_build_filename (GTK_DATA_PREFIX, "pixmaps", NULL);
-  priv->search_path[2] = g_build_filename (GTK_DATA_PREFIX, "icons", NULL);
+  priv->search_path[1] = g_build_filename (GTK_DATADIR, "pixmaps", NULL);
+  priv->search_path[2] = g_build_filename (GTK_DATADIR, "icons", NULL);
   priv->search_path[3] = g_strdup ("/usr/share/icons");
   priv->search_path[4] = g_strdup ("/usr/share/pixmaps");
   priv->search_path_len = 5;
