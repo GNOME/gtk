477e349f84a795b6ffd89bbcd71517cdc49251ba
Manish Singh <yosh@gimp.org>
2003-11-18 00:26:51 +0000

    gdk-pixbuf/Makefile.am Use install-data-hook for gdk-pixbuf.loaders and
    Mon Nov 17 16:25:04 2003  Manish Singh <yosh@gimp.org>
    
            * gdk-pixbuf/Makefile.am
            * modules/input/Makefile.am: Use install-data-hook for
            gdk-pixbuf.loaders and gtk.immodules install generation targets.
            Fixes #112806.
    
            * gtk/gtkexpander.h: Apply missing declarations from #124449.

--- gtk/gtkexpander.h
+++ gtk/gtkexpander.h
@@ -78,6 +78,10 @@ void                  gtk_expander_set_use_underline (GtkExpander *expander,
 						      gboolean     use_underline);
 gboolean              gtk_expander_get_use_underline (GtkExpander *expander);
 
+void                  gtk_expander_set_use_markup    (GtkExpander *expander,
+						      gboolean    use_markup);
+gboolean              gtk_expander_get_use_markup    (GtkExpander *expander);
+
 void                  gtk_expander_set_label_widget  (GtkExpander *expander,
 						      GtkWidget   *label_widget);
 GtkWidget            *gtk_expander_get_label_widget  (GtkExpander *expander);
