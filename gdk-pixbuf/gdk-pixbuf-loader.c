44c206420a03800e5d7b2e469c97fe05530e02ee
Matthias Clasen <maclas@gmx.de>
2003-12-11 00:57:20 +0000

    Move the documentation for signals inline, to get proper parameter
    Thu Dec 11 01:57:05 2003  Matthias Clasen  <maclas@gmx.de>
    
    	* gdk-pixbuf-loader.c (gdk_pixbuf_loader_class_init): Move
    	the documentation for signals inline, to get proper parameter
    	documentation.  (#128977)

--- gdk-pixbuf/gdk-pixbuf-loader.c
+++ gdk-pixbuf/gdk-pixbuf-loader.c
@@ -70,7 +70,6 @@ typedef struct
 
 /**
  * gdk_pixbuf_loader_get_type:
- * @void:
  *
  * Registers the #GdkPixbufLoader class if necessary, and returns the type ID
  * associated to it.
@@ -116,6 +115,18 @@ gdk_pixbuf_loader_class_init (GdkPixbufLoaderClass *class)
   
         object_class->finalize = gdk_pixbuf_loader_finalize;
 
+        /**
+         * GdkPixbufLoader::size-prepared:
+         * @loader: the object which received the signal.
+         * @width: the original width of the image
+         * @height: the original height of the image
+         *
+         * This signal is emitted when the pixbuf loader has been fed the
+         * initial amount of data that is required to figure out the size
+         * of the image that it will create.  Applications can call  
+         * gdk_pixbuf_loader_set_size() in response to this signal to set
+         * the desired size to which the image should be scaled.
+         */
         pixbuf_loader_signals[SIZE_PREPARED] =
                 g_signal_new ("size_prepared",
                               G_TYPE_FROM_CLASS (object_class),
@@ -127,6 +138,15 @@ gdk_pixbuf_loader_class_init (GdkPixbufLoaderClass *class)
                               G_TYPE_INT,
                               G_TYPE_INT);
   
+        /**
+         * GdkPixbufLoader::area-prepared:
+         * @loader: the object which received the signal.
+         *
+         * This signal is emitted when the pixbuf loader has allocated the 
+         * pixbuf in the desired size.  After this signal is emitted, 
+         * applications can call gdk_pixbuf_loader_get_pixbuf() to fetch 
+         * the partially-loaded pixbuf.
+         */
         pixbuf_loader_signals[AREA_PREPARED] =
                 g_signal_new ("area_prepared",
                               G_TYPE_FROM_CLASS (object_class),
@@ -135,7 +155,21 @@ gdk_pixbuf_loader_class_init (GdkPixbufLoaderClass *class)
                               NULL, NULL,
                               gdk_pixbuf_marshal_VOID__VOID,
                               G_TYPE_NONE, 0);
-  
+
+        /**
+         * GdkPixbufLoader::area-updated:
+         * @loader: the object which received the signal.
+         * @x: X offset of upper-left corner of the updated area.
+         * @y: Y offset of upper-left corner of the updated area.
+         * @width: Width of updated area.
+         * @height: Height of updated area.
+         *
+         * This signal is emitted when a significant area of the image being
+         * loaded has been updated.  Normally it means that a complete
+         * scanline has been read in, but it could be a different area as
+         * well.  Applications can use this signal to know when to repaint
+         * areas of an image that is being loaded.
+         */
         pixbuf_loader_signals[AREA_UPDATED] =
                 g_signal_new ("area_updated",
                               G_TYPE_FROM_CLASS (object_class),
@@ -149,6 +183,15 @@ gdk_pixbuf_loader_class_init (GdkPixbufLoaderClass *class)
                               G_TYPE_INT,
                               G_TYPE_INT);
   
+        /**
+         * GdkPixbufLoader::closed:
+         * @loader: the object which received the signal.
+         *
+         * This signal is emitted when gdk_pixbuf_loader_close() is called.
+         * It can be used by different parts of an application to receive
+         * notification when an image loader is closed by the code that
+         * drives it.
+         */
         pixbuf_loader_signals[CLOSED] =
                 g_signal_new ("closed",
                               G_TYPE_FROM_CLASS (object_class),
