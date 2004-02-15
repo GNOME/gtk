d7042395c44e79c11c7fc275112216a7903a847d
Matthias Clasen <matthiasc@src.gnome.org>
2004-02-15 21:06:53 +0000

    Add deprecation notes pointing people to GtkComboBox.

--- gtk/gtkoptionmenu.c
+++ gtk/gtkoptionmenu.c
@@ -337,6 +337,7 @@ gtk_option_menu_set_history (GtkOptionMenu *option_menu,
  * items are numbered from top to bottom, starting with 0. 
  * 
  * Return value: index of the selected menu item, or -1 if there are no menu items
+ * Deprecated: Use #GtkComboBox instead.
  **/
 gint
 gtk_option_menu_get_history (GtkOptionMenu *option_menu)
