22bc71a66747ad9e024a1c34e154dca09c0eb62d
Federico Mena Quintero <federico@ximian.com>
2004-03-09 19:47:09 +0000

    Handle the returned parent_path being NULL. Fixes #136662.
    2004-03-09  Federico Mena Quintero  <federico@ximian.com>
    
    	* gtk/gtkfilechooserdefault.c (get_file_info): Handle the returned
    	parent_path being NULL.  Fixes #136662.

--- gtk/gtkfilechooserdefault.c
+++ gtk/gtkfilechooserdefault.c
@@ -757,10 +757,12 @@ get_file_info (GtkFileSystem *file_system, const GtkFilePath *path, gboolean nam
   GtkFileFolder *parent_folder;
   GtkFileInfo *info;
 
+  info = NULL;
+
   if (!gtk_file_system_get_parent (file_system, path, &parent_path, error))
     return NULL;
 
-  parent_folder = gtk_file_system_get_folder (file_system, parent_path,
+  parent_folder = gtk_file_system_get_folder (file_system, parent_path ? parent_path : path,
 					      GTK_FILE_INFO_DISPLAY_NAME
 #if 0
 					      | GTK_FILE_INFO_ICON
