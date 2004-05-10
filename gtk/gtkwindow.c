8f74f43f3ba4d24365acb75e7627ba57f7a4fb9c
Matthias Clasen <mclasen@redhat.com>
2004-05-10 21:13:41 +0000

    Make static.
    2004-05-10  Matthias Clasen  <mclasen@redhat.com>
    
    	* gtk/gtkwindow.c (get_screen_icon_info):
    	Make static.

--- gtk/gtkwindow.c
+++ gtk/gtkwindow.c
@@ -2455,7 +2455,7 @@ typedef struct {
   GdkPixmap *mask;
 } ScreenIconInfo;
 
-ScreenIconInfo *
+static ScreenIconInfo *
 get_screen_icon_info (GdkScreen *screen)
 {
   ScreenIconInfo *info = g_object_get_data (G_OBJECT (screen), 
