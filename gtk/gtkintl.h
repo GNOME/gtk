261d5404386a176a6a209d3deee08dc9b94f2d1a
Matthias Clasen <mclasen@redhat.com>
2004-05-18 19:52:41 +0000

    Include glib/gi18n-lib.h and only define the P_() macros ourselves.
    2004-05-18  Matthias Clasen  <mclasen@redhat.com>
    
    	* gtk/gtkintl.h: Include glib/gi18n-lib.h and only define
    	the P_() macros ourselves.

--- gtk/gtkintl.h
+++ gtk/gtkintl.h
@@ -2,28 +2,12 @@
 #define __GTKINTL_H__
 
 #include "config.h"
-#include <glib.h>
+#include <glib/gi18n-lib.h>
 
 #ifdef ENABLE_NLS
-#include <libintl.h>
-#define _(String) dgettext(GETTEXT_PACKAGE,String)
 #define P_(String) dgettext(GETTEXT_PACKAGE "-properties",String)
-#define Q_(String) g_strip_context ((String), gettext (String))
-#ifdef gettext_noop
-#define N_(String) gettext_noop(String)
-#else
-#define N_(String) (String)
-#endif
-#else /* NLS is disabled */
-#define _(String) (String)
+#else 
 #define P_(String) (String)
-#define N_(String) (String)
-#define Q_(String) (String)
-#define textdomain(String) (String)
-#define gettext(String) (String)
-#define dgettext(Domain,String) (String)
-#define dcgettext(Domain,String,Type) (String)
-#define bindtextdomain(Domain,Directory) (Domain) 
 #endif
 
 #endif
