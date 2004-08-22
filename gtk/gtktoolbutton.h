cb004a2e5a8a9dc95ac95d61fcc13376da223d6f
Matthias Clasen <maclas@gmx.de>
2004-08-22 03:32:34 +0000

    Fix macro definitions.
    Sat Aug 21 23:29:54 2004  Matthias Clasen  <maclas@gmx.de>
    
    	* gtk/gtktoolitem.h (GTK_IS_TOOL_ITEM_CLASS):
    	* gtk/gtktoolbutton.h (GTK_IS_TOOL_BUTTON_CLASS):
    	* gtk/gtktoggleaction.h (GTK_IS_TOGGLE_ACTION_CLASS):
    	* gtk/gtkseparatortoolitem.h (GTK_IS_SEPARATOR_TOOL_ITEM_CLASS):
    	* gtk/gtkradiotoolbutton.h (GTK_IS_RADIO_TOOL_BUTTON_CLASS):
    	* gtk/gtkradioaction.h (GTK_IS_RADIO_ACTION_CLASS):
    	* gtk/gtkuimanager.h (GTK_IS_UI_MANAGER_CLASS):
    	* gtk/gtkaction.h (GTK_IS_ACTION_CLASS): Fix macro definitions.
    
    Mon Aug 16 01:30:12 2004  Matthias Clasen  <maclas@gmx.de>
    
    	* gtk/gtkbutton.c (gtk_button_set_focus_on_click): Fix typo in
    	the docs.

--- gtk/gtktoolbutton.h
+++ gtk/gtktoolbutton.h
@@ -31,7 +31,7 @@ G_BEGIN_DECLS
 #define GTK_TOOL_BUTTON(obj)            (G_TYPE_CHECK_INSTANCE_CAST ((obj), GTK_TYPE_TOOL_BUTTON, GtkToolButton))
 #define GTK_TOOL_BUTTON_CLASS(klass)    (G_TYPE_CHECK_CLASS_CAST ((klass), GTK_TYPE_TOOL_BUTTON, GtkToolButtonClass))
 #define GTK_IS_TOOL_BUTTON(obj)         (G_TYPE_CHECK_INSTANCE_TYPE ((obj), GTK_TYPE_TOOL_BUTTON))
-#define GTK_IS_TOOL_BUTTON_CLASS(klass) (G_TYPE_CHECK_CLASS_TYPE ((obj), GTK_TYPE_TOOL_BUTTON))
+#define GTK_IS_TOOL_BUTTON_CLASS(klass) (G_TYPE_CHECK_CLASS_TYPE ((klass), GTK_TYPE_TOOL_BUTTON))
 #define GTK_TOOL_BUTTON_GET_CLASS(obj)  (G_TYPE_INSTANCE_GET_CLASS((obj), GTK_TYPE_TOOL_BUTTON, GtkToolButtonClass))
 
 typedef struct _GtkToolButton        GtkToolButton;
--- gtk/gtkuimanager.h
+++ gtk/gtkuimanager.h
@@ -44,7 +44,7 @@ G_BEGIN_DECLS
 #define GTK_UI_MANAGER(obj)            (G_TYPE_CHECK_INSTANCE_CAST ((obj), GTK_TYPE_UI_MANAGER, GtkUIManager))
 #define GTK_UI_MANAGER_CLASS(klass)    (G_TYPE_CHECK_CLASS_CAST ((klass), GTK_TYPE_UI_MANAGER, GtkUIManagerClass))
 #define GTK_IS_UI_MANAGER(obj)         (G_TYPE_CHECK_INSTANCE_TYPE ((obj), GTK_TYPE_UI_MANAGER))
-#define GTK_IS_UI_MANAGER_CLASS(klass) (G_TYPE_CHECK_CLASS_TYPE ((obj), GTK_TYPE_UI_MANAGER))
+#define GTK_IS_UI_MANAGER_CLASS(klass) (G_TYPE_CHECK_CLASS_TYPE ((klass), GTK_TYPE_UI_MANAGER))
 #define GTK_UI_MANAGER_GET_CLASS(obj)  (G_TYPE_INSTANCE_GET_CLASS((obj), GTK_TYPE_UI_MANAGER, GtkUIManagerClass))
 
 typedef struct _GtkUIManager      GtkUIManager;
