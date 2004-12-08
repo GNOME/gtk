2851547be28ce8bd8b1e5eac1ea623315c939690
Matthias Clasen <mclasen@redhat.com>
2004-12-08 05:57:04 +0000

    Use button->event_window for the fake leave event, otherwise the button
    2004-12-08  Matthias Clasen  <mclasen@redhat.com>
    
    	* gtk/gtktreeview.c (_gtk_tree_view_column_start_drag): Use
    	button->event_window for the fake leave event, otherwise
    	the button ignores it.

--- gtk/gtktreeview.c
+++ gtk/gtktreeview.c
@@ -8115,7 +8115,7 @@ _gtk_tree_view_column_start_drag (GtkTreeView       *tree_view,
 
   send_event = gdk_event_new (GDK_LEAVE_NOTIFY);
   send_event->crossing.send_event = TRUE;
-  send_event->crossing.window = g_object_ref (column->button->window);
+  send_event->crossing.window = g_object_ref (GTK_BUTTON (column->button)->event_window);
   send_event->crossing.subwindow = NULL;
   send_event->crossing.detail = GDK_NOTIFY_ANCESTOR;
   send_event->crossing.time = GDK_CURRENT_TIME;
