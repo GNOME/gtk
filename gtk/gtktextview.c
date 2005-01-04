cfeab5787d127e4ea39db0ba82a31b93215e0b51
Søren Sandmann <sandmann@redhat.com>
2005-01-04 16:39:56 +0000

    Bug 162112.
    Tue Jan  4 11:36:43 2005  Søren Sandmann  <sandmann@redhat.com>
    
    	Bug 162112.
    
    	* gtk/gtktextview.c (text_window_realize): Add GDK_EXPOSURE_MASK
    	to win->window's event_mask
    
    	* gtk/gtkviewport.c (gtk_viewport_realize): Add GDK_EXPOSURE_MASK
    	to viewport->view_window.

--- gtk/gtktextview.c
+++ gtk/gtktextview.c
@@ -7174,7 +7174,7 @@ text_window_realize (GtkTextWindow *win,
   attributes.wclass = GDK_INPUT_OUTPUT;
   attributes.visual = gtk_widget_get_visual (win->widget);
   attributes.colormap = gtk_widget_get_colormap (win->widget);
-  attributes.event_mask = GDK_VISIBILITY_NOTIFY_MASK;
+  attributes.event_mask = GDK_VISIBILITY_NOTIFY_MASK | GDK_EXPOSURE_MASK;
 
   attributes_mask = GDK_WA_X | GDK_WA_Y | GDK_WA_VISUAL | GDK_WA_COLORMAP;
 
--- gtk/gtkviewport.c
+++ gtk/gtkviewport.c
@@ -621,7 +621,7 @@ gtk_viewport_realize (GtkWidget *widget)
   attributes.y = view_allocation.y;
   attributes.width = view_allocation.width;
   attributes.height = view_allocation.height;
-  attributes.event_mask = 0;
+  attributes.event_mask = GDK_EXPOSURE_MASK;
 
   viewport->view_window = gdk_window_new (widget->window, &attributes, attributes_mask);
   gdk_window_set_user_data (viewport->view_window, viewport);
