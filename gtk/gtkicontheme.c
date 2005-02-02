b635954523bd823a7097c807020aa7ea46b3c28d
Matthias Clasen <mclasen@redhat.com>
2005-02-02 04:31:21 +0000

    Look for icons in XDG_DATA_DIRS/pixmaps. (#165950, Thomas Zajic)
    2005-02-01  Matthias Clasen  <mclasen@redhat.com>
    
    	* gtk/gtkicontheme.c (gtk_icon_theme_init): Look for icons
    	in XDG_DATA_DIRS/pixmaps.  (#165950, Thomas Zajic)

--- gtk/gtkicontheme.c
+++ gtk/gtkicontheme.c
@@ -568,10 +568,7 @@ gtk_icon_theme_init (GtkIconTheme *icon_theme)
   xdg_data_dirs = g_get_system_data_dirs ();
   for (i = 0; xdg_data_dirs[i]; i++) ;
 
-  priv->search_path_len = i + 2;
-#ifdef G_OS_UNIX
-  priv->search_path_len++;
-#endif
+  priv->search_path_len = 2 * i + 2;
   
   priv->search_path = g_new (char *, priv->search_path_len);
   
@@ -582,9 +579,8 @@ gtk_icon_theme_init (GtkIconTheme *icon_theme)
   for (j = 0; xdg_data_dirs[j]; j++) 
     priv->search_path[i++] = g_build_filename (xdg_data_dirs[j], "icons", NULL);
 
-#ifdef G_OS_UNIX
-  priv->search_path[i++] = g_strdup ("/usr/share/pixmaps");
-#endif
+  for (j = 0; xdg_data_dirs[j]; j++) 
+    priv->search_path[i++] = g_build_filename (xdg_data_dirs[j], "pixmaps", NULL);
 
   priv->themes_valid = FALSE;
   priv->themes = NULL;
