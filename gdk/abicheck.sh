64d9d86991568f4f61e51cecb5548f5c2e744620
Matthias Clasen <matthiasc@src.gnome.org>
2005-04-11 05:35:10 +0000

    2.6.6

--- gdk/abicheck.sh
+++ gdk/abicheck.sh
@@ -1,5 +1,5 @@
 #! /bin/sh
 
-cpp -P -DALL_FILES -DGDK_ENABLE_BROKEN -DGDK_WINDOWING_X11 ${srcdir:-.}/gdk.symbols | sed -e '/^$/d' -e 's/ G_GNUC.*$//' | sort > expected-abi
+cpp -P -DALL_FILES -DGDK_ENABLE_BROKEN -DGDK_WINDOWING_X11 ${srcdir:-.}/gdk.symbols | sed -e '/^$/d' -e 's/ G_GNUC.*$//' | sort | uniq > expected-abi
 nm -D .libs/libgdk-x11-2.0.so | grep " T " | cut -d ' ' -f 3 | sort > actual-abi
 diff -u expected-abi actual-abi && rm expected-abi actual-abi
