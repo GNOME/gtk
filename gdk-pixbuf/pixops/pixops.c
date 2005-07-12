84a144edda4cc795a88659028dee232c758f6eec
Matthias Clasen <matthiasc@src.gnome.org>
2005-07-12 15:58:57 +0000

    Correct copyright information

--- gdk-pixbuf/pixops/pixops.c
+++ gdk-pixbuf/pixops/pixops.c
@@ -1,5 +1,5 @@
 /*
- * Copyright (C) 2000 Owen Taylor
+ * Copyright (C) 2000 Red Hat, Inc
  *
  * This library is free software; you can redistribute it and/or
  * modify it under the terms of the GNU Lesser General Public
