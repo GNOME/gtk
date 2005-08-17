47b2b9dd190a19f4591e13d3d1c07f2e1d78070c
Matthias Clasen <mclasen@redhat.com>
2005-08-17 16:35:54 +0000

    Complete the backported fix.
    2005-08-17  Matthias Clasen  <mclasen@redhat.com>
    
    	* gtk/gtkmenu.c (gtk_menu_grab_notify): Complete the
    	backported fix.

--- gtk/gtkmenu.c
+++ gtk/gtkmenu.c
@@ -4395,7 +4395,7 @@ gtk_menu_grab_notify (GtkWidget *widget,
 
   if (!was_grabbed)
     {
-      if (!GTK_IS_MENU (gtk_grab_get_current ()))
+      if (!GTK_IS_MENU_SHELL (grab))
 	gtk_menu_shell_cancel (GTK_MENU_SHELL (widget));
     }
 }
