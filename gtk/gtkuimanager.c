4a481c6d87bfe17753236303ba01447c78c5ef81
Matthias Clasen <mclasen@redhat.com>
2005-08-30 14:11:50 +0000

    Another fix for the default value of the "ui" property.
    2005-08-30  Matthias Clasen  <mclasen@redhat.com>
    
    	* gtk/gtkuimanager.c (gtk_ui_manager_class_init): Another
    	fix for the default value of the "ui" property.

--- gtk/gtkuimanager.c
+++ gtk/gtkuimanager.c
@@ -239,7 +239,7 @@ gtk_ui_manager_class_init (GtkUIManagerClass *klass)
 				   g_param_spec_string ("ui",
  							P_("Merged UI definition"),
 							P_("An XML string describing the merged UI"),
-							"<ui>\n</ui>",
+							"<ui>\n</ui>\n",
 							GTK_PARAM_READABLE));
 
 
