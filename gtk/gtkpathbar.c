f692b06467613ab43dad59e8800eb9ea87d1befa
Matthias Clasen <mclasen@redhat.com>
2005-11-18 19:55:24 +0000

    Fix a C99ism. (#321777, Jens Granseuer)
    2005-11-18  Matthias Clasen  <mclasen@redhat.com>
    
    	* gtk/gtkpathbar.c (button_clicked_cb): Fix a C99ism.
    	(#321777, Jens Granseuer)

--- gtk/gtkpathbar.c
+++ gtk/gtkpathbar.c
@@ -933,6 +933,7 @@ button_clicked_cb (GtkWidget *button,
   GtkPathBar *path_bar;
   GList *button_list;
   gboolean child_is_hidden;
+  GtkFilePath *child_path;
 
   button_data = BUTTON_DATA (data);
   if (button_data->ignore_changes)
@@ -945,7 +946,6 @@ button_clicked_cb (GtkWidget *button,
 
   gtk_toggle_button_set_active (GTK_TOGGLE_BUTTON (button), TRUE);
 
-  GtkFilePath *child_path;
   if (button_list->prev)
     {
       ButtonData *child_data;
