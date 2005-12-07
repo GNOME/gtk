c3624ad4011dbceb557b5027f5069f1884ceb495
Matthias Clasen <mclasen@redhat.com>
2005-12-07 20:05:11 +0000

    Don't leak text.
    2005-12-07  Matthias Clasen  <mclasen@redhat.com>
    
    	* demos/gtk-demo/iconview_edit.c (set_cell_color): Don't
    	leak text.

--- demos/gtk-demo/iconview_edit.c
+++ demos/gtk-demo/iconview_edit.c
@@ -63,6 +63,7 @@ set_cell_color (GtkCellLayout   *cell_layout,
       (color.red   >> 8) << 24 |
       (color.green >> 8) << 16 |
       (color.blue  >> 8) << 8;
+  g_free (text);
 
   pixbuf = gdk_pixbuf_new (GDK_COLORSPACE_RGB, FALSE, 8, 24, 24);
   gdk_pixbuf_fill (pixbuf, pixel);
