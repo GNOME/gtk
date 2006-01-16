be820c5dc410ea65638b7798733159f0ea9906f1
Anders Carlsson <andersca@src.gnome.org>
2006-01-16 11:11:20 +0000

    Fix ChangeLog entry

--- gdk/quartz/gdkproperty-quartz.c
+++ gdk/quartz/gdkproperty-quartz.c
@@ -119,7 +119,7 @@ ensure_atom_tables (void)
 
   names_to_atoms = g_hash_table_new (g_str_hash, g_str_equal);
   atoms_to_names = g_ptr_array_sized_new (G_N_ELEMENTS (xatoms_offset));
-  
+
   for (i = 0; i < G_N_ELEMENTS (xatoms_offset); i++)
     {
       g_hash_table_insert(names_to_atoms, (gchar *)xatoms_string + xatoms_offset[i], GINT_TO_POINTER (i));
@@ -140,7 +140,7 @@ intern_atom_internal (const gchar *atom_name, gboolean allocate)
     return result;
   
   result = GINT_TO_POINTER (atoms_to_names->len);
-  name = allocate ? g_strdup (atom_name) : (gchar *)allocate;
+  name = allocate ? g_strdup (atom_name) : (gchar *)atom_name;
   g_hash_table_insert(names_to_atoms, name, result);
   g_ptr_array_add(atoms_to_names, name);
   
@@ -149,7 +149,7 @@ intern_atom_internal (const gchar *atom_name, gboolean allocate)
 
 GdkAtom
 gdk_atom_intern (const gchar *atom_name,
-		 		         gboolean     only_if_exists)
+		 gboolean     only_if_exists)
 {
   return intern_atom_internal (atom_name, TRUE);
 }
