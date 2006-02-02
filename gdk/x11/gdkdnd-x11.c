afab67f99a6245041fae9f45f959cf25794fdaa7
Matthias Clasen <mclasen@redhat.com>
2006-02-02 20:51:49 +0000

    Make drops on the root window work again. (#145243, Andrew S. Dixon)
    2006-02-02  Matthias Clasen  <mclasen@redhat.com>
    
    	* gdk/x11/gdkdnd-x11.c (_gdk_drag_get_protocol_for_display):
    	Make drops on the root window work again.  (#145243, Andrew S. Dixon)

--- gdk/x11/gdkdnd-x11.c
+++ gdk/x11/gdkdnd-x11.c
@@ -3035,7 +3035,13 @@ _gdk_drag_get_protocol_for_display (GdkDisplay      *display,
 	  GDK_NOTE (DND, g_message ("Entering local Xdnd window %#x\n", xid));
 	  return xid;
 	}
-      else
+      else if (_gdk_x11_display_is_root_window (display, (Window) xid))
+	{
+	  *protocol = GDK_DRAG_PROTO_ROOTWIN;
+	  GDK_NOTE (DND, g_message ("Entering root window\n"));
+	  return xid;
+	}
+	       
 	return None;
     }
   
@@ -3103,6 +3109,7 @@ _gdk_drag_get_protocol_for_display (GdkDisplay      *display,
 
       if (rootwin)
 	{
+	  GDK_NOTE (DND, g_message ("Entering root window\n"));
 	  *protocol = GDK_DRAG_PROTO_ROOTWIN;
 	  return xid;
 	}
