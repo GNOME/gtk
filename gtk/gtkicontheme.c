a32b9712f3a44fae57d03dba2424f6e2844bd0e6
Matthias Clasen <mclasen@redhat.com>
2006-02-13 02:31:28 +0000

    Give working examples in the docs. (#330944, John Spray)
    2006-02-12  Matthias Clasen  <mclasen@redhat.com>
    
    	* gtk/gtkicontheme.c (gtk_icon_theme_list_icons): Give working
    	examples in the docs.  (#330944, John Spray)

--- gtk/gtkicontheme.c
+++ gtk/gtkicontheme.c
@@ -1589,8 +1589,8 @@ add_key_to_list (gpointer  key,
  * Lists the icons in the current icon theme. Only a subset
  * of the icons can be listed by providing a context string.
  * The set of values for the context string is system dependent,
- * but will typically include such values as 'apps' and
- * 'mimetypes'.
+ * but will typically include such values as "Applications" and
+ * "MimeTypes".
  * 
  * Return value: a #GList list holding the names of all the
  *  icons in the theme. You must first free each element
@@ -2039,8 +2039,9 @@ theme_lookup_icon (IconTheme          *theme,
 }
 
 static void
-theme_list_icons (IconTheme *theme, GHashTable *icons,
-		  GQuark context)
+theme_list_icons (IconTheme  *theme, 
+		  GHashTable *icons,
+		  GQuark      context)
 {
   GList *l = theme->dirs;
   IconThemeDir *dir;
