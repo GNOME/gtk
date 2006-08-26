4ab827e20a047eb4b6ef033f9d8bddb5a403e72d
Matthias Clasen <mclasen@redhat.com>
2006-08-26 00:27:11 +0000

    Use local binding when loading modules. (#351868)
    2006-08-25  Matthias Clasen  <mclasen@redhat.com>
    
    	* gtk/gtkmodules.c (find_module): Use local binding when
    	loading modules.  (#351868)

--- gtk/gtkmodules.c
+++ gtk/gtkmodules.c
@@ -236,7 +236,7 @@ find_module (const gchar *name)
       module_name = g_module_build_path (NULL, name);
     }
 
-  module = g_module_open (module_name, G_MODULE_BIND_LAZY);
+  module = g_module_open (module_name, G_MODULE_BIND_LOCAL | G_MODULE_BIND_LAZY);
   g_free(module_name);
 
   return module;
