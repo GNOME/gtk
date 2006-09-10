da05f10d2d3a27e438727de14077fcebd1d3733e
Matthias Clasen <matthiasc@src.gnome.org>
2006-09-10 06:36:11 +0000

    add new files

---
+++ gtk/pltcheck.sh
@@ -0,0 +1,17 @@
+#!/bin/sh
+
+LANG=C
+
+status=0
+
+if ! which readelf 2>/dev/null >/dev/null; then
+	echo "'readelf' not found; skipping test"
+	exit 0
+fi
+
+for so in .libs/lib*.so; do
+	echo Checking $so for local PLT entries
+	readelf -r $so | grep 'JU\?MP_SLOT' | grep '\<gtk' && status=1
+done
+
+exit $status
