418f80e294909017a8b764421f91fbb69e799655
Behdad Esfahbod <behdad@gnome.org>
2006-10-05 15:53:42 +0000

    Propagate srcdir into the test correctly. (#359845)
    2006-10-05  Behdad Esfahbod  <behdad@gnome.org>
    
            * gtk/Makefile.am, gtk/aliasfilescheck.sh:  Propagate srcdir into the
            test correctly. (#359845)

--- gtk/aliasfilescheck.sh
+++ gtk/aliasfilescheck.sh
@@ -5,8 +5,7 @@ if test "x$gtk_all_c_sources" = x; then
 	exit 1
 fi
 
-grep 'IN_FILE' gtk.symbols | sed 's/.*(//;s/).*//' | grep __ | sort -u > expected-files
-grep '^ *# *define __' $gtk_all_c_sources | sed 's/.*define //;s/ *$//' | sort > actual-files
+grep 'IN_FILE' ${srcdir-.}/gtk.symbols | sed 's/.*(//;s/).*//' | grep __ | sort -u > expected-files
+{ cd ${srcdir-.}; grep '^ *# *define __' $gtk_all_c_sources; } | sed 's/.*define //;s/ *$//' | sort > actual-files
 
 diff expected-files actual-files && rm -f expected-files actual-files
-
