08bd23daa162fb9932aadd0004388dcc94e80a1d
Benjamin Berg <benjamin@sipsolutions.net>
2006-11-26 19:42:15 +0000

    Protect against NULL widget. (#379503)
    2006-11-26  Benjamin Berg  <benjamin@sipsolutions.net>
    
    	* gtk/gtkstyle.c: (gtk_default_draw_flat_box): Protect against NULL
    	  widget. (#379503)

--- gtk/gtkstyle.c
+++ gtk/gtkstyle.c
@@ -3435,7 +3435,7 @@ gtk_default_draw_flat_box (GtkStyle      *style,
 		   !strcmp ("cell_even_ruled_sorted", detail))
             {
 	      /* This has to be really broken; alex made me do it. -jrb */
-	      if (GTK_WIDGET_HAS_FOCUS (widget))
+	      if (widget && GTK_WIDGET_HAS_FOCUS (widget))
 		gc1 = style->base_gc[state_type];
 	      else
 	        gc1 = style->base_gc[GTK_STATE_ACTIVE];
@@ -3443,7 +3443,7 @@ gtk_default_draw_flat_box (GtkStyle      *style,
 	  else if (!strcmp ("cell_odd_ruled", detail) ||
 		   !strcmp ("cell_odd_ruled_sorted", detail))
 	    {
-	      if (GTK_WIDGET_HAS_FOCUS (widget))
+	      if (widget && GTK_WIDGET_HAS_FOCUS (widget))
 	        freeme = get_darkened_gc (window, &style->base[state_type], 1);
 	      else
 	        freeme = get_darkened_gc (window, &style->base[GTK_STATE_ACTIVE], 1);
