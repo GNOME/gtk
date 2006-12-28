6d2ffa04356b5d26df87a53d1cf85d41197e28f2
Matthias Clasen <mclasen@redhat.com>
2006-12-28 04:24:30 +0000

    Close the io channel on unref. (#390159, Joe Markus Clarke)
    2006-12-27  Matthias Clasen  <mclasen@redhat.com>
    
            * modules/printbackends/lpr/gtkprintbackendlpr.c
            (gtk_print_backend_lpr_print_stream): Close the io channel
            on unref.  (#390159, Joe Markus Clarke)

--- modules/printbackends/lpr/gtkprintbackendlpr.c
+++ modules/printbackends/lpr/gtkprintbackendlpr.c
@@ -372,6 +372,8 @@ gtk_print_backend_lpr_print_stream (GtkPrintBackend        *print_backend,
       goto out;
     }
 
+  g_io_channel_set_close_on_unref (ps->in, TRUE);
+
   g_io_add_watch (data_io, 
                   G_IO_IN | G_IO_PRI | G_IO_ERR | G_IO_HUP,
                   (GIOFunc) lpr_write,
