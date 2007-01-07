d8723a7375d1c65556dc2593a74261148b4ab313
Matthias Clasen <mclasen@redhat.com>
2007-01-07 07:19:07 +0000

    Call text_window_realize() with the correct parameters. (#393813, Yevgen
    2007-01-07  Matthias Clasen  <mclasen@redhat.com>
    
            * gtk/gtktextview.c (gtk_text_view_set_border_window_size):
            Call text_window_realize() with the correct parameters.
            (#393813, Yevgen Muntyan)
    
    
    
    svn path=/trunk/; revision=17101

--- gtk/gtktextview.c
+++ gtk/gtktextview.c
@@ -8182,7 +8182,7 @@ set_window_width (GtkTextView      *text_view,
                                    width, 0);
           /* if the widget is already realized we need to realize the child manually */
           if (GTK_WIDGET_REALIZED (text_view))
-            text_window_realize (*winp, GTK_WIDGET (text_view)->window);
+            text_window_realize (*winp, GTK_WIDGET (text_view));
         }
       else
         {
@@ -8222,7 +8222,7 @@ set_window_height (GtkTextView      *text_view,
 
           /* if the widget is already realized we need to realize the child manually */
           if (GTK_WIDGET_REALIZED (text_view))
-            text_window_realize (*winp, GTK_WIDGET (text_view)->window);
+            text_window_realize (*winp, GTK_WIDGET (text_view));
         }
       else
         {
