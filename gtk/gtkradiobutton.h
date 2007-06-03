1fff6b4fbd658e92eb67f70ecf15bbe274fd41b2
Emmanuele Bassi <ebassi@gnome.org>
2007-06-03 23:39:43 +0000

    Add gtk_radio_button_set_value(), gtk_radio_button_get_current_value() and
    2007-06-04  Emmanuele Bassi  <ebassi@gnome.org>
    
    	* gtk/gtkradiobutton.h:
    	* gtk/gtkradiobutton.c:
    	* gtk/gtk.symbols: Add gtk_radio_button_set_value(),
    	gtk_radio_button_get_current_value() and gtk_radio_button_get_value().
    	Use these functions to set and get an arbitrary integer associated to
    	a GtkRadioButton in a group, like the value associated to a
    	GtkRadioAction.
    
    	* tests/testgtk.c:
    	(create_radio_buttons), (radio_toggled_cb): Exercise the new API.
    
    svn path=/trunk/; revision=18019

--- gtk/gtkradiobutton.h
+++ gtk/gtkradiobutton.h
@@ -41,7 +41,6 @@ G_BEGIN_DECLS
 #define GTK_IS_RADIO_BUTTON_CLASS(klass)       (G_TYPE_CHECK_CLASS_TYPE ((klass), GTK_TYPE_RADIO_BUTTON))
 #define GTK_RADIO_BUTTON_GET_CLASS(obj)        (G_TYPE_INSTANCE_GET_CLASS ((obj), GTK_TYPE_RADIO_BUTTON, GtkRadioButtonClass))
 
-
 typedef struct _GtkRadioButton	     GtkRadioButton;
 typedef struct _GtkRadioButtonClass  GtkRadioButtonClass;
 
@@ -81,6 +80,10 @@ GtkWidget* gtk_radio_button_new_with_mnemonic_from_widget (GtkRadioButton *group
 GSList*    gtk_radio_button_get_group                     (GtkRadioButton *radio_button);
 void       gtk_radio_button_set_group                     (GtkRadioButton *radio_button,
                                                            GSList         *group);
+gint       gtk_radio_button_get_value                     (GtkRadioButton *radio_button);
+void       gtk_radio_button_set_value                     (GtkRadioButton *radio_button,
+                                                           gint            value);
+gint       gtk_radio_button_get_current_value             (GtkRadioButton *radio_button);
 
 #ifndef GTK_DISABLE_DEPRECATED
 #define gtk_radio_button_group gtk_radio_button_get_group
