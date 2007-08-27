bf4b559a31312feb0f3d8926e2e8b27234026647
Stefan Kost <stefkost@src.gnome.org>
2007-08-27 17:46:53 +0000

    Update menu accelerators, when adding/removing AccelGroups dynamically.
    	* gtk/gtkwindow.c: Update menu accelerators, when adding/removing
    	AccelGroups dynamically. (#469374)
    
    
    svn path=/trunk/; revision=18691

--- gtk/gtkwindow.c
+++ gtk/gtkwindow.c
@@ -1574,6 +1574,7 @@ gtk_window_add_accel_group (GtkWindow     *window,
   g_signal_connect_object (accel_group, "accel_changed",
 			   G_CALLBACK (gtk_window_notify_keys_changed),
 			   window, G_CONNECT_SWAPPED);
+  gtk_window_notify_keys_changed (window);
 }
 
 /**
@@ -1594,6 +1595,7 @@ gtk_window_remove_accel_group (GtkWindow     *window,
 					gtk_window_notify_keys_changed,
 					window);
   _gtk_accel_group_detach (accel_group, G_OBJECT (window));
+  gtk_window_notify_keys_changed (window);
 }
 
 static GtkMnemonicHash *
