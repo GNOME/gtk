0670cac1a89ad3c09db542d606914ad1f3ce6a42
Murray Cumming <murrayc@murrayc.com>
2007-10-05 08:39:51 +0000

    Revert the change on 2007-04-29 (Also check the default handler when
    2007-10-05  Murray Cumming  <murrayc@murrayc.com>
    
    * gtk/gtkprintoperation.c: (print_pages_idle):
    Revert the change on 2007-04-29 (Also check
    the default handler when deciding whether to emit the
    paginate signal.  (#345345, Yevgen Muntyan)).
    This stopped printing from working in gtkmm, and is apparently 
    not appropriate anyway. Bug #482089.
    
    svn path=/branches/gtk-2-10/; revision=18886

--- gtk/gtkprintoperation.c
+++ gtk/gtkprintoperation.c
@@ -2087,8 +2087,7 @@ print_pages_idle (gpointer user_data)
 	  goto out;
 	}
       
-      if (GTK_PRINT_OPERATION_GET_CLASS (data->op)->paginate != NULL ||
-          g_signal_has_handler_pending (data->op, signals[PAGINATE], 0, FALSE))
+      if (g_signal_has_handler_pending (data->op, signals[PAGINATE], 0, FALSE))
 	{
 	  gboolean paginated = FALSE;
 
