9e1fbc93a2f64e8da1c8697a679d57dd724631ff
Cody Russell <bratsche@gnome.org>
2008-01-31 17:53:06 +0000

    Change queue_resize() to queue_draw(), and add a check for child2 != NULL
    2008-01-31  Cody Russell  <bratsche@gnome.org>
    
            * gtk/gtkpaned.c (gtk_paned_set_position):
            Change queue_resize() to queue_draw(), and add a check for
            child2 != NULL in case someone calls this before there is
            a child packed in there.  (#144269 again)
    
    
    svn path=/trunk/; revision=19449

--- gtk/gtkpaned.c
+++ gtk/gtkpaned.c
@@ -1235,7 +1235,10 @@ gtk_paned_set_position (GtkPaned *paned,
 
 #ifdef G_OS_WIN32
   /* Hacky work-around for bug #144269 */
-  gtk_widget_queue_resize (paned->child2);
+  if (paned->child2 != NULL)
+    {
+      gtk_widget_queue_draw (paned->child2);
+    }
 #endif
 }
 
