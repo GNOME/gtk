a21cbfc27277a4aab3f42dabf127d5b1a797c862
Matthias Clasen <mclasne@redhat.com>
2008-02-12 16:38:15 +0000

    Don't let the width or height go below 1. (#516024, Christian Persch)
    2008-02-12  Matthias Clasen  <mclasne@redhat.com>
    
            * gdk-pixbuf-io.c (at_scale_size_prepared_cb): Don't let
            the width or height go below 1.  (#516024, Christian Persch)
    
    
    
    svn path=/branches/gtk-2-12/; revision=19536

--- gdk-pixbuf/gdk-pixbuf-io.c
+++ gdk-pixbuf/gdk-pixbuf-io.c
@@ -975,6 +975,9 @@ size_prepared_cb (GdkPixbufLoader *loader,
 			height = info->height;
 	}
 	
+	width = MAX (width, 1);
+        height = MAX (height, 1);
+
 	gdk_pixbuf_loader_set_size (loader, width, height);
 }
 
