a6d56c1581232df8eae4f7206ff3618fda681f57
Richard Hult <richard@imendio.com>
2008-03-01 14:21:50 +0000

    Merged from trunk:
    2008-03-01  Richard Hult  <richard@imendio.com>
    
    	Merged from trunk:
    
    	* gdk/quartz/gdkprivate-quartz.h:
    	* gdk/quartz/gdkwindow-quartz.c: Add debugging code to highlight a
    	gdkwindow.
    
    svn path=/branches/gtk-2-12/; revision=19683

--- gdk/quartz/gdkprivate-quartz.h
+++ gdk/quartz/gdkprivate-quartz.h
@@ -144,6 +144,7 @@ void       _gdk_quartz_window_attach_to_parent      (GdkWindow *window);
 void       _gdk_quartz_window_detach_from_parent    (GdkWindow *window);
 void       _gdk_quartz_window_did_become_main       (GdkWindow *window);
 void       _gdk_quartz_window_did_resign_main       (GdkWindow *window);
+void       _gdk_quartz_window_debug_highlight       (GdkWindow *window);
 
 /* Events */
 typedef enum {
