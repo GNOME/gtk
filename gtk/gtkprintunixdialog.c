1ed1d38dbd327d39d87fc9246a878d2c1f410c67
Matthias Clasen <matthiasc@src.gnome.org>
2008-06-02 22:05:21 +0000

    Bug 531008 – Crash in gtkprintunixdialog.c
            * gtk/gtkprintunixdialog.c: Disconnect signal handlers when
            the dialog closes. Patch by Yevgen Muntyan.
    
    svn path=/branches/gtk-2-12/; revision=20283

--- gtk/gtkprintunixdialog.c
+++ gtk/gtkprintunixdialog.c
@@ -194,6 +194,7 @@ struct GtkPrintUnixDialogPrivate
   GList *print_backends;
   
   GtkPrinter *current_printer;
+  GtkPrinter *request_details_printer;
   guint request_details_tag;
   GtkPrinterOptionSet *options;
   gulong options_changed_handler;
@@ -443,6 +444,21 @@ gtk_print_unix_dialog_destroy (GtkPrintUnixDialog *dialog)
 }
 
 static void
+disconnect_printer_details_request (GtkPrintUnixDialog *dialog)
+{
+  GtkPrintUnixDialogPrivate *priv = dialog->priv;
+
+  if (priv->request_details_tag)
+    {
+      g_signal_handler_disconnect (priv->request_details_printer,
+                                   priv->request_details_tag);
+      priv->request_details_tag = 0;
+      g_object_unref (priv->request_details_printer);
+      priv->request_details_printer = NULL;
+    }
+}
+
+static void
 gtk_print_unix_dialog_finalize (GObject *object)
 {
   GtkPrintUnixDialog *dialog = GTK_PRINT_UNIX_DIALOG (object);
@@ -451,13 +467,8 @@ gtk_print_unix_dialog_finalize (GObject *object)
   GList *node;
 
   unschedule_idle_mark_conflicts (dialog);
+  disconnect_printer_details_request (dialog);
 
-  if (priv->request_details_tag)
-    {
-      g_source_remove (priv->request_details_tag);
-      priv->request_details_tag = 0;
-    }
-  
   if (priv->current_printer)
     {
       g_object_unref (priv->current_printer);
@@ -1351,7 +1362,7 @@ printer_details_acquired (GtkPrinter         *printer,
 {
   GtkPrintUnixDialogPrivate *priv = dialog->priv;
 
-  priv->request_details_tag = 0;
+  disconnect_printer_details_request (dialog);
   
   if (success)
     {
@@ -1378,13 +1389,9 @@ selected_printer_changed (GtkTreeSelection   *selection,
       g_free (priv->waiting_for_printer);
       priv->waiting_for_printer = NULL;
     }
-  
-  if (priv->request_details_tag)
-    {
-      g_source_remove (priv->request_details_tag);
-      priv->request_details_tag = 0;
-    }
-  
+
+  disconnect_printer_details_request (dialog);
+
   printer = NULL;
   if (gtk_tree_selection_get_selected (selection, NULL, &filter_iter))
     {
@@ -1403,8 +1410,9 @@ selected_printer_changed (GtkTreeSelection   *selection,
       priv->request_details_tag =
 	g_signal_connect (printer, "details-acquired",
 			  G_CALLBACK (printer_details_acquired), dialog);
+      /* take the reference */
+      priv->request_details_printer = printer;
       gtk_printer_request_details (printer);
-      g_object_unref (printer);
       return;
     }
   
