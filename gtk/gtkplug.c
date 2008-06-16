9dd2aa7edc150d6f21c6a7346d1ad62d414778d9
Michael Natterer <mitch@imendio.com>
2008-06-16 22:47:50 +0000

    use g_object_ref() and unref() instead of the deprecated gtk_widget_ref()
    2008-06-17  Michael Natterer  <mitch@imendio.com>
    
    	* gtk/gtkplug.c (_gtk_plug_send_delete_event): use g_object_ref()
    	and unref() instead of the deprecated gtk_widget_ref() and unref().
    
    
    svn path=/trunk/; revision=20411

--- gtk/gtkplug.c
+++ gtk/gtkplug.c
@@ -294,16 +294,16 @@ void
 _gtk_plug_send_delete_event (GtkWidget *widget)
 {
   GdkEvent *event = gdk_event_new (GDK_DELETE);
-  
+
   event->any.window = g_object_ref (widget->window);
   event->any.send_event = FALSE;
 
-  gtk_widget_ref (widget);
-  
+  g_object_ref (widget);
+
   if (!gtk_widget_event (widget, event))
     gtk_widget_destroy (widget);
-  
-  gtk_widget_unref (widget);
+
+  g_object_unref (widget);
 
   gdk_event_free (event);
 }
