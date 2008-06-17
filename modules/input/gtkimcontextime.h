7ff875bb1acbf90c57381df1ff8e4c2e6725f32d
Tor Lillqvist <tml@novell.com>
2008-06-17 19:11:54 +0000

    Don't use the deprecated GTK_* GtkType macros.
    2008-06-17  Tor Lillqvist  <tml@novell.com>
    
    	* modules/input/gtkimcontextime.h: Don't use the deprecated GTK_*
    	GtkType macros.
    
    
    svn path=/trunk/; revision=20434

--- modules/input/gtkimcontextime.h
+++ modules/input/gtkimcontextime.h
@@ -26,11 +26,11 @@
 extern GType gtk_type_im_context_ime;
 
 #define GTK_TYPE_IM_CONTEXT_IME            gtk_type_im_context_ime
-#define GTK_IM_CONTEXT_IME(obj)            (GTK_CHECK_CAST ((obj), GTK_TYPE_IM_CONTEXT_IME, GtkIMContextIME))
-#define GTK_IM_CONTEXT_IME_CLASS(klass)    (GTK_CHECK_CLASS_CAST ((klass), GTK_TYPE_IM_CONTEXT_IME, GtkIMContextIMEClass))
-#define GTK_IS_IM_CONTEXT_IME(obj)         (GTK_CHECK_TYPE ((obj), GTK_TYPE_IM_CONTEXT_IME))
-#define GTK_IS_IM_CONTEXT_IME_CLASS(klass) (GTK_CHECK_CLASS_TYPE ((klass), GTK_TYPE_IM_CONTEXT_IME))
-#define GTK_IM_CONTEXT_IME_GET_CLASS(obj)  (GTK_CHECK_GET_CLASS ((obj), GTK_TYPE_IM_CONTEXT_IME, GtkIMContextIMEClass))
+#define GTK_IM_CONTEXT_IME(obj)            (G_TYPE_CHECK_INSTANCE_CAST ((obj), GTK_TYPE_IM_CONTEXT_IME, GtkIMContextIME))
+#define GTK_IM_CONTEXT_IME_CLASS(klass)    (G_TYPE_CHECK_CLASS_CAST ((klass), GTK_TYPE_IM_CONTEXT_IME, GtkIMContextIMEClass))
+#define GTK_IS_IM_CONTEXT_IME(obj)         (G_TYPE_CHECK_INSTANCE_TYPE ((obj), GTK_TYPE_IM_CONTEXT_IME))
+#define GTK_IS_IM_CONTEXT_IME_CLASS(klass) (G_TYPE_CHECK_CLASS_TYPE ((klass), GTK_TYPE_IM_CONTEXT_IME))
+#define GTK_IM_CONTEXT_IME_GET_CLASS(obj)  (G_TYPE_INSTANCE_GET_CLASS ((obj), GTK_TYPE_IM_CONTEXT_IME, GtkIMContextIMEClass))
 
 typedef struct _GtkIMContextIME GtkIMContextIME;
 typedef struct _GtkIMContextIMEPrivate GtkIMContextIMEPrivate;
