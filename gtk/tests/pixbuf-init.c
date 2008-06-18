d5565f5b638a39c8c33f290e860a6cc38f5631be
Michael Natterer <mitch@imendio.com>
2008-06-18 13:29:02 +0000

    tests/Makefile.am tests/autotestfilechooser.c tests/buildertest.c
    2008-06-18  Michael Natterer  <mitch@imendio.com>
    
    	* tests/Makefile.am
    	* tests/autotestfilechooser.c
    	* tests/buildertest.c
    	* tests/defaultvaluetest.c
    	* tests/floatingtest.c
    	* tests/objecttests.c
    	* tests/pixbuf-init.c
    	* tests/textbuffertest.c: remove unit tests here...
    
    	* gtk/tests/Makefile.am
    	* gtk/tests/builder.c
    	* gtk/tests/defaultvalue.c
    	* gtk/tests/filechooser.c
    	* gtk/tests/floating.c
    	* gtk/tests/object.c
    	* gtk/tests/pixbuf-init.c
    	* gtk/tests/textbuffer.c: ...and add them here.
    
    
    svn path=/trunk/; revision=20450

--- gtk/tests/pixbuf-init.c
+++ gtk/tests/pixbuf-init.c
@@ -15,6 +15,6 @@ file_exists (const char *filename)
 void
 pixbuf_init (void)
 {
-  if (file_exists ("../gdk-pixbuf/libpixbufloader-pnm.la"))
-    g_setenv ("GDK_PIXBUF_MODULE_FILE", "../gdk-pixbuf/gdk-pixbuf.loaders", TRUE);
+  if (file_exists ("../../gdk-pixbuf/libpixbufloader-pnm.la"))
+    g_setenv ("GDK_PIXBUF_MODULE_FILE", "../../gdk-pixbuf/gdk-pixbuf.loaders", TRUE);
 }
