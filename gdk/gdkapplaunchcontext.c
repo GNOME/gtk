ed383758c687215b42221826f24f508fc200e75a
Matthias Clasen <matthiasc@src.gnome.org>
2008-08-02 04:12:55 +0000

    Improve docs
    svn path=/trunk/; revision=20929

--- gdk/gdkapplaunchcontext.c
+++ gdk/gdkapplaunchcontext.c
@@ -134,7 +134,10 @@ gdk_app_launch_context_set_display (GdkAppLaunchContext *context,
  *
  * Sets the screen on which applications will be launched when
  * using this context. See also gdk_app_launch_context_set_display().
+ *
  * If both @screen and @display are set, the @screen takes priority.
+ * If neither @screen or @display are set, the default screen and
+ * display are used.
  *
  * Since: 2.14
  */
@@ -163,6 +166,10 @@ gdk_app_launch_context_set_screen (GdkAppLaunchContext *context,
  * <ulink url="http://www.freedesktop.org/Standards/wm-spec">Extended 
  * Window Manager Hints</ulink>. 
  *
+ * When the workspace is not specified or @desktop is set to -1, 
+ * it is up to the window manager to pick one, typically it will
+ * be the current workspace.
+ *
  * Since: 2.14
  */
 void
@@ -178,7 +185,12 @@ gdk_app_launch_context_set_desktop (GdkAppLaunchContext *context,
  * @timestamp: a timestamp
  *
  * Sets the timestamp of @context. The timestamp should ideally
- * be taken from the event that triggered the launch.
+ * be taken from the event that triggered the launch. 
+ *
+ * Window managers can use this information to avoid moving the
+ * focus to the newly launched application when the user is busy
+ * typing in another window. This is also known as 'focus stealing
+ * prevention'.
  *
  * Since: 2.14
  */
@@ -218,11 +230,14 @@ gdk_app_launch_context_set_icon (GdkAppLaunchContext *context,
  * @context: a #GdkAppLaunchContext
  * @icon_name: an icon name, or %NULL
  *
- * Sets the icon for applications that are launched with this
- * context. The @icon_name will be interpreted in the same way 
- * as the Icon field in desktop files. 
- * See also gdk_app_launch_context_set_icon(). If both @icon 
- * and @icon_name are set, the @icon_name takes priority.
+ * Sets the icon for applications that are launched with this context. 
+ * The @icon_name will be interpreted in the same way as the Icon field 
+ * in desktop files. See also gdk_app_launch_context_set_icon(). 
+ *
+ * If both @icon and @icon_name are set, the @icon_name takes priority.
+ * If neither @icon or @icon_name is set, the icon is taken from either 
+ * the file that is passed to launched application or from the #GAppInfo 
+ * for the launched application itself.
  * 
  * Since: 2.14
  */
