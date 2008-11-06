71f27b36de67fe83662cc0c5e98d2b13ed4da3b0
Tristan Van Berkom <tvb@src.gnome.org>
2008-11-06 17:03:35 +0000

    Fixed a crasher in GTK_BUILDER_WARN_INVALID_CHILD_TYPE()
    	* gtk/gtkbuilder.h: Fixed a crasher in GTK_BUILDER_WARN_INVALID_CHILD_TYPE()
    
    
    svn path=/trunk/; revision=21765

--- gtk/gtkbuilder.h
+++ gtk/gtkbuilder.h
@@ -135,7 +135,7 @@ gboolean     gtk_builder_value_from_string_type  (GtkBuilder    *builder,
 						  GError       **error);
 
 #define GTK_BUILDER_WARN_INVALID_CHILD_TYPE(object, type) \
-  g_warning ("'%s' is not a valid child type of '%s'", type, g_type_name (G_OBJECT_TYPE (type)))
+  g_warning ("'%s' is not a valid child type of '%s'", type, g_type_name (G_OBJECT_TYPE (object)))
 
 G_END_DECLS
 
