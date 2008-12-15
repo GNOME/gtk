7c568f8dd06eaa15764010830d13a66314438091
Matthias Clasen <matthiasc@src.gnome.org>
2008-12-15 19:52:26 +0000

    Fix exports
    svn path=/branches/gtk-2-14/; revision=21908

--- gtk/gtkprintunixdialog.c
+++ gtk/gtkprintunixdialog.c
@@ -1696,7 +1696,7 @@ update_range_sensitivity (GtkWidget *button,
   gtk_widget_set_sensitive (range, active);
 }
 
-void
+static void
 emit_ok_response (GtkTreeView       *tree_view,
                   GtkTreePath       *path,
                   GtkTreeViewColumn *column,
