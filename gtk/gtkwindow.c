e173e3f72b50258ee8922e7739304be1d24420b9
Cody Russell <crussell@canonical.com>
2009-07-08 15:02:24 +0100

    tooltip text on min/max/close buttons

--- gtk/gtkwindow.c
+++ gtk/gtkwindow.c
@@ -1561,6 +1561,7 @@ ensure_title_box (GtkWindow *window)
       gtk_button_set_relief (GTK_BUTTON (button), GTK_RELIEF_NONE);
       gtk_widget_set_name (button, "gtk-window-decorated-minimize-button");
       image = gtk_image_new_from_stock (GTK_STOCK_ZOOM_OUT, GTK_ICON_SIZE_MENU);
+      gtk_widget_set_tooltip_text (button, _("Minimize Window"));
       gtk_container_add (GTK_CONTAINER (button), image);
       gtk_box_pack_start (GTK_BOX (hbox), button, TRUE, TRUE, 0);
       priv->min_button = button;
@@ -1571,6 +1572,7 @@ ensure_title_box (GtkWindow *window)
       gtk_button_set_relief (GTK_BUTTON (button), GTK_RELIEF_NONE);
       gtk_widget_set_name (button, "gtk-window-decorated-maximize-button");
       image = gtk_image_new_from_stock (GTK_STOCK_ZOOM_IN, GTK_ICON_SIZE_MENU);
+      gtk_widget_set_tooltip_text (button, _("Maximize Window"));
       gtk_container_add (GTK_CONTAINER (button), image);
       gtk_box_pack_start (GTK_BOX (hbox), button, TRUE, TRUE, 0);
       priv->max_button = button;
@@ -1581,6 +1583,7 @@ ensure_title_box (GtkWindow *window)
       gtk_button_set_relief (GTK_BUTTON (button), GTK_RELIEF_NONE);
       gtk_widget_set_name (button, "gtk-window-decorated-close-button");
       image = gtk_image_new_from_stock (GTK_STOCK_CLOSE, GTK_ICON_SIZE_MENU);
+      gtk_widget_set_tooltip_text (button, _("Close Window"));
       gtk_container_add (GTK_CONTAINER (button), image);
       gtk_box_pack_start (GTK_BOX (hbox), button, TRUE, TRUE, 0);
       priv->close_button = button;
