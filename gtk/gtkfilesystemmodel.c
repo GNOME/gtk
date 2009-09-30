988e4f113456f171353f1208733f6f92adb78a7c
Federico Mena Quintero <federico@novell.com>
2009-09-30 18:49:33 -0500

    Fix thinkos in which fields get used
    Signed-off-by: Federico Mena Quintero <federico@novell.com>

--- gtk/gtkfilesystemmodel.c
+++ gtk/gtkfilesystemmodel.c
@@ -984,8 +984,8 @@ gtk_file_system_model_finalize (GObject *object)
       if (node->info)
         g_object_unref (node->info);
 
-      for (v = 0; v < model->column_types; v++)
-	if (G_VALUE_TYPE (node->values[v]) != G_TYPE_INVALID)
+      for (v = 0; v < model->n_columns; v++)
+	if (G_VALUE_TYPE (&node->values[v]) != G_TYPE_INVALID)
 	  g_value_unset (&node->values[v]);
     }
   g_array_free (model->files, TRUE);
@@ -1000,7 +1000,7 @@ gtk_file_system_model_finalize (GObject *object)
   if (model->filter)
     g_object_unref (model->filter);
 
-  g_slice_free1 (sizeof (GType) * n_columns, model->column_types);
+  g_slice_free1 (sizeof (GType) * model->n_columns, model->column_types);
 
   _gtk_tree_data_list_header_free (model->sort_list);
   if (model->default_sort_destroy)
