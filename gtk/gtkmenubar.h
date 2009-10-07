7d3dbb95329ee8a2b17ea81ef6ee61737a5e70d6
Christian Dywan <christian@twotoasts.de>
2009-10-07 17:55:58 +0200

    Remove deprecated GtkMenuBar functions (macros)

--- gtk/gtkmenubar.h
+++ gtk/gtkmenubar.h
@@ -75,12 +75,6 @@ GtkPackDirection gtk_menu_bar_get_child_pack_direction (GtkMenuBar       *menuba
 void             gtk_menu_bar_set_child_pack_direction (GtkMenuBar       *menubar,
 							GtkPackDirection  child_pack_dir);
 
-#ifndef GTK_DISABLE_DEPRECATED
-#define gtk_menu_bar_append(menu,child)	    gtk_menu_shell_append  ((GtkMenuShell *)(menu),(child))
-#define gtk_menu_bar_prepend(menu,child)    gtk_menu_shell_prepend ((GtkMenuShell *)(menu),(child))
-#define gtk_menu_bar_insert(menu,child,pos) gtk_menu_shell_insert ((GtkMenuShell *)(menu),(child),(pos))
-#endif /* GTK_DISABLE_DEPRECATED */
-
 /* Private functions */
 void _gtk_menu_bar_cycle_focus (GtkMenuBar       *menubar,
 				GtkDirectionType  dir);
