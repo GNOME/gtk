GNOME/gtk

commit 72b69e0094db5ac1f9bf38bea429b45bf2e449e3

Don't run system("touch conf.gtktest") in the AM_PATH_GTK_2_0 macro
Just use the C library instead to create the file. Helps building
using Wine. Not that I think we want to endorse that way to build GTK+
for Windows, proper cross-compiling surely probably more sense, but
accepting this minimal patch doesn't hurt. From bug #590017.

Signed-off-by: Tor Lillqvist <tml@iki.fi>

m4macros/gtk-2.0.m4
@@ -85,7 +85,7 @@ main ()
   int major, minor, micro;
   char *tmp_version;
 
-  system ("touch conf.gtktest");
+  fclose (fopen ("conf.gtktest", "w");
 
   /* HP/UX 9 (%@#!) writes to sscanf strings */
   tmp_version = g_strdup("$min_gtk_version");

commit 78acc785db13f45e82d565eda5715dc00182e45c

Fix a typo
This was introduced in commit 72b69e0094db5ac1f9bf38bea429b45bf2e449e3
Reported by BugByteMan here:
http://mail.gnome.org/archives/gtk-devel-list/2009-November/msg00014.html

m4macros/gtk-2.0.m4
@@ -85,7 +85,7 @@ main ()
   int major, minor, micro;
   char *tmp_version;
 
-  fclose (fopen ("conf.gtktest", "w");
+  fclose (fopen ("conf.gtktest", "w"));
 
   /* HP/UX 9 (%@#!) writes to sscanf strings */
   tmp_version = g_strdup("$min_gtk_version");