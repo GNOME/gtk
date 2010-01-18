b242c02e71f00e21ed6b1f5146035db75c607642
Matthias Clasen <mclasen@redhat.com>
2010-01-17 23:33:19 -0500

    Reset effective_attrs when necessary
    This fixes both bug 607217 and bug 607269.

--- gtk/gtklabel.c
+++ gtk/gtklabel.c
@@ -1865,23 +1865,21 @@ gtk_label_recalculate (GtkLabel *label)
   guint keyval = label->mnemonic_keyval;
 
   if (label->use_markup)
-    {
-      gtk_label_set_markup_internal (label, label->label, label->use_underline);
-      gtk_label_compose_effective_attrs (label);
-    }
+    gtk_label_set_markup_internal (label, label->label, label->use_underline);
   else
     {
       if (label->use_underline)
-	{
-	  gtk_label_set_uline_text_internal (label, label->label);
-	  gtk_label_compose_effective_attrs (label);
-	}
+	gtk_label_set_uline_text_internal (label, label->label);
       else
-	{
-	  gtk_label_set_text_internal (label, g_strdup (label->label));
-	  gtk_label_compose_effective_attrs (label);
-	}
+        {
+          if (label->effective_attrs)
+            pango_attr_list_unref (label->effective_attrs);
+          label->effective_attrs = NULL;
+          gtk_label_set_text_internal (label, g_strdup (label->label));
+        }
     }
+
+  gtk_label_compose_effective_attrs (label);
 
   if (!label->use_underline)
     label->mnemonic_keyval = GDK_VoidSymbol;
@@ -1949,7 +1947,7 @@ gtk_label_set_attributes (GtkLabel         *label,
 
   gtk_label_recalculate (label);
 
-  gtk_label_clear_layout (label);  
+  gtk_label_clear_layout (label);
   gtk_widget_queue_resize (GTK_WIDGET (label));
 }
 
