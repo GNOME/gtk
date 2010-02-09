5438a7b608f79147859970988a9702744fe5662b
Matthias Clasen <mclasen@redhat.com>
2010-02-09 13:40:58 -0500

    Add some more goddamn accessors

--- gtk/gtkwidget.h
+++ gtk/gtkwidget.h
@@ -880,6 +880,12 @@ void	gtk_widget_child_notify		  (GtkWidget	       *widget,
 					   const gchar	       *child_property);
 void	gtk_widget_thaw_child_notify	  (GtkWidget	       *widget);
 
+void       gtk_widget_set_realized        (GtkWidget           *widget,
+                                           gboolean             realized);
+gboolean   gtk_widget_get_realized        (GtkWidget           *widget);
+void       gtk_widget_set_mapped          (GtkWidget           *widget,
+                                           gboolean             mapped);
+gboolean   gtk_widget_get_mapped          (GtkWidget           *widget);
 void       gtk_widget_set_can_focus       (GtkWidget           *widget,
                                            gboolean             can_focus);
 gboolean   gtk_widget_get_can_focus       (GtkWidget           *widget);
@@ -922,6 +928,12 @@ gboolean              gtk_widget_get_has_window         (GtkWidget    *widget);
 
 gboolean              gtk_widget_is_toplevel            (GtkWidget    *widget);
 gboolean              gtk_widget_is_drawable            (GtkWidget    *widget);
+void                  gtk_widget_set_realized           (GtkWidget    *widget,
+                                                         gboolean      realized);
+gboolean              gtk_widget_get_realized           (GtkWidget    *widget);
+void                  gtk_widget_set_mapped             (GtkWidget    *widget,
+                                                         gboolean      mapped);
+gboolean              gtk_widget_get_mapped             (GtkWidget    *widget);
 
 void                  gtk_widget_set_app_paintable      (GtkWidget    *widget,
 							 gboolean      app_paintable);
