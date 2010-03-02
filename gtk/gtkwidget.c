GNOME/gtk

commit a27d5a2c9eba7af5b056de32ff9b2b4dd1eb97e1

Deprecate widget flag: GTK_WIDGET_IS_SENSITIVE
Use gtk_widget_is_sensitive() instead

https://bugzilla.gnome.org/show_bug.cgi?id=69872

gtk/gtkwidget.c
@@ -4284,7 +4284,9 @@ gtk_widget_real_can_activate_accel (GtkWidget *widget,
                                     guint      signal_id)
 {
   /* widgets must be onscreen for accels to take effect */
-  return GTK_WIDGET_IS_SENSITIVE (widget) && gtk_widget_is_drawable (widget) && gdk_window_is_viewable (widget->window);
+  return gtk_widget_is_sensitive (widget) &&
+         gtk_widget_is_drawable (widget) &&
+         gdk_window_is_viewable (widget->window);
 }
 
 /**
@@ -4640,7 +4642,7 @@ gtk_widget_mnemonic_activate (GtkWidget *widget,
   g_return_val_if_fail (GTK_IS_WIDGET (widget), FALSE);
 
   group_cycling = group_cycling != FALSE;
-  if (!GTK_WIDGET_IS_SENSITIVE (widget))
+  if (!gtk_widget_is_sensitive (widget))
     handled = TRUE;
   else
     g_signal_emit (widget,
@@ -5253,7 +5255,7 @@ gtk_widget_grab_focus (GtkWidget *widget)
 {
   g_return_if_fail (GTK_IS_WIDGET (widget));
 
-  if (!GTK_WIDGET_IS_SENSITIVE (widget))
+  if (!gtk_widget_is_sensitive (widget))
     return;
   
   g_object_ref (widget);
@@ -5780,7 +5782,7 @@ gtk_widget_set_state (GtkWidget           *widget,
       data.state_restoration = FALSE;
       data.use_forall = FALSE;
       if (widget->parent)
-	data.parent_sensitive = (GTK_WIDGET_IS_SENSITIVE (widget->parent) != FALSE);
+	data.parent_sensitive = (gtk_widget_is_sensitive (widget->parent) != FALSE);
       else
 	data.parent_sensitive = TRUE;
 
@@ -6235,7 +6237,7 @@ gtk_widget_set_sensitive (GtkWidget *widget,
   data.use_forall = TRUE;
 
   if (widget->parent)
-    data.parent_sensitive = (GTK_WIDGET_IS_SENSITIVE (widget->parent) != FALSE);
+    data.parent_sensitive = (gtk_widget_is_sensitive (widget->parent) != FALSE);
   else
     data.parent_sensitive = TRUE;
 
@@ -6284,7 +6286,7 @@ gtk_widget_is_sensitive (GtkWidget *widget)
 {
   g_return_val_if_fail (GTK_IS_WIDGET (widget), FALSE);
 
-  return GTK_WIDGET_IS_SENSITIVE (widget);
+  return gtk_widget_is_sensitive (widget);
 }
 
 /**
@@ -6330,8 +6332,8 @@ gtk_widget_set_parent (GtkWidget *widget,
   else
     data.state = GTK_WIDGET_STATE (widget);
   data.state_restoration = FALSE;
-  data.parent_sensitive = (GTK_WIDGET_IS_SENSITIVE (parent) != FALSE);
-  data.use_forall = GTK_WIDGET_IS_SENSITIVE (parent) != GTK_WIDGET_IS_SENSITIVE (widget);
+  data.parent_sensitive = (gtk_widget_is_sensitive (parent) != FALSE);
+  data.use_forall = gtk_widget_is_sensitive (parent) != gtk_widget_is_sensitive (widget);
 
   gtk_widget_propagate_state (widget, &data);
   
@@ -7646,7 +7648,7 @@ gtk_widget_child_focus (GtkWidget       *widget,
   g_return_val_if_fail (GTK_IS_WIDGET (widget), FALSE);
 
   if (!GTK_WIDGET_VISIBLE (widget) ||
-      !GTK_WIDGET_IS_SENSITIVE (widget))
+      !gtk_widget_is_sensitive (widget))
     return FALSE;
 
   /* child widgets must set CAN_FOCUS, containers
@@ -9169,7 +9171,7 @@ gtk_widget_propagate_state (GtkWidget           *widget,
   else
     GTK_WIDGET_UNSET_FLAGS (widget, GTK_PARENT_SENSITIVE);
 
-  if (GTK_WIDGET_IS_SENSITIVE (widget))
+  if (gtk_widget_is_sensitive (widget))
     {
       if (data->state_restoration)
         GTK_WIDGET_STATE (widget) = widget->saved_state;
@@ -9188,7 +9190,7 @@ gtk_widget_propagate_state (GtkWidget           *widget,
       GTK_WIDGET_STATE (widget) = GTK_STATE_INSENSITIVE;
     }
 
-  if (gtk_widget_is_focus (widget) && !GTK_WIDGET_IS_SENSITIVE (widget))
+  if (gtk_widget_is_focus (widget) && !gtk_widget_is_sensitive (widget))
     {
       GtkWidget *window;
 
@@ -9202,14 +9204,14 @@ gtk_widget_propagate_state (GtkWidget           *widget,
     {
       g_object_ref (widget);
 
-      if (!GTK_WIDGET_IS_SENSITIVE (widget) && gtk_widget_has_grab (widget))
+      if (!gtk_widget_is_sensitive (widget) && gtk_widget_has_grab (widget))
 	gtk_grab_remove (widget);
 
       g_signal_emit (widget, widget_signals[STATE_CHANGED], 0, old_state);
 
       if (GTK_WIDGET_HAS_POINTER (widget) && !GTK_WIDGET_SHADOWED (widget))
 	{
-	  if (!GTK_WIDGET_IS_SENSITIVE (widget))
+	  if (!gtk_widget_is_sensitive (widget))
 	    _gtk_widget_synthesize_crossing (widget, NULL, 
 					     GDK_CROSSING_STATE_CHANGED);
 	  else if (old_state == GTK_STATE_INSENSITIVE)
@@ -9219,7 +9221,7 @@ gtk_widget_propagate_state (GtkWidget           *widget,
 
       if (GTK_IS_CONTAINER (widget))
 	{
-	  data->parent_sensitive = (GTK_WIDGET_IS_SENSITIVE (widget) != FALSE);
+	  data->parent_sensitive = (gtk_widget_is_sensitive (widget) != FALSE);
 	  if (data->use_forall)
 	    gtk_container_forall (GTK_CONTAINER (widget),
 				  (GtkCallback) gtk_widget_propagate_state,

gtk/gtkbutton.c
@@ -2180,7 +2180,7 @@ gtk_button_state_changed (GtkWidget    *widget,
 {
   GtkButton *button = GTK_BUTTON (widget);
 
-  if (!GTK_WIDGET_IS_SENSITIVE (widget))
+  if (!gtk_widget_is_sensitive (widget))
     {
       button->in_button = FALSE;
       gtk_real_button_released (button);

gtk/gtkcalendar.c
@@ -1433,7 +1433,7 @@ calendar_realize_arrows (GtkCalendar *calendar)
 	  priv->arrow_win[i] = gdk_window_new (priv->header_win,
 					       &attributes, 
 					       attributes_mask);
-	  if (GTK_WIDGET_IS_SENSITIVE (widget))
+	  if (gtk_widget_is_sensitive (widget))
 	    priv->arrow_state[i] = GTK_STATE_NORMAL;
 	  else 
 	    priv->arrow_state[i] = GTK_STATE_INSENSITIVE;
@@ -3261,14 +3261,14 @@ gtk_calendar_state_changed (GtkWidget	   *widget,
   GtkCalendarPrivate *priv = GTK_CALENDAR_GET_PRIVATE (widget);
   int i;
   
-  if (!GTK_WIDGET_IS_SENSITIVE (widget))
+  if (!gtk_widget_is_sensitive (widget))
     {
       priv->in_drag = 0;
       calendar_stop_spinning (calendar);    
     }
 
   for (i = 0; i < 4; i++)
-    if (GTK_WIDGET_IS_SENSITIVE (widget))
+    if (gtk_widget_is_sensitive (widget))
       priv->arrow_state[i] = GTK_STATE_NORMAL;
     else 
       priv->arrow_state[i] = GTK_STATE_INSENSITIVE;

gtk/gtkcheckbutton.c
@@ -390,7 +390,7 @@ gtk_real_check_button_draw_indicator (GtkCheckButton *check_button,
 	state_type = GTK_STATE_ACTIVE;
       else if (button->in_button)
 	state_type = GTK_STATE_PRELIGHT;
-      else if (!GTK_WIDGET_IS_SENSITIVE (widget))
+      else if (!gtk_widget_is_sensitive (widget))
 	state_type = GTK_STATE_INSENSITIVE;
       else
 	state_type = GTK_STATE_NORMAL;

gtk/gtkcheckmenuitem.c
@@ -488,7 +488,7 @@ gtk_real_check_menu_item_draw_indicator (GtkCheckMenuItem *check_menu_item,
 	  else 
 	    shadow_type = GTK_SHADOW_OUT;
 	  
-	  if (!GTK_WIDGET_IS_SENSITIVE (widget))
+	  if (!gtk_widget_is_sensitive (widget))
 	    state_type = GTK_STATE_INSENSITIVE;
 
 	  if (check_menu_item->draw_as_radio)

gtk/gtkclist.c
@@ -6447,7 +6447,7 @@ gtk_clist_focus (GtkWidget        *widget,
   GtkWidget *focus_child;
   gboolean is_current_focus;
 
-  if (!GTK_WIDGET_IS_SENSITIVE (widget))
+  if (!gtk_widget_is_sensitive (widget))
     return FALSE;
 
   focus_child = GTK_CONTAINER (widget)->focus_child;

gtk/gtkcolorbutton.c
@@ -319,7 +319,7 @@ render (GtkColorButton *color_button)
       
     for (i = 0; i < width; i++) 
       {
-        if (!GTK_WIDGET_IS_SENSITIVE (GTK_WIDGET (color_button)) && (i+j)%2) 
+        if (!gtk_widget_is_sensitive (GTK_WIDGET (color_button)) && (i+j)%2)
           {
             *(pixels + j * rowstride + i * 3) = insensitive_r;
             *(pixels + j * rowstride + i * 3 + 1) = insensitive_g;

gtk/gtkcurve.c
@@ -380,7 +380,7 @@ gtk_curve_draw (GtkCurve *c, gint width, gint height)
     gtk_curve_interpolate (c, width, height);
 
   state = GTK_STATE_NORMAL;
-  if (!GTK_WIDGET_IS_SENSITIVE (GTK_WIDGET (c)))
+  if (!gtk_widget_is_sensitive (GTK_WIDGET (c)))
     state = GTK_STATE_INSENSITIVE;
 
   style = GTK_WIDGET (c)->style;

commit f8fb228752a2873d5395fe71981c8dc57ca2a301

Fix gtk_widget_is_sensitive() function
The bug was introduced in
commit a27d5a2c9eba7af5b056de32ff9b2b4dd1eb97e1

gtk/gtkwidget.c
@@ -6286,7 +6286,8 @@ gtk_widget_is_sensitive (GtkWidget *widget)
 {
   g_return_val_if_fail (GTK_IS_WIDGET (widget), FALSE);
 
-  return gtk_widget_is_sensitive (widget);
+  return (gtk_widget_get_sensitive (widget) &&
+          gtk_widget_get_sensitive (widget->parent));
 }
 
 /**