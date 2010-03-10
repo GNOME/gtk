6927a24df467c112e14cf54e218433e7586cd6af
Javier Jardón <jjardon@gnome.org>
2010-03-10 23:28:45 +0100

    Restore gtk_paned_get_handle_window()

--- gtk/gtkpaned.h
+++ gtk/gtkpaned.h
@@ -127,6 +127,8 @@ void        gtk_paned_set_position (GtkPaned       *paned,
 GtkWidget * gtk_paned_get_child1   (GtkPaned       *paned);
 GtkWidget * gtk_paned_get_child2   (GtkPaned       *paned);
 
+GdkWindow * gtk_paned_get_handle_window (GtkPaned  *paned);
+
 
 G_END_DECLS
 
