bb86e61171bba1853c04366535b9df949fae4952
Tristan Van Berkom <tristan.van.berkom@gmail.com>
2010-04-06 03:03:03 -0400

    Added GtkExtendedCell interface (last commit)

--- gtk/gtk.h
+++ gtk/gtk.h
@@ -83,6 +83,7 @@
 #include <gtk/gtkenums.h>
 #include <gtk/gtkeventbox.h>
 #include <gtk/gtkexpander.h>
+#include <gtk/gtkextendedcell.h>
 #include <gtk/gtkextendedlayout.h>
 #include <gtk/gtkfixed.h>
 #include <gtk/gtkfilechooser.h>
