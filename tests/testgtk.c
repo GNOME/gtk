6536ad7299cd94b78132d93c05f5ec5351ade8b4
Christian Dywan <christian@twotoasts.de>
2010-04-06 17:45:56 +0200

    Use new 2.14 GtkColorSelection API in testgtk

--- tests/testgtk.c
+++ tests/testgtk.c
@@ -5692,24 +5692,25 @@ void
 color_selection_ok (GtkWidget               *w,
                     GtkColorSelectionDialog *cs)
 {
-  GtkColorSelection *colorsel;
-  gdouble color[4];
+  GtkWidget *colorsel;
+  GdkColor color;
 
-  colorsel=GTK_COLOR_SELECTION(cs->colorsel);
+  colorsel = gtk_color_selection_dialog_get_color_selection (cs);
 
-  gtk_color_selection_get_color(colorsel,color);
-  gtk_color_selection_set_color(colorsel,color);
+  gtk_color_selection_get_current_color (GTK_COLOR_SELECTION (colorsel), &color);
+  gtk_color_selection_set_current_color (GTK_COLOR_SELECTION (colorsel), &color);
 }
 
 void
 color_selection_changed (GtkWidget *w,
                          GtkColorSelectionDialog *cs)
 {
-  GtkColorSelection *colorsel;
-  gdouble color[4];
+  GtkWidget *colorsel;
+  GdkColor color;
 
-  colorsel=GTK_COLOR_SELECTION(cs->colorsel);
-  gtk_color_selection_get_color(colorsel,color);
+  colorsel = gtk_color_selection_dialog_get_color_selection (cs);
+  gtk_color_selection_get_current_color (GTK_COLOR_SELECTION (colorsel), &color);
+  gtk_color_selection_set_current_color (GTK_COLOR_SELECTION (colorsel), &color);
 }
 
 #if 0 /* unused */
