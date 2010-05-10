39f3d5e182a8b134a48b8dfcb4329dbe7d7e75f2
Sebastian Dröge <sebastian.droege@collabora.co.uk>
2010-05-10 09:43:42 +0200

    Don't include __bss_start, _edata and _end symbols in the abichecks
    They are added by the binutils gold linker.

--- gdk/abicheck.sh
+++ gdk/abicheck.sh
@@ -1,5 +1,5 @@
 #! /bin/sh
 
 cpp -DINCLUDE_VARIABLES -P -DALL_FILES -DGDK_ENABLE_BROKEN -DGDK_WINDOWING_X11 ${srcdir:-.}/gdk.symbols | sed -e '/^$/d' -e 's/ G_GNUC.*$//' | sort | uniq > expected-abi
-nm -D -g --defined-only .libs/libgdk-x11-2.0.so | cut -d ' ' -f 3 | sort > actual-abi
+nm -D -g --defined-only .libs/libgdk-x11-2.0.so | cut -d ' ' -f 3 | egrep -v '^(__bss_start|_edata|_end)' | sort > actual-abi
 diff -u expected-abi actual-abi && rm -f expected-abi actual-abi
