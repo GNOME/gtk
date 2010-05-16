98e773d93def3e9c24ae040d256f81ba16491ce8
Javier Jardón <jjardon@gnome.org>
2010-05-16 02:31:45 +0200

    Move documentation to inline comments: GtkSeparatorToolItem

--- gtk/gtkseparatortoolitem.c
+++ gtk/gtkseparatortoolitem.c
@@ -27,6 +27,26 @@
 #include "gtkprivate.h"
 #include "gtkalias.h"
 
+
+/**
+ * SECTION:gtkseparatortoolitem
+ * @Short_description: A toolbar item that separates groups of other
+ *   toolbar items
+ * @Title: GtkSeparatorToolItem
+ * @See_also: #GtkToolbar, #GtkRadioToolButton
+ *
+ * A #GtkSeparatorItem is a #GtkToolItem that separates groups of other
+ * #GtkToolItems. Depending on the theme, a #GtkSeparatorToolItem will
+ * often look like a vertical line on horizontally docked toolbars.
+ *
+ * If the #GtkToolbar child property "expand" is %TRUE and the property
+ * #GtkSeparatorToolItem:draw is %FALSE, a #GtkSeparatorToolItem will act as
+ * a "spring" that forces other items to the ends of the toolbar.
+ *
+ * Use gtk_separator_tool_item_new() to create a new #GtkSeparatorToolItem.
+ */
+
+
 #define MENU_ID "gtk-separator-tool-item-menu-id"
 
 enum {
