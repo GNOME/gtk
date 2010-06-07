4212943079045a3f49cbc05b9c71f76fff7070d5
Carlos Garnacho <carlosg@gnome.org>
2010-06-07 15:32:05 +0200

    Turn on multidevice support by default.
    gdk_enable_multidevice() has been replaced with gdk_disable_multidevice(), although
    there would be usually little reasons to call that function, unless the application
    is doing X calls itself that count on old fashioned core devices.

--- gdk/gdk.h
+++ gdk/gdk.h
@@ -66,7 +66,7 @@ G_BEGIN_DECLS
 /* Initialization, exit and events
  */
 #define	  GDK_PRIORITY_EVENTS		(G_PRIORITY_DEFAULT)
-void      gdk_enable_multidevice        (void);
+void      gdk_disable_multidevice       (void);
 void 	  gdk_parse_args	   	(gint	   	*argc,
 					 gchar        ***argv);
 void 	  gdk_init		   	(gint	   	*argc,
