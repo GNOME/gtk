2738786c6f332df9962a9c3ef4a953ea83356d37
Federico Mena Quintero <federico@novell.com>
2010-06-24 15:00:44 -0500

    Remove superfluous NULL check
    Now that _gtk_file_chooser_uri_has_prefix() deals with NULL prefix lists,
    we can remove all such checks.
    
    Signed-off-by: Federico Mena Quintero <federico@novell.com>

--- gtk/gtkfilechooserentry.c
+++ gtk/gtkfilechooserentry.c
@@ -1488,9 +1488,8 @@ start_loading_current_folder (GtkFileChooserEntry *chooser_entry)
   g_assert (chooser_entry->load_folder_cancellable == NULL);
 
   if ((chooser_entry->local_only
-       && !g_file_is_native (chooser_entry->current_folder_file)) ||
-      (chooser_entry->root_uris != NULL
-       && !is_file_in_roots (chooser_entry, chooser_entry->current_folder_file)))
+       && !g_file_is_native (chooser_entry->current_folder_file))
+      || !is_file_in_roots (chooser_entry, chooser_entry->current_folder_file))
     {
       g_object_unref (chooser_entry->current_folder_file);
       chooser_entry->current_folder_file = NULL;
