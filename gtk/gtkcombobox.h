922324553be10f847d4bdc3a177e641887a2921d
Ryan Lortie <desrt@desrt.ca>
2010-11-30 00:07:08 -0500

    Bug 619148 - "active ID" properties (GtkComboBox)
    Based on a patch by Matthias, add an "active-id" property for
    GtkComboBox and add some convenience API for GtkComboBoxText.
    
    Also, add a test case to gtk-demo.

--- gtk/gtkcombobox.h
+++ gtk/gtkcombobox.h
@@ -134,6 +134,12 @@ void          gtk_combo_box_popup_for_device (GtkComboBox     *combo_box,
 void          gtk_combo_box_popdown          (GtkComboBox     *combo_box);
 AtkObject*    gtk_combo_box_get_popup_accessible (GtkComboBox *combo_box);
 
+gint          gtk_combo_box_get_id_column        (GtkComboBox *combo_box);
+void          gtk_combo_box_set_id_column        (GtkComboBox *combo_box,
+                                                  gint         id_column);
+const gchar * gtk_combo_box_get_active_id        (GtkComboBox *combo_box);
+void          gtk_combo_box_set_active_id        (GtkComboBox *combo_box,
+                                                  const gchar *active_id);
 
 G_END_DECLS
 
