a66f095b62c19e768e848b119ec1a49c22aedc53
Javier Jardón <jjardon@gnome.org>
2010-12-15 23:57:13 +0000

    demos/gtk-demo/clipboard.c: Use accessor functions to access GtkSelectionData

--- demos/gtk-demo/clipboard.c
+++ demos/gtk-demo/clipboard.c
@@ -125,7 +125,7 @@ drag_data_received (GtkWidget        *widget,
 {
   GdkPixbuf *pixbuf;
 
-  if (selection_data->length > 0)
+  if (gtk_selection_data_get_length (selection_data) > 0)
     {
       pixbuf = gtk_selection_data_get_pixbuf (selection_data);
       gtk_image_set_from_pixbuf (GTK_IMAGE (data), pixbuf);
