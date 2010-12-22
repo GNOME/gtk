b447cd972ef60f77e1aaf86cee37da16ab036dcb
Matthias Clasen <mclasen@redhat.com>
2010-12-22 14:52:50 -0500

    Fix a typo in GtkCombo
    There is no GtkButton::depressed signal.
    Reported by Nicolas Setton in bug 633089.

--- gtk/gtkcombo.c
+++ gtk/gtkcombo.c
@@ -668,7 +668,7 @@ gtk_combo_popup_button_press (GtkWidget        *button,
   popup_grab_on_window (combo->popwin->window,
 			gtk_get_current_event_time ());
 
-  g_signal_emit_by_name (button, "depressed");
+  g_signal_emit_by_name (button, "pressed");
 
   gtk_grab_add (combo->popwin);
 
