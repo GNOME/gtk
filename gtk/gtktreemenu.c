185744d4029f23840e9cd99e44c9c7afb6bc3e85
Tristan Van Berkom <tristan.van.berkom@gmail.com>
2010-12-30 17:35:44 +0900

    Mentioned that GtkTreeMenu is based on some GtkComboBox code in the copyright header.

--- gtk/gtktreemenu.c
+++ gtk/gtktreemenu.c
@@ -5,6 +5,8 @@
  * Authors:
  *      Tristan Van Berkom <tristanvb@openismus.com>
  *
+ * Based on some GtkComboBox menu code by Kristian Rietveld <kris@gtk.org>
+ *
  * This library is free software; you can redistribute it and/or
  * modify it under the terms of the GNU Library General Public
  * License as published by the Free Software Foundation; either
