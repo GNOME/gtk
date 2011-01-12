b7caeb7adbf9dc14cf3ce6733d100eb35c5d381c
Carlos Garnacho <carlosg@gnome.org>
2011-01-12 20:29:08 +0100

    Fix rendering glitch in menu radiobuttons
    The arcs needed a new subpath.

--- gtk/gtkthemingengine.c
+++ gtk/gtkthemingengine.c
@@ -1211,6 +1211,8 @@ gtk_theming_engine_render_option (GtkThemingEngine *engine,
   if (border_style == GTK_BORDER_STYLE_SOLID)
     {
       cairo_set_line_width (cr, border_width);
+
+      cairo_new_sub_path (cr);
       cairo_arc (cr,
 		 x + exterior_size / 2.,
 		 y + exterior_size / 2.,
@@ -1266,6 +1268,7 @@ gtk_theming_engine_render_option (GtkThemingEngine *engine,
 	  pad = MAX (0, (exterior_size - interior_size) / 2);
 	}
 
+      cairo_new_sub_path (cr);
       cairo_arc (cr,
 		 x + pad + interior_size / 2.,
 		 y + pad + interior_size / 2.,
