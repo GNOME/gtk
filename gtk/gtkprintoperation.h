242c2a389d04d8b099e4942371318a4cd1e33f8e
Matthias Clasen <mclasen@redhat.com>
2011-01-30 02:36:17 -0500

    Whitespace fixes

--- gtk/gtkprintoperation.h
+++ gtk/gtkprintoperation.h
@@ -37,12 +37,12 @@
 
 G_BEGIN_DECLS
 
-#define GTK_TYPE_PRINT_OPERATION		(gtk_print_operation_get_type ())
-#define GTK_PRINT_OPERATION(obj)		(G_TYPE_CHECK_INSTANCE_CAST ((obj), GTK_TYPE_PRINT_OPERATION, GtkPrintOperation))
-#define GTK_PRINT_OPERATION_CLASS(klass)    	(G_TYPE_CHECK_CLASS_CAST ((klass), GTK_TYPE_PRINT_OPERATION, GtkPrintOperationClass))
-#define GTK_IS_PRINT_OPERATION(obj) 		(G_TYPE_CHECK_INSTANCE_TYPE ((obj), GTK_TYPE_PRINT_OPERATION))
-#define GTK_IS_PRINT_OPERATION_CLASS(klass)	(G_TYPE_CHECK_CLASS_TYPE ((klass), GTK_TYPE_PRINT_OPERATION))
-#define GTK_PRINT_OPERATION_GET_CLASS(obj)	(G_TYPE_INSTANCE_GET_CLASS ((obj), GTK_TYPE_PRINT_OPERATION, GtkPrintOperationClass))
+#define GTK_TYPE_PRINT_OPERATION                (gtk_print_operation_get_type ())
+#define GTK_PRINT_OPERATION(obj)                (G_TYPE_CHECK_INSTANCE_CAST ((obj), GTK_TYPE_PRINT_OPERATION, GtkPrintOperation))
+#define GTK_PRINT_OPERATION_CLASS(klass)        (G_TYPE_CHECK_CLASS_CAST ((klass), GTK_TYPE_PRINT_OPERATION, GtkPrintOperationClass))
+#define GTK_IS_PRINT_OPERATION(obj)             (G_TYPE_CHECK_INSTANCE_TYPE ((obj), GTK_TYPE_PRINT_OPERATION))
+#define GTK_IS_PRINT_OPERATION_CLASS(klass)     (G_TYPE_CHECK_CLASS_TYPE ((klass), GTK_TYPE_PRINT_OPERATION))
+#define GTK_PRINT_OPERATION_GET_CLASS(obj)      (G_TYPE_INSTANCE_GET_CLASS ((obj), GTK_TYPE_PRINT_OPERATION, GtkPrintOperationClass))
 
 typedef struct _GtkPrintOperationClass   GtkPrintOperationClass;
 typedef struct _GtkPrintOperationPrivate GtkPrintOperationPrivate;
@@ -132,30 +132,30 @@ struct _GtkPrintOperationClass
   GObjectClass parent_class;
 
   void     (*done)               (GtkPrintOperation *operation,
-				  GtkPrintOperationResult result);
+                                  GtkPrintOperationResult result);
   void     (*begin_print)        (GtkPrintOperation *operation,
-				  GtkPrintContext   *context);
+                                  GtkPrintContext   *context);
   gboolean (*paginate)           (GtkPrintOperation *operation,
-				  GtkPrintContext   *context);
+                                  GtkPrintContext   *context);
   void     (*request_page_setup) (GtkPrintOperation *operation,
-				  GtkPrintContext   *context,
-				  gint               page_nr,
-				  GtkPageSetup      *setup);
+                                  GtkPrintContext   *context,
+                                  gint               page_nr,
+                                  GtkPageSetup      *setup);
   void     (*draw_page)          (GtkPrintOperation *operation,
-				  GtkPrintContext   *context,
-				  gint               page_nr);
+                                  GtkPrintContext   *context,
+                                  gint               page_nr);
   void     (*end_print)          (GtkPrintOperation *operation,
-				  GtkPrintContext   *context);
+                                  GtkPrintContext   *context);
   void     (*status_changed)     (GtkPrintOperation *operation);
 
   GtkWidget *(*create_custom_widget) (GtkPrintOperation *operation);
   void       (*custom_widget_apply)  (GtkPrintOperation *operation,
-				      GtkWidget         *widget);
+                                      GtkWidget         *widget);
 
-  gboolean (*preview)	     (GtkPrintOperation        *operation,
-			      GtkPrintOperationPreview *preview,
-			      GtkPrintContext          *context,
-			      GtkWindow                *parent);
+  gboolean (*preview)        (GtkPrintOperation        *operation,
+                              GtkPrintOperationPreview *preview,
+                              GtkPrintContext          *context,
+                              GtkWindow                *parent);
 
   void     (*update_custom_widget) (GtkPrintOperation *operation,
                                     GtkWidget         *widget,
@@ -204,37 +204,37 @@ GQuark gtk_print_error_quark (void);
 GType                   gtk_print_operation_get_type               (void) G_GNUC_CONST;
 GtkPrintOperation *     gtk_print_operation_new                    (void);
 void                    gtk_print_operation_set_default_page_setup (GtkPrintOperation  *op,
-								    GtkPageSetup       *default_page_setup);
+                                                                    GtkPageSetup       *default_page_setup);
 GtkPageSetup *          gtk_print_operation_get_default_page_setup (GtkPrintOperation  *op);
 void                    gtk_print_operation_set_print_settings     (GtkPrintOperation  *op,
-								    GtkPrintSettings   *print_settings);
+                                                                    GtkPrintSettings   *print_settings);
 GtkPrintSettings *      gtk_print_operation_get_print_settings     (GtkPrintOperation  *op);
 void                    gtk_print_operation_set_job_name           (GtkPrintOperation  *op,
-								    const gchar        *job_name);
+                                                                    const gchar        *job_name);
 void                    gtk_print_operation_set_n_pages            (GtkPrintOperation  *op,
-								    gint                n_pages);
+                                                                    gint                n_pages);
 void                    gtk_print_operation_set_current_page       (GtkPrintOperation  *op,
-								    gint                current_page);
+                                                                    gint                current_page);
 void                    gtk_print_operation_set_use_full_page      (GtkPrintOperation  *op,
-								    gboolean            full_page);
+                                                                    gboolean            full_page);
 void                    gtk_print_operation_set_unit               (GtkPrintOperation  *op,
-								    GtkUnit             unit);
+                                                                    GtkUnit             unit);
 void                    gtk_print_operation_set_export_filename    (GtkPrintOperation  *op,
-								    const gchar        *filename);
+                                                                    const gchar        *filename);
 void                    gtk_print_operation_set_track_print_status (GtkPrintOperation  *op,
-								    gboolean            track_status);
+                                                                    gboolean            track_status);
 void                    gtk_print_operation_set_show_progress      (GtkPrintOperation  *op,
-								    gboolean            show_progress);
+                                                                    gboolean            show_progress);
 void                    gtk_print_operation_set_allow_async        (GtkPrintOperation  *op,
-								    gboolean            allow_async);
+                                                                    gboolean            allow_async);
 void                    gtk_print_operation_set_custom_tab_label   (GtkPrintOperation  *op,
-								    const gchar        *label);
+                                                                    const gchar        *label);
 GtkPrintOperationResult gtk_print_operation_run                    (GtkPrintOperation  *op,
-								    GtkPrintOperationAction action,
-								    GtkWindow          *parent,
-								    GError            **error);
+                                                                    GtkPrintOperationAction action,
+                                                                    GtkWindow          *parent,
+                                                                    GError            **error);
 void                    gtk_print_operation_get_error              (GtkPrintOperation  *op,
-								    GError            **error);
+                                                                    GError            **error);
 GtkPrintStatus          gtk_print_operation_get_status             (GtkPrintOperation  *op);
 G_CONST_RETURN gchar *  gtk_print_operation_get_status_string      (GtkPrintOperation  *op);
 gboolean                gtk_print_operation_is_finished            (GtkPrintOperation  *op);
@@ -248,31 +248,32 @@ void                    gtk_print_operation_set_has_selection      (GtkPrintOper
                                                                     gboolean            has_selection);
 gboolean                gtk_print_operation_get_has_selection      (GtkPrintOperation  *op);
 void                    gtk_print_operation_set_embed_page_setup   (GtkPrintOperation  *op,
- 								    gboolean            embed);
+                                                                    gboolean            embed);
 gboolean                gtk_print_operation_get_embed_page_setup   (GtkPrintOperation  *op);
 gint                    gtk_print_operation_get_n_pages_to_print   (GtkPrintOperation  *op);
 
 GtkPageSetup           *gtk_print_run_page_setup_dialog            (GtkWindow          *parent,
-								    GtkPageSetup       *page_setup,
-								    GtkPrintSettings   *settings);
+                                                                    GtkPageSetup       *page_setup,
+                                                                    GtkPrintSettings   *settings);
 
 /**
  * GtkPageSetupDoneFunc:
  * @page_setup: the #GtkPageSetup that has been
- * @data: (closure): user data that has been passed to 
- *        gtk_print_run_page_setup_dialog_async().
+ * @data: (closure): user data that has been passed to
+ *     gtk_print_run_page_setup_dialog_async()
  *
  * The type of function that is passed to
- * gtk_print_run_page_setup_dialog_async().  This function will be
- * called when the page setup dialog is dismissed, and also serves as
- * destroy notify for @data.
+ * gtk_print_run_page_setup_dialog_async().
+ *
+ * This function will be called when the page setup dialog
+ * is dismissed, and also serves as destroy notify for @data.
  */
 typedef void  (* GtkPageSetupDoneFunc) (GtkPageSetup *page_setup,
-					gpointer      data);
+                                        gpointer      data);
 
 void                    gtk_print_run_page_setup_dialog_async      (GtkWindow            *parent,
-								    GtkPageSetup         *page_setup,
-								    GtkPrintSettings     *settings,
+                                                                    GtkPageSetup         *page_setup,
+                                                                    GtkPrintSettings     *settings,
                                                                     GtkPageSetupDoneFunc  done_cb,
                                                                     gpointer              data);
 
