GNOME/gtk

commit b04f4c536aeab4281a9f0117e67accae19d691b9

notebook: Don't set notebook style with hidden tabs
When tabs are not shown, and the notebook is used as a container,
we should not set the notebook style, otherwise the theme has no
way to know whether tabs are shown or not, and which colour to draw
the background.

https://bugzilla.gnome.org/show_bug.cgi?id=640692

gtk/gtknotebook.c
@@ -7078,6 +7078,7 @@ gtk_notebook_set_show_tabs (GtkNotebook *notebook,
 {
   GtkNotebookPrivate *priv;
   GtkNotebookPage *page;
+  GtkStyleContext *context;
   GList *children;
   gint i;
 
@@ -7092,6 +7093,7 @@ gtk_notebook_set_show_tabs (GtkNotebook *notebook,
 
   priv->show_tabs = show_tabs;
   children = priv->children;
+  context = gtk_widget_get_style_context (GTK_WIDGET (notebook));
 
   if (!show_tabs)
     {
@@ -7109,11 +7111,14 @@ gtk_notebook_set_show_tabs (GtkNotebook *notebook,
           else
             gtk_widget_hide (page->tab_label);
         }
+
+      gtk_style_context_remove_class (context, GTK_STYLE_CLASS_NOTEBOOK);
     }
   else
     {
       gtk_widget_set_can_focus (GTK_WIDGET (notebook), TRUE);
       gtk_notebook_update_labels (notebook);
+      gtk_style_context_add_class (context, GTK_STYLE_CLASS_NOTEBOOK);
     }
 
   for (i = 0; i < N_ACTION_WIDGETS; i++)

commit 0bc9efd230c359e2becff818739ba6310396759d

notebook: don't forget to reset the style after setting show-tabs
This fixes a regression after commit
b04f4c536aeab4281a9f0117e67accae19d691b9

gtk/gtknotebook.c
@@ -7127,6 +7127,7 @@ gtk_notebook_set_show_tabs (GtkNotebook *notebook,
         gtk_widget_set_child_visible (priv->action_widget[i], show_tabs);
     }
 
+  gtk_widget_reset_style (GTK_WIDGET (notebook));
   gtk_widget_queue_resize (GTK_WIDGET (notebook));
 
   g_object_notify (G_OBJECT (notebook), "show-tabs");