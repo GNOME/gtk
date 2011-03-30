GNOME/gtk

commit 9992efdb0e7d0cf52e47817fe049b97967400731

label: Don't create the layout in size_allocate() if not needed
Just update its size if it already exists.

gtk/gtklabel.c
@@ -3803,13 +3803,8 @@ gtk_label_size_allocate (GtkWidget     *widget,
 
   GTK_WIDGET_CLASS (gtk_label_parent_class)->size_allocate (widget, allocation);
 
-  /* The layout may have been recently cleared in get_size_for_orientation(),
-   * but the width at that point may not be the same as the allocated width
-   */
-  if (priv->wrap)
-    gtk_label_clear_layout (label);
-
-  gtk_label_ensure_layout (label);
+  if (priv->layout)
+    gtk_label_update_layout_width (label);
 
   if (priv->select_info && priv->select_info->window)
     {

commit 93b0cecd7e563a4f391652e7308700c15ccce7da

label: Remove dead code
This code has been useless for a while, and now it's breaking things.
Moving the paned in tests/testheightforwidth's wrapping label shows that
bug.

Broken since 9992efdb0e7d0cf52e47817fe049b97967400731

gtk/gtklabel.c
@@ -3313,20 +3313,13 @@ gtk_label_update_layout_width (GtkLabel *label)
         pango_layout_set_width (priv->layout, aux_width * PANGO_SCALE);
       else
         {
-          PangoRectangle rect;
-          gint xpad, ypad, natural_width;
+          gint xpad, ypad;
           gtk_misc_get_padding (GTK_MISC (label), &xpad, &ypad);
 
           if (angle == 90 || angle == 270)
             width = allocation.height - ypad * 2;
           else
             width = allocation.width  - xpad * 2;
-
-          /* dont set a wrap width wider than the label's natural width
-           * incase we're allocated more space than needed */
-          pango_layout_get_extents (priv->layout, NULL, &rect);
-          natural_width = PANGO_PIXELS (rect.width);
-          width = MIN (natural_width, width);
 
           pango_layout_set_wrap (priv->layout, priv->wrap_mode);
           pango_layout_set_width (priv->layout, MAX (width, 1) * PANGO_SCALE);