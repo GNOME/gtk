1f9f5e8890bd63ec76258b61a15d21af202db14a
Javier Jardón <jjardon@gnome.org>
2011-04-11 22:14:35 +0100

    Move documentation to online comments: GtkCellRendererSpin

--- gtk/gtkcellrendererspin.c
+++ gtk/gtkcellrendererspin.c
@@ -28,6 +28,28 @@
 #include "gtkcellrendererspin.h"
 
 
+/**
+ * SECTION:gtkcellrendererspin
+ * @Short_description: Renders a spin button in a cell
+ * @Title: GtkCellRendererSpin
+ * @See_also: #GtkCellRendererText, #GtkSpinButton
+ *
+ * #GtkCellRendererSpin renders text in a cell like #GtkCellRendererText from
+ * which it is derived. But while #GtkCellRendererText offers a simple entry to
+ * edit the text, #GtkCellRendererSpin offers a #GtkSpinButton widget. Of course,
+ * that means that the text has to be parseable as a floating point number.
+ *
+ * The range of the spinbutton is taken from the adjustment property of the
+ * cell renderer, which can be set explicitly or mapped to a column in the
+ * tree model, like all properties of cell renders. #GtkCellRendererSpin
+ * also has properties for the #GtkCellRendererSpin:climb-rate and the number
+ * of #GtkCellRendererSpin:digits to display. Other #GtkSpinButton properties
+ * can be set in a handler for the #GtkCellRenderer::editing-started signal.
+ *
+ * The #GtkCellRendererSpin cell renderer was added in GTK+ 2.10.
+ */
+
+
 struct _GtkCellRendererSpinPrivate
 {
   GtkAdjustment *adjustment;
