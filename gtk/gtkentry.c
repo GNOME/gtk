GNOME/gtk

commit 1351d40cfe0df7e1d12017af4323fb3ab73b19d2

entry: Fix gtk_entry_get_icon_at_pos()
The passed in coordinates are widget-relative, not window-relative.

gtk/gtkentry.c
@@ -8267,10 +8267,11 @@ gtk_entry_get_icon_storage_type (GtkEntry             *entry,
 /**
  * gtk_entry_get_icon_at_pos:
  * @entry: a #GtkEntry
- * @x: the x coordinate of the position to find
+ * @x: the x coordinate of the position to fine
  * @y: the y coordinate of the position to find
  *
- * Finds the icon at the given position and return its index.
+ * Finds the icon at the given position and return its index. The
+ * position's coordinates are relative to the @entry's top left corner.
  * If @x, @y doesn't lie inside an icon, -1 is returned.
  * This function is intended for use in a #GtkWidget::query-tooltip
  * signal handler.
@@ -8290,7 +8291,7 @@ gtk_entry_get_icon_at_pos (GtkEntry *entry,
 
   g_return_val_if_fail (GTK_IS_ENTRY (entry), -1);
 
-  get_frame_size (entry, TRUE, &frame_x, &frame_y, NULL, NULL);
+  get_frame_size (entry, FALSE, &frame_x, &frame_y, NULL, NULL);
   x -= frame_x;
   y -= frame_y;
 

commit 49c7869582de41411624a19fb781fa34c9fdf0c8

docs fix typo
Was accidentally introduced in 1351d40cfe0df7e1d12017af4323fb3ab73b19d2

Thanks Paolo for noticing.

gtk/gtkentry.c
@@ -8267,7 +8267,7 @@ gtk_entry_get_icon_storage_type (GtkEntry             *entry,
 /**
  * gtk_entry_get_icon_at_pos:
  * @entry: a #GtkEntry
- * @x: the x coordinate of the position to fine
+ * @x: the x coordinate of the position to find
  * @y: the y coordinate of the position to find
  *
  * Finds the icon at the given position and return its index. The