4bf22639d13a86f4ab38a4435ef5fc00bc2b4bce
Alberto Ruiz <aruiz@gnome.org>
2011-05-11 15:48:21 +0200

    GtkFontChooser: Expose the new widget in gtk.h and add test cases

---
+++ tests/testfontchooserdialog.c
@@ -0,0 +1,38 @@
+/* testfontchooserdialog.c
+ * Copyright (C) 2011 Alberto Ruiz <aruiz@gnome.org>
+ *
+ * This library is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Library General Public
+ * License as published by the Free Software Foundation; either
+ * version 2 of the License, or (at your option) any later version.
+ *
+ * This library is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * Library General Public License for more details.
+ *
+ * You should have received a copy of the GNU Library General Public
+ * License along with this library; if not, write to the
+ * Free Software Foundation, Inc., 59 Temple Place - Suite 330,
+ * Boston, MA 02111-1307, USA.
+ */
+
+#include "config.h"
+#include <gtk/gtk.h>
+
+int
+main (int argc, char *argv[])
+{
+  GtkWidget *dialog;
+  GtkWidget *ok;
+
+  gtk_init (&argc, &argv);
+
+  dialog = gtk_font_chooser_dialog_new (NULL);
+
+  gtk_dialog_run (GTK_DIALOG (dialog));
+
+  gtk_widget_destroy (dialog);
+
+  return 0;
+}
