GNOME/gtk

commit 5f2ef59d6968f311abe3ebf9f477120240c1acb2

menuitem: Position submenus properly
Now that we can actually query the menu's size, don't query the
preferred minimum size and hope it's correct, but use its actual size.

gtk/gtkmenuitem.c
@@ -2147,9 +2147,8 @@ gtk_menu_item_position_menu (GtkMenu  *menu,
 
   direction = gtk_widget_get_direction (widget);
 
-  gtk_widget_get_preferred_size (GTK_WIDGET (menu), &requisition, NULL);
-  twidth = requisition.width;
-  theight = requisition.height;
+  twidth = gtk_widget_get_allocated_width (GTK_WIDGET (menu));
+  theight = gtk_widget_get_allocated_width (GTK_WIDGET (menu));
 
   screen = gtk_widget_get_screen (GTK_WIDGET (menu));
   monitor_num = gdk_screen_get_monitor_at_window (screen, priv->event_window);

commit f6ffd6df8afb0037c216895dbcf1dd4f19a556f7

menuitem: Remove unused variable
Seriously, can't people compile with -Werror? Broke with
5f2ef59d6968f311abe3ebf9f477120240c1acb2

gtk/gtkmenuitem.c
@@ -2121,7 +2121,6 @@ gtk_menu_item_position_menu (GtkMenu  *menu,
   GtkAllocation allocation;
   GtkWidget *widget;
   GtkMenuItem *parent_menu_item;
-  GtkRequisition requisition;
   GtkWidget *parent;
   GdkScreen *screen;
   gint twidth, theight;