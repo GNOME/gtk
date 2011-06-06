fab1209b1d6b58185be189e2d92db7c79648d13d
Javier Jardón <jjardon@gnome.org>
2011-06-06 19:02:06 +0100

    gail: use const instead G_CONST_RETURN

--- modules/other/gail/gailtreeview.c
+++ modules/other/gail/gailtreeview.c
@@ -116,7 +116,7 @@ static void             gail_tree_view_set_caption      (AtkTable               
 static AtkObject*       gail_tree_view_get_summary      (AtkTable               *table);
 static void             gail_tree_view_set_summary      (AtkTable               *table,
                                                          AtkObject              *accessible);
-static G_CONST_RETURN gchar*
+static const gchar*
                         gail_tree_view_get_row_description 
                                                         (AtkTable               *table,
                                                          gint                   row);
@@ -124,7 +124,7 @@ static void             gail_tree_view_set_row_description
                                                         (AtkTable               *table,
                                                          gint                   row,
                                                          const gchar            *description);
-static G_CONST_RETURN gchar*
+static const gchar*
                         gail_tree_view_get_column_description
                                                         (AtkTable               *table,
                                                          gint                   column);
@@ -1618,7 +1618,7 @@ gail_tree_view_set_caption (AtkTable	*table,
     g_object_unref (old_caption);
 }
 
-static G_CONST_RETURN gchar*
+static const gchar*
 gail_tree_view_get_column_description (AtkTable	  *table,
                                        gint       in_col)
 {
@@ -1684,7 +1684,7 @@ gail_tree_view_set_column_description (AtkTable	   *table,
                          &values, NULL);
 }
 
-static G_CONST_RETURN gchar*
+static const gchar*
 gail_tree_view_get_row_description (AtkTable    *table,
                                     gint        row)
 {
