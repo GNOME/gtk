b0dd7e1cfdd3217cff9cb5e0a932e155821556b2
Matthias Clasen <mclasen@redhat.com>
2011-07-25 10:20:56 -0400

    widget-factory: small tweaks
    Use our canonical example url in the link button, use the placeholder-text
    feature for the search entry, and add a runtime switch for the dark theme.

--- tests/widget-factory.c
+++ tests/widget-factory.c
@@ -24,25 +24,39 @@
 
 #include <gtk/gtk.h>
 
+static void
+dark_toggled (GtkCheckMenuItem *item, gpointer data)
+{
+  gboolean dark;
+
+  dark = gtk_check_menu_item_get_active (item);
+  g_object_set (gtk_settings_get_default (),
+                "gtk-application-prefer-dark-theme", dark,
+                NULL);
+}
+
 int
 main (int argc, char *argv[])
 {
   GtkBuilder *builder;
   GtkWidget  *window;
+  GtkWidget  *widget;
+  gboolean    dark = FALSE;
 
   gtk_init (&argc, &argv);
 
-  if (argc > 1 && 
-      (g_strcmp0 (argv[1], "--dark") == 0))
-    g_object_set (gtk_settings_get_default (),
-                  "gtk-application-prefer-dark-theme", TRUE,
-                  NULL);
+  if (argc > 1 && (g_strcmp0 (argv[1], "--dark") == 0))
+    dark = TRUE;
 
   builder = gtk_builder_new ();
   gtk_builder_add_from_file (builder, "./widget-factory.ui", NULL);
 
   window = GTK_WIDGET (gtk_builder_get_object (builder, "window"));
   gtk_builder_connect_signals (builder, NULL);
+
+  widget = (GtkWidget*) gtk_builder_get_object (builder, "darkmenuitem");
+  g_signal_connect (widget, "toggled", G_CALLBACK (dark_toggled), NULL);
+  gtk_check_menu_item_set_active (GTK_CHECK_MENU_ITEM (widget), dark);
 
   g_object_unref (G_OBJECT (builder));
 
