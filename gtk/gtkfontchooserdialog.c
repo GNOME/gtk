5c5cdf23a8353eca5f5ea34e2b9ccb697fe17b14
Matthias Clasen <mclasen@redhat.com>
2011-08-23 10:08:33 -0400

    Add a mnemonic to the 'Select' button
    This makes the string the same as in the app chooser dialog.

--- gtk/gtkfontchooserdialog.c
+++ gtk/gtkfontchooserdialog.c
@@ -122,7 +122,7 @@ gtk_font_chooser_dialog_init (GtkFontChooserDialog *fontchooserdiag)
                                                GTK_STOCK_CANCEL,
                                                GTK_RESPONSE_CANCEL);
   priv->select_button = gtk_dialog_add_button (dialog,
-                                               _("Select"),
+                                               _("_Select"),
                                                GTK_RESPONSE_OK);
   gtk_widget_grab_default (priv->select_button);
 
