2e39400e18f7e1cb5bd1213f4e46ac9fa2c439cb
Matthias Clasen <mclasen@redhat.com>
2011-09-04 23:46:51 -0400

    Clean up the docs of GTK_SELECTION_MULTIPLE
    As requested in bug 566202.

--- gtk/gtkenums.h
+++ gtk/gtkenums.h
@@ -480,16 +480,16 @@ typedef enum
  * GtkSelectionMode:
  * @GTK_SELECTION_NONE: No selection is possible.
  * @GTK_SELECTION_SINGLE: Zero or one element may be selected.
- * @GTK_SELECTION_BROWSE: Exactly one element is selected. In some circumstances,
- *  such as initially or during a search operation, it's possible for no element
- *  to be selected with %GTK_SELECTION_BROWSE. What is really enforced is that
- *  the user can't deselect a currently selected element except by selecting
- *  another element.
+ * @GTK_SELECTION_BROWSE: Exactly one element is selected.
+ *     In some circumstances, such as initially or during a search
+ *     operation, it's possible for no element to be selected with
+ *     %GTK_SELECTION_BROWSE. What is really enforced is that the user
+ *     can't deselect a currently selected element except by selecting
+ *     another element.
  * @GTK_SELECTION_MULTIPLE: Any number of elements may be selected.
- *  Clicks toggle the state of an item. Any number of elements may be selected.
- *  The Ctrl key may be used to enlarge the selection, and Shift key to select
- *  between the focus and the child pointed to. Some widgets may also allow
- *  Click-drag to select a range of elements.
+ *      The Ctrl key may be used to enlarge the selection, and Shift
+ *      key to select between the focus and the child pointed to.
+ *      Some widgets may also allow Click-drag to select a range of elements.
  * @GTK_SELECTION_EXTENDED: Deprecated, behaves identical to %GTK_SELECTION_MULTIPLE.
  *
  * Used to control what selections users are allowed to make.
