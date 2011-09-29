GNOME/gtk

commit a9affcdd3f9c888c5e68dc76b0c5173d918bc725

notebook: Remove unused variables

gtk/gtknotebook.c
@@ -5044,7 +5044,6 @@ gtk_notebook_paint (GtkWidget    *widget,
   gboolean is_rtl;
   gint tab_pos;
   GtkStyleContext *context;
-  GtkRegionFlags tab_flags;
 
   notebook = GTK_NOTEBOOK (widget);
   priv = notebook->priv;
@@ -5256,7 +5255,6 @@ gtk_notebook_draw_tab (GtkNotebook     *notebook,
   GtkNotebookPrivate *priv;
   GtkWidget *widget;
   GtkStyleContext *context;
-  GtkStateFlags state = 0;
 
   if (!NOTEBOOK_IS_TAB_LABEL_PARENT (notebook, page) ||
       !gtk_widget_get_mapped (page->tab_label) ||
@@ -5268,7 +5266,6 @@ gtk_notebook_draw_tab (GtkNotebook     *notebook,
 
   context = gtk_widget_get_style_context (widget);
   gtk_style_context_save (context);
-  state = notebook_tab_prepare_style_context (notebook, page, context, use_flags);
 
   gtk_render_extension (context, cr,
                        page->allocation.x,

commit bdfa05bc811bf78311ee986ba03115c3154d6341

notebook: don't forget to prepare the tab style context before drawing
The previous commit a9affcdd3f9c888c5e68dc76b0c5173d918bc725
accidentally removed the call to notebook_tab_prepare_style_context()

gtk/gtknotebook.c
@@ -5266,6 +5266,7 @@ gtk_notebook_draw_tab (GtkNotebook     *notebook,
 
   context = gtk_widget_get_style_context (widget);
   gtk_style_context_save (context);
+  notebook_tab_prepare_style_context (notebook, page, context, use_flags);
 
   gtk_render_extension (context, cr,
                        page->allocation.x,