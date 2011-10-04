06a9d19f9a2b86f67c02e26be388a0a898302814
Federico Mena Quintero <federico@gnome.org>
2011-10-04 17:09:33 -0500

    Don't use gtk_icon_theme_get_default(); use the screen-specific theme
    Signed-off-by: Federico Mena Quintero <federico@gnome.org>

--- gtk/gtkplacessidebar.c
+++ gtk/gtkplacessidebar.c
@@ -257,7 +257,7 @@ get_eject_icon (GtkPlacesSidebar *sidebar,
 	GtkIconTheme *icon_theme;
 	GtkStyleContext *style;
 
-	icon_theme = gtk_icon_theme_get_default ();
+	icon_theme = gtk_icon_theme_get_for_screen (gtk_widget_get_screen (GTK_WIDGET (sidebar)));
 	icon_size = get_icon_size (sidebar);
 	icon = g_themed_icon_new_with_default_fallbacks ("media-eject-symbolic");
 	icon_info = gtk_icon_theme_lookup_by_gicon (icon_theme, icon, icon_size, 0);
@@ -368,7 +368,7 @@ get_pixbuf_from_gicon (GtkPlacesSidebar *sidebar, GIcon *icon)
 	GtkIconInfo *icon_info;
 	GdkPixbuf *pixbuf;
 
-	icon_theme = gtk_icon_theme_get_default ();
+	icon_theme = gtk_icon_theme_get_for_screen (gtk_widget_get_screen (GTK_WIDGET (sidebar)));
 	icon_size = get_icon_size (sidebar);
 	icon_info = gtk_icon_theme_lookup_by_gicon (icon_theme, icon, icon_size, 0);
 
