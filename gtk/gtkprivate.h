31863fc1b4ff77f233acca81b0e8c9712b187294
Michael Natterer <mitch@lanedo.com>
2011-10-07 20:13:09 +0200

    gtk: also define the default *virtual* accel mod mask
    and use it in stock items and GtkAccelGroup. Change the non-virtual
    one to MOD2 on OSX, so hardcoded accelerators in the IM context and
    the treeview work.
    (cherry picked from commit 21e971b3cbddf49c3ec74c98054686cd8881a971)

--- gtk/gtkprivate.h
+++ gtk/gtkprivate.h
@@ -63,8 +63,10 @@ const gchar *_gtk_get_data_prefix ();
  * Copy, Paste). This symbol is for those simple cases. */
 #ifndef GDK_WINDOWING_QUARTZ
 #define GTK_DEFAULT_ACCEL_MOD_MASK GDK_CONTROL_MASK
+#define GTK_DEFAULT_ACCEL_MOD_MASK_VIRTUAL GDK_CONTROL_MASK
 #else
-#define GTK_DEFAULT_ACCEL_MOD_MASK GDK_META_MASK
+#define GTK_DEFAULT_ACCEL_MOD_MASK GDK_MOD2_MASK
+#define GTK_DEFAULT_ACCEL_MOD_MASK_VIRTUAL GDK_META_MASK
 #endif
 
 /* When any of these modifiers are active, a key
