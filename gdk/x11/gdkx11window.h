a003eef9d47a633a6c1b1fe2bbce155fdbf05803
Colin Walters <walters@verbum.org>
2011-11-30 15:41:02 -0500

    gdk: Fix prototype for gdk_x11_window_set_utf8_property

--- gdk/x11/gdkx11window.h
+++ gdk/x11/gdkx11window.h
@@ -57,6 +57,9 @@ GType    gdk_x11_window_get_type          (void);
 Window   gdk_x11_window_get_xid           (GdkWindow   *window);
 void     gdk_x11_window_set_user_time     (GdkWindow   *window,
                                            guint32      timestamp);
+void     gdk_x11_window_set_utf8_property    (GdkWindow *window,
+					      const gchar *name,
+					      const gchar *value);
 void     gdk_x11_window_set_theme_variant (GdkWindow   *window,
                                            char        *variant);
 void     gdk_x11_window_set_hide_titlebar_when_maximized (GdkWindow *window,
