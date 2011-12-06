GNOME/gtk

commit 7bd2446801ffd0fc2ac5cc98fcd675ac4f3935d6

Remove broken optimization in style modifier overrides
We used to only set the override color or font if it was different
than the existing value. However, that means you can't change it to
an override that is the same as the default value for the property.

With this fixed you can e.g. override with a color of 0,0,0,0 which
you couldn't before.

gtk/gtkmodifierstyle.c
@@ -162,16 +162,6 @@ modifier_style_set_color (GtkModifierStyle *style,
   g_return_if_fail (GTK_IS_MODIFIER_STYLE (style));
 
   priv = style->priv;
-  gtk_style_properties_get (priv->style, state,
-                            prop, &old_color,
-                            NULL);
-
-  if ((!color && !old_color) ||
-      (color && old_color && gdk_rgba_equal (color, old_color)))
-    {
-      gdk_rgba_free (old_color);
-      return;
-    }
 
   if (color)
     gtk_style_properties_set (priv->style, state,
@@ -214,19 +204,6 @@ _gtk_modifier_style_set_font (GtkModifierStyle           *style,
   g_return_if_fail (GTK_IS_MODIFIER_STYLE (style));
 
   priv = style->priv;
-  gtk_style_properties_get (priv->style, 0,
-                            "font", &old_font,
-                            NULL);
-
-  if ((!old_font && !font_desc) ||
-      (old_font && font_desc &&
-       pango_font_description_equal (old_font, font_desc)))
-    {
-      if (old_font)
-        pango_font_description_free (old_font);
-
-      return;
-    }
 
   if (font_desc)
     gtk_style_properties_set (priv->style, 0,

commit c24742e7890caba218ff19d3acce4c1c8ffc1f0d

modifierstyle: Fix compile errors
Introduced in 7bd2446801ffd0fc2ac5cc98fcd675ac4f3935d6

gtk/gtkmodifierstyle.c
@@ -157,7 +157,6 @@ modifier_style_set_color (GtkModifierStyle *style,
                           const GdkRGBA    *color)
 {
   GtkModifierStylePrivate *priv;
-  GdkRGBA *old_color;
 
   g_return_if_fail (GTK_IS_MODIFIER_STYLE (style));
 
@@ -171,7 +170,6 @@ modifier_style_set_color (GtkModifierStyle *style,
     gtk_style_properties_unset_property (priv->style, prop, state);
 
   g_signal_emit (style, signals[CHANGED], 0);
-  gdk_rgba_free (old_color);
 }
 
 void
@@ -199,7 +197,6 @@ _gtk_modifier_style_set_font (GtkModifierStyle           *style,
                               const PangoFontDescription *font_desc)
 {
   GtkModifierStylePrivate *priv;
-  PangoFontDescription *old_font;
 
   g_return_if_fail (GTK_IS_MODIFIER_STYLE (style));
 
@@ -211,9 +208,6 @@ _gtk_modifier_style_set_font (GtkModifierStyle           *style,
                               NULL);
   else
     gtk_style_properties_unset_property (priv->style, "font", 0);
-
-  if (old_font)
-    pango_font_description_free (old_font);
 
   g_signal_emit (style, signals[CHANGED], 0);
 }