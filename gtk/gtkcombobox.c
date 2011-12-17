721920e9e95d7279d7398ab8252e8a99691da81f
Rui Matos <tiagomatos@gmail.com>
2011-12-17 18:55:09 +0000

    combobox: Stop setting state flags on the style context for drawing

--- gtk/gtkcombobox.c
+++ gtk/gtkcombobox.c
@@ -2810,11 +2810,8 @@ gtk_combo_box_draw (GtkWidget *widget,
   if (priv->shadow_type != GTK_SHADOW_NONE)
     {
       GtkStyleContext *context;
-      GtkStateFlags state;
 
       context = gtk_widget_get_style_context (widget);
-      state = gtk_widget_get_state_flags (widget);
-      gtk_style_context_set_state (context, state);
 
       gtk_render_background (context, cr, 0, 0,
                              gtk_widget_get_allocated_width (widget),
