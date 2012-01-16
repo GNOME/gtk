5df9b0188a082b607d211bfef9b8277c88d397d2
Benjamin Otte <otte@redhat.com>
2012-01-16 18:28:58 +0100

    css: Implement CSS 3 linear-gradient() syntax

--- gtk/gtkcssimage.c
+++ gtk/gtkcssimage.c
@@ -24,6 +24,7 @@
 
 /* for the types only */
 #include "gtk/gtkcssimagegradientprivate.h"
+#include "gtk/gtkcssimagelinearprivate.h"
 #include "gtk/gtkcssimageurlprivate.h"
 #include "gtk/gtkcssimagewin32private.h"
 
@@ -322,7 +323,8 @@ gtk_css_image_get_parser_type (GtkCssParser *parser)
   } image_types[] = {
     { "url", _gtk_css_image_url_get_type },
     { "-gtk-gradient", _gtk_css_image_gradient_get_type },
-    { "-gtk-win32-theme-part", _gtk_css_image_win32_get_type }
+    { "-gtk-win32-theme-part", _gtk_css_image_win32_get_type },
+    { "linear-gradient", _gtk_css_image_linear_get_type }
   };
   guint i;
 
