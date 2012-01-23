b19508c453d3e746082fb7cb97662e0c735e3e11
Carlos Garnacho <carlosg@gnome.org>
2012-01-23 23:51:24 +0100

    gdk: deal with GDK_SMOOTH_SCROLL events as not having a direction
    gdk_event_get_scroll_direction() will return FALSE on these, so
    gdk_event_get_scroll_deltas() has to be used to retrieve dx/dy

--- gdk/gdkevents.c
+++ gdk/gdkevents.c
@@ -1169,7 +1169,10 @@ gdk_event_get_scroll_direction (const GdkEvent *event,
   switch (event->type)
     {
     case GDK_SCROLL:
-      dir = event->scroll.direction;
+      if (event->scroll.direction == GDK_SCROLL_SMOOTH)
+        fetched = FALSE;
+      else
+        dir = event->scroll.direction;
       break;
     default:
       fetched = FALSE;
