54cf99fb2e3ff1cb04ea776a22774bfd6ffe5cdf
Carlos Garnacho <carlosg@gnome.org>
2012-01-24 12:18:45 +0100

    viewport: set GDK_SMOOTH_SCROLL_MASK
    This is so smooth scroll events are send/handled by the
    parent GtkScrolledWindow if any.

--- gtk/gtkviewport.c
+++ gtk/gtkviewport.c
@@ -682,9 +682,7 @@ gtk_viewport_realize (GtkWidget *widget)
   attributes.visual = gtk_widget_get_visual (widget);
 
   event_mask = gtk_widget_get_events (widget) | GDK_EXPOSURE_MASK;
-  /* We select on button_press_mask so that button 4-5 scrolls are trapped.
-   */
-  attributes.event_mask = event_mask | GDK_BUTTON_PRESS_MASK;
+  attributes.event_mask = event_mask | GDK_SCROLL_MASK | GDK_SMOOTH_SCROLL_MASK;
 
   attributes_mask = GDK_WA_X | GDK_WA_Y | GDK_WA_VISUAL;
 
