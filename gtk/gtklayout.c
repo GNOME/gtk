2dd58d948275c4e50228f4c23d35f6819eb1b882
Carlos Garnacho <carlosg@gnome.org>
2012-01-24 12:17:42 +0100

    layout: set GDK_SMOOTH_SCROLL_MASK
    This is so smooth scroll events are send/handled by the
    parent GtkScrolledWindow if any.

--- gtk/gtklayout.c
+++ gtk/gtklayout.c
@@ -894,7 +894,8 @@ gtk_layout_realize (GtkWidget *widget)
   attributes.y = - gtk_adjustment_get_value (priv->vadjustment);
   attributes.width = MAX (priv->width, allocation.width);
   attributes.height = MAX (priv->height, allocation.height);
-  attributes.event_mask = GDK_EXPOSURE_MASK | GDK_SCROLL_MASK | 
+  attributes.event_mask = GDK_EXPOSURE_MASK | GDK_SCROLL_MASK |
+                          GDK_SMOOTH_SCROLL_MASK | 
                           gtk_widget_get_events (widget);
 
   priv->bin_window = gdk_window_new (window,
