d9b3086df63698d4dd75d2f01e3c08f673d456c9
Cosimo Cecchi <cosimoc@gnome.org>
2012-02-14 11:19:04 -0500

    about-dialog: add a stroke around the credits area
    Makes it consistent with e.g. the license page.
    
    https://bugzilla.gnome.org/show_bug.cgi?id=670078

--- gtk/gtkaboutdialog.c
+++ gtk/gtkaboutdialog.c
@@ -2357,7 +2357,7 @@ create_credits_page (GtkAboutDialog *about)
   priv->credits_page = gtk_notebook_append_page (GTK_NOTEBOOK (priv->notebook), page_vbox, NULL);
 
   sw = gtk_scrolled_window_new (NULL, NULL);
-  gtk_scrolled_window_set_shadow_type (GTK_SCROLLED_WINDOW (sw), GTK_SHADOW_NONE);
+  gtk_scrolled_window_set_shadow_type (GTK_SCROLLED_WINDOW (sw), GTK_SHADOW_IN);
   gtk_scrolled_window_set_policy (GTK_SCROLLED_WINDOW (sw),
                                   GTK_POLICY_NEVER,
                                   GTK_POLICY_AUTOMATIC);
