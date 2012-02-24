064937ba1c6b89f4717f4f21477b1e6f093b6deb
Matthias Clasen <mclasen@redhat.com>
2012-02-24 10:18:40 -0500

    gtk: Adapt event propagation code to new touch event types

--- gtk/gtkmain.c
+++ gtk/gtkmain.c
@@ -1332,17 +1332,22 @@ rewrite_event_for_window (GdkEvent  *event,
     case GDK_2BUTTON_PRESS:
     case GDK_3BUTTON_PRESS:
     case GDK_BUTTON_RELEASE:
-    case GDK_TOUCH_PRESS:
-    case GDK_TOUCH_RELEASE:
       rewrite_events_translate (event->any.window,
                                 new_window,
                                 &event->button.x, &event->button.y);
       break;
     case GDK_MOTION_NOTIFY:
-    case GDK_TOUCH_MOTION:
       rewrite_events_translate (event->any.window,
                                 new_window,
                                 &event->motion.x, &event->motion.y);
+      break;
+    case GDK_TOUCH_BEGIN:
+    case GDK_TOUCH_UPDATE:
+    case GDK_TOUCH_END:
+    case GDK_TOUCH_CANCEL:
+      rewrite_events_translate (event->any.window,
+                                new_window,
+                                &event->touch.x, &event->touch.y);
       break;
     case GDK_KEY_PRESS:
     case GDK_KEY_RELEASE:
@@ -1389,9 +1394,10 @@ rewrite_event_for_grabs (GdkEvent *event)
     case GDK_PROXIMITY_OUT:
     case GDK_KEY_PRESS:
     case GDK_KEY_RELEASE:
-    case GDK_TOUCH_PRESS:
-    case GDK_TOUCH_RELEASE:
-    case GDK_TOUCH_MOTION:
+    case GDK_TOUCH_BEGIN:
+    case GDK_TOUCH_UPDATE:
+    case GDK_TOUCH_END:
+    case GDK_TOUCH_CANCEL:
       display = gdk_window_get_display (event->any.window);
       device = gdk_event_get_device (event);
 
@@ -1651,7 +1657,7 @@ gtk_main_do_event (GdkEvent *event)
     case GDK_BUTTON_PRESS:
     case GDK_2BUTTON_PRESS:
     case GDK_3BUTTON_PRESS:
-    case GDK_TOUCH_PRESS:
+    case GDK_TOUCH_BEGIN:
       if (!_gtk_propagate_captured_event (grab_widget, event, topmost_widget))
         gtk_propagate_event (grab_widget, event);
       break;
@@ -1702,8 +1708,9 @@ gtk_main_do_event (GdkEvent *event)
     case GDK_BUTTON_RELEASE:
     case GDK_PROXIMITY_IN:
     case GDK_PROXIMITY_OUT:
-    case GDK_TOUCH_MOTION:
-    case GDK_TOUCH_RELEASE:
+    case GDK_TOUCH_UPDATE:
+    case GDK_TOUCH_END:
+    case GDK_TOUCH_CANCEL:
       if (!_gtk_propagate_captured_event (grab_widget, event, topmost_widget))
         gtk_propagate_event (grab_widget, event);
       break;
@@ -1754,7 +1761,7 @@ gtk_main_do_event (GdkEvent *event)
       || event->type == GDK_DRAG_ENTER
       || event->type == GDK_GRAB_BROKEN
       || event->type == GDK_MOTION_NOTIFY
-      || event->type == GDK_TOUCH_MOTION
+      || event->type == GDK_TOUCH_UPDATE
       || event->type == GDK_SCROLL)
     {
       _gtk_tooltip_handle_event (event);
