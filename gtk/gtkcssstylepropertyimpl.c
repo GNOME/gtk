GNOME/gtk

commit 8243b16ad79cb4965ab5314e53a20ef16b2af9c3

styleproperty: Save some memory
Only create a new CssValue when we actually need one.

gtk/gtkcssstylepropertyimpl.c
@@ -400,10 +400,20 @@ css_image_value_compute (GtkCssStyleProperty    *property,
                          GtkStyleContext        *context,
                          GtkCssValue            *specified)
 {
-  GtkCssImage *image = _gtk_css_value_get_image (specified);
+  GtkCssImage *image, *computed;
+  
+  image = _gtk_css_value_get_image (specified);
 
-  if (image)
-    image = _gtk_css_image_compute (image, context);
+  if (image == NULL)
+    return _gtk_css_value_ref (specified);
+
+  computed = _gtk_css_image_compute (image, context);
+
+  if (computed == image)
+    {
+      g_object_unref (computed);
+      return _gtk_css_value_ref (specified);
+    }
 
   return _gtk_css_value_new_take_image (image);
 }

commit 8b5910f8d7802c1653e5b596928014da34a04e9d

styleproperty: Fix thinko
Introduced in 8243b16ad79cb4965ab5314e53a20ef16b2af9c3 - seems I wasn't
really on top of my own capabilities on Saturday.

gtk/gtkcssstylepropertyimpl.c
@@ -415,7 +415,7 @@ css_image_value_compute (GtkCssStyleProperty    *property,
       return _gtk_css_value_ref (specified);
     }
 
-  return _gtk_css_value_new_take_image (image);
+  return _gtk_css_value_new_take_image (computed);
 }
 
 static gboolean 