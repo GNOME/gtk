GNOME/gtk

commit fa8e6f7857e9b4afba227c3108111f359dc826d5

XI2: More logging for smooth scrolling
In addition to the delta, log device information.
This was triggered by
https://bugzilla.gnome.org/show_bug.cgi?id=673644

gdk/x11/gdkdevicemanager-xi2.c
@@ -1318,7 +1318,9 @@ gdk_x11_device_manager_xi2_translate_event (GdkEventTranslator *translator,
             event->scroll.direction = GDK_SCROLL_SMOOTH;
 
             GDK_NOTE(EVENTS,
-                     g_message ("smooth scroll:\twindow %ld\n\tdeltas: %f %f",
+                     g_message ("smooth scroll: %s\n\tdevice: %u\n\tsource device: %u\n\twindow %ld\n\tdeltas: %f %f",
+                                (xev->flags & XIPointerEmulated) ? "emulated" : "",
+                                xev->deviceid, xev->sourceid,
                                 xev->event, delta_x, delta_y));
 
 

commit 60274b15f5c956556d72c99b12fbc2d14ed5909c

Fix build with XInput < 2.2
Fixes regression introduced in fa8e6f7857e9b4afba227c3108111f359dc826d5

gdk/x11/gdkdevicemanager-xi2.c
@@ -1330,7 +1330,11 @@ gdk_x11_device_manager_xi2_translate_event (GdkEventTranslator *translator,
 
             GDK_NOTE(EVENTS,
                      g_message ("smooth scroll: %s\n\tdevice: %u\n\tsource device: %u\n\twindow %ld\n\tdeltas: %f %f",
+#ifdef XINPUT_2_2
                                 (xev->flags & XIPointerEmulated) ? "emulated" : "",
+#else
+                                 "",
+#endif
                                 xev->deviceid, xev->sourceid,
                                 xev->event, delta_x, delta_y));
 