779591b62fbc5ee4ebf9912a4416b655cdcf4e32
Bastien Nocera <hadess@hadess.net>
2012-05-10 15:23:57 +0100

    gdk: Don't crash when resetting cursor
    When the toplevel is a GdkOffscreenWindow which doesn't
    implement the set_device_cursor() vfunc.
    
    https://bugzilla.gnome.org/show_bug.cgi?id=675809

--- gdk/gdkwindow.c
+++ gdk/gdkwindow.c
@@ -7805,7 +7805,8 @@ update_cursor (GdkDisplay *display,
    * which native window has what cursor set. */
   toplevel = get_event_toplevel (pointer_window);
   impl_class = GDK_WINDOW_IMPL_GET_CLASS (toplevel->impl);
-  impl_class->set_device_cursor (toplevel, device, cursor);
+  if (impl_class->set_device_cursor)
+    impl_class->set_device_cursor (toplevel, device, cursor);
 }
 
 static gboolean
