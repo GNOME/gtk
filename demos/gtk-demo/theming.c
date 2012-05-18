6480f3c685bbcfa04f62d054e04447ab88eaa340
Matthias Clasen <mclasen@redhat.com>
2012-05-18 01:23:45 -0400

    gtk-demo: Add a theming example

---
+++ demos/gtk-demo/theming.c
@@ -0,0 +1,61 @@
+/* Theming :: theming.ui
+ *
+ * GTK+ uses CSS for theming. Style classes can be associated
+ * with widgets to inform the theme about intended rendering.
+ *
+ * This demo shows some common examples where theming features
+ * of GTK+ are used for certain effects: primary toolbars,
+ * inline toolbars and linked buttons.
+ */
+
+#include <gtk/gtk.h>
+#include "demo-common.h"
+
+static GtkWidget *window = NULL;
+
+GtkWidget *
+do_theming (GtkWidget *do_widget)
+{
+  GtkWidget *grid;
+  GtkBuilder *builder;
+  gchar *filename;
+  GError *err = NULL;
+
+  if (!window)
+    {
+      window = gtk_window_new (GTK_WINDOW_TOPLEVEL);
+      gtk_window_set_screen (GTK_WINDOW (window),
+                             gtk_widget_get_screen (do_widget));
+      gtk_window_set_title (GTK_WINDOW (window), "Theming");
+      gtk_container_set_border_width (GTK_CONTAINER (window), 12);
+      g_signal_connect (window, "destroy",
+                        G_CALLBACK (gtk_widget_destroyed), &window);
+
+      builder = gtk_builder_new ();
+      filename = demo_find_file ("theming.ui", NULL);
+      gtk_builder_add_from_file (builder, filename, &err);
+      g_free (filename);
+      if (err)
+        {
+          g_error ("ERROR: %s\n", err->message);
+          return NULL;
+        }
+
+      grid = (GtkWidget *)gtk_builder_get_object (builder, "grid");
+      gtk_widget_show_all (grid);
+      gtk_container_add (GTK_CONTAINER (window), grid);
+      g_object_unref (builder);
+    }
+
+  if (!gtk_widget_get_visible (window))
+    {
+      gtk_widget_show (window);
+    }
+  else
+    {
+      gtk_widget_destroy (window);
+      window = NULL;
+    }
+
+  return window;
+}
