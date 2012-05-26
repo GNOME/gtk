c7e7d32c68ea416a1c7f473875527064eff3421b
Matthias Clasen <mclasen@redhat.com>
2012-05-26 12:09:13 -0400

    Change the stock help accel to F1
    https://bugzilla.gnome.org/show_bug.cgi?id=162865

--- gtk/gtkstock.c
+++ gtk/gtkstock.c
@@ -369,7 +369,7 @@ static const GtkStockItem builtin_items [] =
   /* This is a navigation label as in "go up" */
   { GTK_STOCK_GO_UP, NC_("Stock label, navigation", "_Up"), 0, 0, GETTEXT_PACKAGE "-navigation" },
   { GTK_STOCK_HARDDISK, NC_("Stock label", "_Hard Disk"), 0, 0, GETTEXT_PACKAGE },
-  { GTK_STOCK_HELP, NC_("Stock label", "_Help"), PRIMARY_MODIFIER, 'h', GETTEXT_PACKAGE },
+  { GTK_STOCK_HELP, NC_("Stock label", "_Help"), 0, GDK_KEY_F1, GETTEXT_PACKAGE },
   { GTK_STOCK_HOME, NC_("Stock label", "_Home"), 0, 0, GETTEXT_PACKAGE },
   { GTK_STOCK_INDENT, NC_("Stock label", "Increase Indent"), 0, 0, GETTEXT_PACKAGE },
   { GTK_STOCK_UNINDENT, NC_("Stock label", "Decrease Indent"), 0, 0, GETTEXT_PACKAGE },
