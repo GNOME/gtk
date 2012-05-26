1bb7e28c90b4180301bb05bae5fa89a27ca273bb
Matthias Clasen <mclasen@redhat.com>
2012-05-25 23:13:42 -0400

    Fix the build on Solaris
    http://bugzilla.gnome.org/show_bug.cgi?id=663991

--- gtk/gtksearchenginesimple.c
+++ gtk/gtksearchenginesimple.c
@@ -26,8 +26,8 @@
  * export ftw() and friends only if _XOPEN_SOURCE and _GNU_SOURCE
  * are defined. see bug #444097.
  */
-#define _XOPEN_SOURCE 500
-#define _GNU_SOURCE 
+#define _XOPEN_SOURCE 600
+#define _GNU_SOURCE
 
 #ifdef HAVE_FTW_H
 #include <ftw.h>
