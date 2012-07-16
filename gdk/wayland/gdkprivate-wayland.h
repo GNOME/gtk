bef037f5d2ec6a2d9221845a1737028a4878a8db
Rob Bradford <rob@linux.intel.com>
2012-07-16 19:45:57 +0100

    wayland: Remove unused function declaration

--- gdk/wayland/gdkprivate-wayland.h
+++ gdk/wayland/gdkprivate-wayland.h
@@ -51,7 +51,6 @@ void _gdk_wayland_window_remove_focus (GdkWindow *window);
 GdkKeymap *_gdk_wayland_keymap_new (void);
 GdkKeymap *_gdk_wayland_keymap_new_from_fd (uint32_t format,
                                             uint32_t fd, uint32_t size);
-struct xkb_desc *_gdk_wayland_keymap_get_xkb_desc (GdkKeymap *keymap);
 struct xkb_state *_gdk_wayland_keymap_get_xkb_state (GdkKeymap *keymap);
 
 GdkCursor *_gdk_wayland_display_get_cursor_for_type (GdkDisplay    *display,
