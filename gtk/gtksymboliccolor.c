e063a0fdf13a09ca44588862d6b0b4f04232759f
Benjamin Otte <otte@redhat.com>
2012-11-08 15:26:33 +0100

    symboliccolor: Split out HSLA code

--- gtk/gtksymboliccolor.c
+++ gtk/gtksymboliccolor.c
@@ -18,6 +18,7 @@
 #include "config.h"
 #include "gtkcssrgbavalueprivate.h"
 #include "gtkcssstylepropertyprivate.h"
+#include "gtkhslaprivate.h"
 #include "gtkstylepropertyprivate.h"
 #include "gtksymboliccolorprivate.h"
 #include "gtkstyleproperties.h"
@@ -510,178 +511,20 @@ gtk_symbolic_color_unref (GtkSymbolicColor *color)
 }
 
 static void
-rgb_to_hls (gdouble *r,
-            gdouble *g,
-            gdouble *b)
-{
-  gdouble min;
-  gdouble max;
-  gdouble red;
-  gdouble green;
-  gdouble blue;
-  gdouble h, l, s;
-  gdouble delta;
-  
-  red = *r;
-  green = *g;
-  blue = *b;
-  
-  if (red > green)
-    {
-      if (red > blue)
-        max = red;
-      else
-        max = blue;
-      
-      if (green < blue)
-        min = green;
-      else
-        min = blue;
-    }
-  else
-    {
-      if (green > blue)
-        max = green;
-      else
-        max = blue;
-      
-      if (red < blue)
-        min = red;
-      else
-        min = blue;
-    }
-  
-  l = (max + min) / 2;
-  s = 0;
-  h = 0;
-  
-  if (max != min)
-    {
-      if (l <= 0.5)
-        s = (max - min) / (max + min);
-      else
-        s = (max - min) / (2 - max - min);
-      
-      delta = max -min;
-      if (red == max)
-        h = (green - blue) / delta;
-      else if (green == max)
-        h = 2 + (blue - red) / delta;
-      else if (blue == max)
-        h = 4 + (red - green) / delta;
-      
-      h *= 60;
-      if (h < 0.0)
-        h += 360;
-    }
-  
-  *r = h;
-  *g = l;
-  *b = s;
-}
-
-static void
-hls_to_rgb (gdouble *h,
-            gdouble *l,
-            gdouble *s)
-{
-  gdouble hue;
-  gdouble lightness;
-  gdouble saturation;
-  gdouble m1, m2;
-  gdouble r, g, b;
-  
-  lightness = *l;
-  saturation = *s;
-  
-  if (lightness <= 0.5)
-    m2 = lightness * (1 + saturation);
-  else
-    m2 = lightness + saturation - lightness * saturation;
-  m1 = 2 * lightness - m2;
-  
-  if (saturation == 0)
-    {
-      *h = lightness;
-      *l = lightness;
-      *s = lightness;
-    }
-  else
-    {
-      hue = *h + 120;
-      while (hue > 360)
-        hue -= 360;
-      while (hue < 0)
-        hue += 360;
-      
-      if (hue < 60)
-        r = m1 + (m2 - m1) * hue / 60;
-      else if (hue < 180)
-        r = m2;
-      else if (hue < 240)
-        r = m1 + (m2 - m1) * (240 - hue) / 60;
-      else
-        r = m1;
-      
-      hue = *h;
-      while (hue > 360)
-        hue -= 360;
-      while (hue < 0)
-        hue += 360;
-      
-      if (hue < 60)
-        g = m1 + (m2 - m1) * hue / 60;
-      else if (hue < 180)
-        g = m2;
-      else if (hue < 240)
-        g = m1 + (m2 - m1) * (240 - hue) / 60;
-      else
-        g = m1;
-      
-      hue = *h - 120;
-      while (hue > 360)
-        hue -= 360;
-      while (hue < 0)
-        hue += 360;
-      
-      if (hue < 60)
-        b = m1 + (m2 - m1) * hue / 60;
-      else if (hue < 180)
-        b = m2;
-      else if (hue < 240)
-        b = m1 + (m2 - m1) * (240 - hue) / 60;
-      else
-        b = m1;
-      
-      *h = r;
-      *l = g;
-      *s = b;
-    }
-}
-
-static void
 _shade_color (GdkRGBA *color,
               gdouble  factor)
 {
-  GdkRGBA temp;
+  GtkHSLA hsla;
 
-  temp = *color;
-  rgb_to_hls (&temp.red, &temp.green, &temp.blue);
+  _gtk_hsla_init_from_rgba (&hsla, color);
 
-  temp.green *= factor;
-  if (temp.green > 1.0)
-    temp.green = 1.0;
-  else if (temp.green < 0.0)
-    temp.green = 0.0;
+  hsla.lightness *= factor;
+  hsla.lightness = CLAMP (hsla.lightness, 0.0, 1.0);
 
-  temp.blue *= factor;
-  if (temp.blue > 1.0)
-    temp.blue = 1.0;
-  else if (temp.blue < 0.0)
-    temp.blue = 0.0;
+  hsla.hue *= factor;
+  hsla.hue = CLAMP (hsla.hue, 0.0, 1.0);
 
-  hls_to_rgb (&temp.red, &temp.green, &temp.blue);
-  *color = temp;
+  _gdk_rgba_init_from_hsla (color, &hsla);
 }
 
 /**
