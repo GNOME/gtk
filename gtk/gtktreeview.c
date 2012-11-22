16687615edec87f8f0b3db3b2556ce5ad5ab93de
David King <amigadave@amigadave.com>
2012-11-22 18:57:15 +0000

    docs: Fix typo in gtk_tree_view_set_tooltip_column comment
    https://bugzilla.gnome.org/show_bug.cgi?id=688884

--- gtk/gtktreeview.c
+++ gtk/gtktreeview.c
@@ -16422,7 +16422,7 @@ gtk_tree_view_set_tooltip_query_cb (GtkWidget  *widget,
  * for you. @column should be set to the column in @tree_view's model
  * containing the tooltip texts, or -1 to disable this feature.
  *
- * When enabled, #GtkWidget::has-tooltip will be set to %TRUE and
+ * When enabled, #GtkWidget:has-tooltip will be set to %TRUE and
  * @tree_view will connect a #GtkWidget::query-tooltip signal handler.
  *
  * Note that the signal handler sets the text with gtk_tooltip_set_markup(),
