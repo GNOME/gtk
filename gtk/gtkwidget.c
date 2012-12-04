cd016ef8e3f72a666582d76c75bd3111ccf2a0e7
Alexander Larsson <alexl@redhat.com>
2012-12-04 21:26:37 +0100

    Revert "Don't queue resize for hidden widgets without a size group"
    This seems to break redraw of the middle pane in glade.
    
    This reverts commit faaae520c98af1c27ca1e97ca8f33655ec6b575e.

--- gtk/gtkwidget.c
+++ gtk/gtkwidget.c
@@ -4654,9 +4654,7 @@ gtk_widget_queue_resize (GtkWidget *widget)
   if (gtk_widget_get_realized (widget))
     gtk_widget_queue_draw (widget);
 
-  if (gtk_widget_get_visible (widget) ||
-      widget->priv->have_size_groups)
-    _gtk_size_group_queue_resize (widget, 0);
+  _gtk_size_group_queue_resize (widget, 0);
 }
 
 /**
