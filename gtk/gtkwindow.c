0cf03dd13b7a636131c4d5f7c6b4d7e51e73175b
Alexander Larsson <alexl@redhat.com>
2012-12-04 16:38:23 +0100

    Don't unnecessarily queue resize in GtkWindow.style_updated
    GtkWindow always queues a resize on style updates if there is
    a grip, because it may have been the grip size style properties
    that changed. However, even if it *were*, and it likely wasn't
    that would not affect the windows size request, so no need
    to queue a resize.

--- gtk/gtkwindow.c
+++ gtk/gtkwindow.c
@@ -5735,7 +5735,6 @@ gtk_window_style_updated (GtkWidget *widget)
                               rect.width, rect.height);
 
       set_grip_shape (window);
-      gtk_widget_queue_resize (widget);
     }
 }
 
