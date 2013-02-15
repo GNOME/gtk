c0ea0b5aadbde2be83997fb7e8006c161f209d02
Michael Natterer <mitch@gimp.org>
2013-02-15 15:35:13 +0100

    gtk: add gtk_widget_get_scale_factor()

--- gtk/gtkwidget.h
+++ gtk/gtkwidget.h
@@ -848,6 +848,8 @@ GDK_AVAILABLE_IN_ALL
 GdkScreen *   gtk_widget_get_screen      (GtkWidget *widget);
 GDK_AVAILABLE_IN_ALL
 gboolean      gtk_widget_has_screen      (GtkWidget *widget);
+GDK_AVAILABLE_IN_3_10
+gint          gtk_widget_get_scale_factor (GtkWidget *widget);
 GDK_AVAILABLE_IN_ALL
 GdkDisplay *  gtk_widget_get_display     (GtkWidget *widget);
 GDK_AVAILABLE_IN_ALL
