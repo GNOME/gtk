GNOME/gtk

commit e09957a47da9425cc26d1b33cb4e9cc3e92e9ac7

label: refactor code
If text didn't change, don't change it.

And if that sentence made no sense, that might explain why I refactored.

gtk/gtklabel.c
@@ -1980,15 +1980,14 @@ gtk_label_set_text_internal (GtkLabel *label,
                              gchar    *str)
 {
   GtkLabelPrivate *priv = label->priv;
-  gboolean text_changed;
 
-  text_changed = g_strcmp0 (priv->text, str) != 0;
+  if (g_strcmp0 (priv->text, str) == 0)
+    return;
 
   g_free (priv->text);
   priv->text = str;
 
-  if (text_changed)
-    gtk_label_select_region_index (label, 0, 0);
+  gtk_label_select_region_index (label, 0, 0);
 }
 
 static void

commit 5d011386a69aa59d6eda1e3ef32005efd831c179

label: Fix memleak
Caused by recent refactoring in e09957a47da9425cc26d1b33cb4e9cc3e92e9ac7

gtk/gtklabel.c
@@ -1982,7 +1982,10 @@ gtk_label_set_text_internal (GtkLabel *label,
   GtkLabelPrivate *priv = label->priv;
 
   if (g_strcmp0 (priv->text, str) == 0)
-    return;
+    {
+      g_free (str);
+      return;
+    }
 
   g_free (priv->text);
   priv->text = str;