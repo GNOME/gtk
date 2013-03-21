361ff3612038592d7f5062fe20e9efc07109ab34
Alexander Larsson <alexl@redhat.com>
2013-03-21 18:25:29 +0100

    Add GTK_DEBUG=baselines support
    This draws red lines to show where the baselines are

--- gtk/gtkdebug.h
+++ gtk/gtkdebug.h
@@ -47,7 +47,8 @@ typedef enum {
   GTK_DEBUG_PRINTING        = 1 << 10,
   GTK_DEBUG_BUILDER         = 1 << 11,
   GTK_DEBUG_SIZE_REQUEST    = 1 << 12,
-  GTK_DEBUG_NO_CSS_CACHE    = 1 << 13
+  GTK_DEBUG_NO_CSS_CACHE    = 1 << 13,
+  GTK_DEBUG_BASELINES       = 1 << 14
 } GtkDebugFlag;
 
 #ifdef G_ENABLE_DEBUG
