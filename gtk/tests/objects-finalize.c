c771a8a3a6d6def8a2b5841b1b1bc2b99c84f9e8
Tristan Van Berkom <tristanvb@openismus.com>
2013-04-05 20:44:12 +0900

    objects-finalize test: Blacklist gdk_pixbuf_simple_anim_iter_get_type()

--- gtk/tests/objects-finalize.c
+++ gtk/tests/objects-finalize.c
@@ -99,7 +99,8 @@ main (int argc, char **argv)
 	  all_types[i] != GDK_TYPE_X11_DISPLAY_MANAGER &&
 #endif
 	  /* Not allowed to finalize a GdkPixbufLoader without calling gdk_pixbuf_loader_close() */
-	  all_types[i] != GDK_TYPE_PIXBUF_LOADER)
+	  all_types[i] != GDK_TYPE_PIXBUF_LOADER &&
+	  all_types[i] != gdk_pixbuf_simple_anim_iter_get_type())
 	{
 	  gchar *test_path = g_strdup_printf ("/FinalizeObject/%s", g_type_name (all_types[i]));
 
