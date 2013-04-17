19560bf0d44e6adb03c1cb88bf89b92ce3ea9c8b
Alexander Larsson <alexl@redhat.com>
2013-04-17 14:15:51 +0200

    gdkwindow: Remove translate vfunc
    This is not used anymore

--- gdk/wayland/gdkwindow-wayland.c
+++ gdk/wayland/gdkwindow-wayland.c
@@ -1194,15 +1194,6 @@ gdk_wayland_window_queue_antiexpose (GdkWindow *window,
 }
 
 static void
-gdk_wayland_window_translate (GdkWindow      *window,
-			      cairo_region_t *area,
-			      gint            dx,
-			      gint            dy)
-{
-  _gdk_window_invalidate_for_expose (window, area);
-}
-
-static void
 gdk_wayland_window_destroy (GdkWindow *window,
 			    gboolean   recursing,
 			    gboolean   foreign_destroy)
@@ -1908,7 +1899,6 @@ _gdk_window_impl_wayland_class_init (GdkWindowImplWaylandClass *klass)
   impl_class->input_shape_combine_region = gdk_window_wayland_input_shape_combine_region;
   impl_class->set_static_gravities = gdk_window_wayland_set_static_gravities;
   impl_class->queue_antiexpose = gdk_wayland_window_queue_antiexpose;
-  impl_class->translate = gdk_wayland_window_translate;
   impl_class->destroy = gdk_wayland_window_destroy;
   impl_class->destroy_foreign = gdk_window_wayland_destroy_foreign;
   impl_class->resize_cairo_surface = gdk_window_wayland_resize_cairo_surface;
