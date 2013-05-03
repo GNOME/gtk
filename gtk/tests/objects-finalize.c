c538bdc7dcdb39c9d35d6996041a93ee6ea61986
Matthias Clasen <mclasen@redhat.com>
2013-05-02 20:43:34 -0400

    GtkInvisible behaves like GtkWindow
    As far as refcounting is concerned, invisibles are like
    windows: you have to destroy them.

--- gtk/tests/objects-finalize.c
+++ gtk/tests/objects-finalize.c
@@ -63,7 +63,7 @@ test_finalize_object (gconstpointer data)
   g_object_weak_ref (object, check_finalized, &finalized);
 
   /* Toplevels are owned by GTK+, just tell GTK+ to destroy it */
-  if (GTK_IS_WINDOW (object))
+  if (GTK_IS_WINDOW (object) || GTK_IS_INVISIBLE (object))
     gtk_widget_destroy (GTK_WIDGET (object));
   else
     g_object_unref (object);
