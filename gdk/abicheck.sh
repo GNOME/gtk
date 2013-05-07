019ad96479a9bafbf2948aa240dd57e2229f5985
Matthias Clasen <mclasen@redhat.com>
2013-05-07 06:00:13 -0400

    Remove abi checking scripts
    The exported symbols are now controlled explicitly from the headers,
    no need to double-check.

--- gdk/abicheck.sh
+++
@@ -1,5 +0,0 @@
-#! /bin/sh
-
-cpp -P -DGDK_WINDOWING_X11  ${srcdir:-.}/gdk.symbols | sed -e '/^$/d' -e 's/ G_GNUC.*$//' | sort | uniq > expected-abi
-nm -D -g --defined-only .libs/libgdk-3.so | cut -d ' ' -f 3 | egrep -v '^(__bss_start|_edata|_end)' | sort > actual-abi
-diff -u expected-abi actual-abi && rm -f expected-abi actual-abi
--- gtk/abicheck.sh
+++
@@ -1,5 +0,0 @@
-#! /bin/sh
-
-cpp -P -DG_OS_UNIX -DGDK_WINDOWING_X11 ${srcdir:-.}/gtk.symbols | sed -e '/^$/d' -e 's/ G_GNUC.*$//' -e 's/ PRIVATE//' | sort > expected-abi
-nm -D -g --defined-only .libs/libgtk-3.so | cut -d ' ' -f 3 | egrep -v '^(__bss_start|_edata|_end)' | sort > actual-abi
-diff -u expected-abi actual-abi && rm -f expected-abi actual-abi
