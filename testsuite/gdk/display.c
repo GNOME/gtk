c9890d6cbb353e2df77f8a675d82e674a894ae06
Matthias Clasen <mclasen@redhat.com>
2013-05-29 21:11:14 -0400

    Port display tests to g_test_trap_subprocess

--- testsuite/gdk/display.c
+++ testsuite/gdk/display.c
@@ -3,61 +3,63 @@
 #include <gdk/gdk.h>
 
 static void
+test_unset_display_subprocess1 (void)
+{
+  GdkDisplayManager *manager;
+
+  g_unsetenv ("DISPLAY");
+
+  g_assert (!gdk_init_check (NULL, NULL));
+  manager = gdk_display_manager_get ();
+  g_assert (manager != NULL);
+  g_assert (gdk_display_manager_get_default_display (manager) == NULL);
+}
+
+static void
+test_unset_display_subprocess2 (void)
+{
+  g_unsetenv ("DISPLAY");
+  gdk_init (NULL, NULL);
+}
+
+static void
 test_unset_display (void)
 {
-  if (g_test_trap_fork (0, 0))//G_TEST_TRAP_SILENCE_STDOUT | G_TEST_TRAP_SILENCE_STDERR))
-    {
-      GdkDisplayManager *manager;
-
-      g_unsetenv ("DISPLAY");
-
-      g_assert (!gdk_init_check (NULL, NULL));
-      manager = gdk_display_manager_get ();
-      g_assert (manager != NULL);
-      g_assert (gdk_display_manager_get_default_display (manager) == NULL);
-
-      exit (0);
-    }
+  g_test_trap_subprocess ("/display/unset-display/subprocess/1", 0, 0);
   g_test_trap_assert_passed ();
 
-  if (g_test_trap_fork (0, G_TEST_TRAP_SILENCE_STDOUT | G_TEST_TRAP_SILENCE_STDERR))
-    {
-      g_unsetenv ("DISPLAY");
-
-      gdk_init (NULL, NULL);
-
-      exit (0);
-    }
+  g_test_trap_subprocess ("/display/unset-display/subprocess/2", 0, 0);
   g_test_trap_assert_failed ();
   g_test_trap_assert_stderr ("*cannot open display*");
 }
 
 static void
+test_bad_display_subprocess1 (void)
+{
+  GdkDisplayManager *manager;
+
+  g_setenv ("DISPLAY", "poo", TRUE);
+
+  g_assert (!gdk_init_check (NULL, NULL));
+  manager = gdk_display_manager_get ();
+  g_assert (manager != NULL);
+  g_assert (gdk_display_manager_get_default_display (manager) == NULL);
+}
+
+static void
+test_bad_display_subprocess2 (void)
+{
+  g_setenv ("DISPLAY", "poo", TRUE);
+  gdk_init (NULL, NULL);
+}
+
+static void
 test_bad_display (void)
 {
-  if (g_test_trap_fork (0, G_TEST_TRAP_SILENCE_STDOUT | G_TEST_TRAP_SILENCE_STDERR))
-    {
-      GdkDisplayManager *manager;
-
-      g_setenv ("DISPLAY", "poo", TRUE);
-
-      g_assert (!gdk_init_check (NULL, NULL));
-      manager = gdk_display_manager_get ();
-      g_assert (manager != NULL);
-      g_assert (gdk_display_manager_get_default_display (manager) == NULL);
-
-      exit (0);
-    }
+  g_test_trap_subprocess ("/display/bad-display/subprocess/1", 0, 0);
   g_test_trap_assert_passed ();
 
-  if (g_test_trap_fork (0, G_TEST_TRAP_SILENCE_STDOUT | G_TEST_TRAP_SILENCE_STDERR))
-    {
-      g_setenv ("DISPLAY", "poo", TRUE);
-
-      gdk_init (NULL, NULL);
-
-      exit (0);
-    }
+  g_test_trap_subprocess ("/display/bad-display/subprocess/2", 0, 0);
   g_test_trap_assert_failed ();
   g_test_trap_assert_stderr ("*cannot open display*");
 }
@@ -68,7 +70,11 @@ main (int argc, char *argv[])
   g_test_init (&argc, &argv, NULL);
 
   g_test_add_func ("/display/unset-display", test_unset_display);
+  g_test_add_func ("/display/unset-display/subprocess/1", test_unset_display_subprocess1);
+  g_test_add_func ("/display/unset-display/subprocess/2", test_unset_display_subprocess2);
   g_test_add_func ("/display/bad-display", test_bad_display);
+  g_test_add_func ("/display/bad-display/subprocess/1", test_bad_display_subprocess1);
+  g_test_add_func ("/display/bad-display/subprocess/2", test_bad_display_subprocess2);
 
   return g_test_run ();
 }
