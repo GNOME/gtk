GNOME/gtk

commit e9f182e37a7f6e2dc339054841a3c9f930f573ed

Fix a few memory leaks wrt to translations
Pointed out in https://bugzilla.gnome.org/show_bug.cgi?id=699016
The fix here is slightly different. We make
_gtk_builder_parser_translate return a const char * instead of
a dup'ed string, and fix up the callers.

gtk/gtkbuilderparser.c
@@ -1015,19 +1015,19 @@ start_element (GMarkupParseContext *context,
 		   element_name);
 }
 
-gchar *
+const gchar *
 _gtk_builder_parser_translate (const gchar *domain,
 			       const gchar *context,
 			       const gchar *text)
 {
-  const char *s;
+  const gchar *s;
 
   if (context)
     s = g_dpgettext2 (domain, context, text);
   else
     s = g_dgettext (domain, text);
 
-  return g_strdup (s);
+  return s;
 }
 
 /* Called for close tags </foo> */
@@ -1128,15 +1128,14 @@ end_element (GMarkupParseContext *context,
 
           if (prop_info->translatable && prop_info->text->len)
             {
-	      prop_info->data = _gtk_builder_parser_translate (data->domain,
-							       prop_info->context,
-							       prop_info->text->str);
+              prop_info->data = g_strdup (_gtk_builder_parser_translate (data->domain,
+                                                                         prop_info->context,
+                                                                         prop_info->text->str));
               g_string_free (prop_info->text, TRUE);
             }
           else
             {
               prop_info->data = g_string_free (prop_info->text, FALSE);
-              
             }
 
           object_info->properties =

gtk/gtkcelllayout.c
@@ -812,7 +812,7 @@ cell_packing_end_element (GMarkupParseContext *context,
       /* translate the string */
       if (parser_data->string->len && parser_data->translatable)
 	{
-	  gchar *translated;
+	  const gchar *translated;
 	  const gchar* domain;
 
 	  domain = gtk_builder_get_translation_domain (parser_data->builder);

gtk/gtkcomboboxtext.c
@@ -234,7 +234,7 @@ item_end_element (GMarkupParseContext *context,
     {
       if (data->translatable)
 	{
-	  gchar *translated;
+	  const gchar *translated;
 
 	  /* FIXME: This will not use the domain set in the .ui file,
 	   * since the parser is not telling the builder about the domain.

gtk/gtkcontainer.c
@@ -664,8 +664,8 @@ attributes_end_element (GMarkupParseContext *context,
   /* translate the string */
   if (parser_data->string->len && parser_data->translatable)
     {
-      gchar *translated;
-      const gchar* domain;
+      const gchar *translated;
+      const gchar *domain;
 
       domain = gtk_builder_get_translation_domain (parser_data->builder);
 

gtk/gtkbuilderprivate.h
@@ -148,9 +148,9 @@ gboolean  _gtk_builder_flags_from_string (GType       type,
 					  const char *string,
 					  guint      *value,
 					  GError    **error);
-gchar * _gtk_builder_parser_translate (const gchar *domain,
-				       const gchar *context,
-				       const gchar *text);
+const gchar * _gtk_builder_parser_translate (const gchar *domain,
+                                             const gchar *context,
+                                             const gchar *text);
 gchar *   _gtk_builder_get_resource_path (GtkBuilder *builder,
 					  const gchar *string);
 gchar *   _gtk_builder_get_absolute_filename (GtkBuilder *builder,

gtk/gtkliststore.c
@@ -2555,9 +2555,9 @@ list_store_text (GMarkupParseContext *context,
        * since the parser is not telling the builder about the domain.
        * However, it will work for gtk_builder_set_translation_domain() calls.
        */
-      translated = _gtk_builder_parser_translate (data->domain,
-						  info->context,
-						  string);
+      translated = g_strdup (_gtk_builder_parser_translate (data->domain,
+                                                            info->context,
+                                                            string));
       g_free (string);
       string = translated;
     }

gtk/gtkscale.c
@@ -1880,7 +1880,7 @@ gtk_scale_buildable_custom_finished (GtkBuildable *buildable,
   if (strcmp (tagname, "marks") == 0)
     {
       GSList *m;
-      gchar *markup;
+      const gchar *markup;
 
       marks_data = (MarksSubparserData *)user_data;
 

gtk/gtkwidget.c
@@ -13522,7 +13522,7 @@ gtk_widget_buildable_custom_finished (GtkBuildable *buildable,
 
 	          if (i < n_actions)
                     {
-                      gchar *description;
+                      const gchar *description;
 
                       if (action_data->translatable && action_data->description->len)
                         description = _gtk_builder_parser_translate (gtk_builder_get_translation_domain (builder),

commit a37450110b74605041318a76dac51838953d406d

Fix string allocation handling with GtkBuilder translations
Use g_string_assign to avoid issues with assigning GString's internal
buffer back to the same string. This can happen when no translations are
available and _gtk_builder_parser_translate returns back the same
pointer that was passed in.

This fixes a regression from commit e9f182e37a7f6e2dc339054841a3c9f930f
that caused GtkComboBoxText <items> from GtkBuilder to show up empty if
no translations are available.

https://bugzilla.gnome.org/show_bug.cgi?id=700629

gtk/gtkbuilderparser.c
@@ -1128,15 +1128,15 @@ end_element (GMarkupParseContext *context,
 
           if (prop_info->translatable && prop_info->text->len)
             {
-              prop_info->data = g_strdup (_gtk_builder_parser_translate (data->domain,
-                                                                         prop_info->context,
-                                                                         prop_info->text->str));
-              g_string_free (prop_info->text, TRUE);
+              const gchar *translated;
+
+              translated = _gtk_builder_parser_translate (data->domain,
+                                                          prop_info->context,
+                                                          prop_info->text->str);
+              g_string_assign (prop_info->text, translated);
             }
-          else
-            {
-              prop_info->data = g_string_free (prop_info->text, FALSE);
-            }
+
+          prop_info->data = g_string_free (prop_info->text, FALSE);
 
           object_info->properties =
             g_slist_prepend (object_info->properties, prop_info);

gtk/gtkcelllayout.c
@@ -820,8 +820,7 @@ cell_packing_end_element (GMarkupParseContext *context,
 	  translated = _gtk_builder_parser_translate (domain,
 						      parser_data->context,
 						      parser_data->string->str);
-	  g_string_set_size (parser_data->string, 0);
-	  g_string_append (parser_data->string, translated);
+	  g_string_assign (parser_data->string, translated);
 	}
 
       if (parser_data->cell_prop_name)

gtk/gtkcomboboxtext.c
@@ -240,8 +240,7 @@ item_end_element (GMarkupParseContext *context,
 	  translated = _gtk_builder_parser_translate (data->domain,
 						      data->context,
 						      data->string->str);
-	  g_string_set_size (data->string, 0);
-	  g_string_append (data->string, translated);
+	  g_string_assign (data->string, translated);
 	}
 
       gtk_combo_box_text_append (GTK_COMBO_BOX_TEXT (data->object), data->id, data->string->str);

gtk/gtkcontainer.c
@@ -667,17 +667,15 @@ attributes_end_element (GMarkupParseContext *context,
   /* translate the string */
   if (parser_data->string->len && parser_data->translatable)
     {
-      gchar *translated;
+      const gchar *translated;
       const gchar *domain;
 
       domain = gtk_builder_get_translation_domain (parser_data->builder);
 
-      translated = g_strdup (_gtk_builder_parser_translate (domain,
-                                                            parser_data->context,
-                                                            parser_data->string->str));
-      g_string_set_size (parser_data->string, 0);
-      g_string_append (parser_data->string, translated);
-      g_free (translated);
+      translated = _gtk_builder_parser_translate (domain,
+                                                  parser_data->context,
+                                                  parser_data->string->str);
+      g_string_assign (parser_data->string, translated);
     }
 
   if (parser_data->child_prop_name)