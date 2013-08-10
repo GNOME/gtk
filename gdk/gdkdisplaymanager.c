04d6d622d52d544728563a83e76070748af9e6f4
Matthias Clasen <mclasen@redhat.com>
2013-08-09 20:50:57 -0400

    Try Wayland before X11
    When both the Wayland and the X11 backends are built, prefer
    Wayland over X11 in the absence of other information.
    
    https://bugzilla.gnome.org/show_bug.cgi?id=705498

--- gdk/gdkdisplaymanager.c
+++ gdk/gdkdisplaymanager.c
@@ -267,11 +267,11 @@ static GdkBackend gdk_backends[] = {
 #ifdef GDK_WINDOWING_WIN32
   { "win32",    _gdk_win32_display_open },
 #endif
-#ifdef GDK_WINDOWING_X11
-  { "x11",      _gdk_x11_display_open },
-#endif
 #ifdef GDK_WINDOWING_WAYLAND
   { "wayland",  _gdk_wayland_display_open },
+#endif
+#ifdef GDK_WINDOWING_X11
+  { "x11",      _gdk_x11_display_open },
 #endif
 #ifdef GDK_WINDOWING_BROADWAY
   { "broadway", _gdk_broadway_display_open },
