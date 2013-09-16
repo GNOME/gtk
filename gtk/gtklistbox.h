e6ac9b08621a75a5f16383ab3f6a3ff5c9e3da37
Timm Bäder <mail@baedert.org>
2013-09-16 20:08:35 +0200

    ListBox: Update docs of GtkListBoxUpdateHeaderFunc
    before is always NULL for the first row.

--- gtk/gtklistbox.h
+++ gtk/gtklistbox.h
@@ -130,7 +130,7 @@ typedef gint (*GtkListBoxSortFunc) (GtkListBoxRow *row1,
 /**
  * GtkListBoxUpdateHeaderFunc:
  * @row: the row to update
- * @before: the row before @row, or %NULL if it is first
+ * @before: (allow-none): the row before @row, or %NULL if it is first
  * @user_data: (closure): user data
  *
  * Whenever @row changes or which row is before @row changes this
