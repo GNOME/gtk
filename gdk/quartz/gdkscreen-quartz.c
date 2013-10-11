4cf5ce2e08e3a94db9f081fe91269c16d8a2c31d
Charles Reiss <woggling@gmail.com>
2013-10-11 15:53:37 -0700

    Bug 709939 - monitor reconfiguration while GtkOffscreenWindows
    exist triggers crash on quartz. Special case the position update
    to ignore the off-screen windows.

--- gdk/quartz/gdkscreen-quartz.c
+++ gdk/quartz/gdkscreen-quartz.c
@@ -208,7 +208,11 @@ _gdk_quartz_screen_update_window_sizes (GdkScreen *screen)
   windows = gdk_screen_get_toplevel_windows (screen);
 
   for (list = windows; list; list = list->next)
-    _gdk_quartz_window_update_position (list->data);
+    {
+      if (GDK_WINDOW_TYPE(list->data) == GDK_WINDOW_OFFSCREEN)
+        continue;
+      _gdk_quartz_window_update_position (list->data);
+    }
 
   g_list_free (windows);
 }
