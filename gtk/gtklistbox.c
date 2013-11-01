f23e1cdde5f4b46e4146ad2d89cfb780fe1b6e6d
Timm Bäder <mail@baedert.org>
2013-11-01 12:30:07 +0100

    GtkListBox: Expand the placeholder
    https://bugzilla.gnome.org/show_bug.cgi?id=711255

--- gtk/gtklistbox.c
+++ gtk/gtklistbox.c
@@ -1994,7 +1994,7 @@ gtk_list_box_real_size_allocate (GtkWidget     *widget,
     {
       gtk_widget_get_preferred_height_for_width (priv->placeholder,
                                                  allocation->width, &child_min, NULL);
-      header_allocation.height = child_min;
+      header_allocation.height = allocation->height;
       header_allocation.y = child_allocation.y;
       gtk_widget_size_allocate (priv->placeholder,
                                 &header_allocation);
