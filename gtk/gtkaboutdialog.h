bfd27445e6eb66fa5448e333990aba8a26d22882
William Jon McCann <william.jon.mccann@gmail.com>
2013-12-16 11:49:02 -0500

    about dialog: add docs for new GtkLicense values

--- gtk/gtkaboutdialog.h
+++ gtk/gtkaboutdialog.h
@@ -47,13 +47,17 @@ typedef struct _GtkAboutDialogPrivate GtkAboutDialogPrivate;
  * @GTK_LICENSE_UNKNOWN: No license specified
  * @GTK_LICENSE_CUSTOM: A license text is going to be specified by the
  *   developer
- * @GTK_LICENSE_GPL_2_0: The GNU General Public License, version 2.0
- * @GTK_LICENSE_GPL_3_0: The GNU General Public License, version 3.0
- * @GTK_LICENSE_LGPL_2_1: The GNU Lesser General Public License, version 2.1
- * @GTK_LICENSE_LGPL_3_0: The GNU Lesser General Public License, version 3.0
+ * @GTK_LICENSE_GPL_2_0: The GNU General Public License, version 2.0 or later
+ * @GTK_LICENSE_GPL_3_0: The GNU General Public License, version 3.0 or later
+ * @GTK_LICENSE_LGPL_2_1: The GNU Lesser General Public License, version 2.1 or later
+ * @GTK_LICENSE_LGPL_3_0: The GNU Lesser General Public License, version 3.0 or later
  * @GTK_LICENSE_BSD: The BSD standard license
  * @GTK_LICENSE_MIT_X11: The MIT/X11 standard license
  * @GTK_LICENSE_ARTISTIC: The Artistic License, version 2.0
+ * @GTK_LICENSE_GPL_2_0_ONLY: The GNU General Public License, version 2.0 only. Since 3.12.
+ * @GTK_LICENSE_GPL_3_0_ONLY: The GNU General Public License, version 3.0 only. Since 3.12.
+ * @GTK_LICENSE_LGPL_2_1_ONLY: The GNU Lesser General Public License, version 2.1 only. Since 3.12.
+ * @GTK_LICENSE_LGPL_3_0_ONLY: The GNU Lesser General Public License, version 3.0 only. Since 3.12.
  *
  * The type of license for an application.
  *
