ab41ef518af16d94161c1f1b2338c4ed10d45748
William Jon McCann <william.jon.mccann@gmail.com>
2014-01-21 11:31:21 -0500

    docs: add docs for GtkDirectionType and GtkDeleteType

--- gtk/gtkenums.h
+++ gtk/gtkenums.h
@@ -178,6 +178,25 @@ typedef enum
 } GtkButtonBoxStyle;
 
 
+/**
+ * GtkDeleteType:
+ * @GTK_DELETE_CHARS: Delete characters.
+ * @GTK_DELETE_WORD_ENDS: Delete only the portion of the word to the
+ *   left/right of cursor if we're in the middle of a word.
+ * @GTK_DELETE_WORDS: Delete words.
+ * @GTK_DELETE_DISPLAY_LINES: Delete display-lines. Display-lines
+ *   refers to the visible lines, with respect to to the current line
+ *   breaks. As opposed to paragraphs, which are defined by line
+ *   breaks in the input.
+ * @GTK_DELETE_DISPLAY_LINE_ENDS: Delete only the portion of the
+ *   display-line to the left/right of cursor.
+ * @GTK_DELETE_PARAGRAPH_ENDS: Delete to the end of the
+ *   paragraph. Like C-k in Emacs (or its reverse).
+ * @GTK_DELETE_PARAGRAPHS: Delete entire line. Like C-k in pico.
+ * @GTK_DELETE_WHITESPACE: Delete only whitespace. Like M-\ in Emacs.
+ *
+ * See also: #GtkEntry::delete-from-cursor.
+ */
 typedef enum
 {
   GTK_DELETE_CHARS,
@@ -193,6 +212,17 @@ typedef enum
 } GtkDeleteType;
 
 /* Focus movement types */
+/**
+ * GtkDirectionType:
+ * @GTK_DIR_TAB_FORWARD: Move forward.
+ * @GTK_DIR_TAB_BACKWARD: Move backward.
+ * @GTK_DIR_UP: Move up.
+ * @GTK_DIR_DOWN: Move down.
+ * @GTK_DIR_LEFT: Move left.
+ * @GTK_DIR_RIGHT: Move right.
+ *
+ * Focus movement types.
+ */
 typedef enum
 {
   GTK_DIR_TAB_FORWARD,
