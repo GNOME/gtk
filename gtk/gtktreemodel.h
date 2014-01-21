123fa640d26494b7583c03ad02808e6622ad8846
William Jon McCann <william.jon.mccann@gmail.com>
2014-01-20 23:08:34 -0500

    docs: add docs for GtkTreeModelIface

--- gtk/gtktreemodel.h
+++ gtk/gtktreemodel.h
@@ -98,9 +98,41 @@ struct _GtkTreeIter
   gpointer user_data3;
 };
 
+/**
+ * GtkTreeModelIface:
+ * @row_changed: Signal emitted when a row in the model has changed.
+ * @row_inserted: Signal emitted when a new row has been inserted in
+ *    the model.
+ * @row_has_child_toggled: Signal emitted when a row has gotten the
+ *    first child row or lost its last child row.
+ * @row_deleted: Signal emitted when a row has been deleted.
+ * @rows_reordered: Signal emitted when the children of a node in the
+ *    GtkTreeModel have been reordered.
+ * @get_flags: Get #GtkTreeModelFlags supported by this interface.
+ * @get_n_columns: Get the number of columns supported by the model.
+ * @get_column_type: Get the type of the column.
+ * @get_iter: Sets iter to a valid iterator pointing to path.
+ * @get_path: Gets a newly-created #GtkTreePath referenced by iter.
+ * @get_value: Initializes and sets value to that at column.
+ * @iter_next: Sets iter to point to the node following it at the
+ *    current level.
+ * @iter_previous: Sets iter to point to the previous node at the
+ *    current level.
+ * @iter_children: Sets iter to point to the first child of parent.
+ * @iter_has_child: %TRUE if iter has children, %FALSE otherwise.
+ * @iter_n_children: Gets the number of children that iter has.
+ * @iter_nth_child: Sets iter to be the child of parent, using the
+ *    given index.
+ * @iter_parent: Sets iter to be the parent of child.
+ * @ref_node: Lets the tree ref the node.
+ * @unref_node: Lets the tree unref the node.
+ */
 struct _GtkTreeModelIface
 {
+  /*< private >*/
   GTypeInterface g_iface;
+
+  /*< public >*/
 
   /* Signals */
   void         (* row_changed)           (GtkTreeModel *tree_model,
