88a070e84d303d2dd8133268ff1b826dfdf2329e
Matthias Clasen <mclasen@redhat.com>
2014-01-25 14:20:51 -0500

    Exclude GtkPopover::pointing-to from the defaultvalue test

--- testsuite/gtk/defaultvalue.c
+++ testsuite/gtk/defaultvalue.c
@@ -137,6 +137,10 @@ test_type (gconstpointer data)
 	  (strcmp (pspec->name, "use-header-bar") == 0))
 	continue;
 
+      if (g_type_is_a (type, GTK_TYPE_POPOVER) &&
+	  (strcmp (pspec->name, "pointing-to") == 0))
+	continue;
+
       if (g_type_is_a (type, GDK_TYPE_DISPLAY_MANAGER) &&
 	  (strcmp (pspec->name, "default-display") == 0))
 	continue;
