841a8d811a366152e8a45103c55bd39323f60c0f
Matthias Clasen <mclasen@redhat.com>
2014-02-03 16:43:45 -0500

    Update gtkvolumebutton.ui.h
    Removing the tooltips removed some strings.

--- gtk/resources/ui/gtkvolumebutton.ui.h
+++ gtk/resources/ui/gtkvolumebutton.ui.h
@@ -1,10 +1,6 @@
 N_("Volume");
 N_("Turns volume up or down");
-N_("+");
-N_("Volume Up");
 N_("Volume Up");
 N_("Increases the volume");
-N_("-");
-N_("Volume Down");
 N_("Volume Down");
 N_("Decreases the volume");
