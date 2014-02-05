GNOME/gtk

commit 5230cfe805b8f0046896c029612180fc9d4cc5df

GtkLabel: Fix a pointless expression
Clearly, what was meant here was that we create a new attribute
list if either of the input attribute lists are non-NULL.
This was found by Coverity.

gtk/gtklabel.c
@@ -3384,7 +3384,7 @@ gtk_label_ensure_layout (GtkLabel *label)
               pango_attr_list_insert (attrs, attribute);
             }
         }
-      else if (priv->markup_attrs && priv->markup_attrs)
+      else if (priv->markup_attrs || priv->attrs)
         attrs = pango_attr_list_new ();
       else
         attrs = NULL;

commit a0bfbc8ae1720fe9e82b0113df58d90c2931de73

label: Avoid creating new attribute list
Only create an attribute list for merging if we actually need to merge.

This bug was introduced in 5230cfe805b8f0046896c029612180fc9d4cc5df

gtk/gtklabel.c
@@ -3361,7 +3361,7 @@ gtk_label_ensure_layout (GtkLabel *label)
               pango_attr_list_insert (attrs, attribute);
             }
         }
-      else if (priv->markup_attrs || priv->attrs)
+      else if (priv->markup_attrs && priv->attrs)
         attrs = pango_attr_list_new ();
       else
         attrs = NULL;