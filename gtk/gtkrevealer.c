GNOME/gtk

commit b9e37f8d2d4d14d3cb23f56a795fcdd2e6128e88

revealer: Allocate extra size to child
Do not constrain the child widget to its preferred size when the
revealer is larger.

https://bugzilla.gnome.org/show_bug.cgi?id=724742

gtk/gtkrevealer.c
@@ -303,8 +303,8 @@ gtk_revealer_get_child_allocation (GtkRevealer   *revealer,
 
   child_allocation->x = 0;
   child_allocation->y = 0;
-  child_allocation->width = allocation->width;
-  child_allocation->height = allocation->height;
+  child_allocation->width = 0;
+  child_allocation->height = 0;
 
   child = gtk_bin_get_child (GTK_BIN (revealer));
   if (child != NULL && gtk_widget_get_visible (child))
@@ -318,6 +318,9 @@ gtk_revealer_get_child_allocation (GtkRevealer   *revealer,
         gtk_widget_get_preferred_height_for_width (child, child_allocation->width, NULL,
                                                    &child_allocation->height);
     }
+
+  child_allocation->width = MAX (child_allocation->width, allocation->width);
+  child_allocation->height = MAX (child_allocation->height, allocation->height);
 }
 
 static void

commit 9add8e261f8d3e4c99e342e757659ef74f0cf16e

revealer: Pass correct size to child hfw function
The refactoring in b9e37f8d2d4d14d3cb23f56a795fcdd2e6128e88 broke this.

https://bugzilla.gnome.org/show_bug.cgi?id=724999

gtk/gtkrevealer.c
@@ -312,10 +312,10 @@ gtk_revealer_get_child_allocation (GtkRevealer   *revealer,
       transition = effective_transition (revealer);
       if (transition == GTK_REVEALER_TRANSITION_TYPE_SLIDE_LEFT ||
           transition == GTK_REVEALER_TRANSITION_TYPE_SLIDE_RIGHT)
-        gtk_widget_get_preferred_width_for_height (child, child_allocation->height, NULL,
+        gtk_widget_get_preferred_width_for_height (child, allocation->height, NULL,
                                                    &child_allocation->width);
       else
-        gtk_widget_get_preferred_height_for_width (child, child_allocation->width, NULL,
+        gtk_widget_get_preferred_height_for_width (child, allocation->width, NULL,
                                                    &child_allocation->height);
     }
 