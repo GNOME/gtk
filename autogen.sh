949c6a15a6093335fe07ff7016c27b57de406e3e
Andrés G. Aragoneses <knocte@gmail.com>
2014-03-02 09:51:47 +0100

    autogen.sh: fail faster/clearer if g-i not found
    GOBJECT_INTROSPECTION_CHECK macro can be missing if introspection
    is not installed, so this way the following error is prevented:
    
    "gtk/Makefile.am:1324: error: HAVE_INTROSPECTION does not appear
    in AM_CONDITIONAL"
    
    https://bugzilla.gnome.org/show_bug.cgi?id=723438

--- autogen.sh
+++ autogen.sh
@@ -17,6 +17,18 @@ else
         gtkdocize || exit $?
 fi
 
+PKGCONFIG=`which pkg-config`
+if test -z "$PKGCONFIG"; then
+        echo "*** pkg-config not found, please install it ***"
+        exit 1
+fi
+
+pkg-config --print-errors gobject-introspection-1.0
+if [ $? != 0 ]; then
+	echo "You probably need to install 'libgirepository1.0-dev'"
+	exit 1
+fi
+
 # README and INSTALL are required by automake, but may be deleted by clean
 # up rules. to get automake to work, simply touch these here, they will be
 # regenerated from their corresponding *.in files by ./configure anyway.
