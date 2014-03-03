ab69814b5dcd9e98af17401db6571faae1b635c3
Matthias Clasen <mclasen@redhat.com>
2014-03-03 05:50:25 -0500

    Revert "GtkEntryCompletion: fix warning"
    This reverts commit 2f0c610c35215229e53aac4c37fdea49e25f3742.

--- gtk/gtkentrycompletion.c
+++ gtk/gtkentrycompletion.c
@@ -833,7 +833,7 @@ gtk_entry_completion_clear_text_column_renderer (GtkEntryCompletion *completion)
       GtkCellArea *area;
 
       area = gtk_entry_completion_get_area (GTK_CELL_LAYOUT (completion));
-      gtk_cell_layout_clear (GTK_CELL_LAYOUT (area));
+      gtk_cell_layout_clear (area);
 
       completion->priv->text_column = -1;
       g_object_notify (G_OBJECT (completion), "text_column");
