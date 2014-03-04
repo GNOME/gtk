d60a903407ada24b8712d1769ddfd2c6a892f284
Marek Kasik <mkasik@redhat.com>
2014-03-04 15:17:23 +0100

    GtkIconViewAccessible: Don't access freed item
    Don't get index from deleted GtkIconViewItem.
    Remove the item before traversing list of items.
    
    https://bugzilla.gnome.org/show_bug.cgi?id=701884

--- gtk/a11y/gtkiconviewaccessible.c
+++ gtk/a11y/gtkiconviewaccessible.c
@@ -1126,7 +1126,7 @@ gtk_icon_view_accessible_model_row_deleted (GtkTreeModel *tree_model,
         {
           deleted_item = items;
         }
-      if (info->index != item->item->index)
+      else if (info->index != item->item->index)
         {
           if (tmp_list == NULL)
             tmp_list = items;
@@ -1136,7 +1136,6 @@ gtk_icon_view_accessible_model_row_deleted (GtkTreeModel *tree_model,
 
       items = items->next;
     }
-  gtk_icon_view_accessible_traverse_items (view, tmp_list);
   if (deleted_item)
     {
       info = deleted_item->data;
@@ -1147,6 +1146,7 @@ gtk_icon_view_accessible_model_row_deleted (GtkTreeModel *tree_model,
       g_object_unref (info->item);
       g_free (info);
     }
+  gtk_icon_view_accessible_traverse_items (view, tmp_list);
 
   return;
 }
