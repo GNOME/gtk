2a0167f82c8a9bd890822491e3d092fa1c648e63
Matthias Clasen <mclasen@redhat.com>
2014-04-17 11:27:11 -0700

    example: Fix indentation

--- examples/application6/exampleappwin.c
+++ examples/application6/exampleappwin.c
@@ -2,19 +2,22 @@
 #include "exampleappwin.h"
 #include <gtk/gtk.h>
 
-struct ExampleAppWindow {
-        GtkApplicationWindow parent;
+struct ExampleAppWindow
+{
+  GtkApplicationWindow parent;
 };
 
-struct ExampleAppWindowClass {
-        GtkApplicationWindowClass parent_class;
+struct ExampleAppWindowClass
+{
+  GtkApplicationWindowClass parent_class;
 };
 
 typedef struct ExampleAppWindowPrivate ExampleAppWindowPrivate;
 
-struct ExampleAppWindowPrivate {
-        GSettings *settings;
-        GtkWidget *stack;
+struct ExampleAppWindowPrivate
+{
+  GSettings *settings;
+  GtkWidget *stack;
 };
 
 G_DEFINE_TYPE_WITH_PRIVATE(ExampleAppWindow, example_app_window, GTK_TYPE_APPLICATION_WINDOW);
@@ -22,87 +25,88 @@ G_DEFINE_TYPE_WITH_PRIVATE(ExampleAppWindow, example_app_window, GTK_TYPE_APPLIC
 static void
 example_app_window_init (ExampleAppWindow *win)
 {
-        ExampleAppWindowPrivate *priv;
+  ExampleAppWindowPrivate *priv;
 
-        priv = example_app_window_get_instance_private (win);
-        gtk_widget_init_template (GTK_WIDGET (win));
-        priv->settings = g_settings_new ("org.gtk.exampleapp");
+  priv = example_app_window_get_instance_private (win);
+  gtk_widget_init_template (GTK_WIDGET (win));
+  priv->settings = g_settings_new ("org.gtk.exampleapp");
 
-        g_settings_bind (priv->settings, "transition",
-                         priv->stack, "transition-type",
-                         G_SETTINGS_BIND_DEFAULT);
+  g_settings_bind (priv->settings, "transition",
+                   priv->stack, "transition-type",
+                   G_SETTINGS_BIND_DEFAULT);
 }
 
 static void
 example_app_window_dispose (GObject *object)
 {
-        ExampleAppWindow *win;
-        ExampleAppWindowPrivate *priv;
+  ExampleAppWindow *win;
+  ExampleAppWindowPrivate *priv;
 
-        win = EXAMPLE_APP_WINDOW (object);
-        priv = example_app_window_get_instance_private (win);
+  win = EXAMPLE_APP_WINDOW (object);
+  priv = example_app_window_get_instance_private (win);
 
-        g_clear_object (&priv->settings);
+  g_clear_object (&priv->settings);
 
-        G_OBJECT_CLASS (example_app_window_parent_class)->dispose (object);
+  G_OBJECT_CLASS (example_app_window_parent_class)->dispose (object);
 }
 
 static void
 example_app_window_class_init (ExampleAppWindowClass *class)
 {
-        G_OBJECT_CLASS (class)->dispose = example_app_window_dispose;
+  G_OBJECT_CLASS (class)->dispose = example_app_window_dispose;
 
-        gtk_widget_class_set_template_from_resource (GTK_WIDGET_CLASS (class),
-                                                     "/org/gtk/exampleapp/window.ui");
-        gtk_widget_class_bind_template_child_private (GTK_WIDGET_CLASS (class), ExampleAppWindow, stack);
+  gtk_widget_class_set_template_from_resource (GTK_WIDGET_CLASS (class),
+                                               "/org/gtk/exampleapp/window.ui");
+  gtk_widget_class_bind_template_child_private (GTK_WIDGET_CLASS (class), ExampleAppWindow, stack);
 }
 
 ExampleAppWindow *
 example_app_window_new (ExampleApp *app)
 {
-        return g_object_new (EXAMPLE_APP_WINDOW_TYPE, "application", app, NULL);
+  return g_object_new (EXAMPLE_APP_WINDOW_TYPE, "application", app, NULL);
 }
 
 void
 example_app_window_open (ExampleAppWindow *win,
                          GFile            *file)
 {
-        ExampleAppWindowPrivate *priv;
-        gchar *basename;
-        GtkWidget *scrolled, *view;
-        gchar *contents;
-        gsize length;
-        GtkTextBuffer *buffer;
-        GtkTextTag *tag;
-        GtkTextIter start_iter, end_iter;
+  ExampleAppWindowPrivate *priv;
+  gchar *basename;
+  GtkWidget *scrolled, *view;
+  gchar *contents;
+  gsize length;
+  GtkTextBuffer *buffer;
+  GtkTextTag *tag;
+  GtkTextIter start_iter, end_iter;
 
-        priv = example_app_window_get_instance_private (win);
-        basename = g_file_get_basename (file);
+  priv = example_app_window_get_instance_private (win);
+  basename = g_file_get_basename (file);
 
-        scrolled = gtk_scrolled_window_new (NULL, NULL);
-        gtk_widget_show (scrolled);
-        gtk_widget_set_hexpand (scrolled, TRUE);
-        gtk_widget_set_vexpand (scrolled, TRUE);
-        view = gtk_text_view_new ();
-        gtk_text_view_set_editable (GTK_TEXT_VIEW (view), FALSE);
-        gtk_text_view_set_cursor_visible (GTK_TEXT_VIEW (view), FALSE);
-        gtk_widget_show (view);
-        gtk_container_add (GTK_CONTAINER (scrolled), view);
-        gtk_stack_add_titled (GTK_STACK (priv->stack), scrolled, basename, basename);
+  scrolled = gtk_scrolled_window_new (NULL, NULL);
+  gtk_widget_show (scrolled);
+  gtk_widget_set_hexpand (scrolled, TRUE);
+  gtk_widget_set_vexpand (scrolled, TRUE);
+  view = gtk_text_view_new ();
+  gtk_text_view_set_editable (GTK_TEXT_VIEW (view), FALSE);
+  gtk_text_view_set_cursor_visible (GTK_TEXT_VIEW (view), FALSE);
+  gtk_widget_show (view);
+  gtk_container_add (GTK_CONTAINER (scrolled), view);
+  gtk_stack_add_titled (GTK_STACK (priv->stack), scrolled, basename, basename);
 
-        buffer = gtk_text_view_get_buffer (GTK_TEXT_VIEW (view));
+  buffer = gtk_text_view_get_buffer (GTK_TEXT_VIEW (view));
 
-        if (g_file_load_contents (file, NULL, &contents, &length, NULL, NULL)) {
-                gtk_text_buffer_set_text (buffer, contents, length);
-                g_free (contents);
-        }
+  if (g_file_load_contents (file, NULL, &contents, &length, NULL, NULL))
+    {
+      gtk_text_buffer_set_text (buffer, contents, length);
+      g_free (contents);
+    }
 
-        tag = gtk_text_buffer_create_tag (buffer, NULL, NULL);
-        g_settings_bind (priv->settings, "font", tag, "font", G_SETTINGS_BIND_DEFAULT);
+  tag = gtk_text_buffer_create_tag (buffer, NULL, NULL);
+  g_settings_bind (priv->settings, "font", tag, "font", G_SETTINGS_BIND_DEFAULT);
 
-        gtk_text_buffer_get_start_iter (buffer, &start_iter);
-        gtk_text_buffer_get_end_iter (buffer, &end_iter);
-        gtk_text_buffer_apply_tag (buffer, tag, &start_iter, &end_iter);
+  gtk_text_buffer_get_start_iter (buffer, &start_iter);
+  gtk_text_buffer_get_end_iter (buffer, &end_iter);
+  gtk_text_buffer_apply_tag (buffer, tag, &start_iter, &end_iter);
 
-        g_free (basename);
+  g_free (basename);
 }
