71fe1a5fe31d234c110f5cd27eb6fcb040af4b7d
Benjamin Otte <otte@redhat.com>
2014-04-30 08:37:52 +0200

    alignment: Remove unneeded checks
    Size vfuncs always get non-null out variables passed, so no need to
    check for NULL.

--- gtk/gtkalignment.c
+++ gtk/gtkalignment.c
@@ -679,11 +679,8 @@ gtk_alignment_get_preferred_size (GtkWidget      *widget,
       natural += child_nat;
     }
 
-  if (minimum_size)
-    *minimum_size = minimum;
-
-  if (natural_size)
-    *natural_size = natural;
+  *minimum_size = minimum;
+  *natural_size = natural;
 }
 
 static void
