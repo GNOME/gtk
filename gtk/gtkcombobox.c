GNOME/gtk

commit 7f60cab47d9651ed3ed53b86f1f74de71b55eee0

combobox: fix a segfault
Don't dereference a NULL pointer.

gtk/gtkcombobox.c
@@ -5602,11 +5602,11 @@ gtk_combo_box_get_preferred_height (GtkWidget *widget,
                                     gint      *minimum_size,
                                     gint      *natural_size)
 {
-  gint min_width;
+  gint min_width, nat_width;
 
   /* Combo box is height-for-width only
    * (so we always just reserve enough height for the minimum width) */
-  GTK_WIDGET_GET_CLASS (widget)->get_preferred_width (widget, &min_width, NULL);
+  GTK_WIDGET_GET_CLASS (widget)->get_preferred_width (widget, &min_width, &nat_width);
   GTK_WIDGET_GET_CLASS (widget)->get_preferred_height_for_width (widget, min_width, minimum_size, natural_size);
 }
 

commit a89c5986177385117ae14c95c38b43b2fe92c896

combobox: Add workaround so evolution doesn't crash
Removing the NULL checks in 7f60cab47d9651ed3ed53b86f1f74de71b55eee0
caused evolution to crash. It chains up with NULL and that used to work.

This patch should be reverted for GTK 4.0.

https://bugzilla.gnome.org/show_bug.cgi?id=729496

gtk/gtkcombobox.c
@@ -5505,6 +5505,11 @@ gtk_combo_box_get_preferred_width (GtkWidget *widget,
   gint                   child_min, child_nat;
   GtkBorder              padding;
   gfloat                 arrow_scaling;
+  gint                   dummy;
+
+  /* https://bugzilla.gnome.org/show_bug.cgi?id=729496 */
+  if (natural_size == NULL)
+    natural_size = &dummy;
 
   child = gtk_bin_get_child (GTK_BIN (widget));
 