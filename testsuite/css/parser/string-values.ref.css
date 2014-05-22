GNOME/gtk

commit 65c4c1555d0634c063c3aa354620de260e4aaf7f

css: Fix printing code for CSS strings and idents
It used to infloop on special characters.

gtk/gtkcssparser.c
@@ -1051,6 +1051,7 @@ _gtk_css_print_string (GString    *str,
         g_assert_not_reached ();
         break;
       }
+    string++;
   } while (*string);
 
   g_string_append_c (str, '"');

gtk/gtkcssstringvalue.c
@@ -111,6 +111,7 @@ gtk_css_value_ident_print (const GtkCssValue *value,
         g_assert_not_reached ();
         break;
       }
+    string++;
   } while (*string);
 }
 

commit b90802f7e44f280b9ea233cf71692704e5b52651

css: Actually break at end of the string
Introduced in 65c4c1555d0634c063c3aa354620de260e4aaf7f.

Found by gnome-continuous running the testsuite.

Includes fixing a broken test in the testsuite.

gtk/gtkcssparser.c
@@ -1025,13 +1025,13 @@ _gtk_css_print_string (GString    *str,
   g_string_append_c (str, '"');
 
   do {
-    len = strcspn (string, "\"\n\r\f");
+    len = strcspn (string, "\\\"\n\r\f");
     g_string_append_len (str, string, len);
     string += len;
     switch (*string)
       {
       case '\0':
-        break;
+        goto out;
       case '\n':
         g_string_append (str, "\\A ");
         break;
@@ -1054,6 +1054,7 @@ _gtk_css_print_string (GString    *str,
     string++;
   } while (*string);
 
+out:
   g_string_append_c (str, '"');
 }
 

gtk/gtkcssstringvalue.c
@@ -88,7 +88,7 @@ gtk_css_value_ident_print (const GtkCssValue *value,
     switch (*string)
       {
       case '\0':
-        break;
+        goto out;
       case '\n':
         g_string_append (str, "\\A ");
         break;
@@ -113,6 +113,9 @@ gtk_css_value_ident_print (const GtkCssValue *value,
       }
     string++;
   } while (*string);
+
+out:
+  ;
 }
 
 static const GtkCssValueClass GTK_CSS_VALUE_STRING = {

testsuite/css/parser/Makefile.am
@@ -334,6 +334,7 @@ test_data = \
 	single-slash.errors\
 	single-slash.ref.css \
 	string-values.css \
+	string-values.ref.css \
 	test.png \
 	transition.css \
 	transition.ref.css \

testsuite/css/parser/string-values.ref.css
@@ -1,0 +1,27 @@
+n {
+  font-family: "\A ";
+}
+
+r {
+  font-family: "\C ";
+}
+
+f {
+  font-family: "\D ";
+}
+
+a {
+  font-family: "\"";
+}
+
+b {
+  font-family: "'";
+}
+
+c {
+  font-family: "'";
+}
+
+d {
+  font-family: "\\";
+}