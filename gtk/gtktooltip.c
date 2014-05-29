73864a350bf4c68213eddead3f239c3cf0f171d7
Robert Ancell <robert.ancell@canonical.com>
2014-05-29 17:58:58 +0200

    Draw transient windows onto their parents

--- gtk/gtktooltip.c
+++ gtk/gtktooltip.c
@@ -39,6 +39,9 @@
 #ifdef GDK_WINDOWING_WAYLAND
 #include "wayland/gdkwayland.h"
 #endif
+#ifdef GDK_WINDOWING_MIR
+#include "mir/gdkmir.h"
+#endif
 
 
 /**
@@ -1228,6 +1231,19 @@ found:
       /* set the transient parent on the tooltip when running with the Wayland
        * backend to allow correct positioning of the tooltip windows */
       if (GDK_IS_WAYLAND_DISPLAY (display))
+        {
+          GtkWidget *toplevel;
+
+          toplevel = gtk_widget_get_toplevel (tooltip->tooltip_widget);
+          if (GTK_IS_WINDOW (toplevel))
+            gtk_window_set_transient_for (GTK_WINDOW (tooltip->current_window),
+                                          GTK_WINDOW (toplevel));
+        }
+#endif
+#ifdef GDK_WINDOWING_MIR
+      /* Set the transient parent on the tooltip when running with the Mir
+       * backend to allow correct positioning of the tooltip windows */
+      if (GDK_IS_MIR_DISPLAY (display))
         {
           GtkWidget *toplevel;
 
