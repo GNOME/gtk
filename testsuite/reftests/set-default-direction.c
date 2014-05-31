a18ec83b3b5c0ceb3e9e757411b9b2e5e7e74af6
Benjamin Otte <otte@redhat.com>
2014-05-31 05:17:51 +0200

    reftests: Add reftest for direction change
    This reftest makes use of the new feature to add signal handlers.
    
    It adds a libreftest.so module containing all the code for the reftests.
    When adding a test named reftest.ui, please keep code contained in a
    source file names reftest.c and add that file to Makefile.am.
    
    https://bugzilla.gnome.org/show_bug.cgi?id=730833

---
+++ testsuite/reftests/set-default-direction.c
@@ -0,0 +1,74 @@
+/*
+ * Copyright (C) 2014 Red Hat Inc.
+ *
+ * Author:
+ *      Benjamin Otte <otte@gnome.org>
+ *
+ * This library is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Library General Public
+ * License as published by the Free Software Foundation; either
+ * version 2 of the License, or (at your option) any later version.
+ *
+ * This library is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * Library General Public License for more details.
+ *
+ * You should have received a copy of the GNU Library General Public
+ * License along with this library. If not, see <http://www.gnu.org/licenses/>.
+ */
+
+#include "config.h"
+
+#include <gtk/gtk.h>
+
+G_MODULE_EXPORT void
+switch_default_direction (void)
+{
+  switch (gtk_widget_get_default_direction ())
+    {
+    case GTK_TEXT_DIR_LTR:
+      g_test_message ("Attention: globally switching default text direction from LTR to RTL");
+      gtk_widget_set_default_direction (GTK_TEXT_DIR_RTL);
+      break;
+    case GTK_TEXT_DIR_RTL:
+      g_test_message ("Attention: globally switching default text direction from RTL to LTR");
+      gtk_widget_set_default_direction (GTK_TEXT_DIR_LTR);
+      break;
+    case GTK_TEXT_DIR_NONE:
+    default:
+      g_assert_not_reached ();
+      break;
+    }
+}
+
+G_MODULE_EXPORT void
+switch_direction (GtkWidget *widget)
+{
+  switch (gtk_widget_get_direction (widget))
+    {
+    case GTK_TEXT_DIR_LTR:
+      gtk_widget_set_direction (widget, GTK_TEXT_DIR_RTL);
+      break;
+    case GTK_TEXT_DIR_RTL:
+      gtk_widget_set_direction (widget, GTK_TEXT_DIR_LTR);
+      break;
+    case GTK_TEXT_DIR_NONE:
+    default:
+      g_assert_not_reached ();
+      break;
+    }
+}
+
+G_MODULE_EXPORT void
+swap_child (GtkWidget *window)
+{
+  GtkWidget *image;
+
+  gtk_container_remove (GTK_CONTAINER (window), gtk_bin_get_child (GTK_BIN (window)));
+
+  image = gtk_image_new_from_icon_name ("go-next", GTK_ICON_SIZE_BUTTON);
+  gtk_widget_show (image);
+  gtk_container_add (GTK_CONTAINER (window), image);
+}
+
