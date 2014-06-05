2bbea2bc93203b8585e11d29143defdbe8accc0c
Matthias Clasen <mclasen@redhat.com>
2014-06-04 21:25:05 -0400

    Expand GtkWindowGroup docs

--- gtk/gtkwindowgroup.c
+++ gtk/gtkwindowgroup.c
@@ -34,8 +34,16 @@
  * @Short_description: Limit the effect of grabs
  * @Title: GtkWindowGroup
  *
- * #GtkWindowGroup objects are referenced by each window in the group,
- * so once you have added all windows to a #GtkWindowGroup, you can drop
+ * A #GtkWindowGroup restricts the effect of grabs to windows
+ * in the same group, thereby making window groups almost behave
+ * like separate applications. 
+ *
+ * A window can be a member in at most one window group at a time.
+ * Windows that have not been explicitly assigned to a group are
+ * implicitly treated like windows of the default window group.
+ *
+ * GtkWindowGroup objects are referenced by each window in the group,
+ * so once you have added all windows to a GtkWindowGroup, you can drop
  * the initial reference to the window group with g_object_unref(). If the
  * windows in the window group are subsequently destroyed, then they will
  * be removed from the window group and drop their references on the window
