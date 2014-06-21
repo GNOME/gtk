8ae546857c5749ec8e59887910eab0328b1dc84e
Jasper St. Pierre <jstpierre@mecheye.net>
2014-06-21 18:36:51 -0400

    gdkwindow-win32: Remove queue_antiexpose implementation
    Since the Win32 code never actually called InvalidateRgn or used the
    Win32 update area at all, that meant the only thing that could possibly
    invalidate the window was the Win32 window manager as part of scrolling
    or resizing, which would also send it a WM_PAINT message.
    
    But the WM_PAINT handling called BeginPaint / EndPaint, which clears the
    update area completely! We also draw out-of-band, not directly when
    handling WM_PAINT, so there's no way that the update area inside the
    Win32 WM would match our local one.
    
    There is no possible way that this queue_antiexpose implementation could
    do anything. Remove it.

--- gdk/win32/gdkwindow-win32.c
+++ gdk/win32/gdkwindow-win32.c
@@ -3242,15 +3242,6 @@ static void
 _gdk_win32_window_queue_antiexpose (GdkWindow *window,
                                     cairo_region_t *area)
 {
-  HRGN hrgn = cairo_region_to_hrgn (area, 0, 0);
-
-  GDK_NOTE (EVENTS, g_print ("_gdk_windowing_window_queue_antiexpose: ValidateRgn %p %s\n",
-			     GDK_WINDOW_HWND (window),
-			     _gdk_win32_cairo_region_to_string (area)));
-
-  ValidateRgn (GDK_WINDOW_HWND (window), hrgn);
-
-  DeleteObject (hrgn);
 }
 
 static void
