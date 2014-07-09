0abb2f57fdc6725336b73363e35c3c17fd324895
Matthias Clasen <mclasen@redhat.com>
2014-07-09 08:03:57 -0400

    Update more places for the changed icon name

--- gtk/gtkmenubutton.c
+++ gtk/gtkmenubutton.c
@@ -597,7 +597,7 @@ set_arrow_type (GtkImage     *image,
   switch (arrow_type)
     {
     case GTK_ARROW_NONE:
-      gtk_image_set_from_icon_name (image, "view-context-menu-symbolic", GTK_ICON_SIZE_BUTTON);
+      gtk_image_set_from_icon_name (image, "open-menu-symbolic", GTK_ICON_SIZE_BUTTON);
       break;
     case GTK_ARROW_DOWN:
       gtk_image_set_from_icon_name (image, "pan-down-symbolic", GTK_ICON_SIZE_BUTTON);
