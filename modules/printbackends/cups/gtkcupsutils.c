d63bee09e0ba4ddd94c26bed97522896dc9be5c6
Marek Kasik <mkasik@redhat.com>
2014-07-11 11:35:35 +0200

    printing: Check connection to remote CUPS server on correct port
    Add parameter for specification of port to gtk_cups_connection_cups_new().
    Use default port returned by ippPort() if the given port is lower than 0.
    
    https://bugzilla.gnome.org/show_bug.cgi?id=693738

--- modules/printbackends/cups/gtkcupsutils.c
+++ modules/printbackends/cups/gtkcupsutils.c
@@ -1534,7 +1534,8 @@ gtk_cups_result_get_error_string (GtkCupsResult *result)
  * a socket for communication with a CUPS server 'server'.
  */
 GtkCupsConnectionTest *
-gtk_cups_connection_test_new (const char *server)
+gtk_cups_connection_test_new (const char *server,
+                              const int   port)
 {
   GtkCupsConnectionTest *result = NULL;
 #ifdef HAVE_CUPS_API_1_2
@@ -1542,7 +1543,10 @@ gtk_cups_connection_test_new (const char *server)
 
   result = g_new (GtkCupsConnectionTest, 1);
 
-  port_str = g_strdup_printf ("%d", ippPort ());
+  if (port >= 0)
+    port_str = g_strdup_printf ("%d", port);
+  else
+    port_str = g_strdup_printf ("%d", ippPort ());
 
   if (server != NULL)
     result->addrlist = httpAddrGetList (server, AF_UNSPEC, port_str);
