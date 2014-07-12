GNOME/gtk

commit 37030a771059f43ad701700380719d19cbc7c966

label: Add ink rect support to GtkLabel

gtk/gtklabel.c
@@ -3956,11 +3956,30 @@ G_GNUC_END_IGNORE_DEPRECATIONS
 }
 
 static void
+gtk_label_get_ink_rect (GtkLabel     *label,
+                        GdkRectangle *rect)
+{
+  GtkLabelPrivate *priv = label->priv;
+  PangoRectangle ink_rect;
+  int x, y;
+
+  gtk_label_ensure_layout (label);
+  get_layout_location (label, &x, &y);
+  pango_layout_get_pixel_extents (priv->layout, &ink_rect, NULL);
+
+  rect->x = x + ink_rect.x;
+  rect->width = ink_rect.width;
+  rect->y = y + ink_rect.y;
+  rect->height = ink_rect.height;
+}
+
+static void
 gtk_label_size_allocate (GtkWidget     *widget,
                          GtkAllocation *allocation)
 {
   GtkLabel *label = GTK_LABEL (widget);
   GtkLabelPrivate *priv = label->priv;
+  GdkRectangle clip_rect;
 
   GTK_WIDGET_CLASS (gtk_label_parent_class)->size_allocate (widget, allocation);
 
@@ -3975,6 +3994,9 @@ gtk_label_size_allocate (GtkWidget     *widget,
                               allocation->width,
                               allocation->height);
     }
+
+  gtk_label_get_ink_rect (label, &clip_rect);
+  gtk_widget_set_clip (widget, &clip_rect);
 }
 
 static void

commit f35ec11578afb6175447d838d8fcbdb25ac15fb4

label: Include all of the allocation in the clip
Otherwise, the CSS background we draw would be clipped if the ink rect
was smaller than the allocation (a very common thing).

Broken since 37030a771059f43ad701700380719d19cbc7c966 where we clipped
to the ink rect.

gtk/gtklabel.c
@@ -3996,6 +3996,7 @@ gtk_label_size_allocate (GtkWidget     *widget,
     }
 
   gtk_label_get_ink_rect (label, &clip_rect);
+  gdk_rectangle_union (&clip_rect, allocation, &clip_rect);
   gtk_widget_set_clip (widget, &clip_rect);
 }
 