e18136f16b4a3ca0a698e991ef58d0d853476be0
Emmanuele Bassi <ebassi@gnome.org>
2014-08-11 21:30:47 +0100

    tests/glarea: Add a button
    Make the test a bit more friendly.

--- tests/testglarea.c
+++ tests/testglarea.c
@@ -27,7 +27,7 @@ render (GtkGLArea    *area,
 int
 main (int argc, char *argv[])
 {
-  GtkWidget *window, *gl_area;
+  GtkWidget *window, *gl_area, *box, *button;
   GdkGLPixelFormat *pixel_format;
 
   gtk_init (&argc, &argv);
@@ -44,12 +44,25 @@ main (int argc, char *argv[])
   g_signal_connect (window, "destroy", G_CALLBACK (gtk_main_quit), NULL);
   gtk_widget_show (window);
 
+  box = gtk_box_new (GTK_ORIENTATION_VERTICAL, FALSE);
+  gtk_box_set_spacing (GTK_BOX (box), 6);
+  gtk_container_add (GTK_CONTAINER (window), box);
+  gtk_widget_show (box);
+
   gl_area = gtk_gl_area_new (pixel_format);
-  gtk_container_add (GTK_CONTAINER (window), gl_area);
+  gtk_widget_set_hexpand (gl_area, TRUE);
+  gtk_widget_set_vexpand (gl_area, TRUE);
+  gtk_container_add (GTK_CONTAINER (box), gl_area);
   gtk_widget_show (gl_area);
   g_object_unref (pixel_format);
 
   g_signal_connect (gl_area, "render", G_CALLBACK (render), NULL);
+
+  button = gtk_button_new_with_label ("Quit");
+  gtk_widget_set_hexpand (button, TRUE);
+  gtk_container_add (GTK_CONTAINER (box), button);
+  g_signal_connect_swapped (button, "clicked", G_CALLBACK (gtk_widget_destroy), window);
+  gtk_widget_show (button);
 
   gtk_main ();
 
