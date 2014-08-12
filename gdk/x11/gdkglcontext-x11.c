30548946eed0d493245c7d3662433a21f7500b02
Emmanuele Bassi <ebassi@gnome.org>
2014-08-12 12:30:30 +0100

    gdk: Add licensing notes for GdkGL*

--- gdk/x11/gdkglcontext-x11.c
+++ gdk/x11/gdkglcontext-x11.c
@@ -1,3 +1,23 @@
+/* GDK - The GIMP Drawing Kit
+ *
+ * gdkglcontext-x11.c: X11 specific OpenGL wrappers
+ * 
+ * Copyright © 2014  Emmanuele Bassi
+ *
+ * This library is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Library General Public
+ * License as published by the Free Software Foundation; either
+ * version 2 of the License, or (at your option) any later version.
+ *
+ * This library is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * Library General Public License for more details.
+ *
+ * You should have received a copy of the GNU Library General Public
+ * License along with this library. If not, see <http://www.gnu.org/licenses/>.
+ */
+
 #include "config.h"
 
 #include "gdkglcontext-x11.h"
