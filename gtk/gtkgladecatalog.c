15a9f1bdf5799d7c47df5cee86b95931578401e7
Chun-wei Fan <fanchunwei@src.gnome.org>
2014-09-15 12:26:32 +0800

    gtk/gtkgladecatalog.c: Fix Visual Studio Builds
    The _GDK_EXTERN decoration needs to be with the function prototype, if one
    exists, so move the _GDK_EXTERN decoration up.

--- gtk/gtkgladecatalog.c
+++ gtk/gtkgladecatalog.c
@@ -31,13 +31,14 @@
 #  include "gtkprinteroptionwidget.h"
 #endif
 
+_GDK_EXTERN
 void gtk_glade_catalog_init (const gchar *catalog_name);
 
 /* This function is referred to in gtk/glade/gtk-private-widgets.xml
  * and is used to ensure the private types for use in Glade while
  * editing UI files that define GTK+’s various composite widget classes.
  */
-_GDK_EXTERN void
+void
 gtk_glade_catalog_init (const gchar *catalog_name)
 {
   g_type_ensure (GTK_TYPE_PATH_BAR);
