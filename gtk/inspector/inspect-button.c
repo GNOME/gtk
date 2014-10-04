GNOME/gtk

commit e7bd73f2b0db50c00ef390b756c6597674d7b473

inspector: Simplify populating the tree
No need to loop toplevels in two places.

gtk/inspector/inspect-button.c
@@ -212,9 +212,6 @@ select_widget (GtkInspectorWindow *iw,
 
   gtk_notebook_set_current_page (GTK_NOTEBOOK (iw->top_notebook), 0);
 
-  gtk_inspector_widget_tree_scan (GTK_INSPECTOR_WIDGET_TREE (iw->widget_tree),
-                                  gtk_widget_get_toplevel (widget));
-
   gtk_inspector_widget_tree_select_object (GTK_INSPECTOR_WIDGET_TREE (iw->widget_tree),
                                            G_OBJECT (widget));
 }

gtk/inspector/widget-tree.c
@@ -497,6 +497,9 @@ gtk_inspector_widget_tree_scan (GtkInspectorWidgetTree *wt,
   if (g_application_get_default ())
     gtk_inspector_widget_tree_append_object (wt, G_OBJECT (g_application_get_default ()), NULL, NULL);
 
+  if (window)
+    gtk_inspector_widget_tree_append_object (wt, G_OBJECT (window), NULL, NULL);
+
   inspector_win = gtk_widget_get_toplevel (GTK_WIDGET (wt));
   toplevels = gtk_window_list_toplevels ();
   for (l = toplevels; l; l = l->next)
@@ -508,8 +511,6 @@ gtk_inspector_widget_tree_scan (GtkInspectorWidgetTree *wt,
         gtk_inspector_widget_tree_append_object (wt, G_OBJECT (l->data), NULL, NULL);
     }
   g_list_free (toplevels);
-
-  gtk_inspector_widget_tree_append_object (wt, G_OBJECT (window), NULL, NULL);
 
   gtk_tree_view_columns_autosize (GTK_TREE_VIEW (wt));
 }

gtk/inspector/window.c
@@ -153,35 +153,13 @@ gtk_inspector_window_init (GtkInspectorWindow *iw)
 }
 
 static void
-gtk_inspector_window_select_initially (GtkInspectorWindow *iw)
-{
-  GList *toplevels, *l;
-  GtkWidget *widget;
-
-  toplevels = gtk_window_list_toplevels ();
-  widget = NULL;
-  for (l = toplevels; l; l = l->next)
-    {
-      if (gtk_widget_get_mapped (GTK_WIDGET (l->data)) &&
-          GTK_IS_WINDOW (l->data) &&
-          !GTK_INSPECTOR_IS_WINDOW (l->data))
-        {
-          widget = l->data;
-          break;
-        }
-    }
-  g_list_free (toplevels);
-
-  if (widget)
-    gtk_inspector_widget_tree_scan (GTK_INSPECTOR_WIDGET_TREE (iw->widget_tree), widget);
-}
-
-static void
 gtk_inspector_window_constructed (GObject *object)
 {
-  gtk_inspector_window_select_initially (GTK_INSPECTOR_WINDOW (object));
+  GtkInspectorWindow *iw = GTK_INSPECTOR_WINDOW (object);
 
   G_OBJECT_CLASS (gtk_inspector_window_parent_class)->constructed (object);
+
+  gtk_inspector_widget_tree_scan (GTK_INSPECTOR_WIDGET_TREE (iw->widget_tree), NULL);
 }
 
 static void

commit fee46bda00a7e01e8cf22a8972bfac003feda6d6

inspector: Fix a thinko
We still need to possibly scan the toplevel after selecting
a widget, since we don't monitor the list of toplevels, so
the window may not be in our tree yet. This was broken in
commit e7bd73f2b0db50c00ef390b756c6597674d7b473.

gtk/inspector/inspect-button.c
@@ -208,12 +208,16 @@ static void
 select_widget (GtkInspectorWindow *iw,
                GtkWidget          *widget)
 {
+  GtkInspectorWidgetTree *wt = GTK_INSPECTOR_WIDGET_TREE (iw->widget_tree);
+  GtkTreeIter iter;
+
   iw->selected_widget = widget;
 
   gtk_notebook_set_current_page (GTK_NOTEBOOK (iw->top_notebook), 0);
 
-  gtk_inspector_widget_tree_select_object (GTK_INSPECTOR_WIDGET_TREE (iw->widget_tree),
-                                           G_OBJECT (widget));
+  if (!gtk_inspector_widget_tree_find_object (wt, G_OBJECT (widget), &iter))
+    gtk_inspector_widget_tree_scan (wt, gtk_widget_get_toplevel (widget));
+ gtk_inspector_widget_tree_select_object (wt, G_OBJECT (widget));
 }
 
 static void