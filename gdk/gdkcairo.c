a0b2cf402f911c5ff7703a256155fd5db3e6e8c3
Kalev Lember <kalevlember@gmail.com>
2014-10-06 14:19:21 +0200

    gdk: Allow calling gdk_cairo_surface_create_from_pixbuf with scale 0
    The documentation explicitly states that 0 is an allowed value for using
    the same scale as the window. This 0 value is also explicitly checked
    down in the call chain and handled.

--- gdk/gdkcairo.c
+++ gdk/gdkcairo.c
@@ -285,7 +285,7 @@ gdk_cairo_surface_create_from_pixbuf (const GdkPixbuf *pixbuf,
   cairo_surface_t *surface;
 
   g_return_val_if_fail (GDK_IS_PIXBUF (pixbuf), NULL);
-  g_return_val_if_fail (scale > 0, NULL);
+  g_return_val_if_fail (scale >= 0, NULL);
   g_return_val_if_fail (for_window == NULL || GDK_IS_WINDOW (for_window), NULL);
 
   if (gdk_pixbuf_get_n_channels (pixbuf) == 3)
