a74c5a1f394a7d40f8bed9bb777e85dd02e91aa8
Matthias Clasen <mclasen@redhat.com>
2014-10-11 23:04:40 -0400

    Correct copyright year

--- gdk/gdkgl.c
+++ gdk/gdkgl.c
@@ -1,5 +1,5 @@
 /* GDK - The GIMP Drawing Kit
- * Copyright (C) 2005 Red Hat, Inc. 
+ * Copyright (C) 2014 Red Hat, Inc.
  *
  * This library is free software; you can redistribute it and/or
  * modify it under the terms of the GNU Lesser General Public
