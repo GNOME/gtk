7b6a6c9421b6e4e8aa974f28eaf54a63f3f9c90b
Emmanuele Bassi <ebassi@gnome.org>
2014-10-22 21:20:52 +0100

    tests/gtk: Add GdkX11GLContext to the whitelist
    We cannot create them directly anyway.

--- testsuite/gtk/defaultvalue.c
+++ testsuite/gtk/defaultvalue.c
@@ -84,7 +84,8 @@ test_type (gconstpointer data)
       g_str_equal (g_type_name (type), "GdkX11DeviceManagerXI2") ||
       g_str_equal (g_type_name (type), "GdkX11Display") ||
       g_str_equal (g_type_name (type), "GdkX11DisplayManager") ||
-      g_str_equal (g_type_name (type), "GdkX11Screen"))
+      g_str_equal (g_type_name (type), "GdkX11Screen") ||
+      g_str_equal (g_type_name (type), "GdkX11GLContext"))
     return;
 
   /* This throws a critical when the connection is dropped */
