268efd14754c6fec67b6f8ffe4af0f0d4b4d64b1
Lapo Calamandrei <calamandrei@gmail.com>
2014-11-15 16:09:39 +0100

    textview: remove osd style class from touch selection popover
    The touch-selection class is sufficient since that needs special
    casing whatever.

--- gtk/gtktextview.c
+++ gtk/gtktextview.c
@@ -9037,8 +9037,6 @@ bubble_targets_received (GtkClipboard     *clipboard,
 
   priv->selection_bubble = gtk_popover_new (GTK_WIDGET (text_view));
   gtk_style_context_add_class (gtk_widget_get_style_context (priv->selection_bubble),
-                               GTK_STYLE_CLASS_OSD);
-  gtk_style_context_add_class (gtk_widget_get_style_context (priv->selection_bubble),
                                GTK_STYLE_CLASS_TOUCH_SELECTION);
   gtk_popover_set_position (GTK_POPOVER (priv->selection_bubble),
                             GTK_POS_TOP);
