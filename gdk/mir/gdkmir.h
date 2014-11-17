da8fdab87b4cc96b9614ccb6abb0682def324f30
Marco Trevisan (Treviño) <mail@3v1n0.net>
2014-11-17 13:38:27 +0100

    mir: fix typo in GDK_IS_WINDOW_MIR definition

--- gdk/mir/gdkmir.h
+++ gdk/mir/gdkmir.h
@@ -25,7 +25,7 @@
 #define GDK_IS_MIR_DISPLAY(object)        (G_TYPE_CHECK_INSTANCE_TYPE ((object), GDK_TYPE_MIR_DISPLAY))
 
 #define GDK_TYPE_MIR_WINDOW              (gdk_mir_window_get_type ())
-#define GDK_IS_WINDOW_MIR(object)        (G_TYPE_CHECK_INSTANCE_TYPE ((object), GDK_TYPE_WINDOW_MIR))
+#define GDK_IS_WINDOW_MIR(object)        (G_TYPE_CHECK_INSTANCE_TYPE ((object), GDK_TYPE_MIR_WINDOW))
 
 GDK_AVAILABLE_IN_3_10
 GType gdk_mir_display_get_type (void);
