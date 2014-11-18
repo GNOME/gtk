e85e320bc0529edc74d299b87a13d58d91250a80
Marco Trevisan (Treviño) <mail@3v1n0.net>
2014-11-18 23:45:33 +0100

    mir: send resize event to also to hardware windows
    this will make testglblend work on first map

--- gdk/mir/gdkmirwindowimpl.c
+++ gdk/mir/gdkmirwindowimpl.c
@@ -217,21 +217,15 @@ ensure_surface_full (GdkWindow *window,
                                       window->width, window->height,
                                       buffer_usage);
 
-  if (buffer_usage == mir_buffer_usage_software)
-    {
-      MirGraphicsRegion region;
-      MirEvent resize_event;
+  MirEvent resize_event;
 
-      mir_surface_get_graphics_region (impl->surface, &region);
+  /* Send the initial configure with the size the server gave... */
+  resize_event.resize.type = mir_event_type_resize;
+  resize_event.resize.surface_id = 0;
+  resize_event.resize.width = window->width;
+  resize_event.resize.height = window->height;
 
-      /* Send the initial configure with the size the server gave... */
-      resize_event.resize.type = mir_event_type_resize;
-      resize_event.resize.surface_id = 0;
-      resize_event.resize.width = region.width;
-      resize_event.resize.height = region.height;
-
-      _gdk_mir_event_source_queue (window_ref, &resize_event);
-    }
+  _gdk_mir_event_source_queue (window_ref, &resize_event);
 
   mir_surface_set_event_handler (impl->surface, &event_delegate); // FIXME: Ignore some events until shown
   set_surface_type (impl, impl->surface_type);
