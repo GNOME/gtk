605f2efa734cdbb1ef2ebb94209ea064b0509110
Emmanuele Bassi <ebassi@gnome.org>
2014-11-19 15:50:55 +0000

    docs: Fix annotation for GtkCssProviderError
    https://bugzilla.gnome.org/show_bug.cgi?id=740374

--- gtk/gtkcssprovider.h
+++ gtk/gtkcssprovider.h
@@ -30,6 +30,11 @@ G_BEGIN_DECLS
 #define GTK_IS_CSS_PROVIDER_CLASS(c)  (G_TYPE_CHECK_CLASS_TYPE    ((c), GTK_TYPE_CSS_PROVIDER))
 #define GTK_CSS_PROVIDER_GET_CLASS(o) (G_TYPE_INSTANCE_GET_CLASS  ((o), GTK_TYPE_CSS_PROVIDER, GtkCssProviderClass))
 
+/**
+ * GTK_CSS_PROVIDER_ERROR:
+ *
+ * Domain for #GtkCssProvider errors.
+ */
 #define GTK_CSS_PROVIDER_ERROR (gtk_css_provider_error_quark ())
 
 /**
@@ -41,7 +46,7 @@ G_BEGIN_DECLS
  * @GTK_CSS_PROVIDER_ERROR_DEPRECATED: Deprecation error.
  * @GTK_CSS_PROVIDER_ERROR_UNKNOWN_VALUE: Unknown value.
  *
- * Deprecated.
+ * Error codes for %GTK_CSS_PROVIDER_ERROR.
  */
 typedef enum
 {
