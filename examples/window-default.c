e9ddff907f0c016e881a2254e82730ece2e36066
Bastian Ilsø <bastianilso@src.gnome.org>
2015-01-28 11:39:01 +0000

    Get started: port example-0.c to GtkApplication
    https://bugzilla.gnome.org/show_bug.cgi?id=743638

--- examples/window-default.c
+++ examples/window-default.c
@@ -1,21 +1,28 @@
 #include <gtk/gtk.h>
 
-int
-main (int   argc,
-      char *argv[])
+static void
+activate (GtkApplication* app,
+          gpointer        user_data)
 {
   GtkWidget *window;
 
-  gtk_init (&argc, &argv);
-
-  window = gtk_window_new (GTK_WINDOW_TOPLEVEL);
+  window = gtk_application_window_new (app);
   gtk_window_set_title (GTK_WINDOW (window), "Window");
+  gtk_window_set_default_size (GTK_WINDOW (window), 200, 200);
+  gtk_widget_show_all (window);
+}
 
-  g_signal_connect (window, "destroy", G_CALLBACK (gtk_main_quit), NULL);
+int
+main (int    argc,
+      char **argv)
+{
+  GtkApplication *app;
+  int status;
 
-  gtk_widget_show (window);
+  app = gtk_application_new ("org.gtk.example", G_APPLICATION_FLAGS_NONE);
+  g_signal_connect (app, "activate", G_CALLBACK (activate), NULL);
+  status = g_application_run (G_APPLICATION (app), argc, argv);
+  g_object_unref (app);
 
-  gtk_main ();
-
-  return 0;
+  return status;
 }
