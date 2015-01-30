GNOME/gtk

commit ff256956b2072cd422ccec5f33949854b0db0ef8

Resume events when disconnecting from a frame clock
If we are disconnecting from a frame clock that has paused event
processing and hasn't issued a resume yet make sure we resume the
events or they will stay blocked forever.

https://bugzilla.gnome.org/show_bug.cgi?id=742636

gdk/gdkwindow.c
@@ -10913,6 +10913,8 @@ gdk_window_flush_events (GdkFrameClock *clock,
   _gdk_display_pause_events (display);
 
   gdk_frame_clock_request_phase (clock, GDK_FRAME_CLOCK_PHASE_RESUME_EVENTS);
+
+  window->frame_clock_events_paused = TRUE;
 }
 
 static void
@@ -10939,6 +10941,8 @@ gdk_window_resume_events (GdkFrameClock *clock,
 
   display = gdk_window_get_display (window);
   _gdk_display_unpause_events (display);
+
+  window->frame_clock_events_paused = TRUE;
 }
 
 static void
@@ -10971,6 +10975,11 @@ gdk_window_set_frame_clock (GdkWindow     *window,
 
   if (window->frame_clock)
     {
+      if (window->frame_clock_events_paused)
+        {
+          gdk_window_resume_events (window->frame_clock, G_OBJECT (window));
+        }
+
       g_signal_handlers_disconnect_by_func (G_OBJECT (window->frame_clock),
                                             G_CALLBACK (gdk_window_flush_events),
                                             window);

gdk/gdkinternals.h
@@ -333,6 +333,7 @@ struct _GdkWindow
   guint in_update : 1;
   guint geometry_dirty : 1;
   guint event_compression : 1;
+  guint frame_clock_events_paused : 1;
 
   /* The GdkWindow that has the impl, ref:ed if another window.
    * This ref is required to keep the wrapper of the impl window alive

commit 561ff51abb9629c11855d346432cb1792b117815

Fix an oversight
Commit ff256956b2072cd422 introduced a frame_clock_events_paused
flag, but only ever set it to TRUE, instead of unsetting it when
events are resumed. This was leading to assertion failures in
_gdk_display_unpause_events().

gdk/gdkwindow.c
@@ -10942,7 +10942,7 @@ gdk_window_resume_events (GdkFrameClock *clock,
   display = gdk_window_get_display (window);
   _gdk_display_unpause_events (display);
 
-  window->frame_clock_events_paused = TRUE;
+  window->frame_clock_events_paused = FALSE;
 }
 
 static void
@@ -10976,9 +10976,7 @@ gdk_window_set_frame_clock (GdkWindow     *window,
   if (window->frame_clock)
     {
       if (window->frame_clock_events_paused)
-        {
-          gdk_window_resume_events (window->frame_clock, G_OBJECT (window));
-        }
+        gdk_window_resume_events (window->frame_clock, G_OBJECT (window));
 
       g_signal_handlers_disconnect_by_func (G_OBJECT (window->frame_clock),
                                             G_CALLBACK (gdk_window_flush_events),