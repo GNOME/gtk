71bd1c7e2c5252b5103bf029e437787e0a002951
Matthias Clasen <mclasen@redhat.com>
2015-03-03 18:02:09 -0500

    file chooser: Really stop typeahead search popups
    GtkTreeView is a bad widget and constantly resets the search-column
    behind our back. We need to re-unset it every time a model is set :-(

--- gtk/gtkfilechooserwidget.c
+++ gtk/gtkfilechooserwidget.c
@@ -1895,6 +1895,8 @@ file_list_set_sort_column_ids (GtkFileChooserWidget *impl)
 {
   GtkFileChooserWidgetPrivate *priv = impl->priv;
 
+  gtk_tree_view_set_search_column (GTK_TREE_VIEW (priv->browse_files_tree_view), -1);
+
   gtk_tree_view_column_set_sort_column_id (priv->list_name_column, MODEL_COL_NAME);
   gtk_tree_view_column_set_sort_column_id (priv->list_mtime_column, MODEL_COL_MTIME);
   gtk_tree_view_column_set_sort_column_id (priv->list_size_column, MODEL_COL_SIZE);
@@ -3462,8 +3464,6 @@ load_set_model (GtkFileChooserWidget *impl)
   gtk_tree_view_set_model (GTK_TREE_VIEW (priv->browse_files_tree_view),
 			   GTK_TREE_MODEL (priv->browse_files_model));
   gtk_tree_view_columns_autosize (GTK_TREE_VIEW (priv->browse_files_tree_view));
-  gtk_tree_view_set_search_column (GTK_TREE_VIEW (priv->browse_files_tree_view),
-				   MODEL_COL_NAME);
   file_list_set_sort_column_ids (impl);
   set_sort_column (impl);
   profile_msg ("    gtk_tree_view_set_model end", NULL);
