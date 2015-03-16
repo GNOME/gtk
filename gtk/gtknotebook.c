6a6350b855437a97b6498595339aae77ef692648
Matthias Clasen <mclasen@redhat.com>
2015-03-16 12:51:56 -0400

    Don't pass invalid rectangles to cairo
    This causes pixman warnings, or worse.
    Clearly, something is not quite right here, if we end up
    redrawing tabs at a time when the allocation is set to (1, 1).
    For now, avoid straining the error handling in the lower layers.
    
    https://bugzilla.gnome.org/show_bug.cgi?id=746301

--- gtk/gtknotebook.c
+++ gtk/gtknotebook.c
@@ -4803,7 +4803,7 @@ gtk_notebook_redraw_tabs (GtkNotebook *notebook)
         page->allocation.height - padding.bottom;
       /* fall through */
     case GTK_POS_TOP:
-      redraw_rect.width = allocation.width - 2 * border;
+      redraw_rect.width = MAX (1, allocation.width - 2 * border);
       redraw_rect.height = page->allocation.height + padding.top;
 
       break;
@@ -4814,7 +4814,7 @@ gtk_notebook_redraw_tabs (GtkNotebook *notebook)
       /* fall through */
     case GTK_POS_LEFT:
       redraw_rect.width = page->allocation.width + padding.left;
-      redraw_rect.height = allocation.height - 2 * border;
+      redraw_rect.height = MAX (1, allocation.height - 2 * border);
 
       break;
     }
