dd62e0410e44928fbce3cc0238ab6af66cb95ce9
Carlos Garnacho <carlosg@gnome.org>
2015-03-28 16:22:09 +0100

    gtkdnd: Restore widget opacity after fading out
    Otherwise the widget is left fully transparent, and will start as such if
    the widget is reused.

--- gtk/gtkdnd.c
+++ gtk/gtkdnd.c
@@ -3992,6 +3992,7 @@ gtk_drag_remove_icon (GtkDragSourceInfo *info)
   if (info->icon_window)
     {
       gtk_widget_hide (info->icon_window);
+      gtk_widget_set_opacity (info->icon_window, 1.0);
       if (info->destroy_icon)
         gtk_widget_destroy (info->icon_window);
 
