69aeaacedde4c5fd7e59220cecc73b5dda70feaf
Руслан Ижбулатов <lrn1986@gmail.com>
2015-05-04 15:32:22 +0000

    Use GDK_KEY_KP_Decimal instead of GDK_KP_Decimal
    Fixes the FTBFS introduced in 578043f97e891e423648c9f70ddf1d185f4615c4

--- gdk/win32/gdkkeys-win32.c
+++ gdk/win32/gdkkeys-win32.c
@@ -165,7 +165,7 @@ handle_special (guint  vk,
     case VK_APPS:
       *ksymp = GDK_KEY_Menu; break;
     case VK_DECIMAL:
-      *ksymp = GDK_KP_Decimal; break;
+      *ksymp = GDK_KEY_KP_Decimal; break;
     case VK_MULTIPLY:
       *ksymp = GDK_KEY_KP_Multiply; break;
     case VK_ADD:
