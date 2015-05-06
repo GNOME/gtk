9c307fedc93caee22a677074aab3b026d1d69835
Garrett Regier <garrettregier@gmail.com>
2015-05-05 22:24:16 -0700

    GtkFontChooserWidget: Remove unused struct field
    This was added accidentally in a recent patch.
    
    Signed-off-by: Garrett Regier <garrettregier@gmail.com>
    
    https://bugzilla.gnome.org/show_bug.cgi?id=748992

--- gtk/gtkfontchooserwidget.c
+++ gtk/gtkfontchooserwidget.c
@@ -97,7 +97,6 @@ struct _GtkFontChooserWidgetPrivate
   gpointer          filter_data;
   GDestroyNotify    filter_data_destroy;
 
-  GtkSettings *settings;
   guint last_fontconfig_timestamp;
 };
 
