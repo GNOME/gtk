99935b14c6db3a5953b00fc61f231e348c7d8546
Matthias Clasen <mclasen@redhat.com>
2015-05-07 22:24:19 -0400

    GtkPrintUnixDialog: Use a better busy cursor

--- gtk/gtkprintunixdialog.c
+++ gtk/gtkprintunixdialog.c
@@ -584,7 +584,11 @@ set_busy_cursor (GtkPrintUnixDialog *dialog,
   display = gtk_widget_get_display (widget);
 
   if (busy)
-    cursor = gdk_cursor_new_for_display (display, GDK_WATCH);
+    {
+      cursor = gdk_cursor_new_from_name (display, "left_ptr_watch");
+      if (cursor == NULL)
+        cursor = gdk_cursor_new_for_display (display, GDK_WATCH);
+    }
   else
     cursor = NULL;
 
