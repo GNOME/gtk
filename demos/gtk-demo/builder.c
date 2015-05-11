9d921ec148bc91ef5e290250d6be5cb2177de957
Matthias Clasen <mclasen@redhat.com>
2015-05-10 21:01:47 -0400

    gtk-demo: Port builder example away from GtkUIManager
    This makes gtk3-demo deprecation-free.

--- demos/gtk-demo/builder.c
+++ demos/gtk-demo/builder.c
@@ -2,13 +2,15 @@
  *
  * Demonstrates an interface loaded from a XML description.
  */
-#undef GTK_DISABLE_DEPRECATED
+
 #include <gtk/gtk.h>
 
 static GtkBuilder *builder;
 
-G_MODULE_EXPORT void
-quit_activate (GtkAction *action)
+static void
+quit_activate (GSimpleAction *action,
+               GVariant      *parameter,
+               gpointer       user_data)
 {
   GtkWidget *window;
 
@@ -16,8 +18,10 @@ quit_activate (GtkAction *action)
   gtk_widget_destroy (window);
 }
 
-G_MODULE_EXPORT void
-about_activate (GtkAction *action)
+static void
+about_activate (GSimpleAction *action,
+                GVariant      *parameter,
+                gpointer       user_data)
 {
   GtkWidget *about_dlg;
 
@@ -26,11 +30,19 @@ about_activate (GtkAction *action)
   gtk_widget_hide (about_dlg);
 }
 
-G_MODULE_EXPORT void
-help_activate (GtkAction *action)
+static void
+help_activate (GSimpleAction *action,
+               GVariant      *parameter,
+               gpointer       user_data)
 {
   g_print ("Help not available\n");
 }
+
+static GActionEntry win_entries[] = {
+  { "quit", quit_activate, NULL, NULL, NULL },
+  { "about", about_activate, NULL, NULL, NULL },
+  { "help", help_activate, NULL, NULL, NULL }
+};
 
 GtkWidget *
 do_builder (GtkWidget *do_widget)
@@ -38,6 +50,9 @@ do_builder (GtkWidget *do_widget)
   static GtkWidget *window = NULL;
   GError *err = NULL;
   GtkWidget *toolbar;
+  GActionGroup *actions;
+  GtkAccelGroup *accel_group;
+  GtkWidget *item;
 
   if (!window)
     {
@@ -57,6 +72,49 @@ do_builder (GtkWidget *do_widget)
       toolbar = GTK_WIDGET (gtk_builder_get_object (builder, "toolbar1"));
       gtk_style_context_add_class (gtk_widget_get_style_context (toolbar),
                                    "primary-toolbar");
+      actions = (GActionGroup*)g_simple_action_group_new ();
+      g_action_map_add_action_entries (G_ACTION_MAP (actions),
+                                       win_entries, G_N_ELEMENTS (win_entries),
+                                       NULL);
+      gtk_widget_insert_action_group (window, "win", actions);
+      accel_group = gtk_accel_group_new ();
+      gtk_window_add_accel_group (GTK_WINDOW (window), accel_group);
+
+      item = (GtkWidget*)gtk_builder_get_object (builder, "new_item");
+      gtk_widget_add_accelerator (item, "activate", accel_group,
+                                  GDK_KEY_n, GDK_CONTROL_MASK, GTK_ACCEL_VISIBLE);
+
+      item = (GtkWidget*)gtk_builder_get_object (builder, "open_item");
+      gtk_widget_add_accelerator (item, "activate", accel_group,
+                                  GDK_KEY_o, GDK_CONTROL_MASK, GTK_ACCEL_VISIBLE);
+
+      item = (GtkWidget*)gtk_builder_get_object (builder, "save_item");
+      gtk_widget_add_accelerator (item, "activate", accel_group,
+                                  GDK_KEY_s, GDK_CONTROL_MASK, GTK_ACCEL_VISIBLE);
+
+      item = (GtkWidget*)gtk_builder_get_object (builder, "quit_item");
+      gtk_widget_add_accelerator (item, "activate", accel_group,
+                                  GDK_KEY_q, GDK_CONTROL_MASK, GTK_ACCEL_VISIBLE);
+
+      item = (GtkWidget*)gtk_builder_get_object (builder, "copy_item");
+      gtk_widget_add_accelerator (item, "activate", accel_group,
+                                  GDK_KEY_c, GDK_CONTROL_MASK, GTK_ACCEL_VISIBLE);
+
+      item = (GtkWidget*)gtk_builder_get_object (builder, "cut_item");
+      gtk_widget_add_accelerator (item, "activate", accel_group,
+                                  GDK_KEY_x, GDK_CONTROL_MASK, GTK_ACCEL_VISIBLE);
+
+      item = (GtkWidget*)gtk_builder_get_object (builder, "paste_item");
+      gtk_widget_add_accelerator (item, "activate", accel_group,
+                                  GDK_KEY_v, GDK_CONTROL_MASK, GTK_ACCEL_VISIBLE);
+
+      item = (GtkWidget*)gtk_builder_get_object (builder, "help_item");
+      gtk_widget_add_accelerator (item, "activate", accel_group,
+                                  GDK_KEY_F1, 0, GTK_ACCEL_VISIBLE);
+
+      item = (GtkWidget*)gtk_builder_get_object (builder, "about_item");
+      gtk_widget_add_accelerator (item, "activate", accel_group,
+                                  GDK_KEY_F7, 0, GTK_ACCEL_VISIBLE);
     }
 
   if (!gtk_widget_get_visible (window))
