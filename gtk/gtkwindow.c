9590879e2f550a29d230099680081a9d3a90d414
Emmanuele Bassi <ebassi@gnome.org>
2015-06-18 16:10:36 +0100

    Revert "window: Reinstate logic for should_use_csd()"
    This reverts commit f043fd532dc66a7abb1d4ea57c666655330cc429.
    
    The whole point of having can_use_csd() return TRUE when GTK_CSD is
    unset is to use it inside enable_csd().
    
    https://bugzilla.gnome.org/show_bug.cgi?id=751140

--- gtk/gtkwindow.c
+++ gtk/gtkwindow.c
@@ -4079,7 +4079,8 @@ gtk_window_can_use_csd (GtkWindow *window)
 
   csd_env = g_getenv ("GTK_CSD");
 
-  return (g_strcmp0 (csd_env, "1") == 0);
+  /* If GTK_CSD is unset we default to CSD support */
+  return csd_env == NULL || (strcmp (csd_env, "1") == 0);
 }
 
 static void
