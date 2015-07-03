GNOME/gtk

commit 6786c9d724ad57ea30e169d2afcb4a314b2e56bb

drawingarea: don't call gtk_style_context_set_background()

gtk/gtkdrawingarea.c
@@ -25,8 +25,8 @@
 #include "config.h"
 #include "gtkdrawingarea.h"
 #include "gtkintl.h"
+#include "gtkrender.h"
 #include "gtkstylecontext.h"
-
 
 /**
  * SECTION:gtkdrawingarea
@@ -112,6 +112,8 @@
  * gtk_render_focus() for one way to draw focus.
  */
 
+static gboolean gtk_drawing_area_draw      (GtkWidget           *widget,
+                                            cairo_t             *cr);
 static void gtk_drawing_area_realize       (GtkWidget           *widget);
 static void gtk_drawing_area_size_allocate (GtkWidget           *widget,
                                             GtkAllocation       *allocation);
@@ -126,6 +128,7 @@ gtk_drawing_area_class_init (GtkDrawingAreaClass *class)
 
   widget_class->realize = gtk_drawing_area_realize;
   widget_class->size_allocate = gtk_drawing_area_size_allocate;
+  widget_class->draw = gtk_drawing_area_draw;
 
   gtk_widget_class_set_accessible_role (widget_class, ATK_ROLE_DRAWING_AREA);
 }
@@ -181,9 +184,6 @@ gtk_drawing_area_realize (GtkWidget *widget)
                                &attributes, attributes_mask);
       gtk_widget_register_window (widget, window);
       gtk_widget_set_window (widget, window);
-
-      gtk_style_context_set_background (gtk_widget_get_style_context (widget),
-                                        window);
     }
 
   gtk_drawing_area_send_configure (GTK_DRAWING_AREA (widget));
@@ -207,6 +207,19 @@ gtk_drawing_area_size_allocate (GtkWidget     *widget,
 
       gtk_drawing_area_send_configure (GTK_DRAWING_AREA (widget));
     }
+}
+
+static gboolean
+gtk_drawing_area_draw (GtkWidget *widget,
+		       cairo_t   *cr)
+{
+  if (gtk_widget_get_has_window (widget))
+    gtk_render_background (gtk_widget_get_style_context (widget), cr,
+                           0, 0,
+                           gtk_widget_get_allocated_width (widget),
+                           gtk_widget_get_allocated_height (widget));
+
+  return GTK_WIDGET_CLASS (gtk_drawing_area_parent_class)->draw (widget, cr);
 }
 
 static void

commit b3166b745f41a901171821ccc9f0c69b9cc35e70

drawingarea: checks parent's draw() before call it
commit 6786c9d7 introduced a bug when a drawing area is used for
rendering videos, since GtkWidgetClass doesn't set a draw()
method by itself.

https://bugzilla.gnome.org/show_bug.cgi?id=751877

gtk/gtkdrawingarea.c
@@ -219,7 +219,10 @@ gtk_drawing_area_draw (GtkWidget *widget,
                            gtk_widget_get_allocated_width (widget),
                            gtk_widget_get_allocated_height (widget));
 
-  return GTK_WIDGET_CLASS (gtk_drawing_area_parent_class)->draw (widget, cr);
+  if (GTK_WIDGET_CLASS (gtk_drawing_area_parent_class)->draw)
+    return GTK_WIDGET_CLASS (gtk_drawing_area_parent_class)->draw (widget, cr);
+
+  return FALSE;
 }
 
 static void