GNOME/gtk

commit e6e511eb460d8fcd241f202f9c3e26b0ef288927

GtkListBox: Fix a bug in unselection
Ensure that gtk_list_box_get_selected_row returns NULL after
gtk_list_box_unselect_all. Add a testcase that detects this
problem.

gtk/gtklistbox.c
@@ -1449,6 +1449,8 @@ gtk_list_box_unselect_all_internal (GtkListBox *box)
       dirty |= gtk_list_box_row_set_selected (row, FALSE);
     }
 
+  BOX_PRIV (box)->selected_row = NULL;
+
   return dirty;
 }
 

testsuite/gtk/listbox.c
@@ -137,11 +137,17 @@ test_selection (void)
   g_assert_cmpint (count, ==, 1);
   row2 = gtk_list_box_get_selected_row (list);
   g_assert (row2 == row);
+  gtk_list_box_unselect_all (list);
+  row2 = gtk_list_box_get_selected_row (list);
+  g_assert (row2 == NULL);
+  gtk_list_box_select_row (list, row);
+  row2 = gtk_list_box_get_selected_row (list);
+  g_assert (row2 == row);
 
   gtk_list_box_set_selection_mode (list, GTK_SELECTION_BROWSE);
   gtk_container_remove (GTK_CONTAINER (list), GTK_WIDGET (row));
   g_assert (callback_row == NULL);
-  g_assert_cmpint (count, ==, 2);
+  g_assert_cmpint (count, ==, 4);
   row2 = gtk_list_box_get_selected_row (list);
   g_assert (row2 == NULL);
 
@@ -149,12 +155,12 @@ test_selection (void)
   gtk_list_box_select_row (list, row);
   g_assert (gtk_list_box_row_is_selected (row));
   g_assert (callback_row == row);
-  g_assert_cmpint (count, ==, 3);
+  g_assert_cmpint (count, ==, 5);
 
   gtk_list_box_set_selection_mode (list, GTK_SELECTION_NONE);
   g_assert (!gtk_list_box_row_is_selected (row));
   g_assert (callback_row == NULL);
-  g_assert_cmpint (count, ==, 4);
+  g_assert_cmpint (count, ==, 6);
   row2 = gtk_list_box_get_selected_row (list);
   g_assert (row2 == NULL);
 

commit 58fa3d96a81e2849dba7d49e7cebf6544d192bc3

listbox: Fix shift-selection
Selecting a range of rows with Shift-click was accidentally broken
in e6e511eb460d8fcd. Make it work again.

gtk/gtklistbox.c
@@ -1034,10 +1034,7 @@ gtk_list_box_set_selection_mode (GtkListBox       *box,
 
   if (mode == GTK_SELECTION_NONE ||
       priv->selection_mode == GTK_SELECTION_MULTIPLE)
-    {
-      dirty = gtk_list_box_unselect_all_internal (box);
-      priv->selected_row = NULL;
-    }
+    dirty = gtk_list_box_unselect_all_internal (box);
 
   priv->selection_mode = mode;
 
@@ -1649,15 +1646,20 @@ gtk_list_box_update_selection (GtkListBox    *box,
     {
       if (extend)
         {
+          GtkListBoxRow *selected_row;
+
+          selected_row = priv->selected_row;
+
           gtk_list_box_unselect_all_internal (box);
-          if (priv->selected_row == NULL)
+
+          if (selected_row == NULL)
             {
               gtk_list_box_row_set_selected (row, TRUE);
               priv->selected_row = row;
               g_signal_emit (box, signals[ROW_SELECTED], 0, row);
             }
           else
-            gtk_list_box_select_all_between (box, priv->selected_row, row, FALSE);
+            gtk_list_box_select_all_between (box, selected_row, row, FALSE);
         }
       else
         {
@@ -1904,7 +1906,6 @@ gtk_list_box_multipress_gesture_released (GtkGestureMultiPress *gesture,
           gboolean extend;
 
           get_current_selection_modifiers (GTK_WIDGET (box), &modify, &extend);
-
           /* With touch, we default to modifying the selection.
            * You can still clear the selection and start over
            * by holding Ctrl.