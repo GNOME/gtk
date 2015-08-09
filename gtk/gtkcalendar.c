b9bced693a9e181bec9f81d1f808df2f94c03d82
Matthias Clasen <mclasen@redhat.com>
2015-08-09 23:39:14 +0200

    Remove some unused data

--- gtk/gtkcalendar.c
+++ gtk/gtkcalendar.c
@@ -89,12 +89,6 @@ static const guint month_length[2][13] =
   { 0, 31, 29, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31 }
 };
 
-static const guint days_in_months[2][14] =
-{
-  { 0, 0, 31, 59, 90, 120, 151, 181, 212, 243, 273, 304, 334, 365 },
-  { 0, 0, 31, 60, 91, 121, 152, 182, 213, 244, 274, 305, 335, 366 }
-};
-
 static gboolean
 leap (guint year)
 {
