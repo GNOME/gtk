4a8ff565bc06d52806ea080cdb436a647d923d7c
David King <dking@redhat.com>
2015-08-11 16:06:17 +0100

    listbox: Describe behavior change in GtkListBoxCreateWidgetFunc
    Help applicaiton authors who may already be binding GtkListBox to a
    GListModel by describing the change for 3.18.
    
    https://bugzilla.gnome.org/show_bug.cgi?id=753392

--- gtk/gtklistbox.h
+++ gtk/gtklistbox.h
@@ -177,6 +177,11 @@ typedef void (*GtkListBoxUpdateHeaderFunc) (GtkListBoxRow *row,
  * Called for list boxes that are bound to a #GListModel with
  * gtk_list_box_bind_model() for each item that gets added to the model.
  *
+ * Versions of GTK+ prior to 3.18 called gtk_widget_show_all() on the rows
+ * created by the GtkListBoxCreateWidgetFunc, but this forced all widgets
+ * inside the row to be shown, and is no longer the case. Applications should
+ * be updated to show the desired row widgets.
+ *
  * Returns: (transfer full): a #GtkWidget that represents @item
  *
  * Since: 3.16
