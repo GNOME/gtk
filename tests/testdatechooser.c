ff5fb53e688f1e836625465da59196a08e80162a
Matthias Clasen <mclasen@redhat.com>
2015-08-31 15:40:59 -0400

    Use the theme by default in testdatechooser
    This adds a --builtin-css commandline switch which lets you test
    my clumsy hardcoded css, instead of the nice styling coming from
    Adwaita.

--- tests/testdatechooser.c
+++ tests/testdatechooser.c
@@ -25,20 +25,24 @@ int
 main (int argc, char *argv[])
 {
   GtkWidget *window, *calendar;
-  GtkCssProvider *provider;
-  GError *error = NULL;
 
   gtk_init (NULL, NULL);
 
-  provider = gtk_css_provider_new ();
-  gtk_css_provider_load_from_data (provider, css, sizeof (css), &error);
-  if (error)
+  if (argc > 1 && g_strcmp0 (argv[1], "--builtin-css") == 0)
     {
-      g_print ("%s", error->message);
-      g_error_free (error);
+      GtkCssProvider *provider;
+      GError *error = NULL;
+
+      provider = gtk_css_provider_new ();
+      gtk_css_provider_load_from_data (provider, css, sizeof (css), &error);
+      if (error)
+        {
+          g_print ("%s", error->message);
+          g_error_free (error);
+        }
+      gtk_style_context_add_provider_for_screen (gdk_screen_get_default (),
+                                                 GTK_STYLE_PROVIDER (provider), 800);
     }
-  gtk_style_context_add_provider_for_screen (gdk_screen_get_default (),
-                                             GTK_STYLE_PROVIDER (provider), 800);
 
   window = gtk_window_new (GTK_WINDOW_TOPLEVEL);
 
