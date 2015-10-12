c05dc28952aa8ae2fb954ebf1fffc1100d8a6057
Timm Bäder <mail@baedert.org>
2015-10-12 13:11:36 +0200

    gtk-query-settings: Add license header

--- gtk/gtk-query-settings.c
+++ gtk/gtk-query-settings.c
@@ -1,3 +1,20 @@
+/*  Copyright 2015 Timm Bäder
+ *
+ * GTK+ is free software; you can redistribute it and/or modify it
+ * under the terms of the GNU Lesser General Public License as
+ * published by the Free Software Foundation; either version 2 of the
+ * License, or (at your option) any later version.
+ *
+ * GLib is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public
+ * License along with GTK+; see the file COPYING.  If not,
+ * see <http://www.gnu.org/licenses/>.
+ */
+
 #include <glib.h>
 #include <gtk/gtk.h>
 #include <string.h>
