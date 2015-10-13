7993feccf510bebb7c5011dbf4b191bddd5190de
Matthias Clasen <mclasen@redhat.com>
2015-10-13 08:28:50 -0400

    Beef up the shortcuts example more
    Add separate buttons for showing the full and filtered Clocks
    example, and make the buttons work more than once.

--- demos/gtk-demo/shortcutswindow.c
+++ demos/gtk-demo/shortcutswindow.c
@@ -1,10 +1,24 @@
 /* Shortcuts Window
  *
- * GtkShortcutsWindow is a window that provides a help overview
- * for shortcuts in an application.
+ * GtkShortcutsWindow is a window that provides a help overlay
+ * for shortcuts and gestures in an application.
  */
 
 #include <gtk/gtk.h>
+
+static void
+show_clock_shortcuts (GtkWidget *window)
+{
+  g_object_set (window, "view-name", NULL, NULL);
+  gtk_widget_show (window);
+}
+
+static void
+show_clock_shortcuts_stopwatch (GtkWidget *window)
+{
+  g_object_set (window, "view-name", "stopwatch", NULL);
+  gtk_widget_show (window);
+}
 
 GtkWidget *
 do_shortcutswindow (GtkWidget *do_widget)
@@ -25,6 +39,10 @@ do_shortcutswindow (GtkWidget *do_widget)
       GtkBuilder *builder;
 
       builder = gtk_builder_new_from_resource ("/shortcuts/shortcuts.ui");
+      gtk_builder_add_callback_symbols (builder,
+                                        "show_clock_shortcuts", G_CALLBACK (show_clock_shortcuts),
+                                        "show_clock_shortcuts_stopwatch", G_CALLBACK (show_clock_shortcuts_stopwatch),
+                                        NULL);
       gtk_builder_connect_signals (builder, NULL);
       window = GTK_WIDGET (gtk_builder_get_object (builder, "window1"));
       gtk_window_set_screen (GTK_WINDOW (window),
