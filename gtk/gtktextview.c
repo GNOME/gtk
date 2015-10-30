2b998aaad7146b1ac300eefc39ed6fb851a763b8
Matthias Clasen <mclasen@redhat.com>
2015-10-30 18:11:13 -0400

    text view: Set an element name
    And it will be...drumroll...textview.

--- gtk/gtktextview.c
+++ gtk/gtktextview.c
@@ -1635,6 +1635,7 @@ G_GNUC_END_IGNORE_DEPRECATIONS
 				GTK_TYPE_DIRECTION_TYPE, GTK_DIR_TAB_BACKWARD);
 
   gtk_widget_class_set_accessible_type (widget_class, GTK_TYPE_TEXT_VIEW_ACCESSIBLE);
+  gtk_widget_class_set_css_name (widget_class, "textview");
 
   quark_text_selection_data = g_quark_from_static_string ("gtk-text-view-text-selection-data");
   quark_gtk_signal = g_quark_from_static_string ("gtk-signal");
