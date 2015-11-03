870461475a74c9dfb8835ed503702566203e8e2a
Matthias Clasen <mclasen@redhat.com>
2015-11-03 13:28:52 -0500

    switch: Add a diagram to CSS node documentation

--- gtk/gtkswitch.c
+++ gtk/gtkswitch.c
@@ -38,6 +38,11 @@
  *
  * # CSS nodes
  *
+ * |[<!-- language="plain" -->
+ * switch
+ * ╰── slider
+ * ]|
+ *
  * GtkSwitch has two css nodes, the main node with the name switch and a subnode
  * named slider. Neither of them is using any style classes.
  */
