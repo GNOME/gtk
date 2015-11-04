452e303ec06bd79d4af98c9e032ab221d0813b4a
Emmanuele Bassi <ebassi@gnome.org>
2015-11-04 11:45:19 +0000

    docs: Fix typo in gdk_screen_get_scale_factor()

--- gdk/gdkscreen.c
+++ gdk/gdkscreen.c
@@ -1095,7 +1095,7 @@ gdk_screen_get_setting (GdkScreen   *screen,
  * on very high density outputs this can be a higher value (often 2).
  *
  * This can be used if you want to create pixel based data for a
- * particula monitor, but most of the time you’re drawing to a window
+ * particular monitor, but most of the time you’re drawing to a window
  * where it is better to use gdk_window_get_scale_factor() instead.
  *
  * Since: 3.10
