871cdf27ec250aba53ca7d6810b5990f684e95de
Carlos Soriano <csoriano@gnome.org>
2015-11-04 16:37:21 +0100

    gtkhidingbox: remove unneeded comment

--- gtk/gtkhidingbox.c
+++ gtk/gtkhidingbox.c
@@ -396,8 +396,6 @@ gtk_hiding_box_class_init (GtkHidingBoxClass *class)
 
   hiding_box_properties[PROP_SPACING] =
            g_param_spec_int ("spacing",
-                             /* TRANSLATORS: Here are 2 strings the same as in gtk/gtkbox.c
-                                in GTK+ project. Please use the same translation. */
                              _("Spacing"),
                              _("The amount of space between children"),
                              0, G_MAXINT, 0,
