bec58dc39e1c4280561a6fca6ff59c461b3d9e16
Matthias Clasen <mclasen@redhat.com>
2015-11-05 07:44:04 -0500

    searchbar: Stop adding a style class
    We have an element name now.

--- gtk/gtksearchbar.c
+++ gtk/gtksearchbar.c
@@ -433,7 +433,6 @@ static void
 gtk_search_bar_init (GtkSearchBar *bar)
 {
   GtkSearchBarPrivate *priv = gtk_search_bar_get_instance_private (bar);
-  GtkStyleContext *context;
 
   gtk_widget_init_template (GTK_WIDGET (bar));
 
@@ -450,11 +449,6 @@ gtk_search_bar_init (GtkSearchBar *bar)
   gtk_widget_set_no_show_all (priv->close_button, TRUE);
   g_signal_connect (priv->close_button, "clicked",
                     G_CALLBACK (close_button_clicked_cb), bar);
-
-  context = gtk_widget_get_style_context (GTK_WIDGET (bar));
-  gtk_style_context_add_class (context, "search-bar");
-  gtk_style_context_add_class (context, GTK_STYLE_CLASS_HORIZONTAL);
-
 };
 
 /**
