GNOME/gtk

commit a5b1cdd0c1178cbebc86a116645c2ecac62aaf65

GtkWindow: Fix the shadow width logic
Previous commit 305b34a "GtkWindow: fix move/get position with CSD"
introduced a regression because some windows presumably use shadows but
actually don't, resulting in a negative offset being wrongly applied.

Problem is that get_shadow_width() would return non-zero shadows even
for windows that have no shadow, thus causing the negative offset.

Fix the logic in get_shadow_width() and gtk_window_should_use_csd() so
that get_shadow_width() returns accurate values.

Bugzilla: https://bugzilla.gnome.org/show_bug.cgi?id=756618

gtk/gtkwindow.c
@@ -5948,14 +5948,17 @@ gtk_window_should_use_csd (GtkWindow *window)
   GtkWindowPrivate *priv = window->priv;
   const gchar *csd_env;
 
-  if (priv->csd_requested)
-    return TRUE;
-
   if (!priv->decorated)
     return FALSE;
 
   if (priv->type == GTK_WINDOW_POPUP)
     return FALSE;
+
+  if (priv->csd_requested)
+    return TRUE;
+
+  if (priv->use_client_shadow)
+    return TRUE;
 
 #ifdef GDK_WINDOWING_BROADWAY
   if (GDK_IS_BROADWAY_DISPLAY (gtk_widget_get_display (GTK_WIDGET (window))))
@@ -6676,8 +6679,10 @@ get_shadow_width (GtkWindow *window,
   if (!priv->decorated)
     return;
 
-  if (!priv->client_decorated &&
-      !(gtk_window_should_use_csd (window) &&
+  if (!priv->client_decorated)
+    return;
+
+  if (!(gtk_window_should_use_csd (window) &&
         gtk_window_supports_client_shadow (window)))
     return;
 

commit 9757ea2c49b21c11710e7148d8cda19275453b80

gtkwindow: Fix resize without "_GTK_FRAME_EXTENTS"
git commit a5b1cdd0 introduced a regression where CSD windows are not
resizable with metacity.

Reason being that metacity does not support "_GTK_FRAME_EXTENTS" and
therefore gtk_window_supports_client_shadow() would always return FALSE.

This explains why it works with window managers which support
"_GTK_FRAME_EXTENTS" such as mutter/gnome-shell or xfwm4.

Partially revert commit a5b1cdd0 to reinstate the logic in
get_shadow_width().

Bugzilla: https://bugzilla.gnome.org/show_bug.cgi?id=757805

gtk/gtkwindow.c
@@ -6722,10 +6722,8 @@ get_shadow_width (GtkWindow *window,
   if (!priv->decorated)
     return;
 
-  if (!priv->client_decorated)
-    return;
-
-  if (!(gtk_window_should_use_csd (window) &&
+  if (!priv->client_decorated &&
+      !(gtk_window_should_use_csd (window) &&
         gtk_window_supports_client_shadow (window)))
     return;
 