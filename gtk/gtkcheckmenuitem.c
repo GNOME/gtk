1190a61c276a122e9ebc300420b13037c1da4bac
Matthias Clasen <mclasen@redhat.com>
2015-11-13 07:11:23 -0500

    check menu item: Fix a typo
    It is check, not checl. Pointed out by Giovanni Campagna in
    https://bugzilla.gnome.org/show_bug.cgi?id=757968

--- gtk/gtkcheckmenuitem.c
+++ gtk/gtkcheckmenuitem.c
@@ -498,7 +498,7 @@ gtk_check_menu_item_set_draw_as_radio (GtkCheckMenuItem *check_menu_item,
       if (draw_as_radio)
         gtk_css_node_set_name (priv->indicator_node, I_("radio"));
       else
-        gtk_css_node_set_name (priv->indicator_node, I_("checl"));
+        gtk_css_node_set_name (priv->indicator_node, I_("check"));
 
       gtk_widget_queue_draw (GTK_WIDGET (check_menu_item));
 
