53e734f2b3076d7196f27105848f129a2d19d2ef
Emmanuele Bassi <ebassi@gnome.org>
2015-11-19 12:44:17 +0000

    Do not use the GPL v3 blurb for LGPL v2.1 files
    GTK+ is licensed under the terms of the GNU LGPL v2.1+.

--- gtk/gtkplacessidebar.h
+++ gtk/gtkplacessidebar.h
@@ -1,27 +1,24 @@
 /* -*- Mode: C; indent-tabs-mode: t; c-basic-offset: 8; tab-width: 8 -*- */
 
-/*
- *  GtkPlacesSidebar - sidebar widget for places in the filesystem
+/* GtkPlacesSidebar - sidebar widget for places in the filesystem
  *
- *  This code comes from Nautilus, GNOME’s file manager.
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Lesser General Public License as published
+ * by the Free Software Foundation, either version 2.1 of the License, or
+ * (at your option) any later version.
  *
- *  This library is free software; you can redistribute it and/or
- *  modify it under the terms of the GNU General Public License as
- *  published by the Free Software Foundation; either version 2 of the
- *  License, or (at your option) any later version.
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Lesser General Public License for more details.
  *
- *  This library is distributed in the hope that it will be useful,
- *  but WITHOUT ANY WARRANTY; without even the implied warranty of
- *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
- *  General Public License for more details.
+ * You should have received a copy of the GNU Lesser General Public License
+ * along with this program. If not, see <http://www.gnu.org/licenses/>.
  *
- *  You should have received a copy of the GNU General Public License
- *  along with this library; if not, write to the Free Software
- *  Foundation, Inc., 675 Mass Ave, Cambridge, MA 02139, USA.
+ * This code comes from Nautilus, GNOME’s file manager.
  *
- *  Authors : Mr Jamie McCracken (jamiemcc at blueyonder dot co dot uk)
- *            Federico Mena Quintero <federico@gnome.org>
- *
+ * Authors : Mr Jamie McCracken (jamiemcc at blueyonder dot co dot uk)
+ *           Federico Mena Quintero <federico@gnome.org>
  */
 
 #ifndef __GTK_PLACES_SIDEBAR_H__
--- gtk/gtkplacessidebarprivate.h
+++ gtk/gtkplacessidebarprivate.h
@@ -12,7 +12,7 @@
  * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
  * Lesser General Public License for more details.
  *
- * You should have received a copy of the GNU General Public License
+ * You should have received a copy of the GNU Lesser General Public License
  * along with this program.  If not, see <http://www.gnu.org/licenses/>.
  *
  * Authors: Carlos Soriano <csoriano@gnome.org>
