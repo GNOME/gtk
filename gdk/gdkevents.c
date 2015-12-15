47632df9a5e86753ff145ce610930321a63f92b0
Carlos Garnacho <carlosg@gnome.org>
2015-12-15 23:24:59 +0100

    gdkevents: Avoid gdk_device_manager_get_client_pointer()
    It's now deprecated.

--- gdk/gdkevents.c
+++ gdk/gdkevents.c
@@ -1634,22 +1634,21 @@ gdk_event_get_device (const GdkEvent *event)
     case GDK_KEY_RELEASE:
       {
         GdkDisplay *display;
-        GdkDeviceManager *device_manager;
         GdkDevice *client_pointer;
+        GdkSeat *seat;
 
         g_warning ("Event with type %d not holding a GdkDevice. "
                    "It is most likely synthesized outside Gdk/GTK+\n",
                    event->type);
 
         display = gdk_window_get_display (event->any.window);
-        device_manager = gdk_display_get_device_manager (display);
-        client_pointer = gdk_device_manager_get_client_pointer (device_manager);
+        seat = gdk_display_get_default_seat (display);
 
         if (event->type == GDK_KEY_PRESS ||
             event->type == GDK_KEY_RELEASE)
-          return gdk_device_get_associated_device (client_pointer);
+          return gdk_seat_get_keyboard (seat);
         else
-          return client_pointer;
+          return gdk_seat_get_pointer (seat);
       }
       break;
     default:
