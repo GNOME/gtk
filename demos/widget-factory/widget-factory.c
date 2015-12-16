GNOME/gtk

commit 1e1064398c7607d197c563c42f8901ac67982a9f

widget-factory: Disconnect clipboard watcher on close
The clipboard emit events after the button we connected it to was
already destroyed (on application close for example), so make sure we
don't try to use that dead button.

demos/widget-factory/widget-factory.c
@@ -1670,8 +1670,8 @@ activate (GApplication *app)
                     G_CALLBACK (textbuffer_notify_selection), widget);
   widget = (GtkWidget *)gtk_builder_get_object (builder, "pastebutton");
   g_signal_connect (widget, "clicked", G_CALLBACK (handle_cutcopypaste), widget2);
-  g_signal_connect (gtk_widget_get_clipboard (widget2, GDK_SELECTION_CLIPBOARD), "owner-change",
-                    G_CALLBACK (clipboard_owner_change), widget);
+  g_signal_connect_object (gtk_widget_get_clipboard (widget2, GDK_SELECTION_CLIPBOARD), "owner-change",
+                           G_CALLBACK (clipboard_owner_change), widget);
 
   widget = (GtkWidget *)gtk_builder_get_object (builder, "osd_frame");
   widget2 = (GtkWidget *)gtk_builder_get_object (builder, "totem_like_osd");

commit fe9301ac1bf272b43ec81dcf7693151d393f5db2

widget-factory: Make things compile again
1e1064398c7607d197c563c42f8901ac67982a9f broke the build.

When I run make, I should make sure to run it in the right directory.
And not in the gtk/ subdirectory that will never build widget-factory...

demos/widget-factory/widget-factory.c
@@ -1671,7 +1671,7 @@ activate (GApplication *app)
   widget = (GtkWidget *)gtk_builder_get_object (builder, "pastebutton");
   g_signal_connect (widget, "clicked", G_CALLBACK (handle_cutcopypaste), widget2);
   g_signal_connect_object (gtk_widget_get_clipboard (widget2, GDK_SELECTION_CLIPBOARD), "owner-change",
-                           G_CALLBACK (clipboard_owner_change), widget);
+                           G_CALLBACK (clipboard_owner_change), widget, 0);
 
   widget = (GtkWidget *)gtk_builder_get_object (builder, "osd_frame");
   widget2 = (GtkWidget *)gtk_builder_get_object (builder, "totem_like_osd");