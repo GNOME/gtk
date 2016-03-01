a3805333361fee37a3b1a974cfa6452a85169f08
Benjamin Otte <otte@redhat.com>
2016-03-01 18:34:43 +0100

    container: Remove check
    The check is not working for many containers anymore as they are not the
    direct parents of their children. We want to allow this behavior in more
    places.

--- gtk/gtkcontainer.c
+++ gtk/gtkcontainer.c
@@ -1897,13 +1897,6 @@ gtk_container_remove (GtkContainer *container,
 {
   g_return_if_fail (GTK_IS_CONTAINER (container));
   g_return_if_fail (GTK_IS_WIDGET (widget));
-  g_return_if_fail (_gtk_widget_get_parent (widget) == GTK_WIDGET (container) ||
-                    GTK_IS_ASSISTANT (container) ||
-                    GTK_IS_ACTION_BAR (container) ||
-                    GTK_IS_POPOVER_MENU (container) ||
-                    GTK_IS_SHORTCUTS_GROUP (container) ||
-                    GTK_IS_SHORTCUTS_SECTION (container) ||
-                    GTK_IS_SHORTCUTS_WINDOW (container));
 
   g_object_ref (container);
   g_object_ref (widget);
