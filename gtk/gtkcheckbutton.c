c48f0d6f8be64955566be5760bcf227211448217
Matthias Clasen <mclasen@redhat.com>
2016-03-03 21:26:39 -0500

    check button: Reinstate rtl allocation behavior
    In rtl mode, we must not just reverse the children,
    but also allocate from the right. Use the newly introduced
    box gadget api to achieve this.
    
    https://bugzilla.gnome.org/show_bug.cgi?id=762945

--- gtk/gtkcheckbutton.c
+++ gtk/gtkcheckbutton.c
@@ -154,6 +154,8 @@ gtk_check_button_direction_changed (GtkWidget        *widget,
   GtkCheckButtonPrivate *priv = gtk_check_button_get_instance_private (GTK_CHECK_BUTTON (widget));
 
   gtk_box_gadget_reverse_children (GTK_BOX_GADGET (priv->gadget));
+  gtk_box_gadget_set_allocate_reverse (GTK_BOX_GADGET (priv->gadget),
+                                       gtk_widget_get_direction (widget) == GTK_TEXT_DIR_RTL);
 
   GTK_WIDGET_CLASS (gtk_check_button_parent_class)->direction_changed (widget, previous_direction);
 }
