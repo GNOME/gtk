a28e76f09c5a2344563b987774092b894d4658a9
Matthias Clasen <mclasen@redhat.com>
2016-03-09 21:13:44 -0500

    GtkActionable: Improve property names
    These names show up as title in the property editor
    in the inspector, so we want them to look consistent
    with the rest.

--- gtk/gtkactionable.c
+++ gtk/gtkactionable.c
@@ -68,12 +68,12 @@ static void
 gtk_actionable_default_init (GtkActionableInterface *iface)
 {
   g_object_interface_install_property (iface,
-    g_param_spec_string ("action-name", P_("action name"),
+    g_param_spec_string ("action-name", P_("Action name"),
                          P_("The name of the associated action, like 'app.quit'"),
                          NULL, G_PARAM_READWRITE | G_PARAM_STATIC_STRINGS));
 
   g_object_interface_install_property (iface,
-    g_param_spec_variant ("action-target", P_("action target value"),
+    g_param_spec_variant ("action-target", P_("Action target value"),
                           P_("The parameter for action invocations"),
                           G_VARIANT_TYPE_ANY, NULL, G_PARAM_READWRITE | G_PARAM_STATIC_STRINGS));
 }
