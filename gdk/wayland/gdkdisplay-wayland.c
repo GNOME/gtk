55ea6e4dcc4bbe6c7fc40c8e814ad2ee99b3b2e2
Matthias Clasen <mclasen@redhat.com>
2016-05-01 10:51:41 -0400

    wayland: Make gdk_display_get_name work
    It should just return the same as gdk_screen_make_display_name.

--- gdk/wayland/gdkdisplay-wayland.c
+++ gdk/wayland/gdkdisplay-wayland.c
@@ -606,7 +606,13 @@ gdk_wayland_display_finalize (GObject *object)
 static const gchar *
 gdk_wayland_display_get_name (GdkDisplay *display)
 {
-  return "Wayland";
+  const gchar *name;
+
+  name = g_getenv ("WAYLAND_DISPLAY");
+  if (name == NULL)
+    name = "wayland-0";
+
+  return name;
 }
 
 static GdkScreen *
