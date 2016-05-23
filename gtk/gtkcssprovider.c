46b51e36b4655ffd54e26b99bfe89b7e9f7946bd
Matthias Clasen <mclasen@redhat.com>
2016-05-23 19:24:07 -0400

    Trivial typo fix

--- gtk/gtkcssprovider.c
+++ gtk/gtkcssprovider.c
@@ -56,20 +56,20 @@
  * It is able to parse [CSS-like][css-overview] input in order to style widgets.
  *
  * An application can make GTK+ parse a specific CSS style sheet by calling
- * gtk_css_provider_load_from_file() or gtk_css_provider_load_from_resouce()
+ * gtk_css_provider_load_from_file() or gtk_css_provider_load_from_resource()
  * and adding the provider with gtk_style_context_add_provider() or
  * gtk_style_context_add_provider_for_screen().
 
  * In addition, certain files will be read when GTK+ is initialized. First, the
- * file $XDG_CONFIG_HOME/gtk-3.0/gtk.css` is loaded if it exists. Then, GTK+
+ * file `$XDG_CONFIG_HOME/gtk-3.0/gtk.css` is loaded if it exists. Then, GTK+
  * loads the first existing file among
  * `XDG_DATA_HOME/themes/theme-name/gtk-VERSION/gtk.css`,
  * `$HOME/.themes/theme-name/gtk-VERSION/gtk.css`,
  * `$XDG_DATA_DIRS/themes/theme-name/gtk-VERSION/gtk.css` and
- * `DATADIR/share/themes/THEME/gtk-VERSION/gtk.css`, where THEME is the name of
- * the current theme (see the #GtkSettings:gtk-theme-name setting), DATADIR
+ * `DATADIR/share/themes/THEME/gtk-VERSION/gtk.css`, where `THEME` is the name of
+ * the current theme (see the #GtkSettings:gtk-theme-name setting), `DATADIR`
  * is the prefix configured when GTK+ was compiled (unless overridden by the
- * `GTK_DATA_PREFIX` environment variable), and VERSION is the GTK+ version number.
+ * `GTK_DATA_PREFIX` environment variable), and `VERSION` is the GTK+ version number.
  * If no file is found for the current version, GTK+ tries older versions all the
  * way back to 3.0.
  *
