32885605c7d589773930f095b4ef55c902cbf36d
Emmanuele Bassi <ebassi@gnome.org>
2016-06-08 14:20:02 +0100

    Fix build
    Commit 023f406c9617284ee3b88727df489f20cd066fd6 has a typo that results
    in this error:
    
    ../../../testsuite/gtk/notify.c: In function 'test_type':
    ../../../testsuite/gtk/notify.c:679:54: error: expected expression
    before ')' token
        (g_str_equal (pspec->name, "max-content-width") ||)
                                                          ^

--- testsuite/gtk/notify.c
+++ testsuite/gtk/notify.c
@@ -676,8 +676,8 @@ test_type (gconstpointer data)
 
       /* these depend on the min-content- properties in a way that breaks our test */
       if (g_type_is_a (type, GTK_TYPE_SCROLLED_WINDOW) &&
-	  (g_str_equal (pspec->name, "max-content-width") ||)
-	   g_str_equal (pspec->name, "max-content-height"))
+	  (g_str_equal (pspec->name, "max-content-width") ||
+	   g_str_equal (pspec->name, "max-content-height")))
 	continue;
 
       if (g_test_verbose ())
