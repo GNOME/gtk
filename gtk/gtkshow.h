f0e5f3da9154ebebc029a249a90ed17e7c706b8a
Matthias Clasen <mclasen@redhat.com>
2016-06-18 17:59:40 -0400

    Add gtk_show_uri_on_window
    The gtk_show_uri API doesn't let us specify a parent window. With
    portals, there may be an intermediate dialog, for which it is nice
    to have parent window information, to place it properly.

--- gtk/gtkshow.h
+++ gtk/gtkshow.h
@@ -25,6 +25,8 @@
 #error "Only <gtk/gtk.h> can be included directly."
 #endif
 
+#include <gtk/gtkwindow.h>
+
 G_BEGIN_DECLS
 
 GDK_AVAILABLE_IN_ALL
@@ -32,6 +34,12 @@ gboolean gtk_show_uri  (GdkScreen   *screen,
                         const gchar *uri,
                         guint32      timestamp,
                         GError     **error);
+
+GDK_AVAILABLE_IN_3_22
+gboolean gtk_show_uri_on_window (GtkWindow   *parent,
+                                 const char  *uri,
+                                 guint32      timestamp,
+                                 GError     **error);
 
 G_END_DECLS
 
