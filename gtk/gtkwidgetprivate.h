657bb588b72e0b624a81cb16c49cc1bc564a0dd9
Emmanuele Bassi <ebassi@gnome.org>
2016-06-23 17:35:42 +0100

    gtk: Use GskRenderNode to render widgets
    We need a virtual function to retrieve the GskRenderNode for each
    widget, which is supposed to attach its own children's GskRenderNodes.
    Additionally, we want to maintain the existing GtkWidget::draw mechanism
    for widgets that do not implement get_render_node() — as well as widgets
    that have handlers connected to the ::draw signal.

--- gtk/gtkwidgetprivate.h
+++ gtk/gtkwidgetprivate.h
@@ -302,6 +302,9 @@ void              gtk_widget_render                        (GtkWidget           
                                                             const cairo_region_t *region);
 
 
+GskRenderNode *   gtk_widget_get_render_node               (GtkWidget   *widget,
+                                                            GskRenderer *renderer);
+
 /* inline getters */
 
 static inline gboolean
