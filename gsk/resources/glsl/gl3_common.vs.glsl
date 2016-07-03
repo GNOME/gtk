28b490f14feaea74cbd406a053b30f62e5832005
Emmanuele Bassi <ebassi@gnome.org>
2016-07-03 21:12:22 +0100

    gsk: Rework how GLSL shaders are built
    The GL renderer should build the GLSL shaders using GskShaderBuilder.
    This allows us to separate the common parts into separate files, and
    assemble them as necessary, instead of shipping one big shader per type
    of GL API (GL3, GL legacy, and GLES).

---
+++ gsk/resources/glsl/gl3_common.vs.glsl
@@ -0,0 +1,7 @@
+uniform mat4 mvp;
+uniform float alpha;
+
+in vec2 position;
+in vec2 uv;
+
+out vec2 vUv;
---
+++ gsk/resources/glsl/gl_common.vs.glsl
@@ -0,0 +1,7 @@
+uniform mat4 mvp;
+uniform float alpha;
+
+attribute vec2 position;
+attribute vec2 uv;
+
+varying vec2 vUv;
