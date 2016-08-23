df965304d4f7cdd0b5b2c4268e28ee4b844cfdf9
Timm Bäder <mail@baedert.org>
2016-08-23 08:21:54 +0200

    meson: make gtk buildable
    x11 only

--- build_enum.py
+++ build_enum.py
@@ -6,21 +6,14 @@
 # backends is a fool's errand.
 
 import sys, os, shutil, subprocess
-
-# HORRIBLE, use current_source_dir() as an argument instead.
-h_array = ['--template', '../gdk/gdkenumtypes.h.template']
-
-c_array = ['--template', '../gdk/gdkenumtypes.c.template']
-
 # [perl, glib-mkenums]
 cmd = [sys.argv[1], sys.argv[2]]
-ofilename = sys.argv[3]
-headers = sys.argv[4:]
+template = sys.argv[3]
+ofilename = sys.argv[4]
+headers = sys.argv[5:]
 
-if ofilename.endswith('.h'):
-    arg_array = h_array
-else:
-    arg_array = c_array
+
+arg_array = ['--template', template];
 
 pc = subprocess.Popen(cmd + arg_array + headers, stdout=subprocess.PIPE)
 (stdo, _) = pc.communicate()
