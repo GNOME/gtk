ed30f271f54113466dff8f5ffdab4152846dbe54
Benjamin Otte <otte@redhat.com>
2016-09-09 14:49:12 +0200

    gdk: Insist axis.min_value < axis.max_value
    Remove fallback code that would substitute screen size otherwise.

--- gdk/gdkdevice.c
+++ gdk/gdkdevice.c
@@ -1708,21 +1708,8 @@ _gdk_device_translate_window_coord (GdkDevice *device,
   device_width = axis_info_x->max_value - axis_info_x->min_value;
   device_height = axis_info_y->max_value - axis_info_y->min_value;
 
-  if (device_width > 0)
-    x_min = axis_info_x->min_value;
-  else
-    {
-      device_width = gdk_screen_get_width (gdk_window_get_screen (window));
-      x_min = 0;
-    }
-
-  if (device_height > 0)
-    y_min = axis_info_y->min_value;
-  else
-    {
-      device_height = gdk_screen_get_height (gdk_window_get_screen (window));
-      y_min = 0;
-    }
+  x_min = axis_info_x->min_value;
+  y_min = axis_info_y->min_value;
 
   window_width = gdk_window_get_width (window);
   window_height = gdk_window_get_height (window);
