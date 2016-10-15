fc9826fb41b7739fff91eb880da670abb26efb1a
Emmanuele Bassi <ebassi@gnome.org>
2016-10-15 21:35:10 +0100

    Remove declarations for getresuid()/getresgid()
    Back in 2001, their prototypes were missing from system headers, but
    since glibc 2.3.2 they are defined in unistd.h if _GNU_SOURCE is
    defined.

--- gtk/gtkmain.c
+++ gtk/gtkmain.c
@@ -361,11 +361,6 @@ check_setugid (void)
   gid_t rgid, egid, sgid; /* Real, effective and saved group ID's */
   
 #ifdef HAVE_GETRESUID
-  /* These aren't in the header files, so we prototype them here.
-   */
-  int getresuid(uid_t *ruid, uid_t *euid, uid_t *suid);
-  int getresgid(gid_t *rgid, gid_t *egid, gid_t *sgid);
-
   if (getresuid (&ruid, &euid, &suid) != 0 ||
       getresgid (&rgid, &egid, &sgid) != 0)
 #endif /* HAVE_GETRESUID */
