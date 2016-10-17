46b10ca419494db39c19cb9de193237814e15772
Benjamin Otte <otte@redhat.com>
2016-10-17 20:18:33 +0200

    expander: Use gdk_window_new_input()

--- gtk/gtkexpander.c
+++ gtk/gtkexpander.c
@@ -571,33 +571,22 @@ gtk_expander_realize (GtkWidget *widget)
   GtkAllocation allocation;
   GtkExpanderPrivate *priv;
   GdkWindow *window;
-  GdkWindowAttr attributes;
-  gint attributes_mask;
 
   priv = GTK_EXPANDER (widget)->priv;
 
   gtk_widget_get_allocation (widget, &allocation);
 
-  attributes.window_type = GDK_WINDOW_CHILD;
-  attributes.x = allocation.x;
-  attributes.y = allocation.y;
-  attributes.width = allocation.width;
-  attributes.height = allocation.height;
-  attributes.wclass = GDK_INPUT_ONLY;
-  attributes.event_mask = gtk_widget_get_events (widget)
-                          | GDK_BUTTON_PRESS_MASK
-                          | GDK_BUTTON_RELEASE_MASK
-                          | GDK_ENTER_NOTIFY_MASK
-                          | GDK_LEAVE_NOTIFY_MASK;
-
-  attributes_mask = GDK_WA_X | GDK_WA_Y;
-
   window = gtk_widget_get_parent_window (widget);
   gtk_widget_set_window (widget, window);
   g_object_ref (window);
 
-  priv->event_window = gdk_window_new (gtk_widget_get_parent_window (widget),
-                                       &attributes, attributes_mask);
+  priv->event_window = gdk_window_new_input (gtk_widget_get_parent_window (widget),
+                                             gtk_widget_get_events (widget)
+                                             | GDK_BUTTON_PRESS_MASK
+                                             | GDK_BUTTON_RELEASE_MASK
+                                             | GDK_ENTER_NOTIFY_MASK
+                                             | GDK_LEAVE_NOTIFY_MASK,
+                                             &allocation);
   gtk_widget_register_window (widget, priv->event_window);
 
   gtk_gesture_set_window (priv->multipress_gesture, priv->event_window);
