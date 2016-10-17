cd90b56627cbd729a5ea386123b09451f08294de
Benjamin Otte <otte@redhat.com>
2016-10-18 00:22:04 +0200

    separatortoolitem: Use gdk_window_new_input()

--- gtk/gtkseparatortoolitem.c
+++ gtk/gtkseparatortoolitem.c
@@ -282,31 +282,21 @@ gtk_separator_tool_item_realize (GtkWidget *widget)
   GtkSeparatorToolItem *separator = GTK_SEPARATOR_TOOL_ITEM (widget);
   GtkSeparatorToolItemPrivate *priv = separator->priv;
   GdkWindow *window;
-  GdkWindowAttr attributes;
-  gint attributes_mask;
 
   gtk_widget_set_realized (widget, TRUE);
 
   gtk_widget_get_allocation (widget, &allocation);
 
-  attributes.window_type = GDK_WINDOW_CHILD;
-  attributes.x = allocation.x;
-  attributes.y = allocation.y;
-  attributes.width = allocation.width;
-  attributes.height = allocation.height;
-  attributes.wclass = GDK_INPUT_ONLY;
-  attributes.event_mask = gtk_widget_get_events (widget) |
-                          GDK_BUTTON_PRESS_MASK |
-                          GDK_BUTTON_RELEASE_MASK |
-                          GDK_POINTER_MOTION_MASK;
-  attributes_mask = GDK_WA_X | GDK_WA_Y;
-
   window = gtk_widget_get_parent_window (widget);
   gtk_widget_set_window (widget, window);
   g_object_ref (window);
 
-  priv->event_window = gdk_window_new (gtk_widget_get_parent_window (widget),
-                                       &attributes, attributes_mask);
+  priv->event_window = gdk_window_new_input (gtk_widget_get_parent_window (widget),
+                                             gtk_widget_get_events (widget)
+                                             | GDK_BUTTON_PRESS_MASK
+                                             | GDK_BUTTON_RELEASE_MASK
+                                             | GDK_POINTER_MOTION_MASK,
+                                             &allocation);
   gtk_widget_register_window (widget, priv->event_window);
 }
 
