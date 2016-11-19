c9c391bd55c23b1b3733ebe88be5db5a1a1b819d
Matthias Clasen <mclasen@redhat.com>
2016-11-19 12:27:57 -0500

    Drop macro wrappers for get_version functions
    These are not really useful, and were added long ago
    as a clutch for transitioning to the new names.

--- gtk/gtkmain.h
+++ gtk/gtkmain.h
@@ -63,12 +63,6 @@ guint gtk_get_binary_age    (void) G_GNUC_CONST;
 GDK_AVAILABLE_IN_ALL
 guint gtk_get_interface_age (void) G_GNUC_CONST;
 
-#define gtk_major_version gtk_get_major_version ()
-#define gtk_minor_version gtk_get_minor_version ()
-#define gtk_micro_version gtk_get_micro_version ()
-#define gtk_binary_age gtk_get_binary_age ()
-#define gtk_interface_age gtk_get_interface_age ()
-
 GDK_AVAILABLE_IN_ALL
 const gchar* gtk_check_version (guint   required_major,
                                 guint   required_minor,
