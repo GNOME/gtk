7d695068e2634e7dcc2aa9943ed577b806cb0af7
Matthias Clasen <mclasen@redhat.com>
2016-11-23 14:44:37 -0500

    notebook: Don't leak arrow gadgets
    This was pointed out in
    https://bugzilla.gnome.org/show_bug.cgi?id=774743

--- gtk/gtknotebook.c
+++ gtk/gtknotebook.c
@@ -1843,6 +1843,10 @@ gtk_notebook_finalize (GObject *object)
   g_clear_object (&priv->gadget);
   g_clear_object (&priv->header_gadget);
   g_clear_object (&priv->tabs_gadget);
+  g_clear_object (&priv->arrow_gadget[0]);
+  g_clear_object (&priv->arrow_gadget[1]);
+  g_clear_object (&priv->arrow_gadget[2]);
+  g_clear_object (&priv->arrow_gadget[3]);
   g_clear_object (&priv->stack_gadget);
 
   G_OBJECT_CLASS (gtk_notebook_parent_class)->finalize (object);
