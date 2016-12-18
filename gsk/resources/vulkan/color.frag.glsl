1760e0d3f3d9f447bfdb1c29675497d23a60fd4f
Benjamin Otte <otte@redhat.com>
2016-12-18 07:08:37 +0100

    vulkan: Get rid of color in push constants
    The color comes in instance data now, no need to abuse push constants
    for it.

--- gsk/resources/vulkan/color.frag.glsl
+++ gsk/resources/vulkan/color.frag.glsl
@@ -2,11 +2,6 @@
 
 layout(location = 0) in vec4 inColor;
 
-layout(push_constant) uniform PushConstants {
-    mat4 mvp;
-    vec4 color;
-} push;
-
 layout(location = 0) out vec4 color;
 
 void main()
