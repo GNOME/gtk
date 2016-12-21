75510f7123a4fa479357ee7c138957e6fbb63bd0
Benjamin Otte <otte@redhat.com>
2016-12-21 07:20:28 +0100

    gsk: Export gsk_render_node_get_bounds()
    I'll need it in tests/testsuite soon.

--- gsk/gskrendernode.h
+++ gsk/gskrendernode.h
@@ -166,6 +166,9 @@ void                    gsk_render_node_set_name                (GskRenderNode *
                                                                  const char    *name);
 GDK_AVAILABLE_IN_3_90
 const char *            gsk_render_node_get_name                (GskRenderNode *node);
+GDK_AVAILABLE_IN_3_90
+void                    gsk_render_node_get_bounds              (GskRenderNode   *node,
+                                                                 graphene_rect_t *frame);
 
 GDK_AVAILABLE_IN_3_90
 void                    gsk_render_node_draw                    (GskRenderNode *node,
