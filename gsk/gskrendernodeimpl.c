08f9a6078bf9a4a748ef8ad6a039e97f33937b9a
Benjamin Otte <otte@redhat.com>
2016-12-23 02:05:54 +0100

    gsk: Include config.h
    The more you know:
    Not including config.h causes the public API implemented in this file to
    not be exported in the resulting library.

--- gsk/gskrendernodeimpl.c
+++ gsk/gskrendernodeimpl.c
@@ -16,6 +16,8 @@
  * License along with this library. If not, see <http://www.gnu.org/licenses/>.
  */
 
+#include "config.h"
+
 #include "gskrendernodeprivate.h"
 
 #include "gskcairoblurprivate.h"
