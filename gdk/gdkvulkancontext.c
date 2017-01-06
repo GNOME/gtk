fc1cfd569a9897ec01b6661df1e0d054a74f5ac3
Georges Basile Stavracas Neto <georges.stavracas@gmail.com>
2017-01-06 01:41:20 -0200

    vulkancontext: Improve debug output

--- gdk/gdkvulkancontext.c
+++ gdk/gdkvulkancontext.c
@@ -682,23 +682,23 @@ gdk_vulkan_debug_report (VkDebugReportFlagsEXT      flags,
 {
   if (flags & VK_DEBUG_REPORT_ERROR_BIT_EXT)
     {
-      g_critical ("Vulkan: %s: %s\n", pLayerPrefix, pMessage);
+      g_critical ("Vulkan: %s: %s", pLayerPrefix, pMessage);
     }
   else if (flags & VK_DEBUG_REPORT_WARNING_BIT_EXT)
     {
-      g_critical ("Vulkan: %s: %s\n", pLayerPrefix, pMessage);
+      g_critical ("Vulkan: %s: %s", pLayerPrefix, pMessage);
     }
   else if (flags & VK_DEBUG_REPORT_PERFORMANCE_WARNING_BIT_EXT)
     {
-      g_warning ("Vulkan: %s: %s\n", pLayerPrefix, pMessage);
+      g_warning ("Vulkan: %s: %s", pLayerPrefix, pMessage);
     }
   else if (flags & VK_DEBUG_REPORT_DEBUG_BIT_EXT)
     {
-      g_debug ("Vulkan: %s: %s\n", pLayerPrefix, pMessage);
+      g_debug ("Vulkan: %s: %s", pLayerPrefix, pMessage);
     }
   else
     {
-      g_message ("Vulkan: %s: %s\n", pLayerPrefix, pMessage);
+      g_message ("Vulkan: %s: %s", pLayerPrefix, pMessage);
     }
   
   return VK_FALSE;
