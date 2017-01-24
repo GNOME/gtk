98454d7470043d127196a74c4219d9e8b105732c
Benjamin Otte <otte@redhat.com>
2017-01-24 03:14:53 +0100

    image: Don't take icon size in set_from_definition()

--- gtk/gtkimageprivate.h
+++ gtk/gtkimageprivate.h
@@ -33,8 +33,7 @@
 G_BEGIN_DECLS
 
 void            gtk_image_set_from_definition           (GtkImage               *image,
-                                                         GtkImageDefinition     *def,
-                                                         GtkIconSize             size);
+                                                         GtkImageDefinition     *def);
 
 G_END_DECLS
 
