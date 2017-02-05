4934e04239a2238635a38be7328121364f1face3
Timm Bäder <mail@baedert.org>
2017-02-05 17:11:24 +0100

    gtkcontainerprivate: Remove unused function prototypes

--- gtk/gtkcontainerprivate.h
+++ gtk/gtkcontainerprivate.h
@@ -28,15 +28,11 @@ G_BEGIN_DECLS
 
 void     gtk_container_queue_resize_handler    (GtkContainer *container);
 void     _gtk_container_queue_restyle          (GtkContainer *container);
-void     _gtk_container_clear_resize_widgets   (GtkContainer *container);
-gchar*   _gtk_container_child_composite_name   (GtkContainer *container,
-                                                GtkWidget    *child);
 void     _gtk_container_dequeue_resize_handler (GtkContainer *container);
 GList *  _gtk_container_focus_sort             (GtkContainer     *container,
                                                 GList            *children,
                                                 GtkDirectionType  direction,
                                                 GtkWidget        *old_focus);
-gboolean _gtk_container_get_reallocate_redraws (GtkContainer *container);
 
 void      _gtk_container_stop_idle_sizer        (GtkContainer *container);
 void      _gtk_container_maybe_start_idle_sizer (GtkContainer *container);
