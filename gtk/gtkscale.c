3e28b1fc6435ba811dcf60a835084b470b12b2b4
Daniel Boles <dboles@src.gnome.org>
2017-02-19 10:13:34 +0000

    Scale: Fix signal documentation syntax
    Oops, sorry. Thanks to Cristoph Reiter for correcting me on this.

--- gtk/gtkscale.c
+++ gtk/gtkscale.c
@@ -727,7 +727,7 @@ gtk_scale_class_init (GtkScaleClass *class)
   class->get_layout_offsets = gtk_scale_real_get_layout_offsets;
 
   /**
-   * GtkScale:format-value:
+   * GtkScale::format-value:
    * @scale: the object which received the signal
    * @value: the value to format
    *
@@ -767,7 +767,7 @@ gtk_scale_class_init (GtkScaleClass *class)
    *
    * The number of decimal places to which the value is rounded when it is
    * changed. This also sets the number of digits shown in the displayed value
-   * when using the default handler for the #GtkScale:format-value signal.
+   * when using the default handler for the #GtkScale::format-value signal.
    */
   properties[PROP_DIGITS] =
       g_param_spec_int ("digits",
@@ -1091,11 +1091,11 @@ gtk_scale_new_with_range (GtkOrientation orientation,
  *
  * Sets the number of decimal places to which the value is rounded when it is
  * changed. This also sets the number of digits shown in the displayed value
- * when using the default handler for the #GtkScale:format-value signal.
+ * when using the default handler for the #GtkScale::format-value signal.
  *
  * Note that rounding to a small number of digits can interfere with
  * the smooth autoscrolling that is built into #GtkScale. As an alternative,
- * you can use the #GtkScale:format-value signal to format the displayed
+ * you can use the #GtkScale::format-value signal to format the displayed
  * value yourself.
  */
 void
@@ -1129,7 +1129,7 @@ gtk_scale_set_digits (GtkScale *scale,
  * @scale: a #GtkScale
  *
  * Gets the number of decimal places to which the value is rounded on change.
- * This number is also used by the default #GtkScale:format-value handler.
+ * This number is also used by the default #GtkScale::format-value handler.
  *
  * Returns: the number of decimal places
  */
@@ -1904,7 +1904,7 @@ weed_out_neg_zero (gchar *str,
 }
 
 /*
- * Emits the #GtkScale:format-value signal.
+ * Emits the #GtkScale::format-value signal.
  *
  * Returns: formatted value
  */
