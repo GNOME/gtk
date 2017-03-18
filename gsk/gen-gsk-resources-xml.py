4f99513b1897ea5dac87e6cc65401b2de72fa2ff
Tim-Philipp Müller <tim@centricular.com>
2017-03-18 12:14:12 +0000

    meson: gsk: generate gsk.resources.xml

---
+++ gsk/gen-gsk-resources-xml.py
@@ -0,0 +1,52 @@
+#!/usr/bin/env python3
+#
+# Generate gsk.resources.xml
+#
+# Usage: gen-gsk-resources-xml OUTPUT-FILE [INPUT-FILE1] [INPUT-FILE2] ...
+
+import os, sys
+
+source_shaders = []
+vulkan_compiled_shaders = []
+vulkan_shaders = []
+
+for f in sys.argv[2:]:
+  if f.endswith('.glsl'):
+    source_shaders.append(f)
+  elif f.endswith('.spv'):
+    vulkan_compiled_shaders.append(f)
+  elif f.endswith('.frag') or f.endswith('.vert'):
+    vulkan_shaders.append(f)
+  else:
+    sys.exit(-1) # FIXME: error message
+
+xml = '''<?xml version='1.0' encoding='UTF-8'?>
+<gresources>
+  <gresource prefix='/org/gtk/libgsk'>
+
+'''
+
+for f in source_shaders:
+  xml += '    <file alias=\'glsl/{0}\'>resources/glsl/{0}</file>\n'.format(os.path.basename(f))
+
+xml += '\n'
+
+for f in vulkan_compiled_shaders:
+  xml += '    <file alias=\'vulkan/{0}\'>resources/vulkan/{0}</file>\n'.format(os.path.basename(f))
+
+xml += '\n'
+
+for f in vulkan_shaders:
+  xml += '    <file alias=\'vulkan/{0}\'>resources/vulkan/{0}</file>\n'.format(os.path.basename(f))
+
+xml += '''
+  </gresource>
+</gresources>'''
+
+if len(sys.argv) > 1 and sys.argv[1] != '-':
+  outfile = sys.argv[1]
+  f = open(outfile, 'w')
+  f.write(xml)
+  f.close()
+else:
+  print(xml)
