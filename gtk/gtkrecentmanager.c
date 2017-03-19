7074607635c61163f70d86ebacdfad6816100ccb
Timm Bäder <mail@baedert.org>
2017-03-19 14:28:32 +0100

    GtkRecentManager: Remove superfluous local variable

--- gtk/gtkrecentmanager.c
+++ gtk/gtkrecentmanager.c
@@ -786,8 +786,6 @@ gtk_recent_manager_add_item_query_info (GObject      *source_object,
 
   if (file_info)
     {
-      gchar *content_type;
-
       content_type = g_file_info_get_attribute_as_string (file_info, G_FILE_ATTRIBUTE_STANDARD_FAST_CONTENT_TYPE);
 
       if (G_LIKELY (content_type))
