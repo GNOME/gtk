b20f04d18d4260566c449c0cda3134a10d61c4eb
Carlos Garnacho <carlosg@gnome.org>
2017-03-31 18:03:42 +0200

    gtktreeview: Drop gtk_gesture_set_window() API
    It is now meaningless and wrong, since GdkWindows aren't used anymore
    to determine the event target.

--- gtk/gtktreeview.c
+++ gtk/gtktreeview.c
@@ -2295,11 +2295,6 @@ gtk_tree_view_realize (GtkWidget *widget)
   gtk_tree_view_set_enable_tree_lines (tree_view, tree_view->priv->tree_lines_enabled);
 
   install_presize_handler (tree_view); 
-
-  gtk_gesture_set_window (tree_view->priv->multipress_gesture,
-                          tree_view->priv->bin_window);
-  gtk_gesture_set_window (tree_view->priv->drag_gesture,
-                          tree_view->priv->bin_window);
 }
 
 static void
@@ -2375,9 +2370,6 @@ gtk_tree_view_unrealize (GtkWidget *widget)
       gdk_window_destroy (priv->drag_highlight_window);
       priv->drag_highlight_window = NULL;
     }
-
-  gtk_gesture_set_window (tree_view->priv->multipress_gesture, NULL);
-  gtk_gesture_set_window (tree_view->priv->drag_gesture, NULL);
 
   GTK_WIDGET_CLASS (gtk_tree_view_parent_class)->unrealize (widget);
 }
