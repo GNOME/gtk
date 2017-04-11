42aa4c138c5a1ef2aafba3bd4a2b0f55bc822b29
Georges Basile Stavracas Neto <georges.stavracas@gmail.com>
2017-04-11 16:46:14 -0300

    placesview: fix testsuite
    Because the network monitor can perfectly be NULL,
    the tests were failing on that for GtkPlacesView
    always tries to disconnect this handler.
    
    Fix that by only disconnecting the handler when
    the network monitor exists.
    
    https://bugzilla.gnome.org/show_bug.cgi?id=781195

--- gtk/gtkplacesview.c
+++ gtk/gtkplacesview.c
@@ -398,7 +398,9 @@ gtk_places_view_destroy (GtkWidget *widget)
   priv->destroyed = 1;
 
   g_signal_handlers_disconnect_by_func (priv->volume_monitor, update_places, widget);
-  g_signal_handlers_disconnect_by_func (priv->network_monitor, update_places, widget);
+
+  if (priv->network_monitor)
+    g_signal_handlers_disconnect_by_func (priv->network_monitor, update_places, widget);
 
   g_cancellable_cancel (priv->cancellable);
   g_cancellable_cancel (priv->networks_fetching_cancellable);
