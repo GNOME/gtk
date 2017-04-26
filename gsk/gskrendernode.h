6c7f1d88854f3f7a91baa31ce9a8e672c3f2a7a3
Timm Bäder <mail@baedert.org>
2017-04-26 11:06:32 +0200

    GskRenderNode: add missing prototype

--- gsk/gskrendernode.h
+++ gsk/gskrendernode.h
@@ -166,6 +166,9 @@ GskRenderNode *         gsk_blend_node_new                      (GskRenderNode  
                                                                  GskRenderNode            *top,
                                                                  GskBlendMode              blend_mode);
 
+GDK_AVAILABLE_IN_3_92
+GskBlendMode            gsk_blend_node_get_blend_mode           (GskRenderNode            *node);
+
 GDK_AVAILABLE_IN_3_90
 GskRenderNode *         gsk_cross_fade_node_new                 (GskRenderNode            *start,
                                                                  GskRenderNode            *end,
