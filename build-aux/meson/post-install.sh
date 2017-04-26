c1965150c72cc11d51febd4fec0e4334601f947c
Emmanuele Bassi <ebassi@gnome.org>
2017-04-26 15:59:43 +0100

    build: Add post-install script
    When building GTK+ straight from the repository without any assistance
    from packaging tools, we need to trigger system-wide updates, like the
    icon theme cache update, or the schema compilation.

---
+++ build-aux/meson/post-install.sh
@@ -0,0 +1,21 @@
+#!/bin/sh
+
+gtk_version=$1
+gtk_abi_version=$2
+gtk_libdir=$3
+gtk_datadir=$4
+
+# Package managers set this so we don't need to run
+if [ -z "$DESTDIR" ]; then
+  echo Compiling GSettings schemas...
+  glib-compile-schemas ${gtk_datadir}/glib-2.0/schemas
+
+  echo Updating desktop database...
+  update-desktop-database -q ${gtk_datadir}/applications
+
+  echo Updating icon cache...
+  gtk-update-icon-cache -q -t -f ${gtk_datadir}/icons/hicolor
+
+  echo Updating input method modules cache...
+  gtk4-query-immodules > ${gtk_libdir}/gtk-${version}/${gtk_abi_version}/immodules.cache
+fi
