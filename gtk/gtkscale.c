aac544b742c48327e52cf145b826f709ce63cdf8
Christoph Reiter <reiter.christoph@gmail.com>
2017-04-30 19:25:14 +0200

    Scale: Fix documentation of digits per last revert
    Add a clarifying note that, under the original behaviour, :digits only
    rounds the Adjustment if :draw-value is TRUE at the time of the change.
    
    This is based on a similar commit made by Daniel Boles for the gtk3
    branches like 1cd0a8de0b70
    
    https://bugzilla.gnome.org/show_bug.cgi?id=781605
    https://bugzilla.gnome.org/show_bug.cgi?id=358970

--- gtk/gtkscale.c
+++ gtk/gtkscale.c
@@ -596,9 +596,10 @@ gtk_scale_new_with_range (GtkOrientation orientation,
  * @digits: the number of decimal places to display, 
  *     e.g. use 1 to display 1.0, 2 to display 1.00, etc
  * 
- * Sets the number of decimal places that are displayed in the value.
- * Also causes the value of the adjustment to be rounded off to this
- * number of digits, so the retrieved value matches the value the user saw.
+ * Sets the number of decimal places that are displayed in the value. Also
+ * causes the value of the adjustment to be rounded to this number of digits
+ * whenever it is changed, so the retrieved value matches the displayed one, if
+ * #GtkScale:draw-value is TRUE at the time of the change.
  */
 void
 gtk_scale_set_digits (GtkScale *scale,
