866bd834e4000780dc990ccae82249c70d0d8c01
Matthias Clasen <mclasen@redhat.com>
2017-05-11 15:01:05 -0400

    Revert "listbox: Properly remove placeholders"
    This reverts commit b7fdc5b447b44b5b7638a3ac41cac58136bc5ca2.
    
    This was found to break the expectations of existing listbox
    users.

--- gtk/gtklistbox.c
+++ gtk/gtklistbox.c
@@ -2451,16 +2451,6 @@ gtk_list_box_remove (GtkContainer *container,
 
   was_visible = gtk_widget_get_visible (child);
 
-  if (child == priv->placeholder)
-    {
-      gtk_widget_unparent (child);
-      priv->placeholder = NULL;
-      if (was_visible && gtk_widget_get_visible (widget))
-        gtk_widget_queue_resize (widget);
-
-      return;
-    }
-
   if (!GTK_IS_LIST_BOX_ROW (child))
     {
       row = g_hash_table_lookup (priv->header_hash, child);
@@ -2542,7 +2532,7 @@ gtk_list_box_forall (GtkContainer *container,
   GSequenceIter *iter;
   GtkListBoxRow *row;
 
-  if (priv->placeholder != NULL)
+  if (priv->placeholder != NULL && include_internals)
     callback (priv->placeholder, callback_target);
 
   iter = g_sequence_get_begin_iter (priv->children);
