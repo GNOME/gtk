67953e9cfb5dec4a509525fd572c3d21c38d8cff
Lapo Calamandrei <calamandrei@gmail.com>
2017-05-17 18:05:13 +0200

    Adwaita: modify parse-sass.sh script to use sassc

--- gtk/theme/Adwaita/parse-sass.sh
+++ gtk/theme/Adwaita/parse-sass.sh
@@ -1,3 +1,13 @@
-#!/bin/sh
+#! /bin/bash
 
-bundle exec sass --update --sourcemap=none .
+if [ ! "$(which sassc 2> /dev/null)" ]; then
+   echo sassc needs to be installed to generate the css.
+   exit 1
+fi
+
+SASSC_OPT="-M -t compact"
+
+echo Generating the css...
+
+sassc $SASSC_OPT gtk-contained.scss gtk-contained.css
+sassc $SASSC_OPT gtk-contained-dark.scss gtk-contained-dark.css
