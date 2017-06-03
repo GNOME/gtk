81049ea5c5b64ecca15367cc67364b2ff4ccb119
Matthias Clasen <mclasen@redhat.com>
2017-06-03 09:08:13 -0400

    export the center box api
    Without this, its not a public api after all...

--- gtk/gtkcenterbox.h
+++ gtk/gtkcenterbox.h
@@ -48,18 +48,26 @@ struct _GtkCenterBoxClass
   GtkWidgetClass parent_class;
 };
 
+GDK_AVAILABLE_IN_3_92
 GType      gtk_center_box_get_type (void) G_GNUC_CONST;
 
+GDK_AVAILABLE_IN_3_92
 GtkWidget *gtk_center_box_new (void);
+GDK_AVAILABLE_IN_3_92
 void       gtk_center_box_set_start_widget   (GtkCenterBox *self,
                                               GtkWidget    *child);
+GDK_AVAILABLE_IN_3_92
 void       gtk_center_box_set_center_widget  (GtkCenterBox *self,
                                               GtkWidget    *child);
+GDK_AVAILABLE_IN_3_92
 void       gtk_center_box_set_end_widget     (GtkCenterBox *self,
                                               GtkWidget    *child);
 
+GDK_AVAILABLE_IN_3_92
 GtkWidget * gtk_center_box_get_start_widget  (GtkCenterBox *self);
+GDK_AVAILABLE_IN_3_92
 GtkWidget * gtk_center_box_get_center_widget (GtkCenterBox *self);
+GDK_AVAILABLE_IN_3_92
 GtkWidget * gtk_center_box_get_end_widget    (GtkCenterBox *self);
 
 
