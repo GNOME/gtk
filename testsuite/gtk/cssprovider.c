b497b066b0cf7b3fae28cdc9ecf6a01410f4f41b
Timm Bäder <mail@baedert.org>
2017-06-16 14:31:54 +0200

    cssprovider test: Remove style property test case
    It's getting harder and harder to find a dummy style property to use
    here, so remove the test case since style properties should be going
    away soon anyway.

--- testsuite/gtk/cssprovider.c
+++ testsuite/gtk/cssprovider.c
@@ -22,35 +22,6 @@ test_section_in_load_from_data (void)
 }
 
 static void
-test_section_in_style_property (void)
-{
-  GtkCssProvider *provider;
-  GtkWidgetClass *widget_class;
-  GtkWidgetPath *path;
-  GParamSpec *pspec;
-  GValue value = G_VALUE_INIT;
-
-  provider = gtk_css_provider_new ();
-  g_signal_connect (provider, "parsing-error",
-                    G_CALLBACK (assert_section_is_not_null), NULL);
-  gtk_css_provider_load_from_data (provider, "* { -GtkScrollbar::has-backward-stepper random garbage goes here; }", -1);
-
-  widget_class = g_type_class_ref (GTK_TYPE_SCROLLBAR);
-  pspec = gtk_widget_class_find_style_property (widget_class, "has-backward-stepper");
-  g_assert (pspec);
-  path = gtk_widget_path_new ();
-  gtk_widget_path_append_type (path, GTK_TYPE_SCROLLBAR);
-
-G_GNUC_BEGIN_IGNORE_DEPRECATIONS;
-  gtk_style_provider_get_style_property (GTK_STYLE_PROVIDER (provider), path, 0, pspec, &value);
-G_GNUC_END_IGNORE_DEPRECATIONS;
-
-  gtk_widget_path_unref (path);
-  g_type_class_unref (widget_class);
-  g_object_unref (provider);
-}
-
-static void
 test_section_load_nonexisting_file (void)
 {
   GtkCssProvider *provider;
@@ -69,7 +40,6 @@ main (int argc, char *argv[])
   g_test_init (&argc, &argv, NULL);
 
   g_test_add_func ("/cssprovider/section-in-load-from-data", test_section_in_load_from_data);
-  g_test_add_func ("/cssprovider/section-in-style-property", test_section_in_style_property);
   g_test_add_func ("/cssprovider/load-nonexisting-file", test_section_load_nonexisting_file);
 
   return g_test_run ();
