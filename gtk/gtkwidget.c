4ce5bed724b4c7afd2d9dcd6acf4647405a33448
Daniel Boles <dboles.src@gmail.com>
2017-08-07 19:38:12 +0100

    Revert "Widget: Remove obsolete hack-arounds for HandleBox"
    This reverts commit 12805a4fbfa66b10efbe48c03595b0d5b7608d0e.
    
    I must have been looking at the wrong tree because HandleBox is only
    deprecated in GTK+ 3, not removed.

--- gtk/gtkwidget.c
+++ gtk/gtkwidget.c
@@ -6321,6 +6321,9 @@ gtk_widget_translate_coordinates (GtkWidget  *src_widget,
       src_y = dy;
 
       window = gdk_window_get_effective_parent (window);
+
+      if (!window)		/* Handle GtkHandleBox */
+	return FALSE;
     }
 
   /* And back */
@@ -6330,6 +6333,12 @@ gtk_widget_translate_coordinates (GtkWidget  *src_widget,
       dest_list = g_list_prepend (dest_list, window);
 
       window = gdk_window_get_effective_parent (window);
+
+      if (!window)		/* Handle GtkHandleBox */
+        {
+          g_list_free (dest_list);
+          return FALSE;
+        }
     }
 
   while (dest_list)
