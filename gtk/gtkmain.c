GNOME/gtk

commit 8a7d0ab4813457088335685e956313afbd1d65a1

Make gtk_init_with_args behave as documented
This docs say it should return FALSE if the display can't be opened,
but it was failing.

https://bugzilla.gnome.org/show_bug.cgi?id=771959

gtk/gtkmain.c
@@ -963,14 +963,14 @@ gtk_init_with_args (gint                 *argc,
   gboolean retval;
 
   if (gtk_initialized)
-    return GDK_PRIVATE_CALL (gdk_display_open_default) () != NULL;
+    goto done;
 
   gettext_initialization ();
 
   if (!check_setugid ())
     return FALSE;
 
-  gtk_group = gtk_get_option_group (TRUE);
+  gtk_group = gtk_get_option_group (FALSE);
 
   context = g_option_context_new (parameter_string);
   g_option_context_add_group (context, gtk_group);
@@ -982,7 +982,11 @@ gtk_init_with_args (gint                 *argc,
 
   g_option_context_free (context);
 
-  return retval;
+  if (!retval)
+    return FALSE;
+
+done:
+  return GDK_PRIVATE_CALL (gdk_display_open_default) () != NULL;
 }
 
 

commit 65b18a4fc1605e2a142895c2e995b50a4964adbd

main: Set the error if gtk_init_with_args fails
This fixes a fallout from 8a7d0ab481345708 where the error wasn't being
set when a display couldn't be opened right after parsing the
commandline.

It also fixes an older bug where the error would be left unset if the
commandline had already been parsed before (ie. when gtk_initialized
is TRUE).

https://bugzilla.gnome.org/show_bug.cgi?id=771959

gtk/gtkmain.c
@@ -992,15 +992,22 @@ gtk_init_with_args (gint                 *argc,
     return FALSE;
 
 done:
-  if (GDK_PRIVATE_CALL (gdk_display_open_default) () != NULL)
+  if (GDK_PRIVATE_CALL (gdk_display_open_default) () == NULL)
     {
-      if (gtk_get_debug_flags () & GTK_DEBUG_INTERACTIVE)
-        gtk_window_set_interactive_debugging (TRUE);
+      const char *display_name = gdk_get_display_arg_name ();
+      g_set_error (error,
+                   G_OPTION_ERROR,
+                   G_OPTION_ERROR_FAILED,
+                   _("Cannot open display: %s"),
+                   display_name ? display_name : "" );
 
-      return TRUE;
+      return FALSE;
     }
 
-  return FALSE;
+  if (gtk_get_debug_flags () & GTK_DEBUG_INTERACTIVE)
+    gtk_window_set_interactive_debugging (TRUE);
+
+  return TRUE;
 }
 
 