95ae7b7c1e7a0f49893785998fbfa38aa98835f3
Matthias Clasen <mclasen@redhat.com>
2017-08-26 22:13:25 -0400

    x11: Fix managed dnd
    We need to actually trigger the drop from the gdk side.

--- gdk/x11/gdkdnd-x11.c
+++ gdk/x11/gdkdnd-x11.c
@@ -2855,6 +2855,7 @@ static void
 gdk_x11_drag_context_drop_performed (GdkDragContext *context,
                                      guint32         time_)
 {
+  gdk_drag_drop (context, time_);
   drag_context_ungrab (context);
 }
 
