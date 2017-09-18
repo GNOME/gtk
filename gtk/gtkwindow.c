47879de0901bb5521b6fa697be29d0b1adc499f8
Daniel Boles <dboles@src.gnome.org>
2017-09-19 00:05:09 +0100

    Window: Clarify documentation of set_application()
    Clarify that ::destroy, not ::hide*, removes a window from its app, by
    replacing the mention of open windows with the blurb on destruction from
    :application, completing commit 7db4bee4b62bc9b6f20ba4e599e8d030314984fd
    
    Also link to the equivalent gtk_application_(add|remove)_window() calls,
    since Application.add_window() already links back to Window:application.
    
    * unless you use gtkmm…
    
    https://bugzilla.gnome.org/show_bug.cgi?id=639931

--- gtk/gtkwindow.c
+++ gtk/gtkwindow.c
@@ -3515,12 +3515,20 @@ gtk_window_release_application (GtkWindow *window)
 /**
  * gtk_window_set_application:
  * @window: a #GtkWindow
- * @application: (allow-none): a #GtkApplication, or %NULL
+ * @application: (allow-none): a #GtkApplication, or %NULL to unset
  *
  * Sets or unsets the #GtkApplication associated with the window.
  *
- * The application will be kept alive for at least as long as the window
- * is open.
+ * The application will be kept alive for at least as long as it has any windows
+ * associated with it (see g_application_hold() for a way to keep it alive
+ * without windows).
+ *
+ * Normally, the connection between the application and the window will remain
+ * until the window is destroyed, but you can explicitly remove it by setting
+ * the @application to %NULL.
+ *
+ * This is equivalent to calling gtk_application_remove_window() and/or
+ * gtk_application_add_window() on the old/new applications as relevant.
  *
  * Since: 3.0
  **/
