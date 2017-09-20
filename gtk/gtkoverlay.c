761194dad7e532cabc627c35a38b80af015797b8
Daniel Boles <dboles@src.gnome.org>
2017-09-20 17:34:43 +0100

    Overlay: Document style classes added per position

--- gtk/gtkoverlay.c
+++ gtk/gtkoverlay.c
@@ -53,6 +53,12 @@
  * The GtkOverlay implementation of the GtkBuildable interface
  * supports placing a child as an overlay by specifying “overlay” as
  * the “type” attribute of a `<child>` element.
+ *
+ * # CSS nodes
+ *
+ * GtkOverlay has a single CSS node with the name “overlay”. Overlay children
+ * whose alignments cause them to be positioned at an edge get the style classes
+ * “.left”, “.right”, “.top”, and/or “.bottom” according to their position.
  */
 
 struct _GtkOverlayPrivate
