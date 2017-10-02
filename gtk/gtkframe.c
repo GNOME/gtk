1d80453e47b48feb239ff73d56166a39fc3ad8a2
Daniel Boles <dboles@src.gnome.org>
2017-10-02 19:26:22 +0100

    Frame: Enhance CSS docs
    Note optional .flat class and that the old shadow types are replaced by
    CSS border-style et al.; quote names of nodes & classes for readability.

--- gtk/gtkframe.c
+++ gtk/gtkframe.c
@@ -74,17 +74,18 @@
  *
  * |[<!-- language="plain" -->
  * frame 
- * ├── border
+ * ├── border[.flat]
  * ├── <label widget>
  * ╰── <child>
  * ]|
  *
- * GtkFrame has a main CSS node with name frame and a subnode with
- * name border. The border node is used to render the visible border.
+ * GtkFrame has a main CSS node named “frame” and a subnode named “border”. The
+ * “border” node is used to draw the visible border. You can set the appearance
+ * of the border using CSS properties like “border-style” on the “border” node.
  *
- * The border node can be given the style class .flat, which is used by themes
+ * The border node can be given the style class “.flat”, which is used by themes
  * to disable drawing of the border. To do this from code, call
- * gtk_frame_set_shadow_type() with %GTK_SHADOW_NONE to add the .flat class or
+ * gtk_frame_set_shadow_type() with %GTK_SHADOW_NONE to add the “.flat” class or
  * any other shadow type to remove it.
  */
 
