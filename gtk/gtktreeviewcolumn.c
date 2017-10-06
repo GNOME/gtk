2c28c627c1527d4ee2dfc4d3805f7afc482049e2
Timm Bäder <mail@baedert.org>
2017-10-06 17:23:34 +0200

    treeviewcolumn: Don't leak button
    We g_object_ref_sink the button in _create_button, so we need to unref
    it ourselves.

--- gtk/gtktreeviewcolumn.c
+++ gtk/gtktreeviewcolumn.c
@@ -1336,6 +1336,7 @@ _gtk_tree_view_column_unset_tree_view (GtkTreeViewColumn *column)
   if (priv->tree_view)
     {
       gtk_container_remove (GTK_CONTAINER (priv->tree_view), priv->button);
+      g_clear_object (&priv->button);
     }
   if (priv->property_changed_signal)
     {
