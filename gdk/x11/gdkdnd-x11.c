3021edc38d0dfe88f8e01f49e057dc1c4b212e4e
Carlos Garnacho <carlosg@gnome.org>
2017-10-31 13:35:11 +0100

    gdk/x11: s/gdk_event_free/g_object_unref/

--- gdk/x11/gdkdnd-x11.c
+++ gdk/x11/gdkdnd-x11.c
@@ -1259,7 +1259,7 @@ send_client_message_async_cb (Window   window,
 
       gdk_display_put_event (gdk_drag_context_get_display (context), temp_event);
 
-      gdk_event_free (temp_event);
+      g_object_unref (temp_event);
     }
 
   g_object_unref (context);
@@ -1312,7 +1312,7 @@ xdnd_send_xevent (GdkX11DragContext *context_x11,
               if ((*xdnd_filters[i].func) (event_send, temp_event, NULL) == GDK_FILTER_TRANSLATE)
                 gdk_display_put_event (display, temp_event);
 
-              gdk_event_free (temp_event);
+              g_object_unref (temp_event);
 
               return TRUE;
             }
@@ -2362,7 +2362,7 @@ gdk_x11_drag_context_drag_motion (GdkDragContext *context,
       gdk_event_set_device (temp_event, gdk_drag_context_get_device (context));
 
       gdk_display_put_event (gdk_drag_context_get_display (context), temp_event);
-      gdk_event_free (temp_event);
+      g_object_unref (temp_event);
     }
   else
     {
@@ -2407,7 +2407,7 @@ gdk_x11_drag_context_drag_motion (GdkDragContext *context,
                 gdk_event_set_device (temp_event, gdk_drag_context_get_device (context));
 
                 gdk_display_put_event (gdk_drag_context_get_display (context), temp_event);
-                gdk_event_free (temp_event);
+                g_object_unref (temp_event);
               }
               break;
             case GDK_DRAG_PROTO_MOTIF:
