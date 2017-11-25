bf82853385026cea495b4feb2c7e429c9a252910
Matthias Clasen <mclasen@redhat.com>
2017-11-25 08:37:49 -0500

    gdk: Remove a leftover GdkScreen

--- gdk/gdktypes.h
+++ gdk/gdktypes.h
@@ -128,7 +128,6 @@ typedef struct _GdkDragContext        GdkDragContext;
 
 typedef struct _GdkDisplayManager     GdkDisplayManager;
 typedef struct _GdkDisplay            GdkDisplay;
-typedef struct _GdkScreen             GdkScreen;
 typedef struct _GdkWindow             GdkWindow;
 typedef struct _GdkKeymap             GdkKeymap;
 typedef struct _GdkAppLaunchContext   GdkAppLaunchContext;
