b03f08960b3d1b960591bb53c696f00ff6c48242
Timm Bäder <mail@baedert.org>
2017-12-09 09:25:47 +0100

    gl renderer: Fix cairo node surface size in hidpi

--- gsk/gl/gskglrenderer.c
+++ gsk/gl/gskglrenderer.c
@@ -607,8 +607,8 @@ render_cairo_node (GskGLRenderer       *self,
   get_gl_scaling_filters (node, &gl_min_filter, &gl_mag_filter);
 
   texture_id = gsk_gl_driver_create_texture (self->gl_driver,
-                                             node->bounds.size.width,
-                                             node->bounds.size.height);
+                                             node->bounds.size.width * self->scale_factor,
+                                             node->bounds.size.height * self->scale_factor);
   gsk_gl_driver_bind_source_texture (self->gl_driver, texture_id);
   gsk_gl_driver_init_texture_with_surface (self->gl_driver,
                                            texture_id,
