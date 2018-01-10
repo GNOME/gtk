0290b28564dc30c42bb9ad354b87e68a803340b0
Matthias Clasen <mclasen@redhat.com>
2018-01-09 23:16:30 -0500

    Drop unused debug keys
    The multihead, xim and xinerama keys do nothing anymore.

--- gdk/gdkinternals.h
+++ gdk/gdkinternals.h
@@ -60,19 +60,16 @@ typedef enum {
   GDK_DEBUG_MISC          = 1 <<  0,
   GDK_DEBUG_EVENTS        = 1 <<  1,
   GDK_DEBUG_DND           = 1 <<  2,
-  GDK_DEBUG_XIM           = 1 <<  3,
-  GDK_DEBUG_NOGRABS       = 1 <<  4,
-  GDK_DEBUG_INPUT         = 1 <<  5,
-  GDK_DEBUG_CURSOR        = 1 <<  6,
-  GDK_DEBUG_MULTIHEAD     = 1 <<  7,
-  GDK_DEBUG_XINERAMA      = 1 <<  8,
-  GDK_DEBUG_EVENTLOOP     = 1 <<  9,
-  GDK_DEBUG_FRAMES        = 1 << 10,
-  GDK_DEBUG_SETTINGS      = 1 << 11,
-  GDK_DEBUG_OPENGL        = 1 << 12,
-  GDK_DEBUG_VULKAN        = 1 << 13,
-  GDK_DEBUG_SELECTION     = 1 << 14,
-  GDK_DEBUG_CLIPBOARD     = 1 << 15
+  GDK_DEBUG_NOGRABS       = 1 <<  3,
+  GDK_DEBUG_INPUT         = 1 <<  4,
+  GDK_DEBUG_CURSOR        = 1 <<  5,
+  GDK_DEBUG_EVENTLOOP     = 1 <<  6,
+  GDK_DEBUG_FRAMES        = 1 <<  7,
+  GDK_DEBUG_SETTINGS      = 1 <<  8,
+  GDK_DEBUG_OPENGL        = 1 <<  9,
+  GDK_DEBUG_VULKAN        = 1 << 10,
+  GDK_DEBUG_SELECTION     = 1 << 11,
+  GDK_DEBUG_CLIPBOARD     = 1 << 12
 } GdkDebugFlag;
 
 typedef enum {
