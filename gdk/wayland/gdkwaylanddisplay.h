2aa1d6294edf6958b55a27dbe9f5b0ddf95cf367
Carlos Garnacho <carlosg@gnome.org>
2018-02-13 14:28:44 +0100

    gdk/wayland: Add GdkDisplay call to query available globals
    The internal known_globals hashtable is used to carry accounting for
    interfaces that depend on others (as ordering is not guaranteed), extend
    its usage so it also keeps track of unimplemented interfaces (here at
    least).
    
    The API call will then use this to allow querying the globals offered by
    the compositor, it will be useful to determine whether we can use
    text-input protocols or should fallback to other IMs.

--- gdk/wayland/gdkwaylanddisplay.h
+++ gdk/wayland/gdkwaylanddisplay.h
@@ -60,6 +60,10 @@ void                    gdk_wayland_display_set_startup_notification_id (GdkDisp
 GDK_AVAILABLE_IN_3_22
 gboolean                gdk_wayland_display_prefers_ssd         (GdkDisplay *display);
 
+GDK_AVAILABLE_IN_3_22
+gboolean                gdk_wayland_display_query_registry      (GdkDisplay  *display,
+								 const gchar *global);
+
 G_END_DECLS
 
 #endif /* __GDK_WAYLAND_DISPLAY_H__ */
