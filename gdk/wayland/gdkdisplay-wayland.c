3b568fbe7701eeeab89cc995f2a616bb43d17985
Carlos Garnacho <carlosg@gnome.org>
2018-02-13 14:28:44 +0100

    gdk/wayland: Add GdkDisplay call to query available globals
    The internal known_globals hashtable is used to carry accounting for
    interfaces that depend on others (as ordering is not guaranteed), extend
    its usage so it also keeps track of unimplemented interfaces (here at
    least).
    
    The API call will then use this to allow querying the globals offered by
    the compositor, it will be useful to determine whether we can use
    text-input protocols or should fallback to other IMs.

--- gdk/wayland/gdkdisplay-wayland.c
+++ gdk/wayland/gdkdisplay-wayland.c
@@ -384,7 +384,6 @@ gdk_registry_handle_global (void               *data,
 {
   GdkWaylandDisplay *display_wayland = data;
   struct wl_output *output;
-  gboolean handled = TRUE;
 
   GDK_NOTE (MISC,
             g_message ("add global %u, interface %s, version %u", id, interface, version));
@@ -507,12 +506,9 @@ gdk_registry_handle_global (void               *data,
                                                            &server_decoration_listener,
                                                            display_wayland);
     }
-  else
-    handled = FALSE;
 
-  if (handled)
-    g_hash_table_insert (display_wayland->known_globals,
-                         GUINT_TO_POINTER (id), g_strdup (interface));
+  g_hash_table_insert (display_wayland->known_globals,
+                       GUINT_TO_POINTER (id), g_strdup (interface));
 
   process_on_globals_closures (display_wayland);
 }
@@ -2076,4 +2072,38 @@ gdk_wayland_display_get_output_scale (GdkWaylandDisplay *display_wayland,
     return gdk_monitor_get_scale_factor (GDK_MONITOR (monitor));
 
   return 0;
+}
+
+/**
+ * gdk_wayland_display_query_registry:
+ * @display: a wayland #GdkDisplay
+ * @interface: global interface to query in the registry
+ *
+ * Returns %TRUE if the the interface was found in the display
+ * wl_registry.global handler.
+ *
+ * Returns: %TRUE if the global is offered by the compositor
+ *
+ * Since: 3.94
+ **/
+gboolean
+gdk_wayland_display_query_registry (GdkDisplay  *display,
+				    const gchar *global)
+{
+  GdkWaylandDisplay *display_wayland = GDK_WAYLAND_DISPLAY (display);
+  GHashTableIter iter;
+  gchar *value;
+
+  g_return_val_if_fail (GDK_IS_WAYLAND_DISPLAY (display), FALSE);
+  g_return_val_if_fail (global != NULL, FALSE);
+
+  g_hash_table_iter_init (&iter, display_wayland->known_globals);
+
+  while (g_hash_table_iter_next (&iter, NULL, (gpointer*) &value))
+    {
+      if (strcmp (value, global) == 0)
+        return TRUE;
+    }
+
+  return FALSE;
 }
