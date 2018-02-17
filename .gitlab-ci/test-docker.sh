1058c45821a5921591840b7de5a31e501d3da1f9
Christoph Reiter <creiter@src.gnome.org>
2018-02-17 11:55:40 +0100

    gitlab-ci: run parts of the test suite
    This runs the tests in testsuite/{a11y,css,gtk,tools}
    All others are failing atm and need more work.

--- .gitlab-ci/test-docker.sh
+++ .gitlab-ci/test-docker.sh
@@ -7,3 +7,10 @@ meson -Denable-x11-backend=true -Denable-wayland-backend=true \
     -Denable-broadway-backend=true -Denable-vulkan=yes _build_full
 cd _build_full
 ninja
+
+xvfb-run -a -s "-screen 0 1024x768x24" \
+    meson test \
+        --print-errorlogs \
+        --suite=gtk+ \
+        --no-suite=gtk+:gdk \
+        --no-suite=gtk+:gsk
