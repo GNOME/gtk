7f2935c2c85bbefcc8d5592c71b6313532050ced
Benjamin Otte <otte@redhat.com>
2018-02-26 01:32:01 +0100

    gtk: Add GtkMediaControls
    This is a basic first implementation of controls for a GtkMediaStream,
    modeled after Totem's UI.

--- gtk/gtk.h
+++ gtk/gtk.h
@@ -138,6 +138,7 @@
 #include <gtk/gtkliststore.h>
 #include <gtk/gtklockbutton.h>
 #include <gtk/gtkmain.h>
+#include <gtk/gtkmediacontrols.h>
 #include <gtk/gtkmediafile.h>
 #include <gtk/gtkmediastream.h>
 #include <gtk/gtkmenu.h>
