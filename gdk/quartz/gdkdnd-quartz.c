94d6d56913a1e0564c54d10a3a31fbb4303be2d9
Christoph Reiter <creiter@src.gnome.org>
2018-02-27 19:53:43 +0100

    macos: Fix gdk_quartz_drag_context_get_dragging_info_libgtk_only symbol export
    The header got included without config.h being included first which resulted in the
    wrong _GDK_EXTERN macro being used. As a result some symbols weren't exported
    and starting a DnD action would crash in the linker.
    
    This patch adds config.h includes in all places where clang complained about
    _GDK_EXTERN redefinitions.
    
    See #32 for more info.

--- gdk/quartz/gdkdnd-quartz.c
+++ gdk/quartz/gdkdnd-quartz.c
@@ -16,6 +16,7 @@
  * License along with this library. If not, see <http://www.gnu.org/licenses/>.
  */
 
+#include "config.h"
 #include "gdkdnd.h"
 #include "gdkquartzdnd.h"
 #include "gdkprivate-quartz.h"
