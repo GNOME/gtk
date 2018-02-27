b518445674d983ee83e5488c5ccfe5c2bbca76f1
Benjamin Otte <otte@redhat.com>
2018-02-27 03:09:33 +0100

    demos: Add an animated paintable demo
    This builds on the paintable demo.

---
+++ demos/gtk-demo/paintable.h
@@ -0,0 +1,14 @@
+#ifndef __PAINTABLE_H__
+#define __PAINTABLE_H__
+
+#include <gtk/gtk.h>
+
+void            gtk_nuclear_snapshot           (GtkSnapshot     *snapshot,
+                                                double           width,
+                                                double           height,
+                                                double           rotation);
+
+GdkPaintable *  gtk_nuclear_icon_new            (double          rotation);
+GdkPaintable *  gtk_nuclear_animation_new       (void);
+
+#endif /* __PAINTABLE_H__ */
