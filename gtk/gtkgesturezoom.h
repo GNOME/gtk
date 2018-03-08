48cfa915be69408a534190f813499660970ba1af
Benjamin Otte <otte@redhat.com>
2018-03-08 23:40:45 +0100

    zoomgesture: Port to new API model

--- gtk/gtkgesturezoom.h
+++ gtk/gtkgesturezoom.h
@@ -43,7 +43,7 @@ GDK_AVAILABLE_IN_ALL
 GType        gtk_gesture_zoom_get_type        (void) G_GNUC_CONST;
 
 GDK_AVAILABLE_IN_ALL
-GtkGesture * gtk_gesture_zoom_new             (GtkWidget      *widget);
+GtkGesture * gtk_gesture_zoom_new             (void);
 
 GDK_AVAILABLE_IN_ALL
 gdouble      gtk_gesture_zoom_get_scale_delta (GtkGestureZoom *gesture);
