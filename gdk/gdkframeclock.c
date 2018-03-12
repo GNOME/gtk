a20d9dec0e78ff93d0dfc040af7c24dc758d7d2b
Sander Sweers <infirit@gmail.com>
2018-03-12 14:29:09 +0100

    Add proper annotation for gdk_frame_clock_get_refresh_info
    See https://gitlab.gnome.org/GNOME/gtk/issues/77

--- gdk/gdkframeclock.c
+++ gdk/gdkframeclock.c
@@ -541,11 +541,11 @@ _gdk_frame_clock_debug_print_timings (GdkFrameClock   *clock,
  * gdk_frame_clock_get_refresh_info:
  * @frame_clock: a #GdkFrameClock
  * @base_time: base time for determining a presentaton time
- * @refresh_interval_return: a location to store the determined refresh
- *  interval, or %NULL. A default refresh interval of 1/60th of
- *  a second will be stored if no history is present.
- * @presentation_time_return: a location to store the next
- *  candidate presentation time after the given base time.
+ * @refresh_interval_return: (out) (optional): a location to store the
+ * determined refresh interval, or %NULL. A default refresh interval of
+ * 1/60th of a second will be stored if no history is present.
+ * @presentation_time_return: (out) (not nullable): a location to store
+ * the next candidate presentation time after the given base time.
  *  0 will be will be stored if no history is present.
  *
  * Using the frame history stored in the frame clock, finds the last
