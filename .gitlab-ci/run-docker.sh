5838ac3d1d50cdd2007f1249e960061032c5a218
Christoph Reiter <creiter@src.gnome.org>
2018-03-13 21:08:56 +0100

    run-docker.sh: Disable SELinux for the container and update the image tag
    On Fedora and similar SELinux by default prevents containers accessing
    mounted host directories. This script is just used for testing,
    so disable it.

--- .gitlab-ci/run-docker.sh
+++ .gitlab-ci/run-docker.sh
@@ -2,10 +2,10 @@
 
 set -e
 
-TAG="lazka/gitlab-gtk:v1"
+TAG="ebassi/gitlab-gtk:latest"
 
 sudo docker build --build-arg HOST_USER_ID="$UID" --tag "${TAG}" \
     --file "Dockerfile" .
-sudo docker run --rm \
+sudo docker run --rm --security-opt label=disable \
     --volume "$(pwd)/..:/home/user/app" --workdir "/home/user/app" \
     --tty --interactive "${TAG}" bash
