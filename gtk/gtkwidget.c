a8608618a909e50309a400c9c857aadd2e1afa6d
Benjamin Otte <otte@redhat.com>
2018-03-15 02:53:17 +0100

    dragsource: Replace gtk_drag_source_set_icon_surface()
    ... with gtk_drag_source_set_icon_paintable().

--- gtk/gtkwidget.c
+++ gtk/gtkwidget.c
@@ -1911,7 +1911,7 @@ gtk_widget_class_init (GtkWidgetClass *klass)
    *
    * The ::drag-begin signal is emitted on the drag source when a drag is
    * started. A typical reason to connect to this signal is to set up a
-   * custom drag icon with e.g. gtk_drag_source_set_icon_surface().
+   * custom drag icon with e.g. gtk_drag_source_set_icon_paintable().
    *
    * Note that some widgets set up a drag icon in the default handler of
    * this signal, so you may have to use g_signal_connect_after() to
