ed3e7e79775c62b384c414d77e33772d5656aec9
Samuel Thibault <samuel.thibault@ens-lyon.org>
2018-03-23 18:33:58 +0100

    a11y: restore window:activate/deactivate emission
    1b9aa1b708c2 ('a11y: drop the focus tracker') removed a bit too much.  We
    still have to emit window:activate/deactivate events.  They are easy to
    emit anyway.
    
    Fixes #127

--- gtk/a11y/gtkaccessibility.c
+++ gtk/a11y/gtkaccessibility.c
@@ -20,11 +20,91 @@
 #include "gtkaccessibility.h"
 #include "gtkaccessibilityutil.h"
 
+#include "gtkwindowaccessible.h"
+
+#include <gdk/gdk.h>
+#include <gtk/gtkwindow.h>
+#include <gtk/gtkaccessible.h>
+
 #ifdef GDK_WINDOWING_X11
 #include <atk-bridge.h>
 #endif
 
 static int initialized = FALSE;
+
+static gboolean
+window_focus (GtkWidget     *widget,
+              GdkEventFocus *event)
+{
+  AtkObject *atk_obj;
+  gboolean focus_in;
+
+  g_return_val_if_fail (GTK_IS_WIDGET (widget), FALSE);
+
+  gdk_event_get_focus_in ((GdkEvent *)event, &focus_in);
+  atk_obj = gtk_widget_get_accessible (widget);
+  g_signal_emit_by_name (atk_obj, focus_in ? "activate" : "deactivate");
+
+  return FALSE;
+}
+
+static void
+window_added (AtkObject *atk_obj,
+              guint      index,
+              AtkObject *child)
+{
+  GtkWidget *widget;
+
+  if (!GTK_IS_WINDOW_ACCESSIBLE (child))
+    return;
+
+  widget = gtk_accessible_get_widget (GTK_ACCESSIBLE (child));
+  if (!widget)
+    return;
+
+  g_signal_connect (widget, "focus-in-event", (GCallback) window_focus, NULL);
+  g_signal_connect (widget, "focus-out-event", (GCallback) window_focus, NULL);
+  g_signal_emit_by_name (child, "create");
+}
+
+static void
+window_removed (AtkObject *atk_obj,
+                guint      index,
+                AtkObject *child)
+{
+  GtkWidget *widget;
+  GtkWindow *window;
+
+  if (!GTK_IS_WINDOW_ACCESSIBLE (child))
+    return;
+
+  widget = gtk_accessible_get_widget (GTK_ACCESSIBLE (child));
+  if (!widget)
+    return;
+
+  window = GTK_WINDOW (widget);
+  /*
+   * Deactivate window if it is still focused and we are removing it. This
+   * can happen when a dialog displayed by gok is removed.
+   */
+  if (gtk_window_is_active (window))
+    g_signal_emit_by_name (child, "deactivate");
+
+  g_signal_handlers_disconnect_by_func (widget, (gpointer) window_focus, NULL);
+  g_signal_emit_by_name (child, "destroy");
+}
+
+static void
+do_window_event_initialization (void)
+{
+  AtkObject *root;
+
+  g_type_class_ref (GTK_TYPE_WINDOW_ACCESSIBLE);
+
+  root = atk_get_root ();
+  g_signal_connect (root, "children-changed::add", (GCallback) window_added, NULL);
+  g_signal_connect (root, "children-changed::remove", (GCallback) window_removed, NULL);
+}
 
 void
 _gtk_accessibility_init (void)
@@ -35,6 +115,7 @@ _gtk_accessibility_init (void)
   initialized = TRUE;
 
   _gtk_accessibility_override_atk_util ();
+  do_window_event_initialization ();
 
 #ifdef GDK_WINDOWING_X11
   atk_bridge_adaptor_init (NULL, NULL);
