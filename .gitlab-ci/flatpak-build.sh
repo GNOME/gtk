1b62d28cbb561c12f43f08819e1714c8bf068ef7
Emmanuele Bassi <ebassi@gnome.org>
2018-03-27 12:45:20 +0100

    ci: Move flatpak building to a script
    The YAML is getting hard to modify, so let's use a simple script like we
    do for the other CI jobs.

---
+++ .gitlab-ci/flatpak-build.sh
@@ -0,0 +1,44 @@
+#!/bin/bash
+
+set -e
+
+appid=$1
+
+builddir=app
+repodir=repo
+
+flatpak-builder \
+        --stop-at=gtk \
+        ${builddir} \
+        build-aux/flatpak/${appid}.json
+
+flatpak-builder \
+        --run ${builddir} build-aux/flatpak/${appid}.json \
+        meson \
+                --prefix /app \
+                --libdir /app/lib \
+                --buildtype debug \
+                -Dx11-backend=true \
+                -Dwayland-backend=true \
+                -Dprint-backends=file \
+                -Dbuild-tests=false \
+                -Dbuild-examples=false \
+                -Dintrospection=false \
+                -Ddemos=true \
+                _build .
+
+flatpak-builder \
+        --run ${builddir} build-aux/flatpak/${appid}.json \
+        ninja -C _build install
+
+flatpak-builder \
+        --finish-only \
+        --repo=${repodir} \
+        ${builddir} \
+        build-aux/flatpak/${appid}.json
+
+flatpak build-bundle \
+        ${repodir} \
+        ${appid}-dev.flatpak \
+        --runtime-repo=https://flathub.org/repo/flathub.flatpakrepo \
+        ${appid}
