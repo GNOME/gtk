ccde20ea08695780828466a21cdf8e2703cf4200
Benjamin Otte <otte@redhat.com>
2018-03-29 00:04:47 +0200

    frameclockidle: Remove nonexisting functions from header

--- gdk/gdkframeclockidle.h
+++ gdk/gdkframeclockidle.h
@@ -59,8 +59,6 @@ struct _GdkFrameClockIdleClass
 GType           gdk_frame_clock_idle_get_type       (void) G_GNUC_CONST;
 
 GdkFrameClock *_gdk_frame_clock_idle_new            (void);
-void           _gdk_frame_clock_idle_freeze_updates (GdkFrameClockIdle *clock_idle);
-void           _gdk_frame_clock_idle_thaw_updates   (GdkFrameClockIdle *clock_idle);
 
 G_END_DECLS
 
