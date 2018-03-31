bc20ebe73d8b0134710cf298303da6fb0c28ba8d
Tomasz Gąsior <kontakt@tomaszgasior.pl>
2018-03-31 20:46:27 +0200

    Some code cleanup.

--- gtk/gtkplacessidebar.c
+++ gtk/gtkplacessidebar.c
@@ -3508,10 +3508,7 @@ add_actions (GtkPlacesSidebar *sidebar)
 }
 
 static GMenuItem *
-add_menu_item (GMenu       *menu,
-               const gchar *label,
-               const gchar *action,
-               gboolean     hide_when_disabled)
+add_menu_item (GMenu *menu, const gchar *label, const gchar *action, gboolean hide_when_disabled)
 {
   GMenuItem *menu_item;
 
@@ -3524,9 +3521,7 @@ add_menu_item (GMenu       *menu,
 }
 
 static void
-add_open_menu_item (GMenu              *menu,
-                    const gchar        *label,
-                    GtkPlacesOpenFlags  flags)
+add_open_menu_item (GMenu *menu, const gchar *label, GtkPlacesOpenFlags flags)
 {
   GMenuItem *menu_item;
 
