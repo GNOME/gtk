1880540a07e647ccaa316f204c429f5386c4f4f0
Benjamin Otte <otte@redhat.com>
2018-03-31 20:02:55 +0200

    broadway: Improve error message
    This error message is printed into the journal if a GTK app can't
    connect to eithre Wayland or X11. Make it at least mention who is not
    capable of connecting to a server.
    
    Unrelated, we might want to improve our error reporting when a GTK app
    can't start, so that debugging issues with system startup / login get
    easier to resolve.

--- gdk/broadway/gdkdisplay-broadway.c
+++ gdk/broadway/gdkdisplay-broadway.c
@@ -263,7 +263,7 @@ _gdk_broadway_display_open (const gchar *display_name)
   broadway_display->server = _gdk_broadway_server_new (display_name, &error);
   if (broadway_display->server == NULL)
     {
-      g_printerr ("Unable to init server: %s\n", error->message);
+      g_printerr ("Unable to init Broadway server: %s\n", error->message);
       g_error_free (error);
       return NULL;
     }
