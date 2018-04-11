f9ffb316e15eb9cbec5fc6a45d0cdce6971b33f9
Christoph Reiter <creiter@src.gnome.org>
2018-04-11 12:18:55 +0200

    gitlab-ci: Reenable ccache but disable it during the initial meson call
    There where some problems (??) with ccache not detecting changes during meson
    checks. Setting CCACHE_DISABLE during the meson execution makes ccache not use
    the cache and pass things directly to the compiler.

--- .gitlab-ci/test-docker.sh
+++ .gitlab-ci/test-docker.sh
@@ -4,12 +4,18 @@ set -e
 
 srcdir=$(pwd)
 
+mkdir -p _ccache
+export CCACHE_BASEDIR="$(pwd)"
+export CCACHE_DIR="${CCACHE_BASEDIR}/_ccache"
+
+export CCACHE_DISABLE=true
 meson \
         -Dx11-backend=true \
         -Dwayland-backend=true \
         -Dbroadway-backend=true \
         -Dvulkan=yes \
         _build $srcdir
+unset CCACHE_DISABLE
 
 cd _build
 
