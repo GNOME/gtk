cfbbcf5fc4968af4e1b75ca3b20c48a3c543e71f
Daniel Boles <dboles.src@gmail.com>
2018-04-18 23:31:47 +0100

    UIManager: Document deprecation; link replacements
    Close https://gitlab.gnome.org/GNOME/gtk/issues/193

--- gtk/deprecated/gtkuimanager.c
+++ gtk/deprecated/gtkuimanager.c
@@ -54,7 +54,13 @@
  * SECTION:gtkuimanager
  * @Short_description: Constructing menus and toolbars from an XML description
  * @Title: GtkUIManager
- * @See_also:#GtkBuilder
+ * @See_also: #GtkBuilder
+ *
+ * > GtkUIManager is deprecated since GTK+ 3.10. To construct user interfaces
+ * > from XML definitions, you should use #GtkBuilder, #GMenuModel, et al. To
+ * > work with actions, use #GAction, #GtkActionable et al. These newer classes
+ * > support richer functionality and integration with various desktop shells.
+ * > It should be possible to migrate most/all functionality from GtkUIManager.
  *
  * A #GtkUIManager constructs a user interface (menus and toolbars) from
  * one or more UI definitions, which reference actions from one or more
