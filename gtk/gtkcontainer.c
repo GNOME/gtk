GNOME/gtk

commit 07cbbc6fa9db2a630270d2960d436c0e687a4955

container: Propagate child properties if parent != container
This can happen when e.g. GtkNotebook adds the child page widgets not
directly inside the notebook, but inside the inner GtkStack.

gtk/gtkcontainer.c
@@ -580,8 +580,7 @@ gtk_container_buildable_set_child_property (GtkContainer *container,
   GError *error = NULL;
   GObjectNotifyQueue *nqueue;
 
-  if (_gtk_widget_get_parent (child) != (GtkWidget *)container &&
-      !SPECIAL_CONTAINER (container))
+  if (SPECIAL_CONTAINER (container))
     {
       /* This can happen with internal children of complex widgets.
        * Silently ignore the child properties in this case. We explicitly

commit 83c8263d9fc304b4d8cbc768cbfe115ca8a44bd2

container: Propagate child properties to GtkAssistant
It implements set_child_property() and doesn't behave special here.
That's also what the comment says.

Fixes: 07cbbc6fa9db2a630270d2960d436c0e687a4955

gtk/gtkcontainer.c
@@ -454,7 +454,7 @@ gtk_container_buildable_set_child_property (GtkContainer *container,
   GError *error = NULL;
   GObjectNotifyQueue *nqueue;
 
-  if (SPECIAL_CONTAINER (container))
+  if (SPECIAL_CONTAINER (container) && !GTK_IS_ASSISTANT (container))
     {
       /* This can happen with internal children of complex widgets.
        * Silently ignore the child properties in this case. We explicitly