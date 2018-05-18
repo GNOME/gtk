e1ec2322d5a24988c5b076f60136775edfa4b639
Mario Sanchez Prada <msanchez@gnome.org>
2018-05-18 13:56:10 +0100

    application: Complete the startup sequence before quitting remote instances
    When running a remote GtkApplication via the command line, such application
    will hand things over to the primary instance and quit, which can leave the
    launcher (e.g. Shell) waiting forever for the event that would complete the
    startup sequence if StartupNotification=true is present in the .desktop file.
    
    This can cause undesired effects in the launcher, which will likely end up
    terminating the sequence itself at some point after a timeout if such event
    is never issued. To fix it, simply check whether we're in that situation
    once the command line has been handled for remote instances and notify the
    completion at that moment, which will only issue the event that the launcher
    is expecting if an startup ID was set in the first place.
    
    https://gitlab.gnome.org/GNOME/gtk/issues/1084

--- gtk/gtkapplication.c
+++ gtk/gtkapplication.c
@@ -322,7 +322,19 @@ gtk_application_local_command_line (GApplication   *application,
                                     gchar        ***arguments,
                                     gint           *exit_status)
 {
-  return G_APPLICATION_CLASS (gtk_application_parent_class)->local_command_line (application, arguments, exit_status);
+  gboolean result;
+
+  result = G_APPLICATION_CLASS (gtk_application_parent_class)->local_command_line (application, arguments, exit_status);
+
+  /* At this point we should know whether the application is registered
+   * as a remote one, in which case we need to make sure that the startup
+   * notification sequence gets completed, if needed, before handing things
+   * over to the primary instance and quitting.
+   */
+  if (g_application_get_is_registered (application) && g_application_get_is_remote (application))
+      gdk_notify_startup_complete ();
+
+  return result;
 }
 
 static void
