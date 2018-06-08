d00460b13592b429a10546fee959f86892d64fad
Chun-wei Fan <fanchunwei@src.gnome.org>
2018-06-08 18:04:30 +0800

    build: Make post-install script a Python script
    The existing post-install shell script will most likely not work on
    Visual Studio builds as there is normally no shell interpreter installed
    on the system where the build is done, but the build is normally done in
    a standard Windows cmd.exe console.
    
    Instead, use a Python script so that it will work on the platforms that
    Python supports.

---
+++ build-aux/meson/post-install.py
@@ -0,0 +1,33 @@
+#!/usr/bin/env python
+
+import os
+import sys
+import subprocess
+
+if 'DESTDIR' not in os.environ:
+    gtk_api_version = sys.argv[1]
+    gtk_abi_version = sys.argv[2]
+    gtk_libdir = sys.argv[3].replace('/', os.sep)
+    gtk_datadir = sys.argv[4].replace('/', os.sep)
+
+    gtk_moduledir = os.path.join(gtk_libdir, 'gtk-' + gtk_api_version, gtk_abi_version)
+    gtk_printmodule_dir = os.path.join(gtk_moduledir, 'printbackends')
+    gtk_immodule_dir = os.path.join(gtk_moduledir, 'immodules')
+
+    print('Compiling GSettings schemas...')
+    subprocess.call(['glib-compile-schemas',
+                    os.path.join(gtk_datadir, 'glib-2.0', 'schemas')])
+
+    print('Updating icon cache...')
+    subprocess.call(['gtk-update-icon-cache', '-q', '-t' ,'-f',
+                    os.path.join(gtk_datadir, 'icons', 'hicolor')])
+
+    print('Updating module cache for print backends...')
+    if not os.path.isdir(gtk_printmodule_dir):
+        os.mkdir(gtk_printmodule_dir)
+    subprocess.call(['gio-querymodules', gtk_printmodule_dir])
+
+    print('Updating module cache for input methods...')
+    if not os.path.isdir(gtk_immodule_dir):
+        os.mkdir(gtk_immodule_dir)
+    subprocess.call(['gio-querymodules', gtk_immodule_dir])
