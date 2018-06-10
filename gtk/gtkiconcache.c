0365dadad9a7713e242a757ebd69f5b725624454
Руслан Ижбулатов <lrn1986@gmail.com>
2018-06-10 21:04:50 +0000

    See if _MSC_VER is defined before checking its value

--- gtk/gtkiconcache.c
+++ gtk/gtkiconcache.c
@@ -108,7 +108,7 @@ gtk_icon_cache_new_for_path (const gchar *path)
 /* Bug 660730: _fstat32 is only defined in msvcrt80.dll+/VS 2005+ */
 /*             or possibly in the msvcrt.dll linked to by the Windows DDK */
 /*             (will need to check on the Windows DDK part later) */
-#if ((_MSC_VER >= 1400 || __MSVCRT_VERSION__ >= 0x0800) || defined (__MINGW64_VERSION_MAJOR)) && !defined(_WIN64)
+#if ((defined (_MSC_VER) && (_MSC_VER >= 1400 || __MSVCRT_VERSION__ >= 0x0800)) || defined (__MINGW64_VERSION_MAJOR)) && !defined(_WIN64)
 #undef fstat /* Just in case */
 #define fstat _fstat32  
 #endif
