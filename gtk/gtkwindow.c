5d00f6ae1bdbe9eafbf21f8877cd7aab6a45f7a3
Bastien Nocera <hadess@hadess.net>
2018-07-21 14:04:18 +0200

    Window: Make gtk_window_present() work in Wayland
    Translate GDK_CURRENT_TIME to the best approximation of the current time
    when using Wayland, as we would do under X11, so that windows are
    correctly raised in response to user actions in other processes.
    (eg. raising a window in response to a notification being clicked)
    
    See https://gitlab.gnome.org/GNOME/gtk/issues/624

--- gtk/gtkwindow.c
+++ gtk/gtkwindow.c
@@ -10491,6 +10491,18 @@ gtk_window_present_with_time (GtkWindow *window,
 	    }
 	  else
 #endif
+#ifdef GDK_WINDOWING_WAYLAND
+	  if (GDK_IS_WAYLAND_WINDOW(gdk_window))
+	    {
+	      timestamp = gtk_get_current_event_time ();
+	      if (timestamp == GDK_CURRENT_TIME)
+		{
+		  /* Convert µsec to msec */
+		  timestamp = g_get_monotonic_time () / 1000;
+		}
+	    }
+	  else
+#endif
 	    timestamp = gtk_get_current_event_time ();
         }
 
