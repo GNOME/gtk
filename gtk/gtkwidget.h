4d71d2303dafe1c02eb79040e32fe2172d1f295f
Matthias Clasen <mclasen@redhat.com>
2018-07-31 13:31:57 -0400

    Deprecate show_all apis
    These functions are gone in GTK+ 4, so deprecate them here.

--- gtk/gtkwidget.h
+++ gtk/gtkwidget.h
@@ -628,12 +628,12 @@ GDK_AVAILABLE_IN_ALL
 void       gtk_widget_hide                (GtkWidget           *widget);
 GDK_AVAILABLE_IN_ALL
 void       gtk_widget_show_now            (GtkWidget           *widget);
-GDK_AVAILABLE_IN_ALL
+GDK_DEPRECATED_IN_3_24
 void       gtk_widget_show_all            (GtkWidget           *widget);
-GDK_AVAILABLE_IN_ALL
+GDK_DEPRECATED_IN_3_24
 void       gtk_widget_set_no_show_all     (GtkWidget           *widget,
                                            gboolean             no_show_all);
-GDK_AVAILABLE_IN_ALL
+GDK_DEPRECATED_IN_3_24
 gboolean   gtk_widget_get_no_show_all     (GtkWidget           *widget);
 GDK_AVAILABLE_IN_ALL
 void	   gtk_widget_map		  (GtkWidget	       *widget);
