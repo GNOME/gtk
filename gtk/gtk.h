ff13ee4b668ed019e310e45af7fbf49c00aa93c3
Benjamin Otte <otte@redhat.com>
2018-08-04 12:14:13 +0200

    gtk: Add GtkShortcut
    This is the base class for what is about to take over all sorts of
    keyboard shortcuts. The initial version barely deals with keybindings.

--- gtk/gtk.h
+++ gtk/gtk.h
@@ -198,6 +198,7 @@
 #include <gtk/gtkselectionmodel.h>
 #include <gtk/gtkseparator.h>
 #include <gtk/gtksettings.h>
+#include <gtk/gtkshortcut.h>
 #include <gtk/gtkshortcutcontroller.h>
 #include <gtk/gtkshortcutlabel.h>
 #include <gtk/gtkshortcutsgroup.h>
