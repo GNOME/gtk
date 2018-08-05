abb6d23943dabf1af7854de8aca067cda85f93ee
Benjamin Otte <otte@redhat.com>
2018-08-05 04:20:58 +0200

    assistant: Port bindings to use shortcuts

--- gtk/gtkassistant.c
+++ gtk/gtkassistant.c
@@ -65,7 +65,6 @@
 
 #include "gtkassistant.h"
 
-#include "gtkbindings.h"
 #include "gtkbox.h"
 #include "gtkbuildable.h"
 #include "gtkbutton.h"
@@ -510,7 +509,6 @@ gtk_assistant_class_init (GtkAssistantClass *class)
   GtkWidgetClass *widget_class;
   GtkContainerClass *container_class;
   GtkWindowClass *window_class;
-  GtkBindingSet *binding_set;
 
   gobject_class   = (GObjectClass *) class;
   widget_class    = (GtkWidgetClass *) class;
@@ -619,8 +617,10 @@ gtk_assistant_class_init (GtkAssistantClass *class)
                                 NULL,
                                 G_TYPE_NONE, 0);
 
-  binding_set = gtk_binding_set_by_class (class);
-  gtk_binding_entry_add_signal (binding_set, GDK_KEY_Escape, 0, "escape", 0);
+  gtk_widget_class_add_binding_signal (widget_class,
+                                       GDK_KEY_Escape, 0,
+                                       "escape",
+                                       NULL);
 
   /**
    * GtkAssistant:use-header-bar:
