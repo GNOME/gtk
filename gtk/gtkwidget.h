cd493b37fcc0de1f7d05a9c3dcd2006f3a25d3f7
Benjamin Otte <otte@redhat.com>
2018-08-05 03:39:04 +0200

    shortcut: Add GtkShortcutTrigger
    Triggers are meant to describe how to trigger a shortcut. So far only a
    keyval + modifiers trigger exists.

--- gtk/gtkwidget.h
+++ gtk/gtkwidget.h
@@ -422,11 +422,12 @@ GType                   gtk_widget_class_get_layout_manager_type        (GtkWidg
 GDK_AVAILABLE_IN_ALL
 void       gtk_widget_class_add_binding_signal
                                           (GtkWidgetClass      *widget_class,
-                                           GdkModifierType      mods,
                                            guint                keyval,
+                                           GdkModifierType      mods,
                                            const gchar         *signal,
                                            const gchar         *format_string,
                                            ...);
+
 GDK_AVAILABLE_IN_ALL
 void       gtk_widget_class_add_shortcut  (GtkWidgetClass      *widget_class,
                                            GtkShortcut         *shortcut);
