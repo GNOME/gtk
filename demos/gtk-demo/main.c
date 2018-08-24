41e21e169b428f98073b3d27521e15d05fe3de22
Benjamin Otte <otte@redhat.com>
2018-08-24 08:46:54 +0200

    accels: Remove GtkAccelGroup

--- demos/gtk-demo/main.c
+++ demos/gtk-demo/main.c
@@ -310,7 +310,6 @@ static const char *types[] =
   "GtkIconView ",
   "GtkCellRendererText ",
   "GtkContainer ",
-  "GtkAccelGroup ",
   "GtkPaned ",
   "GtkPrintOperation ",
   "GtkPrintContext ",
