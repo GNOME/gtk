1937d0d10c94dcaeac5a2b9c8a103531bf393ef1
Benjamin Otte <otte@redhat.com>
2018-08-24 08:46:54 +0200

    accels: Remove GtkAccelGroup

--- gtk/gtkaccellabel.h
+++ gtk/gtkaccellabel.h
@@ -46,15 +46,7 @@ GType	   gtk_accel_label_get_type	     (void) G_GNUC_CONST;
 GDK_AVAILABLE_IN_ALL
 GtkWidget* gtk_accel_label_new		     (const gchar   *string);
 GDK_AVAILABLE_IN_ALL
-GtkWidget* gtk_accel_label_get_accel_widget  (GtkAccelLabel *accel_label);
-GDK_AVAILABLE_IN_ALL
 guint	   gtk_accel_label_get_accel_width   (GtkAccelLabel *accel_label);
-GDK_AVAILABLE_IN_ALL
-void	   gtk_accel_label_set_accel_widget  (GtkAccelLabel *accel_label,
-					      GtkWidget	    *accel_widget);
-GDK_AVAILABLE_IN_ALL
-void	   gtk_accel_label_set_accel_closure (GtkAccelLabel *accel_label,
-					      GClosure	    *accel_closure);
 GDK_AVAILABLE_IN_ALL
 gboolean   gtk_accel_label_refetch           (GtkAccelLabel *accel_label);
 GDK_AVAILABLE_IN_ALL
