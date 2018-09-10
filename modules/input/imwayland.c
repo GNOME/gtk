GNOME/gtk

commit 49b17e6c1e853e81e2087a989524d8e0cad08d05

imwayland: clear preedit on focus out

modules/input/imwayland.c
@@ -135,18 +135,7 @@ static void
 text_input_leave (void                     *data,
                   struct zwp_text_input_v3 *text_input,
                   struct wl_surface        *surface)
-{
-  /*
-  GtkIMContextWayland *context;
-
-  if (!global->current)
-    return;
-
-  context = GTK_IM_CONTEXT_WAYLAND (global->current);
-  TODO: does this clear text input or modify text?
-  reset_preedit (context);
-  */
-}
+{}
 
 static void
 text_input_preedit (void                     *data,
@@ -168,7 +157,6 @@ text_input_preedit (void                     *data,
   context->pending_preedit.cursor_begin = cursor_begin;
   context->pending_preedit.cursor_end = cursor_end;
 }
-
 
 static void
 text_input_preedit_apply (GtkIMContextWaylandGlobal *global)
@@ -664,6 +652,11 @@ gtk_im_context_wayland_focus_out (GtkIMContext *context)
 
   zwp_text_input_v3_disable (global->text_input);
   commit_state (context_wayland);
+
+  /* after disable, incoming state changes won't take effect anyway */
+  text_input_preedit (global, global->text_input, "", 0, 0);
+  text_input_preedit_apply (global);
+
   global->current = NULL;
 }
 

commit 542e16608dc1bbb8b02e6c0a50711862a8320604

imwayland: Fix clearing of preedit text
Fixes https://gitlab.gnome.org/GNOME/gtk/issues/1316 introduced in 49b17e6c. The original commit cleared preedit text by setting it to an empty string, which still counted as existing preedit. The fix sets preedit string to null, which is correctly understood as not present.

modules/input/imwayland.c
@@ -654,7 +654,7 @@ gtk_im_context_wayland_focus_out (GtkIMContext *context)
   commit_state (context_wayland);
 
   /* after disable, incoming state changes won't take effect anyway */
-  text_input_preedit (global, global->text_input, "", 0, 0);
+  text_input_preedit (global, global->text_input, NULL, 0, 0);
   text_input_preedit_apply (global);
 
   global->current = NULL;