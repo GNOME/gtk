GNOME/gtk

commit 49b17e6c1e853e81e2087a989524d8e0cad08d05

imwayland: clear preedit on focus out

modules/input/imwayland.c
@@ -135,18 +135,7 @@ static void
 text_input_leave (void                     *data,
                   struct zwp_text_input_v3 *text_input,
                   struct wl_surface        *surface)
-{
-  /*
-  GtkIMContextWayland *context;
-
-  if (!global->current)
-    return;
-
-  context = GTK_IM_CONTEXT_WAYLAND (global->current);
-  TODO: does this clear text input or modify text?
-  reset_preedit (context);
-  */
-}
+{}
 
 static void
 text_input_preedit (void                     *data,
@@ -168,7 +157,6 @@ text_input_preedit (void                     *data,
   context->pending_preedit.cursor_begin = cursor_begin;
   context->pending_preedit.cursor_end = cursor_end;
 }
-
 
 static void
 text_input_preedit_apply (GtkIMContextWaylandGlobal *global)
@@ -664,6 +652,11 @@ gtk_im_context_wayland_focus_out (GtkIMContext *context)
 
   zwp_text_input_v3_disable (global->text_input);
   commit_state (context_wayland);
+
+  /* after disable, incoming state changes won't take effect anyway */
+  text_input_preedit (global, global->text_input, "", 0, 0);
+  text_input_preedit_apply (global);
+
   global->current = NULL;
 }
 

commit 99669503fbc228fcca0edcf9a739354808c46ddb

imwayland: Fix clearing of preedit text in webkitgtk
Fixes webkitgtk misbehaviour as outlined in https://gitlab.gnome.org/GNOME/gtk/issues/1316#note_312942 , which was introduced in 49b17e6c.
The preedit will be cleared on exit only if it is already present.

modules/input/imwayland.c
@@ -654,8 +654,11 @@ gtk_im_context_wayland_focus_out (GtkIMContext *context)
   commit_state (context_wayland);
 
   /* after disable, incoming state changes won't take effect anyway */
-  text_input_preedit (global, global->text_input, NULL, 0, 0);
-  text_input_preedit_apply (global);
+  if (context_wayland->current_preedit.text)
+    {
+      text_input_preedit (global, global->text_input, NULL, 0, 0);
+      text_input_preedit_apply (global);
+    }
 
   global->current = NULL;
 }