ee08eda38232c4181eb9188a82231d6beb69c8f4
Benjamin Otte <otte@redhat.com>
2018-09-17 07:29:50 +0200

    listview: Introduce GtkListItemFactory
    Thisis the abstraction I intend to use for creating widgets and binding
    them to the item out of the listview.
    
    For now this is a very dumb wrapper around the functions that exist in
    the API.
    
    But it leaves the freedom to turn this into public API, make an
    interface out of it and most of all write different implementations, in
    particular one that uses GtkBuilder.

---
+++ gtk/gtklistitemfactory.c
@@ -0,0 +1,105 @@
+/*
+ * Copyright © 2018 Benjamin Otte
+ *
+ * This library is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Lesser General Public
+ * License as published by the Free Software Foundation; either
+ * version 2.1 of the License, or (at your option) any later version.
+ *
+ * This library is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public
+ * License along with this library. If not, see <http://www.gnu.org/licenses/>.
+ *
+ * Authors: Benjamin Otte <otte@gnome.org>
+ */
+
+#include "config.h"
+
+#include "gtklistitemfactoryprivate.h"
+
+struct _GtkListItemFactory
+{
+  GObject parent_instance;
+
+  GtkListCreateWidgetFunc create_func;
+  GtkListBindWidgetFunc bind_func;
+  gpointer user_data;
+  GDestroyNotify user_destroy;
+};
+
+struct _GtkListItemFactoryClass
+{
+  GObjectClass parent_class;
+};
+
+G_DEFINE_TYPE (GtkListItemFactory, gtk_list_item_factory, G_TYPE_OBJECT)
+
+static void
+gtk_list_item_factory_finalize (GObject *object)
+{
+  GtkListItemFactory *self = GTK_LIST_ITEM_FACTORY (object);
+
+  if (self->user_destroy)
+    self->user_destroy (self->user_data);
+
+  G_OBJECT_CLASS (gtk_list_item_factory_parent_class)->finalize (object);
+}
+
+static void
+gtk_list_item_factory_class_init (GtkListItemFactoryClass *klass)
+{
+  GObjectClass *object_class = G_OBJECT_CLASS (klass);
+
+  object_class->finalize = gtk_list_item_factory_finalize;
+}
+
+static void
+gtk_list_item_factory_init (GtkListItemFactory *self)
+{
+}
+
+GtkListItemFactory *
+gtk_list_item_factory_new (GtkListCreateWidgetFunc create_func,
+                           GtkListBindWidgetFunc   bind_func,
+                           gpointer                user_data,
+                           GDestroyNotify          user_destroy)
+{
+  GtkListItemFactory *self;
+
+  g_return_val_if_fail (create_func, NULL);
+  g_return_val_if_fail (bind_func, NULL);
+  g_return_val_if_fail (user_data != NULL || user_destroy == NULL, NULL);
+
+  self = g_object_new (GTK_TYPE_LIST_ITEM_FACTORY, NULL);
+
+  self->create_func = create_func;
+  self->bind_func = bind_func;
+  self->user_data = user_data;
+  self->user_destroy = user_destroy;
+
+  return self;
+}
+
+GtkWidget *
+gtk_list_item_factory_create (GtkListItemFactory *self)
+{
+  g_return_val_if_fail (GTK_IS_LIST_ITEM_FACTORY (self), NULL);
+
+  return self->create_func (self->user_data);
+}
+
+void
+gtk_list_item_factory_bind (GtkListItemFactory *self,
+                            GtkWidget          *widget,
+                            gpointer            item)
+{
+  g_return_if_fail (GTK_IS_LIST_ITEM_FACTORY (self));
+  g_return_if_fail (GTK_IS_WIDGET (widget));
+
+  self->bind_func (widget, item, self->user_data);
+}
+
