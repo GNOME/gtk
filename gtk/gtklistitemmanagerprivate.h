3a1d8d2e0bc9498b88681b52cadaddc865d2f3d1
Benjamin Otte <otte@redhat.com>
2018-09-20 05:18:34 +0200

    listview: Add GtkListItemManager
    It's all stubs for now, but here's the basic ideas about what
    this object is supposed to do:
    
    (1) It's supposed to be handling all the child GtkWidgets that are
        used by the listview, so that the listview can concern
        itself with how many items it needs and where to put them.
    (2) It's meant to do the caching of widgets that are not (currently)
        used.
    (3) It's meant to track items that remain in the model across
        items-changed emissions and just change position.
    (2) It's code that can be shared between listview and potential
        other widgets like a GridView.
    
    It's also free to assume that the number of items it's supposed to
    manage doesn't grow too much, so it's free to use O(N) algorithms.

---
+++ gtk/gtklistitemmanagerprivate.h
@@ -0,0 +1,62 @@
+/*
+ * Copyright © 2018 Benjamin Otte
+ *
+ * This library is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Lesser General Public
+ * License as published by the Free Software Foundation; either
+ * version 2.1 of the License, or (at your option) any later version.
+ *
+ * This library is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public
+ * License along with this library. If not, see <http://www.gnu.org/licenses/>.
+ *
+ * Authors: Benjamin Otte <otte@gnome.org>
+ */
+
+
+#ifndef __GTK_LIST_ITEM_MANAGER_H__
+#define __GTK_LIST_ITEM_MANAGER_H__
+
+#include "gtk/gtktypes.h"
+
+#include "gtk/gtklistitemfactoryprivate.h"
+
+G_BEGIN_DECLS
+
+#define GTK_TYPE_LIST_ITEM_MANAGER         (gtk_list_item_manager_get_type ())
+#define GTK_LIST_ITEM_MANAGER(o)           (G_TYPE_CHECK_INSTANCE_CAST ((o), GTK_TYPE_LIST_ITEM_MANAGER, GtkListItemManager))
+#define GTK_LIST_ITEM_MANAGER_CLASS(k)     (G_TYPE_CHECK_CLASS_CAST ((k), GTK_TYPE_LIST_ITEM_MANAGER, GtkListItemManagerClass))
+#define GTK_IS_LIST_ITEM_MANAGER(o)        (G_TYPE_CHECK_INSTANCE_TYPE ((o), GTK_TYPE_LIST_ITEM_MANAGER))
+#define GTK_IS_LIST_ITEM_MANAGER_CLASS(k)  (G_TYPE_CHECK_CLASS_TYPE ((k), GTK_TYPE_LIST_ITEM_MANAGER))
+#define GTK_LIST_ITEM_MANAGER_GET_CLASS(o) (G_TYPE_INSTANCE_GET_CLASS ((o), GTK_TYPE_LIST_ITEM_MANAGER, GtkListItemManagerClass))
+
+typedef struct _GtkListItemManager GtkListItemManager;
+typedef struct _GtkListItemManagerClass GtkListItemManagerClass;
+
+GType                   gtk_list_item_manager_get_type          (void) G_GNUC_CONST;
+
+GtkListItemManager *    gtk_list_item_manager_new               (GtkWidget              *widget);
+
+void                    gtk_list_item_manager_set_factory       (GtkListItemManager     *self,
+                                                                 GtkListItemFactory     *factory);
+GtkListItemFactory *    gtk_list_item_manager_get_factory       (GtkListItemManager     *self);
+void                    gtk_list_item_manager_set_model         (GtkListItemManager     *self,
+                                                                 GListModel             *model);
+GListModel *            gtk_list_item_manager_get_model         (GtkListItemManager     *self);
+
+
+void                    gtk_list_item_manager_model_changed     (GtkListItemManager     *self,
+                                                                 guint                   position,
+                                                                 guint                   removed,
+                                                                 guint                   added);
+GtkWidget *             gtk_list_item_manager_create_list_item  (GtkListItemManager     *self,
+                                                                 guint                   position,
+                                                                 GtkWidget              *next_sibling);
+
+G_END_DECLS
+
+#endif /* __GTK_LIST_ITEM_MANAGER_H__ */
