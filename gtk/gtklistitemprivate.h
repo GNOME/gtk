e2354e5f3b86bbdaf65538567c26f19cb98f4394
Benjamin Otte <otte@redhat.com>
2018-09-21 05:05:34 +0200

    listview: Add GtkListItem
    GtkListItem is a generic row widget that is supposed to replace
    GtkListBoxRow and GtkFlowBoxChild.

---
+++ gtk/gtklistitemprivate.h
@@ -0,0 +1,38 @@
+/*
+ * Copyright © 2018 Benjamin Otte
+ *
+ * This library is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Lesser General Public
+ * License as published by the Free Software Foundation; either
+ * version 2.1 of the License, or (at your option) any later version.
+ *
+ * This library is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public
+ * License along with this library. If not, see <http://www.gnu.org/licenses/>.
+ *
+ * Authors: Benjamin Otte <otte@gnome.org>
+ */
+
+#ifndef __GTK_LIST_ITEM_PRIVATE_H__
+#define __GTK_LIST_ITEM_PRIVATE_H__
+
+#include "gtklistitem.h"
+
+G_BEGIN_DECLS
+
+GtkWidget *     gtk_list_item_new                               (const char             *css_name);
+
+void            gtk_list_item_set_child                         (GtkListItem            *item,
+                                                                 GtkWidget              *child);
+
+void            gtk_list_item_bind                              (GtkListItem            *self,
+                                                                 gpointer                item);
+void            gtk_list_item_unbind                            (GtkListItem            *self);
+
+G_END_DECLS
+
+#endif  /* __GTK_LIST_ITEM_PRIVATE_H__ */
