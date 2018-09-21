ae1331ee92b65fcfba46446a362860377078041e
Benjamin Otte <otte@redhat.com>
2018-09-21 05:32:00 +0200

    listview: Make the listitemmanager stricter
    Require that items created with the manager get destroyed via the
    manager.
    
    To that purpose, renamed create_list_item() to acquire_list_item() and
    add a matching release_list_item() function.
    
    This way, the manager can in the future keep track of all items and
    cache information about them.

--- gtk/gtklistview.c
+++ gtk/gtklistview.c
@@ -126,7 +126,7 @@ list_row_clear (gpointer _row)
 {
   ListRow *row = _row;
 
-  g_clear_pointer (&row->widget, gtk_widget_unparent);
+  g_assert (row->widget == NULL);
 }
 
 static ListRow *
@@ -489,6 +489,8 @@ gtk_list_view_remove_rows (GtkListView *self,
   for (i = 0; i < n_rows; i++)
     {
       ListRow *next = gtk_css_rb_tree_get_next (self->rows, row);
+      gtk_list_item_manager_release_list_item (self->item_manager, row->widget);
+      row->widget = NULL;
       gtk_css_rb_tree_remove (self->rows, row);
       row = next;
     }
@@ -522,9 +524,9 @@ gtk_list_view_add_rows (GtkListView *self,
         
       new_row = gtk_css_rb_tree_insert_before (self->rows, row);
       new_row->n_rows = 1;
-      new_row->widget = gtk_list_item_manager_create_list_item (self->item_manager,
-                                                                position + i,
-                                                                row ? row->widget : NULL);
+      new_row->widget = gtk_list_item_manager_acquire_list_item (self->item_manager,
+                                                                 position + i,
+                                                                 row ? row->widget : NULL);
     }
 
   gtk_widget_queue_resize (GTK_WIDGET (self));
