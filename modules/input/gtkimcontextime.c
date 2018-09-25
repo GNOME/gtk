GNOME/gtk

commit c255ba68fc2c918dd84da48a472e7973d3c00b03

input/IME: Defer the emit of the "commit" signal
On Windows, when IME is used, each keystroke results in the
WM_IME_COMPOSITION event being sent first.  This means that in our case
when one decides on to accept the input that is in the preedit buffer,
we first get from Windows the WM_IME_COMPOSITION event
(where we emit the commit signal), followed by the WM_IME_ENDCOMPOSITION
event (where we emit the pair of preedit-changed and preedit-end
signals).

Since commit f11f989 (GtkEntry: Remove recompute idle), we do the input
recomputation directly, this will cause a pair of "Pango-WARNING:
Assertion failed: (index >= 0 && index <= layout->length)" being shown,
as gtkentry.c's priv->preedit_length and priv->preedit_cursor was unable
to be reset to 0 in time as a result of the recomputation triggered by
the commit being done before the reset of priv->preedit_length and
priv->preedit_cursor (which are no longer valid as we essentially say
that we are done with the preedit buffer).

As we could only acquire the final string that was entered in this
preedit session when we handle the WM_IME_COMPOSITION event, fix this by
saving up the final string we acquire from Windows IME in UTF-8 when we
handle the WM_IME_COMPOSITION event from Windows, and emit the commit
signal with that string after we emit the preedit-changed and
preedit-end signals when we handle the WM_IME_ENDCOMPOSITION event from
Windows, which comes afterwards.

Also fix the formatting of the code around the parts of the files that
was changed.

https://bugzilla.gnome.org/show_bug.cgi?id=787142

modules/input/gtkimcontextime.c
@@ -187,6 +187,7 @@ gtk_im_context_ime_init (GtkIMContextIME *context_ime)
   context_ime->cursor_location.y      = 0;
   context_ime->cursor_location.width  = 0;
   context_ime->cursor_location.height = 0;
+  context_ime->commit_string          = NULL;
 
   context_ime->priv = g_malloc0 (sizeof (GtkIMContextIMEPrivate));
   context_ime->priv->conversion_mode  = 0;
@@ -1061,14 +1062,14 @@ gtk_im_context_ime_message_filter (GdkXEvent *xevent,
             gchar *utf8str = NULL;
             GError *error = NULL;
 
-	    len = ImmGetCompositionStringW (himc, GCS_RESULTSTR, NULL, 0);
+            len = ImmGetCompositionStringW (himc, GCS_RESULTSTR, NULL, 0);
 
             if (len > 0)
               {
-		gpointer buf = g_alloca (len);
-		ImmGetCompositionStringW (himc, GCS_RESULTSTR, buf, len);
-		len /= 2;
-		utf8str = g_utf16_to_utf8 (buf, len, NULL, NULL, &error);
+                gpointer buf = g_alloca (len);
+                ImmGetCompositionStringW (himc, GCS_RESULTSTR, buf, len);
+                len /= 2;
+                context_ime->commit_string = g_utf16_to_utf8 (buf, len, NULL, NULL, &error);
                 if (error)
                   {
                     g_warning ("%s", error->message);
@@ -1076,12 +1077,8 @@ gtk_im_context_ime_message_filter (GdkXEvent *xevent,
                   }
               }
 
-            if (utf8str)
-              {
-                g_signal_emit_by_name (context, "commit", utf8str);
-                g_free (utf8str);
-		retval = TRUE;
-              }
+            if (context_ime->commit_string)
+              retval = TRUE;
           }
 
         if (context_ime->use_preedit)
@@ -1101,6 +1098,14 @@ gtk_im_context_ime_message_filter (GdkXEvent *xevent,
       context_ime->preediting = FALSE;
       g_signal_emit_by_name (context, "preedit-changed");
       g_signal_emit_by_name (context, "preedit-end");
+
+      if (context_ime->commit_string)
+        {
+          g_signal_emit_by_name (context, "commit", context_ime->commit_string);
+          g_free (context_ime->commit_string);
+          context_ime->commit_string = NULL;
+        }
+
       if (context_ime->use_preedit)
         retval = TRUE;
       break;

modules/input/gtkimcontextime.h
@@ -45,6 +45,7 @@ struct _GtkIMContextIME
   guint opened : 1;
   guint focus : 1;
   GdkRectangle cursor_location;
+  gchar *commit_string;
 
   GtkIMContextIMEPrivate *priv;
 };

commit cfee4e48199178da0d6f4ebf5ab443d267889282

gtkimcontextime.c: Fix Korean input
Commit c255ba68 inadvertently introduced a regression that broke Korean
text input because the changes there resulted that only the last input
string that we have from ImmGetCompositionStringW() for each time the
commit signal is emitted is kept, and also as a result the final Korean
character that is input by hitting space is also lost as a result, as we
didn't check for whether we are done with preediting.

Fix these issues by doing the following when we receive the
WM_IME_COMPOSITION message with GCS_RESULTSTR from Windows:
-Append to the string to commit, rather than replacing that string, if
 that string already has something in it (i.e. we did not receive the
 WM_IME_ENDCOMPOSITION from Windows IME yet).
-Emit the commit signal immediately if we are already done with
 preediting, without waiting for the WM_IME_ENDCOMPOSITION message.

Fixes issue #1350.

modules/input/gtkimcontextime.c
@@ -997,6 +997,15 @@ ERROR_OUT:
   ImmReleaseContext (hwnd, himc);
 }
 
+static void
+gtk_im_context_ime_commit_input_string (GtkIMContext *context)
+{
+  GtkIMContextIME *context_ime = GTK_IM_CONTEXT_IME (context);
+
+  g_signal_emit_by_name (context, "commit", context_ime->commit_string);
+  g_free (context_ime->commit_string);
+  context_ime->commit_string = NULL;
+}
 
 static GdkFilterReturn
 gtk_im_context_ime_message_filter (GdkXEvent *xevent,
@@ -1069,11 +1078,29 @@ gtk_im_context_ime_message_filter (GdkXEvent *xevent,
                 gpointer buf = g_alloca (len);
                 ImmGetCompositionStringW (himc, GCS_RESULTSTR, buf, len);
                 len /= 2;
-                context_ime->commit_string = g_utf16_to_utf8 (buf, len, NULL, NULL, &error);
+                if (context_ime->commit_string == NULL)
+                  context_ime->commit_string = g_utf16_to_utf8 (buf, len, NULL, NULL, &error);
+                else
+                  {
+                    gchar *tmp_commit_string = g_strdup (context_ime->commit_string);
+                    gchar *utf8str = g_utf16_to_utf8 (buf, len, NULL, NULL, &error);
+
+                    g_free (context_ime->commit_string);
+                    context_ime->commit_string = g_strconcat (context_ime->commit_string, utf8str, NULL);
+                    g_free (tmp_commit_string);
+                    g_free (utf8str);
+                  }
+
                 if (error)
                   {
                     g_warning ("%s", error->message);
                     g_error_free (error);
+                  }
+
+                if (!context_ime->preediting)
+                  {
+                    gtk_im_context_ime_commit_input_string (context);
+                    retval = TRUE;
                   }
               }
 
@@ -1083,6 +1110,7 @@ gtk_im_context_ime_message_filter (GdkXEvent *xevent,
 
         if (context_ime->use_preedit)
           retval = TRUE;
+
         break;
       }
 
@@ -1100,11 +1128,7 @@ gtk_im_context_ime_message_filter (GdkXEvent *xevent,
       g_signal_emit_by_name (context, "preedit-end");
 
       if (context_ime->commit_string)
-        {
-          g_signal_emit_by_name (context, "commit", context_ime->commit_string);
-          g_free (context_ime->commit_string);
-          context_ime->commit_string = NULL;
-        }
+        gtk_im_context_ime_commit_input_string (context);
 
       if (context_ime->use_preedit)
         retval = TRUE;