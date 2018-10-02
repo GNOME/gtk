GNOME/gtk

commit 42b02d9d01cc7b3cf4282550e93bef8363408eea

window: ignore resize increments for maximized/fullscreen
Once a window is maximized/fullscreen, resize increments should be
ignored otherwise the window may appear smaller than the screen size.

That also applies to configure requests as well.

https://bugzilla.gnome.org/show_bug.cgi?id=751368

gtk/gtkwindow.c
@@ -8799,7 +8799,12 @@ gtk_window_compute_configure_request_size (GtkWindow   *window,
     }
 
   /* Override any size with gtk_window_resize() values */
-  if (info)
+  if (priv->maximized || priv->fullscreen)
+    {
+      /* Unless we are maximized or fullscreen */
+      gtk_window_get_remembered_size (window, width, height);
+    }
+  else if (info)
     {
       if (info->resize_width > 0)
        *width = info->resize_width;

commit 786f4721704d7b1f76aabbbe7379fc8ae532bea8

window: handle configure request when maximized/fullscreen
Commit 42b02d9d ("ignore resize increments for maximized/fullscreen")
introduced a regression which prevents window from being resized while
maximized or fullscreen.

Move the check for maximized/fullscreen where the geometry is handled
rather than ignoring the geometry info when maximized/fullscreen so that
a client issuing a "move_resize()" while maximized or fullscreen can get
the requested size when restoring the original window state.

Closes: https://gitlab.gnome.org/GNOME/gtk/issues/1044

gtk/gtkwindow.c
@@ -9362,7 +9362,8 @@ gtk_window_compute_configure_request_size (GtkWindow   *window,
   
   info = gtk_window_get_geometry_info (window, FALSE);
 
-  if (priv->need_default_size)
+  if (priv->need_default_size &&
+      !(priv->maximized || priv->fullscreen))
     {
       gtk_window_guess_default_size (window, width, height);
       gtk_window_get_remembered_size (window, &w, &h);
@@ -9398,13 +9399,7 @@ gtk_window_compute_configure_request_size (GtkWindow   *window,
       gtk_window_get_remembered_size (window, width, height);
     }
 
-  /* Override any size with gtk_window_resize() values */
-  if (priv->maximized || priv->fullscreen)
-    {
-      /* Unless we are maximized or fullscreen */
-      gtk_window_get_remembered_size (window, width, height);
-    }
-  else if (info)
+  if (info)
     {
       gint resize_width_csd = info->resize_width;
       gint resize_height_csd = info->resize_height;