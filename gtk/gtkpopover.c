ec1786fdbc65bd21a6a7ecb23fd51a2577169965
Daniel Boles <dboles.src@gmail.com>
2018-11-05 23:36:54 +0000

    Popover: Minimally document the ::closed signal
    This is better than nothing at all. The wording is taken from Carlos's
    commit message when he added this shortly before 3.12, so add Since too.
    Skip the bit from his commit message explaining what this replaced; we
    don't need to say all the less good things our convenience API replaces.

--- gtk/gtkpopover.c
+++ gtk/gtkpopover.c
@@ -1789,6 +1789,14 @@ gtk_popover_class_init (GtkPopoverClass *klass)
 
   g_object_class_install_properties (object_class, NUM_PROPERTIES, properties);
 
+  /**
+   * GtkPopover::closed:
+   *
+   * This signal is emitted when the popover is dismissed either through
+   * API or user interaction.
+   *
+   * Since: 3.12
+   */
   signals[CLOSED] =
     g_signal_new (I_("closed"),
                   G_TYPE_FROM_CLASS (object_class),
