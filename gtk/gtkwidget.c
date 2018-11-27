eebb8497605f9981a7232879d9f31805dfffa970
Timm Bäder <mail@baedert.org>
2018-11-27 06:00:30 +0100

    widget: Query CSS values later in size_allocate
    Move it below the "this widget only changed its position" check.

--- gtk/gtkwidget.c
+++ gtk/gtkwidget.c
@@ -4128,11 +4128,6 @@ gtk_widget_size_allocate (GtkWidget           *widget,
   priv->allocated_size = *allocation;
   priv->allocated_size_baseline = baseline;
 
-  style = gtk_css_node_get_style (priv->cssnode);
-  get_box_margin (style, &margin);
-  get_box_border (style, &border);
-  get_box_padding (style, &padding);
-
   adjusted_allocation = real_allocation;
   if (gtk_widget_get_request_mode (widget) == GTK_SIZE_REQUEST_HEIGHT_FOR_WIDTH)
     {
@@ -4244,6 +4239,11 @@ gtk_widget_size_allocate (GtkWidget           *widget,
 
       goto check_clip;
     }
+
+  style = gtk_css_node_get_style (priv->cssnode);
+  get_box_margin (style, &margin);
+  get_box_border (style, &border);
+  get_box_padding (style, &padding);
 
   /* Since gtk_widget_measure does it for us, we can be sure here that
    * the given alloaction is large enough for the css margin/bordder/padding */
