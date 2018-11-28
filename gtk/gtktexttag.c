dcd21e12cdc8006e7ad768c85210fa01392dc92a
Mohammed Sadiq <sadiq@sadiqpk.org>
2018-11-28 16:51:45 +0530

    texttag: Replace gtk3-demo reference with gtk4-demo

--- gtk/gtktexttag.c
+++ gtk/gtktexttag.c
@@ -61,7 +61,7 @@
  * before using them with that buffer.
  *
  * gtk_text_buffer_create_tag() is the best way to create tags.
- * See “gtk3-demo” for numerous examples.
+ * See “gtk4-demo” for numerous examples.
  *
  * For each property of #GtkTextTag, there is a “set” property, e.g.
  * “font-set” corresponds to “font”. These “set” properties reflect
