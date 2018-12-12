24754c32593c9a6c6cf1e2c6a57988cd7e416b5c
Emmanuele Bassi <ebassi@gnome.org>
2018-12-12 15:24:41 +0000

    Add GtkLayoutManager
    A base abstract class for layout manager delegate objects.
    
    Layout managers are associated to a single widget, like event
    controllers, and are responsible for measuring and allocating the
    children of the widget they are bound to.

---
+++ gtk/gtklayoutmanagerprivate.h
@@ -0,0 +1,10 @@
+#pragma once
+
+#include "gtklayoutmanager.h"
+
+G_BEGIN_DECLS
+
+void gtk_layout_manager_set_widget (GtkLayoutManager *manager,
+                                    GtkWidget        *widget);
+
+G_END_DECLS
