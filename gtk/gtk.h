ded37e5f5ae6a6ae75aa5116fc70ad6e8cde1924
Emmanuele Bassi <ebassi@gnome.org>
2018-12-19 17:32:24 +0000

    Add GtkBoxLayout
    The same layout policy of GtkBox, without all the GtkContainer calories.

--- gtk/gtk.h
+++ gtk/gtk.h
@@ -47,6 +47,7 @@
 #include <gtk/gtkbin.h>
 #include <gtk/gtkbindings.h>
 #include <gtk/gtkborder.h>
+#include <gtk/gtkboxlayout.h>
 #include <gtk/gtkbox.h>
 #include <gtk/gtkbuildable.h>
 #include <gtk/gtkbuilder.h>
