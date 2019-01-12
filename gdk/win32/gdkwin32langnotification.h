3b244c9f11edb92e4196f6d0ba25e2c8b928a166
Руслан Ижбулатов <lrn1986@gmail.com>
2019-01-12 21:32:48 +0000

    GDK W32: Test for IME correctly
    ImmIsIME() doesn't work (always returns TRUE) since Vista.
    Use ITfActiveLanguageProfileNotifySink to detect TSF changes,
    which are equal to IME changes for us.
    
    Also make sure that IMMultiContext re-loads the IM when keyboard layout
    changes, otherwise there's a subtle bug that could happen:
    * Run GTK application with non-IME layout (US, for example)
    * Focus on an editable widget (GtkEntry, for example)
    * IM Context is initialized to use the simple IM
    * Switch to an IME layout (such as Korean)
    * Start typing
    * Since IME module is not loaded yet, keypresses are handled
      by a default MS IME handler
    * Once IME commits a character, GDK will get a WM_KEYDOWN,
      which will trigger a GdkKeyEvent, which will be handled by
      an event filter in IM Context, which will finally re-evaluate
      its status and load IME, and only after that GTK will get
      to handle IME by itself - but by that point input would
      already be broken.
    To avoid this we can emit a dummy event (with Void keyval),
    which will cause IM Context to load the appropriate module
    immediately.

---
+++ gdk/win32/gdkwin32langnotification.h
@@ -0,0 +1,26 @@
+/*
+ * gdkwin32langnotification.h
+ *
+ * Copyright 2019 Руслан Ижбулатов <lrn1986@gmail.com>
+ *
+ * This library is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Library General Public
+ * License as published by the Free Software Foundation; either
+ * version 2 of the License, or (at your option) any later version.
+ *
+ * This library is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * Library General Public License for more details.
+ *
+ * You should have received a copy of the GNU Library General Public
+ * License along with this library. If not, see <http://www.gnu.org/licenses/>.
+ */
+
+#ifndef __GDK_WIN32_LANGNOTIFICATION_H__
+#define __GDK_WIN32_LANGNOTIFICATION_H__
+
+void _gdk_win32_lang_notification_init (void);
+void _gdk_win32_lang_notification_exit (void);
+
+#endif
