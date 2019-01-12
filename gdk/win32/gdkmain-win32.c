6b44bb992c89aa1579501ba6411977502f241298
Руслан Ижбулатов <lrn1986@gmail.com>
2019-01-12 21:32:48 +0000

    GDK W32: Test for IME correctly
    ImmIsIME() doesn't work (always returns TRUE) since Vista.
    Use ITfActiveLanguageProfileNotifySink to detect TSF changes,
    which are equal to IME changes for us.
    
    Also make sure that IMMultiContext re-loads the IM when keyboard layout
    changes, otherwise there's a subtle bug that could happen:
    * Run GTK application with non-IME layout (US, for example)
    * Focus on an editable widget (GtkEntry, for example)
    * IM Context is initialized to use the simple IM
    * Switch to an IME layout (such as Korean)
    * Start typing
    * Since IME module is not loaded yet, keypresses are handled
      by a default MS IME handler
    * Once IME commits a character, GDK will get a WM_KEYDOWN,
      which will trigger a GdkKeyEvent, which will be handled by
      an event filter in IM Context, which will finally re-evaluate
      its status and load IME, and only after that GTK will get
      to handle IME by itself - but by that point input would
      already be broken.
    To avoid this we can emit a dummy event (with Void keyval),
    which will cause IM Context to load the appropriate module
    immediately.

--- gdk/win32/gdkmain-win32.c
+++ gdk/win32/gdkmain-win32.c
@@ -93,7 +93,6 @@ _gdk_win32_windowing_init (void)
   _gdk_display_hdc = CreateDC ("DISPLAY", NULL, NULL, NULL);
   _gdk_input_locale = GetKeyboardLayout (0);
   _gdk_win32_keymap_set_active_layout (GDK_WIN32_KEYMAP (_gdk_win32_display_get_keymap (_gdk_display)), _gdk_input_locale);
-  _gdk_input_locale_is_ime = ImmIsIME (_gdk_input_locale);
   GetLocaleInfo (MAKELCID (LOWORD (_gdk_input_locale), SORT_DEFAULT),
 		 LOCALE_IDEFAULTANSICODEPAGE,
 		 buf, sizeof (buf));
