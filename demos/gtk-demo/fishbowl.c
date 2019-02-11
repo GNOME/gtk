220ccaf363e48a1dc30861da6501090ad020fea1
Matthias Clasen <mclasen@redhat.com>
2019-02-11 08:24:06 -0500

    gtk-demo: Fix fishbowl setup
    The fishbowl demo was failing to reset a static variable
    when setting up, causing it to crash the second time around.
    
    Closes: #1665

--- demos/gtk-demo/fishbowl.c
+++ demos/gtk-demo/fishbowl.c
@@ -264,7 +264,10 @@ do_fishbowl (GtkWidget *do_widget)
                                         NULL);
       gtk_builder_connect_signals (builder, NULL);
       window = GTK_WIDGET (gtk_builder_get_object (builder, "window"));
+      g_signal_connect (window, "destroy",
+                        G_CALLBACK (gtk_widget_destroyed), &window);
       bowl = GTK_WIDGET (gtk_builder_get_object (builder, "bowl"));
+      selected_widget_type = -1;
       set_widget_type (GTK_FISHBOWL (bowl), 0);
       gtk_window_set_display (GTK_WINDOW (window),
                               gtk_widget_get_display (do_widget));
@@ -278,7 +281,6 @@ do_fishbowl (GtkWidget *do_widget)
     gtk_widget_show (window);
   else
     gtk_widget_destroy (window);
-
 
   return window;
 }
