a07341b291b79645ff12c713d51a8b03dc13800a
Timm Bäder <mail@baedert.org>
2019-02-14 10:45:29 +0100

    gl renderer: Move texture atlas into its own file
    We want to reuse this somewhere else later.

---
+++ gsk/gl/gskgltextureatlasprivate.h
@@ -0,0 +1,49 @@
+
+#ifndef __GSK_GL_TEXTURE_ATLAS_H__
+#define __GSK_GL_TEXTURE_ATLAS_H__
+
+#include "stb_rect_pack.h"
+#include "gskglimageprivate.h"
+#include "gskgldriverprivate.h"
+
+struct _GskGLTextureAtlas
+{
+  struct stbrp_context context;
+  struct stbrp_node *nodes;
+
+  int width;
+  int height;
+
+  GskGLImage image;
+
+  int unused_pixels; /* Pixels of rects that have been used at some point,
+                        But are now unused. */
+
+  void *user_data;
+};
+typedef struct _GskGLTextureAtlas GskGLTextureAtlas;
+
+void        gsk_gl_texture_atlas_init              (GskGLTextureAtlas       *self,
+                                                    int                      width,
+                                                    int                      height);
+
+void        gsk_gl_texture_atlas_free              (GskGLTextureAtlas       *self);
+
+void        gsk_gl_texture_atlas_mark_unused       (GskGLTextureAtlas       *self,
+                                                    int                      width,
+                                                    int                      height);
+
+void        gsk_gl_texture_atlas_mark_used         (GskGLTextureAtlas       *self,
+                                                    int                      width,
+                                                    int                      height);
+
+
+gboolean    gsk_gl_texture_atlas_pack              (GskGLTextureAtlas       *self,
+                                                    int                      width,
+                                                    int                      height,
+                                                    int                     *out_x,
+                                                    int                     *out_y);
+
+double      gsk_gl_texture_atlas_get_unused_ratio  (const GskGLTextureAtlas *self);
+
+#endif
