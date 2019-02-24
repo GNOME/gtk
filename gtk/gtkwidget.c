023f799603d2e321eb1da77a9e7420a78195c68f
Matthias Clasen <mclasen@redhat.com>
2019-02-24 18:41:26 -0500

    widget: guard popover-specific code paths
    This is essentially a special case just for GtkPopover,
    which is the last widget with a child surface.

--- gtk/gtkwidget.c
+++ gtk/gtkwidget.c
@@ -4330,7 +4330,8 @@ gtk_widget_allocate (GtkWidget    *widget,
     {
       /* Still have to move the window... */
       if (_gtk_widget_get_realized (widget) &&
-          _gtk_widget_get_has_surface (widget))
+          _gtk_widget_get_has_surface (widget) &&
+          GTK_IS_POPOVER (widget))
          {
            GtkAllocation window_alloc;
 
@@ -4532,7 +4533,8 @@ gtk_widget_real_size_allocate (GtkWidget *widget,
   GtkWidgetPrivate *priv = gtk_widget_get_instance_private (widget);
 
   if (_gtk_widget_get_realized (widget) &&
-      _gtk_widget_get_has_surface (widget))
+      _gtk_widget_get_has_surface (widget) &&
+      GTK_IS_POPOVER (widget))
      {
        GtkAllocation window_alloc;
 
