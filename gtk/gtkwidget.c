5af98031e5c5067dc3a97e1b83022bfc93e7e76e
Matthias Clasen <mclasen@redhat.com>
2019-03-02 11:40:09 -0500

    Move a check to gtk_window_set_focus
    gtk_widget_grab_focus is just a wrapper for
    gtk_window_set_focus. We should do all the enforcement
    there.

--- gtk/gtkwidget.c
+++ gtk/gtkwidget.c
@@ -5337,9 +5337,6 @@ gtk_widget_grab_focus (GtkWidget *widget)
 {
   g_return_if_fail (GTK_IS_WIDGET (widget));
 
-  if (!gtk_widget_is_sensitive (widget))
-    return;
-
   GTK_WIDGET_GET_CLASS (widget)->grab_focus (widget);
 }
 
