4cd4896f30f3e210f46e772b651f18e10a418e9f
Timm Bäder <mail@baedert.org>
2019-03-02 16:55:17 +0100

    Parse render nodes from text files
    Instead of the previous approach using GVariant, this new approach uses
    human-readable text files as the serialization format for render nodes.
    
    The format is a custom one, but it is inspired by QML and conforms to
    the CSS syntax. Because of that, we can use the CSS machinery from GTK
    to parse it, and in particular share code to parse properties that GTK's
    CSS machinery also supports, such as colors.
    
    This commit breaks all existing usages of node files - such as the
    testsuite and various test tools - they will be fixed in further
    commits.

---
+++ gsk/gskrendernodeparserprivate.h
@@ -0,0 +1,11 @@
+
+#ifndef __GSK_RENDER_NODE_PARSER_PRIVATE_H__
+#define __GSK_RENDER_NODE_PARSER_PRIVATE_H__
+
+#include "gskrendernode.h"
+
+GskRenderNode * gsk_render_node_deserialize_from_bytes  (GBytes        *bytes,
+                                                         GError       **error);
+char *          gsk_render_node_serialize_to_string     (GskRenderNode *root);
+
+#endif
