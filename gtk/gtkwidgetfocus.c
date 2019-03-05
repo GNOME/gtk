d3d6e389a97fe34ec1c60d816c5bbf0c8687569e
Matthias Clasen <mclasen@redhat.com>
2019-03-04 19:00:30 -0500

    Add a missing include
    If we don't include config.h, GDK_AVAILABLE_...
    does not have the desired effect, and your symbols
    are not exported. And you spend hours tearing out
    your hair.

--- gtk/gtkwidgetfocus.c
+++ gtk/gtkwidgetfocus.c
@@ -15,6 +15,8 @@
  * License along with this library. If not, see <http://www.gnu.org/licenses/>.
  */
 
+#include "config.h"
+
 #include "gtkwidgetprivate.h"
 
 typedef struct _CompareInfo CompareInfo;
