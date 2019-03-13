de68f576c67a81b07767453995074afee9a25994
Chun-wei Fan <fanchunwei@src.gnome.org>
2019-03-13 13:06:04 +0800

    tests/: Declare variables at the top-of-block
    This will ensure that the code will build for pre-C99 compilers.

--- tests/testspinbutton.c
+++ tests/testspinbutton.c
@@ -56,12 +56,13 @@ prepare_window_for_orientation (GtkOrientation orientation)
                                                0.0);
 
       GtkWidget *spin = gtk_spin_button_new (adj, 1.0, 0);
+      GtkWidget *hbox;
       gtk_orientable_set_orientation (GTK_ORIENTABLE (spin), orientation);
       gtk_widget_set_halign (GTK_WIDGET (spin), GTK_ALIGN_CENTER);
 
       g_object_bind_property (wrap_button, "active", spin, "wrap", G_BINDING_SYNC_CREATE);
 
-      GtkWidget *hbox = gtk_box_new (GTK_ORIENTATION_HORIZONTAL, 2);
+      hbox = gtk_box_new (GTK_ORIENTATION_HORIZONTAL, 2);
       gtk_box_pack_start (GTK_BOX (hbox), spin, FALSE, FALSE, 2);
       gtk_container_add (GTK_CONTAINER (mainbox), hbox);
     }
