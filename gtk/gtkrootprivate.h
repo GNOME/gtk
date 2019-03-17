ec7227b4a63492b12499e324f67567069c7d02cf
Matthias Clasen <mclasen@redhat.com>
2019-03-17 00:36:08 -0400

    root: Move the idle sizer here
    Move the idle sizer from GtkContainer to GtkRoot
    and call it layout phase. The new (private) entry
    points are gtk_root_queue_restyle() and
    gtk_root_start/stop_layout_phase().

--- gtk/gtkrootprivate.h
+++ gtk/gtkrootprivate.h
@@ -5,12 +5,17 @@
 
 G_BEGIN_DECLS
 
-GdkDisplay *            gtk_root_get_display            (GtkRoot                *root);
+GdkDisplay *            gtk_root_get_display            (GtkRoot                *self);
 GskRenderer *           gtk_root_get_renderer           (GtkRoot                *self);
 
 void                    gtk_root_get_surface_transform  (GtkRoot                *self,
                                                          int                    *x,
                                                          int                    *y);
+
+void                    gtk_root_queue_restyle          (GtkRoot                *self);
+void                    gtk_root_start_layout_phase     (GtkRoot                *self);
+void                    gtk_root_stop_layout_phase      (GtkRoot                *self);
+
 enum {
   GTK_ROOT_PROP_FOCUS_WIDGET,
   GTK_ROOT_NUM_PROPERTIES
