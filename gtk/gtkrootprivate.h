acededd6aaac4cc6b866a94a3f41fb7d9954d1e3
Matthias Clasen <mclasen@redhat.com>
2019-03-17 09:24:49 -0400

    root: Add pointer focus apis
    This copies all the private GtkWindow apis for
    handling pointer focus.

--- gtk/gtkrootprivate.h
+++ gtk/gtkrootprivate.h
@@ -19,6 +19,31 @@ void                    gtk_root_stop_layout_phase      (GtkRoot                
 gboolean                gtk_root_activate_key           (GtkRoot                *self,
                                                          GdkEventKey            *event);
 
+void                    gtk_root_update_pointer_focus   (GtkRoot                *root,
+                                                         GdkDevice              *device,
+                                                         GdkEventSequence       *sequence,
+                                                         GtkWidget              *target,
+                                                         double                  x,
+                                                         double                  y);
+void                    gtk_root_update_pointer_focus_on_state_change (GtkRoot   *root,
+                                                                       GtkWidget *widget);
+GtkWidget *             gtk_root_lookup_pointer_focus   (GtkRoot                 *root,
+                                                         GdkDevice              *device,
+                                                         GdkEventSequence       *sequence);
+GtkWidget *             gtk_root_lookup_pointer_focus_implicit_grab (GtkRoot          *root,
+                                                                     GdkDevice        *device,
+                                                                     GdkEventSequence *sequence);
+GtkWidget *             gtk_root_lookup_effective_pointer_focus (GtkRoot          *root,
+                                                                 GdkDevice        *device,
+                                                                 GdkEventSequence *sequence);
+void                    gtk_root_set_pointer_focus_grab  (GtkRoot                *root,
+                                                          GdkDevice              *device,
+                                                          GdkEventSequence       *sequence,
+                                                          GtkWidget              *target);
+void                    gtk_root_maybe_update_cursor     (GtkRoot                *root,
+                                                          GtkWidget              *widget,
+                                                          GdkDevice              *device);
+
 enum {
   GTK_ROOT_PROP_FOCUS_WIDGET,
   GTK_ROOT_PROP_DEFAULT_WIDGET,
