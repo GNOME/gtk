a938c14d11b1a0036f4bddd6b5c46382523381bb
Benjamin Otte <otte@redhat.com>
2019-03-22 04:20:17 +0100

    build: Add gtk-css static library
    This library is meant to be the new CSS library that gets used from GDK,
    GSK and GTK for string printing and parsing.
    
    As a first step, move GtkCssProviderError into it.
    
    While doing so, split it into GtkCssParserError (for critical problems)
    and GtkCssParserWarning (for non-critical problems).

---
+++ gtk/css/gtkcsserror.c
@@ -0,0 +1,35 @@
+/*
+ * Copyright © 2019 Benjamin Otte
+ *
+ * This library is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Lesser General Public
+ * License as published by the Free Software Foundation; either
+ * version 2.1 of the License, or (at your option) any later version.
+ *
+ * This library is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public
+ * License along with this library. If not, see <http://www.gnu.org/licenses/>.
+ *
+ * Authors: Benjamin Otte <otte@gnome.org>
+ */
+
+#include "config.h"
+
+#include "gtkcsserror.h"
+
+GQuark
+gtk_css_parser_error_quark (void)
+{
+  return g_quark_from_static_string ("gtk-css-parser-error-quark");
+}
+
+GQuark
+gtk_css_parser_warning_quark (void)
+{
+  return g_quark_from_static_string ("gtk-css-parser-warning-quark");
+}
+
