5719b8c89d67b6de98214e2e8b282a14a54094da
Benjamin Otte <otte@redhat.com>
2019-03-22 04:20:17 +0100

    build: Add gtk-css static library
    This library is meant to be the new CSS library that gets used from GDK,
    GSK and GTK for string printing and parsing.
    
    As a first step, move GtkCssProviderError into it.
    
    While doing so, split it into GtkCssParserError (for critical problems)
    and GtkCssParserWarning (for non-critical problems).

--- testsuite/css/parser/test-css-parser.c
+++ testsuite/css/parser/test-css-parser.c
@@ -150,8 +150,8 @@ parsing_error_cb (GtkCssProvider *provider,
                           basename, gtk_css_section_get_end_line (section) + 1);
   g_free (basename);
 
-  if (error->domain == GTK_CSS_PROVIDER_ERROR)
-      append_error_value (errors, GTK_TYPE_CSS_PROVIDER_ERROR, error->code);
+  if (error->domain == GTK_CSS_PARSER_ERROR)
+      append_error_value (errors, GTK_TYPE_CSS_PARSER_ERROR, error->code);
   else
     g_string_append_printf (errors, 
                             "%s %u\n",
