d1e14ba6cef56a525daa5efdcfec7feb47c36959
Benjamin Otte <otte@redhat.com>
2019-03-26 17:39:07 +0100

    cssimage: Make cross-fade() an array
    The new spec at https://drafts.csswg.org/css-images-4/#cross-fade-function
    allows infinite images to cross-fade and we want to, too.

--- gtk/gtkcssimage.c
+++ gtk/gtkcssimage.c
@@ -502,7 +502,7 @@ gtk_css_image_get_parser_type (GtkCssParser *parser)
     { "repeating-linear-gradient", _gtk_css_image_linear_get_type },
     { "radial-gradient", _gtk_css_image_radial_get_type },
     { "repeating-radial-gradient", _gtk_css_image_radial_get_type },
-    { "cross-fade", _gtk_css_image_cross_fade_get_type },
+    { "cross-fade", gtk_css_image_cross_fade_get_type },
     { "image", _gtk_css_image_fallback_get_type }
   };
   guint i;
