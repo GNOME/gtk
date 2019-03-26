0f5a5a4b2334da955f663578bcbc05145840bde5
Chun-wei Fan <fanchunwei@src.gnome.org>
2019-03-26 16:53:20 +0800

    meson: Really fix Broadway builds
    We must also feed rawinflate.min.js into gen-c-array.py, so that the
    windows really show up in the browser when running GTK programs using
    broadway.

--- gdk/broadway/gen-c-array.py
+++ gdk/broadway/gen-c-array.py
@@ -9,13 +9,14 @@ parser.add_argument('--output', metavar='FILE', help='Output file',
                     type=argparse.FileType('w'),
                     default=sys.stdout)
 parser.add_argument('input', metavar='FILE', help='The input file',
-                    type=argparse.FileType('r'))
+                    type=argparse.FileType('r'), nargs='+')
 
 args = parser.parse_args()
 
 args.output.write('static const char {}[] = {{\n'.format(args.array_name))
-for line in args.input:
-    for ch in line:
-        args.output.write('  0x{:02x},\n'.format(ord(ch)))
+for input in args.input:
+    for line in input:
+        for ch in line:
+            args.output.write('  0x{:02x},\n'.format(ord(ch)))
 
 args.output.write('};')
