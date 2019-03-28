80c26ab9045030b51e00117dccbbd85313e2cf7a
Christoph Reiter <creiter@src.gnome.org>
2019-03-28 22:47:18 +0100

    CI: install meson 0.50

--- .gitlab-ci/run-docker.sh
+++ .gitlab-ci/run-docker.sh
@@ -2,7 +2,7 @@
 
 set -e
 
-TAG="registry.gitlab.gnome.org/gnome/gtk/master:v2"
+TAG="registry.gitlab.gnome.org/gnome/gtk/master:v3"
 
 sudo docker build --build-arg HOST_USER_ID="$UID" --tag "${TAG}" \
     --file "Dockerfile" .
