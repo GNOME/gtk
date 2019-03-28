43650d1b4f770a0ad0b3e2942b2e4950f2ffb3fa
Christoph Reiter <creiter@src.gnome.org>
2019-03-28 19:54:13 +0100

    CI: run the tests with meson
    Not all are ported I think, but this is a start.

--- .gitlab-ci/test-docker-meson.sh
+++ .gitlab-ci/test-docker-meson.sh
@@ -10,4 +10,11 @@ export PATH="${HOME}/.local/bin:${PATH}"
 python3 -m pip install --user meson==0.49.2
 
 meson _build
-ninja -C _build
+cd _build
+ninja
+
+xvfb-run -a -s "-screen 0 1024x768x24" \
+    meson test \
+        --timeout-multiplier 4 \
+        --print-errorlogs \
+        --suite=gtk+-3.0 \
