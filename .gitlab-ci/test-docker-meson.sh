878dea0b1593545f35158440a82de16c42f928d8
Christoph Reiter <creiter@src.gnome.org>
2019-03-29 17:29:36 +0100

    meson: build docs and man pages
    Lots of warnings, so something is probably still missing.

--- .gitlab-ci/test-docker-meson.sh
+++ .gitlab-ci/test-docker-meson.sh
@@ -9,7 +9,7 @@ export CCACHE_DIR="${CCACHE_BASEDIR}/_ccache"
 export PATH="${HOME}/.local/bin:${PATH}"
 python3 -m pip install --user meson==0.49.2
 
-meson _build
+meson -Ddocumentation=true -Dman-pages=true _build
 cd _build
 ninja
 
@@ -18,3 +18,5 @@ xvfb-run -a -s "-screen 0 1024x768x24" \
         --timeout-multiplier 4 \
         --print-errorlogs \
         --suite=gtk+-3.0 \
+
+ninja gail-libgail-util3-doc gdk3-doc gtk3-doc
