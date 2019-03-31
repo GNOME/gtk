058986714c5e21b91d3a4d85a410ee2e940394e0
Matthias Clasen <mclasen@redhat.com>
2019-03-31 16:16:01 -0400

    tooltips: Don't use a cursor_size of 0
    This leads to flickering tooltips.

--- gtk/gtktooltip.c
+++ gtk/gtktooltip.c
@@ -603,6 +603,9 @@ gtk_tooltip_position (GtkTooltip *tooltip,
                 "gtk-cursor-theme-size", &cursor_size,
                 NULL);
 
+  if (cursor_size == 0)
+    cursor_size = 16;
+
   if (device)
     anchor_rect_padding = MAX (4, cursor_size - 32);
   else
