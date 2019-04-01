d3870c9af468472925974ff56f4121229ff1000d
Benjamin Otte <otte@redhat.com>
2019-04-01 11:47:53 +0200

    cssparser: Drop _gtk_css_parser_has_prefix()
    Replace it with has_ident/has_function. The old function is a typical
    string matching API, not a tokenizing one.

--- gtk/gtkcssnumbervalue.c
+++ gtk/gtkcssnumbervalue.c
@@ -130,14 +130,14 @@ gboolean
 gtk_css_number_value_can_parse (GtkCssParser *parser)
 {
   return _gtk_css_parser_has_number (parser)
-      || _gtk_css_parser_has_prefix (parser, "calc");
+      || gtk_css_parser_has_function (parser, "calc");
 }
 
 GtkCssValue *
 _gtk_css_number_value_parse (GtkCssParser           *parser,
                              GtkCssNumberParseFlags  flags)
 {
-  if (_gtk_css_parser_has_prefix (parser, "calc"))
+  if (gtk_css_parser_has_function (parser, "calc"))
     return gtk_css_calc_value_parse (parser, flags);
 
   return gtk_css_dimension_value_parse (parser, flags);
