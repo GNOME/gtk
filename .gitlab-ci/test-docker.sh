7f5477735ba8e4c7afc06323bd9cf51eee24dffe
Emmanuele Bassi <ebassi@gnome.org>
2019-04-05 20:09:05 +0100

    ci: Test the a11y suite
    Check what happens on test failure to the JUnit report.

--- .gitlab-ci/test-docker.sh
+++ .gitlab-ci/test-docker.sh
@@ -32,8 +32,7 @@ xvfb-run -a -s "-screen 0 1024x768x24" \
         --print-errorlogs \
         --suite=gtk \
         --no-suite=gtk:gsk \
-        --no-suite=gtk:reftest \
-        --no-suite=gtk:a11y
+        --no-suite=gtk:reftest
 
 # Save the exit code
 exit_code=$?
