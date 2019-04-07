5b78a3048f7f7fbdd23b7338aa81d7bec237a7ee
Matthias Clasen <mclasen@redhat.com>
2019-04-07 15:47:24 +0000

    gizmo: Add a contains_func
    Let GtkGizmo override the contains() implementation.
    Update all callers to pass NULL for the contains_func.

--- gtk/gtkprogressbar.c
+++ gtk/gtkprogressbar.c
@@ -481,10 +481,11 @@ gtk_progress_bar_init (GtkProgressBar *pbar)
   priv->trough_widget = gtk_gizmo_new ("trough",
                                        NULL,
                                        allocate_trough,
-                                       snapshot_trough);
+                                       snapshot_trough,
+                                       NULL);
   gtk_widget_set_parent (priv->trough_widget, GTK_WIDGET (pbar));
 
-  priv->progress_widget = gtk_gizmo_new ("progress", NULL, NULL, NULL);
+  priv->progress_widget = gtk_gizmo_new ("progress", NULL, NULL, NULL, NULL);
   gtk_widget_set_parent (priv->progress_widget, priv->trough_widget);
 
   update_node_classes (pbar);
