7addea1b01762d573413aa1393f023b3456ee08a
Olivier Fourdan <ofourdan@redhat.com>
2019-04-12 15:14:50 +0200

    wayland: Add support for xdg-output
    Previously, the GDK backend for Wayland would deduce the logical size
    of the monitors from the wl_output size and scale.
    
    With the addition of fractional scaling which advertises a larger scale
    value and then scale down the client surface, the computed logical size
    of the monitors in GDK would be wrong and confuse applications which
    insist on using the monitor size and position (like Firefox).
    
    The xdg-output protocol aims at describing outputs in a way which is more
    in line with the concept of an output on desktop oriented systems by
    presenting the outputs using their logical size and position appropriately
    transformed.
    
    Add support for the optional xdg-output protocol so that the size and
    position of the monitors as reported by GDK is correct even when using
    fractional scaling.
    
    Fixes: https://gitlab.gnome.org/GNOME/gtk/issues/1828

--- gdk/wayland/gdkdisplay-wayland.h
+++ gdk/wayland/gdkdisplay-wayland.h
@@ -34,6 +34,7 @@
 #include <gdk/wayland/xdg-foreign-unstable-v1-client-protocol.h>
 #include <gdk/wayland/keyboard-shortcuts-inhibit-unstable-v1-client-protocol.h>
 #include <gdk/wayland/server-decoration-client-protocol.h>
+#include <gdk/wayland/xdg-output-unstable-v1-client-protocol.h>
 
 #include <glib.h>
 #include <gdk/gdkkeys.h>
@@ -93,6 +94,7 @@ struct _GdkWaylandDisplay
   struct zxdg_importer_v1 *xdg_importer;
   struct zwp_keyboard_shortcuts_inhibit_manager_v1 *keyboard_shortcuts_inhibit;
   struct org_kde_kwin_server_decoration_manager *server_decoration_manager;
+  struct zxdg_output_manager_v1 *xdg_output_manager;
 
   GList *async_roundtrips;
 
@@ -141,6 +143,10 @@ struct _GdkWaylandDisplay
   guint have_egl_swap_buffers_with_damage : 1;
   guint have_egl_surfaceless_context : 1;
   EGLint egl_min_swap_interval;
+
+  /* wl/xdg output completion */
+  guint wl_output_done : 1;
+  guint xdg_output_done : 1;
 };
 
 struct _GdkWaylandDisplayClass
