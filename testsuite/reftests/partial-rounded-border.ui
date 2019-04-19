GNOME/gtk

commit c18d2872f292607c921c959e6e97c4d6635bdfd2

GskRenderNode: Fix fallback border drawing

gsk/gskrendernodeimpl.c
@@ -464,56 +464,52 @@ gsk_border_node_draw (GskRenderNode *node,
   else
     {
       const graphene_rect_t *bounds = &self->outline.bounds;
-      /* distance to center "line":
-       * +-------------------------+
-       * |                         |
-       * |                         |
-       * |     ---this-line---     |
-       * |                         |
-       * |                         |
-       * +-------------------------+
-       * That line is equidistant from all sides. It's either horiontal
-       * or vertical, depending on if the rect is wider or taller.
-       * We use the 4 sides spanned up by connecting the line to the corner
-       * points to color the regions of the rectangle differently.
-       * Note that the call to cairo_fill() will add the potential final
-       * segment by closing the path, so we don't have to care.
-       */
-      float dst = MIN (bounds->size.width, bounds->size.height) / 2.0;
 
       cairo_clip (cr);
 
-      /* top */
-      cairo_move_to (cr, bounds->origin.x + dst, bounds->origin.y + dst);
-      cairo_rel_line_to (cr, - dst, - dst);
-      cairo_rel_line_to (cr, bounds->size.width, 0);
-      cairo_rel_line_to (cr, - dst, dst);
-      gdk_cairo_set_source_rgba (cr, &self->border_color[0]);
-      cairo_fill (cr);
+      /* Top */
+      if (self->border_width[0] > 0)
+        {
+          cairo_move_to (cr, bounds->origin.x, bounds->origin.y);
+          cairo_rel_line_to (cr, self->border_width[3], self->border_width[0]);
+          cairo_rel_line_to (cr, bounds->size.width - self->border_width[3] - self->border_width[1], 0);
+          cairo_rel_line_to (cr, self->border_width[1], - self->border_width[0]);
+          gdk_cairo_set_source_rgba (cr, &self->border_color[0]);
+          cairo_fill (cr);
+        }
 
-      /* right */
-      cairo_move_to (cr, bounds->origin.x + bounds->size.width - dst, bounds->origin.y + dst);
-      cairo_rel_line_to (cr, dst, - dst);
-      cairo_rel_line_to (cr, 0, bounds->size.height);
-      cairo_rel_line_to (cr, - dst, - dst);
-      gdk_cairo_set_source_rgba (cr, &self->border_color[1]);
-      cairo_fill (cr);
+      /* Right */
+      if (self->border_width[1] > 0)
+        {
+          cairo_move_to (cr, bounds->origin.x + bounds->size.width, bounds->origin.y);
+          cairo_rel_line_to (cr, - self->border_width[1], self->border_width[0]);
+          cairo_rel_line_to (cr, 0, bounds->size.height - self->border_width[0] - self->border_width[2]);
+          cairo_rel_line_to (cr, self->border_width[1], self->border_width[2]);
+          gdk_cairo_set_source_rgba (cr, &self->border_color[1]);
+          cairo_fill (cr);
+        }
 
-      /* bottom */
-      cairo_move_to (cr, bounds->origin.x + bounds->size.width - dst, bounds->origin.y + bounds->size.height - dst);
-      cairo_rel_line_to (cr, dst, dst);
-      cairo_rel_line_to (cr, - bounds->size.width, 0);
-      cairo_rel_line_to (cr, dst, - dst);
-      gdk_cairo_set_source_rgba (cr, &self->border_color[2]);
-      cairo_fill (cr);
+      /* Bottom */
+      if (self->border_width[2] > 0)
+        {
+          cairo_move_to (cr, bounds->origin.x, bounds->origin.y + bounds->size.height);
+          cairo_rel_line_to (cr, self->border_width[3], - self->border_width[2]);
+          cairo_rel_line_to (cr, bounds->size.width - self->border_width[3] - self->border_width[1], 0);
+          cairo_rel_line_to (cr, self->border_width[1], self->border_width[2]);
+          gdk_cairo_set_source_rgba (cr, &self->border_color[2]);
+          cairo_fill (cr);
+        }
 
-      /* left */
-      cairo_move_to (cr, bounds->origin.x + dst, bounds->origin.y + bounds->size.height - dst);
-      cairo_rel_line_to (cr, - dst, dst);
-      cairo_rel_line_to (cr, 0, - bounds->size.height);
-      cairo_rel_line_to (cr, dst, dst);
-      gdk_cairo_set_source_rgba (cr, &self->border_color[3]);
-      cairo_fill (cr);
+      /* Left */
+      if (self->border_width[3] > 0)
+        {
+          cairo_move_to (cr, bounds->origin.x, bounds->origin.y);
+          cairo_rel_line_to (cr, self->border_width[3], self->border_width[0]);
+          cairo_rel_line_to (cr, 0, bounds->size.height - self->border_width[0] - self->border_width[2]);
+          cairo_rel_line_to (cr, - self->border_width[3], self->border_width[2]);
+          gdk_cairo_set_source_rgba (cr, &self->border_color[3]);
+          cairo_fill (cr);
+        }
     }
 
   cairo_restore (cr);

commit 70fb29e81af3bd6cd26805ef6824170f0f6108e4

reftests: Add a reftest for rendering failure
c18d2872f292607c921c959e6e97c4d6635bdfd2 introduced a misrendering in
the Cairo fallback rendering code while fixing another one.

This test exposes that problem.

Related: !756

testsuite/reftests/meson.build
@@ -325,6 +325,9 @@ testdata = [
   'paned-undersized.css',
   'paned-undersized.ref.ui',
   'paned-undersized.ui',
+  'partial-rounded-border.css',
+  'partial-rounded-border.ref.ui',
+  'partial-rounded-border.ui',
   'picture-load-invalid-file.ui',
   'picture-load-invalid-file.ref.ui',
   'pseudoclass-on-box.css',

testsuite/reftests/partial-rounded-border.css
@@ -1,0 +1,40 @@
+* {
+  all: unset;
+}
+
+.background {
+  background-color: red;
+}
+
+button {
+  padding: 10px;
+}
+
+#topleft {
+  border-top-left-radius: 20px;
+  border-left: 5px solid blue;
+  border-top: 5px solid blue;
+}
+
+#topright {
+  border-top-right-radius: 20px;
+  border-right: 5px solid blue;
+  border-top: 5px solid blue;
+}
+
+#bottomleft {
+  border-bottom-left-radius: 20px;
+  border-left: 5px solid blue;
+  border-bottom: 5px solid blue;
+}
+
+#bottomright {
+  border-bottom-right-radius: 20px;
+  border-right: 5px solid blue;
+  border-bottom: 5px solid blue;
+}
+
+#reference {
+  border: 5px solid blue;
+  border-radius: 20px;
+}

testsuite/reftests/partial-rounded-border.ref.ui
@@ -1,0 +1,47 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<interface>
+  <object class="GtkWindow">
+    <property name="type">popup</property>
+    <child>
+      <object class="GtkGrid">
+        <property name="name">reference</property>
+        <child>
+          <object class="GtkButton">
+            <property name="label">topleft</property>
+            <layout>
+              <property name="left-attach">0</property>
+              <property name="top-attach">0</property>
+            </layout>
+          </object>
+        </child>
+        <child>
+          <object class="GtkButton">
+            <property name="label">topright</property>
+            <layout>
+              <property name="left-attach">1</property>
+              <property name="top-attach">0</property>
+            </layout>
+          </object>
+        </child>
+        <child>
+          <object class="GtkButton">
+            <property name="label">bottomleft</property>
+            <layout>
+              <property name="left-attach">0</property>
+              <property name="top-attach">1</property>
+            </layout>
+          </object>
+        </child>
+        <child>
+          <object class="GtkButton">
+            <property name="label">bottomright</property>
+            <layout>
+              <property name="left-attach">1</property>
+              <property name="top-attach">1</property>
+            </layout>
+          </object>
+        </child>
+      </object>
+    </child>
+  </object>
+</interface>

testsuite/reftests/partial-rounded-border.ui
@@ -1,0 +1,50 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<interface>
+  <object class="GtkWindow">
+    <property name="type">popup</property>
+    <child>
+      <object class="GtkGrid">
+        <child>
+          <object class="GtkButton">
+            <property name="label">topleft</property>
+            <property name="name">topleft</property>
+            <layout>
+              <property name="left-attach">0</property>
+              <property name="top-attach">0</property>
+            </layout>
+          </object>
+        </child>
+        <child>
+          <object class="GtkButton">
+            <property name="label">topright</property>
+            <property name="name">topright</property>
+            <layout>
+              <property name="left-attach">1</property>
+              <property name="top-attach">0</property>
+            </layout>
+          </object>
+        </child>
+        <child>
+          <object class="GtkButton">
+            <property name="label">bottomleft</property>
+            <property name="name">bottomleft</property>
+            <layout>
+              <property name="left-attach">0</property>
+              <property name="top-attach">1</property>
+            </layout>
+          </object>
+        </child>
+        <child>
+          <object class="GtkButton">
+            <property name="label">bottomright</property>
+            <property name="name">bottomright</property>
+            <layout>
+              <property name="left-attach">1</property>
+              <property name="top-attach">1</property>
+            </layout>
+          </object>
+        </child>
+      </object>
+    </child>
+  </object>
+</interface>