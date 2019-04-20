3d3e93213a0fb1b8f08485677546cbe591e6a3f1
Matthias Clasen <mclasen@redhat.com>
2019-04-20 04:31:27 +0000

    x11: Stop using gdk_surface_new_temp
    We can just as well use gdk_surface_new_popup.

--- gdk/x11/gdksurface-x11.c
+++ gdk/x11/gdksurface-x11.c
@@ -4125,10 +4125,11 @@ create_moveresize_surface (MoveResizeData *mv_resize,
                           guint32         timestamp)
 {
   GdkGrabStatus status;
+  GdkRectangle rect = { -100, -100, 1, 1 };
 
   g_assert (mv_resize->moveresize_emulation_surface == NULL);
 
-  mv_resize->moveresize_emulation_surface = gdk_surface_new_temp (mv_resize->display);
+  mv_resize->moveresize_emulation_surface = gdk_surface_new_popup (mv_resize->display, &rect);
   gdk_surface_show (mv_resize->moveresize_emulation_surface);
 
   status = gdk_seat_grab (gdk_device_get_seat (mv_resize->device),
