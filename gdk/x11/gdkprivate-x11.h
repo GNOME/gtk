bd8ce2705d4ee4ade02be8b81ee70641c3fae0a0
Matthias Clasen <mclasen@redhat.com>
2019-04-21 12:24:07 -0400

    gdk: Drop GdkWindowAttr
    All the information in it is already contained
    in the surface object we pass along, and none
    of the backend implementations were using the
    attributes at all.

--- gdk/x11/gdkprivate-x11.h
+++ gdk/x11/gdkprivate-x11.h
@@ -178,8 +178,7 @@ void       _gdk_x11_display_get_maximal_cursor_size (GdkDisplay *display,
                                                      guint      *height);
 void       _gdk_x11_display_create_surface_impl     (GdkDisplay    *display,
                                                      GdkSurface     *window,
-                                                     GdkSurface     *real_parent,
-                                                     GdkSurfaceAttr *attributes);
+                                                     GdkSurface     *real_parent);
 GList *    gdk_x11_display_get_toplevel_windows     (GdkDisplay *display);
 
 void _gdk_x11_precache_atoms (GdkDisplay          *display,
