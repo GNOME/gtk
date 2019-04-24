34fcfb154ab2e0cd1d51120a9b11a83b4868c9ad
Timm Bäder <mail@baedert.org>
2019-04-24 12:56:23 +0200

    gl renderer: Properly apply opacity in cross-fade shader

--- gsk/resources/glsl/cross_fade.fs.glsl
+++ gsk/resources/glsl/cross_fade.fs.glsl
@@ -6,7 +6,8 @@ void main() {
   vec4 source1 = Texture(u_source, vUv);  // start child
   vec4 source2 = Texture(u_source2, vUv); // end child
 
-  float p = u_progress;
-  vec4 color = ((1.0 - p) * source1) + (p * source2);
+  float p_start = (1.0 - u_progress) * u_alpha;
+  float p_end = u_progress * u_alpha;
+  vec4 color = (p_start * source1) + (p_end * source2);
   setOutputColor(color);
 }
