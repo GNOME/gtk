f42175b55f4c98f3b3427cd6756f63f5f6a3523b
Matthias Clasen <mclasen@redhat.com>
2019-04-26 23:48:13 +0000

    window: Set cursor on the right surface
    This fixes cursors for entries in popovers.

--- gtk/gtkwindow.c
+++ gtk/gtkwindow.c
@@ -10078,6 +10078,9 @@ update_cursor (GtkRoot   *toplevel,
                GtkWidget *target)
 {
   GdkCursor *cursor = NULL;
+  GdkSurface *surface;
+
+  surface = gtk_widget_get_surface (target);
 
   if (grab_widget && !gtk_widget_is_ancestor (target, grab_widget))
     {
@@ -10105,8 +10108,7 @@ update_cursor (GtkRoot   *toplevel,
         }
     }
 
-  gdk_surface_set_device_cursor (gtk_widget_get_surface (GTK_WIDGET (toplevel)),
-                                 device, cursor);
+  gdk_surface_set_device_cursor (surface, device, cursor);
 }
 
 void
