44621354be60ab2c016b080fdd8aaaffb7903221
Matthias Clasen <mclasen@redhat.com>
2019-04-27 03:20:41 +0000

    Stop using gtk_widget_grab_default
    Replace gtk_widget_grab_default by
    gtk_window_set_default.

--- gtk/gtkwindow.c
+++ gtk/gtkwindow.c
@@ -2709,9 +2709,7 @@ gtk_window_set_startup_id (GtkWindow   *window,
  *
  * The default widget is the widget that’s activated when the user
  * presses Enter in a dialog (for example). This function sets or
- * unsets the default widget for a #GtkWindow. When setting (rather
- * than unsetting) the default widget it’s generally easier to call
- * gtk_widget_grab_default() on the widget. Before making a widget
+ * unsets the default widget for a #GtkWindow. Before making a widget
  * the default widget, you must call gtk_widget_set_can_default() on
  * the widget you’d like to make the default.
  */
