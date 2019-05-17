da8f83fee6c580c0723299a90825d3ce7f42b27a
Matthias Clasen <mclasen@redhat.com>
2019-05-17 21:08:38 +0000

    Remove gtk_widget_get/set_has_surface
    These serve no purpose anymore - widgets don't
    have surfaces, unless they're a GtkNative.

--- gtk/gtkfontchooserwidget.c
+++ gtk/gtkfontchooserwidget.c
@@ -904,8 +904,6 @@ gtk_font_chooser_widget_init (GtkFontChooserWidget *fontchooser)
   fontchooser->priv = gtk_font_chooser_widget_get_instance_private (fontchooser);
   priv = fontchooser->priv;
 
-  gtk_widget_set_has_surface (GTK_WIDGET (fontchooser), FALSE);
-
   gtk_widget_init_template (GTK_WIDGET (fontchooser));
 
   priv->axes = g_hash_table_new_full (axis_hash, axis_equal, NULL, axis_free);
