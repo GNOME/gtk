05c27c520a5aba9163070b2301b84255e9cb2e11
Matthias Clasen <mclasen@redhat.com>
2019-05-17 21:08:38 +0000

    Remove gtk_widget_get/set_has_surface
    These serve no purpose anymore - widgets don't
    have surfaces, unless they're a GtkNative.

--- gtk/gtkglarea.c
+++ gtk/gtkglarea.c
@@ -957,8 +957,6 @@ gtk_gl_area_init (GtkGLArea *area)
 {
   GtkGLAreaPrivate *priv = gtk_gl_area_get_instance_private (area);
 
-  gtk_widget_set_has_surface (GTK_WIDGET (area), FALSE);
-
   priv->auto_render = TRUE;
   priv->needs_render = TRUE;
   priv->required_gl_version = 0;
