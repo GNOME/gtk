c7aea6da6881e9ee3406c205a425c1790080bb04
Matthias Clasen <mclasen@redhat.com>
2019-05-19 03:41:08 +0000

    Export gtk_widget_render privately
    We need it in gtkwindow.c and gtkpopover.c.

--- gtk/gtkwidget.c
+++ gtk/gtkwidget.c
@@ -13011,7 +13011,7 @@ gtk_widget_snapshot (GtkWidget   *widget,
     gtk_snapshot_append_node (snapshot, priv->render_node);
 }
 
-static void
+void
 gtk_widget_render (GtkWidget            *widget,
                    GdkSurface           *surface,
                    const cairo_region_t *region)
