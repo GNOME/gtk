6b9eb6d33c7e5f8dd597d5f31350ceb9f9f4299d
Matthias Clasen <mclasen@redhat.com>
2019-05-20 13:19:12 +0000

    surface: Don't dispose the frame clock prematurely
    Since we are now sharing frame clocks with multiple
    surfaces, we can no longer dispose them unconditionally
    when a surface goes away. Only do it if we are a
    toplevel (without parent).
    
    This was showing up as criticals on exit when opening
    and closing any popover in widget factory.

--- gdk/gdksurface.c
+++ gdk/gdksurface.c
@@ -899,7 +899,8 @@ _gdk_surface_destroy_hierarchy (GdkSurface *surface,
 
   if (surface->frame_clock)
     {
-      g_object_run_dispose (G_OBJECT (surface->frame_clock));
+      if (surface->parent == NULL)
+        g_object_run_dispose (G_OBJECT (surface->frame_clock));
       gdk_surface_set_frame_clock (surface, NULL);
     }
 
