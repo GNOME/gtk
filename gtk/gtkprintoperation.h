cc1c1df405e16b907d6b18c0b8863310ce65a1ad
Matthias Clasen <mclasen@redhat.com>
2019-05-26 22:24:37 +0000

    print operation: Use standard class padding

--- gtk/gtkprintoperation.h
+++ gtk/gtkprintoperation.h
@@ -186,15 +186,7 @@ struct _GtkPrintOperationClass
 
   /*< private >*/
 
-  /* Padding for future expansion */
-  void (*_gtk_reserved1) (void);
-  void (*_gtk_reserved2) (void);
-  void (*_gtk_reserved3) (void);
-  void (*_gtk_reserved4) (void);
-  void (*_gtk_reserved5) (void);
-  void (*_gtk_reserved6) (void);
-  void (*_gtk_reserved7) (void);
-  void (*_gtk_reserved8) (void);
+  gpointer padding[8];
 };
 
 /**
