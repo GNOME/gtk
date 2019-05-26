e9bc767691d320b6842e52c1ef263d645643ac6c
Matthias Clasen <mclasen@redhat.com>
2019-05-26 15:18:42 -0400

    viewport: Cleanup

--- gtk/gtkviewport.h
+++ gtk/gtkviewport.h
@@ -38,10 +38,7 @@ G_BEGIN_DECLS
 
 #define GTK_TYPE_VIEWPORT            (gtk_viewport_get_type ())
 #define GTK_VIEWPORT(obj)            (G_TYPE_CHECK_INSTANCE_CAST ((obj), GTK_TYPE_VIEWPORT, GtkViewport))
-#define GTK_VIEWPORT_CLASS(klass)    (G_TYPE_CHECK_CLASS_CAST ((klass), GTK_TYPE_VIEWPORT, GtkViewportClass))
 #define GTK_IS_VIEWPORT(obj)         (G_TYPE_CHECK_INSTANCE_TYPE ((obj), GTK_TYPE_VIEWPORT))
-#define GTK_IS_VIEWPORT_CLASS(klass) (G_TYPE_CHECK_CLASS_TYPE ((klass), GTK_TYPE_VIEWPORT))
-#define GTK_VIEWPORT_GET_CLASS(obj)  (G_TYPE_INSTANCE_GET_CLASS ((obj), GTK_TYPE_VIEWPORT, GtkViewportClass))
 
 
 typedef struct _GtkViewport              GtkViewport;
