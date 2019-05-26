c2fd6b9fa45f39e43968c1481dd92fa7bcc17b0b
Matthias Clasen <mclasen@redhat.com>
2019-05-26 15:25:46 -0400

    textview: Use standard padding
    Given that GtkSourceView exists, we will have
    to keep GtkTextView subclassable until a replacement
    appears.

--- gtk/gtktextview.h
+++ gtk/gtktextview.h
@@ -189,11 +189,7 @@ struct _GtkTextViewClass
 
   /*< private >*/
 
-  /* Padding for future expansion */
-  void (*_gtk_reserved1) (void);
-  void (*_gtk_reserved2) (void);
-  void (*_gtk_reserved3) (void);
-  void (*_gtk_reserved4) (void);
+  gpointer padding[8];
 };
 
 GDK_AVAILABLE_IN_ALL
