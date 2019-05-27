f111e3f46755b8f023c5c637ebaa0764cde5da55
Matthias Clasen <mclasen@redhat.com>
2019-05-27 22:31:10 +0000

    text mark: Use standard padding

--- gtk/gtktextmark.h
+++ gtk/gtktextmark.h
@@ -78,11 +78,9 @@ struct _GtkTextMarkClass
 {
   GObjectClass parent_class;
 
-  /* Padding for future expansion */
-  void (*_gtk_reserved1) (void);
-  void (*_gtk_reserved2) (void);
-  void (*_gtk_reserved3) (void);
-  void (*_gtk_reserved4) (void);
+  /*< private >*/
+
+  gpointer padding[8];
 };
 
 GDK_AVAILABLE_IN_ALL
