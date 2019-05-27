bf2ff5f582e6eb3002b7e651f896eeb30a9abf3a
Matthias Clasen <mclasen@redhat.com>
2019-05-26 23:30:21 -0400

    grid: Use standard padding

--- gtk/gtkgrid.h
+++ gtk/gtkgrid.h
@@ -56,14 +56,7 @@ struct _GtkGridClass
 
   /*< private >*/
 
-  void (*_gtk_reserved1) (void);
-  void (*_gtk_reserved2) (void);
-  void (*_gtk_reserved3) (void);
-  void (*_gtk_reserved4) (void);
-  void (*_gtk_reserved5) (void);
-  void (*_gtk_reserved6) (void);
-  void (*_gtk_reserved7) (void);
-  void (*_gtk_reserved8) (void);
+  gpointer padding[8];
 };
 
 GDK_AVAILABLE_IN_ALL
