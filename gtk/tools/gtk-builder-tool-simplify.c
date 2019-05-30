6b5ba72f316734854c9b96910d1e7d74f8ada48e
Lubomir Rintel <lkundrak@v3.sk>
2019-05-30 09:44:44 +0200

    builder-tool: Rewrite GtkDialog
    Changes
    
      <object class="GtkDialog">
        <child internal-child="vbox">
          <...>
            <child internal-child="action_area">...</child>
            ...
          </...>
          <packing />
        </child>
      <object>
    
    to
    
      <object class="GtkDialog">
        <child internal-child="content_area">
          <...>
            ...
          </...>
        </child>
        <child internal-child="action_area">...</child>
      <object>

--- gtk/tools/gtk-builder-tool-simplify.c
+++ gtk/tools/gtk-builder-tool-simplify.c
@@ -1258,6 +1258,66 @@ rewrite_paned (Element *element,
 }
 
 static void
+rewrite_dialog (Element *element,
+               MyParserData *data)
+{
+  Element *content_area = NULL;
+  Element *vbox = NULL;
+  Element *action_area = NULL;
+  GList *l;
+
+  for (l = element->children; l; l = l->next)
+    {
+      Element *elt = l->data;
+
+      if (g_str_equal (elt->element_name, "child") &&
+          g_strcmp0 (get_attribute_value (elt, "internal-child"), "vbox") == 0)
+        {
+          content_area = elt;
+          break;
+        }
+    }
+
+  if (!content_area || !content_area->children)
+    return;
+
+  vbox = content_area->children->data;
+
+  for (l = vbox->children; l; l = l->next)
+    {
+      Element *elt = l->data;
+
+      if (g_str_equal (elt->element_name, "child") &&
+          g_strcmp0 (get_attribute_value (elt, "internal-child"), "action_area") == 0)
+        {
+          action_area = elt;
+          break;
+        }
+    }
+
+  if (!action_area)
+    return;
+
+  set_attribute_value (content_area, "internal-child", "content_area");
+  vbox->children = g_list_remove (vbox->children, action_area);
+  action_area->parent = element;
+  element->children = g_list_append (element->children, action_area);
+
+  for (l = action_area->children; l; l = l->next)
+    {
+      Element *elt = l->data;
+
+      if (g_str_equal (elt->element_name, "packing"))
+        {
+          action_area->children = g_list_remove (action_area->children, elt);
+          free_element (elt);
+          break;
+        }
+    }
+
+}
+
+static void
 rewrite_layout_props (Element *element,
                       MyParserData *data)
 {
@@ -1478,6 +1538,10 @@ rewrite_element (Element      *element,
   if (g_str_equal (element->element_name, "object") &&
       g_str_equal (get_class_name (element), "GtkPaned"))
     rewrite_paned (element, data);
+
+  if (g_str_equal (element->element_name, "object") &&
+      g_str_equal (get_class_name (element), "GtkDialog"))
+    rewrite_dialog (element, data);
 
   if (g_str_equal (element->element_name, "object") &&
       g_str_equal (get_class_name (element), "GtkOverlay"))
