0fcd8bcf2a10f1d62861baed426d440b166a7f0f
Benjamin Otte <otte@redhat.com>
2019-06-09 22:50:46 +0200

    listitemfactory: Sanitize APIs
    Make sure the APIs follow a predictable path:
    
    setup
      bind
        rebind/update (0-N times)
      unbind
    teardown
    
    This is the first step towards providing multiple different factories.

--- gtk/gtklistitemfactory.c
+++ gtk/gtklistitemfactory.c
@@ -23,21 +23,6 @@
 
 #include "gtklistitemprivate.h"
 
-struct _GtkListItemFactory
-{
-  GObject parent_instance;
-
-  GtkListItemSetupFunc setup_func;
-  GtkListItemBindFunc bind_func;
-  gpointer user_data;
-  GDestroyNotify user_destroy;
-};
-
-struct _GtkListItemFactoryClass
-{
-  GObjectClass parent_class;
-};
-
 G_DEFINE_TYPE (GtkListItemFactory, gtk_list_item_factory, G_TYPE_OBJECT)
 
 static void
@@ -87,7 +72,7 @@ gtk_list_item_factory_new (GtkListItemSetupFunc setup_func,
 
 void
 gtk_list_item_factory_setup (GtkListItemFactory *self,
-                             GtkListItem         *list_item)
+                             GtkListItem        *list_item)
 {
   g_return_if_fail (GTK_IS_LIST_ITEM_FACTORY (self));
 
@@ -101,6 +86,28 @@ gtk_list_item_factory_bind (GtkListItemFactory *self,
                             guint               position,
                             gpointer            item,
                             gboolean            selected)
+{
+  g_return_if_fail (GTK_IS_LIST_ITEM_FACTORY (self));
+  g_return_if_fail (GTK_IS_LIST_ITEM (list_item));
+
+  g_object_freeze_notify (G_OBJECT (list_item));
+
+  gtk_list_item_set_item (list_item, item);
+  gtk_list_item_set_position (list_item, position);
+  gtk_list_item_set_selected (list_item, selected);
+
+  if (self->bind_func)  
+    self->bind_func (list_item, self->user_data);
+
+  g_object_thaw_notify (G_OBJECT (list_item));
+}
+
+void
+gtk_list_item_factory_rebind (GtkListItemFactory *self,
+                              GtkListItem        *list_item,
+                              guint               position,
+                              gpointer            item,
+                              gboolean            selected)
 {
   g_return_if_fail (GTK_IS_LIST_ITEM_FACTORY (self));
   g_return_if_fail (GTK_IS_LIST_ITEM (list_item));
@@ -149,3 +156,19 @@ gtk_list_item_factory_unbind (GtkListItemFactory *self,
 
   g_object_thaw_notify (G_OBJECT (list_item));
 }
+
+void
+gtk_list_item_factory_teardown (GtkListItemFactory *self,
+                                GtkListItem        *list_item)
+{
+  GtkWidget *child;
+
+  g_return_if_fail (GTK_IS_LIST_ITEM_FACTORY (self));
+
+  child = gtk_bin_get_child (GTK_BIN (list_item));
+  if (child)
+    gtk_container_remove (GTK_CONTAINER (list_item), child);
+
+  gtk_list_item_set_selectable (list_item, TRUE);
+}
+
