66e81438cb0252a940d2c260257b44dc5390ccce
Matthias Clasen <mclasen@redhat.com>
2019-06-14 12:12:10 +0000

    Allow registering actions per-class
    Add a facility to register and install actions
    at class init time. We avoid creating an action
    group for these by teaching the action muxer
    about these actions.

--- gtk/gtkactionmuxerprivate.h
+++ gtk/gtkactionmuxerprivate.h
@@ -31,10 +31,20 @@ G_BEGIN_DECLS
 #define GTK_IS_ACTION_MUXER(inst)                           (G_TYPE_CHECK_INSTANCE_TYPE ((inst),                     \
                                                              GTK_TYPE_ACTION_MUXER))
 
+typedef struct {
+  char *name;
+
+  GtkWidgetActionActivateFunc    activate;
+  GtkWidgetActionQueryFunc       query;
+  GtkWidgetActionChangeStateFunc change_state;
+  GtkWidgetActionQueryStateFunc  query_state;
+} GtkWidgetAction;
+
 typedef struct _GtkActionMuxer                              GtkActionMuxer;
 
 GType                   gtk_action_muxer_get_type                       (void);
-GtkActionMuxer *        gtk_action_muxer_new                            (GtkWidget      *widget);
+GtkActionMuxer *        gtk_action_muxer_new                            (GtkWidget      *widget,
+                                                                         GPtrArray      *actions);
 
 void                    gtk_action_muxer_insert                         (GtkActionMuxer *muxer,
                                                                          const gchar    *prefix,
@@ -58,6 +68,16 @@ void                    gtk_action_muxer_set_primary_accel              (GtkActi
 
 const gchar *           gtk_action_muxer_get_primary_accel              (GtkActionMuxer *muxer,
                                                                          const gchar    *action_and_target);
+
+void
+gtk_action_muxer_action_enabled_changed (GtkActionMuxer *muxer,
+                                         const char     *action_name,
+                                         gboolean        enabled);
+void
+gtk_action_muxer_action_state_changed (GtkActionMuxer *muxer,
+                                       const gchar    *action_name,
+                                       GVariant       *state);
+
 
 /* No better place for these... */
 gchar *                 gtk_print_action_and_target                     (const gchar    *action_namespace,
