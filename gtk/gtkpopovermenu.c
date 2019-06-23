11233b4e4b8672d4cce7c7f35c8338bf57f05a06
Matthias Clasen <mclasen@redhat.com>
2019-06-23 17:20:38 +0000

    popover: Switch activate-default to an action
    This matches what we already did for GtkWindow.

--- gtk/gtkpopovermenu.c
+++ gtk/gtkpopovermenu.c
@@ -445,16 +445,16 @@ gtk_popover_menu_class_init (GtkPopoverMenuClass *klass)
   add_tab_bindings (widget_class, GDK_SHIFT_MASK, GTK_DIR_TAB_BACKWARD);
   add_tab_bindings (widget_class, GDK_CONTROL_MASK | GDK_SHIFT_MASK, GTK_DIR_TAB_BACKWARD);
 
-  gtk_widget_class_add_binding_signal (widget_class, GDK_KEY_Return, 0,
-                                "activate-default", NULL);
-  gtk_widget_class_add_binding_signal (widget_class, GDK_KEY_ISO_Enter, 0,
-                                "activate-default", NULL);
-  gtk_widget_class_add_binding_signal (widget_class, GDK_KEY_KP_Enter, 0,
-                                "activate-default", NULL);
-  gtk_widget_class_add_binding_signal (widget_class, GDK_KEY_space, 0,
-                                "activate-default", NULL);
-  gtk_widget_class_add_binding_signal (widget_class, GDK_KEY_KP_Space, 0,
-                                "activate-default", NULL);
+  gtk_widget_class_bind_action (widget_class, GDK_KEY_Return, 0,
+                                "default.activate", NULL);
+  gtk_widget_class_bind_action (widget_class, GDK_KEY_ISO_Enter, 0,
+                                "default.activate", NULL);
+  gtk_widget_class_bind_action (widget_class, GDK_KEY_KP_Enter, 0,
+                                "default.activate", NULL);
+  gtk_widget_class_bind_action (widget_class, GDK_KEY_space, 0,
+                                "default.activate", NULL);
+  gtk_widget_class_bind_action (widget_class, GDK_KEY_KP_Space, 0,
+                                "default.activate", NULL);
 }
 
 /**
