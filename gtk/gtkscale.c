GNOME/gtk

commit 77769a52b3cc0b836b226457963d7b5fe92ee115

adwaita: Add some spacing to the scale value
So the slider does not overlap the value label. Since the value label is
allocated at the widget edge in gtk3, the correct fix here would
probably be to remove the 12px padding we apply to the entire scale and
instead apply it only to the trough.

gtk/gtkscale.c
@@ -124,7 +124,8 @@
  * added depending on what marks are present.
  *
  * If the scale is displaying the value (see #GtkScale:draw-value), there is
- * subnode with name value.
+ * subnode with name value. This node will get the .top or .bottom style classes
+ * similar to the marks node.
  */
 
 

gtk/theme/Adwaita/_common.scss
@@ -3055,6 +3055,9 @@ scale {
       }
     }
 
+    >value.top    { margin-bottom: 9px; }
+    >value.bottom { margin-top: 9px; }
+
     &.fine-tune indicator { min-height: ($_marks_length - 3px); }
   }
 
@@ -3070,6 +3073,9 @@ scale {
         min-width: $_marks_length;
       }
     }
+
+    >value.top    { margin-bottom: 9px; }
+    >value.bottom { margin-top: 9px; }
 
     &.fine-tune indicator { min-width: ($_marks_length - 3px); }
   }

gtk/theme/Adwaita/gtk-contained-dark.css
@@ -1281,6 +1281,10 @@ scale.horizontal > marks.bottom { margin-top: 6px; }
 
 scale.horizontal > marks indicator { background-color: currentColor; min-height: 6px; min-width: 1px; }
 
+scale.horizontal > value.top { margin-bottom: 9px; }
+
+scale.horizontal > value.bottom { margin-top: 9px; }
+
 scale.horizontal.fine-tune indicator { min-height: 3px; }
 
 scale.vertical > marks { color: alpha(currentColor,0.55); }
@@ -1290,6 +1294,10 @@ scale.vertical > marks.top { margin-right: 6px; }
 scale.vertical > marks.bottom { margin-left: 6px; }
 
 scale.vertical > marks indicator { background-color: currentColor; min-height: 1px; min-width: 6px; }
+
+scale.vertical > value.top { margin-bottom: 9px; }
+
+scale.vertical > value.bottom { margin-top: 9px; }
 
 scale.vertical.fine-tune indicator { min-width: 3px; }
 

gtk/theme/Adwaita/gtk-contained.css
@@ -1301,6 +1301,10 @@ scale.horizontal > marks.bottom { margin-top: 6px; }
 
 scale.horizontal > marks indicator { background-color: currentColor; min-height: 6px; min-width: 1px; }
 
+scale.horizontal > value.top { margin-bottom: 9px; }
+
+scale.horizontal > value.bottom { margin-top: 9px; }
+
 scale.horizontal.fine-tune indicator { min-height: 3px; }
 
 scale.vertical > marks { color: alpha(currentColor,0.55); }
@@ -1310,6 +1314,10 @@ scale.vertical > marks.top { margin-right: 6px; }
 scale.vertical > marks.bottom { margin-left: 6px; }
 
 scale.vertical > marks indicator { background-color: currentColor; min-height: 1px; min-width: 6px; }
+
+scale.vertical > value.top { margin-bottom: 9px; }
+
+scale.vertical > value.bottom { margin-top: 9px; }
 
 scale.vertical.fine-tune indicator { min-width: 3px; }
 

commit af200d2e2378289f025aff4f6076fc39b9fbecdd

scale: Use top/left/bottom/right style classes on value label
Just top/bottom is not enough anymore.

77769a52b3cc0b836b226457963d7b5fe92ee115 broke e.g. horizontal scales
with the value on top/bottom, adding too much space.

gtk/gtkscale.c
@@ -85,7 +85,7 @@
  * │   ┊    ╰── indicator
  * ┊   ┊
  * │   ╰── mark
- * ├── [value][.top][.bottom]
+ * ├── [value][.top][.right][.bottom][.left]
  * ├── trough
  * │   ├── [fill]
  * │   ├── [highlight]
@@ -1049,15 +1049,19 @@ update_value_position (GtkScale *scale)
 
   context = gtk_widget_get_style_context (priv->value_widget);
 
-  if (priv->value_pos == GTK_POS_TOP || priv->value_pos == GTK_POS_LEFT)
+  gtk_style_context_remove_class (context, GTK_STYLE_CLASS_TOP);
+  gtk_style_context_remove_class (context, GTK_STYLE_CLASS_RIGHT);
+  gtk_style_context_remove_class (context, GTK_STYLE_CLASS_BOTTOM);
+  gtk_style_context_remove_class (context, GTK_STYLE_CLASS_LEFT);
+
+  switch (priv->value_pos)
     {
-      gtk_style_context_remove_class (context, GTK_STYLE_CLASS_BOTTOM);
-      gtk_style_context_add_class (context, GTK_STYLE_CLASS_TOP);
-    }
-  else
-    {
-      gtk_style_context_remove_class (context, GTK_STYLE_CLASS_TOP);
-      gtk_style_context_add_class (context, GTK_STYLE_CLASS_BOTTOM);
+    case GTK_POS_TOP:    gtk_style_context_add_class (context, GTK_STYLE_CLASS_TOP); break;
+    case GTK_POS_RIGHT:  gtk_style_context_add_class (context, GTK_STYLE_CLASS_RIGHT); break;
+    case GTK_POS_BOTTOM: gtk_style_context_add_class (context, GTK_STYLE_CLASS_BOTTOM); break;
+    case GTK_POS_LEFT:   gtk_style_context_add_class (context, GTK_STYLE_CLASS_LEFT); break;
+
+    default: g_assert_not_reached ();
     }
 }
 

gtk/theme/Adwaita/_common.scss
@@ -3197,8 +3197,8 @@ scale {
       }
     }
 
-    >value.top    { margin-bottom: 9px; }
-    >value.bottom { margin-top: 9px; }
+    > value.left  { margin-right: 9px; }
+    > value.right { margin-left: 9px; }
 
     &.fine-tune >marks {
       &.top { margin-top: 3px; }
@@ -3221,8 +3221,8 @@ scale {
       }
     }
 
-    >value.top    { margin-bottom: 9px; }
-    >value.bottom { margin-top: 9px; }
+    > value.top    { margin-bottom: 9px; }
+    > value.bottom { margin-top: 9px; }
 
     &.fine-tune >marks {
       &.top { margin-left: 3px; }