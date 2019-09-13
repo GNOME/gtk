382fa653de05b09ac03bee79a251cd97959b780c
Timm Bäder <mail@baedert.org>
2019-09-13 05:53:17 +0200

    pixbuf: Add a missing return value transfer annotation
    And add a (nullable)

--- gdk/gdkpixbuf-drawable.c
+++ gdk/gdkpixbuf-drawable.c
@@ -232,7 +232,8 @@ gdk_pixbuf_get_from_surface  (cairo_surface_t *surface,
  * in newly written code as later stages will almost certainly convert
  * the pixbuf back into a texture to draw it on screen.
  *
- * returns: a new #GdkPixbuf
+ * Returns: (transfer full) (nullable): a new #GdkPixbuf or %NULL
+ *   in case of an error
  */
 GdkPixbuf *
 gdk_pixbuf_get_from_texture (GdkTexture *texture)
