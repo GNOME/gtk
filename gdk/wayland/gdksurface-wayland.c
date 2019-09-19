f1425905d83da782a0688bc9471be68424a4f049
William Wold <wm@wmww.sh>
2019-09-19 20:45:40 +0000

    Always show Wayland surfaces (fixes #2155)
    In some conditions, a Wayland popup needs to be unmapped and remapped
    (see gdk_wayland_surface_maybe_resize for details). This was broken in
    5db079b052fcb934566f69ea49dd7fc3f6314813. The problem was that
    surface's update_area was getting set while it was unmapped (during the
    resize) and then the surface wasn't getting repainted. This fixes that.
    Both calls are inexpensive if they're not needed.

--- gdk/wayland/gdksurface-wayland.c
+++ gdk/wayland/gdksurface-wayland.c
@@ -2438,6 +2438,8 @@ gdk_wayland_surface_map (GdkSurface *surface)
   if (impl->mapped)
     return;
 
+  _gdk_surface_clear_update_area (surface);
+
   if (should_map_as_popup (surface))
     {
       gboolean create_fallback = FALSE;
@@ -2527,6 +2529,8 @@ gdk_wayland_surface_map (GdkSurface *surface)
     {
       gdk_wayland_surface_create_xdg_toplevel (surface);
     }
+
+  gdk_surface_queue_expose (surface);
 
   impl->mapped = TRUE;
 }
