efa32d118b3f00ca6f176ec107efd221dca63588
Benjamin Otte <otte@redhat.com>
2019-10-02 05:38:11 +0200

    Add GtkDirectoryList
    Adds a new listmodel called GtkDirectoryList that lists the children of
    a GFile as GFileInfos.
    
    This is supposed to be used by the filechooser.

--- gtk/gtk.h
+++ gtk/gtk.h
@@ -90,6 +90,7 @@
 #include <gtk/gtkcustomlayout.h>
 #include <gtk/gtkdebug.h>
 #include <gtk/gtkdialog.h>
+#include <gtk/gtkdirectorylist.h>
 #include <gtk/gtkdragicon.h>
 #include <gtk/gtkdragsource.h>
 #include <gtk/gtkdrawingarea.h>
