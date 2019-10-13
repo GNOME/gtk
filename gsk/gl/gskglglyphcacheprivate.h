a758a1bafead44dd83a8008ba899d5451d7951b5
Matthias Clasen <mclasen@redhat.com>
2019-10-12 20:09:14 -0400

    glyph cache: Go back to memcmp
    Be careful to avoid padding data, and only
    compare the relevant parts, leaving out the
    hash key.

--- gsk/gl/gskglglyphcacheprivate.h
+++ gsk/gl/gskglglyphcacheprivate.h
@@ -19,15 +19,24 @@ typedef struct
   int atlas_timestamp; /* incremented whenever an atlas is dropped */
 } GskGLGlyphCache;
 
-typedef struct
+struct _CacheKeyData
 {
   PangoFont *font;
   PangoGlyph glyph;
   guint xshift : 3;
   guint yshift : 3;
   guint scale  : 26; /* times 1024 */
+};
+
+typedef struct _CacheKeyData CacheKeyData;
+
+struct _GlyphCacheKey
+{
+  CacheKeyData data;
   guint hash;
-} GlyphCacheKey;
+};
+
+typedef struct _GlyphCacheKey GlyphCacheKey;
 
 #define PHASE(x) ((int)(floor (4 * (x + 0.125)) - 4 * floor (x + 0.125)))
 
@@ -37,14 +46,14 @@ glyph_cache_key_set_glyph_and_shift (GlyphCacheKey *key,
                                      float x,
                                      float y)
 {
-  key->glyph = glyph;
-  key->xshift = PHASE (x);
-  key->yshift = PHASE (y);
-  key->hash = GPOINTER_TO_UINT (key->font) ^
-              key->glyph ^
-              (key->xshift << 24) ^
-              (key->yshift << 26) ^
-              key->scale;
+  key->data.glyph = glyph;
+  key->data.xshift = PHASE (x);
+  key->data.yshift = PHASE (y);
+  key->hash = GPOINTER_TO_UINT (key->data.font) ^
+              key->data.glyph ^
+              (key->data.xshift << 24) ^
+              (key->data.yshift << 26) ^
+              key->data.scale;
 }
 
 typedef struct _GskGLCachedGlyph GskGLCachedGlyph;
