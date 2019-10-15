ca8819c92c7255036f57f3e7efdc5d8776c757b0
Benjamin Otte <otte@redhat.com>
2019-10-15 23:06:36 +0200

    listview: Implement activation
    - a GtkListview::activate signal
    - a GtkListItem::activatable property
    - activate list items on double clicks and <Enter> presses

--- gtk/gtklistitem.h
+++ gtk/gtklistitem.h
@@ -44,15 +44,19 @@ GType           gtk_list_item_get_type                          (void) G_GNUC_CO
 GDK_AVAILABLE_IN_ALL
 gpointer        gtk_list_item_get_item                          (GtkListItem            *self);
 GDK_AVAILABLE_IN_ALL
-guint           gtk_list_item_get_position                      (GtkListItem            *self);
+guint           gtk_list_item_get_position                      (GtkListItem            *self) G_GNUC_PURE;
 GDK_AVAILABLE_IN_ALL
-gboolean        gtk_list_item_get_selected                      (GtkListItem            *self);
+gboolean        gtk_list_item_get_selected                      (GtkListItem            *self) G_GNUC_PURE;
 GDK_AVAILABLE_IN_ALL
-gboolean        gtk_list_item_get_selectable                    (GtkListItem            *self);
+gboolean        gtk_list_item_get_selectable                    (GtkListItem            *self) G_GNUC_PURE;
 GDK_AVAILABLE_IN_ALL
 void            gtk_list_item_set_selectable                    (GtkListItem            *self,
                                                                  gboolean                selectable);
-
+GDK_AVAILABLE_IN_ALL
+gboolean        gtk_list_item_get_activatable                   (GtkListItem            *self) G_GNUC_PURE;
+GDK_AVAILABLE_IN_ALL
+void            gtk_list_item_set_activatable                   (GtkListItem            *self,
+                                                                 gboolean                activatable);
 
 GDK_AVAILABLE_IN_ALL
 void            gtk_list_item_set_child                         (GtkListItem            *self,
