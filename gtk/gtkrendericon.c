GNOME/gtk

commit d1ea591f18f71e059a76af8d659540c10293270a

rendericon: Ignore transparent icons
If the recoloring would end up multiplying the alpha component with 0
anyway, just skip drawing anything altogether.

This increases the icon count in the switch demo of the fishbowl from
~260 to ~280 on my system.

gtk/gtkrendericon.c
@@ -133,6 +133,9 @@ gtk_css_style_snapshot_icon_paintable (GtkCssStyle  *style,
 
       gtk_icon_theme_lookup_symbolic_colors (style, &fg, &sc, &wc, &ec);
 
+      if (fg.alpha == 0.0f)
+        goto transparent;
+
       graphene_matrix_init_from_float (&color_matrix, (float[16]) {
                                          sc.red - fg.red, sc.green - fg.green, sc.blue - fg.blue, 0,
                                          wc.red - fg.red, wc.green - fg.green, wc.blue - fg.blue, 0,
@@ -171,5 +174,6 @@ gtk_css_style_snapshot_icon_paintable (GtkCssStyle  *style,
   if (has_shadow)
     gtk_snapshot_pop (snapshot);
 
+transparent:
   gtk_css_filter_value_pop_snapshot (filter_value, snapshot);
 }

commit 9b87cace47a3ab01a4b8d99b53fe501fa1e54d63

render: Fix goto
if we have pushed a shadow, we better pop it again.

Broken since d1ea591f18f

gtk/gtkrendericon.c
@@ -166,10 +166,10 @@ gtk_css_style_snapshot_icon_paintable (GtkCssStyle  *style,
   if (recolor)
     gtk_snapshot_pop (snapshot);
 
+transparent:
   if (has_shadow)
     gtk_snapshot_pop (snapshot);
 
-transparent:
   gtk_css_filter_value_pop_snapshot (filter_value, snapshot);
 
   gsk_transform_unref (transform);