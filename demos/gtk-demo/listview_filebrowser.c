13bc2341a3b023b0b6fb1e9756a165fba4f7a8a2
Benjamin Otte <otte@redhat.com>
2019-10-18 20:47:48 +0200

    demo: Add a file browser demo

---
+++ demos/gtk-demo/listview_filebrowser.c
@@ -0,0 +1,74 @@
+/* Icon View/Icon View Basics
+ *
+ * The GtkIconView widget is used to display and manipulate icons.
+ * It uses a GtkTreeModel for data storage, so the list store
+ * example might be helpful.
+ */
+
+#include <glib/gi18n.h>
+#include <gtk/gtk.h>
+
+static GtkWidget *window = NULL;
+
+static void
+up_clicked_cb (GtkButton        *button,
+               GtkDirectoryList *list)
+{
+  GFile *file;
+
+  file = g_file_get_parent (gtk_directory_list_get_file (list));
+  if (file == NULL)
+    return;
+
+  gtk_directory_list_set_file (list, file);
+}
+
+static void
+view_button_clicked_cb (GtkButton   *button,
+                        GtkListView *view)
+{
+  g_print ("Somebody clicked me!");
+}
+
+GtkWidget *
+do_listview_filebrowser (GtkWidget *do_widget)
+{
+  if (!window)
+    {
+      GtkWidget *view;
+      GtkBuilder *builder;
+      GtkDirectoryList *dirlist;
+      GFile *file;
+
+      builder = gtk_builder_new_from_resource ("/listview_filebrowser/listview_filebrowser.ui");
+      gtk_builder_add_callback_symbols (builder,
+                                        "up_clicked_cb", G_CALLBACK (up_clicked_cb),
+                                        "view_button_clicked_cb", G_CALLBACK (view_button_clicked_cb),
+                                        NULL);
+      gtk_builder_connect_signals (builder, NULL);
+      window = GTK_WIDGET (gtk_builder_get_object (builder, "window"));
+      gtk_window_set_display (GTK_WINDOW (window),
+                              gtk_widget_get_display (do_widget));
+      g_signal_connect (window, "destroy",
+                        G_CALLBACK (gtk_widget_destroyed), &window);
+
+      /* Create the model and fill it with the contents of the current directory */
+      file = g_file_new_for_path (g_get_current_dir ());
+      dirlist = GTK_DIRECTORY_LIST (gtk_builder_get_object (builder, "dirlist"));
+      gtk_directory_list_set_file (dirlist, file);
+      g_object_unref (file);
+
+      /* grab focus in the view */
+      view = gtk_list_view_new_with_factory (gtk_builder_list_item_factory_new_from_resource ("/listview_filebrowser/listview_filebrowser_list.ui"));
+      gtk_widget_grab_focus (view);
+
+      g_object_unref (builder);
+    }
+
+  if (!gtk_widget_get_visible (window))
+    gtk_widget_show (window);
+  else
+    gtk_widget_destroy (window);
+
+  return window;
+}
