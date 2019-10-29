f05acd7c62e20d644b9fa563d6f296577099f0d6
Benjamin Otte <otte@redhat.com>
2019-10-29 05:58:02 +0100

    Add GtkSignalListItemFactory
    So the poor Rust users can actually use this.
    
    I would totally not use this ever!

--- gtk/gtk.h
+++ gtk/gtk.h
@@ -222,6 +222,7 @@
 #include <gtk/gtkshortcutsshortcut.h>
 #include <gtk/gtkshortcutswindow.h>
 #include <gtk/gtkshow.h>
+#include <gtk/gtksignallistitemfactory.h>
 #include <gtk/gtksingleselection.h>
 #include <gtk/gtkslicelistmodel.h>
 #include <gtk/gtksnapshot.h>
