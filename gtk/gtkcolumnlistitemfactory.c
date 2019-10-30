df38ac878a54967782befc99600054c0334a5396
Benjamin Otte <otte@redhat.com>
2019-10-30 18:03:23 +0100

    columnview: Add listitems for the columns
    They are not aligned in columns yet, but they do exist.

---
+++ gtk/gtkcolumnlistitemfactory.c
@@ -0,0 +1,178 @@
+/*
+ * Copyright © 2019 Benjamin Otte
+ *
+ * This library is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Lesser General Public
+ * License as published by the Free Software Foundation; either
+ * version 2.1 of the License, or (at your option) any later version.
+ *
+ * This library is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public
+ * License along with this library. If not, see <http://www.gnu.org/licenses/>.
+ *
+ * Authors: Benjamin Otte <otte@gnome.org>
+ */
+
+#include "config.h"
+
+#include "gtkcolumnlistitemfactoryprivate.h"
+
+#include "gtkboxlayout.h"
+#include "gtkcolumnviewcolumnprivate.h"
+#include "gtklistitemfactoryprivate.h"
+#include "gtklistitemprivate.h"
+
+struct _GtkColumnListItemFactory
+{
+  GtkListItemFactory parent_instance;
+
+  GtkColumnView *view; /* no reference, the view references us */
+};
+
+struct _GtkColumnListItemFactoryClass
+{
+  GtkListItemFactoryClass parent_class;
+};
+
+G_DEFINE_TYPE (GtkColumnListItemFactory, gtk_column_list_item_factory, GTK_TYPE_LIST_ITEM_FACTORY)
+
+static GtkListItemWidget *
+get_nth_child (GtkListItemWidget *parent,
+               guint              pos)
+{
+  GtkWidget *child;
+  guint i;
+
+  child = gtk_widget_get_first_child (GTK_WIDGET (parent));
+  for (i = 1; i < pos && child; i++)
+    child = gtk_widget_get_next_sibling (child);
+
+  return GTK_LIST_ITEM_WIDGET (child);
+}
+
+static void
+gtk_column_list_item_factory_setup (GtkListItemFactory *factory,
+                                    GtkListItemWidget  *widget,
+                                    GtkListItem        *list_item)
+{
+  GtkColumnListItemFactory *self = GTK_COLUMN_LIST_ITEM_FACTORY (factory);
+  GListModel *columns;
+  guint i;
+
+  gtk_widget_set_layout_manager (GTK_WIDGET (list_item->owner),
+                                 gtk_box_layout_new (GTK_ORIENTATION_HORIZONTAL));
+
+  GTK_LIST_ITEM_FACTORY_CLASS (gtk_column_list_item_factory_parent_class)->setup (factory, widget, list_item);
+
+  columns = gtk_column_view_get_columns (self->view);
+
+  for (i = 0; i < g_list_model_get_n_items (columns); i++)
+    {
+      GtkColumnViewColumn *column = g_list_model_get_item (columns, i);
+
+      gtk_column_list_item_factory_add_column (self, 
+                                               list_item->owner,
+                                               column,
+                                               FALSE);
+    }
+}
+
+static void
+gtk_column_list_item_factory_teardown (GtkListItemFactory *factory,
+                                       GtkListItemWidget  *widget,
+                                       GtkListItem        *list_item)
+{
+  GtkWidget *child;
+
+  GTK_LIST_ITEM_FACTORY_CLASS (gtk_column_list_item_factory_parent_class)->teardown (factory, widget, list_item);
+
+  for (child = gtk_widget_get_first_child (GTK_WIDGET (list_item->owner));
+       child;
+       child = gtk_widget_get_first_child (GTK_WIDGET (list_item->owner)))
+    {
+      gtk_list_item_widget_remove_child (list_item->owner, child);
+    }
+}
+
+static void
+gtk_column_list_item_factory_update (GtkListItemFactory *factory,
+                                     GtkListItemWidget  *widget,
+                                     GtkListItem        *list_item,
+                                     guint               position,
+                                     gpointer            item,
+                                     gboolean            selected)
+{
+  GtkWidget *child;
+
+  GTK_LIST_ITEM_FACTORY_CLASS (gtk_column_list_item_factory_parent_class)->update (factory, widget, list_item, position, item, selected);
+
+  for (child = gtk_widget_get_first_child (GTK_WIDGET (widget));
+       child;
+       child = gtk_widget_get_next_sibling (child))
+    {
+      gtk_list_item_widget_update (GTK_LIST_ITEM_WIDGET (child), position, item, selected);
+    }
+}
+
+static void
+gtk_column_list_item_factory_class_init (GtkColumnListItemFactoryClass *klass)
+{
+  GtkListItemFactoryClass *factory_class = GTK_LIST_ITEM_FACTORY_CLASS (klass);
+
+  factory_class->setup = gtk_column_list_item_factory_setup;
+  factory_class->teardown = gtk_column_list_item_factory_teardown;
+  factory_class->update = gtk_column_list_item_factory_update;
+}
+
+static void
+gtk_column_list_item_factory_init (GtkColumnListItemFactory *self)
+{
+}
+
+GtkColumnListItemFactory *
+gtk_column_list_item_factory_new (GtkColumnView *view)
+{
+  GtkColumnListItemFactory *result;
+
+  result = g_object_new (GTK_TYPE_COLUMN_LIST_ITEM_FACTORY, NULL);
+
+  result->view = view;
+
+  return result;
+}
+
+void
+gtk_column_list_item_factory_add_column (GtkColumnListItemFactory *factory,
+                                         GtkListItemWidget        *list_item,
+                                         GtkColumnViewColumn      *column,
+                                         gboolean                  check_bind)
+{
+  GtkListItemFactory *column_factory;
+  GtkWidget *cell;
+
+  column_factory = gtk_column_view_column_get_factory (column);
+
+  cell = gtk_list_item_widget_new (column_factory, "cell");
+  gtk_list_item_widget_add_child (GTK_LIST_ITEM_WIDGET (list_item), GTK_WIDGET (cell));
+  gtk_list_item_widget_update (GTK_LIST_ITEM_WIDGET (cell),
+                               gtk_list_item_widget_get_position (list_item),
+                               gtk_list_item_widget_get_item (list_item),
+                               gtk_list_item_widget_get_selected (list_item));
+}
+
+void
+gtk_column_list_item_factory_remove_column (GtkColumnListItemFactory *factory,
+                                            GtkListItemWidget        *list_item,
+                                            guint                     col_pos,
+                                            GtkColumnViewColumn      *column)
+{
+  GtkListItemWidget *cell;
+
+  cell = get_nth_child (list_item, col_pos);
+
+  gtk_list_item_widget_remove_child (GTK_LIST_ITEM_WIDGET (list_item), GTK_WIDGET (cell));
+}
