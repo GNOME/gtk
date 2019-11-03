3e3e685d609783c810e5b04f4fea7068fdca0319
Benjamin Otte <otte@redhat.com>
2019-11-03 02:23:46 +0100

    listitemfactory: Simplify
    Instead of 6 vfuncs, we now have 3 and rely on the factory keeping track
    of what it needs to do.
    
    We're doing lots of dancing from one object to another here, but this
    will hopefully get simpler with further commits.

--- gtk/gtklistitemfactory.c
+++ gtk/gtklistitemfactory.c
@@ -76,30 +76,31 @@ G_DEFINE_TYPE (GtkListItemFactory, gtk_list_item_factory, G_TYPE_OBJECT)
 
 static void
 gtk_list_item_factory_default_setup (GtkListItemFactory *self,
+                                     GtkListItemWidget  *widget,
                                      GtkListItem        *list_item)
 {
+  gtk_list_item_widget_default_setup (widget, list_item);
 }
 
 static void
 gtk_list_item_factory_default_teardown (GtkListItemFactory *self,
+                                        GtkListItemWidget  *widget,
                                         GtkListItem        *list_item)
 {
+  gtk_list_item_widget_default_teardown (widget, list_item);
+
   gtk_list_item_set_child (list_item, NULL);
-
-  gtk_list_item_set_selectable (list_item, TRUE);
-
 }
 
 static void                  
 gtk_list_item_factory_default_update (GtkListItemFactory *self,
+                                      GtkListItemWidget  *widget,
                                       GtkListItem        *list_item,
                                       guint               position,
                                       gpointer            item,
                                       gboolean            selected)
 {
-  gtk_list_item_set_item (list_item, item);
-  gtk_list_item_set_position (list_item, position);
-  gtk_list_item_set_selected (list_item, selected);
+  gtk_list_item_widget_default_update (widget, list_item, position, item, selected);
 }
 
 static void
@@ -117,35 +118,49 @@ gtk_list_item_factory_init (GtkListItemFactory *self)
 
 void
 gtk_list_item_factory_setup (GtkListItemFactory *self,
-                             GtkListItem        *list_item)
+                             GtkListItemWidget  *widget)
 {
+  GtkListItem *list_item;
+
   g_return_if_fail (GTK_IS_LIST_ITEM_FACTORY (self));
 
-  GTK_LIST_ITEM_FACTORY_GET_CLASS (self)->setup (self, list_item);
+  list_item = gtk_list_item_new ();
+
+  GTK_LIST_ITEM_FACTORY_GET_CLASS (self)->setup (self, widget, list_item);
 }
 
 void
 gtk_list_item_factory_teardown (GtkListItemFactory *self,
-                                GtkListItem        *list_item)
+                                GtkListItemWidget  *widget)
 {
+  GtkListItem *list_item;
+
   g_return_if_fail (GTK_IS_LIST_ITEM_FACTORY (self));
 
-  GTK_LIST_ITEM_FACTORY_GET_CLASS (self)->teardown (self, list_item);
+  list_item = gtk_list_item_widget_get_list_item (widget);
+
+  GTK_LIST_ITEM_FACTORY_GET_CLASS (self)->teardown (self, widget, list_item);
+
+  g_object_unref (list_item);
 }
 
 void
 gtk_list_item_factory_update (GtkListItemFactory *self,
-                              GtkListItem        *list_item,
+                              GtkListItemWidget  *widget,
                               guint               position,
                               gpointer            item,
                               gboolean            selected)
 {
+  GtkListItem *list_item;
+
   g_return_if_fail (GTK_IS_LIST_ITEM_FACTORY (self));
-  g_return_if_fail (GTK_IS_LIST_ITEM (list_item));
+  g_return_if_fail (GTK_IS_LIST_ITEM_WIDGET (widget));
+
+  list_item = gtk_list_item_widget_get_list_item (widget);
 
   g_object_freeze_notify (G_OBJECT (list_item));
 
-  GTK_LIST_ITEM_FACTORY_GET_CLASS (self)->update (self, list_item, position, item, selected);
+  GTK_LIST_ITEM_FACTORY_GET_CLASS (self)->update (self, widget, list_item, position, item, selected);
 
   g_object_thaw_notify (G_OBJECT (list_item));
 }
