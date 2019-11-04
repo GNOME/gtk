53c9eb5a5b008aa64bf385aca367ccce1592b298
Matthias Clasen <mclasen@redhat.com>
2019-11-04 23:24:49 +0000

    Move the ifdef to the right spot

--- gtk/gtkcellrenderertext.c
+++ gtk/gtkcellrenderertext.c
@@ -1521,11 +1521,11 @@ get_layout (GtkCellRendererText *celltext,
           uline = PANGO_UNDERLINE_SINGLE;
           break;
 
-#if PANGO_VERSION_CHECK(1,45,0)
         case PANGO_UNDERLINE_SINGLE:
           uline = PANGO_UNDERLINE_DOUBLE;
           break;
 
+#if PANGO_VERSION_CHECK(1,45,0)
         case PANGO_UNDERLINE_SINGLE_LINE:
           uline = PANGO_UNDERLINE_DOUBLE_LINE;
           break;
