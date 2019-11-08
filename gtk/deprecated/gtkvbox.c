d1936c1ae3e5e133d95477cb4db3724f0de42f0c
Emmanuele Bassi <ebassi@gnome.org>
2019-11-08 18:02:13 +0000

    docs: Clarify when to replace old boxes with Grid
    Mention that GtkGrid should only replace grid-like layouts achieved with
    nested boxes.
    
    Additionally, remove any mention of "future proofing": boxes are not
    going away.

--- gtk/deprecated/gtkvbox.c
+++ gtk/deprecated/gtkvbox.c
@@ -45,20 +45,24 @@ G_GNUC_BEGIN_IGNORE_DEPRECATIONS
  *
  * All children are allocated the same width.
  *
- * GtkVBox has been deprecated. You can use #GtkBox instead, which is a
- * very quick and easy change. If you have derived your own classes from
- * GtkVBox, you can simply change the inheritance to derive directly
- * from #GtkBox, and set the #GtkOrientable:orientation property to
- * %GTK_ORIENTATION_VERTICAL in your instance init function, with a
- * call like:
+ * GtkVBox has been deprecated. You can use #GtkBox with a #GtkOrientable:orientation
+ * set to %GTK_ORIENTATION_VERTICAL instead when calling gtk_box_new(),
+ * which is a very quick and easy change.
+ *
+ * If you have derived your own classes from GtkVBox, you can change the
+ * inheritance to derive directly from #GtkBox, and set the #GtkOrientable:orientation
+ * property to %GTK_ORIENTATION_VERTICAL in your instance init function,
+ * with a call like:
+ *
  * |[<!-- language="C" -->
  *   gtk_orientable_set_orientation (GTK_ORIENTABLE (object),
  *                                   GTK_ORIENTATION_VERTICAL);
  * ]|
- * If you don’t need first-child or last-child styling and want your code
- * to be future-proof, the recommendation is to switch to #GtkGrid instead
- * of nested boxes. For more information about migrating to #GtkGrid,
- * see [Migrating from other containers to GtkGrid][gtk-migrating-GtkGrid].
+ *
+ * If you have a grid-like layout composed of nested boxes, and you don’t
+ * need first-child or last-child styling, the recommendation is to switch
+ * to #GtkGrid. For more information about migrating to #GtkGrid, see
+ * [Migrating from other containers to GtkGrid][gtk-migrating-GtkGrid].
  */
 
 G_DEFINE_TYPE (GtkVBox, gtk_vbox, GTK_TYPE_BOX)
