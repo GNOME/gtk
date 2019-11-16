30dab76ecfb77f81599502d740ac1e715f6118ee
Benjamin Otte <otte@redhat.com>
2019-11-16 21:46:57 +0100

    filter: Add GtkAnyFilter

--- gtk/gtkfilters.c
+++ gtk/gtkfilters.c
@@ -24,6 +24,8 @@
 #include "gtkintl.h"
 #include "gtktypebuiltins.h"
 
+/*** CUSTOM FILTER ***/
+
 struct _GtkCustomFilter
 {
   GtkFilter parent_instance;
@@ -101,3 +103,166 @@ gtk_custom_filter_new (GtkCustomFilterFunc     filter_func,
   return GTK_FILTER (result);
 }
 
+/*** ANY FILTER ***/
+
+struct _GtkAnyFilter
+{
+  GtkFilter parent_instance;
+
+  GSequence *filters;
+};
+
+static GType
+gtk_any_filter_get_item_type (GListModel *list)
+{
+  return GTK_TYPE_FILTER;
+}
+
+static guint
+gtk_any_filter_get_n_items (GListModel *list)
+{
+  GtkAnyFilter *self = GTK_ANY_FILTER (list);
+
+  return g_sequence_get_length (self->filters);
+}
+
+static gpointer
+gtk_any_filter_get_item (GListModel *list,
+                         guint       position)
+{
+  GtkAnyFilter *self = GTK_ANY_FILTER (list);
+  GSequenceIter *iter;
+
+  iter = g_sequence_get_iter_at_pos (self->filters, position);
+
+  if (g_sequence_iter_is_end (iter))
+    return NULL;
+  else
+    return g_object_ref (g_sequence_get (iter));
+}
+
+static void
+gtk_any_filter_list_model_init (GListModelInterface *iface)
+{
+  iface->get_item_type = gtk_any_filter_get_item_type;
+  iface->get_n_items = gtk_any_filter_get_n_items;
+  iface->get_item = gtk_any_filter_get_item;
+}
+
+G_DEFINE_TYPE_WITH_CODE (GtkAnyFilter, gtk_any_filter, GTK_TYPE_FILTER,
+                         G_IMPLEMENT_INTERFACE (G_TYPE_LIST_MODEL, gtk_any_filter_list_model_init))
+
+static gboolean
+gtk_any_filter_filter (GtkFilter *filter,
+                       gpointer   item)
+{
+  GtkAnyFilter *self = GTK_ANY_FILTER (filter);
+  GSequenceIter *iter;
+
+  for (iter = g_sequence_get_begin_iter (self->filters);
+       !g_sequence_iter_is_end (iter);
+       iter = g_sequence_iter_next (iter))
+    {
+      GtkFilter *child = g_sequence_get (iter);
+
+      if (gtk_filter_filter (child, item))
+        return TRUE;
+    }
+
+  return FALSE;
+}
+
+static void
+gtk_any_filter_dispose (GObject *object)
+{
+  GtkAnyFilter *self = GTK_ANY_FILTER (object);
+
+  g_clear_pointer (&self->filters, g_sequence_free);
+
+  G_OBJECT_CLASS (gtk_any_filter_parent_class)->dispose (object);
+}
+
+static void
+gtk_any_filter_class_init (GtkAnyFilterClass *class)
+{
+  GtkFilterClass *filter_class = GTK_FILTER_CLASS (class);
+  GObjectClass *object_class = G_OBJECT_CLASS (class);
+
+  filter_class->filter = gtk_any_filter_filter;
+
+  object_class->dispose = gtk_any_filter_dispose;
+}
+
+static void
+gtk_any_filter_init (GtkAnyFilter *self)
+{
+  self->filters = g_sequence_new (g_object_unref);
+
+  gtk_filter_changed (GTK_FILTER (self), GTK_FILTER_CHANGE_MATCH_NONE);
+}
+
+/**
+ * gtk_any_filter_new:
+ *
+ * Creates a new "any" filter.
+ *
+ * This filter matches an item if any of the filters added to it
+ * matches the item.
+ * In particular, this means that if no filter has been added to
+ * it, the filter matches no item.
+ *
+ * Returns: a new #GtkFilter
+ **/
+GtkFilter *
+gtk_any_filter_new (void)
+{
+  return g_object_new (GTK_TYPE_ANY_FILTER, NULL);
+}
+
+/**
+ * gtk_any_filter_append:
+ * @self: a #GtkAnyFilter
+ * @filter: (tranfer none): A new filter to use
+ *
+ * Adds a @filter to @self to use for matching.
+ **/
+void
+gtk_any_filter_append (GtkAnyFilter *self,
+                       GtkFilter    *filter)
+{
+  g_return_if_fail (GTK_IS_ANY_FILTER (self));
+  g_return_if_fail (GTK_IS_FILTER (filter));
+
+  g_sequence_append (self->filters, g_object_ref (filter));
+
+  gtk_filter_changed (GTK_FILTER (self), GTK_FILTER_CHANGE_LESS_STRICT);
+}
+
+/**
+ * gtk_any_filter_remove:
+ * @self: a #GtkAnyFilter
+ * @position: position of filter to remove
+ *
+ * Removes the filter at the given @position from the list of filters used
+ * by @self.
+ * If @position is larger than the number of filters, nothing happens and
+ * the function returns.
+ **/
+void
+gtk_any_filter_remove (GtkAnyFilter *self,
+                       guint         position)
+{
+  GSequenceIter *iter;
+  guint length;
+
+  length = g_sequence_get_length (self->filters);
+  if (position >= length)
+    return;
+
+  iter = g_sequence_get_iter_at_pos (self->filters, position);
+  g_sequence_remove (iter);
+
+  gtk_filter_changed (GTK_FILTER (self), length == 1 
+                                         ? GTK_FILTER_CHANGE_MATCH_NONE
+                                         : GTK_FILTER_CHANGE_MORE_STRICT);
+}
