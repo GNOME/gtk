021547bf3d1927ded5bbe6a9395867082f2a2dde
Matthias Clasen <mclasen@redhat.com>
2019-12-02 21:00:22 -0500

    inspector: Use a column view for the property list
    This is not quite complete - the sorting got lost.

---
+++ gtk/inspector/prop-holder.h
@@ -0,0 +1,17 @@
+#ifndef __PROP_HOLDER_H__
+#define __PROP_HOLDER_H__
+
+#include <gtk/gtk.h>
+
+#define PROP_TYPE_HOLDER (prop_holder_get_type ())
+
+G_DECLARE_FINAL_TYPE (PropHolder, prop_holder, PROP, HOLDER, GObject)
+
+PropHolder * prop_holder_new         (GObject    *object,
+                                      GParamSpec *pspeC);
+
+GObject    *prop_holder_get_object   (PropHolder *holder);
+GParamSpec *prop_holder_get_pspec    (PropHolder *holder);
+const char *prop_holder_get_name     (PropHolder *holder);
+
+#endif /* __PROP_HOLDER_H__ */
