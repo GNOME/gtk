0c734efa013f85c1421e99c03694e9d2839857f7
Matthias Clasen <mclasen@redhat.com>
2019-12-02 23:44:22 -0500

    Redo sort list model with GtkSorter
    Reshuffle the api to take full advantage
    of GtkSorter. Update all callers.

--- tests/testlistview-animating.c
+++ tests/testlistview-animating.c
@@ -108,11 +108,11 @@ do_stuff (gpointer store)
 }
 
 static gboolean
-revert_sort (gpointer sort)
+revert_sort (gpointer sorter)
 {
   reverse_sort = !reverse_sort;
 
-  gtk_sort_list_model_resort (sort);
+  gtk_sorter_set_sort_direction (GTK_SORTER (sorter), reverse_sort ? GTK_SORT_ASCENDING : GTK_SORT_DESCENDING);
 
   return G_SOURCE_CONTINUE;
 }
@@ -125,15 +125,15 @@ main (int   argc,
   GListStore *store;
   GtkSortListModel *sort;
   guint i;
+  GtkSorter *sorter;
 
   gtk_init ();
 
   store = g_list_store_new (G_TYPE_OBJECT);
   for (i = 0; i < AVERAGE; i++)
     add (store);
-  sort = gtk_sort_list_model_new (G_LIST_MODEL (store),
-                                  compare,
-                                  NULL, NULL);
+  sorter = gtk_custom_sorter_new (compare, NULL, NULL);
+  sort = gtk_sort_list_model_new (G_LIST_MODEL (store), sorter);
 
   win = gtk_window_new (GTK_WINDOW_TOPLEVEL);
   gtk_window_set_default_size (GTK_WINDOW (win), 400, 600);
@@ -180,13 +180,14 @@ main (int   argc,
                            NULL, NULL);
 
   g_timeout_add (100, do_stuff, store);
-  g_timeout_add_seconds (3, revert_sort, sort);
+  g_timeout_add_seconds (3, revert_sort, sorter);
 
   gtk_widget_show (win);
 
   gtk_main ();
 
   g_object_unref (store);
+  g_object_unref (sorter);
 
   return 0;
 }
