5dd39b8830f163d741eab1f2d00298bad150961b
Timm Bäder <mail@baedert.org>
2019-12-14 13:53:59 +0100

    gl renderer: Rewort passing rounded rects to shaders

--- gsk/resources/glsl/border.fs.glsl
+++ gsk/resources/glsl/border.fs.glsl
@@ -1,25 +1,15 @@
 uniform vec4 u_color;
 uniform vec4 u_widths;
-
-uniform vec4 u_outline;
-uniform vec2 u_corner_sizes[4];
+uniform RoundedRect u_outline_rect;
 
 void main() {
-  vec4 bounds = u_outline;
-
-  bounds.z = bounds.x + bounds.z;
-  bounds.w = bounds.y + bounds.w;
-
   vec4 f = gl_FragCoord;
   f.x += u_viewport.x;
   f.y = (u_viewport.y + u_viewport.w) - f.y;
 
-  vec4 corner_widths = vec4(u_corner_sizes[0].x, u_corner_sizes[1].x, u_corner_sizes[2].x, u_corner_sizes[3].x);
-  vec4 corner_heights = vec4(u_corner_sizes[0].y, u_corner_sizes[1].y, u_corner_sizes[2].y, u_corner_sizes[3].y);
-  RoundedRect routside = RoundedRect (bounds, corner_widths, corner_heights);
-  RoundedRect rinside = rounded_rect_shrink (routside, u_widths);
+  RoundedRect rinside = rounded_rect_shrink (u_outline_rect, u_widths);
 
-  float alpha = clamp (rounded_rect_coverage (routside, f.xy) -
+  float alpha = clamp (rounded_rect_coverage (u_outline_rect, f.xy) -
                        rounded_rect_coverage (rinside, f.xy),
                        0.0, 1.0);
 
--- gsk/resources/glsl/unblurred_outset_shadow.fs.glsl
+++ gsk/resources/glsl/unblurred_outset_shadow.fs.glsl
@@ -1,10 +1,7 @@
 uniform float u_spread;
 uniform vec4 u_color;
 uniform vec2 u_offset;
-uniform vec4 u_outline;
-uniform vec4 u_corner_widths;
-uniform vec4 u_corner_heights;
-
+uniform RoundedRect u_outline_rect;
 
 void main() {
   vec4 f = gl_FragCoord;
@@ -12,15 +9,12 @@ void main() {
   f.x += u_viewport.x;
   f.y = (u_viewport.y + u_viewport.w) - f.y;
 
-  RoundedRect inside = RoundedRect(vec4(u_outline.xy, u_outline.xy + u_outline.zw),
-                                    u_corner_widths, u_corner_heights);
-
-  RoundedRect outline = rounded_rect_shrink(inside, vec4(- u_spread));
+  RoundedRect outline = rounded_rect_shrink(u_outline_rect, vec4(- u_spread));
 
   vec2 offset = vec2(u_offset.x, - u_offset.y);
   vec4 color = vec4(u_color.rgb * u_color.a, u_color.a);
   color = color * clamp (rounded_rect_coverage (outline, f.xy - offset) -
-                         rounded_rect_coverage (inside, f.xy),
+                         rounded_rect_coverage (u_outline_rect, f.xy),
                          0.0, 1.0);
   setOutputColor(color * u_alpha);
 }
