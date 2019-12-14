e4f9c9c76a1361da8c41d98e435a915399496455
Timm Bäder <mail@baedert.org>
2019-12-14 13:53:59 +0100

    gl renderer: Rewort passing rounded rects to shaders

--- gsk/resources/glsl/inset_shadow.fs.glsl
+++ gsk/resources/glsl/inset_shadow.fs.glsl
@@ -1,9 +1,7 @@
 uniform float u_spread;
 uniform vec4 u_color;
 uniform vec2 u_offset;
-uniform vec4 u_outline;
-uniform vec4 u_corner_widths;
-uniform vec4 u_corner_heights;
+uniform RoundedRect u_outline_rect;
 
 
 void main() {
@@ -12,14 +10,10 @@ void main() {
   f.x += u_viewport.x;
   f.y = (u_viewport.y + u_viewport.w) - f.y;
 
-  RoundedRect outline = RoundedRect(vec4(u_outline.xy, u_outline.xy + u_outline.zw),
-                                    u_corner_widths, u_corner_heights);
-  RoundedRect inside = rounded_rect_shrink(outline, vec4(u_spread));
-
-
+  RoundedRect inside = rounded_rect_shrink(u_outline_rect, vec4(u_spread));
   vec2 offset = vec2(u_offset.x, - u_offset.y);
   vec4 color = vec4(u_color.rgb * u_color.a, u_color.a);
-  color = color * clamp (rounded_rect_coverage (outline, f.xy) -
+  color = color * clamp (rounded_rect_coverage (u_outline_rect, f.xy) -
                          rounded_rect_coverage (inside, f.xy - offset),
                          0.0, 1.0);
   setOutputColor(color * u_alpha);
