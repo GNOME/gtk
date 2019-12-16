b5c7ec9cd77cce37a1d164ce3ed8ecae24021556
Timm Bäder <mail@baedert.org>
2019-12-16 06:16:26 +0100

    gl renderer: Make RoundedRect work in gles
    Which can't return struct types containing arrays. So let's revert to
    the previous version but still send the rect along as a vec4[3];

--- gsk/resources/glsl/border.glsl
+++ gsk/resources/glsl/border.glsl
@@ -9,17 +9,18 @@ void main() {
 // FRAGMENT_SHADER:
 uniform vec4 u_color;
 uniform vec4 u_widths;
-uniform RoundedRect u_outline_rect;
+uniform vec4[3] u_outline_rect;
 
 void main() {
   vec4 f = gl_FragCoord;
   f.x += u_viewport.x;
   f.y = (u_viewport.y + u_viewport.w) - f.y;
 
-  RoundedRect rinside = rounded_rect_shrink (u_outline_rect, u_widths);
+  RoundedRect outside = create_rect(u_outline_rect);
+  RoundedRect inside = rounded_rect_shrink (outside, u_widths);
 
-  float alpha = clamp (rounded_rect_coverage (u_outline_rect, f.xy) -
-                       rounded_rect_coverage (rinside, f.xy),
+  float alpha = clamp (rounded_rect_coverage (outside, f.xy) -
+                       rounded_rect_coverage (inside, f.xy),
                        0.0, 1.0);
 
   /* Pre-multiply */
