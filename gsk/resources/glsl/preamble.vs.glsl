fdce30d3f8b00c69d8160278afaf2e82e933fcdc
Timm Bäder <mail@baedert.org>
2019-12-17 12:19:37 +0100

    gl renderer: Add some in/out compat glue to the shaders
    so we can use _IN_ and _OUT_ and get the right things for
    desktop/es/legacy GL.

--- gsk/resources/glsl/preamble.vs.glsl
+++ gsk/resources/glsl/preamble.vs.glsl
@@ -1,17 +1,22 @@
 uniform mat4 u_projection;
 uniform mat4 u_modelview;
 
-
 #if GSK_GLES
+#define _OUT_ varying
+#define _IN_ varying
 attribute vec2 aPosition;
 attribute vec2 aUv;
-varying vec2 vUv;
+_OUT_ vec2 vUv;
 #elif GSK_LEGACY
+#define _OUT_ varying
+#define _IN_ varying
 attribute vec2 aPosition;
 attribute vec2 aUv;
-varying vec2 vUv;
+_OUT_ vec2 vUv;
 #else
-in vec2 aPosition;
-in vec2 aUv;
-out vec2 vUv;
+#define _OUT_ out
+#define _IN_ in
+_IN_ vec2 aPosition;
+_IN_ vec2 aUv;
+_OUT_ vec2 vUv;
 #endif
