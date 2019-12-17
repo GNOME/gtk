6ef0bb8beaf7bc408bf6adb1d989117d2b001e49
Timm Bäder <mail@baedert.org>
2019-12-17 15:59:42 +0100

    gl renderer: Move final color computation to vertex shader
    .. of the coloring program, which is used all the time for text.

--- gsk/resources/glsl/coloring.glsl
+++ gsk/resources/glsl/coloring.glsl
@@ -1,23 +1,25 @@
 // VERTEX_SHADER:
+uniform vec4 u_color;
+
+_OUT_ vec4 final_color;
+
 void main() {
   gl_Position = u_projection * u_modelview * vec4(aPosition, 0.0, 1.0);
 
   vUv = vec2(aUv.x, aUv.y);
+
+  final_color = u_color;
+  // pre-multiply
+  final_color.rgb *= final_color.a;
+  final_color *= u_alpha;
 }
 
 // FRAGMENT_SHADER:
-uniform vec4 u_color;
+
+_IN_ vec4 final_color;
 
 void main() {
   vec4 diffuse = Texture(u_source, vUv);
-  vec4 color = u_color;
 
-  // pre-multiply
-  color.rgb *= color.a;
-
-  // u_source is drawn using cairo, so already pre-multiplied.
-  color = vec4(color.rgb * diffuse.a * u_alpha,
-               color.a * diffuse.a * u_alpha);
-
-  setOutputColor(color);
+  setOutputColor(final_color * diffuse.a);
 }
