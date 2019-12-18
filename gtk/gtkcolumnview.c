61286c8ba12dba482bff916b9fa28ebf0532fc40
Matthias Clasen <mclasen@redhat.com>
2019-12-18 02:19:48 -0500

    columnview: Add column reordering
    Add an API to allow reordering columns.

--- gtk/gtkcolumnview.c
+++ gtk/gtkcolumnview.c
@@ -724,11 +724,26 @@ gtk_column_view_remove_column (GtkColumnView       *self,
       g_object_unref (item);
       if (item == column)
         break;
+
     }
 
   gtk_column_view_sorter_remove_column (GTK_COLUMN_VIEW_SORTER (self->sorter), column);
   gtk_column_view_column_set_column_view (column, NULL);
   g_list_store_remove (self->columns, i);
+}
+
+void
+gtk_column_view_insert_column (GtkColumnView       *self,
+                               guint                position,
+                               GtkColumnViewColumn *column)
+{
+  g_return_if_fail (GTK_IS_COLUMN_VIEW (self));
+  g_return_if_fail (GTK_IS_COLUMN_VIEW_COLUMN (column));
+  g_return_if_fail (gtk_column_view_column_get_column_view (column) == NULL);
+  g_return_if_fail (position <= g_list_model_get_n_items (G_LIST_MODEL (self->columns)));
+
+  gtk_column_view_column_set_column_view (column, self);
+  g_list_store_insert (self->columns, position, column);
 }
 
 void
