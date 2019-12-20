c9d5b77403c19fa851616e9b7857a7b8c1cbf362
Matthias Clasen <mclasen@redhat.com>
2019-12-20 17:19:55 -0500

    columnviewcolumn: Add reordering helpers
    Add helper functions that let us temporarily give
    a different allocation to headers. These will be
    used to implement interactive column reordering
    in GtkColumnView.

--- gtk/gtkcolumnviewcolumn.c
+++ gtk/gtkcolumnviewcolumn.c
@@ -60,6 +60,7 @@ struct _GtkColumnViewColumn
   int natural_size_request;
   int allocation_offset;
   int allocation_size;
+  int header_position;
 
   int fixed_width;
 
@@ -478,6 +479,7 @@ gtk_column_view_column_allocate (GtkColumnViewColumn *self,
 {
   self->allocation_offset = offset;
   self->allocation_size = size;
+  self->header_position = offset;
 }
 
 void
@@ -946,4 +948,23 @@ GtkWidget *
 gtk_column_view_column_get_header (GtkColumnViewColumn *self)
 {
   return self->header;
+}
+
+void
+gtk_column_view_column_set_header_position (GtkColumnViewColumn *self,
+                                            int                  offset)
+{
+  self->header_position = offset;
+}
+
+void
+gtk_column_view_column_get_header_allocation (GtkColumnViewColumn *self,
+                                              int                 *offset,
+                                              int                 *size)
+{
+  if (offset)
+    *offset = self->header_position;
+
+  if (size)
+    *size = self->allocation_size;
 }
