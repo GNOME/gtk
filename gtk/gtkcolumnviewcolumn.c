151cd0a296630283e5791542530fb42a389d26a7
Matthias Clasen <mclasen@redhat.com>
2019-12-20 23:01:55 -0500

    columnviewcolumn: Add a helper
    We need to check whether clicks are in the headers
    of columns, so let the column view get at the the
    header widget.

--- gtk/gtkcolumnviewcolumn.c
+++ gtk/gtkcolumnviewcolumn.c
@@ -874,3 +874,9 @@ gtk_column_view_column_get_fixed_width (GtkColumnViewColumn *self)
 
   return self->fixed_width;
 }
+
+GtkWidget *
+gtk_column_view_column_get_header (GtkColumnViewColumn *self)
+{
+  return self->header;
+}
