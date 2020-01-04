270e8f2bf5b8768e4bff1ebf534707d5222c5bac
Alex Samorukov <samm@net-art.cz>
2020-01-04 12:29:21 +0000

    impl_class->set_functions listed twice in the object

--- gdk/quartz/gdksurface-quartz.c
+++ gdk/quartz/gdksurface-quartz.c
@@ -2710,7 +2710,6 @@ gdk_surface_impl_quartz_class_init (GdkSurfaceImplQuartzClass *klass)
   impl_class->set_decorations = gdk_quartz_surface_set_decorations;
   impl_class->get_decorations = gdk_quartz_surface_get_decorations;
   impl_class->set_functions = gdk_quartz_surface_set_functions;
-  impl_class->set_functions = gdk_quartz_surface_set_functions;
   impl_class->begin_resize_drag = gdk_quartz_surface_begin_resize_drag;
   impl_class->begin_move_drag = gdk_quartz_surface_begin_move_drag;
   impl_class->set_opacity = gdk_quartz_surface_set_opacity;
