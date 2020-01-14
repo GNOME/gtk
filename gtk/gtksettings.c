d668d2fa25fdbbb9198bc0765bc5fe004b8a2e41
Alex Samorukov <samm@net-art.cz>
2020-01-14 22:15:05 +0000

    Use bundle id instead of package name for the macOS preview as filename is now changed

--- gtk/gtksettings.c
+++ gtk/gtksettings.c
@@ -58,7 +58,7 @@
 #endif
 
 #ifdef GDK_WINDOWING_QUARTZ
-#define PRINT_PREVIEW_COMMAND "open -a /Applications/Preview.app %f"
+#define PRINT_PREVIEW_COMMAND "open -b com.apple.Preview %f"
 #else
 #define PRINT_PREVIEW_COMMAND "evince --unlink-tempfile --preview --print-settings %s %f"
 #endif
