d3d5a03da55369920b8bac1905ed5f14a512a074
Matthias Clasen <mclasen@redhat.com>
2020-01-15 02:03:59 -0500

    Make the superset matcher more exact
    When the superset matcher is built from a node matcher,
    we can make the parent or previous matchers be superset
    matchers for the corresponding node, with the same
    relevant parts. This is more precise than the any matcher
    that we were returning previously.

--- gtk/gtkcssmatcher.c
+++ gtk/gtkcssmatcher.c
@@ -469,18 +469,34 @@ static gboolean
 gtk_css_matcher_superset_get_parent (GtkCssMatcher       *matcher,
                                      const GtkCssMatcher *child)
 {
-  _gtk_css_matcher_any_init (matcher);
+  gboolean ret = TRUE;
 
-  return TRUE;
+  if (child->klass->type == GTK_CSS_MATCHER_TYPE_NODE)
+    {
+      ret = gtk_css_matcher_node_get_parent (matcher, child);
+      matcher->klass = child->klass;
+     }
+  else
+    _gtk_css_matcher_any_init (matcher);
+
+  return ret;
 }
 
 static gboolean
 gtk_css_matcher_superset_get_previous (GtkCssMatcher       *matcher,
                                        const GtkCssMatcher *next)
 {
-  _gtk_css_matcher_any_init (matcher);
+  gboolean ret = TRUE;
 
-  return TRUE;
+  if (next->klass->type == GTK_CSS_MATCHER_TYPE_NODE)
+    {
+      ret = gtk_css_matcher_node_get_previous (matcher, next);
+      matcher->klass = next->klass;
+     }
+  else
+    _gtk_css_matcher_any_init (matcher);
+
+  return ret;
 }
 
 static GtkStateFlags
