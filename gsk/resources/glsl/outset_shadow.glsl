307c45afb023c37b6fb459f4e4bcd72888325f40
Timm Bäder <mail@baedert.org>
2020-01-16 08:02:48 +0100

    gl renderer: Draw outset shadows white
    and only apply the actual shadow color when we draw them from the
    texture. This way we can reuse the cached shadows during color
    transitions.

--- gsk/resources/glsl/outset_shadow.glsl
+++ gsk/resources/glsl/outset_shadow.glsl
@@ -1,12 +1,23 @@
 // VERTEX_SHADER:
+uniform vec4 u_color;
+
+_OUT_ vec4 final_color;
+
 void main() {
   gl_Position = u_projection * u_modelview * vec4(aPosition, 0.0, 1.0);
 
   vUv = vec2(aUv.x, aUv.y);
+
+  final_color = u_color;
+  // pre-multiply
+  final_color.rgb *= final_color.a;
+  final_color *= u_alpha;
 }
 
 // FRAGMENT_SHADER:
 uniform vec4[3] u_outline_rect;
+
+_IN_ vec4 final_color;
 
 void main() {
   vec4 f = gl_FragCoord;
@@ -15,7 +26,11 @@ void main() {
   f.y = (u_viewport.y + u_viewport.w) - f.y;
 
   RoundedRect outline = create_rect(u_outline_rect);
-  vec4 color = Texture(u_source, vUv);
-  color = color * (1.0 -  clamp(rounded_rect_coverage(outline, f.xy), 0.0, 1.0));
-  setOutputColor(color * u_alpha);
+
+  float alpha = Texture(u_source, vUv).a;
+  alpha *= (1.0 -  clamp(rounded_rect_coverage(outline, f.xy), 0.0, 1.0));
+
+  vec4 color = final_color * alpha;
+
+  setOutputColor(color);
 }
