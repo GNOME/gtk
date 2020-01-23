GNOME/gtk

commit 360a3c16902ec80a3f0e69aba3b4f743a75efc1e

Use a cheaper way to store settings per display
This was another very frequent use of qdata. Since we typically
have only one or two display objects, storing the display-settings
association in a simple array is faster than using object data
or a hash table.

gtk/gtksettings.c
@@ -278,6 +278,13 @@ static GQuark            quark_gtk_settings = 0;
 static GSList           *object_list = NULL;
 static guint             class_n_properties = 0;
 
+typedef struct {
+  GdkDisplay *display;
+  GtkSettings *settings;
+} DisplaySettings;
+
+static GArray *display_settings;
+
 
 G_DEFINE_TYPE_EXTENDED (GtkSettings, gtk_settings, G_TYPE_OBJECT, 0,
                         G_ADD_PRIVATE (GtkSettings)
@@ -1868,6 +1875,66 @@ settings_init_style (GtkSettings *settings)
   settings_update_key_theme (settings);
 }
 
+static GtkSettings *
+gtk_settings_create_for_display (GdkDisplay *display)
+{
+  GtkSettings *settings;
+  DisplaySettings v;
+
+#ifdef GDK_WINDOWING_QUARTZ
+  if (GDK_IS_QUARTZ_DISPLAY (display))
+    settings = g_object_new (GTK_TYPE_SETTINGS,
+                             "gtk-key-theme-name", "Mac",
+                             "gtk-shell-shows-app-menu", TRUE,
+                             "gtk-shell-shows-menubar", TRUE,
+                             NULL);
+  else
+#endif
+#ifdef GDK_WINDOWING_BROADWAY
+    if (GDK_IS_BROADWAY_DISPLAY (display))
+      settings = g_object_new (GTK_TYPE_SETTINGS,
+                               "gtk-im-module", "broadway",
+                               NULL);
+  else
+#endif
+    settings = g_object_new (GTK_TYPE_SETTINGS, NULL);
+
+  settings->priv->screen = gdk_display_get_default_screen (display);
+
+  v.display = display;
+  v.settings = settings;
+  g_array_append_val (display_settings, v);
+
+  settings_init_style (settings);
+  settings_update_xsettings (settings);
+  settings_update_modules (settings);
+  settings_update_double_click (settings);
+  settings_update_cursor_theme (settings);
+  settings_update_resolution (settings);
+  settings_update_font_options (settings);
+
+  return settings;
+}
+
+static GtkSettings *
+gtk_settings_get_for_display (GdkDisplay *display)
+{
+  DisplaySettings *ds;
+  int i;
+
+  if G_UNLIKELY (display_settings == NULL)
+    display_settings = g_array_new (FALSE, TRUE, sizeof (DisplaySettings));
+
+  ds = (DisplaySettings *)display_settings->data;
+  for (i = 0; i < display_settings->len; i++)
+    {
+      if (ds[i].display == display)
+        return ds[i].settings;
+    }
+
+  return gtk_settings_create_for_display (display);
+}
+
 /**
  * gtk_settings_get_for_screen:
  * @screen: a #GdkScreen.
@@ -1878,53 +1945,12 @@ settings_init_style (GtkSettings *settings)
  *
  * Since: 2.2
  */
-GtkSettings*
+GtkSettings *
 gtk_settings_get_for_screen (GdkScreen *screen)
 {
-  GtkSettings *settings;
-
   g_return_val_if_fail (GDK_IS_SCREEN (screen), NULL);
 
-  settings = g_object_get_qdata (G_OBJECT (screen), quark_gtk_settings);
-  if (!settings)
-    {
-#ifdef GDK_WINDOWING_QUARTZ
-      if (GDK_IS_QUARTZ_SCREEN (screen))
-        {
-          settings = g_object_new (GTK_TYPE_SETTINGS,
-                                   "gtk-key-theme-name", "Mac",
-                                   "gtk-shell-shows-app-menu", TRUE,
-                                   "gtk-shell-shows-menubar", TRUE,
-                                   NULL);
-        }
-      else
-#endif
-#ifdef GDK_WINDOWING_BROADWAY
-      if (GDK_IS_BROADWAY_DISPLAY (gdk_screen_get_display (screen)))
-        {
-          settings = g_object_new (GTK_TYPE_SETTINGS,
-                                   "gtk-im-module", "broadway",
-                                   NULL);
-        }
-      else
-#endif
-        settings = g_object_new (GTK_TYPE_SETTINGS, NULL);
-      settings->priv->screen = screen;
-      g_object_set_qdata_full (G_OBJECT (screen),
-                               quark_gtk_settings,
-                               settings,
-                               g_object_unref);
-
-      settings_init_style (settings);
-      settings_update_xsettings (settings);
-      settings_update_modules (settings);
-      settings_update_double_click (settings);
-      settings_update_cursor_theme (settings);
-      settings_update_resolution (settings);
-      settings_update_font_options (settings);
-    }
-
-  return settings;
+  return gtk_settings_get_for_display (gdk_screen_get_display (screen));
 }
 
 /**
@@ -1939,10 +1965,10 @@ gtk_settings_get_for_screen (GdkScreen *screen)
 GtkSettings*
 gtk_settings_get_default (void)
 {
-  GdkScreen *screen = gdk_screen_get_default ();
+  GdkDisplay *display = gdk_display_get_default ();
 
-  if (screen)
-    return gtk_settings_get_for_screen (screen);
+  if (display)
+    return gtk_settings_get_for_display (display);
   else
     return NULL;
 }

commit b1d55f2c85203d92cecd0bc4112a78e7d8abc3a3

gtksettings: Remove display from cache on closing
GTK caches the settings per display in a static `GArray`, keeping a
reference to the `GdkDisplay` as the key.

However, when closing the display, the corresponding entry is not
removed from the cache in `GtkSettings`.

So when reopening again a `GdkDisplay`, if the new address matches one
of the previously closed display, the cache will return the existing
`GtkSettings` from the cache, which still holds a reference to the old
`GdkScreen` which was freed along the `GdkDisplay`.

To avoid the issue, make sure to remove the `GdkDisplay` and
corresponding `GdkSettings` when closing the `GdkDisplay`.

Also, care must be taken not to recreate the `GdkSettings` and re-add
the `GdkDisplay` to the cache once the display is closed, and make sure
callers of `gtk_settings_get_for_display()` can deal with a returned
value being `NULL` if the display is closed.

Fixes: commit 360a3c16902 - "Use a cheaper way to store settings per
display"

gtk/gtkicontheme.c
@@ -608,9 +608,10 @@ unset_screen (GtkIconTheme *icon_theme)
       g_signal_handlers_disconnect_by_func (display,
                                             (gpointer) display_closed,
                                             icon_theme);
-      g_signal_handlers_disconnect_by_func (settings,
-                                            (gpointer) theme_changed,
-                                            icon_theme);
+      if (settings)
+        g_signal_handlers_disconnect_by_func (settings,
+                                              (gpointer) theme_changed,
+                                              icon_theme);
 
       priv->screen = NULL;
     }

gtk/gtkmodules.c
@@ -463,10 +463,10 @@ display_closed_cb (GdkDisplay *display,
 
   screen = gdk_display_get_default_screen (display);
   settings = gtk_settings_get_for_screen (screen);
-
-  g_object_set_data_full (G_OBJECT (settings),
-			  I_("gtk-modules"),
-			  NULL, NULL);
+  if (settings)
+    g_object_set_data_full (G_OBJECT (settings),
+			    I_("gtk-modules"),
+			    NULL, NULL);
 }
 		   
 

gtk/gtksettings.c
@@ -1900,6 +1900,29 @@ settings_init_style (GtkSettings *settings)
   settings_update_key_theme (settings);
 }
 
+static void
+settings_display_closed (GdkDisplay *display,
+                         gboolean    is_error,
+                         gpointer    data)
+{
+  DisplaySettings *ds;
+  int i;
+
+  if (G_UNLIKELY (display_settings == NULL))
+    return;
+
+  ds = (DisplaySettings *)display_settings->data;
+  for (i = 0; i < display_settings->len; i++)
+    {
+      if (ds[i].display == display)
+        {
+          g_clear_object (&ds[i].settings);
+          display_settings = g_array_remove_index_fast (display_settings, i);
+          break;
+        }
+    }
+}
+
 static GtkSettings *
 gtk_settings_create_for_display (GdkDisplay *display)
 {
@@ -1955,7 +1978,9 @@ gtk_settings_create_for_display (GdkDisplay *display)
 
   v.display = display;
   v.settings = settings;
-  g_array_append_val (display_settings, v);
+  display_settings = g_array_append_val (display_settings, v);
+  g_signal_connect (display, "closed",
+                    G_CALLBACK (settings_display_closed), NULL);
 
   settings_init_style (settings);
   settings_update_xsettings (settings);
@@ -1974,6 +1999,10 @@ gtk_settings_get_for_display (GdkDisplay *display)
 {
   DisplaySettings *ds;
   int i;
+
+  /* If the display is closed, we don't want to recreate the settings! */
+  if G_UNLIKELY (gdk_display_is_closed (display))
+    return NULL;
 
   if G_UNLIKELY (display_settings == NULL)
     display_settings = g_array_new (FALSE, TRUE, sizeof (DisplaySettings));