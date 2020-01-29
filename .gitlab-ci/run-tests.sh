33473fd3d115d21f861fb5ef312e8ed14d26341e
Emmanuele Bassi <ebassi@gnome.org>
2020-01-29 14:36:23 +0100

    ci: Generate the report for the release builds
    We are going to need to hide the report generation into the test runner
    script, as we want the job to produce the reports even in case of
    failure, instead of bailing out immediately.

--- .gitlab-ci/run-tests.sh
+++ .gitlab-ci/run-tests.sh
@@ -4,8 +4,26 @@ set -e
 
 builddir=$1
 
-cd ${builddir}
 xvfb-run -a -s "-screen 0 1024x768x24" \
-        meson test --print-errorlogs \
+        meson test -C ${builddir} \
+                --print-errorlogs \
                 --suite=gtk \
                 --no-suite=gtk:a11y
+
+# Store the exit code for the CI run, but always
+# generate the reports
+exit_code=$?
+
+.gitlab-ci/meson-junit-report.py \
+        --project-name=gtk \
+        --job-id="${CI_JOB_NAME}" \
+        --output=${builddir}/report.xml \
+        ${builddir}/meson-logs/testlog.json
+.gitlab-ci/meson-html-report.py \
+        --project-name=gtk \
+        --job-id="${CI_JOB_NAME}" \
+        --reftest-output-dir="${builddir}/testsuite/reftests/output" \
+        --output=${builddir}/report.html \
+        ${builddir}/meson-logs/testlog.json
+
+exit $exit_code
