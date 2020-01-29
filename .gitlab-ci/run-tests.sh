105ffb2631141487700ae1fb7c9bac87a8fcfca6
Emmanuele Bassi <ebassi@gnome.org>
2020-01-29 12:31:00 +0100

    ci: Add missing newline escape
    Otherwise we're going to run the accessibility test suite, and it's
    going to fail.

--- .gitlab-ci/run-tests.sh
+++ .gitlab-ci/run-tests.sh
@@ -7,5 +7,5 @@ builddir=$1
 cd ${builddir}
 xvfb-run -a -s "-screen 0 1024x768x24" \
         meson test --print-errorlogs \
-                --suite=gtk
+                --suite=gtk \
                 --no-suite=gtk:a11y
