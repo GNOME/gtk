51e155bffaf2697ced9e697bb025d0ae2fc348af
Emmanuele Bassi <ebassi@gnome.org>
2020-01-29 09:16:57 +0100

    ci: Deploy the API reference on our GitLab pages
    Now that we can build gtk-doc as a sub-project, we can go back to
    publishing the API reference for the master branch every time we
    build it.

--- .gitlab-ci/run-docker.sh
+++ .gitlab-ci/run-docker.sh
@@ -2,7 +2,7 @@
 
 set -e
 
-TAG="registry.gitlab.gnome.org/gnome/gtk/master:v7"
+TAG="registry.gitlab.gnome.org/gnome/gtk/master:v10"
 
 sudo docker build --build-arg HOST_USER_ID="$UID" --tag "${TAG}" \
     --file "Dockerfile" .
