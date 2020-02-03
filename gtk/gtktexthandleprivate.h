fa1699b3855a38035b56ce2b96c6094d25d235fb
Carlos Garnacho <carlosg@gnome.org>
2020-02-03 15:59:49 +0100

    gtktexthandle: Refactor and use native surfaces
    Instead of being a GObject managing two GtkWidgets, make GtkTextHandle
    a GtkWidget subclass, representing a single handle.
    
    From the perspective of users (GtkText and GtkTextView), this is not a
    big leap since they have to be aware of a great deal of text handles'
    state. It actually makes things more direct and simple.
    
    With text handles being widgets, those can be actual children of the
    widget, and may have their own GdkSurface that we move around at will.
    This is the second major aspect of this refactor.

--- gtk/gtktexthandleprivate.h
+++ gtk/gtktexthandleprivate.h
@@ -22,68 +22,27 @@
 
 G_BEGIN_DECLS
 
-#define GTK_TYPE_TEXT_HANDLE           (_gtk_text_handle_get_type ())
-#define GTK_TEXT_HANDLE(o)             (G_TYPE_CHECK_INSTANCE_CAST ((o), GTK_TYPE_TEXT_HANDLE, GtkTextHandle))
-#define GTK_TEXT_HANDLE_CLASS(c)       (G_TYPE_CHECK_CLASS_CAST ((c), GTK_TYPE_TEXT_HANDLE, GtkTextHandleClass))
-#define GTK_IS_TEXT_HANDLE(o)          (G_TYPE_CHECK_INSTANCE_TYPE ((o), GTK_TYPE_TEXT_HANDLE))
-#define GTK_IS_TEXT_HANDLE_CLASS(o)    (G_TYPE_CHECK_CLASS_TYPE ((o), GTK_TYPE_TEXT_HANDLE))
-#define GTK_TEXT_HANDLE_GET_CLASS(o)   (G_TYPE_INSTANCE_GET_CLASS ((o), GTK_TYPE_TEXT_HANDLE, GtkTextHandleClass))
-
-typedef struct _GtkTextHandle GtkTextHandle;
-typedef struct _GtkTextHandleClass GtkTextHandleClass;
+#define GTK_TYPE_TEXT_HANDLE (gtk_text_handle_get_type ())
+G_DECLARE_FINAL_TYPE (GtkTextHandle, gtk_text_handle,
+		      GTK, TEXT_HANDLE, GtkWidget)
 
 typedef enum
 {
-  GTK_TEXT_HANDLE_POSITION_CURSOR,
-  GTK_TEXT_HANDLE_POSITION_SELECTION_START,
-  GTK_TEXT_HANDLE_POSITION_SELECTION_END = GTK_TEXT_HANDLE_POSITION_CURSOR
-} GtkTextHandlePosition;
+  GTK_TEXT_HANDLE_ROLE_CURSOR,
+  GTK_TEXT_HANDLE_ROLE_SELECTION_START,
+  GTK_TEXT_HANDLE_ROLE_SELECTION_END,
+} GtkTextHandleRole;
 
-typedef enum
-{
-  GTK_TEXT_HANDLE_MODE_NONE,
-  GTK_TEXT_HANDLE_MODE_CURSOR,
-  GTK_TEXT_HANDLE_MODE_SELECTION
-} GtkTextHandleMode;
+GtkTextHandle *    gtk_text_handle_new          (GtkWidget             *parent);
 
-struct _GtkTextHandle
-{
-  GObject parent_instance;
-  gpointer priv;
-};
+void               gtk_text_handle_set_role (GtkTextHandle     *handle,
+					     GtkTextHandleRole  role);
+GtkTextHandleRole  gtk_text_handle_get_role (GtkTextHandle     *handle);
 
-struct _GtkTextHandleClass
-{
-  GObjectClass parent_class;
+void               gtk_text_handle_set_position (GtkTextHandle      *handle,
+						 const GdkRectangle *rect);
 
-  void (* handle_dragged) (GtkTextHandle         *handle,
-                           GtkTextHandlePosition  pos,
-                           gint                   x,
-                           gint                   y);
-  void (* drag_finished)  (GtkTextHandle         *handle,
-                           GtkTextHandlePosition  pos);
-};
-
-GType           _gtk_text_handle_get_type     (void) G_GNUC_CONST;
-
-GtkTextHandle * _gtk_text_handle_new          (GtkWidget             *parent);
-
-void            _gtk_text_handle_set_mode     (GtkTextHandle         *handle,
-                                               GtkTextHandleMode      mode);
-GtkTextHandleMode
-                _gtk_text_handle_get_mode     (GtkTextHandle         *handle);
-void            _gtk_text_handle_set_position (GtkTextHandle         *handle,
-                                               GtkTextHandlePosition  pos,
-                                               GdkRectangle          *rect);
-void            _gtk_text_handle_set_visible  (GtkTextHandle         *handle,
-                                               GtkTextHandlePosition  pos,
-                                               gboolean               visible);
-
-gboolean        _gtk_text_handle_get_is_dragged (GtkTextHandle         *handle,
-                                                 GtkTextHandlePosition  pos);
-void            _gtk_text_handle_set_direction (GtkTextHandle         *handle,
-                                                GtkTextHandlePosition  pos,
-                                                GtkTextDirection       dir);
+gboolean           gtk_text_handle_get_is_dragged (GtkTextHandle *handle);
 
 G_END_DECLS
 
