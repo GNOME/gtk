327441fa3e5edf81c3cf7e03d45f14bfc4bbebb0
Emmanuele Bassi <ebassi@gnome.org>
2020-02-06 15:02:02 +0000

    cssprovider: Remove unused include

--- gtk/gtkcssprovider.c
+++ gtk/gtkcssprovider.c
@@ -31,7 +31,6 @@
 #include "gtkstylecontextprivate.h"
 #include "gtkstylepropertyprivate.h"
 #include "gtkstyleproviderprivate.h"
-#include "gtkbindings.h"
 #include "gtkmarshalers.h"
 #include "gtkprivate.h"
 #include "gtkintl.h"
