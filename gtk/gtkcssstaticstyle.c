498f816f1f11c84f3a935f904e074527c5aa7896
Matthias Clasen <mclasen@redhat.com>
2020-02-11 19:06:24 -0500

    Keep an empty lookup singleton
    This is a simple optimization to avoid wasting a good
    chunk of memory (~16k in widget-factory) on identical
    structures. This is the one case that it easy to handle.

--- gtk/gtkcssstaticstyle.c
+++ gtk/gtkcssstaticstyle.c
@@ -335,6 +335,8 @@ gtk_css_static_style_get_static_style (GtkCssStyle *style)
   return (GtkCssStaticStyle *)style;
 }
 
+static GtkCssLookup *the_empty_lookup;
+
 static void
 gtk_css_static_style_class_init (GtkCssStaticStyleClass *klass)
 {
@@ -359,6 +361,8 @@ gtk_css_static_style_class_init (GtkCssStaticStyleClass *klass)
   gtk_css_other_values_init ();
 
   verify_style_groups ();
+
+  the_empty_lookup = gtk_css_lookup_new ();
 }
 
 static void
@@ -976,6 +980,8 @@ gtk_css_static_style_new_compute (GtkStyleProvider             *provider,
   GtkCssStaticStyle *result;
   GtkCssNode *parent;
 
+  result = g_object_new (GTK_TYPE_CSS_STATIC_STYLE, NULL);
+
   if (lookup != NULL && change != 0)
     {
       gtk_css_lookup_ref (lookup);
@@ -989,9 +995,13 @@ gtk_css_static_style_new_compute (GtkStyleProvider             *provider,
                                    node,
                                    lookup,
                                    change == 0 ? &change : NULL);
-    }
 
-  result = g_object_new (GTK_TYPE_CSS_STATIC_STYLE, NULL);
+      if (_gtk_bitmask_is_empty (gtk_css_lookup_get_set_values (lookup)))
+        {
+          gtk_css_lookup_unref (lookup);
+          lookup = gtk_css_lookup_ref (the_empty_lookup);
+        }
+    }
 
   result->lookup = lookup;
   result->change = change;
