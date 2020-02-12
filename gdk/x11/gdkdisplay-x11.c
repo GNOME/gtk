e3a67385f445050721878d82e0b3dc5b054fe52d
Alexander Larsson <alexl@redhat.com>
2020-02-12 12:44:43 +0100

    profiler: Add profiler marks for when surfaces are mapped and unmapped
    This is done on the X11 side so we can tell when the map/unmap takes
    effect and how long it took since the map request.

--- gdk/x11/gdkdisplay-x11.c
+++ gdk/x11/gdkdisplay-x11.c
@@ -881,6 +881,9 @@ gdk_x11_display_translate_event (GdkEventTranslator *translator,
             gdk_surface_freeze_updates (surface);
 
           _gdk_x11_surface_grab_check_unmap (surface, xevent->xany.serial);
+
+          if (GDK_PROFILER_IS_RUNNING)
+            gdk_profiler_add_markf (g_get_monotonic_time (), 0, "Unmapped window", "0x%lx", GDK_SURFACE_XID (surface));
         }
 
       return_val = FALSE;
@@ -902,6 +905,12 @@ gdk_x11_display_translate_event (GdkEventTranslator *translator,
 
 	  if (toplevel)
 	    gdk_surface_thaw_updates (surface);
+
+          if (GDK_PROFILER_IS_RUNNING)
+            {
+              gdk_profiler_end_markf (surface_impl->map_time, "Mapped window", "0x%lx", GDK_SURFACE_XID (surface));
+              surface_impl->map_time = 0;
+            }
 	}
 
       return_val = FALSE;
