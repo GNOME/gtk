8ebc26ca2511265e58c60d0515f4f62153d49dfc
Matthias Clasen <mclasen@redhat.com>
2020-02-15 10:43:29 -0500

    x11: Change the wm protocols filter api
    Instead of passing a half-constructed event and expect
    it to be filled in, pass the surface as in argument, and
    add an out argument for a newly constructed GdkEvent.

--- gdk/x11/gdkdisplay-x11.c
+++ gdk/x11/gdkdisplay-x11.c
@@ -1206,11 +1206,11 @@ server_time_to_monotonic_time (GdkX11Display *display_x11,
 }
 
 GdkFilterReturn
-_gdk_wm_protocols_filter (const XEvent *xevent,
-			  GdkEvent     *event,
-			  gpointer      data)
+_gdk_wm_protocols_filter (const XEvent  *xevent,
+                          GdkSurface    *win,
+                          GdkEvent     **event,
+                          gpointer       data)
 {
-  GdkSurface *win = event->any.surface;
   GdkDisplay *display;
   Atom atom;
 
@@ -1248,7 +1248,7 @@ _gdk_wm_protocols_filter (const XEvent *xevent,
           if (surface_impl->toplevel->frame_pending)
             {
               surface_impl->toplevel->frame_pending = FALSE;
-              gdk_surface_thaw_updates (event->any.surface);
+              gdk_surface_thaw_updates (win);
             }
 
           gdk_frame_clock_get_refresh_info (clock,
@@ -1320,7 +1320,7 @@ _gdk_wm_protocols_filter (const XEvent *xevent,
 		g_message ("delete window:\t\twindow: %ld",
 			   xevent->xclient.window));
 
-      event->any.type = GDK_DELETE;
+      *event = gdk_event_delete_new (win);
 
       gdk_x11_surface_set_user_time (win, xevent->xclient.data.l[1]);
 
