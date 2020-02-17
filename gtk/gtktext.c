b1a8c40adf9aff81d9121b38364c719a04d75103
Timm Bäder <mail@baedert.org>
2020-02-17 15:46:41 +0100

    text: Remove gtk_text_get_im_context()
    Unused.

--- gtk/gtktext.c
+++ gtk/gtktext.c
@@ -4683,14 +4683,6 @@ gtk_text_reset_im_context (GtkText *self)
     }
 }
 
-GtkIMContext *
-gtk_text_get_im_context (GtkText *self)
-{
-  GtkTextPrivate *priv = gtk_text_get_instance_private (self);
-
-  return priv->im_context;
-}
-
 static int
 gtk_text_find_position (GtkText *self,
                         int      x)
