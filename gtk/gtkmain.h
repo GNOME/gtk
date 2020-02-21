5ebe5be225868ce0b5adbe05a3b5b6e69252042a
Matthias Clasen <mclasen@redhat.com>
2020-02-20 21:28:05 -0500

    Stop exporting gtk_get_event_widget
    This is non-essential convenience API, and we
    don't really expose events to applications anymore.

--- gtk/gtkmain.h
+++ gtk/gtkmain.h
@@ -131,8 +131,6 @@ gboolean   gtk_get_current_event_state  (GdkModifierType *state);
 GDK_AVAILABLE_IN_ALL
 GdkDevice *gtk_get_current_event_device (void);
 
-GDK_AVAILABLE_IN_ALL
-GtkWidget *gtk_get_event_widget         (GdkEvent  *event);
 
 G_END_DECLS
 
