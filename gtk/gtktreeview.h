fdb39b095b5e3fb2c66f0e81d172310c21c46ece
Benjamin Otte <otte@redhat.com>
2020-02-28 04:19:36 +0100

    treeview, iconview: Don't return the drop target
    Not a good idea to hand internal event controllers out to public API.

--- gtk/gtktreeview.h
+++ gtk/gtktreeview.h
@@ -26,8 +26,6 @@
 #include <gtk/gtktreemodel.h>
 #include <gtk/gtktreeviewcolumn.h>
 #include <gtk/gtkentry.h>
-#include <gtk/gtkdragsource.h>
-#include <gtk/gtkdragdest.h>
 
 G_BEGIN_DECLS
 
@@ -320,7 +318,7 @@ void                   gtk_tree_view_enable_model_drag_source      (GtkTreeView 
 								    GdkContentFormats         *formats,
 								    GdkDragAction              actions);
 GDK_AVAILABLE_IN_ALL
-GtkDropTarget *        gtk_tree_view_enable_model_drag_dest        (GtkTreeView               *tree_view,
+void                   gtk_tree_view_enable_model_drag_dest        (GtkTreeView               *tree_view,
 								    GdkContentFormats         *formats,
 								    GdkDragAction              actions);
 GDK_AVAILABLE_IN_ALL
