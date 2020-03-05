f6b11a51581cf443208db5a861b63c9e9bc5e384
Benjamin Otte <otte@redhat.com>
2020-03-05 03:58:29 +0100

    build: Add more useful warning flags
    I found those on the interwebs and decided they are useful.

--- gdk/gdkrgbaprivate.h
+++ gdk/gdkrgbaprivate.h
@@ -28,7 +28,7 @@
                                         ((c) >= 'a' && (c) <= 'f') ? ((c)-'a'+10) : \
                                         ((c) >= '0' && (c) <= '9') ? ((c)-'0') : \
                                         -1))
-#define _GDK_RGBA_SELECT_COLOR(_str, index3, index6) _GDK_RGBA_DECODE (sizeof(_str) <= 4 ? (_str)[index3] : (_str)[index6])
+#define _GDK_RGBA_SELECT_COLOR(_str, index3, index6) (sizeof(_str) <= 4 ? _GDK_RGBA_DECODE ((_str)[index3]) : _GDK_RGBA_DECODE ((_str)[index6]))
 #define GDK_RGBA(str) ((GdkRGBA) {\
     ((_GDK_RGBA_SELECT_COLOR(str, 0, 0) << 4) | _GDK_RGBA_SELECT_COLOR(str, 0, 1)) / 255., \
     ((_GDK_RGBA_SELECT_COLOR(str, 1, 2) << 4) | _GDK_RGBA_SELECT_COLOR(str, 1, 3)) / 255., \
