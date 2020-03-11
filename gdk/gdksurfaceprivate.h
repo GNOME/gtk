a7865d17dcc8263be5617980eacd36a612096783
Matthias Clasen <mclasen@redhat.com>
2020-03-11 16:19:58 -0400

    surface: Drop unused vfuncs
    These are now entirely unused.

--- gdk/gdksurfaceprivate.h
+++ gdk/gdksurfaceprivate.h
@@ -113,24 +113,7 @@ struct _GdkSurfaceClass
   cairo_surface_t *
                (* ref_cairo_surface)    (GdkSurface       *surface);
 
-  void         (* show)                 (GdkSurface       *surface,
-                                         gboolean         already_mapped);
   void         (* hide)                 (GdkSurface       *surface);
-  void         (* withdraw)             (GdkSurface       *surface);
-  void         (* raise)                (GdkSurface       *surface);
-  void         (* lower)                (GdkSurface       *surface);
-  void         (* restack_toplevel)     (GdkSurface       *surface,
-                                         GdkSurface       *sibling,
-                                         gboolean        above);
-
-  void         (* toplevel_resize)      (GdkSurface      *surface,
-                                         gint             width,
-                                         gint             height);
-  gboolean     (* present_popup)        (GdkSurface     *surface,
-                                         int             width,
-                                         int             height,
-                                         GdkPopupLayout *layout);
-
   void         (* get_geometry)         (GdkSurface       *surface,
                                          gint            *x,
                                          gint            *y,
@@ -150,14 +133,14 @@ struct _GdkSurfaceClass
   void         (* set_input_region)     (GdkSurface      *surface,
                                          cairo_region_t  *shape_region);
 
-/* Called to do the windowing system specific part of gdk_surface_destroy(),
- *
- * surface: The window being destroyed
- * foreign_destroy: If TRUE, the surface or a parent was destroyed by some
- *     external agency. The surface has already been destroyed and no
- *     windowing system calls should be made. (This may never happen
- *     for some windowing systems.)
- */
+  /* Called to do the windowing system specific part of gdk_surface_destroy(),
+   *
+   * surface: The window being destroyed
+   * foreign_destroy: If TRUE, the surface or a parent was destroyed by some
+   *     external agency. The surface has already been destroyed and no
+   *     windowing system calls should be made. (This may never happen
+   *     for some windowing systems.)
+   */
   void         (* destroy)              (GdkSurface       *surface,
                                          gboolean         foreign_destroy);
 
@@ -165,53 +148,7 @@ struct _GdkSurfaceClass
   /* optional */
   gboolean     (* beep)                 (GdkSurface       *surface);
 
-  void         (* focus)                (GdkSurface       *surface,
-                                         guint32          timestamp);
-  void         (* set_type_hint)        (GdkSurface       *surface,
-                                         GdkSurfaceTypeHint hint);
-  GdkSurfaceTypeHint (* get_type_hint)   (GdkSurface       *surface);
-  void         (* set_modal_hint)       (GdkSurface *surface,
-                                         gboolean   modal);
-  void         (* set_geometry_hints)   (GdkSurface         *surface,
-                                         const GdkGeometry *geometry,
-                                         GdkSurfaceHints     geom_mask);
-  void         (* set_title)            (GdkSurface   *surface,
-                                         const gchar *title);
-  void         (* set_startup_id)       (GdkSurface   *surface,
-                                         const gchar *startup_id);
-  void         (* set_transient_for)    (GdkSurface *surface,
-                                         GdkSurface *parent);
-  void         (* get_frame_extents)    (GdkSurface    *surface,
-                                         GdkRectangle *rect);
-  void         (* set_accept_focus)     (GdkSurface *surface,
-                                         gboolean accept_focus);
-  void         (* set_focus_on_map)     (GdkSurface *surface,
-                                         gboolean focus_on_map);
-  void         (* set_icon_list)        (GdkSurface *surface,
-                                         GList     *pixbufs);
-  void         (* set_icon_name)        (GdkSurface   *surface,
-                                         const gchar *name);
-  void         (* minimize)             (GdkSurface *surface);
-  void         (* unminimize)           (GdkSurface *surface);
-  void         (* stick)                (GdkSurface *surface);
-  void         (* unstick)              (GdkSurface *surface);
-  void         (* maximize)             (GdkSurface *surface);
-  void         (* unmaximize)           (GdkSurface *surface);
-  void         (* fullscreen)           (GdkSurface *surface);
-  void         (* fullscreen_on_monitor) (GdkSurface  *surface,
-                                          GdkMonitor *monitor);
   void         (* apply_fullscreen_mode) (GdkSurface *surface);
-  void         (* unfullscreen)         (GdkSurface *surface);
-  void         (* set_keep_above)       (GdkSurface *surface,
-                                         gboolean   setting);
-  void         (* set_keep_below)       (GdkSurface *surface,
-                                         gboolean   setting);
-  void         (* set_decorations)      (GdkSurface      *surface,
-                                         GdkWMDecoration decorations);
-  gboolean     (* get_decorations)      (GdkSurface       *surface,
-                                         GdkWMDecoration *decorations);
-  void         (* set_functions)        (GdkSurface    *surface,
-                                         GdkWMFunction functions);
   void         (* begin_resize_drag)    (GdkSurface     *surface,
                                          GdkSurfaceEdge  edge,
                                          GdkDevice     *device,
@@ -225,10 +162,8 @@ struct _GdkSurfaceClass
                                          gint       root_x,
                                          gint       root_y,
                                          guint32    timestamp);
-  void         (* set_opacity)          (GdkSurface *surface,
-                                         gdouble    opacity);
   void         (* destroy_notify)       (GdkSurface *surface);
-  GdkDrag * (*drag_begin)               (GdkSurface        *surface,
+  GdkDrag *    (* drag_begin)           (GdkSurface        *surface,
                                          GdkDevice        *device,
                                          GdkContentProvider*content,
                                          GdkDragAction     actions,
@@ -247,8 +182,6 @@ struct _GdkSurfaceClass
                                            gint            right,
                                            gint            top,
                                            gint            bottom);
-  gboolean     (* show_window_menu)       (GdkSurface      *surface,
-                                           GdkEvent       *event);
   GdkGLContext *(*create_gl_context)      (GdkSurface      *surface,
                                            gboolean        attached,
                                            GdkGLContext   *share,
