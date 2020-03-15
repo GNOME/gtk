79f4db19f5143d0e9fe04bbbea0dcae7434e0888
Matthias Clasen <mclasen@redhat.com>
2020-03-15 17:38:56 -0400

    Move shortcut manager initialization code
    It is just too ugly to use quarks across multiple
    source files, so add a private helper function that
    attaches the controllers.

---
+++ gtk/gtkshortcutmanagerprivate.h
@@ -0,0 +1,12 @@
+#ifndef __GTK_SHORTCUT_MANAGER_PRIVATE_H__
+#define __GTK_SHORTCUT_MANAGER__PRIVATE_H__
+
+#include "gtkshortcutmanager.h"
+
+G_BEGIN_DECLS
+
+void gtk_shortcut_manager_create_controllers (GtkWidget *widget);
+
+G_END_DECLS
+
+#endif /* __GTK_SHORTCUT_MANAGER_PRIVATE_H__ */
