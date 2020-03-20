c3fe41c1d886d5be0f7d44dc0f170a5ab98efaf6
Olivier Fourdan <ofourdan@redhat.com>
2020-03-20 15:17:41 +0100

    gdk: Add gdk_toplevel_inhibit_system_shortcuts API
    With the removal of grabs from the public API, we need a replacement API
    to let applications bypass system keyboard shortcuts.
    
    A typical use case for this API is remote desktop or virtual machine
    viewers which need to inhibit the default system keyboard shortcuts so
    that the remote session or virtual host gets those instead of the local
    environment.
    
    Close: https://gitlab.gnome.org/GNOME/gtk/issues/982

--- gdk/gdktoplevel.h
+++ gdk/gdktoplevel.h
@@ -24,6 +24,7 @@
 #error "Only <gdk/gdk.h> can be included directly."
 #endif
 
+#include <gdk/gdkseat.h>
 #include <gdk/gdksurface.h>
 #include <gdk/gdktoplevellayout.h>
 
@@ -86,6 +87,16 @@ void          gdk_toplevel_set_deletable         (GdkToplevel      *toplevel,
                                                   gboolean          deletable);
 GDK_AVAILABLE_IN_ALL
 gboolean      gdk_toplevel_supports_edge_constraints (GdkToplevel *toplevel);
+
+GDK_AVAILABLE_IN_ALL
+gboolean      gdk_toplevel_inhibit_system_shortcuts  (GdkToplevel *toplevel,
+                                                      GdkSeat     *seat,
+                                                      GdkEvent    *event);
+
+GDK_AVAILABLE_IN_ALL
+void          gdk_toplevel_restore_system_shortcuts  (GdkToplevel *toplevel,
+                                                      GdkSeat     *seat);
+
 
 G_END_DECLS
 
