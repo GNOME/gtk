GNOME/gtk

commit 7b1b3fa0cbd31901085122ec04f3182edd8ac286

colorpickerportal: Use convenience function to fetch uint
Rather than open coding it.

gtk/gtkcolorpickerportal.c
@@ -81,7 +81,7 @@ gtk_color_picker_portal_initable_init (GInitable     *initable,
   g_free (owner);
 
   ret = g_dbus_proxy_get_cached_property (picker->portal_proxy, "version");
-  g_variant_get (ret, "u", &version);
+  version = g_variant_get_uint32 (ret);
   g_variant_unref (ret);
   if (version != 2)
     {

commit fa16b682ef12a401b152f361b3cef7392619cdae

colorpickerportal: Remove g_auto usage
Fixes: 7b1b3fa0cbd31901085122ec04f3182edd8ac286

gtk/gtkcolorpickerportal.c
@@ -49,8 +49,8 @@ gtk_color_picker_portal_initable_init (GInitable     *initable,
                                        GError       **error)
 {
   GtkColorPickerPortal *picker = GTK_COLOR_PICKER_PORTAL (initable);
-  g_autofree char *owner = NULL;
-  g_autoptr(GVariant) ret = NULL;
+  char *owner;
+  GVariant *ret;
   guint version = 0;
 
   if (!gtk_should_use_portal ())
@@ -78,10 +78,14 @@ gtk_color_picker_portal_initable_init (GInitable     *initable,
       g_clear_object (&picker->portal_proxy);
       return FALSE;
     }
+  g_free (owner);
 
   ret = g_dbus_proxy_get_cached_property (picker->portal_proxy, "version");
   if (ret)
-    version = g_variant_get_uint32 (ret);
+    {
+      version = g_variant_get_uint32 (ret);
+      g_variant_unref (ret);
+    }
 
   if (version != 2)
     {