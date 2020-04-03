339d3e41b028344e647d7a5795ace4eb53085ddd
Matthias Clasen <mclasen@redhat.com>
2020-04-03 15:14:26 -0400

    Add GtkScrollbarAccessible
    When GtkScrollbar stopped being a GtkRange,
    GtkRangeAccessible stopped working for scrollbars.
    
    Update a11y tests to match.

--- gtk/gtkscrollbar.c
+++ gtk/gtkscrollbar.c
@@ -35,6 +35,8 @@
 #include "gtkprivate.h"
 #include "gtkboxlayout.h"
 
+#include "a11y/gtkscrollbaraccessible.h"
+
 
 /**
  * SECTION:gtkscrollbar
@@ -204,7 +206,7 @@ gtk_scrollbar_class_init (GtkScrollbarClass *class)
 
   g_object_class_override_property (object_class, PROP_ORIENTATION, "orientation");
 
-  gtk_widget_class_set_accessible_role (widget_class, ATK_ROLE_SCROLL_BAR);
+  gtk_widget_class_set_accessible_type (widget_class, GTK_TYPE_SCROLLBAR_ACCESSIBLE);
   gtk_widget_class_set_css_name (widget_class, I_("scrollbar"));
   gtk_widget_class_set_layout_manager_type (widget_class, GTK_TYPE_BOX_LAYOUT);
 }
