5a52af20cba76474e631b2a7548963bcad22d66d
William Wold <wm@wmww.sh>
2020-04-05 07:11:45 -0400

    Do not require an initial configure for DnD windows
    Fixes #2075

--- gdk/wayland/gdkwindow-wayland.c
+++ gdk/wayland/gdkwindow-wayland.c
@@ -1089,6 +1089,8 @@ needs_initial_configure (GdkWindow *window)
     return FALSE;
   else if (impl->use_custom_surface)
     return FALSE;
+  else if (impl->hint == GDK_WINDOW_TYPE_HINT_DND)
+    return FALSE;
   else if (is_realized_toplevel (window))
     return TRUE;
   else if (is_realized_popup (window))
