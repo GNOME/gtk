5f65b0a6edf172ee320349ed956abea7e0d5ed74
Timm Bäder <mail@baedert.org>
2020-04-10 16:33:12 +0200

    label: Avoid duplicating a string
    We only use str_for_accel if with_uline is TRUE.

--- gtk/gtklabel.c
+++ gtk/gtklabel.c
@@ -2327,7 +2327,6 @@ gtk_label_set_markup_internal (GtkLabel    *label,
       return;
     }
 
-  str_for_accel = g_strdup (str_for_display);
 
   if (links)
     {
@@ -2343,6 +2342,8 @@ gtk_label_set_markup_internal (GtkLabel    *label,
     {
       gboolean enable_mnemonics = TRUE;
       gboolean auto_mnemonics = TRUE;
+
+      str_for_accel = g_strdup (str_for_display);
 
       if (!(enable_mnemonics && priv->mnemonics_visible &&
             (!auto_mnemonics ||
