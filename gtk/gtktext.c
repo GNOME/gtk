GNOME/gtk

commit c297d45b8a19d84e0bc04ffad17df1382d7a9bf7

gtk: Stop using modifier intents
Reviewing the existing settings, the only backend with
some differences in the modifier intent settings is OS X,
and we would rather have that implemented by interpreting
the existing modifiers in the appropriate way.

                X11      Wayland  Win32    OS X

primary         ctrl     ctrl     ctrl     mod2
mnemonic        alt      alt      alt      alt
context menu    -        -        -        ctrl
extend sel      shift    shift    shift    shift
modify sel      ctrl     ctrl     ctrl     mod2
no text         alt|ctrl alt|ctrl alt|ctrl mod2|ctrl
shift group     varies   -        -        alt

GTK now uses the following modifiers:

primary         ctrl
mnemonic        alt
extend sel      shift
modify sel      ctrl
no text         alt|ctrl

The context menu and shift group intents were not used
in GTK at all.

Update tests to no longer expect <Primary> to roundtrip
through the accelerator parsing and formatting code.

gtk/gtktext.c
@@ -2657,7 +2657,6 @@ gtk_text_click_gesture_pressed (GtkGestureClick *gesture,
       gboolean have_selection;
       gboolean is_touchscreen, extend_selection;
       GdkDevice *source;
-      guint state;
 
       sel_start = priv->selection_bound;
       sel_end = priv->current_pos;
@@ -2673,12 +2672,7 @@ gtk_text_click_gesture_pressed (GtkGestureClick *gesture,
       priv->select_words = FALSE;
       priv->select_lines = FALSE;
 
-      state = gdk_event_get_modifier_state (event);
-
-      extend_selection =
-        (state &
-         gtk_widget_get_modifier_mask (widget,
-                                       GDK_MODIFIER_INTENT_EXTEND_SELECTION));
+      extend_selection = GDK_SHIFT_MASK;
 
       /* Always emit reset when preedit is shown */
       priv->need_im_reset = TRUE;

gtk/gtkaccelgroup.c
@@ -43,7 +43,12 @@
  */
 
 /* --- variables --- */
-static guint  default_accel_mod_mask     = 0;
+static guint default_accel_mod_mask = GDK_CONTROL_MASK|
+                                      GDK_SHIFT_MASK|
+                                      GDK_ALT_MASK|
+                                      GDK_SUPER_MASK|
+                                      GDK_HYPER_MASK|
+                                      GDK_META_MASK;
 
 
 /* --- functions --- */
@@ -276,7 +281,6 @@ gtk_accelerator_parse_with_keycode (const gchar     *accelerator,
   GdkModifierType mods;
   gint len;
   gboolean error;
-  GdkModifierType primary;
 
   if (accelerator_key)
     *accelerator_key = 0;
@@ -290,9 +294,6 @@ gtk_accelerator_parse_with_keycode (const gchar     *accelerator,
   if (!display)
     display = gdk_display_get_default ();
 
-  primary = gdk_display_get_modifier_mask (display,
-                                           GDK_MODIFIER_INTENT_PRIMARY_ACCELERATOR);
-
   error = FALSE;
   keyval = 0;
   mods = 0;
@@ -305,7 +306,7 @@ gtk_accelerator_parse_with_keycode (const gchar     *accelerator,
             {
               accelerator += 9;
               len -= 9;
-              mods |= primary;
+              mods |= GDK_CONTROL_MASK;
             }
           else if (len >= 9 && is_control (accelerator))
             {
@@ -578,7 +579,6 @@ gchar*
 gtk_accelerator_name (guint           accelerator_key,
                       GdkModifierType accelerator_mods)
 {
-  static const gchar text_primary[] = "<Primary>";
   static const gchar text_shift[] = "<Shift>";
   static const gchar text_control[] = "<Control>";
   static const gchar text_alt[] = "<Alt>";
@@ -589,10 +589,6 @@ gtk_accelerator_name (guint           accelerator_key,
   guint l;
   const char *keyval_name;
   gchar *accelerator;
-  GdkModifierType primary;
-
-  primary = gdk_display_get_modifier_mask (gdk_display_get_default (),
-                                           GDK_MODIFIER_INTENT_PRIMARY_ACCELERATOR);
 
   accelerator_mods &= GDK_MODIFIER_MASK;
 
@@ -602,11 +598,6 @@ gtk_accelerator_name (guint           accelerator_key,
 
   saved_mods = accelerator_mods;
   l = 0;
-  if (accelerator_mods & primary)
-    {
-      l += sizeof (text_primary) - 1;
-      accelerator_mods &= ~primary; /* consume the default accel */
-    }
   if (accelerator_mods & GDK_SHIFT_MASK)
     l += sizeof (text_shift) - 1;
   if (accelerator_mods & GDK_CONTROL_MASK)
@@ -625,12 +616,6 @@ gtk_accelerator_name (guint           accelerator_key,
   accelerator_mods = saved_mods;
   l = 0;
   accelerator[l] = 0;
-  if (accelerator_mods & primary)
-    {
-      strcpy (accelerator + l, text_primary);
-      l += sizeof (text_primary) - 1;
-      accelerator_mods &= ~primary; /* consume the default accel */
-    }
   if (accelerator_mods & GDK_SHIFT_MASK)
     {
       strcpy (accelerator + l, text_shift);
@@ -1045,19 +1030,5 @@ gtk_accelerator_set_default_mod_mask (GdkModifierType default_mod_mask)
 GdkModifierType
 gtk_accelerator_get_default_mod_mask (void)
 {
-  if (!default_accel_mod_mask)
-    {
-      GdkDisplay *display;
-
-      display = gdk_display_get_default ();
-
-      if (!display)
-        return GDK_CONTROL_MASK | GDK_SHIFT_MASK | GDK_ALT_MASK;
-
-      default_accel_mod_mask =
-          gdk_display_get_modifier_mask (display,
-                                         GDK_MODIFIER_INTENT_DEFAULT_MOD_MASK);
-    }
-
   return default_accel_mod_mask;
 }

gtk/gtkflowbox.c
@@ -142,10 +142,10 @@ get_current_selection_modifiers (GtkWidget *widget,
 
   if (gtk_get_current_event_state (&state))
     {
-      mask = gtk_widget_get_modifier_mask (widget, GDK_MODIFIER_INTENT_MODIFY_SELECTION);
+      mask = GDK_CONTROL_MASK;
       if ((state & mask) == mask)
         *modify = TRUE;
-      mask = gtk_widget_get_modifier_mask (widget, GDK_MODIFIER_INTENT_EXTEND_SELECTION);
+      mask = GDK_SHIFT_MASK;
       if ((state & mask) == mask)
         *extend = TRUE;
     }
@@ -2992,18 +2992,8 @@ gtk_flow_box_add_move_binding (GtkWidgetClass  *widget_class,
                                GtkMovementStep  step,
                                gint             count)
 {
-  GdkDisplay *display;
   GdkModifierType extend_mod_mask = GDK_SHIFT_MASK;
   GdkModifierType modify_mod_mask = GDK_CONTROL_MASK;
-
-  display = gdk_display_get_default ();
-  if (display != NULL)
-    {
-      extend_mod_mask = gdk_display_get_modifier_mask (display,
-                                                        GDK_MODIFIER_INTENT_EXTEND_SELECTION);
-      modify_mod_mask = gdk_display_get_modifier_mask (display,
-                                                       GDK_MODIFIER_INTENT_MODIFY_SELECTION);
-    }
 
   gtk_widget_class_add_binding_signal (widget_class,
                                        keyval, modmask,

gtk/gtkiconview.c
@@ -2125,7 +2125,7 @@ gtk_icon_view_button_press (GtkGestureClick *gesture,
   GtkCellRenderer *cell = NULL, *cursor_cell = NULL;
   int button = gtk_gesture_single_get_current_button (GTK_GESTURE_SINGLE (gesture));
   GdkEventSequence *sequence = gtk_gesture_single_get_current_sequence (GTK_GESTURE_SINGLE (gesture));
-  GdkEventButton *event = (GdkEventButton *)gtk_gesture_get_last_event (GTK_GESTURE (gesture), sequence);
+  GdkEvent *event = gtk_gesture_get_last_event (GTK_GESTURE (gesture), sequence);
   GdkModifierType state;
 
   if (!gtk_widget_has_focus (widget))
@@ -2133,15 +2133,10 @@ gtk_icon_view_button_press (GtkGestureClick *gesture,
 
   if (button == GDK_BUTTON_PRIMARY)
     {
-      GdkModifierType extend_mod_mask;
-      GdkModifierType modify_mod_mask;
+      GdkModifierType extend_mod_mask = GDK_SHIFT_MASK;
+      GdkModifierType modify_mod_mask = GDK_CONTROL_MASK;
 
-      state = gdk_event_get_modifier_state ((GdkEvent *)event);
-      extend_mod_mask =
-        gtk_widget_get_modifier_mask (widget, GDK_MODIFIER_INTENT_EXTEND_SELECTION);
-
-      modify_mod_mask =
-        gtk_widget_get_modifier_mask (widget, GDK_MODIFIER_INTENT_MODIFY_SELECTION);
+      state = gdk_event_get_modifier_state (event);
 
       item = _gtk_icon_view_get_item_at_widget_coords (icon_view,
                                                        x, y,
@@ -3468,15 +3463,8 @@ gtk_icon_view_real_move_cursor (GtkIconView     *icon_view,
 
   if (gtk_get_current_event_state (&state))
     {
-      GdkModifierType extend_mod_mask;
-      GdkModifierType modify_mod_mask;
-
-      extend_mod_mask =
-        gtk_widget_get_modifier_mask (GTK_WIDGET (icon_view),
-                                      GDK_MODIFIER_INTENT_EXTEND_SELECTION);
-      modify_mod_mask =
-        gtk_widget_get_modifier_mask (GTK_WIDGET (icon_view),
-                                      GDK_MODIFIER_INTENT_MODIFY_SELECTION);
+      GdkModifierType extend_mod_mask = GDK_SHIFT_MASK;
+      GdkModifierType modify_mod_mask = GDK_CONTROL_MASK;
 
       if ((state & modify_mod_mask) == modify_mod_mask)
         icon_view->priv->modify_selection_pressed = TRUE;

gtk/gtkimcontextsimple.c
@@ -1034,7 +1034,6 @@ gtk_im_context_simple_filter_keypress (GtkIMContext *context,
   GtkIMContextSimple *context_simple = GTK_IM_CONTEXT_SIMPLE (context);
   GtkIMContextSimplePrivate *priv = context_simple->priv;
   GdkSurface *surface = gdk_event_get_surface ((GdkEvent *) event);
-  GdkDisplay *display = gdk_surface_get_display (surface);
   GSList *tmp_list;
   int n_compose = 0;
   GdkModifierType hex_mod_mask;
@@ -1100,8 +1099,7 @@ gtk_im_context_simple_filter_keypress (GtkIMContext *context,
     if (keyval == gtk_compose_ignore[i])
       return FALSE;
 
-  hex_mod_mask = gdk_display_get_modifier_mask (display, GDK_MODIFIER_INTENT_PRIMARY_ACCELERATOR);
-  hex_mod_mask |= GDK_SHIFT_MASK;
+  hex_mod_mask = GDK_CONTROL_MASK|GDK_SHIFT_MASK;
 
   if (priv->in_hex_sequence && priv->modifiers_dropped)
     have_hex_mods = TRUE;
@@ -1132,7 +1130,7 @@ gtk_im_context_simple_filter_keypress (GtkIMContext *context,
     {
       GdkModifierType no_text_input_mask;
 
-      no_text_input_mask = gdk_display_get_modifier_mask (display, GDK_MODIFIER_INTENT_NO_TEXT_INPUT);
+      no_text_input_mask = GDK_ALT_MASK|GDK_CONTROL_MASK;
 
       if (state & no_text_input_mask ||
 	  (priv->in_hex_sequence && priv->modifiers_dropped &&

gtk/gtkimmulticontext.c
@@ -361,16 +361,12 @@ gtk_im_multicontext_filter_keypress (GtkIMContext *context,
     }
   else
     {
-      GdkDisplay *display;
       GdkModifierType no_text_input_mask;
 
       keyval = gdk_key_event_get_keyval (event);
       state = gdk_event_get_modifier_state (event);
-      display = gdk_event_get_display (event);
 
-      no_text_input_mask =
-        gdk_display_get_modifier_mask (display,
-                                       GDK_MODIFIER_INTENT_NO_TEXT_INPUT);
+      no_text_input_mask = GDK_ALT_MASK|GDK_CONTROL_MASK;
 
       if (gdk_event_get_event_type (event) == GDK_KEY_PRESS &&
           (state & no_text_input_mask) == 0)

gtk/gtklistbox.c
@@ -1425,18 +1425,8 @@ gtk_list_box_add_move_binding (GtkWidgetClass  *widget_class,
                                GtkMovementStep  step,
                                gint             count)
 {
-  GdkDisplay *display;
   GdkModifierType extend_mod_mask = GDK_SHIFT_MASK;
   GdkModifierType modify_mod_mask = GDK_CONTROL_MASK;
-
-  display = gdk_display_get_default ();
-  if (display)
-    {
-      extend_mod_mask = gdk_display_get_modifier_mask (display,
-                                                       GDK_MODIFIER_INTENT_EXTEND_SELECTION);
-      modify_mod_mask = gdk_display_get_modifier_mask (display,
-                                                       GDK_MODIFIER_INTENT_MODIFY_SELECTION);
-    }
 
   gtk_widget_class_add_binding_signal (widget_class,
                                        keyval, modmask,
@@ -1786,10 +1776,10 @@ get_current_selection_modifiers (GtkWidget *widget,
 
   if (gtk_get_current_event_state (&state))
     {
-      mask = gtk_widget_get_modifier_mask (widget, GDK_MODIFIER_INTENT_MODIFY_SELECTION);
+      mask = GDK_CONTROL_MASK;
       if ((state & mask) == mask)
         *modify = TRUE;
-      mask = gtk_widget_get_modifier_mask (widget, GDK_MODIFIER_INTENT_EXTEND_SELECTION);
+      mask = GDK_SHIFT_MASK;
       if ((state & mask) == mask)
         *extend = TRUE;
     }

gtk/gtkprivate.c
@@ -158,22 +158,6 @@ _gtk_single_string_accumulator (GSignalInvocationHint *ihint,
   return continue_emission;
 }
 
-GdkModifierType
-_gtk_get_primary_accel_mod (void)
-{
-  static GdkModifierType primary = 0;
-
-  if (! primary)
-    {
-      GdkDisplay *display = gdk_display_get_default ();
-
-      primary = gdk_display_get_modifier_mask (display,
-                                               GDK_MODIFIER_INTENT_PRIMARY_ACCELERATOR);
-    }
-
-  return primary;
-}
-
 static gpointer
 register_resources (gpointer data)
 {

commit 8a1a96a47efd0a168452ccfa9ff8031b9e61defd

text: Fix placing the cursor on click
Commit c297d45b8a19d84e0 accidentally removed the
check for the right modifier mask, causing us to
always extend the selection.

gtk/gtktext.c
@@ -2714,6 +2714,7 @@ gtk_text_click_gesture_pressed (GtkGestureClick *gesture,
       gboolean have_selection;
       gboolean is_touchscreen, extend_selection;
       GdkDevice *source;
+      guint state;
 
       sel_start = priv->selection_bound;
       sel_end = priv->current_pos;
@@ -2729,7 +2730,9 @@ gtk_text_click_gesture_pressed (GtkGestureClick *gesture,
       priv->select_words = FALSE;
       priv->select_lines = FALSE;
 
-      extend_selection = GDK_SHIFT_MASK;
+      state = gdk_event_get_modifier_state (event);
+
+      extend_selection = (state & GDK_SHIFT_MASK) != 0;
 
       /* Always emit reset when preedit is shown */
       priv->need_im_reset = TRUE;