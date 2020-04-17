97b1a7192f8dd0ce9b2a6c21b9604f1db1a14d54
Timm Bäder <mail@baedert.org>
2020-04-17 21:28:02 +0200

    label: Remove a debug leftover

--- gtk/gtklabel.c
+++ gtk/gtklabel.c
@@ -1796,8 +1796,6 @@ gtk_label_set_attributes (GtkLabel         *self,
   if (!attrs && !self->attrs)
     return;
 
-  if (attrs == self->attrs) g_error ("Z");
-
   if (attrs)
     pango_attr_list_ref (attrs);
 
