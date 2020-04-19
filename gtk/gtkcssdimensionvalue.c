GNOME/gtk

commit c9e972eecb3a78ea1471cecf320ac1866f9dc790

cssdimensionvalue: Implement transition()
Instead of falling back to the generic gtk_css_number_value_transition
(which can allocate multiple new css values), just implement this here.

gtk/gtkcssdimensionvalue.c
@@ -243,13 +243,34 @@ gtk_css_value_dimension_get_calc_term_order (const GtkCssValue *value)
   return 1000 + order_per_unit[value->unit];
 }
 
+static GtkCssValue *
+gtk_css_value_dimension_transition (GtkCssValue *start,
+                                    GtkCssValue *end,
+                                    guint        property_id,
+                                    double       progress)
+{
+  if (progress == 0)
+    return _gtk_css_value_ref (start);
+
+  if (progress == 1)
+    return _gtk_css_value_ref (end);
+
+  if (start == end)
+    return _gtk_css_value_ref (start);
+
+  if (start->unit != end->unit)
+    return NULL;
+
+  return gtk_css_dimension_value_new (start->value + (end->value - start->value) * progress, start->unit);
+}
+
 static const GtkCssNumberValueClass GTK_CSS_VALUE_DIMENSION = {
   {
     "GtkCssDimensionValue",
     gtk_css_value_dimension_free,
     gtk_css_value_dimension_compute,
     gtk_css_value_dimension_equal,
-    gtk_css_number_value_transition,
+    gtk_css_value_dimension_transition,
     NULL,
     NULL,
     gtk_css_value_dimension_print

commit a7cad7909261094b46de548033983bf2fd3f7619

css: Allow transitioning between different units again
This turns out to be necessary for transitioning filters,
and we run into criticals if transitioning numbers
unexpectedly returns NULL. I've observed that with

* {
  filter: invert(80%);
}

This was broken in c9e972eecb3a78ea.

gtk/gtkcssdimensionvalue.c
@@ -248,10 +248,10 @@ gtk_css_value_dimension_transition (GtkCssValue *start,
                                     guint        property_id,
                                     double       progress)
 {
-  if (start->unit != end->unit)
-    return NULL;
+  if (start->unit == end->unit)
+    return gtk_css_dimension_value_new (start->value + (end->value - start->value) * progress, start->unit);
 
-  return gtk_css_dimension_value_new (start->value + (end->value - start->value) * progress, start->unit);
+  return gtk_css_number_value_transition (start, end, property_id, progress);
 }
 
 static const GtkCssNumberValueClass GTK_CSS_VALUE_DIMENSION = {