98611735ec4f14fdc92719fbe9a983b5508c6e6b
Matthias Clasen <mclasen@redhat.com>
2020-04-21 08:37:04 -0400

    Stop using the -gtk-icon-theme style property
    We want to just use the current icon theme, always.

--- gtk/gtkcssimageicontheme.c
+++ gtk/gtkcssimageicontheme.c
@@ -23,7 +23,6 @@
 
 #include <math.h>
 
-#include "gtkcssiconthemevalueprivate.h"
 #include "gtksettingsprivate.h"
 #include "gtksnapshot.h"
 #include "gtkstyleproviderprivate.h"
@@ -149,7 +148,7 @@ gtk_css_image_icon_theme_compute (GtkCssImage      *image,
 
   copy = g_object_new (GTK_TYPE_CSS_IMAGE_ICON_THEME, NULL);
   copy->name = g_strdup (icon_theme->name);
-  copy->icon_theme = gtk_css_icon_theme_value_get_icon_theme (style->core->icon_theme);
+  copy->icon_theme = icon_theme->icon_theme;
   copy->scale = gtk_style_provider_get_scale (provider);
   gtk_icon_theme_lookup_symbolic_colors (style, &copy->color, &copy->success, &copy->warning, &copy->error);
 
