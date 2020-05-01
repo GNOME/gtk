8927cd53f6b62dd51c894735b253062f288c57bc
Alexander Mikhaylenko <alexm@gnome.org>
2020-05-01 18:48:03 +0500

    builder-tool: Rename GtkHeaderBar:custom-title to title-widget
    Add a test.

--- gtk/tools/gtk-builder-tool-simplify.c
+++ gtk/tools/gtk-builder-tool-simplify.c
@@ -733,6 +733,7 @@ maybe_rename_property (Element *element, MyParserData *data)
     { "GtkWidget", "margin-left", GTK_TYPE_WIDGET, PROP_KIND_OBJECT, "margin-start", { NULL, NULL, NULL } },
     { "GtkWidget", "margin-right", GTK_TYPE_WIDGET, PROP_KIND_OBJECT, "margin-end", { NULL, NULL, NULL } },
     { "GtkHeaderBar", "show-close-button", GTK_TYPE_HEADER_BAR, PROP_KIND_OBJECT, "show-title-buttons", { NULL, NULL, NULL } },
+    { "GtkHeaderBar", "custom-title", GTK_TYPE_HEADER_BAR, PROP_KIND_OBJECT, "title-widget", { NULL, NULL, NULL } },
     { "GtkStack", "homogeneous", GTK_TYPE_STACK, PROP_KIND_OBJECT, "hhomogeneous", { "vhomogeneous", NULL, NULL } }
   };
   int i, k, l;
