GNOME/gtk

commit 5f070ff233386eae3b3290d12ba67745833dcb8c

Remove filename/URI API from GtkFileChooser
GtkFileChooser's API predates GIO by a few years, so it started off with
filenames and URI as character arrays. After introducing GIO as a
dependency, the API included GFile-based entry points.

It's much more appropriate to use GFile everywhere, as we want to
encourage people to use GIO instead of passing random bytes to low level
POSIX API.

See: #2455

gtk/gtkprinteroptionwidget.c
@@ -68,7 +68,7 @@ struct GtkPrinterOptionWidgetPrivate
   GtkWidget *button;
 
   /* the last location for save to file, that the user selected */
-  gchar *last_location;
+  GFile *last_location;
 };
 
 enum {
@@ -485,42 +485,49 @@ dialog_response_callback (GtkDialog              *dialog,
                           GtkPrinterOptionWidget *widget)
 {
   GtkPrinterOptionWidgetPrivate *priv = widget->priv;
-  gchar *uri = NULL;
-  gchar *new_location = NULL;
+  GFile *new_location = NULL;
+  char *uri = NULL;
 
   if (response_id == GTK_RESPONSE_ACCEPT)
     {
-      gchar *filename;
-      gchar *filename_utf8;
-      gchar *filename_short;
+      GFileInfo *info;
 
-      new_location = gtk_file_chooser_get_uri (GTK_FILE_CHOOSER (dialog));
+      new_location = gtk_file_chooser_get_file (GTK_FILE_CHOOSER (dialog));
+      info = g_file_query_info (new_location,
+                                "standard::display-name",
+                                0,
+                                NULL,
+                                NULL);
+      if (info != NULL)
+        {
+          const char *filename_utf8 = g_file_info_get_display_name (info);
 
-      filename = gtk_file_chooser_get_filename (GTK_FILE_CHOOSER (dialog));
-      filename_utf8 = g_filename_to_utf8 (filename, -1, NULL, NULL, NULL);
-      filename_short = trim_long_filename (filename_utf8);
-      gtk_button_set_label (GTK_BUTTON (priv->button), filename_short);
-      g_free (filename_short);
-      g_free (filename_utf8);
-      g_free (filename);
+          char *filename_short = trim_long_filename (filename_utf8);
+          gtk_button_set_label (GTK_BUTTON (priv->button), filename_short);
+
+          g_free (filename_short);
+          g_object_unref (info);
+        }
+
+      g_object_unref (new_location);
     }
 
   gtk_widget_destroy (GTK_WIDGET (dialog));
 
   if (new_location)
-    uri = new_location;
+    uri = g_file_get_uri (new_location);
   else
-    uri = priv->last_location;
+    uri = g_file_get_uri (priv->last_location);
 
-  if (uri)
+  if (uri != NULL)
     {
       gtk_printer_option_set (priv->source, uri);
       emit_changed (widget);
+      g_free (uri);
     }
 
-  g_free (new_location);
-  g_free (priv->last_location);
-  priv->last_location = NULL;
+  g_object_unref (new_location);
+  g_clear_object (&priv->last_location);
 
   /* unblock the handler which was blocked in the filesave_choose_cb function */
   g_signal_handler_unblock (priv->source, priv->source_changed_handler);
@@ -531,7 +538,6 @@ filesave_choose_cb (GtkWidget              *button,
                     GtkPrinterOptionWidget *widget)
 {
   GtkPrinterOptionWidgetPrivate *priv = widget->priv;
-  gchar *last_location = NULL;
   GtkWidget *dialog;
   GtkWindow *toplevel;
 
@@ -550,23 +556,21 @@ filesave_choose_cb (GtkWidget              *button,
   gtk_file_chooser_set_do_overwrite_confirmation (GTK_FILE_CHOOSER (dialog), FALSE);
 
   /* select the current filename in the dialog */
-  if (priv->source != NULL)
+  if (priv->source != NULL && priv->source->value != NULL)
     {
-      priv->last_location = last_location = g_strdup (priv->source->value);
-      if (last_location)
+      priv->last_location = g_file_new_for_uri (priv->source->value);
+      if (priv->last_location)
         {
-          GFile *file;
-          gchar *basename;
-          gchar *basename_utf8;
+          char *basename;
+          char *basename_utf8;
 
-          gtk_file_chooser_select_uri (GTK_FILE_CHOOSER (dialog), last_location);
-          file = g_file_new_for_uri (last_location);
-          basename = g_file_get_basename (file);
+          gtk_file_chooser_select_file (GTK_FILE_CHOOSER (dialog), priv->last_location, NULL);
+
+          basename = g_file_get_basename (priv->last_location);
           basename_utf8 = g_filename_to_utf8 (basename, -1, NULL, NULL, NULL);
           gtk_file_chooser_set_current_name (GTK_FILE_CHOOSER (dialog), basename_utf8);
           g_free (basename_utf8);
           g_free (basename);
-          g_object_unref (file);
         }
     }
 

demos/constraint-editor/constraint-editor-window.c
@@ -221,9 +221,12 @@ open_cb (GtkWidget              *button,
                                         GTK_FILE_CHOOSER_ACTION_OPEN,
                                         "_Load",
                                         "_Cancel");
-
   gtk_native_dialog_set_modal (GTK_NATIVE_DIALOG (dialog), TRUE);
-  gtk_file_chooser_set_current_folder (GTK_FILE_CHOOSER (dialog), ".");
+
+  GFile *cwd = g_file_new_for_path (".");
+  gtk_file_chooser_set_current_folder (GTK_FILE_CHOOSER (dialog), cwd, NULL);
+  g_object_unref (cwd);
+
   g_signal_connect (dialog, "response", G_CALLBACK (open_response_cb), self);
   gtk_native_dialog_show (GTK_NATIVE_DIALOG (dialog));
 }
@@ -290,14 +293,20 @@ save_response_cb (GtkNativeDialog        *dialog,
   if (response == GTK_RESPONSE_ACCEPT)
     {
       GListModel *model;
-      char *text, *filename;
+      GFile *file;
+      char *text;
       GError *error = NULL;
 
       model = constraint_view_get_model (CONSTRAINT_VIEW (self->view));
       text = serialize_model (model);
-
-      filename = gtk_file_chooser_get_filename (GTK_FILE_CHOOSER (dialog));
-      if (!g_file_set_contents (filename, text, -1, &error))
+      file = gtk_file_chooser_get_file (GTK_FILE_CHOOSER (dialog));
+      g_file_replace_contents (file, text, -1,
+                               NULL, FALSE,
+                               G_FILE_CREATE_NONE,
+                               NULL,
+                               NULL,
+                               &error);
+      if (error != NULL)
         {
           GtkWidget *dialog;
 
@@ -312,7 +321,9 @@ save_response_cb (GtkNativeDialog        *dialog,
           gtk_widget_show (dialog);
           g_error_free (error);
         }
-      g_free (filename);
+
+      g_free (text);
+      g_object_unref (file);
     }
 
   gtk_native_dialog_destroy (dialog);
@@ -329,9 +340,12 @@ save_cb (GtkWidget              *button,
                                         GTK_FILE_CHOOSER_ACTION_SAVE,
                                         "_Save",
                                         "_Cancel");
-
   gtk_native_dialog_set_modal (GTK_NATIVE_DIALOG (dialog), TRUE);
-  gtk_file_chooser_set_current_folder (GTK_FILE_CHOOSER (dialog), ".");
+
+  GFile *cwd = g_file_new_for_path (".");
+  gtk_file_chooser_set_current_folder (GTK_FILE_CHOOSER (dialog), cwd, NULL);
+  g_object_unref (cwd);
+
   g_signal_connect (dialog, "response", G_CALLBACK (save_response_cb), self);
   gtk_native_dialog_show (GTK_NATIVE_DIALOG (dialog));
 }

demos/node-editor/node-editor-window.c
@@ -385,7 +385,11 @@ show_open_filechooser (NodeEditorWindow *self)
 
   gtk_dialog_set_default_response (GTK_DIALOG (dialog), GTK_RESPONSE_ACCEPT);
   gtk_window_set_modal (GTK_WINDOW (dialog), TRUE);
-  gtk_file_chooser_set_current_folder (GTK_FILE_CHOOSER (dialog), ".");
+
+  GFile *cwd = g_file_new_for_path (".");
+  gtk_file_chooser_set_current_folder (GTK_FILE_CHOOSER (dialog), cwd, NULL);
+  g_object_unref (cwd);
+
   g_signal_connect (dialog, "response", G_CALLBACK (open_response_cb), self);
   gtk_widget_show (dialog);
 }
@@ -406,13 +410,20 @@ save_response_cb (GtkWidget        *dialog,
 
   if (response == GTK_RESPONSE_ACCEPT)
     {
-      char *text, *filename;
+      GFile *file;
+      char *text;
       GError *error = NULL;
 
       text = get_current_text (self->text_buffer);
 
-      filename = gtk_file_chooser_get_filename (GTK_FILE_CHOOSER (dialog));
-      if (!g_file_set_contents (filename, text, -1, &error))
+      file = gtk_file_chooser_get_file (GTK_FILE_CHOOSER (dialog));
+      g_file_replace_contents (file, text, -1,
+                               NULL, FALSE,
+                               G_FILE_CREATE_NONE,
+                               NULL,
+                               NULL,
+                               &error);
+      if (error != NULL)
         {
           GtkWidget *dialog;
 
@@ -427,7 +438,9 @@ save_response_cb (GtkWidget        *dialog,
           gtk_widget_show (dialog);
           g_error_free (error);
         }
-      g_free (filename);
+
+      g_free (text);
+      g_object_unref (file);
     }
 
   gtk_widget_destroy (dialog);
@@ -449,7 +462,11 @@ save_cb (GtkWidget        *button,
   gtk_dialog_set_default_response (GTK_DIALOG (dialog), GTK_RESPONSE_ACCEPT);
   gtk_window_set_modal (GTK_WINDOW (dialog), TRUE);
   gtk_file_chooser_set_do_overwrite_confirmation (GTK_FILE_CHOOSER (dialog), TRUE);
-  gtk_file_chooser_set_current_folder (GTK_FILE_CHOOSER (dialog), ".");
+
+  GFile *cwd = g_file_new_for_path (".");
+  gtk_file_chooser_set_current_folder (GTK_FILE_CHOOSER (dialog), cwd, NULL);
+  g_object_unref (cwd);
+
   g_signal_connect (dialog, "response", G_CALLBACK (save_response_cb), self);
   gtk_widget_show (dialog);
 }
@@ -523,10 +540,10 @@ export_image_response_cb (GtkWidget  *dialog,
 
   if (response == GTK_RESPONSE_ACCEPT)
     {
-      char *filename;
+      GFile *file;
 
-      filename = gtk_file_chooser_get_filename (GTK_FILE_CHOOSER (dialog));
-      if (!gdk_texture_save_to_png (texture, filename))
+      file = gtk_file_chooser_get_file (GTK_FILE_CHOOSER (dialog));
+      if (!gdk_texture_save_to_png (texture, g_file_peek_path (file)))
         {
           GtkWidget *message_dialog;
 
@@ -538,7 +555,8 @@ export_image_response_cb (GtkWidget  *dialog,
           g_signal_connect (message_dialog, "response", G_CALLBACK (gtk_widget_destroy), NULL);
           gtk_widget_show (message_dialog);
         }
-      g_free (filename);
+
+      g_object_unref (file);
     }
 
   gtk_widget_destroy (dialog);

gtk/gtkfilechooser.h
@@ -156,52 +156,10 @@ void        gtk_file_chooser_set_current_name  (GtkFileChooser *chooser,
 GDK_AVAILABLE_IN_ALL
 gchar *gtk_file_chooser_get_current_name (GtkFileChooser *chooser);
 
-/* Filename manipulation
- */
-GDK_AVAILABLE_IN_ALL
-gchar *  gtk_file_chooser_get_filename       (GtkFileChooser *chooser);
-GDK_AVAILABLE_IN_ALL
-gboolean gtk_file_chooser_set_filename       (GtkFileChooser *chooser,
-					      const char     *filename);
-GDK_AVAILABLE_IN_ALL
-gboolean gtk_file_chooser_select_filename    (GtkFileChooser *chooser,
-					      const char     *filename);
-GDK_AVAILABLE_IN_ALL
-void     gtk_file_chooser_unselect_filename  (GtkFileChooser *chooser,
-					      const char     *filename);
 GDK_AVAILABLE_IN_ALL
 void     gtk_file_chooser_select_all         (GtkFileChooser *chooser);
 GDK_AVAILABLE_IN_ALL
 void     gtk_file_chooser_unselect_all       (GtkFileChooser *chooser);
-GDK_AVAILABLE_IN_ALL
-GSList * gtk_file_chooser_get_filenames      (GtkFileChooser *chooser);
-GDK_AVAILABLE_IN_ALL
-gboolean gtk_file_chooser_set_current_folder (GtkFileChooser *chooser,
-					      const gchar    *filename);
-GDK_AVAILABLE_IN_ALL
-gchar *  gtk_file_chooser_get_current_folder (GtkFileChooser *chooser);
-
-
-/* URI manipulation
- */
-GDK_AVAILABLE_IN_ALL
-gchar *  gtk_file_chooser_get_uri                (GtkFileChooser *chooser);
-GDK_AVAILABLE_IN_ALL
-gboolean gtk_file_chooser_set_uri                (GtkFileChooser *chooser,
-						  const char     *uri);
-GDK_AVAILABLE_IN_ALL
-gboolean gtk_file_chooser_select_uri             (GtkFileChooser *chooser,
-						  const char     *uri);
-GDK_AVAILABLE_IN_ALL
-void     gtk_file_chooser_unselect_uri           (GtkFileChooser *chooser,
-						  const char     *uri);
-GDK_AVAILABLE_IN_ALL
-GSList * gtk_file_chooser_get_uris               (GtkFileChooser *chooser);
-GDK_AVAILABLE_IN_ALL
-gboolean gtk_file_chooser_set_current_folder_uri (GtkFileChooser *chooser,
-						  const gchar    *uri);
-GDK_AVAILABLE_IN_ALL
-gchar *  gtk_file_chooser_get_current_folder_uri (GtkFileChooser *chooser);
 
 /* GFile manipulation */
 GDK_AVAILABLE_IN_ALL
@@ -220,11 +178,11 @@ void     gtk_file_chooser_unselect_file           (GtkFileChooser  *chooser,
 GDK_AVAILABLE_IN_ALL
 GSList * gtk_file_chooser_get_files               (GtkFileChooser  *chooser);
 GDK_AVAILABLE_IN_ALL
-gboolean gtk_file_chooser_set_current_folder_file (GtkFileChooser  *chooser,
+gboolean gtk_file_chooser_set_current_folder      (GtkFileChooser  *chooser,
                                                    GFile           *file,
                                                    GError         **error);
 GDK_AVAILABLE_IN_ALL
-GFile *  gtk_file_chooser_get_current_folder_file (GtkFileChooser  *chooser);
+GFile *  gtk_file_chooser_get_current_folder      (GtkFileChooser  *chooser);
 
 /* Preview widget
  */
@@ -244,10 +202,6 @@ void       gtk_file_chooser_set_use_preview_label     (GtkFileChooser *chooser,
 GDK_AVAILABLE_IN_ALL
 gboolean   gtk_file_chooser_get_use_preview_label     (GtkFileChooser *chooser);
 
-GDK_AVAILABLE_IN_ALL
-char  *gtk_file_chooser_get_preview_filename (GtkFileChooser *chooser);
-GDK_AVAILABLE_IN_ALL
-char  *gtk_file_chooser_get_preview_uri      (GtkFileChooser *chooser);
 GDK_AVAILABLE_IN_ALL
 GFile *gtk_file_chooser_get_preview_file     (GtkFileChooser *chooser);
 
@@ -274,7 +228,7 @@ GSList *gtk_file_chooser_list_filters  (GtkFileChooser *chooser);
  */
 GDK_AVAILABLE_IN_ALL
 void           gtk_file_chooser_set_filter (GtkFileChooser *chooser,
-					   GtkFileFilter  *filter);
+					    GtkFileFilter  *filter);
 GDK_AVAILABLE_IN_ALL
 GtkFileFilter *gtk_file_chooser_get_filter (GtkFileChooser *chooser);
 
@@ -282,25 +236,14 @@ GtkFileFilter *gtk_file_chooser_get_filter (GtkFileChooser *chooser);
 
 GDK_AVAILABLE_IN_ALL
 gboolean gtk_file_chooser_add_shortcut_folder    (GtkFileChooser *chooser,
-						  const char     *folder,
+						  GFile          *folder,
 						  GError        **error);
 GDK_AVAILABLE_IN_ALL
 gboolean gtk_file_chooser_remove_shortcut_folder (GtkFileChooser *chooser,
-						  const char     *folder,
+						  GFile          *folder,
 						  GError        **error);
 GDK_AVAILABLE_IN_ALL
 GSList *gtk_file_chooser_list_shortcut_folders   (GtkFileChooser *chooser);
-
-GDK_AVAILABLE_IN_ALL
-gboolean gtk_file_chooser_add_shortcut_folder_uri    (GtkFileChooser *chooser,
-						      const char     *uri,
-						      GError        **error);
-GDK_AVAILABLE_IN_ALL
-gboolean gtk_file_chooser_remove_shortcut_folder_uri (GtkFileChooser *chooser,
-						      const char     *uri,
-						      GError        **error);
-GDK_AVAILABLE_IN_ALL
-GSList *gtk_file_chooser_list_shortcut_folder_uris   (GtkFileChooser *chooser);
 
 GDK_AVAILABLE_IN_ALL
 void        gtk_file_chooser_add_choice              (GtkFileChooser  *chooser,

gtk/gtkfilechooserbutton.c
@@ -80,11 +80,12 @@
  * |[<!-- language="C" -->
  * {
  *   GtkWidget *button;
+ *   GFile *cwd = g_file_new_for_path ("/etc");
  *
  *   button = gtk_file_chooser_button_new (_("Select a file"),
  *                                         GTK_FILE_CHOOSER_ACTION_OPEN);
- *   gtk_file_chooser_set_current_folder (GTK_FILE_CHOOSER (button),
- *                                        "/etc");
+ *   gtk_file_chooser_set_current_folder (GTK_FILE_CHOOSER (button), cwd);
+ *   g_object_unref (cwd);
  * }
  * ]|
  *
@@ -556,7 +557,7 @@ gtk_file_chooser_button_set_current_folder (GtkFileChooser    *chooser,
   g_signal_emit_by_name (button, "current-folder-changed");
 
   if (priv->active)
-    gtk_file_chooser_set_current_folder_file (GTK_FILE_CHOOSER (priv->chooser), file, NULL);
+    gtk_file_chooser_set_current_folder (GTK_FILE_CHOOSER (priv->chooser), file, NULL);
 
   return TRUE;
 }
@@ -688,7 +689,7 @@ gtk_file_chooser_button_add_shortcut_folder (GtkFileChooser  *chooser,
 
   delegate = g_object_get_qdata (G_OBJECT (chooser),
 				 GTK_FILE_CHOOSER_DELEGATE_QUARK);
-  retval = _gtk_file_chooser_add_shortcut_folder (delegate, file, error);
+  retval = gtk_file_chooser_add_shortcut_folder (delegate, file, error);
 
   if (retval)
     {
@@ -728,7 +729,7 @@ gtk_file_chooser_button_remove_shortcut_folder (GtkFileChooser  *chooser,
   delegate = g_object_get_qdata (G_OBJECT (chooser),
 				 GTK_FILE_CHOOSER_DELEGATE_QUARK);
 
-  retval = _gtk_file_chooser_remove_shortcut_folder (delegate, file, error);
+  retval = gtk_file_chooser_remove_shortcut_folder (delegate, file, error);
 
   if (retval)
     {
@@ -2550,7 +2551,7 @@ save_inactive_state (GtkFileChooserButton *button)
   if (priv->selection_while_inactive)
     g_object_unref (priv->selection_while_inactive);
 
-  priv->current_folder_while_inactive = gtk_file_chooser_get_current_folder_file (GTK_FILE_CHOOSER (priv->chooser));
+  priv->current_folder_while_inactive = gtk_file_chooser_get_current_folder (GTK_FILE_CHOOSER (priv->chooser));
   priv->selection_while_inactive = gtk_file_chooser_get_file (GTK_FILE_CHOOSER (priv->chooser));
 }
 
@@ -2560,7 +2561,7 @@ restore_inactive_state (GtkFileChooserButton *button)
   GtkFileChooserButtonPrivate *priv = gtk_file_chooser_button_get_instance_private (button);
 
   if (priv->current_folder_while_inactive)
-    gtk_file_chooser_set_current_folder_file (GTK_FILE_CHOOSER (priv->chooser), priv->current_folder_while_inactive, NULL);
+    gtk_file_chooser_set_current_folder (GTK_FILE_CHOOSER (priv->chooser), priv->current_folder_while_inactive, NULL);
 
   if (priv->selection_while_inactive)
     gtk_file_chooser_select_file (GTK_FILE_CHOOSER (priv->chooser), priv->selection_while_inactive, NULL);

gtk/gtkfilechooserdialog.c
@@ -85,11 +85,9 @@
  * res = gtk_dialog_run (GTK_DIALOG (dialog));
  * if (res == GTK_RESPONSE_ACCEPT)
  *   {
- *     char *filename;
  *     GtkFileChooser *chooser = GTK_FILE_CHOOSER (dialog);
- *     filename = gtk_file_chooser_get_filename (chooser);
- *     open_file (filename);
- *     g_free (filename);
+ *     g_autoptr(GFile) filen = gtk_file_chooser_get_file (chooser);
+ *     open_file (file);
  *   }
  *
  * gtk_widget_destroy (dialog);
@@ -116,20 +114,15 @@
  * gtk_file_chooser_set_do_overwrite_confirmation (chooser, TRUE);
  *
  * if (user_edited_a_new_document)
- *   gtk_file_chooser_set_current_name (chooser,
- *                                      _("Untitled document"));
+ *   gtk_file_chooser_set_current_name (chooser, _("Untitled document"));
  * else
- *   gtk_file_chooser_set_filename (chooser,
- *                                  existing_filename);
+ *   gtk_file_chooser_set_file (chooser, existing_filename);
  *
  * res = gtk_dialog_run (GTK_DIALOG (dialog));
  * if (res == GTK_RESPONSE_ACCEPT)
  *   {
- *     char *filename;
- *
- *     filename = gtk_file_chooser_get_filename (chooser);
- *     save_to_file (filename);
- *     g_free (filename);
+ *     g_autoptr(GFile) file = gtk_file_chooser_get_file (chooser);
+ *     save_to_file (file);
  *   }
  *
  * gtk_widget_destroy (dialog);
@@ -145,7 +138,7 @@
  *   and suggest a name such as “Untitled” with gtk_file_chooser_set_current_name().
  *
  * - To save a file under a different name. Use #GTK_FILE_CHOOSER_ACTION_SAVE,
- *   and set the existing filename with gtk_file_chooser_set_filename().
+ *   and set the existing file with gtk_file_chooser_set_file().
  *
  * - To choose a folder instead of a file. Use #GTK_FILE_CHOOSER_ACTION_SELECT_FOLDER.
  *
@@ -156,7 +149,7 @@
  * considered to be a good policy, as now the file chooser is
  * able to make good suggestions on its own.  In general, you
  * should only cause the file chooser to show a specific folder
- * when it is appropriate to use gtk_file_chooser_set_filename(),
+ * when it is appropriate to use gtk_file_chooser_set_file(),
  * i.e. when you are doing a Save As command and you already
  * have a file saved somewhere.
 
@@ -362,18 +355,18 @@ file_chooser_widget_selection_changed (GtkWidget            *widget,
 {
   GtkFileChooserDialogPrivate *priv = gtk_file_chooser_dialog_get_instance_private (dialog);
   GtkWidget *button;
-  GSList *uris;
+  GSList *files;
   gboolean sensitive;
 
   button = get_accept_action_widget (GTK_DIALOG (dialog), FALSE);
   if (button == NULL)
     return;
 
-  uris = gtk_file_chooser_get_uris (GTK_FILE_CHOOSER (priv->widget));
-  sensitive = (uris != NULL);
+  files = gtk_file_chooser_get_files (GTK_FILE_CHOOSER (priv->widget));
+  sensitive = (files != NULL);
   gtk_widget_set_sensitive (button, sensitive);
 
-  g_slist_free_full (uris, g_free);
+  g_slist_free_full (files, g_object_unref);
 }
 
 static void

gtk/gtkfilechoosernative.c
@@ -644,8 +644,8 @@ gtk_file_chooser_native_set_current_folder (GtkFileChooser    *chooser,
   GtkFileChooserNative *self = GTK_FILE_CHOOSER_NATIVE (chooser);
   gboolean res;
 
-  res = gtk_file_chooser_set_current_folder_file (GTK_FILE_CHOOSER (self->dialog),
-                                                  file, error);
+  res = gtk_file_chooser_set_current_folder (GTK_FILE_CHOOSER (self->dialog),
+                                             file, error);
 
 
   if (res)

commit cdb2b21467ff2156f83adffd41f695cb8af4a360

Do not release the GFile prematurely
Otherwise we won't be able to access it to get the URI for the
GtkPrinterOption.

This fixes a regression introduced in commit 5f070ff2333.

gtk/gtkprinteroptionwidget.c
@@ -508,8 +508,6 @@ dialog_response_callback (GtkDialog              *dialog,
           g_free (filename_short);
           g_object_unref (info);
         }
-
-      g_object_unref (new_location);
     }
 
   gtk_widget_destroy (GTK_WIDGET (dialog));
@@ -572,11 +570,10 @@ filesave_choose_cb (GtkWidget              *button,
     }
 
   g_signal_connect (dialog, "response",
-                    G_CALLBACK (dialog_response_callback), widget);
+                    G_CALLBACK (dialog_response_callback),
+                    widget);
   gtk_window_set_modal (GTK_WINDOW (dialog), TRUE);
-  G_GNUC_BEGIN_IGNORE_DEPRECATIONS
   gtk_window_present (GTK_WINDOW (dialog));
-  G_GNUC_END_IGNORE_DEPRECATIONS
 }
 
 static gchar *