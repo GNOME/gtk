0ab16440483af2cc58e5e714f76b2890008fac17
Matthias Clasen <mclasen@redhat.com>
2020-05-07 00:05:51 -0400

    Use gtk_expander_set_child throughout
    Replace all uses of gtk_container_add on expanders
    with gtk_expander_set_child.

--- tests/testflowbox.c
+++ tests/testflowbox.c
@@ -423,7 +423,7 @@ create_window (void)
   flowbox_cntl = gtk_box_new (GTK_ORIENTATION_VERTICAL, 2);
   gtk_widget_show (flowbox_cntl);
   gtk_widget_show (expander);
-  gtk_container_add (GTK_CONTAINER (expander), flowbox_cntl);
+  gtk_expander_set_child (GTK_EXPANDER (expander), flowbox_cntl);
   gtk_container_add (GTK_CONTAINER (vbox), expander);
 
   widget = gtk_check_button_new_with_label ("Homogeneous");
@@ -597,7 +597,7 @@ create_window (void)
   items_cntl = gtk_box_new (GTK_ORIENTATION_VERTICAL, 2);
   gtk_widget_show (items_cntl);
   gtk_widget_show (expander);
-  gtk_container_add (GTK_CONTAINER (expander), items_cntl);
+  gtk_expander_set_child (GTK_EXPANDER (expander), items_cntl);
   gtk_container_add (GTK_CONTAINER (vbox), expander);
 
   /* Add Items control */
