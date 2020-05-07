08abad134fcb0f1f9f4785b3afccc101d3e7b6ef
Matthias Clasen <mclasen@redhat.com>
2020-05-07 14:56:37 -0400

    grid: Derive from GtkWidget
    GtkContainer is going away.

--- gtk/gtkgrid.h
+++ gtk/gtkgrid.h
@@ -43,7 +43,7 @@ typedef struct _GtkGridClass         GtkGridClass;
 struct _GtkGrid
 {
   /*< private >*/
-  GtkContainer parent_instance;
+  GtkWidget parent_instance;
 };
 
 /**
@@ -52,7 +52,7 @@ struct _GtkGrid
  */
 struct _GtkGridClass
 {
-  GtkContainerClass parent_class;
+  GtkWidgetClass parent_class;
 
   /*< private >*/
 
