f7daa26f764662b1e376e68c4bf3ecb5e9ba4781
Matthias Clasen <mclasen@redhat.com>
2020-05-08 08:48:00 -0400

    listbox: Derive from GtkWidget

--- gtk/a11y/gtklistboxaccessible.c
+++ gtk/a11y/gtklistboxaccessible.c
@@ -23,7 +23,7 @@
 
 static void atk_selection_interface_init (AtkSelectionIface *iface);
 
-G_DEFINE_TYPE_WITH_CODE (GtkListBoxAccessible, gtk_list_box_accessible, GTK_TYPE_CONTAINER_ACCESSIBLE,
+G_DEFINE_TYPE_WITH_CODE (GtkListBoxAccessible, gtk_list_box_accessible, GTK_TYPE_WIDGET_ACCESSIBLE,
                          G_IMPLEMENT_INTERFACE (ATK_TYPE_SELECTION, atk_selection_interface_init))
 
 static void
