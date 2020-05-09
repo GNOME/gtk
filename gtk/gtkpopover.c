fa403caf4f40eca039edf06b9e7b5920fe63a1e3
Matthias Clasen <mclasen@redhat.com>
2020-05-09 15:20:15 -0400

    popover: Fix a reference leak
    Don't leak the reference to the default widget.

--- gtk/gtkpopover.c
+++ gtk/gtkpopover.c
@@ -1025,6 +1025,7 @@ gtk_popover_dispose (GObject *object)
   GtkPopover *popover = GTK_POPOVER (object);
   GtkPopoverPrivate *priv = gtk_popover_get_instance_private (popover);
 
+  g_clear_object (&priv->default_widget);
   g_clear_pointer (&priv->contents_widget, gtk_widget_unparent);
   g_clear_pointer (&priv->arrow_render_node, gsk_render_node_unref);
 
