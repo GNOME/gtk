b2da99fd4ec0558b3ddcb99b079bc73b9a509619
Alexander Mikhaylenko <alexm@gnome.org>
2020-05-11 19:21:03 +0500

    tests: Fix testselectionmode
    Stop using <packing/> and GtkContainer API for random widgets.

--- tests/testselectionmode.c
+++ tests/testselectionmode.c
@@ -30,9 +30,9 @@ selectable_row_init (SelectableRow *row)
   gtk_widget_show (row->box);
   gtk_widget_show (row->check);
 
-  gtk_container_add (GTK_CONTAINER (row), row->box);
+  gtk_list_box_row_set_child (GTK_LIST_BOX_ROW (row), row->box);
   gtk_container_add (GTK_CONTAINER (row->box), row->revealer);
-  gtk_container_add (GTK_CONTAINER (row->revealer), row->check);
+  gtk_revealer_set_child (GTK_REVEALER (row->revealer), row->check);
 }
 
 static void
