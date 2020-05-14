707229ce17a68adbfcce165a60b0e37e4c62d0ce
Alexander Larsson <alexl@redhat.com>
2020-05-14 10:13:05 +0200

    gtk-demo/dnd: Use gtk_popover_set_child(), not gtk_box_append

--- demos/gtk-demo/dnd.c
+++ demos/gtk-demo/dnd.c
@@ -204,7 +204,7 @@ pressed_cb (GtkGesture *gesture,
       gtk_popover_set_has_arrow (GTK_POPOVER (menu), FALSE);
       gtk_popover_set_pointing_to (GTK_POPOVER (menu), &(GdkRectangle){ x, y, 1, 1});
       box = gtk_box_new (GTK_ORIENTATION_VERTICAL, 0);
-      gtk_box_append (GTK_BOX (menu), box);
+      gtk_popover_set_child (GTK_POPOVER (menu), box);
 
       item = gtk_button_new_with_label ("New");
       gtk_button_set_has_frame (GTK_BUTTON (item), FALSE);
