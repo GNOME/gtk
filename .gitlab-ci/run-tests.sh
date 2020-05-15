24d4e6b9f282f3eebc9bbd2030ae71cbf6d3e402
Matthias Clasen <mclasen@redhat.com>
2020-05-15 09:44:24 -0400

    ci: Run tests with different backends
    Run the testsuite under x11, wayland and broadway
    in the fedora-x86_64 job. This requires us to use
    the v17 image which includes weston.

--- .gitlab-ci/run-tests.sh
+++ .gitlab-ci/run-tests.sh
@@ -5,30 +5,61 @@ set +e
 
 srcdir=$( pwd )
 builddir=$1
+backend=$2
 
-export GDK_BACKEND=x11
-xvfb-run -a -s "-screen 0 1024x768x24" \
-        meson test -C ${builddir} \
+case "${backend}"
+  x11)
+    xvfb-run -a -s "-screen 0 1024x768x24" \
+          meson test -C ${builddir} \
                 --print-errorlogs \
+                --setup=${backend} \
                 --suite=gtk \
                 --no-suite=gtk:a11y
 
-# Store the exit code for the CI run, but always
-# generate the reports
-exit_code=$?
+    # Store the exit code for the CI run, but always
+    # generate the reports
+    exit_code=$?
+    ;;
+
+  wayland)
+    weston --backend=headless-backend.so --socket=wayland-5 &
+    compositor=$!
+    export WAYLAND_DISPLAY=wayland-5
+    meson test -C ${builddir} \
+                --print-errorlogs \
+                --setup=${backend} \
+                --suite=gtk \
+                --no-suite=gtk:a11y
+    exit_code=$?
+    kill ${compositor}
+    ;;
+
+  broadway)
+    ${builddir}/gdk/broadway/gtk4-broadwayd :5 &
+    server=$!
+    export BROADWAY_DISPLAY=:5
+    meson test -C ${builddir} \
+                --print-errorlogs \
+                --setup=${backend} \
+                --suite=gtk \
+                --no-suite=gtk:a11y
+    exit_code=$?
+    kill ${server}
+    ;;
+esac
 
 cd ${builddir}
 
 $srcdir/.gitlab-ci/meson-junit-report.py \
         --project-name=gtk \
         --job-id="${CI_JOB_NAME}" \
-        --output=report.xml \
+        --output=report-${backend}.xml \
         meson-logs/testlog.json
 $srcdir/.gitlab-ci/meson-html-report.py \
         --project-name=gtk \
         --job-id="${CI_JOB_NAME}" \
-        --reftest-output-dir="testsuite/reftests/output" \
-        --output=report.html \
+        --reftest-output-dir="testsuite/reftests/output/${backend}" \
+        --output=report-{backend}.html \
         meson-logs/testlog.json
 
 exit $exit_code
