b890e878185a0bffe2669a210e1eddbfc9485dec
Carlos Garnacho <carlosg@gnome.org>
2020-05-17 00:00:22 +0200

    gtksearchengine: Add "got_results" argument to ::finished
    The filechooser tries to figure out whether it got results by poking
    the model, but all files might go through the async GFileInfo querying
    state.
    
    Make all search engines (and the composite one) just notify about this
    fact, so the file chooser can behave appropriately without waiting for
    the async operations to finish.

--- gtk/gtksearchengine.h
+++ gtk/gtksearchengine.h
@@ -82,7 +82,8 @@ void	         _gtk_search_engine_stop            (GtkSearchEngine *engine);
 
 void	         _gtk_search_engine_hits_added      (GtkSearchEngine *engine, 
 						     GList           *hits);
-void	         _gtk_search_engine_finished        (GtkSearchEngine *engine);
+void             _gtk_search_engine_finished        (GtkSearchEngine *engine,
+                                                     gboolean         got_results);
 void	         _gtk_search_engine_error           (GtkSearchEngine *engine, 
 						     const gchar     *error_message);
 void             _gtk_search_engine_set_recursive   (GtkSearchEngine *engine,
