f1c2a1586f93426b43d9e3e1e54b2292ec982069
Benjamin Otte <otte@redhat.com>
2020-05-17 05:58:20 +0200

    display: Remove unneeded getters
    Applications can use the listmodel instead.

--- gdk/gdkdisplay.h
+++ gdk/gdkdisplay.h
@@ -107,11 +107,6 @@ GDK_AVAILABLE_IN_ALL
 GList   * gdk_display_list_seats       (GdkDisplay *display);
 
 GDK_AVAILABLE_IN_ALL
-int          gdk_display_get_n_monitors        (GdkDisplay *display);
-GDK_AVAILABLE_IN_ALL
-GdkMonitor * gdk_display_get_monitor           (GdkDisplay *display,
-                                                int         monitor_num);
-GDK_AVAILABLE_IN_ALL
 GListModel * gdk_display_get_monitors          (GdkDisplay *self) G_GNUC_PURE;
 GDK_AVAILABLE_IN_ALL
 GdkMonitor * gdk_display_get_monitor_at_surface (GdkDisplay *display,
