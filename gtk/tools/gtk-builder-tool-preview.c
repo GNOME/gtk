af16972933f8cee431dc86e96b10011e9e76aa66
Timm Bäder <mail@baedert.org>
2020-05-18 14:02:32 +0200

    builder-tool Make preview closeable

--- gtk/tools/gtk-builder-tool-preview.c
+++ gtk/tools/gtk-builder-tool-preview.c
@@ -51,6 +51,17 @@ set_window_title (GtkWindow  *window,
 }
 
 static void
+quit_cb (GtkWidget *widget,
+         gpointer   user_data)
+{
+  gboolean *is_done = user_data;
+
+  *is_done = TRUE;
+
+  g_main_context_wakeup (NULL);
+}
+
+static void
 preview_file (const char *filename,
               const char *id,
               const char *cssfile)
@@ -59,6 +70,7 @@ preview_file (const char *filename,
   GError *error = NULL;
   GObject *object;
   GtkWidget *window;
+  gboolean done = FALSE;
 
   if (cssfile)
     {
@@ -144,8 +156,9 @@ preview_file (const char *filename,
     }
 
   gtk_window_present (GTK_WINDOW (window));
+  g_signal_connect (window, "destroy", G_CALLBACK (quit_cb), &done);
 
-  while (TRUE)
+  while (!done)
     g_main_context_iteration (NULL, TRUE);
 
   g_object_unref (builder);
