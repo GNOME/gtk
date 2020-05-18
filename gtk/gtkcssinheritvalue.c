c0d71d8e296730392320fe08e80193416836874f
Timm Bäder <mail@baedert.org>
2020-05-18 09:41:47 +0200

    Add G_GNUC_NORETURN to functions that never return

--- gtk/gtkcssinheritvalue.c
+++ gtk/gtkcssinheritvalue.c
@@ -26,7 +26,7 @@ struct _GtkCssValue {
   GTK_CSS_VALUE_BASE
 };
 
-static void
+static void G_GNUC_NORETURN
 gtk_css_value_inherit_free (GtkCssValue *value)
 {
   /* Can only happen if the unique value gets unreffed too often */
