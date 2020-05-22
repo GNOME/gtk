340a8e11b03368468e448cad9b12df3c7a7293f1
Qiu Wenbo <qiuwenbo@phytium.com.cn>
2020-05-22 16:45:44 +0800

    Build media backends with common_cflags and export the symbol
    Fixes: #2771

--- modules/media/gtkgstmediafile.c
+++ modules/media/gtkgstmediafile.c
@@ -96,6 +96,8 @@ gtk_gst_media_file_paintable_init (GdkPaintableInterface *iface)
 G_DEFINE_TYPE_EXTENDED (GtkGstMediaFile, gtk_gst_media_file, GTK_TYPE_MEDIA_FILE, 0,
                         G_IMPLEMENT_INTERFACE (GDK_TYPE_PAINTABLE,
                                                gtk_gst_media_file_paintable_init))
+
+G_MODULE_EXPORT
 void
 g_io_module_load (GIOModule *module)
 {
@@ -107,17 +109,20 @@ g_io_module_load (GIOModule *module)
                                   10);
 }
 
+G_MODULE_EXPORT
+G_GNUC_NORETURN
 void
 g_io_module_unload (GIOModule *module)
 {
   g_assert_not_reached ();
 }
 
+G_MODULE_EXPORT
 char **
 g_io_module_query (void)
 {
   char *eps[] = {
-    GTK_MEDIA_FILE_EXTENSION_POINT_NAME,
+    (char *) GTK_MEDIA_FILE_EXTENSION_POINT_NAME,
     NULL
   };
 
