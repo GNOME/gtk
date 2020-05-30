GNOME/gtk

commit dcbecdac31181854451e41289c417ee900e8d7e9

gesture: Actually track targets
Due to a mismerge, this wasn't actually working,
and was causing criticals from gtk_widget_has_grab
when dragging windows.

gtk/gtkgesture.c
@@ -501,6 +501,7 @@ _gtk_gesture_update_point (GtkGesture     *gesture,
     gdk_event_unref (data->event);
 
   data->event = gdk_event_ref ((GdkEvent *)event);
+  data->target = g_object_ref (target);
   _update_touchpad_deltas (data);
   data->widget_x = x + data->accum_dx;
   data->widget_y = y + data->accum_dy;

commit 0fe9f1c907143db186f23e965ef616ec2d06c776

gesture: Don't leak target widgets
This was broken when we started tracking event targets
in dcbecdac31181854451e.

Fixes: #2801

gtk/gtkgesture.c
@@ -496,9 +496,11 @@ _gtk_gesture_update_point (GtkGesture     *gesture,
       group_state = gtk_gesture_get_group_state (gesture, sequence);
       gtk_gesture_set_sequence_state (gesture, sequence, group_state);
     }
-
-  if (data->event)
-    gdk_event_unref (data->event);
+  else
+    {
+      g_clear_pointer (&data->event, gdk_event_unref);
+      g_clear_object (&data->target);
+    }
 
   data->event = gdk_event_ref ((GdkEvent *)event);
   data->target = g_object_ref (target);