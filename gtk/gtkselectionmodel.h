c4fdf8e8932831ffc4f79ef5e205b66e16e06ea2
Benjamin Otte <otte@redhat.com>
2020-06-14 07:07:30 +0200

    selectionmodel: Remove select_callback() functions
    gtk_selection_model_set_selection() takes care of those now.

--- gtk/gtkselectionmodel.h
+++ gtk/gtkselectionmodel.h
@@ -34,36 +34,6 @@ GDK_AVAILABLE_IN_ALL
 G_DECLARE_INTERFACE (GtkSelectionModel, gtk_selection_model, GTK, SELECTION_MODEL, GListModel)
 
 /**
- * GtkSelectionCallback:
- * @position: the position to query
- * @start_range: (out): returns the position of the first element of the range
- * @n_items: (out): returns the size of the range
- * @selected: (out): returns whether items in @range are selected
- * @data: callback data
- *
- * Callback type for determining items to operate on with
- * gtk_selection_model_select_callback() or
- * gtk_selection_model_unselect_callback().
- *
- * The callback determines a range of consecutive items around
- * @position which should either all
- * be changed, in which case @selected is set to %TRUE, or all not
- * be changed, in which case @selected is set to %FALSE.
- *
- * @start_range and @n_items are set to return the range.
- *
- * The callback will be called repeatedly to find all ranges
- * to operate on until it has exhausted the items of the model,
- * or until it returns an empty range (ie @n_items == 0).
- */
-typedef void     (* GtkSelectionCallback) (guint     position,
-                                           guint    *start_range,
-                                           guint    *n_items,
-                                           gboolean *selected,
-                                           gpointer  data);
-
-
-/**
  * GtkSelectionModelInterface:
  * @is_selected: Return if the item at the given position is selected.
  * @get_selection_in_range: Return a bitset with all currently selected
@@ -128,13 +98,6 @@ struct _GtkSelectionModelInterface
   gboolean              (* set_selection)                       (GtkSelectionModel      *model,
                                                                  GtkBitset              *selected,
                                                                  GtkBitset              *mask);
-  gboolean              (* select_callback)                     (GtkSelectionModel      *model,
-                                                                 gboolean                unselect_rest,
-                                                                 GtkSelectionCallback    callback,
-                                                                 gpointer                data);
-  gboolean              (* unselect_callback)                   (GtkSelectionModel      *model,
-                                                                 GtkSelectionCallback    callback,
-                                                                 gpointer                data);
 };
 
 GDK_AVAILABLE_IN_ALL
@@ -172,16 +135,6 @@ GDK_AVAILABLE_IN_ALL
 gboolean                gtk_selection_model_set_selection       (GtkSelectionModel      *model,
                                                                  GtkBitset              *selected,
                                                                  GtkBitset              *mask);
-
-GDK_AVAILABLE_IN_ALL
-gboolean                gtk_selection_model_select_callback     (GtkSelectionModel      *model,
-                                                                 gboolean                unselect_rest,
-                                                                 GtkSelectionCallback    callback,
-                                                                 gpointer                data);
-GDK_AVAILABLE_IN_ALL
-gboolean                gtk_selection_model_unselect_callback   (GtkSelectionModel      *model,
-                                                                 GtkSelectionCallback    callback,
-                                                                 gpointer                data);
 
 /* for implementations only */
 GDK_AVAILABLE_IN_ALL
