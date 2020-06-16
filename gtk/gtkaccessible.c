d1f92a79b6a08b88690837a7a61874896677f42a
Emmanuele Bassi <ebassi@gnome.org>
2020-06-16 17:40:14 +0100

    Introduce GtkAccessible
    GtkAccessible is an interface for accessible UI elements.
    
    Currently, it doesn't do much except exist as a type; in the future, it
    will be the entry point for all accessible state in GTK.

---
+++ gtk/gtkaccessible.c
@@ -0,0 +1,30 @@
+/* gtkaccessible.c: Accessible interface
+ *
+ * Copyright 2020  GNOME Foundation
+ *
+ * SPDX-License-Identifier: LGPL-2.1-or-later
+ *
+ * This library is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Lesser General Public
+ * License as published by the Free Software Foundation; either
+ * version 2.1 of the License, or (at your option) any later version.
+ *
+ * This library is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public
+ * License along with this library; if not, see <http://www.gnu.org/licenses/>.
+ */
+
+#include "config.h"
+
+#include "gtkaccessible.h"
+
+G_DEFINE_INTERFACE (GtkAccessible, gtk_accessible, G_TYPE_OBJECT)
+
+static void
+gtk_accessible_default_init (GtkAccessibleInterface *iface)
+{
+}
