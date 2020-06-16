7c1cf7781b6da82b24eb71ba99718fa8408f209b
Rico Tzschichholz <ricotz@ubuntu.com>
2020-06-16 15:31:29 +0200

    gdk: Drop gdk_device_get_axis_value leftovers

--- gdk/gdkdevice.h
+++ gdk/gdkdevice.h
@@ -131,11 +131,6 @@ GdkSurface * gdk_device_get_surface_at_position (GdkDevice *device,
 GDK_AVAILABLE_IN_ALL
 gint     gdk_device_get_n_axes     (GdkDevice       *device);
 GDK_AVAILABLE_IN_ALL
-gboolean gdk_device_get_axis_value (GdkDevice       *device,
-                                    gdouble         *axes,
-                                    gdouble         *value);
-
-GDK_AVAILABLE_IN_ALL
 gboolean gdk_device_get_axis     (GdkDevice         *device,
                                   gdouble           *axes,
                                   GdkAxisUse         use,
