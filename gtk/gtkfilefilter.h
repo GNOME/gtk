a777931213af2d6c289f00d93bc78811246d9f40
Matthias Clasen <mclasen@redhat.com>
2020-07-04 11:06:36 -0400

    Drop gtk_file_filter_filter
    It has been superseded by gtk_filter_match.

--- gtk/gtkfilefilter.h
+++ gtk/gtkfilefilter.h
@@ -56,9 +56,6 @@ void gtk_file_filter_add_pixbuf_formats (GtkFileFilter      *filter);
 
 GDK_AVAILABLE_IN_ALL
 const char **      gtk_file_filter_get_attributes (GtkFileFilter           *filter);
-GDK_AVAILABLE_IN_ALL
-gboolean           gtk_file_filter_filter     (GtkFileFilter           *filter,
-                                               GFileInfo               *info);
 
 GDK_AVAILABLE_IN_ALL
 GVariant      *gtk_file_filter_to_gvariant       (GtkFileFilter *filter);
