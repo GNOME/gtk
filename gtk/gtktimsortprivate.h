115d14e8d1ac924fb428c6a301dac47577146ebe
Benjamin Otte <otte@redhat.com>
2020-07-12 17:57:03 +0200

    timsort: Add progress estimation

--- gtk/gtktimsortprivate.h
+++ gtk/gtktimsortprivate.h
@@ -108,6 +108,8 @@ void            gtk_tim_sort_set_runs                           (GtkTimSort     
 void            gtk_tim_sort_set_max_merge_size                 (GtkTimSort             *self,
                                                                  gsize                   max_merge_size);
 
+gsize           gtk_tim_sort_get_progress                       (GtkTimSort             *self);
+
 gboolean        gtk_tim_sort_step                               (GtkTimSort             *self,
                                                                  GtkTimSortRun          *out_change);
 
