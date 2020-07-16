28920f1b8ec36f167a8e01e08ca96f8f334d4b51
Matthias Clasen <mclasen@redhat.com>
2020-07-16 08:49:38 -0400

    treesorter tests: Plug a leak
    g_list_model_get_item is transfer full.

--- testsuite/gtk/treesorter.c
+++ testsuite/gtk/treesorter.c
@@ -69,6 +69,8 @@ model_to_string (GListModel *model)
       if (i > 0)
         g_string_append (string, " ");
       g_string_append_printf (string, "%u", get_with_parents (object));
+
+      g_object_unref (object);
     }
 
   return g_string_free (string, FALSE);
