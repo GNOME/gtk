0b6df9efe77a66c86ea46770a64d4ce734986ef1
Matthias Clasen <mclasen@redhat.com>
2020-07-17 10:36:43 -0400

    testatcontext: Use the new debug flag
    Only dump a11y state if GTK_DEBUG=accessibility is set.

--- gtk/gtktestatcontext.c
+++ gtk/gtktestatcontext.c
@@ -23,7 +23,9 @@
 #include "gtktestatcontextprivate.h"
 
 #include "gtkatcontextprivate.h"
+#include "gtkdebug.h"
 #include "gtkenums.h"
+#include "gtkprivate.h"
 #include "gtktypebuiltins.h"
 
 struct _GtkTestATContext
@@ -53,15 +55,16 @@ gtk_test_at_context_state_change (GtkATContext                *self,
   char *properties_str = gtk_accessible_attribute_set_to_string (properties);
   char *relations_str = gtk_accessible_attribute_set_to_string (relations);
 
-  g_print ("*** Accessible state changed for accessible “%s”, with role %d:\n"
-           "***     states = %s\n"
-           "*** properties = %s\n"
-           "***  relations = %s\n",
-           G_OBJECT_TYPE_NAME (accessible),
-           role,
-           states_str,
-           properties_str,
-           relations_str);
+  GTK_NOTE(A11Y,
+   g_print ("*** Accessible state changed for accessible “%s”, with role %d:\n"
+            "***     states = %s\n"
+            "*** properties = %s\n"
+            "***  relations = %s\n",
+             G_OBJECT_TYPE_NAME (accessible),
+            role,
+            states_str,
+            properties_str,
+            relations_str));
 
   g_free (states_str);
   g_free (properties_str);
