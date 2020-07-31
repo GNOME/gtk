fc31f3d6299dd43e8608acb9007f604e00f44c39
Matthias Clasen <mclasen@redhat.com>
2020-07-31 12:18:54 -0400

    placesview: Keep up with GLib deprecations
    We were using deprecated GBookmarkFile api here too.

--- gtk/gtkplacesview.c
+++ gtk/gtkplacesview.c
@@ -278,6 +278,7 @@ server_list_add_server (GtkPlacesView *view,
   GError *error;
   char *title;
   char *uri;
+  GDateTime *now;
 
   error = NULL;
   bookmarks = server_list_load (view);
@@ -295,7 +296,9 @@ server_list_add_server (GtkPlacesView *view,
   title = g_file_info_get_attribute_as_string (info, G_FILE_ATTRIBUTE_STANDARD_DISPLAY_NAME);
 
   g_bookmark_file_set_title (bookmarks, uri, title);
-  g_bookmark_file_set_visited (bookmarks, uri, -1);
+  now = g_date_time_new_now_utc ();
+  g_bookmark_file_set_visited_date_time (bookmarks, uri, now);
+  g_date_time_unref (now);
   g_bookmark_file_add_application (bookmarks, uri, NULL, NULL);
 
   server_list_save (bookmarks);
