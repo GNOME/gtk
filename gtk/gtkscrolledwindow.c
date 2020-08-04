GNOME/gtk

commit 971ee2d28a65b816050ccd5bfb332f37c34f09fd

a11y: Set up controls relations for scrollbars
GtkScrolledWindow knows what its scrollbars control,
so set up the accessible relations for this.

gtk/gtkscrolledwindow.c
@@ -4139,6 +4139,7 @@ gtk_scrolled_window_set_child (GtkScrolledWindow *scrolled_window,
 {
   GtkScrolledWindowPrivate *priv = gtk_scrolled_window_get_instance_private (scrolled_window);
   GtkWidget *scrollable_child;
+  GList *list;
 
   g_return_if_fail (GTK_IS_SCROLLED_WINDOW (scrolled_window));
 
@@ -4193,6 +4194,15 @@ gtk_scrolled_window_set_child (GtkScrolledWindow *scrolled_window,
                     "vadjustment", vadj,
                     NULL);
     }
+
+  list = g_list_append (NULL, priv->child);
+  gtk_accessible_update_relation (GTK_ACCESSIBLE (priv->hscrollbar),
+                                  GTK_ACCESSIBLE_RELATION_CONTROLS, list,
+                                  -1);
+  gtk_accessible_update_relation (GTK_ACCESSIBLE (priv->vscrollbar),
+                                  GTK_ACCESSIBLE_RELATION_CONTROLS, list,
+                                  -1);
+  g_list_free (list);
 
   g_object_notify_by_pspec (G_OBJECT (scrolled_window), properties[PROP_CHILD]);
 }

commit 7398323b820ea7d1c432e3a0642d9c32531b6be4

scrolledwindow: Fix a11y support code
This error was introduced by me in 971ee2d28a65b816050c
and causes a crash in the scrolling benchmark in gtk-demo.

Fixes: #3014

gtk/gtkscrolledwindow.c
@@ -4195,7 +4195,10 @@ gtk_scrolled_window_set_child (GtkScrolledWindow *scrolled_window,
                     NULL);
     }
 
-  list = g_list_append (NULL, priv->child);
+  if (priv->child)
+    list = g_list_append (NULL, priv->child);
+  else
+    list = NULL;
   gtk_accessible_update_relation (GTK_ACCESSIBLE (priv->hscrollbar),
                                   GTK_ACCESSIBLE_RELATION_CONTROLS, list,
                                   -1);