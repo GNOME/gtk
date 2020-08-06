dcf37412c169d66dfda8ba8160b80ac02c3cc292
Matthias Clasen <mclasen@redhat.com>
2020-08-05 20:52:22 -0400

    gtk-demo: Fix keynav in the puzzle demo
    Fixes: #3023

--- demos/gtk-demo/sliding_puzzle.c
+++ demos/gtk-demo/sliding_puzzle.c
@@ -272,7 +272,7 @@ start_puzzle (GdkPaintable *paintable)
 
   /* Create a new grid */
   grid = gtk_grid_new ();
-  gtk_widget_set_can_focus (grid, TRUE);
+  gtk_widget_set_focusable (grid, TRUE);
   gtk_aspect_frame_set_child (GTK_ASPECT_FRAME (frame), grid);
   aspect_ratio = gdk_paintable_get_intrinsic_aspect_ratio (paintable);
   if (aspect_ratio == 0.0)
@@ -284,7 +284,7 @@ start_puzzle (GdkPaintable *paintable)
    * keys to move the puzzle */
   controller = gtk_shortcut_controller_new ();
   gtk_shortcut_controller_set_scope (GTK_SHORTCUT_CONTROLLER (controller),
-                                     GTK_SHORTCUT_SCOPE_GLOBAL);
+                                     GTK_SHORTCUT_SCOPE_LOCAL);
   add_move_binding (GTK_SHORTCUT_CONTROLLER (controller),
                     GDK_KEY_Left, GDK_KEY_KP_Left,
                     -1, 0);
