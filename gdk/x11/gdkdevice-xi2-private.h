62a4a356c9eb069bfaa512cdcda9c35dde7f70e1
Matthias Clasen <mclasen@redhat.com>
2020-08-26 15:31:07 -0400

    x11: Stop using _gdk_device_query_state
    Directly use the backend implementation.

---
+++ gdk/x11/gdkdevice-xi2-private.h
@@ -0,0 +1,10 @@
+#ifndef __GDK_DEVICE_XI2_PRIVATE_H__
+#define __GDK_DEVICE_XI2_PRIVATE_H__
+
+void gdk_x11_device_xi2_query_state (GdkDevice        *device,
+                                     GdkSurface        *surface,
+                                     GdkSurface       **child_surface,
+                                     double           *win_x,
+                                     double           *win_y,
+                                     GdkModifierType  *mask);
+#endif
