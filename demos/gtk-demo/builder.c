a3dcfafba689dd646513587930371f9a7384bf11
Matthias Clasen <mclasen@redhat.com>
2020-09-14 17:06:34 -0400

    gtk-demo: Improve the builder demo
    Make the keyboard shortcuts work.

--- demos/gtk-demo/builder.c
+++ demos/gtk-demo/builder.c
@@ -105,6 +105,7 @@ do_builder (GtkWidget *do_widget)
       GtkBuilder *builder;
       GtkWidget *about;
       GtkWidget *status;
+      GtkEventController *controller;
 
       builder = gtk_builder_new_from_resource ("/builder/demo.ui");
 
@@ -117,6 +118,41 @@ do_builder (GtkWidget *do_widget)
                                        win_entries, G_N_ELEMENTS (win_entries),
                                        window);
       gtk_widget_insert_action_group (window, "win", actions);
+
+      controller = gtk_shortcut_controller_new ();
+      gtk_shortcut_controller_set_scope (GTK_SHORTCUT_CONTROLLER (controller),
+                                         GTK_SHORTCUT_SCOPE_GLOBAL);
+      gtk_widget_add_controller (window, controller);
+      gtk_shortcut_controller_add_shortcut (GTK_SHORTCUT_CONTROLLER (controller),
+           gtk_shortcut_new (gtk_keyval_trigger_new (GDK_KEY_n, GDK_CONTROL_MASK),
+                             gtk_named_action_new ("win.new")));
+      gtk_shortcut_controller_add_shortcut (GTK_SHORTCUT_CONTROLLER (controller),
+           gtk_shortcut_new (gtk_keyval_trigger_new (GDK_KEY_o, GDK_CONTROL_MASK),
+                             gtk_named_action_new ("win.open")));
+      gtk_shortcut_controller_add_shortcut (GTK_SHORTCUT_CONTROLLER (controller),
+           gtk_shortcut_new (gtk_keyval_trigger_new (GDK_KEY_s, GDK_CONTROL_MASK),
+                             gtk_named_action_new ("win.save")));
+      gtk_shortcut_controller_add_shortcut (GTK_SHORTCUT_CONTROLLER (controller),
+           gtk_shortcut_new (gtk_keyval_trigger_new (GDK_KEY_s, GDK_CONTROL_MASK|GDK_SHIFT_MASK),
+                             gtk_named_action_new ("win.save-as")));
+      gtk_shortcut_controller_add_shortcut (GTK_SHORTCUT_CONTROLLER (controller),
+           gtk_shortcut_new (gtk_keyval_trigger_new (GDK_KEY_q, GDK_CONTROL_MASK),
+                             gtk_named_action_new ("win.quit")));
+      gtk_shortcut_controller_add_shortcut (GTK_SHORTCUT_CONTROLLER (controller),
+           gtk_shortcut_new (gtk_keyval_trigger_new (GDK_KEY_c, GDK_CONTROL_MASK),
+                             gtk_named_action_new ("win.copy")));
+      gtk_shortcut_controller_add_shortcut (GTK_SHORTCUT_CONTROLLER (controller),
+           gtk_shortcut_new (gtk_keyval_trigger_new (GDK_KEY_x, GDK_CONTROL_MASK),
+                             gtk_named_action_new ("win.cut")));
+      gtk_shortcut_controller_add_shortcut (GTK_SHORTCUT_CONTROLLER (controller),
+           gtk_shortcut_new (gtk_keyval_trigger_new (GDK_KEY_v, GDK_CONTROL_MASK),
+                             gtk_named_action_new ("win.paste")));
+      gtk_shortcut_controller_add_shortcut (GTK_SHORTCUT_CONTROLLER (controller),
+           gtk_shortcut_new (gtk_keyval_trigger_new (GDK_KEY_F1, 0),
+                             gtk_named_action_new ("win.help")));
+      gtk_shortcut_controller_add_shortcut (GTK_SHORTCUT_CONTROLLER (controller),
+           gtk_shortcut_new (gtk_keyval_trigger_new (GDK_KEY_F7, 0),
+                             gtk_named_action_new ("win.about")));
 
       about = GTK_WIDGET (gtk_builder_get_object (builder, "aboutdialog1"));
       gtk_window_set_transient_for (GTK_WINDOW (about), GTK_WINDOW (window));
