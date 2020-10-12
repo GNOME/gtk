3bfb32e699cb7b230382fec5e2058722d3a57515
Matthias Clasen <mclasen@redhat.com>
2020-10-12 16:12:14 -0400

    Cosmetics

--- gtk/gtkaccessibleprivate.h
+++ gtk/gtkaccessibleprivate.h
@@ -37,7 +37,7 @@ GtkATContext *  gtk_accessible_get_at_context   (GtkAccessible *self);
 const char *    gtk_accessible_role_to_name     (GtkAccessibleRole  role,
                                                  const char        *domain);
 
-void gtk_accessible_platform_changed (GtkAccessible               *self,
-                                      GtkAccessiblePlatformChange  change);
+void            gtk_accessible_platform_changed (GtkAccessible               *self,
+                                                 GtkAccessiblePlatformChange  change);
 
 G_END_DECLS
