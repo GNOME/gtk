76b87272a6fc48384405a17a55761b44f535cbe5
Matthias Clasen <mclasen@redhat.com>
2020-10-12 21:34:16 -0400

    spinbutton: Implement GtkAccessible
    This copies what was done for GtkEntry: get
    the focused state from the GtkText within.
    
    We also add a private getter for the text widget,
    which was missing here.

---
+++ gtk/gtkspinbuttonprivate.h
@@ -0,0 +1,27 @@
+/* GTK - The GIMP Toolkit
+ * Copyright (C) 2020 Red Hat, Inc.
+ *
+ * This library is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Lesser General Public
+ * License as published by the Free Software Foundation; either
+ * version 2 of the License, or (at your option) any later version.
+ *
+ * This library is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public
+ * License along with this library. If not, see <http://www.gnu.org/licenses/>.
+ */
+
+#pragma once
+
+#include <gtk/gtkspinbutton.h>
+#include <gtk/gtktext.h>
+
+G_BEGIN_DECLS
+
+GtkText *gtk_spin_button_get_text_widget (GtkSpinButton *spin_button);
+
+G_END_DECLS
