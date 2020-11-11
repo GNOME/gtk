45992b26722a474930d7d54a8e34f82d527a2308
Benjamin Otte <otte@redhat.com>
2020-11-11 07:36:04 +0100

    gsk: Add GskStrokeNode

--- gsk/gl/gskglrenderer.c
+++ gsk/gl/gskglrenderer.c
@@ -3808,6 +3808,7 @@ gsk_gl_renderer_add_render_ops (GskGLRenderer   *self,
     case GSK_REPEATING_LINEAR_GRADIENT_NODE:
     case GSK_REPEATING_RADIAL_GRADIENT_NODE:
     case GSK_FILL_NODE:
+    case GSK_STROKE_NODE:
     case GSK_CAIRO_NODE:
     default:
       {
