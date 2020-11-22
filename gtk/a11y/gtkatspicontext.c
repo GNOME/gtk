0a46baeb5684533c7300b6b5212a7fd080f71d98
Emmanuele Bassi <ebassi@gnome.org>
2020-11-22 18:39:39 +0000

    a11y: Turn critical warnings into debug messages
    The accessibility bus might not be available, and if it isn't the case,
    it means something has failed at a level where the user can't do much
    about it. There's no need to emit a critical warning.

--- gtk/a11y/gtkatspicontext.c
+++ gtk/a11y/gtkatspicontext.c
@@ -1598,7 +1598,7 @@ get_bus_address_dbus (GdkDisplay *display)
 
   if (error != NULL)
     {
-      g_critical ("Unable to acquire session bus: %s", error->message);
+      GTK_NOTE (A11Y, g_message ("Unable to acquire session bus: %s", error->message));
       g_error_free (error);
       return NULL;
     }
@@ -1615,8 +1615,8 @@ get_bus_address_dbus (GdkDisplay *display)
                                   &error);
   if (error != NULL)
     {
-      g_critical ("Unable to acquire the address of the accessibility bus: %s",
-                  error->message);
+      GTK_NOTE (A11Y, g_message ("Unable to acquire the address of the accessibility bus: %s",
+                                 error->message));
       g_error_free (error);
     }
 
