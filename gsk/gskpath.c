03c863e369276a1a8f0bfea1ca7accea89f925e4
Matthias Clasen <mclasen@redhat.com>
2020-11-26 13:25:15 -0500

    Fix bounding boxes for axis-aligned lines
    The docs say "returns FALSE if the path is known
    to be empty". Lines are not empty, so don't return
    FALSE. Even if they are axis-aligned.

--- gsk/gskpath.c
+++ gsk/gskpath.c
@@ -973,7 +973,7 @@ gsk_standard_contour_get_bounds (const GskContour *contour,
       rect_add_point (bounds, &self->points[i]);
     }
 
-  return bounds->size.width > 0 && bounds->size.height > 0;
+  return TRUE;
 }
 
 typedef struct
