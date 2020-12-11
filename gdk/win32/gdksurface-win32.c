fb33e83c96f6eadca409f3240071d5ddeb7c7573
Chun-wei Fan <fanchunwei@src.gnome.org>
2020-12-11 10:37:43 +0800

    gdkwin32-surface.c: Fix return type
    gdk_win32_toplevel_present() should now be returning nothing, not a gboolean.

--- gdk/win32/gdksurface-win32.c
+++ gdk/win32/gdksurface-win32.c
@@ -4924,7 +4924,7 @@ gdk_win32_toplevel_class_init (GdkWin32ToplevelClass *class)
   gdk_toplevel_install_properties (object_class, 1);
 }
 
-static gboolean
+static void
 gdk_win32_toplevel_present (GdkToplevel       *toplevel,
                             GdkToplevelLayout *layout)
 {
@@ -5004,8 +5004,6 @@ gdk_win32_toplevel_present (GdkToplevel       *toplevel,
                                           size.shadow.top,
                                           size.shadow.bottom);
     }
-
-  return TRUE;
 }
 
 static gboolean
