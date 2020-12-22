90dfb5e138c0f4cfcf59cb0cb780667b5de2ae3e
Marc-André Lureau <marcandre.lureau@redhat.com>
2020-12-22 16:36:13 +0400

    gtk: remove GtkClipboard leftovers
    Signed-off-by: Marc-André Lureau <marcandre.lureau@redhat.com>

--- gtk/gtktypes.h
+++ gtk/gtktypes.h
@@ -37,7 +37,6 @@ typedef struct _GtkAdjustment          GtkAdjustment;
 typedef struct _GtkBitset              GtkBitset;
 typedef struct _GtkBuilder             GtkBuilder;
 typedef struct _GtkBuilderScope        GtkBuilderScope;
-typedef struct _GtkClipboard	       GtkClipboard;
 typedef struct _GtkCssStyleChange      GtkCssStyleChange;
 typedef struct _GtkEventController     GtkEventController;
 typedef struct _GtkGesture             GtkGesture;
