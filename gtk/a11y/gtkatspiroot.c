GNOME/gtk

commit 03b60a2d5e40da1b137117aba05a2c818edd09b3

a11y: Sanitize the AT-SPI object path further
When falling back to g_get_prgname(), we need to take into account that
the program name may be the full argv[0] path, which will end up messing
the DBus object path.

gtk/a11y/gtkatspiroot.c
@@ -667,8 +667,16 @@ gtk_at_spi_root_constructed (GObject *gobject)
     {
       const char *program_name = g_get_prgname ();
 
+      char *base_name = NULL;
+      if (program_name == NULL || *program_name == 0)
+        base_name = g_strdup ("unknown");
+      else if (*program_name == '/')
+        base_name = g_path_get_basename (program_name);
+      else
+        base_name = g_strdup (program_name);
+
       self->base_path = g_strconcat ("/org/gtk/application/",
-                                     program_name != NULL ? program_name : "unknown",
+                                     base_name,
                                      "/a11y",
                                      NULL);
 

commit a56b2900dd1d633c3abfc40c394ab5ec2fc16197

a11y: Fix leak
Introduced by me in commit 03b60a2d5e40da1b137117aba05a2c818edd09b3.

gtk/a11y/gtkatspiroot.c
@@ -680,6 +680,8 @@ gtk_at_spi_root_constructed (GObject *gobject)
                                      "/a11y",
                                      NULL);
 
+      g_free (base_name);
+
       /* Turn potentially invalid program names into something that can be
        * used as a DBus path
        */