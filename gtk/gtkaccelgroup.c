260a4db854582fd2129e2549189d708182153173
Matthias Clasen <mclasen@redhat.com>
2021-03-01 11:11:40 -0500

    accelgroup: Convert docs
    Drop an unused SECTION, and convert links.

--- gtk/gtkaccelgroup.c
+++ gtk/gtkaccelgroup.c
@@ -32,20 +32,6 @@
 #include "gtkmarshalers.h"
 #include "gtkprivate.h"
 
-/**
- * SECTION:gtkaccelgroup
- * @Short_description: Utilities for accelerators
- * @Title: Keyboard Accelerators
- *
- * We have various utility functions to parse and generate
- * textual representations of keyboard accelerators.
- *
- * If you want to set up keyboard accelerators for widgets,
- * #GtkShortcutTrigger is probably more convenient than the
- * functions in this section.
- */
-
-
 /* --- functions --- */
 /**
  * gtk_accelerator_valid:
@@ -250,10 +236,11 @@ is_keycode (const char *string)
  * @accelerator_mods: (out) (allow-none): return location for accelerator
  *     modifier mask, %NULL
  *
- * Parses a string representing an accelerator, similarly to
- * gtk_accelerator_parse() but handles keycodes as well. This is only
- * useful for system-level components, applications should use
- * gtk_accelerator_parse() instead.
+ * Parses a string representing an accelerator.
+ *
+ * This is similar to [func@Gtk.accelerator_parse] but handles keycodes as
+ * well. This is only useful for system-level components, applications should
+ * use gtk_accelerator_parse() instead.
  *
  * If @accelerator_codes is given and the result stored in it is non-%NULL,
  * the result must be freed with g_free().
@@ -497,12 +484,13 @@ out:
  * @accelerator_mods: (out) (allow-none): return location for accelerator
  *     modifier mask, %NULL
  *
- * Parses a string representing an accelerator. The format looks like
- * “<Control>a” or “<Shift><Alt>F1”.
+ * Parses a string representing an accelerator.
+ *
+ * The format looks like “<Control>a” or “<Shift><Alt>F1”.
  *
  * The parser is fairly liberal and allows lower or upper case, and also
  * abbreviations such as “<Ctl>” and “<Ctrl>”. Key names are parsed using
- * gdk_keyval_from_name(). For character keys the name is not the symbol,
+ * [func@Gdk.keyval_from_name]. For character keys the name is not the symbol,
  * but the lowercase name, e.g. one would use “<Ctrl>minus” instead of
  * “<Ctrl>-”.
  *
@@ -525,8 +513,9 @@ gtk_accelerator_parse (const char      *accelerator,
  * @accelerator_mods: accelerator modifier mask
  *
  * Converts an accelerator keyval and modifier mask
- * into a string parseable by gtk_accelerator_parse_with_keycode(),
- * similarly to gtk_accelerator_name() but handling keycodes.
+ * into a string parseable by gtk_accelerator_parse_with_keycode().
+ *
+ * This is similar to [func@Gtk.accelerator_name] but handling keycodes.
  * This is only useful for system-level components, applications
  * should use gtk_accelerator_parse() instead.
  *
@@ -565,7 +554,7 @@ gtk_accelerator_name_with_keycode (GdkDisplay      *display,
  * this function returns `<Control>q`.
  *
  * If you need to display accelerators in the user interface,
- * see gtk_accelerator_get_label().
+ * see [func@Gtk.accelerator_get_label].
  *
  * Returns: (transfer full): a newly-allocated accelerator name
  */
@@ -640,9 +629,12 @@ gtk_accelerator_name (guint           accelerator_key,
  * @accelerator_mods: accelerator modifier mask
  *
  * Converts an accelerator keyval and modifier mask
- * into a (possibly translated) string that can be displayed to
- * a user, similarly to gtk_accelerator_get_label(), but handling
- * keycodes.
+ * into a string that can be displayed to the user.
+ *
+ * The string may be translated.
+ *
+ * This function is similar to [func@Gtk.accelerator_get_label],
+ * but handling keycodes.
  *
  * This is only useful for system-level components, applications
  * should use gtk_accelerator_parse() instead.
