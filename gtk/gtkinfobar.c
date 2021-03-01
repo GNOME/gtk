d4f2121bc2796d0fa3059f92ca0e00ad1d65c75c
Matthias Clasen <mclasen@redhat.com>
2021-03-01 01:39:05 -0500

    infobar: Tweak docs

--- gtk/gtkinfobar.c
+++ gtk/gtkinfobar.c
@@ -50,8 +50,7 @@
 /**
  * GtkInfoBar:
  *
- * `GtkInfoBar` is a widget that can be used to show messages to
- * the user without showing a dialog.
+ * `GtkInfoBar` can be show messages to the user without a dialog.
  *
  * ![An example GtkInfoBar](info-bar.png)
  *
