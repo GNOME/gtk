9f18c138d1bb7e412a88d77bed63f4289b7aa16c
Benjamin Otte <otte@redhat.com>
2021-03-06 20:43:52 +0100

    vulkan: Fix invalid read
    Look at the right rect to compute circularness

--- gsk/vulkan/gskvulkanclip.c
+++ gsk/vulkan/gskvulkanclip.c
@@ -113,7 +113,7 @@ gsk_vulkan_clip_intersect_rounded_rect (GskVulkanClip        *dest,
     case GSK_VULKAN_CLIP_RECT:
       if (graphene_rect_contains_rect (&src->rect.bounds, &rounded->bounds))
         {
-          dest->type = gsk_rounded_rect_is_circular (&dest->rect) ? GSK_VULKAN_CLIP_ROUNDED_CIRCULAR : GSK_VULKAN_CLIP_ROUNDED;
+          dest->type = gsk_rounded_rect_is_circular (rounded) ? GSK_VULKAN_CLIP_ROUNDED_CIRCULAR : GSK_VULKAN_CLIP_ROUNDED;
           gsk_rounded_rect_init_copy (&dest->rect, rounded);
           return TRUE;
         }
