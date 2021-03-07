0cfe3d45f5ffabc1ef30c066de65ec7dfcb82870
Emmanuele Bassi <ebassi@gnome.org>
2021-03-07 18:25:07 +0000

    celleditable: Remove class cast check macro

--- gtk/gtkcelleditable.h
+++ gtk/gtkcelleditable.h
@@ -28,7 +28,6 @@ G_BEGIN_DECLS
 
 #define GTK_TYPE_CELL_EDITABLE            (gtk_cell_editable_get_type ())
 #define GTK_CELL_EDITABLE(obj)            (G_TYPE_CHECK_INSTANCE_CAST ((obj), GTK_TYPE_CELL_EDITABLE, GtkCellEditable))
-#define GTK_CELL_EDITABLE_CLASS(obj)      (G_TYPE_CHECK_CLASS_CAST ((obj), GTK_TYPE_CELL_EDITABLE, GtkCellEditableIface))
 #define GTK_IS_CELL_EDITABLE(obj)         (G_TYPE_CHECK_INSTANCE_TYPE ((obj), GTK_TYPE_CELL_EDITABLE))
 #define GTK_CELL_EDITABLE_GET_IFACE(obj)  (G_TYPE_INSTANCE_GET_INTERFACE ((obj), GTK_TYPE_CELL_EDITABLE, GtkCellEditableIface))
 
