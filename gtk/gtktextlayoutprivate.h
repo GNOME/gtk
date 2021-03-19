21aa8ae29fa96db4d0eeab209b4f2f3dcf9d8f3e
Matthias Clasen <mclasen@redhat.com>
2021-03-18 20:30:07 -0400

    Cosmetics
    Remove a stale comment.

--- gtk/gtktextlayoutprivate.h
+++ gtk/gtktextlayoutprivate.h
@@ -113,18 +113,10 @@ struct _GtkTextLayout
   int width;
   int height;
 
-  /* Pixel offsets from the left and from the top to be used when we
-   * draw; these allow us to create left/top margins. We don't need
-   * anything special for bottom/right margins, because those don't
-   * affect drawing.
-   */
-  /* int left_edge; */
-  /* int top_edge; */
-
-  GtkTextBuffer *buffer;
-
   int left_padding;
   int right_padding;
+
+  GtkTextBuffer *buffer;
 
   /* Default style used if no tags override it */
   GtkTextAttributes *default_style;
@@ -140,7 +132,7 @@ struct _GtkTextLayout
 
   /* Whether we are allowed to wrap right now */
   int wrap_loop_count;
-  
+
   /* Whether to show the insertion cursor */
   guint cursor_visible : 1;
 
