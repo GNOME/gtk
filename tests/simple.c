4ced8d1803e1320b43f0d9822d87e91951bdc6ce
Matthias Clasen <mclasen@redhat.com>
2021-04-01 07:49:50 -0400

    Add a simple example

--- tests/simple.c
+++ tests/simple.c
@@ -50,7 +50,7 @@ main (int argc, char *argv[])
   g_signal_connect (window, "destroy", G_CALLBACK (quit_cb), &done);
 
   button = gtk_button_new ();
-  gtk_button_set_label (GTK_BUTTON (button), "hello world");
+  gtk_button_set_label (GTK_BUTTON (button), "good world");
   gtk_widget_set_margin_top (button, 10);
   gtk_widget_set_margin_bottom (button, 10);
   gtk_widget_set_margin_start (button, 10);
