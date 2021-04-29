d8f44c06e8cd417b4a0d1861f06ea68c49cb0fbd
Xavier Claessens <xavier.claessens@collabora.com>
2021-04-29 11:09:04 -0400

    ci: Add MSVC build
    This script is copied from glib CI.

---
+++ .gitlab-ci/test-msvc.bat
@@ -0,0 +1,14 @@
+@echo on
+:: vcvarsall.bat sets various env vars like PATH, INCLUDE, LIB, LIBPATH for the
+:: specified build architecture
+call "C:\Program Files (x86)\Microsoft Visual Studio\2017\BuildTools\VC\Auxiliary\Build\vcvarsall.bat" x64
+@echo on
+
+:: FIXME: make warnings fatal
+pip3 install --upgrade --user meson==0.56.2  || goto :error
+meson _build || goto :error
+ninja -C _build || goto :error
+
+goto :EOF
+:error
+exit /b 1
