4390f8102bb40a351cec1f6065e369a87ed7a09d
Matthias Clasen <mclasen@redhat.com>
2021-07-13 07:10:01 -0400

    imcontext: Improve the docs
    Add more details about supported input methods.

--- gtk/gtkimcontextsimple.c
+++ gtk/gtkimcontextsimple.c
@@ -40,6 +40,8 @@
  *
  * `GtkIMContextSimple` is an input method supporting table-based input methods.
  *
+ * ## Compose sequences
+ *
  * `GtkIMContextSimple` reads compose sequences from the first of the
  * following files that is found: ~/.config/gtk-4.0/Compose, ~/.XCompose,
  * /usr/share/X11/locale/$locale/Compose (for locales that have a nontrivial
@@ -48,6 +50,10 @@
  *
  * If none of these files is found, `GtkIMContextSimple` uses a built-in table
  * of compose sequences that is derived from the X11 Compose files.
+ *
+ * Note that compose sequences typically start with the Compose_key, which is
+ * often not available as a dedicated key on keyboards. Keyboard layouts may
+ * map this keysym to other keys, such as the right Control key.
  *
  * ## Unicode characters
  *
@@ -60,6 +66,15 @@
  *     Ctrl-Shift-u 1 2 3 Enter
  *
  * yields U+0123 LATIN SMALL LETTER G WITH CEDILLA, i.e. ģ.
+ *
+ * ## Dead keys
+ *
+ * `GtkIMContextSimple` supports dead keys. For example, typing
+ *
+ *     dead_acute a
+ *
+ *  yields U+00E! LATIN SMALL LETTER_A WITH ACUTE, i.e. á. Note that this
+ *  depends on the keyboard layout including dead keys.
  */
 
 struct _GtkIMContextSimplePrivate
