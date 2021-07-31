c2e7aac03eea5ce9a6b3f2d83a391bd84c760823
Emmanuele Bassi <ebassi@gnome.org>
2021-07-31 16:41:57 +0100

    Separate the API reference publishing to its own branch
    We still build the API reference as part of the regular CI pipeline, but
    we use a separate branch that allows us to build additional things, like
    the GLib API reference, as well as keep all the content of docs.gtk.org
    separate from the main repository.

---
+++ download-reference.sh
@@ -0,0 +1,29 @@
+#!/bin/sh
+
+REF="master"
+
+curl -L --output "$REF-docs.zip" "https://gitlab.gnome.org/GNOME/gtk/-/jobs/artifacts/$REF/download?job=reference"
+unzip -d "$REF-docs" "$REF-docs.zip"
+
+rm -f "$REF-docs.zip"
+
+DOCS_DIR="docs"
+
+mkdir -p $DOCS_DIR
+
+SECTIONS="
+gdk4
+gdk4-x11
+gdk4-wayland
+gsk4
+gtk4
+"
+
+IFS='
+'
+
+for SECTION in $SECTIONS; do
+        mv "$REF-docs/_reference/$SECTION" "$DOCS_DIR/$SECTION"
+done
+
+rm -rf "$REF-docs"
---
+++ glib/gobject/urlmap.js
@@ -0,0 +1,6 @@
+var baseUrls = [
+    [ 'GLib', 'https://docs.gtk.org/glib/' ],
+    [ 'GObject', 'https://docs.gtk.org/gobject/' ],
+    [ 'Gio', 'https://docs.gtk.org/gio/' ],
+    [ 'Gtk', 'https://docs.gtk.org/gtk4/' ],
+];
