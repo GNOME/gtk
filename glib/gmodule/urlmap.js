5bd66748744c9b8718eff2afe1783901d8d709e3
Emmanuele Bassi <ebassi@gnome.org>
2021-08-24 12:02:55 +0100

    Add GModule API
    In the gtk-doc API reference GModule is part of GLib, but for
    introspection purposes, GModule has its own namespace, which means it
    requires its own documentation module.

---
+++ glib/gmodule/urlmap.js
@@ -0,0 +1,7 @@
+var baseUrls = [
+    [ 'GLib', 'https://docs.gtk.org/glib/' ],
+    [ 'GModule', 'https://docs.gtk.org/gmodule/' ],
+    [ 'GObject', 'https://docs.gtk.org/gobject/' ],
+    [ 'Gio', 'https://docs.gtk.org/gio/' ],
+    [ 'Gtk', 'https://docs.gtk.org/gtk4/' ],
+];
