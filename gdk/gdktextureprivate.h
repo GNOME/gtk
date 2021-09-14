100b0bf7d92d490254e900052f2b351b29fa19fc
Benjamin Otte <otte@redhat.com>
2021-09-14 16:26:05 +0200

    texture: Remove declaration for nonexisting function

--- gdk/gdktextureprivate.h
+++ gdk/gdktextureprivate.h
@@ -35,9 +35,6 @@ struct _GdkTextureClass {
                                                          gsize                   stride);
 };
 
-gpointer                gdk_texture_new                 (const GdkTextureClass  *klass,
-                                                         int                     width,
-                                                         int                     height);
 GdkTexture *            gdk_texture_new_for_surface     (cairo_surface_t        *surface);
 cairo_surface_t *       gdk_texture_download_surface    (GdkTexture             *texture);
 /* NB: GdkMemoryTexture */
