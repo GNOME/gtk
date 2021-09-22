48781cf7f7b67fe2f769423150b8fecce67a3549
Benjamin Otte <otte@redhat.com>
2021-09-22 02:01:41 +0200

    memorytexture: Split out GdkMemoryFormat handling
    Also, now make gdk_memory_convert() the only conversion functions
    and allow conversions between any 2 formats by going via a float[4].
    
    This could be optimized via fast-paths, but so far it isn't.

--- gdk/loaders/gdktiff.c
+++ gdk/loaders/gdktiff.c
@@ -20,6 +20,7 @@
 #include "gdktiffprivate.h"
 
 #include "gdkintl.h"
+#include "gdkmemoryformatprivate.h"
 #include "gdkmemorytextureprivate.h"
 #include "gdkprofilerprivate.h"
 #include "gdktexture.h"
