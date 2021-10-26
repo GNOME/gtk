GNOME/gtk

commit 0886ade182f1a958024202d5c20248d57fcdb053

cssparser: Make gsk_renderer_consume_url() return a string
We don't want to return a GFile because GFile can't handle can't deal
with data: urls.
That makes the code a bit more complicated that doesn't deal with those
URLs, but it makes the other code actually work.

GtkCssImageUrl also now decodes data urls immediately instead of only at
the first load. So don't use data urls if you care about performance.

gtk/gtkcssimageurl.c
@@ -54,31 +54,6 @@ gtk_css_image_url_load_image (GtkCssImageUrl  *url,
       g_free (resource_path);
       g_free (uri);
     }
-  else if (g_file_has_uri_scheme (url->file, "data"))
-    {
-      GInputStream *stream;
-      char *uri;
-      GdkPixbuf *pixbuf;
-      GBytes *bytes;
-
-      uri = g_file_get_uri (url->file);
-      texture = NULL;
-
-      bytes = gtk_css_data_url_parse (uri, NULL, &local_error);
-      if (bytes)
-        {
-          stream = g_memory_input_stream_new_from_bytes (bytes);
-          pixbuf = gdk_pixbuf_new_from_stream (stream, NULL, &local_error);
-          g_object_unref (stream);
-          if (pixbuf != NULL)
-            {
-              texture = gdk_texture_new_for_pixbuf (pixbuf);
-              g_object_unref (pixbuf);
-            }
-        }
-
-      g_free (uri);
-    }
   else
     {
       texture = gdk_texture_new_from_file (url->file, &local_error);
@@ -190,11 +165,51 @@ static gboolean
 gtk_css_image_url_parse (GtkCssImage  *image,
                          GtkCssParser *parser)
 {
-  GtkCssImageUrl *url = GTK_CSS_IMAGE_URL (image);
+  GtkCssImageUrl *self = GTK_CSS_IMAGE_URL (image);
+  char *url, *scheme;
 
-  url->file = gtk_css_parser_consume_url (parser);
-  if (url->file == NULL)
+  url = gtk_css_parser_consume_url (parser);
+  if (url == NULL)
     return FALSE;
+
+  scheme = g_uri_parse_scheme (url);
+  if (scheme && g_ascii_strcasecmp (scheme, "data") == 0)
+    {
+      GInputStream *stream;
+      GdkPixbuf *pixbuf;
+      GBytes *bytes;
+      GError *error = NULL;
+
+      bytes = gtk_css_data_url_parse (url, NULL, &error);
+      if (bytes)
+        {
+          stream = g_memory_input_stream_new_from_bytes (bytes);
+          pixbuf = gdk_pixbuf_new_from_stream (stream, NULL, &error);
+          g_object_unref (stream);
+          if (pixbuf == NULL)
+            {
+              gtk_css_parser_emit_error (parser,
+                                         gtk_css_parser_get_start_location (parser),
+                                         gtk_css_parser_get_end_location (parser),
+                                         error);
+              g_clear_error (&error);
+            }
+          else
+            {
+              GdkTexture *texture = gdk_texture_new_for_pixbuf (pixbuf);
+              self->loaded_image = gtk_css_image_paintable_new (GDK_PAINTABLE (texture), GDK_PAINTABLE (texture));
+              g_object_unref (texture);
+              g_object_unref (pixbuf);
+            }
+        }
+    }
+  else
+    {
+      self->file = gtk_css_parser_resolve_url (parser, url);
+    }
+
+  g_free (url);
+  g_free (scheme);
 
   return TRUE;
 }

gsk/gskrendernodeparser.c
@@ -71,27 +71,26 @@ static gboolean
 parse_texture (GtkCssParser *parser,
                gpointer      out_data)
 {
-  GFile *file;
   GdkTexture *texture;
   GError *error = NULL;
   GtkCssLocation start_location;
+  char *url, *scheme;
 
   start_location = *gtk_css_parser_get_start_location (parser);
-  file = gtk_css_parser_consume_url (parser);
-  if (file == NULL)
+  url = gtk_css_parser_consume_url (parser);
+  if (url == NULL)
     return FALSE;
 
-  if (g_file_has_uri_scheme (file, "data"))
+  scheme = g_uri_parse_scheme (url);
+  if (scheme && g_ascii_strcasecmp (scheme, "data") == 0)
     {
       GInputStream *stream;
-      char *uri;
       GdkPixbuf *pixbuf;
       GBytes *bytes;
 
-      uri = g_file_get_uri (file);
       texture = NULL;
 
-      bytes = gtk_css_data_url_parse (uri, NULL, &error);
+      bytes = gtk_css_data_url_parse (url, NULL, &error);
       if (bytes)
         {
           stream = g_memory_input_stream_new_from_bytes (bytes);
@@ -103,14 +102,18 @@ parse_texture (GtkCssParser *parser,
               g_object_unref (pixbuf);
             }
         }
-
-      g_free (uri);
     }
   else
     {
+      GFile *file;
+
+      file = gtk_css_parser_resolve_url (parser, url);
       texture = gdk_texture_new_from_file (file, &error);
+      g_object_unref (file);
     }
-  g_object_unref (file);
+
+  g_free (scheme);
+  g_free (url);
 
   if (texture == NULL)
     {

gtk/css/gtkcssparser.c
@@ -973,11 +973,10 @@ gtk_css_parser_parse_url_arg (GtkCssParser *parser,
  *
  * Returns: (nullable) (transfer full): the resulting URL or %NULL on error
  **/
-GFile *
+char *
 gtk_css_parser_consume_url (GtkCssParser *self)
 {
   const GtkCssToken *token;
-  GFile *result;
   char *url;
 
   token = gtk_css_parser_get_token (self);
@@ -998,16 +997,7 @@ gtk_css_parser_consume_url (GtkCssParser *self)
       return NULL;
     }
   
-  result = gtk_css_parser_resolve_url (self, url);
-  if (result == NULL)
-    {
-      gtk_css_parser_error_import (self, "Could not resolve \"%s\" to a valid URL", url);
-      g_free (url);
-      return NULL;
-    }
-  g_free (url);
-
-  return result;
+  return url;
 }
 
 gboolean

gtk/css/gtkcssparserprivate.h
@@ -134,7 +134,7 @@ gboolean                gtk_css_parser_try_token                (GtkCssParser   
 
 char *                  gtk_css_parser_consume_ident            (GtkCssParser                   *self) G_GNUC_WARN_UNUSED_RESULT;
 char *                  gtk_css_parser_consume_string           (GtkCssParser                   *self) G_GNUC_WARN_UNUSED_RESULT;
-GFile *                 gtk_css_parser_consume_url              (GtkCssParser                   *self) G_GNUC_WARN_UNUSED_RESULT;
+char *                  gtk_css_parser_consume_url              (GtkCssParser                   *self) G_GNUC_WARN_UNUSED_RESULT;
 gboolean                gtk_css_parser_consume_number           (GtkCssParser                   *self,
                                                                  double                         *number);
 gboolean                gtk_css_parser_consume_integer          (GtkCssParser                   *self,

gtk/gtkcssimagerecolor.c
@@ -245,10 +245,16 @@ gtk_css_image_recolor_parse_arg (GtkCssParser *parser,
   switch (arg)
   {
     case 0:
-      self->file = gtk_css_parser_consume_url (parser);
-      if (self->file == NULL)
-        return 0;
-      return 1;
+      {
+        char *url = gtk_css_parser_consume_url (parser);
+        if (url == NULL)
+          return 0;
+        self->file = gtk_css_parser_resolve_url (parser, url);
+        g_free (url);
+        if (self->file == NULL)
+          return 0;
+        return 1;
+      }
 
     case 1:
       self->palette = gtk_css_palette_value_parse (parser);

gtk/gtkcssprovider.c
@@ -695,7 +695,14 @@ parse_import (GtkCssScanner *scanner)
     }
   else
     {
-      file = gtk_css_parser_consume_url (scanner->parser);
+      char *url = gtk_css_parser_consume_url (scanner->parser);
+      if (url)
+        {
+          file = gtk_css_parser_resolve_url (scanner->parser, url);
+          g_free (url);
+        }
+      else
+        file = NULL;
     }
 
   if (file == NULL)

commit ac8c4245b2fb2f1fe8e660c4ad2541efb39cf3d6

css: Don't throw warnings on broken URL in image css
If a URL can't be loaded, we might end up with a NULL file. Handle that
case properly by creating an invalid image instead and don't crash or
complain to stderr when files are NULL.

This was broken since 0886ade182f1a958024202d5c20248d57fcdb053

A new reftest has been included. We need a reftest instead of a
CSS parser test, because the error only becomes visible when
compute()ing the actual image.

Fixes #4373

gtk/gtkcssimageurl.c
@@ -41,6 +41,12 @@ gtk_css_image_url_load_image (GtkCssImageUrl  *url,
   if (url->loaded_image)
     return url->loaded_image;
 
+  if (url->file == NULL)
+    {
+      url->loaded_image = gtk_css_image_invalid_new ();
+      return url->loaded_image;
+    }
+
   /* We special case resources here so we can use gdk_texture_new_from_resource. */
   if (g_file_has_uri_scheme (url->file, "resource"))
     {
@@ -147,6 +153,10 @@ gtk_css_image_url_equal (GtkCssImage *image1,
 {
   GtkCssImageUrl *url1 = GTK_CSS_IMAGE_URL (image1);
   GtkCssImageUrl *url2 = GTK_CSS_IMAGE_URL (image2);
+  
+  /* FIXME: We don't save data: urls, so we can't compare them here */
+  if (url1->file == NULL || url2->file == NULL)
+    return FALSE;
 
   return g_file_equal (url1->file, url2->file);
 }

testsuite/reftests/css-image-broken-url.css
@@ -1,0 +1,4 @@
+#test {
+  /* The url should fail as early as possible */
+  background-image: url('data::::');
+}

testsuite/reftests/css-image-broken-url.ref.ui
@@ -1,0 +1,8 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<interface>
+  <object class="GtkWindow" id="window1">
+    <property name="width_request">10</property>
+    <property name="height_request">10</property>
+    <property name="decorated">0</property>
+  </object>
+</interface>

testsuite/reftests/css-image-broken-url.ui
@@ -1,0 +1,9 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<interface>
+  <object class="GtkWindow" id="window1">
+    <property name="width_request">10</property>
+    <property name="height_request">10</property>
+    <property name="decorated">0</property>
+    <property name="name">test</property>
+  </object>
+</interface>

testsuite/reftests/meson.build
@@ -221,6 +221,9 @@ testdata = [
   'css-image-aspect-ratio.css',
   'css-image-aspect-ratio.ui',
   'css-image-aspect-ratio.ref.ui',
+  'css-image-broken-url.css',
+  'css-image-broken-url.ref.ui',
+  'css-image-broken-url.ui',
   'css-image-color-aspect-ratio.css',
   'css-image-color-aspect-ratio.ui',
   'css-image-color-aspect-ratio.ref.ui',