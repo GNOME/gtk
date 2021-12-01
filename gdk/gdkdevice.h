fd9e0dd13a117637db0d265be85952a71ef128b8
Matthias Clasen <mclasen@redhat.com>
2021-12-01 14:04:35 -0500

    Deprecate GdkDevice:source
    GdkDeviceTool is more consistently supported across
    backends, and provides the same information.

--- gdk/gdkdevice.h
+++ gdk/gdkdevice.h
@@ -92,7 +92,7 @@ GDK_AVAILABLE_IN_ALL
 GdkDisplay *        gdk_device_get_display              (GdkDevice *device);
 GDK_AVAILABLE_IN_ALL
 GdkSeat *           gdk_device_get_seat                 (GdkDevice *device);
-GDK_AVAILABLE_IN_ALL
+GDK_DEPRECATED_IN_4_6_FOR(gdk_device_tool_get_tool_type)
 GdkDeviceTool *     gdk_device_get_device_tool          (GdkDevice *device);
 
 GDK_AVAILABLE_IN_ALL
