bc7bed751713872cd12408d7896b44a5485a9633
Matthias Clasen <mclasen@redhat.com>
2021-12-10 12:14:04 -0500

    ci: Use meson 0.59 for msvc

--- .gitlab-ci/test-msvc.bat
+++ .gitlab-ci/test-msvc.bat
@@ -5,7 +5,7 @@ call "C:\Program Files (x86)\Microsoft Visual Studio\2017\BuildTools\VC\Auxiliar
 @echo on
 
 :: FIXME: make warnings fatal
-pip3 install --upgrade --user meson==0.56.2  || goto :error
+pip3 install --upgrade --user meson==0.59 || goto :error
 meson -Dmedia-gstreamer=disabled _build || goto :error
 ninja -C _build || goto :error
 
