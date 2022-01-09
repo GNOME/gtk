4e9ddaf086fe315169a655db6e70a80bcd23e57b
Philip Zander <philip.zander@gmail.com>
2022-01-09 11:33:40 +0100

    gdkkeys-win32: Fix handling of SGCAPS
    Contrary to what you can read on the internet, SGCAPS keys don't work
    by having capslock toggle the KBDCTRL bit, they actually have two
    consecutive table entries, the first of which is for the normal
    version and the second of which is for the capslocked version.
    
    Background: SGCAPS is short for Swiss German caps because Swiss German
    was the first layout to use this feature. For keys with the SGCAPS flag,
    capslock has a different effect than pressing shift. For example:
    Shift + ü = è,  CapsLock + ü = Ü,  CapsLock + Shift + ü = È

--- gdk/win32/gdkprivate-win32.h
+++ gdk/win32/gdkprivate-win32.h
@@ -446,7 +446,6 @@ gint      _gdk_win32_display_text_property_to_utf8_list (GdkDisplay    *display,
 							 gchar       ***list);
 gchar     *_gdk_win32_display_utf8_to_string_target (GdkDisplay *display, const gchar *str);
 
-gboolean   _gdk_win32_keymap_has_altgr           (GdkWin32Keymap *keymap);
 guint8     _gdk_win32_keymap_get_active_group    (GdkWin32Keymap *keymap);
 guint8     _gdk_win32_keymap_get_rshift_scancode (GdkWin32Keymap *keymap);
 void       _gdk_win32_keymap_set_active_layout   (GdkWin32Keymap *keymap,
