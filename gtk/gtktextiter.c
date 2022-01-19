ffa93b87f826e7a54c46c0563767d146ddd595ce
Bilal Elmoussaoui <bil.elmoussaoui@gmail.com>
2022-01-19 10:18:40 +0000

    g-i: mark gtk_text_iter_get_child_anchor as nullable

--- gtk/gtktextiter.c
+++ gtk/gtktextiter.c
@@ -1056,7 +1056,7 @@ gtk_text_iter_get_paintable (const GtkTextIter *iter)
  *
  * Otherwise, %NULL is returned.
  *
- * Returns: (transfer none): the anchor at @iter
+ * Returns: (transfer none) (nullable): the anchor at @iter
  **/
 GtkTextChildAnchor*
 gtk_text_iter_get_child_anchor (const GtkTextIter *iter)
