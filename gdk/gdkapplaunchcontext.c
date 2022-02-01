c5c76ad327c30e3680737086988d5b91961da1f8
Phaedrus Leeds <mwleeds@protonmail.com>
2022-02-01 11:14:31 -0800

    gdkapplaunchcontext: Improve set_screen() docs

--- gdk/gdkapplaunchcontext.c
+++ gdk/gdkapplaunchcontext.c
@@ -205,6 +205,9 @@ gdk_app_launch_context_set_display (GdkAppLaunchContext *context,
  * Sets the screen on which applications will be launched when
  * using this context. See also gdk_app_launch_context_set_display().
  *
+ * Note that, typically, a #GdkScreen represents a logical screen,
+ * not a physical monitor.
+ *
  * If both @screen and @display are set, the @screen takes priority.
  * If neither @screen or @display are set, the default screen and
  * display are used.
