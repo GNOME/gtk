473aceea68b8b947e33a735aece5054a2f43a3a8
Christian Hergert <christian@hergert.me>
2022-02-04 09:45:55 -0800

    macos: add API to get monitor colorspace

--- gdk/macos/gdkmacosmonitor-private.h
+++ gdk/macos/gdkmacosmonitor-private.h
@@ -29,10 +29,11 @@
 
 G_BEGIN_DECLS
 
-GdkMacosMonitor   *_gdk_macos_monitor_new           (GdkMacosDisplay   *display,
-                                                     CGDirectDisplayID  screen_id);
-CGDirectDisplayID  _gdk_macos_monitor_get_screen_id (GdkMacosMonitor   *self);
-gboolean           _gdk_macos_monitor_reconfigure   (GdkMacosMonitor   *self);
+GdkMacosMonitor   *_gdk_macos_monitor_new             (GdkMacosDisplay   *display,
+                                                       CGDirectDisplayID  screen_id);
+CGDirectDisplayID  _gdk_macos_monitor_get_screen_id   (GdkMacosMonitor   *self);
+gboolean           _gdk_macos_monitor_reconfigure     (GdkMacosMonitor   *self);
+CGColorSpaceRef    _gdk_macos_monitor_copy_colorspace (GdkMacosMonitor   *self);
 
 G_END_DECLS
 
