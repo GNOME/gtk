GNOME/gtk

commit b7ee2cbc289b4ab8a950cd77bdcd69a6f13932a7

Start using GtkWidget's new style class API

gtk/gtkoverlaylayout.c
@@ -319,22 +319,21 @@ gtk_overlay_child_update_style_classes (GtkOverlay *overlay,
                                         GtkWidget *child,
                                         GtkAllocation *child_allocation)
 {
+  GtkWidget *widget = GTK_WIDGET (overlay);
   int width, height;
   GtkAlign valign, halign;
   gboolean is_left, is_right, is_top, is_bottom;
   gboolean has_left, has_right, has_top, has_bottom;
-  GtkStyleContext *context;
 
-  context = gtk_widget_get_style_context (child);
-  has_left = gtk_style_context_has_class (context, GTK_STYLE_CLASS_LEFT);
-  has_right = gtk_style_context_has_class (context, GTK_STYLE_CLASS_RIGHT);
-  has_top = gtk_style_context_has_class (context, GTK_STYLE_CLASS_TOP);
-  has_bottom = gtk_style_context_has_class (context, GTK_STYLE_CLASS_BOTTOM);
+  has_left = gtk_widget_has_style_class (widget, GTK_STYLE_CLASS_LEFT);
+  has_right = gtk_widget_has_style_class (widget, GTK_STYLE_CLASS_RIGHT);
+  has_top = gtk_widget_has_style_class (widget, GTK_STYLE_CLASS_TOP);
+  has_bottom = gtk_widget_has_style_class (widget, GTK_STYLE_CLASS_BOTTOM);
 
   is_left = is_right = is_top = is_bottom = FALSE;
 
-  width = gtk_widget_get_width (GTK_WIDGET (overlay));
-  height = gtk_widget_get_height (GTK_WIDGET (overlay));
+  width = gtk_widget_get_width (widget);
+  height = gtk_widget_get_height (widget);
 
   halign = effective_align (gtk_widget_get_halign (child),
                             gtk_widget_get_direction (child));
@@ -352,24 +351,24 @@ gtk_overlay_child_update_style_classes (GtkOverlay *overlay,
     is_bottom = (child_allocation->y + child_allocation->height == height);
 
   if (has_left && !is_left)
-    gtk_style_context_remove_class (context, GTK_STYLE_CLASS_LEFT);
+    gtk_widget_remove_style_class (widget, GTK_STYLE_CLASS_LEFT);
   else if (!has_left && is_left)
-    gtk_style_context_add_class (context, GTK_STYLE_CLASS_LEFT);
+    gtk_widget_add_style_class (widget, GTK_STYLE_CLASS_LEFT);
 
   if (has_right && !is_right)
-    gtk_style_context_remove_class (context, GTK_STYLE_CLASS_RIGHT);
+    gtk_widget_remove_style_class (widget, GTK_STYLE_CLASS_RIGHT);
   else if (!has_right && is_right)
-    gtk_style_context_add_class (context, GTK_STYLE_CLASS_RIGHT);
+    gtk_widget_add_style_class (widget, GTK_STYLE_CLASS_RIGHT);
 
   if (has_top && !is_top)
-    gtk_style_context_remove_class (context, GTK_STYLE_CLASS_TOP);
+    gtk_widget_remove_style_class (widget, GTK_STYLE_CLASS_TOP);
   else if (!has_top && is_top)
-    gtk_style_context_add_class (context, GTK_STYLE_CLASS_TOP);
+    gtk_widget_add_style_class (widget, GTK_STYLE_CLASS_TOP);
 
   if (has_bottom && !is_bottom)
-    gtk_style_context_remove_class (context, GTK_STYLE_CLASS_BOTTOM);
+    gtk_widget_remove_style_class (widget, GTK_STYLE_CLASS_BOTTOM);
   else if (!has_bottom && is_bottom)
-    gtk_style_context_add_class (context, GTK_STYLE_CLASS_BOTTOM);
+    gtk_widget_add_style_class (widget, GTK_STYLE_CLASS_BOTTOM);
 }
 
 static void

gtk/gtkappchooserdialog.c
@@ -452,8 +452,8 @@ setup_search (GtkAppChooserDialog *self)
       gtk_widget_set_valign (button, GTK_ALIGN_CENTER);
       image = gtk_image_new_from_icon_name ("edit-find-symbolic");
       gtk_container_add (GTK_CONTAINER (button), image);
-      gtk_style_context_add_class (gtk_widget_get_style_context (button), "image-button");
-      gtk_style_context_remove_class (gtk_widget_get_style_context (button), "text-button");
+      gtk_widget_add_style_class (button, "image-button");
+      gtk_widget_remove_style_class (button, "text-button");
 
       header = gtk_dialog_get_header_bar (GTK_DIALOG (self));
       gtk_header_bar_pack_end (GTK_HEADER_BAR (header), button);

gtk/gtkbutton.c
@@ -654,9 +654,8 @@ gtk_button_new_with_mnemonic (const gchar *label)
  */
 void
 gtk_button_set_relief (GtkButton      *button,
-		       GtkReliefStyle  relief)
+                       GtkReliefStyle  relief)
 {
-  GtkStyleContext *context;
   GtkReliefStyle old_relief;
 
   g_return_if_fail (GTK_IS_BUTTON (button));
@@ -664,11 +663,10 @@ gtk_button_set_relief (GtkButton      *button,
   old_relief = gtk_button_get_relief (button);
   if (old_relief != relief)
     {
-      context = gtk_widget_get_style_context (GTK_WIDGET (button));
       if (relief == GTK_RELIEF_NONE)
-        gtk_style_context_add_class (context, GTK_STYLE_CLASS_FLAT);
+        gtk_widget_add_style_class (GTK_WIDGET (button), GTK_STYLE_CLASS_FLAT);
       else
-        gtk_style_context_remove_class (context, GTK_STYLE_CLASS_FLAT);
+        gtk_widget_remove_style_class (GTK_WIDGET (button), GTK_STYLE_CLASS_FLAT);
 
       g_object_notify_by_pspec (G_OBJECT (button), props[PROP_RELIEF]);
     }
@@ -685,12 +683,9 @@ gtk_button_set_relief (GtkButton      *button,
 GtkReliefStyle
 gtk_button_get_relief (GtkButton *button)
 {
-  GtkStyleContext *context;
-
   g_return_val_if_fail (GTK_IS_BUTTON (button), GTK_RELIEF_NORMAL);
 
-  context = gtk_widget_get_style_context (GTK_WIDGET (button));
-  if (gtk_style_context_has_class (context, GTK_STYLE_CLASS_FLAT))
+  if (gtk_widget_has_style_class (GTK_WIDGET (button), GTK_STYLE_CLASS_FLAT))
     return GTK_RELIEF_NONE;
   else
     return GTK_RELIEF_NORMAL;
@@ -787,11 +782,8 @@ gtk_button_set_label (GtkButton   *button,
 {
   GtkButtonPrivate *priv = gtk_button_get_instance_private (button);
   GtkWidget *child;
-  GtkStyleContext *context;
 
   g_return_if_fail (GTK_IS_BUTTON (button));
-
-  context = gtk_widget_get_style_context (GTK_WIDGET (button));
 
   child = gtk_bin_get_child (GTK_BIN (button));
 
@@ -811,8 +803,8 @@ gtk_button_set_label (GtkButton   *button,
           gtk_label_set_xalign (GTK_LABEL (child), 0.0);
         }
       gtk_container_add (GTK_CONTAINER (button), child);
-      gtk_style_context_remove_class (context, "image-button");
-      gtk_style_context_add_class (context, "text-button");
+      gtk_widget_remove_style_class (GTK_WIDGET (button), "image-button");
+      gtk_widget_add_style_class (GTK_WIDGET (button), "text-button");
     }
 
   gtk_label_set_label (GTK_LABEL (child), label);
@@ -944,13 +936,11 @@ gtk_button_set_icon_name (GtkButton  *button,
 {
   GtkButtonPrivate *priv = gtk_button_get_instance_private (button);
   GtkWidget *child;
-  GtkStyleContext *context;
 
   g_return_if_fail (GTK_IS_BUTTON (button));
   g_return_if_fail (icon_name != NULL);
 
   child = gtk_bin_get_child (GTK_BIN (button));
-  context = gtk_widget_get_style_context (GTK_WIDGET (button));
 
   if (priv->child_type != ICON_CHILD || child == NULL)
     {
@@ -959,8 +949,8 @@ gtk_button_set_icon_name (GtkButton  *button,
 
       child = gtk_image_new_from_icon_name (icon_name);
       gtk_container_add (GTK_CONTAINER (button), child);
-      gtk_style_context_remove_class (context, "text-button");
-      gtk_style_context_add_class (context, "image-button");
+      gtk_widget_remove_style_class (GTK_WIDGET (button), "text-button");
+      gtk_widget_add_style_class (GTK_WIDGET (button), "image-button");
     }
   else
     {

gtk/gtkcalendar.c
@@ -743,16 +743,15 @@ gtk_calendar_init (GtkCalendar *calendar)
 
   gtk_widget_set_can_focus (widget, TRUE);
 
-  gtk_style_context_add_class (gtk_widget_get_style_context (GTK_WIDGET (calendar)),
-                               GTK_STYLE_CLASS_VIEW);
+  gtk_widget_add_style_class (GTK_WIDGET (calendar), GTK_STYLE_CLASS_VIEW);
 
   priv->header_box = g_object_new (GTK_TYPE_BOX,
                                    "css-name", "header",
                                    NULL);
   priv->year_label = gtk_label_new ("");
-  gtk_style_context_add_class (gtk_widget_get_style_context (priv->year_label), "year");
+  gtk_widget_add_style_class (priv->year_label, "year");
   priv->month_name_stack = gtk_stack_new ();
-  gtk_style_context_add_class (gtk_widget_get_style_context (priv->month_name_stack), "month");
+  gtk_widget_add_style_class (priv->month_name_stack, "month");
   priv->arrow_widgets[0] = gtk_button_new_from_icon_name ("pan-start-symbolic");
   g_signal_connect_swapped (priv->arrow_widgets[0], "clicked", G_CALLBACK (calendar_set_month_prev), calendar);
   priv->arrow_widgets[1] = gtk_button_new_from_icon_name ("pan-end-symbolic");

gtk/gtkcheckbutton.c
@@ -393,10 +393,9 @@ gtk_check_button_init (GtkCheckButton *check_button)
 {
   GtkCheckButtonPrivate *priv = gtk_check_button_get_instance_private (check_button);
 
-
   gtk_widget_set_receives_default (GTK_WIDGET (check_button), FALSE);
 
-  gtk_style_context_remove_class (gtk_widget_get_style_context (GTK_WIDGET (check_button)), "toggle");
+  gtk_widget_remove_style_class (GTK_WIDGET (check_button), "toggle");
 
   priv->draw_indicator = TRUE;
   draw_indicator_changed (check_button);

gtk/gtkcolorbutton.c
@@ -299,7 +299,6 @@ gtk_color_button_init (GtkColorButton *button)
   GtkColorButtonPrivate *priv = gtk_color_button_get_instance_private (button);
   PangoLayout *layout;
   PangoRectangle rect;
-  GtkStyleContext *context;
   GdkContentFormats *targets;
   GdkContentProvider *content;
   GtkDragSource *source;
@@ -340,8 +339,7 @@ gtk_color_button_init (GtkColorButton *button)
   g_signal_connect (source, "drag-begin", G_CALLBACK (gtk_color_button_drag_begin), button);
   gtk_widget_add_controller (priv->button, GTK_EVENT_CONTROLLER (source));
 
-  context = gtk_widget_get_style_context (GTK_WIDGET (priv->button));
-  gtk_style_context_add_class (context, "color");
+  gtk_widget_add_style_class (priv->button, "color");
 }
 
 static void

gtk/gtkcolorchooserwidget.c
@@ -378,19 +378,19 @@ add_palette (GtkColorChooserWidget  *cc,
 
       if (orientation == GTK_ORIENTATION_HORIZONTAL)
         {
-            if (pos == left)
-              gtk_style_context_add_class (gtk_widget_get_style_context (p), GTK_STYLE_CLASS_LEFT);
-            else if (pos == right)
-              gtk_style_context_add_class (gtk_widget_get_style_context (p), GTK_STYLE_CLASS_RIGHT);
+          if (pos == left)
+            gtk_widget_add_style_class (p, GTK_STYLE_CLASS_LEFT);
+          else if (pos == right)
+            gtk_widget_add_style_class (p, GTK_STYLE_CLASS_RIGHT);
 
-            gtk_grid_attach (GTK_GRID (grid), p, pos, line, 1, 1);
+          gtk_grid_attach (GTK_GRID (grid), p, pos, line, 1, 1);
         }
       else
         {
           if (pos == 0)
-            gtk_style_context_add_class (gtk_widget_get_style_context (p), GTK_STYLE_CLASS_TOP);
+            gtk_widget_add_style_class (p, GTK_STYLE_CLASS_TOP);
           else if (pos == colors_per_line - 1)
-            gtk_style_context_add_class (gtk_widget_get_style_context (p), GTK_STYLE_CLASS_BOTTOM);
+            gtk_widget_add_style_class (p, GTK_STYLE_CLASS_BOTTOM);
 
           gtk_grid_attach (GTK_GRID (grid), p, line, pos, 1, 1);
        }

gtk/gtkcoloreditor.c
@@ -386,11 +386,9 @@ gtk_color_editor_init (GtkColorEditor *editor)
   gtk_widget_init_template (GTK_WIDGET (editor));
 
   if (gtk_widget_get_direction (editor->priv->h_slider) == GTK_TEXT_DIR_RTL)
-    gtk_style_context_add_class (gtk_widget_get_style_context (editor->priv->h_slider),
-                                 "marks-before");
+    gtk_widget_add_style_class (editor->priv->h_slider, "marks-before");
   else
-    gtk_style_context_add_class (gtk_widget_get_style_context (editor->priv->h_slider),
-                                 "marks-after");
+    gtk_widget_add_style_class (editor->priv->h_slider, "marks-after");
 
   /* Create the scaled popup adjustments manually here because connecting user data is not
    * supported by template GtkBuilder xml (it would be possible to set this up in the xml
@@ -419,7 +417,7 @@ gtk_color_editor_init (GtkColorEditor *editor)
   g_signal_connect (controller, "key-pressed", G_CALLBACK (popup_key_pressed), editor);
   gtk_widget_add_controller (editor->priv->a_entry, controller);
 
-  gtk_style_context_remove_class (gtk_widget_get_style_context (editor->priv->swatch), "activatable");
+  gtk_widget_remove_style_class (editor->priv->swatch, "activatable");
 
   editor->priv->picker = gtk_color_picker_new ();
   if (editor->priv->picker == NULL)

commit 351ffef70481c109f151b6fef84060c4e4847b43

overlaylayout: Set position style class on child
As documented:
> Overlay children whose alignments cause them to be positioned
> at an edge get the style classes “.left”, “.right”, “.top”, 
> and/or “.bottom” according to their position.

Likely accidental regression in b7ee2cbc289b4ab8a950cd77bdcd69a6f13932a7

Fixes https://gitlab.gnome.org/GNOME/nautilus/-/issues/2099

gtk/gtkoverlaylayout.c
@@ -346,10 +346,10 @@ gtk_overlay_child_update_style_classes (GtkOverlay *overlay,
   gboolean is_left, is_right, is_top, is_bottom;
   gboolean has_left, has_right, has_top, has_bottom;
 
-  has_left = gtk_widget_has_css_class (widget, "left");
-  has_right = gtk_widget_has_css_class (widget, "right");
-  has_top = gtk_widget_has_css_class (widget, "top");
-  has_bottom = gtk_widget_has_css_class (widget, "bottom");
+  has_left = gtk_widget_has_css_class (child, "left");
+  has_right = gtk_widget_has_css_class (child, "right");
+  has_top = gtk_widget_has_css_class (child, "top");
+  has_bottom = gtk_widget_has_css_class (child, "bottom");
 
   is_left = is_right = is_top = is_bottom = FALSE;
 
@@ -372,24 +372,24 @@ gtk_overlay_child_update_style_classes (GtkOverlay *overlay,
     is_bottom = (child_allocation->y + child_allocation->height == height);
 
   if (has_left && !is_left)
-    gtk_widget_remove_css_class (widget, "left");
+    gtk_widget_remove_css_class (child, "left");
   else if (!has_left && is_left)
-    gtk_widget_add_css_class (widget, "left");
+    gtk_widget_add_css_class (child, "left");
 
   if (has_right && !is_right)
-    gtk_widget_remove_css_class (widget, "right");
+    gtk_widget_remove_css_class (child, "right");
   else if (!has_right && is_right)
-    gtk_widget_add_css_class (widget, "right");
+    gtk_widget_add_css_class (child, "right");
 
   if (has_top && !is_top)
-    gtk_widget_remove_css_class (widget, "top");
+    gtk_widget_remove_css_class (child, "top");
   else if (!has_top && is_top)
-    gtk_widget_add_css_class (widget, "top");
+    gtk_widget_add_css_class (child, "top");
 
   if (has_bottom && !is_bottom)
-    gtk_widget_remove_css_class (widget, "bottom");
+    gtk_widget_remove_css_class (child, "bottom");
   else if (!has_bottom && is_bottom)
-    gtk_widget_add_css_class (widget, "bottom");
+    gtk_widget_add_css_class (child, "bottom");
 }
 
 static void