6d15549f5156c583421275298094a4cbbaa0f2d3
Benjamin Otte <otte@redhat.com>
2022-06-12 02:58:22 +0200

    inscription: Add ::wrap-mode
    We use a different wrap-mode than GtkLabel, because we cannot just
    resize the widget to make long words fit, we have to fit the size we are
    given.

--- gtk/gtkinscription.h
+++ gtk/gtkinscription.h
@@ -71,6 +71,11 @@ GtkInscriptionOverflow  gtk_inscription_get_text_overflow       (GtkInscription 
 GDK_AVAILABLE_IN_4_8
 void                    gtk_inscription_set_text_overflow       (GtkInscription         *self,
                                                                  GtkInscriptionOverflow  overflow);
+GDK_AVAILABLE_IN_4_8
+PangoWrapMode           gtk_inscription_get_wrap_mode           (GtkInscription         *self);
+GDK_AVAILABLE_IN_4_8
+void                    gtk_inscription_set_wrap_mode           (GtkInscription         *self,
+                                                                 PangoWrapMode           wrap_mode);
 
 GDK_AVAILABLE_IN_4_8
 guint                   gtk_inscription_get_min_chars           (GtkInscription         *self);
