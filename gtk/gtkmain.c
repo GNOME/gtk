GNOME/gtk

commit adba0b972e2a86d8f169222d7e64c346dc746325

gtkwindow: Synthesize pointer crossing events on state changes
When widgets go mapped/unmapped, we repick but don't generate crossing
events. Since there could be stateful controllers that use those in
the previously picked widget (e.g. GtkEventControllerMotion), skipping
those breaks their state.

Ensure to send the relevant crossing events on every situation that
changes the pointer focus, so these controllers get a fair opportunity
to undo their state.

Closes: https://gitlab.gnome.org/GNOME/gtk/-/issues/2877

gtk/gtkwindow.c
@@ -4968,6 +4968,9 @@ synthesize_focus_change_events (GtkWindow       *window,
   GtkWidget *prev;
   gboolean seen_ancestor;
 
+  if (old_focus == new_focus)
+    return;
+
   if (old_focus && new_focus)
     ancestor = gtk_widget_common_ancestor (old_focus, new_focus);
   else
@@ -6455,7 +6458,12 @@ gtk_window_update_pointer_focus_on_state_change (GtkWindow *window,
       else if (focus->target == widget ||
                gtk_widget_is_ancestor (focus->target, widget))
         {
+          GtkWidget *old_target;
+
+          old_target = g_object_ref (focus->target);
           gtk_pointer_focus_repick_target (focus);
+          synthesize_focus_change_events (window, old_target, focus->target, GTK_CROSSING_POINTER);
+          g_object_unref (old_target);
         }
 
       gtk_pointer_focus_unref (focus);

commit 54465adff23c5155ed52580beabf50b2a03dff22

gtkwindow: Use pointer-oriented function to deal with crossing events
Commit adba0b97 fixed missed pointer crossings by using a helper function that
was already present and looked like did everything that was needed. However
this function was oriented to keyboard focus and it also did update the related
widget state. Doing these changes on pointer-based crossing was misuse, and
could cause weird interactions with keyboard focus management.

Fix this by using gtkmain.c gtk_synthesize_crossing_event() that is in fact
oriented to pointers.

Fixes: adba0b97 (gtkwindow: Synthesize pointer crossing events on state changes)
Closes: https://gitlab.gnome.org/GNOME/gtk/-/issues/5094

gtk/gtkmain.c
@@ -1110,7 +1110,7 @@ translate_coordinates (double     event_x,
   return TRUE;
 }
 
-static void
+void
 gtk_synthesize_crossing_events (GtkRoot         *toplevel,
                                 GtkCrossingType  crossing_type,
                                 GtkWidget       *old_target,

gtk/gtkprivate.h
@@ -152,6 +152,15 @@ char * _gtk_elide_underscores (const char *original);
 
 void setlocale_initialization (void);
 
+void gtk_synthesize_crossing_events (GtkRoot         *toplevel,
+                                     GtkCrossingType  crossing_type,
+                                     GtkWidget       *old_target,
+                                     GtkWidget       *new_target,
+                                     double           surface_x,
+                                     double           surface_y,
+                                     GdkCrossingMode  mode,
+                                     GdkDrop         *drop);
+
 G_END_DECLS
 
 #endif /* __GTK_PRIVATE_H__ */

gtk/gtkwindow.c
@@ -6462,7 +6462,12 @@ gtk_window_update_pointer_focus_on_state_change (GtkWindow *window,
 
           old_target = g_object_ref (focus->target);
           gtk_pointer_focus_repick_target (focus);
-          synthesize_focus_change_events (window, old_target, focus->target, GTK_CROSSING_POINTER);
+          gtk_synthesize_crossing_events (GTK_ROOT (window),
+                                          GTK_CROSSING_POINTER,
+                                          old_target, focus->target,
+                                          focus->x, focus->y,
+                                          GDK_CROSSING_NORMAL,
+                                          NULL);
           g_object_unref (old_target);
         }
 