083d023b6bab9820fbda1a5b3e3ef4fabcbf5dc8
Carlos Garnacho <carlosg@gnome.org>
2022-08-09 18:49:35 +0200

    gtkwindow: Use pointer-oriented function to deal with crossing events
    Commit adba0b97 fixed missed pointer crossings by using a helper function that
    was already present and looked like did everything that was needed. However
    this function was oriented to keyboard focus and it also did update the related
    widget state. Doing these changes on pointer-based crossing was misuse, and
    could cause weird interactions with keyboard focus management.
    
    Fix this by using gtkmain.c gtk_synthesize_crossing_event() that is in fact
    oriented to pointers.
    
    Fixes: adba0b97 (gtkwindow: Synthesize pointer crossing events on state changes)
    Closes: https://gitlab.gnome.org/GNOME/gtk/-/issues/5094

--- gtk/gtkmain.c
+++ gtk/gtkmain.c
@@ -1106,7 +1106,7 @@ translate_coordinates (double     event_x,
   return TRUE;
 }
 
-static void
+void
 gtk_synthesize_crossing_events (GtkRoot         *toplevel,
                                 GtkCrossingType  crossing_type,
                                 GtkWidget       *old_target,
