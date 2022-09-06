GNOME/gtk

commit 52ac71b9727b9a63773865309a970c4d97c0a75d

gtktextview: Shuffle the places doing IM reset
During text widget manipulation (inserting or deleting text via keyboard)
the IM context is reset somewhat early, before the actual change took place.
This makes IM lag behind in terms of surrounding text and cursor position.

Shuffle these IM reset calls so that they happen after the changes, and
ensure that the IM is actually reset, since that is currently toggled on
a pretty narrow set of circumstances.

Also, fix a bug during GtkEventControllerKey::im-update where the condition
on cursor position editability to reset the IM context was inverted.

gtk/gtktextview.c
@@ -5533,7 +5533,7 @@ gtk_text_view_key_controller_im_update (GtkEventControllerKey *controller,
   can_insert = gtk_text_iter_can_insert (&iter, priv->editable);
 
   priv->need_im_reset = TRUE;
-  if (!can_insert)
+  if (can_insert)
     gtk_text_view_reset_im_context (text_view);
 }
 
@@ -6366,8 +6366,6 @@ gtk_text_view_move_cursor (GtkTextView     *text_view,
       return;
     }
 
-  gtk_text_view_reset_im_context (text_view);
-
   if (step == GTK_MOVEMENT_PAGES)
     {
       if (!gtk_text_view_scroll_pages (text_view, count, extend_selection))
@@ -6551,6 +6549,9 @@ gtk_text_view_move_cursor (GtkTextView     *text_view,
 
   gtk_text_view_check_cursor_blink (text_view);
   gtk_text_view_pend_cursor_blink (text_view);
+
+  priv->need_im_reset = TRUE;
+  gtk_text_view_reset_im_context (text_view);
 }
 
 static void
@@ -6841,14 +6842,16 @@ gtk_text_view_delete_from_cursor (GtkTextView   *text_view,
 
   priv = text_view->priv;
 
-  gtk_text_view_reset_im_context (text_view);
-
   if (type == GTK_DELETE_CHARS)
     {
       /* Char delete deletes the selection, if one exists */
       if (gtk_text_buffer_delete_selection (get_buffer (text_view), TRUE,
                                             priv->editable))
-        return;
+        {
+          priv->need_im_reset = TRUE;
+          gtk_text_view_reset_im_context (text_view);
+          return;
+        }
     }
 
   gtk_text_buffer_get_iter_at_mark (get_buffer (text_view), &insert,
@@ -6975,6 +6978,9 @@ gtk_text_view_delete_from_cursor (GtkTextView   *text_view,
     {
       gtk_widget_error_bell (GTK_WIDGET (text_view));
     }
+
+  priv->need_im_reset = TRUE;
+  gtk_text_view_reset_im_context (text_view);
 }
 
 static void
@@ -6985,12 +6991,14 @@ gtk_text_view_backspace (GtkTextView *text_view)
 
   priv = text_view->priv;
 
-  gtk_text_view_reset_im_context (text_view);
-
   /* Backspace deletes the selection, if one exists */
   if (gtk_text_buffer_delete_selection (get_buffer (text_view), TRUE,
                                         priv->editable))
-    return;
+    {
+      priv->need_im_reset = TRUE;
+      gtk_text_view_reset_im_context (text_view);
+      return;
+    }
 
   gtk_text_buffer_get_iter_at_mark (get_buffer (text_view),
                                     &insert,
@@ -7003,6 +7011,9 @@ gtk_text_view_backspace (GtkTextView *text_view)
       DV(g_print (G_STRLOC": scrolling onscreen\n"));
       gtk_text_view_scroll_mark_onscreen (text_view,
                                           gtk_text_buffer_get_insert (get_buffer (text_view)));
+
+      priv->need_im_reset = TRUE;
+      gtk_text_view_reset_im_context (text_view);
     }
   else
     {

commit 41f26975a99f3f66db41c5c8c08f6d220a3534d2

gtktext: Avoid early IM reset on updates
Resetting the IM on IM updates is too eager and indeed the simple
IM context doesn't like that this happens in the middle of dead
key handling.

We however want to reset the IM after actual text buffer changes
(say, a committed string) moved the cursor position, altered the
surrounding text, etc. So that the IM implementation does know to
update its state.

Since there is going to be an actual IM reset anyways, it does
no longer make sense to try to preserve the old priv->need_im_reset
status during commit handling.

Fixes: 52ac71b972 ("gtktextview: Shuffle the places doing IM reset")
Closes: https://gitlab.gnome.org/GNOME/gtk/-/issues/5133

gtk/gtktext.c
@@ -4315,10 +4315,8 @@ gtk_text_enter_text (GtkText    *self,
 {
   GtkTextPrivate *priv = gtk_text_get_instance_private (self);
   int tmp_pos;
-  gboolean old_need_im_reset;
   guint text_length;
 
-  old_need_im_reset = priv->need_im_reset;
   priv->need_im_reset = FALSE;
 
   if (priv->selection_bound != priv->current_pos)
@@ -4336,8 +4334,6 @@ gtk_text_enter_text (GtkText    *self,
   tmp_pos = priv->current_pos;
   gtk_editable_insert_text (GTK_EDITABLE (self), str, strlen (str), &tmp_pos);
   gtk_text_set_selection_bounds (self, tmp_pos, tmp_pos);
-
-  priv->need_im_reset = old_need_im_reset;
 }
 
 /* All changes to priv->current_pos and priv->selection_bound

gtk/gtktextview.c
@@ -5524,17 +5524,8 @@ gtk_text_view_key_controller_im_update (GtkEventControllerKey *controller,
                                         GtkTextView           *text_view)
 {
   GtkTextViewPrivate *priv = text_view->priv;
-  GtkTextMark *insert;
-  GtkTextIter iter;
-  gboolean can_insert;
-
-  insert = gtk_text_buffer_get_insert (get_buffer (text_view));
-  gtk_text_buffer_get_iter_at_mark (get_buffer (text_view), &iter, insert);
-  can_insert = gtk_text_iter_can_insert (&iter, priv->editable);
 
   priv->need_im_reset = TRUE;
-  if (can_insert)
-    gtk_text_view_reset_im_context (text_view);
 }
 
 static gboolean
@@ -8494,6 +8485,7 @@ gtk_text_view_commit_handler (GtkIMContext  *context,
   gtk_text_view_commit_text (text_view, str);
   gtk_text_view_reset_blink_time (text_view);
   gtk_text_view_pend_cursor_blink (text_view);
+  gtk_im_context_reset (context);
 }
 
 static void