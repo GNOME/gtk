58980e0b4a351abd6e4df454e0ccae5944af239d
Lukáš Tyrychtr <lukastyrychtr@gmail.com>
2022-09-12 14:40:11 +0200

    Make GtkAccessible public
    Out of tree classes need to be able to implement the GtkAccessible
    interface, in order to implement custom accessible objects.
    
    Fixes: #4240

--- gtk/gtkaccessible.h
+++ gtk/gtkaccessible.h
@@ -35,6 +35,57 @@ G_BEGIN_DECLS
 GDK_AVAILABLE_IN_ALL
 G_DECLARE_INTERFACE (GtkAccessible, gtk_accessible, GTK, ACCESSIBLE, GObject)
 
+typedef enum {
+  GTK_ACCESSIBLE_PLATFORM_STATE_FOCUSABLE,
+  GTK_ACCESSIBLE_PLATFORM_STATE_FOCUSED,
+  GTK_ACCESSIBLE_PLATFORM_STATE_ACTIVE
+} GtkAccessiblePlatformState;
+
+typedef enum {
+  GTK_ACCESSIBLE_PLATFORM_CHANGE_FOCUSABLE = 1 << GTK_ACCESSIBLE_PLATFORM_STATE_FOCUSABLE,
+  GTK_ACCESSIBLE_PLATFORM_CHANGE_FOCUSED   = 1 << GTK_ACCESSIBLE_PLATFORM_STATE_FOCUSED,
+  GTK_ACCESSIBLE_PLATFORM_CHANGE_ACTIVE    = 1 << GTK_ACCESSIBLE_PLATFORM_STATE_ACTIVE,
+} GtkAccessiblePlatformChange;
+
+struct _GtkAccessibleInterface
+{
+  GTypeInterface g_iface;
+
+  GtkATContext *        (* get_at_context)      (GtkAccessible *self);
+
+  gboolean              (* get_platform_state)  (GtkAccessible              *self,
+                                                 GtkAccessiblePlatformState  state);
+
+  GtkAccessible *       (* get_parent)  (GtkAccessible *self);
+  GtkAccessible *       (* get_child_at_index)  (GtkAccessible *self, guint index);
+
+  gboolean              (* get_bounds) (GtkAccessible *self, int *x, int *y,
+                                        int *width, int *height);
+};
+
+GDK_AVAILABLE_IN_ALL
+GtkATContext *  gtk_accessible_get_at_context   (GtkAccessible *self);
+
+GDK_AVAILABLE_IN_ALL
+void            gtk_accessible_platform_changed (GtkAccessible                *self,
+                                                 GtkAccessiblePlatformChange   change);
+
+GDK_AVAILABLE_IN_ALL
+gboolean        gtk_accessible_get_platform_state (GtkAccessible              *self,
+                                                   GtkAccessiblePlatformState  state);
+
+GDK_AVAILABLE_IN_ALL
+GtkAccessible * gtk_accessible_get_parent(GtkAccessible *self);
+
+GDK_AVAILABLE_IN_ALL
+GtkAccessible * gtk_accessible_get_child_at_index(GtkAccessible *self, guint index);
+
+GDK_AVAILABLE_IN_ALL
+gboolean gtk_accessible_get_bounds (GtkAccessible *self, int *x, int *y, int *width, int *height);
+
+GDK_AVAILABLE_IN_ALL
+void            gtk_accessible_bounds_changed   (GtkAccessible *self);
+
 GDK_AVAILABLE_IN_ALL
 GtkAccessibleRole       gtk_accessible_get_accessible_role      (GtkAccessible         *self);
 
--- gtk/gtkaccessibleprivate.h
+++ gtk/gtkaccessibleprivate.h
@@ -25,43 +25,12 @@
 
 G_BEGIN_DECLS
 
-struct _GtkAccessibleInterface
-{
-  GTypeInterface g_iface;
-
-  GtkATContext *        (* get_at_context)      (GtkAccessible *self);
-
-  gboolean              (* get_platform_state)  (GtkAccessible              *self,
-                                                 GtkAccessiblePlatformState  state);
-
-  GtkAccessible *       (* get_parent)  (GtkAccessible *self);
-  GtkAccessible *       (* get_child_at_index)  (GtkAccessible *self, guint index);
-
-  gboolean              (* get_bounds) (GtkAccessible *self, int *x, int *y,
-                                        int *width, int *height);
-};
-
-GtkATContext *  gtk_accessible_get_at_context   (GtkAccessible *self);
-
 const char *    gtk_accessible_role_to_name     (GtkAccessibleRole  role,
                                                  const char        *domain);
 
 gboolean gtk_accessible_role_is_range_subclass (GtkAccessibleRole role);
 
 gboolean        gtk_accessible_should_present   (GtkAccessible     *self);
-
-void            gtk_accessible_platform_changed (GtkAccessible                *self,
-                                                 GtkAccessiblePlatformChange   change);
-gboolean        gtk_accessible_get_platform_state (GtkAccessible              *self,
-                                                   GtkAccessiblePlatformState  state);
-
-GtkAccessible * gtk_accessible_get_parent(GtkAccessible *self);
-
-GtkAccessible * gtk_accessible_get_child_at_index(GtkAccessible *self, guint index);
-
-gboolean gtk_accessible_get_bounds (GtkAccessible *self, int *x, int *y, int *width, int *height);
-
-void            gtk_accessible_bounds_changed   (GtkAccessible *self);
 
 void            gtk_accessible_update_children  (GtkAccessible           *self,
                                                  GtkAccessible           *child,
