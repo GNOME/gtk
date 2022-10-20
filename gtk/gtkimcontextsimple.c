cf701d5db44ed7c1a532b1106816da7ab1078fa1
Jan Tojnar <jtojnar@gmail.com>
2022-10-20 16:19:13 +0200

    imcontextsimple: Document that Compose file support is incomplete
    And that `include "%L"` does something different as introduced in
    https://gitlab.gnome.org/GNOME/gtk/-/commit/3b4b1c68781aad61fc6b91c9dff75b5790b4d8e4
    
    Note that user can still use `include "/.%L"` as a workaround.

--- gtk/gtkimcontextsimple.c
+++ gtk/gtkimcontextsimple.c
@@ -46,8 +46,9 @@
  * `GtkIMContextSimple` reads compose sequences from the first of the
  * following files that is found: ~/.config/gtk-4.0/Compose, ~/.XCompose,
  * /usr/share/X11/locale/$locale/Compose (for locales that have a nontrivial
- * Compose file). The syntax of these files is described in the Compose(5)
- * manual page.
+ * Compose file). A subset of the file syntax described in the Compose(5)
+ * manual page is supported. Additionally, `include "%L"` loads GTK’s built-in
+ * table of compose sequences rather than the locale-specific one from X11.
  *
  * If none of these files is found, `GtkIMContextSimple` uses a built-in table
  * of compose sequences that is derived from the X11 Compose files.
