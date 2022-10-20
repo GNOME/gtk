b738d4a6ac88b1068c8a41d969981f8b5f00455e
Matthias Clasen <mclasen@redhat.com>
2022-10-20 09:40:39 -0400

    columnviewcolumn: Cosmetics

--- gtk/gtkcolumnviewcolumn.h
+++ gtk/gtkcolumnviewcolumn.h
@@ -95,7 +95,7 @@ GDK_AVAILABLE_IN_ALL
 void                    gtk_column_view_column_set_expand               (GtkColumnViewColumn    *self,
                                                                          gboolean                expand);
 GDK_AVAILABLE_IN_ALL
-gboolean                gtk_column_view_column_get_expand            (GtkColumnViewColumn    *self);
+gboolean                gtk_column_view_column_get_expand               (GtkColumnViewColumn    *self);
 
 G_END_DECLS
 
