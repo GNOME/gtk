GNOME/gtk

commit c06b1cc103b8e7da906db25988f5c94d6d64ad1f

Win32: Re-work cursor handling
Like the X11 and Wayland backends, re-work how the cursors are being
handled.  So, we use a hash table to cache up the HCURSORS that we
create along the way.

We still need to cache up the icon/cursor themes since this is something
that is not part of Windows but was added on to support icon/cursor themes
such as Adwaita on Windows, but should be in-line with what is going on in
GdkCursor.

Also, remove the _gdk_grab_cursor global variable in gdkprivate-win32.h,
and replace it with another variable in the GdkWin32Display structure,
to make things cleaner in the process.

https://bugzilla.gnome.org/show_bug.cgi?id=773299

gdk/win32/gdkprivate-win32.h
@@ -132,7 +132,6 @@ struct _GdkWin32Cursor
 {
   GdkCursor cursor;
 
-  gchar *name;
   HCURSOR hcursor;
 };
 
@@ -354,15 +353,13 @@ extern GHashTable	*_format_atom_table;
 /* Hold the result of a delayed rendering */
 extern HGLOBAL		_delayed_rendering_data;
 
-extern GdkCursor *_gdk_win32_grab_cursor;
-
 HGLOBAL _gdk_win32_selection_convert_to_dib (HGLOBAL  hdata,
 					     GdkAtom  target);
 
 /* Convert a pixbuf to an HICON (or HCURSOR).  Supports alpha under
  * Windows XP, thresholds alpha otherwise.
  */
-HICON _gdk_win32_pixbuf_to_hicon   (GdkPixbuf *pixbuf);
+HICON _gdk_win32_texture_to_hicon  (GdkTexture *texture);
 HICON _gdk_win32_pixbuf_to_hcursor (GdkPixbuf *pixbuf,
 				    gint       x_hotspot,
 				    gint       y_hotspot);
@@ -370,6 +367,8 @@ HICON _gdk_win32_pixbuf_to_hcursor (GdkPixbuf *pixbuf,
 void _gdk_win32_display_init_cursors (GdkWin32Display     *display);
 void _gdk_win32_display_finalize_cursors (GdkWin32Display *display);
 void _gdk_win32_display_update_cursors (GdkWin32Display   *display);
+GdkCursor *_gdk_win32_display_get_cursor_for_name (GdkDisplay   *display, const gchar* cursor_name);
+GdkCursor *gdk_win32_display_cursor_from_hcursor (GdkDisplay *display, HCURSOR     hcursor);
 
 typedef struct _Win32CursorTheme Win32CursorTheme;
 
@@ -393,7 +392,6 @@ struct _Win32Cursor {
   gint height;
   guint load_flags;
   gint xcursor_number;
-  GdkCursorType cursor_type;
 };
 
 Win32CursorTheme *win32_cursor_theme_load             (const gchar      *name,

gdk/win32/gdkdevice-virtual.c
@@ -26,6 +26,7 @@
 #include "gdkdevice-virtual.h"
 #include "gdkdevice-win32.h"
 #include "gdkwin32.h"
+#include "gdkdisplay-win32.h"
 
 G_DEFINE_TYPE (GdkDeviceVirtual, gdk_device_virtual, GDK_TYPE_DEVICE)
 
@@ -89,11 +90,20 @@ gdk_device_virtual_get_state (GdkDevice       *device,
 
 static void
 gdk_device_virtual_set_window_cursor (GdkDevice *device,
-				      GdkWindow *window,
-				      GdkCursor *cursor)
+                                      GdkWindow *window,
+                                      GdkCursor *cursor)
 {
-  if (cursor != NULL && GDK_WIN32_CURSOR (cursor)->hcursor != NULL)
-    SetCursor (GDK_WIN32_CURSOR (cursor)->hcursor);
+  if (cursor != NULL)
+    {
+      GdkDisplay *display = gdk_window_get_display (window);
+      HCURSOR hcursor = NULL;
+
+      if (display != NULL)
+        hcursor = gdk_win32_display_get_hcursor (display, cursor);
+
+      if (hcursor != NULL)
+        SetCursor (hcursor);   
+    }
 }
 
 static void
@@ -136,18 +146,19 @@ gdk_device_virtual_grab (GdkDevice    *device,
 
   if (gdk_device_get_source (device) != GDK_SOURCE_KEYBOARD)
     {
-      if (_gdk_win32_grab_cursor != NULL)
-	{
-	  if (GetCursor () == GDK_WIN32_CURSOR (_gdk_win32_grab_cursor)->hcursor)
-	    SetCursor (NULL);
-	}
+      GdkWin32Display *display = GDK_WIN32_DISPLAY (gdk_device_get_display (device));
+      if (display->grab_cursor != NULL)
+        {
+          if (GetCursor () == g_hash_table_lookup (display->cursors, display->grab_cursor))
+	        SetCursor (NULL);
+        }
 
-      g_set_object (&_gdk_win32_grab_cursor, cursor);
+      g_set_object (&display->grab_cursor, cursor);
 
-      if (_gdk_win32_grab_cursor != NULL)
-	SetCursor (GDK_WIN32_CURSOR (_gdk_win32_grab_cursor)->hcursor);
+      if (display->grab_cursor != NULL)
+        SetCursor (g_hash_table_lookup (display->cursors, display->grab_cursor));
       else
-	SetCursor (LoadCursor (NULL, IDC_ARROW));
+        SetCursor (LoadCursor (NULL, IDC_ARROW));
 
       SetCapture (GDK_WINDOW_HWND (window));
     }
@@ -157,12 +168,14 @@ gdk_device_virtual_grab (GdkDevice    *device,
 
 static void
 gdk_device_virtual_ungrab (GdkDevice *device,
-                         guint32    time_)
+                           guint32    time_)
 {
   GdkDeviceGrabInfo *info;
   GdkDisplay *display;
+  GdkWin32Display *win32_display;
 
   display = gdk_device_get_display (device);
+  win32_display = GDK_WIN32_DISPLAY (display);
   info = _gdk_display_get_last_device_grab (display, device);
 
   if (info)
@@ -170,12 +183,13 @@ gdk_device_virtual_ungrab (GdkDevice *device,
 
   if (gdk_device_get_source (device) != GDK_SOURCE_KEYBOARD)
     {
-      if (_gdk_win32_grab_cursor != NULL)
-	{
-	  if (GetCursor () == GDK_WIN32_CURSOR (_gdk_win32_grab_cursor)->hcursor)
-	    SetCursor (NULL);
-	}
-      g_clear_object (&_gdk_win32_grab_cursor);
+      if (win32_display->grab_cursor != NULL)
+        {
+          if (GetCursor () == g_hash_table_lookup (win32_display->cursors, win32_display->grab_cursor))
+            SetCursor (NULL);
+        }
+
+      g_clear_object (&win32_display->grab_cursor);
 
       ReleaseCapture ();
     }

gdk/win32/gdkdisplay-win32.h
@@ -64,7 +64,6 @@ struct _GdkWin32Display
   Win32CursorTheme *cursor_theme;
   gchar *cursor_theme_name;
   int cursor_theme_size;
-  GHashTable *cursor_cache;
 
   HWND hwnd;
   HWND clipboard_hwnd;
@@ -90,6 +89,10 @@ struct _GdkWin32Display
 
   GdkWin32ShcoreFuncs shcore_funcs;
   GdkWin32User32DPIFuncs user32_dpi_funcs;
+  
+  /* Cursor Items (GdkCursor->HCURSOR) */
+  GHashTable *cursors;
+  GdkCursor *grab_cursor;
 };
 
 struct _GdkWin32DisplayClass

gdk/win32/gdkevents-win32.c
@@ -108,8 +108,6 @@ static gboolean gdk_event_dispatch (GSource     *source,
 
 extern gint       _gdk_input_ignore_core;
 
-GdkCursor *_gdk_win32_grab_cursor;
-
 typedef struct
 {
   GSource source;
@@ -2136,6 +2134,7 @@ gdk_event_translate (MSG  *msg,
   GdkDisplay *display;
   GdkWindow *window = NULL;
   GdkWindowImplWin32 *impl;
+  GdkWin32Display *win32_display;
 
   GdkWindow *new_window;
 
@@ -3063,15 +3062,20 @@ gdk_event_translate (MSG  *msg,
       if (grab_window == NULL && LOWORD (msg->lParam) != HTCLIENT)
 	break;
 
-      if (grab_window != NULL && _gdk_win32_grab_cursor != NULL)
-	cursor = _gdk_win32_grab_cursor;
+      if (grab_window != NULL)
+        {
+          win32_display = GDK_WIN32_DISPLAY (gdk_window_get_display (grab_window));
+
+          if (win32_display->grab_cursor != NULL)
+            cursor = win32_display->grab_cursor;
+        }
       else
-	cursor = NULL;
+        cursor = NULL;
 
       if (cursor != NULL)
         {
-	  GDK_NOTE (EVENTS, g_print (" (SetCursor(%p)", cursor));
-	  SetCursor (GDK_WIN32_CURSOR (cursor)->hcursor);
+          GDK_NOTE (EVENTS, g_print (" (SetCursor(%p)", cursor));
+          SetCursor (g_hash_table_lookup (win32_display->cursors, cursor));
           return_val = TRUE;
           *ret_valp = TRUE;
         }

gdk/win32/gdkwin32display.h
@@ -31,6 +31,8 @@
 
 #include <gdk/gdk.h>
 
+#include <windows.h>
+
 G_BEGIN_DECLS
 
 #ifdef GDK_COMPILATION
@@ -54,6 +56,10 @@ GDK_AVAILABLE_IN_3_18
 void       gdk_win32_display_set_cursor_theme    (GdkDisplay  *display,
                                                   const gchar *name,
                                                   gint         size);
+
+GDK_AVAILABLE_IN_3_94
+HCURSOR    gdk_win32_display_get_hcursor         (GdkDisplay  *display,
+                                                  GdkCursor   *cursor);
 
 G_END_DECLS
 

gdk/win32/gdkwindow-win32.c
@@ -688,7 +688,7 @@ _gdk_win32_display_create_window_impl (GdkDisplay    *display,
                                                        (window->window_type == GDK_WINDOW_TEMP ? "TEMP" : "???")),
                                                        (attributes->wclass == GDK_INPUT_OUTPUT ? "" : "input-only")));
 
-  hparent = GDK_WINDOW_HWND (real_parent);
+  hparent = (real_parent != NULL) ? GDK_WINDOW_HWND (real_parent) : NULL;
 
   impl = g_object_new (GDK_TYPE_WINDOW_IMPL_WIN32, NULL);
   impl->wrapper = GDK_WINDOW (window);
@@ -725,7 +725,7 @@ _gdk_win32_display_create_window_impl (GdkDisplay    *display,
 	  hparent = GetDesktopWindow ();
 	}
       /* Children of foreign windows aren't toplevel windows */
-      if (GDK_WINDOW_TYPE (real_parent) == GDK_WINDOW_FOREIGN)
+      if (real_parent != NULL && GDK_WINDOW_TYPE (real_parent) == GDK_WINDOW_FOREIGN)
 	{
 	  dwStyle = WS_CHILDWINDOW | WS_CLIPCHILDREN;
 	}
@@ -744,7 +744,9 @@ _gdk_win32_display_create_window_impl (GdkDisplay    *display,
 
     case GDK_WINDOW_TEMP:
       /* A temp window is not necessarily a top level window */
-      dwStyle = (real_parent == NULL || gdk_win32_display_get_root_window (display) == real_parent) ? WS_POPUP : WS_CHILDWINDOW);
+      dwStyle = (real_parent == NULL ||
+                 gdk_win32_display_get_root_window (display) == real_parent) ?
+                 WS_POPUP : WS_CHILDWINDOW;
       dwStyle |= WS_CLIPCHILDREN | WS_CLIPSIBLINGS;
       dwExStyle |= WS_EX_TOOLWINDOW | WS_EX_TOPMOST;
       offset_x = _gdk_offset_x;
@@ -2005,8 +2007,6 @@ gdk_win32_window_get_geometry (GdkWindow *window,
 
       API_CALL (GetClientRect, (GDK_WINDOW_HWND (window), &rect));
 
-      if (!window_is_root)
-	{
 	  POINT pt;
 	  GdkWindow *parent = gdk_window_get_parent (window);
 
@@ -2033,7 +2033,6 @@ gdk_win32_window_get_geometry (GdkWindow *window,
 	      rect.right += _gdk_offset_x * impl->window_scale;
 	      rect.bottom += _gdk_offset_y * impl->window_scale;
 	    }
-	}
 
       if (x)
 	*x = rect.left / impl->window_scale;
@@ -2246,7 +2245,6 @@ gdk_win32_window_set_icon_list (GdkWindow *window,
                                 GList     *textures)
 {
   GdkTexture *big_texture, *small_texture;
-  GdkPixbuf *big_pixbuf, *small_pixbuf;
   gint big_diff, small_diff;
   gint big_w, big_h, small_w, small_h;
   gint w, h;
@@ -2273,9 +2271,9 @@ gdk_win32_window_set_icon_list (GdkWindow *window,
   big_diff = 0;
   small_diff = 0;
 
-  for (l = textures; l; l = l->next)
+  for (GList *l = textures; l; l = l->next)
     {
-      texture = l->data;
+      GdkTexture *texture = l->data;
       w = gdk_texture_get_width (texture);
       h = gdk_texture_get_height (texture);
 
@@ -2301,10 +2299,10 @@ gdk_win32_window_set_icon_list (GdkWindow *window,
     }
 
   /* Create the icons */
-  big_hicon = gdk_win32_texture_to_hicon (big_texture);
-  g_object_unref (big_pixbuf);
+  big_hicon = _gdk_win32_texture_to_hicon (big_texture);
+  g_object_unref (big_texture);
   small_hicon = _gdk_win32_texture_to_hicon (small_texture);
-  g_object_unref (small_pixbuf);
+  g_object_unref (small_texture);
 
   /* Set the icons */
   SendMessageW (GDK_WINDOW_HWND (window), WM_SETICON, ICON_BIG,
@@ -4399,7 +4397,7 @@ setup_drag_move_resize_context (GdkWindow                   *window,
 
   cursor_name = get_cursor_name_from_op (op, edge);
 
-  context->cursor = _gdk_win32_display_get_cursor_for_name (display, cursor_name);
+  context->cursor = gdk_cursor_new_from_name (cursor_name, NULL);
 
   pointer_window = child_window_at_coordinates (window, root_x, root_y);
 

commit 437a359f1b7f9da0638ffa16055cc18b2bdd4a70

gdk/win32: fix cursor hotspot
This seems to be a regression from commit c06b1cc103b8 "Win32: Re-work cursor handling".

Signed-off-by: Marc-André Lureau <marcandre.lureau@redhat.com>

gdk/win32/gdkcursor-win32.c
@@ -911,7 +911,7 @@ gdk_win32hcursor_create_for_texture (GdkWin32Display *display,
   
   pixbuf = gdk_pixbuf_get_from_surface (surface, 0, 0, width, height);
   
-  icon = pixbuf_to_hicon (pixbuf, TRUE, 0, 0);
+  icon = pixbuf_to_hicon (pixbuf, FALSE, x, y);
   
   g_object_unref (pixbuf);
   
@@ -1471,16 +1471,6 @@ _gdk_win32_texture_to_hicon (GdkTexture *texture)
   
   return icon;
 }
-
-HICON
-_gdk_win32_pixbuf_to_hcursor (GdkPixbuf *pixbuf,
-			      int        x_hotspot,
-			      int        y_hotspot)
-{
-  return pixbuf_to_hicon (pixbuf, FALSE, x_hotspot, y_hotspot);
-}
-
-
 
 /**
  * gdk_win32_display_get_win32hcursor:

gdk/win32/gdkprivate-win32.h
@@ -330,9 +330,6 @@ void  _gdk_win32_end_modal_call (GdkWin32ModalOpKind kind);
  * Windows XP, thresholds alpha otherwise.
  */
 HICON _gdk_win32_texture_to_hicon  (GdkTexture *texture);
-HICON _gdk_win32_pixbuf_to_hcursor (GdkPixbuf *pixbuf,
-                                    int        x_hotspot,
-                                    int        y_hotspot);
 
 void _gdk_win32_display_init_cursors (GdkWin32Display     *display);
 void _gdk_win32_display_finalize_cursors (GdkWin32Display *display);