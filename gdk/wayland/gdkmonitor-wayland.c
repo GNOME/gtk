e82e7823a886f2e443983e2a4436d9a57369a0b5
Vlad Zahorodnii <vlad.zahorodnii@kde.org>
2022-11-22 11:03:05 +0200

    gdk/wayland: Destroy zxdg_output_v1 upon GdkWaylandMonitor destruction
    It prevents potentially leaking memory.

--- gdk/wayland/gdkmonitor-wayland.c
+++ gdk/wayland/gdkmonitor-wayland.c
@@ -47,6 +47,7 @@ gdk_wayland_monitor_finalize (GObject *object)
 
   g_free (monitor->name);
 
+  g_clear_pointer (&monitor->xdg_output, zxdg_output_v1_destroy);
   wl_output_destroy (monitor->output);
 
   G_OBJECT_CLASS (gdk_wayland_monitor_parent_class)->finalize (object);
