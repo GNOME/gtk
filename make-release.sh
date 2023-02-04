907f878c9f2d4bbf17691efb1f4d841a39c1ab1b
Matthias Clasen <mclasen@redhat.com>
2023-02-04 16:31:55 +0100

    make-release: Explicily enable introspection

--- make-release.sh
+++ make-release.sh
@@ -13,7 +13,7 @@ fi
 meson subprojects update gi-docgen
 
 # make the release tarball
-meson setup -Dgtk_doc=true --force-fallback-for gi-docgen ${release_build_dir} || exit
+meson setup -Dintrospection=enabled -Dgtk_doc=true --force-fallback-for gi-docgen ${release_build_dir} || exit
 meson compile -C${release_build_dir} || exit
 meson dist -C${release_build_dir} --include-subprojects || exit
 
