GNOME/gtk

commit 0eb791eaaa2672ffdb69c4bca47125aea6b1c2ba

Make mask nodes more versatile
Add a GskMaskMode enumeration and implement it
in the GL and cairo renderers.

gdk/wayland/gdkglcontext-wayland.c
@@ -25,6 +25,7 @@
 
 #include "gdkdisplay-wayland.h"
 #include "gdksurface-wayland.h"
+#include "gdksurface-wayland-private.h"
 
 #include "gdkwaylanddisplay.h"
 #include "gdkwaylandglcontext.h"
@@ -60,9 +61,14 @@ gdk_wayland_gl_context_end_frame (GdkDrawContext *draw_context,
                                   cairo_region_t *painted)
 {
   GdkSurface *surface = gdk_draw_context_get_surface (draw_context);
+  GdkWaylandSurface *impl = GDK_WAYLAND_SURFACE (surface);
+  int dx = impl->pending_buffer_offset_x;
+  int dy = impl->pending_buffer_offset_y;
 
   gdk_wayland_surface_sync (surface);
   gdk_wayland_surface_request_frame (surface);
+
+  wl_surface_offset (impl->display_server.wl_surface, dx, dy);
 
   GDK_DRAW_CONTEXT_CLASS (gdk_wayland_gl_context_parent_class)->end_frame (draw_context, painted);
 

demos/gtk-demo/demo4widget.c
@@ -92,7 +92,7 @@ demo4_widget_snapshot (GtkWidget   *widget,
   width = gtk_widget_get_width (widget);
   height = gtk_widget_get_height (widget);
 
-  gtk_snapshot_push_mask (snapshot);
+  gtk_snapshot_push_mask (snapshot, GSK_MASK_MODE_INVERTED_ALPHA);
   gtk_snapshot_append_layout (snapshot, self->layout, &(GdkRGBA) { 0, 0, 0, 1 });
   gtk_snapshot_pop (snapshot);
 

gdk/wayland/gdksurface-wayland.c
@@ -229,7 +229,7 @@ gdk_wayland_surface_update_size (GdkSurface *surface,
 
   if (impl->display_server.egl_window)
     wl_egl_window_resize (impl->display_server.egl_window, width * scale, height * scale, 0, 0);
-  if (impl->display_server.wl_surface)
+  if (impl->display_server.wl_surface && scale_changed)
     wl_surface_set_buffer_scale (impl->display_server.wl_surface, scale);
 
   gdk_surface_invalidate_rect (surface, NULL);
@@ -588,6 +588,7 @@ gdk_wayland_surface_sync_offset (GdkSurface *surface)
       WL_SURFACE_OFFSET_SINCE_VERSION)
     return;
 
+#if 0
   if (impl->pending_buffer_offset_x == 0 &&
       impl->pending_buffer_offset_y == 0)
     return;
@@ -597,6 +598,7 @@ gdk_wayland_surface_sync_offset (GdkSurface *surface)
                      impl->pending_buffer_offset_y);
   impl->pending_buffer_offset_x = 0;
   impl->pending_buffer_offset_y = 0;
+#endif
 }
 
 void

gsk/gl/gskglprograms.defs
@@ -62,7 +62,8 @@ GSK_GL_DEFINE_PROGRAM (linear_gradient,
 
 GSK_GL_DEFINE_PROGRAM (mask,
                        GSK_GL_SHADER_SINGLE (GSK_GL_SHADER_RESOURCE ("mask.glsl")),
-                       GSK_GL_ADD_UNIFORM (1, MASK_SOURCE, u_mask))
+                       GSK_GL_ADD_UNIFORM (1, MASK_SOURCE, u_mask)
+                       GSK_GL_ADD_UNIFORM (2, MASK_MODE, u_mode))
 
 GSK_GL_DEFINE_PROGRAM (outset_shadow,
                        GSK_GL_SHADER_SINGLE (GSK_GL_SHADER_RESOURCE ("outset_shadow.glsl")),

gsk/gl/gskglrenderjob.c
@@ -3278,8 +3278,8 @@ gsk_gl_render_job_visit_blend_node (GskGLRenderJob      *job,
 }
 
 static inline void
-gsk_gl_render_job_visit_mask_node (GskGLRenderJob       *job,
-                                    const GskRenderNode *node)
+gsk_gl_render_job_visit_mask_node (GskGLRenderJob      *job,
+                                   const GskRenderNode *node)
 {
   const GskRenderNode *source = gsk_mask_node_get_source (node);
   const GskRenderNode *mask = gsk_mask_node_get_mask (node);
@@ -3293,6 +3293,7 @@ gsk_gl_render_job_visit_mask_node (GskGLRenderJob       *job,
   mask_offscreen.bounds = &node->bounds;
   mask_offscreen.force_offscreen = TRUE;
   mask_offscreen.reset_clip = TRUE;
+  mask_offscreen.do_not_cache = TRUE;
 
   /* TODO: We create 2 textures here as big as the mask node, but both
    * nodes might be a lot smaller than that.
@@ -3323,6 +3324,9 @@ gsk_gl_render_job_visit_mask_node (GskGLRenderJob       *job,
                                       GL_TEXTURE_2D,
                                       GL_TEXTURE1,
                                       mask_offscreen.texture_id);
+  gsk_gl_program_set_uniform1i (job->current_program,
+                                UNIFORM_MASK_MODE, 0,
+                                gsk_mask_node_get_mask_mode (node));
   gsk_gl_render_job_draw_offscreen_rect (job, &node->bounds);
   gsk_gl_render_job_end_draw (job);
 }

gsk/gl/resources/mask.glsl
@@ -10,10 +10,24 @@ void main() {
 // FRAGMENT_SHADER:
 // mask.glsl
 
+uniform int u_mode;
 uniform sampler2D u_mask;
 
 void main() {
   vec4 source = GskTexture(u_source, vUv);
   vec4 mask = GskTexture(u_mask, vUv);
-  gskSetOutputColor(vec4 (source * mask.a));
+  float mask_value;
+
+  if (u_mode == 0)
+    mask_value = mask.a;
+  else if (u_mode == 1)
+    mask_value = 1 - mask.a;
+  else if (u_mode == 2)
+    mask_value = (0.2126 * mask.r + 0.7152 * mask.g + 0.0722 * mask.b) * mask.a;
+  else if (u_mode == 3)
+    mask_value = 1 - (0.2126 * mask.r + 0.7152 * mask.g + 0.0722 * mask.b) * mask.a;
+  else
+    mask_value = 0;
+
+  gskSetOutputColor(vec4 (source * mask_value));
 }

gsk/gskenums.h
@@ -255,5 +255,25 @@ typedef enum
   GSK_GL_UNIFORM_TYPE_VEC4,
 } GskGLUniformType;
 
+/**
+ * GskMaskMode:
+ * @GSK_MASK_MODE_ALPHA: Use the alpha channel of the mask
+ * @GSK_MASK_MODE_INVERTED_ALPHA: Use the inverted alpha channel of the mask
+ * @GSK_MASK_MODE_LUMINANCE: Use the luminance of the mask,
+ *     multiplied by mask alpha
+ * @GSK_MASK_MODE_INVERTED_LUMINANCE: Use the inverted luminance of the mask,
+ *     multiplied by mask alpha
+ *
+ * The mask modes available for mask nodes.
+ *
+ * Since: 4.10
+ */
+typedef enum
+{
+  GSK_MASK_MODE_ALPHA,
+  GSK_MASK_MODE_INVERTED_ALPHA,
+  GSK_MASK_MODE_LUMINANCE,
+  GSK_MASK_MODE_INVERTED_LUMINANCE
+} GskMaskMode;
 
 #endif /* __GSK_TYPES_H__ */

gsk/gskrendernode.h
@@ -535,11 +535,14 @@ GDK_AVAILABLE_IN_4_10
 GType                  gsk_mask_node_get_type                   (void) G_GNUC_CONST;
 GDK_AVAILABLE_IN_4_10
 GskRenderNode *        gsk_mask_node_new                        (GskRenderNode            *source,
-                                                                 GskRenderNode            *mask);
+                                                                 GskRenderNode            *mask,
+                                                                 GskMaskMode               mask_mode);
 GDK_AVAILABLE_IN_4_10
 GskRenderNode *        gsk_mask_node_get_source                 (const GskRenderNode      *node);
 GDK_AVAILABLE_IN_4_10
 GskRenderNode *        gsk_mask_node_get_mask                   (const GskRenderNode      *node);
+GDK_AVAILABLE_IN_4_10
+GskMaskMode            gsk_mask_node_get_mask_mode              (const GskRenderNode      *node);
 
 GDK_AVAILABLE_IN_ALL
 GType                   gsk_gl_shader_node_get_type             (void) G_GNUC_CONST;

commit 3183a213472067534ba907d78edf57af96f277a2

wayland/glcontext: Don't call wl_surface.offset on old objects
The availability of wl_surface.offset depends on the compositor, so we
can't call it unconditionally. Add a version check to so we only call
offset if we know we won't raise a protocol error.

Fixes: 0eb791eaaa ("Make mask nodes more versatile")

gdk/wayland/gdkglcontext-wayland.c
@@ -68,7 +68,9 @@ gdk_wayland_gl_context_end_frame (GdkDrawContext *draw_context,
   gdk_wayland_surface_sync (surface);
   gdk_wayland_surface_request_frame (surface);
 
-  wl_surface_offset (impl->display_server.wl_surface, dx, dy);
+  if (wl_surface_get_version (impl->display_server.wl_surface) >=
+      WL_SURFACE_OFFSET_SINCE_VERSION)
+    wl_surface_offset (impl->display_server.wl_surface, dx, dy);
 
   GDK_DRAW_CONTEXT_CLASS (gdk_wayland_gl_context_parent_class)->end_frame (draw_context, painted);
 