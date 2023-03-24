GNOME/gtk

commit 4291f27b562f58b4d86b4f1816047614f71e6085

gltexture: Try harder to determine the format
We can query these things as long as the texture ID
is valid in the current context.

gdk/gdkgltexture.c
@@ -317,9 +317,9 @@ gdk_gl_texture_determine_format (GdkGLTexture *self)
   GLint internal_format;
 
   /* Abort if somebody else is GL-ing here... */
-  if (self->context != gdk_gl_context_get_current () ||
+  if (!gdk_gl_context_is_shared (self->context, gdk_gl_context_get_current ()) ||
       /* ... or glGetTexLevelParameter() isn't supported */
-      !gdk_gl_context_check_version (self->context, 0, 0, 3, 1))
+      !gdk_gl_context_check_version (gdk_gl_context_get_current (), 0, 0, 3, 1))
     {
       texture->format = GDK_MEMORY_DEFAULT;
       return;

commit 5c558c00912795d7ae82c440ea7b0a790962651a

Fix up criticals from GdkGLTexture
I broke this in 4291f27b562f58.
I overlooked that there may not be a current context.

gdk/gdkgltexture.c
@@ -320,14 +320,17 @@ static void
 gdk_gl_texture_determine_format (GdkGLTexture *self)
 {
   GdkTexture *texture = GDK_TEXTURE (self);
+  GdkGLContext *context;
   GLint active_texture;
   GLint internal_format;
   GLint width, height;
 
   /* Abort if somebody else is GL-ing here... */
-  if (!gdk_gl_context_is_shared (self->context, gdk_gl_context_get_current ()) ||
+  context = gdk_gl_context_get_current ();
+  if (context == NULL ||
+      !gdk_gl_context_is_shared (self->context, context) ||
       /* ... or glGetTexLevelParameter() isn't supported */
-      !gdk_gl_context_check_version (gdk_gl_context_get_current (), 0, 0, 3, 1))
+      !gdk_gl_context_check_version (context, 0, 0, 3, 1))
     {
       texture->format = GDK_MEMORY_DEFAULT;
       self->has_mipmap = FALSE;