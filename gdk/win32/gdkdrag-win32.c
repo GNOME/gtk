GNOME/gtk

commit bc159207bd05ca4bfc8e6f7ea819ee487bf75705

gdk: Drop old debug macros
Drop the old GDK_NOTE() and GDK_DISPLAY_NOTE()
macros from gdkdebug.h. Reintroduce them in the
win32 backend, where they are still used.

gdk/win32/gdkdrag-win32.c
@@ -37,6 +37,8 @@
 #include <math.h>
 #include <string.h>
 
+#define GDK_NOTE(a,b)
+
 /*
  * Support for OLE-2 drag and drop added at Archaeopteryx Software, 2001
  * For more information, do not contact Stephan R.A. Deibel (sdeibel@archaeopteryx.com),

gdk/gdkdebug.h
@@ -65,9 +65,6 @@ void             gdk_display_set_debug_flags    (GdkDisplay       *display,
 
 #define GDK_DISPLAY_DEBUG_CHECK(display,type) \
     G_UNLIKELY (gdk_display_get_debug_flags (display) & GDK_DEBUG_##type)
-#define GDK_DISPLAY_NOTE(display,type,action)          G_STMT_START {     \
-    if (GDK_DISPLAY_DEBUG_CHECK (display,type))                           \
-       { action; };                            } G_STMT_END
 
 #define GDK_DISPLAY_DEBUG(display,type,...)                               \
     if (GDK_DISPLAY_DEBUG_CHECK (display,type))                           \
@@ -76,13 +73,11 @@ void             gdk_display_set_debug_flags    (GdkDisplay       *display,
 #else /* !G_ENABLE_DEBUG */
 
 #define GDK_DISPLAY_DEBUG_CHECK(display,type) 0
-#define GDK_DISPLAY_NOTE(display,type,action)
 #define GDK_DISPLAY_DEBUG(display,type,...)
 
 #endif /* G_ENABLE_DEBUG */
 
 #define GDK_DEBUG_CHECK(type) GDK_DISPLAY_DEBUG_CHECK (NULL,type)
-#define GDK_NOTE(type,action) GDK_DISPLAY_NOTE (NULL,type,action)
 #define GDK_DEBUG(type,...) GDK_DISPLAY_DEBUG (NULL,type,__VA_ARGS__)
 
 #endif

gdk/win32/gdkprivate-win32.h
@@ -26,6 +26,7 @@
 #define __GDK_PRIVATE_WIN32_H__
 
 #include <gdk/gdkcursorprivate.h>
+#include <gdk/gdkdebug.h>
 #include <gdk/win32/gdksurface-win32.h>
 #include <gdk/win32/gdkwin32display.h>
 #include <gdk/win32/gdkwin32screen.h>
@@ -36,6 +37,23 @@
 
 
 #include "config.h"
+
+
+/* Old debug macros */
+
+#ifdef G_ENABLE_DEBUG
+
+#define GDK_NOTE(type,action)                             \
+    G_STMT_START {                                        \
+      if (GDK_DEBUG_CHECK (type))                         \
+         { action; };                                     \
+    } G_STMT_END
+
+#else
+
+#define GDK_NOTE(type,action)
+
+#endif
 
 /* Make up for some minor w32api or MSVC6 header lossage */
 

commit 0e2d7111eb813dd6355a1a90e902e928a5b9abb4

gdk/win32: fix GDK_NOTE redefined warning
In file included from ../gdk/win32/gdkdrag-win32.c:201:
../gdk/win32/gdkprivate-win32.h:45: warning: "GDK_NOTE" redefined
   45 | #define GDK_NOTE(type,action)                             \
      |
../gdk/win32/gdkdrag-win32.c:40: note: this is the location of the previous definition
   40 | #define GDK_NOTE(a,b)

Fixes: bc159207bd0 ("gdk: Drop old debug macros")

Signed-off-by: Marc-André Lureau <marcandre.lureau@redhat.com>

gdk/win32/gdkdrag-win32.c
@@ -37,8 +37,6 @@
 #include <math.h>
 #include <string.h>
 
-#define GDK_NOTE(a,b)
-
 /*
  * Support for OLE-2 drag and drop added at Archaeopteryx Software, 2001
  * For more information, do not contact Stephan R.A. Deibel (sdeibel@archaeopteryx.com),