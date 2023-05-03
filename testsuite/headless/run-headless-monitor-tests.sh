88f27c423c4339f6f38202a93cf36d66934cba7a
Matthias Clasen <mclasen@redhat.com>
2023-05-03 18:45:56 -0400

    ci: Launch pipewire and wireplumber
    The headless tests require pipewire and wireplumber
    for screencast sessions to work. So launch them.

--- testsuite/headless/run-headless-monitor-tests.sh
+++ testsuite/headless/run-headless-monitor-tests.sh
@@ -4,6 +4,14 @@ srcdir=${MESON_CURRENT_SOURCE_DIR:-./testsuite/headless}
 
 dbus-run-session sh <<EOF
 
+export XDG_RUNTIME_DIR="$(mktemp -p $(pwd) -d xdg-runtime-XXXXXX)"
+
+pipewire &
+pipewire_pid=\$!
+wireplumber &
+wireplumber_pid=\$!
+sleep 1
+
 # echo DBUS_SESSION_BUS_ADDRESS=\$DBUS_SESSION_BUS_ADDRESS
 # echo WAYLAND_DISPLAY=gtk-test
 
@@ -11,7 +19,7 @@ export GTK_A11Y=none
 export GIO_USE_VFS=local
 
 mutter --headless --no-x11 --wayland-display gtk-test >&mutter.log &
-pid=\$!
+mutter_pid=\$!
 
 export WAYLAND_DISPLAY=gtk-test
 export GDK_BACKEND=wayland
@@ -19,7 +27,9 @@ export GDK_BACKEND=wayland
 python3 ${srcdir}/headless-monitor-tests.py
 status=\$?
 
-kill \$pid
+kill \$mutter_pid
+kill \$wireplumber_pid
+kill \$pipewire_pid
 
 exit \$status
 
