343472931d46aebc2bd1ec2a5bf47baa8dbad0a5
Matthias Clasen <mclasen@redhat.com>
2023-05-15 15:25:11 -0400

    Cosmetics
    Linkify a reference.

--- gdk/gdktexturedownloader.c
+++ gdk/gdktexturedownloader.c
@@ -225,17 +225,17 @@ gdk_texture_downloader_download_into (const GdkTextureDownloader *self,
 /**
  * gdk_texture_downloader_download_bytes:
  * @self: the downloader
- * @out_stride: (out): The stride of the resulting data in bytes.
+ * @out_stride: (out): The stride of the resulting data in bytes
  *
  * Downloads the given texture pixels into a `GBytes`. The rowstride will
  * be stored in the stride value.
  *
  * This function will abort if it tries to download a large texture and
- * fails to allocate memory. If you think that may happen, you should
- * handle memory allocation yourself and use
- * gdk_texture_downloader_download_into() once allocation succeeded.
+ * fails to allocate memory. If you think that may happen, you should handle
+ * memory allocation yourself and use [method@Gdk.TextureDownloader.download_into]
+ * once allocation succeeded.
  *
- * Returns: The downloaded pixels.
+ * Returns: The downloaded pixels
  *
  * Since: 4.10
  **/
