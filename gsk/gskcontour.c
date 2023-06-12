cd6d88fca56c6a6e0a72e1263a06a3cccb03fe53
Matthias Clasen <mclasen@redhat.com>
2023-06-11 22:55:13 -0400

    Implement stroking
    Implement gsk_contour_default_add_stroke, which takes a contour
    and stroke parameters, and adds contours to a path builder for
    the outline that would be produced by stroking the path with these
    parameters.
    
    The current implementation does not try to handle short segments
    in the vicinity of sharp joins in any special way, so there can
    be some artifacts in that situation.

--- gsk/gskcontour.c
+++ gsk/gskcontour.c
@@ -555,6 +555,8 @@ gsk_rect_contour_add_stroke (const GskContour *contour,
       rect.size.width = - rect.size.width;
       gsk_path_builder_add_rect (builder, &rect);
     }
+  else
+    gsk_contour_default_add_stroke (contour, builder, stroke);
 }
 
 static void
@@ -1016,6 +1018,8 @@ gsk_circle_contour_add_stroke (const GskContour *contour,
                                   self->start_angle);
       gsk_path_builder_add_contour (builder, c);
     }
+  else
+    gsk_contour_default_add_stroke (contour, builder, stroke);
 }
 
 static void
@@ -1891,6 +1895,7 @@ gsk_standard_contour_add_stroke (const GskContour *contour,
                                  GskPathBuilder   *builder,
                                  GskStroke        *stroke)
 {
+  gsk_contour_default_add_stroke (contour, builder, stroke);
 }
 
 static void
