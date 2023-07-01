cf7decae1a1a2af825caca8da018ce09cd9ce31a
Benjamin Otte <otte@redhat.com>
2023-07-01 22:46:47 +0200

    gdkgl: Check for GLsync before using it
    Copy what we do in GTK4: Check for GL >= 3.2 or GLES >= 3.0 or the
    GL_ARB_sync extension.
    Then store that info for a (private) gdk_gl_context_has_sync()
    function.
    And then check that function before using GLsync objects as introduced
    by commit 9811485990b.
    
    Fixes #5749

--- gdk/gdkglcontext.c
+++ gdk/gdkglcontext.c
@@ -101,6 +101,7 @@ typedef struct {
   guint has_gl_framebuffer_blit : 1;
   guint has_frame_terminator : 1;
   guint has_unpack_subimage : 1;
+  guint has_sync : 1;
   guint extensions_checked : 1;
   guint debug_enabled : 1;
   guint forward_compatible : 1;
@@ -439,6 +440,14 @@ gdk_gl_context_has_unpack_subimage (GdkGLContext *context)
   GdkGLContextPrivate *priv = gdk_gl_context_get_instance_private (context);
 
   return priv->has_unpack_subimage;
+}
+
+gboolean
+gdk_gl_context_has_sync (GdkGLContext *context)
+{
+  GdkGLContextPrivate *priv = gdk_gl_context_get_instance_private (context);
+
+  return priv->has_sync;
 }
 
 /**
@@ -809,6 +818,7 @@ gdk_gl_context_check_extensions (GdkGLContext *context)
       priv->has_frame_terminator = FALSE;
 
       priv->has_unpack_subimage = epoxy_has_gl_extension ("GL_EXT_unpack_subimage");
+      priv->has_sync = priv->gl_version >= 30;
     }
   else
     {
@@ -818,6 +828,9 @@ gdk_gl_context_check_extensions (GdkGLContext *context)
       priv->has_gl_framebuffer_blit = priv->gl_version >= 30 || epoxy_has_gl_extension ("GL_EXT_framebuffer_blit");
       priv->has_frame_terminator = epoxy_has_gl_extension ("GL_GREMEDY_frame_terminator");
       priv->has_unpack_subimage = TRUE;
+      priv->has_sync = priv->gl_version >= 32 ||
+                       epoxy_has_gl_extension ("GL_ARB_sync") ||
+                       epoxy_has_gl_extension ("GL_APPLE_sync");
 
       /* We asked for a core profile, but we didn't get one, so we're in legacy mode */
       if (priv->gl_version < 32)
