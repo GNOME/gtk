b2c0aa18be964be32f72fd90585f78206f1ab61d
Matthias Clasen <mclasen@redhat.com>
2023-07-01 21:33:02 -0400

    Add API for boolean operations on paths
    The new APIs here are:
    gsk_path_union
    gsk_path_intersection
    gsk_path_difference
    gsk_path_symmetric_difference
    gsk_path_simplify

--- gsk/gskpath.h
+++ gsk/gskpath.h
@@ -133,6 +133,30 @@ GskPath *               gsk_path_offset                         (GskPath        
                                                                  float                   distance,
                                                                  GskStroke              *stroke);
 
+GDK_AVAILABLE_IN_ALL
+GskPath *               gsk_path_union                          (GskPath                *first,
+                                                                 GskPath                *second,
+                                                                 GskFillRule             fill_rule);
+
+GDK_AVAILABLE_IN_ALL
+GskPath *               gsk_path_intersection                   (GskPath                *first,
+                                                                 GskPath                *second,
+                                                                 GskFillRule             fill_rule);
+
+GDK_AVAILABLE_IN_ALL
+GskPath *               gsk_path_difference                     (GskPath                *first,
+                                                                 GskPath                *second,
+                                                                 GskFillRule             fill_rule);
+
+GDK_AVAILABLE_IN_ALL
+GskPath *               gsk_path_symmetric_difference           (GskPath                *first,
+                                                                 GskPath                *second,
+                                                                 GskFillRule             fill_rule);
+
+GDK_AVAILABLE_IN_ALL
+GskPath *               gsk_path_simplify                       (GskPath                *self,
+                                                                 GskFillRule             fill_rule);
+
 G_DEFINE_AUTOPTR_CLEANUP_FUNC(GskPath, gsk_path_unref)
 
 G_END_DECLS
