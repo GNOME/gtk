380ac5804db7a8482fe878dc093bf719a0f8c21a
Matthias Clasen <mclasen@redhat.com>
2023-07-08 18:34:36 -0400

    curve: Add gsk_curve_intersect
    Add a way to find the intersections of two curves.
    We can handle some curve-line intersections directly,
    the general case is handled via bisecting.
    
    This will be used in stroking and path ops.

--- gsk/gskcurveprivate.h
+++ gsk/gskcurveprivate.h
@@ -170,5 +170,12 @@ int                     gsk_curve_get_curvature_points          (const GskCurve 
 int                     gsk_curve_get_cusps                     (const GskCurve         *curve,
                                                                  float                   t[2]);
 
+int                     gsk_curve_intersect                     (const GskCurve         *curve1,
+                                                                 const GskCurve         *curve2,
+                                                                 float                  *t1,
+                                                                 float                  *t2,
+                                                                 graphene_point_t       *p,
+                                                                 int                     n);
+
 G_END_DECLS
 
