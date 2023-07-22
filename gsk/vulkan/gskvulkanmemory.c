dfb0d18290f22fb47abc883106044b880f2c4ea2
Benjamin Otte <otte@redhat.com>
2023-07-22 21:59:23 +0200

    vulkan: Introduce a memory allocator infrastructure
    The only big difference (apart from lots more vfuncs) is that we map
    mappable memory automatically and there is no longer a map/unmap step
    involved.
    
    According to various Vulkan docs, it is not necessary to do that
    map/unmap shuffling, so we don't do it anymore.

--- gsk/vulkan/gskvulkanmemory.c
+++ gsk/vulkan/gskvulkanmemory.c
@@ -4,15 +4,39 @@
 
 #include "gskvulkanprivate.h"
 
+
 struct _GskVulkanMemory
 {
   GdkVulkanContext *vulkan;
 
   gsize size;
 
-  VkMemoryType vk_memory_type;
-  VkDeviceMemory vk_memory;
+  GskVulkanAllocator *allocator;
+  GskVulkanAllocation allocation;
 };
+
+static GskVulkanAllocator *
+gsk_vulkan_allocator_get (GdkVulkanContext   *context,
+                          gsize               index,
+                          const VkMemoryType *type)
+{
+  GskVulkanAllocator **allocators = g_object_get_data (G_OBJECT (context), "-gsk-allocators");
+
+  g_assert (index <= 10);
+
+  if (allocators == NULL)
+    {
+      allocators = g_new0 (GskVulkanAllocator *, 10);
+      g_object_set_data (G_OBJECT (context), "-gsk-allocators", allocators);
+    }
+
+  if (allocators[index] == NULL)
+    allocators[index] = gsk_vulkan_direct_allocator_new (gdk_vulkan_context_get_device (context),
+                                                         index,
+                                                         type);
+
+  return allocators[index];
+}
 
 GskVulkanMemory *
 gsk_vulkan_memory_new (GdkVulkanContext      *context,
@@ -26,7 +50,6 @@ gsk_vulkan_memory_new (GdkVulkanContext      *context,
 
   self = g_new0 (GskVulkanMemory, 1);
 
-  self->vulkan = g_object_ref (context);
   self->size = size;
 
   vkGetPhysicalDeviceMemoryProperties (gdk_vulkan_context_get_physical_device (context),
@@ -43,15 +66,8 @@ gsk_vulkan_memory_new (GdkVulkanContext      *context,
 
   g_assert (i < properties.memoryTypeCount);
 
-  self->vk_memory_type = properties.memoryTypes[i];
-  GSK_VK_CHECK (vkAllocateMemory, gdk_vulkan_context_get_device (context),
-                                  &(VkMemoryAllocateInfo) {
-                                      .sType = VK_STRUCTURE_TYPE_MEMORY_ALLOCATE_INFO,
-                                      .allocationSize = size,
-                                      .memoryTypeIndex = i
-                                  },
-                                  NULL,
-                                  &self->vk_memory);
+  self->allocator = gsk_vulkan_allocator_get (context, i, &properties.memoryTypes[i]);
+  gsk_vulkan_alloc (self->allocator, size, &self->allocation);
 
   return self;
 }
@@ -59,11 +75,7 @@ gsk_vulkan_memory_new (GdkVulkanContext      *context,
 void
 gsk_vulkan_memory_free (GskVulkanMemory *self)
 {
-  vkFreeMemory (gdk_vulkan_context_get_device (self->vulkan),
-                self->vk_memory,
-                NULL);
-
-  g_object_unref (self->vulkan);
+  gsk_vulkan_free (self->allocator, &self->allocation);
 
   g_free (self);
 }
@@ -71,50 +83,106 @@ gsk_vulkan_memory_free (GskVulkanMemory *self)
 VkDeviceMemory
 gsk_vulkan_memory_get_device_memory (GskVulkanMemory *self)
 {
-  return self->vk_memory;
+  return self->allocation.vk_memory;
 }
 
 gboolean
 gsk_vulkan_memory_can_map (GskVulkanMemory *self,
                            gboolean         fast)
 {
-  if (!(self->vk_memory_type.propertyFlags & VK_MEMORY_PROPERTY_HOST_VISIBLE_BIT))
-    return FALSE;
-
-  /* FIXME: no support implemented for this */
-  if (!(self->vk_memory_type.propertyFlags & VK_MEMORY_PROPERTY_HOST_COHERENT_BIT))
-    return FALSE;
-
-  if (!fast)
-    return TRUE;
-
-  if (!(self->vk_memory_type.propertyFlags & VK_MEMORY_PROPERTY_HOST_CACHED_BIT))
-    return FALSE;
-
-  return TRUE;
+  return self->allocation.map != NULL;
 }
 
 guchar *
 gsk_vulkan_memory_map (GskVulkanMemory *self)
 {
-  void *data;
-
-  g_assert (gsk_vulkan_memory_can_map (self, FALSE));
-
-  GSK_VK_CHECK (vkMapMemory, gdk_vulkan_context_get_device (self->vulkan),
-                             self->vk_memory,
-                             0,
-                             self->size,
-                             0,
-                             &data);
-
-  return data;
+  return self->allocation.map;
 }
 
 void
 gsk_vulkan_memory_unmap (GskVulkanMemory *self)
 {
-  vkUnmapMemory (gdk_vulkan_context_get_device (self->vulkan),
-                 self->vk_memory);
 }
 
+/* {{{ direct allocator ***/
+
+typedef struct _GskVulkanDirectAllocator GskVulkanDirectAllocator;
+
+struct _GskVulkanDirectAllocator
+{
+  GskVulkanAllocator allocator_class;
+
+  VkDevice device; /* no reference held */
+  uint32_t vk_memory_type_index;
+  VkMemoryType vk_memory_type;
+};
+
+static void
+gsk_vulkan_direct_allocator_free_allocator (GskVulkanAllocator *allocator)
+{
+  g_free (allocator);
+}
+
+static void
+gsk_vulkan_direct_allocator_alloc (GskVulkanAllocator  *allocator,
+                                   VkDeviceSize         size,
+                                   GskVulkanAllocation *alloc)
+{
+  GskVulkanDirectAllocator *self = (GskVulkanDirectAllocator *) allocator;
+
+  GSK_VK_CHECK (vkAllocateMemory, self->device,
+                                  &(VkMemoryAllocateInfo) {
+                                      .sType = VK_STRUCTURE_TYPE_MEMORY_ALLOCATE_INFO,
+                                      .allocationSize = size,
+                                      .memoryTypeIndex = self->vk_memory_type_index
+                                  },
+                                  NULL,
+                                  &alloc->vk_memory);
+
+  if ((self->vk_memory_type.propertyFlags & GSK_VULKAN_MEMORY_MAPPABLE) == GSK_VULKAN_MEMORY_MAPPABLE)
+    {
+        GSK_VK_CHECK (vkMapMemory, self->device,
+                                   alloc->vk_memory,
+                                   0,
+                                   size,
+                                   0,
+                                   (void **) &alloc->map);
+    }
+
+  alloc->offset = 0;
+  alloc->size = size;
+}
+
+static void
+gsk_vulkan_direct_allocator_free (GskVulkanAllocator        *allocator,
+                                  const GskVulkanAllocation *alloc)
+{
+  GskVulkanDirectAllocator *self = (GskVulkanDirectAllocator *) allocator;
+
+  if (alloc->map)
+    vkUnmapMemory (self->device, alloc->vk_memory);
+
+  vkFreeMemory (self->device,
+                alloc->vk_memory,
+                NULL);
+}
+
+GskVulkanAllocator *
+gsk_vulkan_direct_allocator_new (VkDevice            device,
+                                 uint32_t            vk_type_index,
+                                 const VkMemoryType *vk_type)
+{
+  GskVulkanDirectAllocator *self;
+
+  self = g_new0 (GskVulkanDirectAllocator, 1);
+  self->allocator_class.free_allocator = gsk_vulkan_direct_allocator_free_allocator;
+  self->allocator_class.alloc = gsk_vulkan_direct_allocator_alloc;
+  self->allocator_class.free = gsk_vulkan_direct_allocator_free;
+  self->device = device;
+  self->vk_memory_type_index = vk_type_index;
+  self->vk_memory_type = *vk_type;
+
+  return (GskVulkanAllocator *) self;
+}
+
+/* }}} */
--- gsk/vulkan/gskvulkanmemoryprivate.h
+++ gsk/vulkan/gskvulkanmemoryprivate.h
@@ -4,7 +4,44 @@
 
 G_BEGIN_DECLS
 
+#define GSK_VULKAN_MEMORY_MAPPABLE (VK_MEMORY_PROPERTY_HOST_VISIBLE_BIT | \
+                                    VK_MEMORY_PROPERTY_HOST_COHERENT_BIT | \
+                                    VK_MEMORY_PROPERTY_HOST_CACHED_BIT)
+
+typedef struct _GskVulkanAllocator GskVulkanAllocator;
+typedef struct _GskVulkanAllocation GskVulkanAllocation;
 typedef struct _GskVulkanMemory GskVulkanMemory;
+
+struct _GskVulkanAllocation
+{
+  VkDeviceMemory vk_memory;
+  guchar *map;
+  VkDeviceSize offset;
+  VkDeviceSize size;
+};
+
+struct _GskVulkanAllocator
+{
+  void                  (* free_allocator)                              (GskVulkanAllocator             *allocator);
+
+  void                  (* alloc)                                       (GskVulkanAllocator             *allocator,
+                                                                         VkDeviceSize                    size,
+                                                                         GskVulkanAllocation            *out_alloc);
+  void                  (* free)                                        (GskVulkanAllocator             *allocator,
+                                                                         const GskVulkanAllocation      *alloc);
+};
+
+static inline void      gsk_vulkan_alloc                                (GskVulkanAllocator             *allocator,
+                                                                         gsize                           size,
+                                                                         GskVulkanAllocation            *out_alloc);
+static inline void      gsk_vulkan_free                                 (GskVulkanAllocator             *allocator,
+                                                                         const GskVulkanAllocation      *alloc);
+
+static inline void      gsk_vulkan_allocator_free                       (GskVulkanAllocator             *allocator);
+
+GskVulkanAllocator *    gsk_vulkan_direct_allocator_new                 (VkDevice                        device,
+                                                                         uint32_t                        vk_type_index,
+                                                                         const VkMemoryType             *vk_type);
 
 GskVulkanMemory *       gsk_vulkan_memory_new                           (GdkVulkanContext       *context,
                                                                          uint32_t                allowed_types,
@@ -18,6 +55,27 @@ gboolean                gsk_vulkan_memory_can_map                       (GskVulk
                                                                          gboolean                fast);
 guchar *                gsk_vulkan_memory_map                           (GskVulkanMemory        *self);
 void                    gsk_vulkan_memory_unmap                         (GskVulkanMemory        *self);
+
+static inline void
+gsk_vulkan_alloc (GskVulkanAllocator  *allocator,
+                  VkDeviceSize         size,
+                  GskVulkanAllocation *out_alloc)
+{
+  allocator->alloc (allocator, size, out_alloc);
+}
+
+static inline void
+gsk_vulkan_free (GskVulkanAllocator        *allocator,
+                 const GskVulkanAllocation *alloc)
+{
+  allocator->free (allocator, alloc);
+}
+
+static inline void
+gsk_vulkan_allocator_free (GskVulkanAllocator *allocator)
+{
+  allocator->free_allocator (allocator);
+}
 
 G_END_DECLS
 
