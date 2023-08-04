a0429c8218b5decdd265d320bbb4d2ec56e79729
Hari Rana <theevilskeleton@riseup.net>
2023-08-04 19:50:08 -0400

    adjustment: Properly use <code> tag

--- gtk/gtkadjustment.c
+++ gtk/gtkadjustment.c
@@ -34,7 +34,7 @@
  *
  * `GtkAdjustment` is a model for a numeric value.
  *
- * The `GtkAdjustment has an associated lower and upper bound.
+ * The `GtkAdjustment` has an associated lower and upper bound.
  * It also contains step and page increments, and a page size.
  *
  * Adjustments are used within several GTK widgets, including
