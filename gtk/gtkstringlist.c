e0bf96f3293b017c2e8c2a744f6b319988856239
Emmanuele Bassi <ebassi@gnome.org>
2023-09-21 15:27:35 +0100

    docs: Link GtkStringObject from GtkStringList
    A frequently asked question is: "what is the object type inside a
    GtkStringList", so let's make the answer more obvious.

--- gtk/gtkstringlist.c
+++ gtk/gtkstringlist.c
@@ -30,12 +30,13 @@
  *
  * `GtkStringList` is a list model that wraps an array of strings.
  *
- * The objects in the model have a "string" property.
+ * The objects in the model are of type [class@Gtk.StringObject] and have
+ * a "string" property that can be used inside expressions.
  *
  * `GtkStringList` is well-suited for any place where you would
  * typically use a `char*[]`, but need a list model.
  *
- * # GtkStringList as GtkBuildable
+ * ## GtkStringList as GtkBuildable
  *
  * The `GtkStringList` implementation of the `GtkBuildable` interface
  * supports adding items directly using the `<items>` element and
