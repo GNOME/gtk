4936965fb6cb75919d8f5c1cd863e3581afd20ec
Matthias Clasen <mclasen@redhat.com>
2023-10-02 23:16:42 -0400

    display: Get supported dmabuf formats
    These are the dmabuf formats that we can import
    into a GL context as an EGLImage, and successfully
    download.
    
    We skip the GdkDisplay:dmabuf-formats property
    in the default value tests, since the nominal
    default value is NULL, but the actual value is
    constructed on demand.

--- gdk/gdkdisplay.h
+++ gdk/gdkdisplay.h
@@ -134,6 +134,10 @@ gboolean     gdk_display_get_setting (GdkDisplay *display,
                                       const char *name,
                                       GValue     *value);
 
+GDK_AVAILABLE_IN_4_14
+GdkDmabufFormats *
+             gdk_display_get_dmabuf_formats (GdkDisplay *display);
+
 G_DEFINE_AUTOPTR_CLEANUP_FUNC(GdkDisplay, g_object_unref)
 
 G_END_DECLS
