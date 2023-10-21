b27592c6d1aec88c28195d16cef6853ec2b3524e
Matthias Clasen <mclasen@redhat.com>
2023-10-21 07:51:30 -0400

    Switch the shader to version 300
    Still works!

--- gsk/gl/resources/external.glsl
+++ gsk/gl/resources/external.glsl
@@ -1,13 +1,13 @@
 // VERTEX_SHADER:
 // external.glsl
 
-#version 100
+#version 300 es
 
 precision highp float;
 
-attribute vec2 aPosition;
-attribute vec2 aUv;
-varying vec2 vUv;
+in vec2 aPosition;
+in vec2 aUv;
+out vec2 vUv;
 uniform mat4 u_projection;
 uniform mat4 u_modelview;
 
@@ -20,16 +20,18 @@ void main() {
 // FRAGMENT_SHADER:
 // external.glsl
 
-#version 100
-#extension GL_OES_EGL_image_external : require
+#version 300 es
+#extension GL_OES_EGL_image_external_essl3 : require
 
 precision highp float;
 
 uniform samplerExternalOES u_external_source;
 
-varying vec2 vUv;
+in vec2 vUv;
+
+out vec4 outputColor;
 
 void main() {
-  vec4 color = texture2D(u_external_source, vUv);
-  gl_FragColor = color;
+  vec4 color = texture(u_external_source, vUv);
+  outputColor = color;
 }
