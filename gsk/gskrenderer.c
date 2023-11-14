5bdc9f71fdc8fa15d24ae1f87e011392a6ac25a3
Benjamin Otte <otte@redhat.com>
2023-11-14 13:25:30 +0100

    renderer: Remove priv->root_node
    It's not used anywhere.

--- gsk/gskrenderer.c
+++ gsk/gskrenderer.c
@@ -78,7 +78,6 @@ typedef struct
 
   GdkSurface *surface;
   GskRenderNode *prev_node;
-  GskRenderNode *root_node;
 
   GskProfiler *profiler;
 
@@ -365,9 +364,6 @@ gsk_renderer_render_texture (GskRenderer           *renderer,
   g_return_val_if_fail (GSK_IS_RENDERER (renderer), NULL);
   g_return_val_if_fail (priv->is_realized, NULL);
   g_return_val_if_fail (GSK_IS_RENDER_NODE (root), NULL);
-  g_return_val_if_fail (priv->root_node == NULL, NULL);
-
-  priv->root_node = gsk_render_node_ref (root);
 
   if (viewport == NULL)
     {
@@ -393,8 +389,6 @@ gsk_renderer_render_texture (GskRenderer           *renderer,
 
       g_string_free (buf, TRUE);
     }
-
-  g_clear_pointer (&priv->root_node, gsk_render_node_unref);
 
   return texture;
 }
@@ -432,13 +426,11 @@ gsk_renderer_render (GskRenderer          *renderer,
   g_return_if_fail (GSK_IS_RENDERER (renderer));
   g_return_if_fail (priv->is_realized);
   g_return_if_fail (GSK_IS_RENDER_NODE (root));
-  g_return_if_fail (priv->root_node == NULL);
 
   if (priv->surface == NULL)
     return;
 
   renderer_class = GSK_RENDERER_GET_CLASS (renderer);
-  priv->root_node = gsk_render_node_ref (root);
 
   if (renderer_class->supports_offload &&
       !GSK_RENDERER_DEBUG_CHECK (renderer, OFFLOAD_DISABLE))
@@ -481,8 +473,7 @@ gsk_renderer_render (GskRenderer          *renderer,
   g_clear_pointer (&priv->prev_node, gsk_render_node_unref);
   cairo_region_destroy (clip);
   g_clear_pointer (&offload, gsk_offload_free);
-  priv->prev_node = priv->root_node;
-  priv->root_node = NULL;
+  priv->prev_node = gsk_render_node_ref (root);
 }
 
 /*< private >
