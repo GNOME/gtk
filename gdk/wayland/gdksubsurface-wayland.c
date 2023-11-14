46d4c70cd766706871731d17d0452596d5ecd98e
Benjamin Otte <otte@redhat.com>
2023-11-14 13:27:18 +0100

    offload: Only print one debug message
    ... when failing to create the wl_buffer.

--- gdk/wayland/gdksubsurface-wayland.c
+++ gdk/wayland/gdksubsurface-wayland.c
@@ -87,8 +87,6 @@ params_buffer_failed (void                              *data,
 {
   CreateBufferData *cd = data;
 
-  GDK_DEBUG (OFFLOAD, "Creating wl_buffer for dmabuf failed");
-
   cd->buffer = NULL;
   cd->done = TRUE;
 }
@@ -211,7 +209,10 @@ gdk_wayland_subsurface_attach (GdkSubsurface         *sub,
   else
     {
       GDK_DISPLAY_DEBUG (gdk_surface_get_display (sub->parent), OFFLOAD,
-                         "Failed to get buffer for texture, hiding subsurface %p", self);
+                         "Failed to create wl_buffer for %dx%d texture, hiding subsurface %p",
+                         gdk_texture_get_width (texture),
+                         gdk_texture_get_height (texture),
+                         self);
 
       g_set_object (&self->texture, NULL);
 
