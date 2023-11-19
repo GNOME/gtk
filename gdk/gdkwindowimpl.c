GNOME/gtk

commit f5bf4aeaa61ca4eb7df1fd57f043263a08d603be

GtkTooltip: Prevent tooltips from appearing offscreen
This commit adds a single additional condition to the maybe_flip_position
function in gdkwindowimpl.c. If a tooltip's unflipped position is below
the bounds of its containing area, the tooltip uses its flipped position
instead. This prevents tooltips from appearing below the bounds of the
screen when a small widget is positioned very close to the bottom edge
of the screen, such as in Budgie and XFCE panel applets.

Fixes #2784.

gdk/gdkwindowimpl.c
@@ -166,7 +166,7 @@ maybe_flip_position (gint      bounds_pos,
   *flipped = TRUE;
   secondary = rect_pos + (1 - rect_sign) * rect_size / 2 - offset - (1 - window_sign) * window_size / 2;
 
-  if (secondary >= bounds_pos && secondary + window_size <= bounds_pos + bounds_size)
+  if (secondary >= bounds_pos && secondary + window_size <= bounds_pos + bounds_size || primary > bounds_pos + bounds_size)
     return secondary;
 
   *flipped = FALSE;

commit 88217f2e4476590982231158573d9c8416f357b9

Add missing parentheses
This problem was introduced in f5bf4aeaa61ca4eb7.

gdk/gdkwindowimpl.c
@@ -166,7 +166,7 @@ maybe_flip_position (gint      bounds_pos,
   *flipped = TRUE;
   secondary = rect_pos + (1 - rect_sign) * rect_size / 2 - offset - (1 - window_sign) * window_size / 2;
 
-  if (secondary >= bounds_pos && secondary + window_size <= bounds_pos + bounds_size || primary > bounds_pos + bounds_size)
+  if ((secondary >= bounds_pos && secondary + window_size <= bounds_pos + bounds_size) || primary > bounds_pos + bounds_size)
     return secondary;
 
   *flipped = FALSE;