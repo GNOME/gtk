cd7fe772a77f518eda6e819212f70d8012cb21d3
Matthias Clasen <mclasen@redhat.com>
2024-01-15 18:43:28 -0500

    tests: Drop fontconfig setup for custom fonts
    We are putting custom fonts into node files now, so there is
    no more need for this.

--- testsuite/gsk/compare-render.c
+++ testsuite/gsk/compare-render.c
@@ -5,10 +5,6 @@
 #include <stdlib.h>
 #include "../reftests/reftest-compare.h"
 
-#ifdef HAVE_PANGOFT
-#include <pango/pangofc-fontmap.h>
-#endif
-
 
 static char *arg_output_dir = NULL;
 static gboolean flip = FALSE;
@@ -238,22 +234,6 @@ apply_mask_to_pixbuf (GdkPixbuf *pixbuf)
   return copy;
 }
 
-static void
-maybe_add_test_fonts (void)
-{
-#ifdef HAVE_PANGOFT
-  const char *dir;
-
-  dir = g_getenv ("TEST_FONT_DIR");
-
-  if (dir && g_file_test (dir, G_FILE_TEST_EXISTS))
-    {
-      FcConfig *config = FcConfigGetCurrent ();
-      FcConfigAppFontAddDir (config, (const FcChar8 *) dir);
-    }
-#endif
-}
-
 /*
  * Non-option arguments:
  *   1) .node file to compare
@@ -273,8 +253,6 @@ main (int argc, char **argv)
   GError *error = NULL;
   GOptionContext *context;
   GdkTexture *diff_texture;
-
-  maybe_add_test_fonts ();
 
   (g_test_init) (&argc, &argv, NULL);
 
