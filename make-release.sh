df8cf0e0302ed5bcfcac496bec99fa88f5deb02e
Matthias Clasen <mclasen@redhat.com>
2024-01-25 16:22:22 -0500

    Use software rendering for testsuite runs
    We need to force software rendering, or test failures will cause
    meson dist to fail.

--- make-release.sh
+++ make-release.sh
@@ -1,5 +1,8 @@
 #! /bin/sh
 
+export LIBGL_ALWAYS_SOFTWARE=1
+export GDK_VULKAN_DEVICE=1
+
 version=$(head -5 meson.build | grep version | sed -e "s/[^']*'//" -e "s/'.*$//")
 release_build_dir="release_build"
 branch=$(git branch --show-current)
