GNOME/gtk

commit 6e7893d5275eb1d3a0a8060a76cb21e459104544

Let GdkDisplay decide if shadows should be drawn
Replace the `#ifdef` blocks by a backend call.

gdk/x11/gdkdisplay-x11.c
@@ -1429,6 +1429,7 @@ gdk_x11_display_open (const char *display_name)
   int ignore;
   int maj, min;
   char *cm_name;
+  gboolean frame_extents;
 
   XInitThreads ();
 
@@ -1642,6 +1643,10 @@ gdk_x11_display_open (const char *display_name)
                               XGetSelectionOwner (GDK_DISPLAY_XDISPLAY (display),
                                                   gdk_x11_get_xatom_by_name_for_display (display, cm_name)) != None);
   g_free (cm_name);
+
+  frame_extents = gdk_x11_screen_supports_net_wm_hint (gdk_x11_display_get_screen (display),
+                                                       g_intern_static_string ("_GTK_FRAME_EXTENTS"));
+  gdk_display_set_shadow_width (display, frame_extents);
 
   gdk_display_emit_opened (display);
 

gdk/gdkdisplay.c
@@ -77,6 +77,7 @@ enum
   PROP_0,
   PROP_COMPOSITED,
   PROP_RGBA,
+  PROP_SHADOW_WIDTH,
   PROP_INPUT_SHAPES,
   PROP_DMABUF_FORMATS,
   LAST_PROP
@@ -111,6 +112,7 @@ struct _GdkDisplayPrivate {
 
   guint rgba : 1;
   guint composited : 1;
+  guint shadow_width: 1;
   guint input_shapes : 1;
 
   GdkDebugFlags debug_flags;
@@ -142,6 +144,10 @@ gdk_display_get_property (GObject    *object,
 
     case PROP_RGBA:
       g_value_set_boolean (value, gdk_display_is_rgba (display));
+      break;
+
+    case PROP_SHADOW_WIDTH:
+      g_value_set_boolean (value, gdk_display_supports_shadow_width (display));
       break;
 
     case PROP_INPUT_SHAPES:
@@ -240,6 +246,18 @@ gdk_display_class_init (GdkDisplayClass *class)
    */
   props[PROP_RGBA] =
     g_param_spec_boolean ("rgba", NULL, NULL,
+                          TRUE,
+                          G_PARAM_READABLE | G_PARAM_STATIC_STRINGS);
+
+  /**
+   * GdkDisplay:shadow-width: (attributes org.gtk.Property.get=gdk_display_supports_shadow_width)
+   *
+   * %TRUE if the display supports extensible frames.
+   *
+   * Since: 4.14
+   */
+  props[PROP_SHADOW_WIDTH] =
+    g_param_spec_boolean ("shadow-width", NULL, NULL,
                           TRUE,
                           G_PARAM_READABLE | G_PARAM_STATIC_STRINGS);
 
@@ -391,6 +409,7 @@ gdk_display_init (GdkDisplay *display)
 
   priv->composited = TRUE;
   priv->rgba = TRUE;
+  priv->shadow_width = TRUE;
   priv->input_shapes = TRUE;
 }
 
@@ -2120,6 +2139,46 @@ gdk_display_set_rgba (GdkDisplay *display,
   priv->rgba = rgba;
 
   g_object_notify_by_pspec (G_OBJECT (display), props[PROP_RGBA]);
+}
+
+/**
+ * gdk_display_supports_shadow_width: (attributes org.gtk.Method.get_property=shadow-width)
+ * @display: a `GdkDisplay`
+ *
+ * Returns whether it's possible for a surface to draw outside of the window area.
+ *
+ * If %TRUE is returned the application decides if it wants to draw shadows.
+ * If %FALSE is returned, the compositor decides if it wants to draw shadows.
+ *
+ * Returns: %TRUE if surfaces can draw shadows or
+ *   %FALSE if the display does not support this functionality.
+ *
+ * Since: 4.14
+ */
+gboolean
+gdk_display_supports_shadow_width (GdkDisplay *display)
+{
+  GdkDisplayPrivate *priv = gdk_display_get_instance_private (display);
+
+  g_return_val_if_fail (GDK_IS_DISPLAY (display), FALSE);
+
+  return priv->shadow_width;
+}
+
+void
+gdk_display_set_shadow_width (GdkDisplay *display,
+                              gboolean    shadow_width)
+{
+  GdkDisplayPrivate *priv = gdk_display_get_instance_private (display);
+
+  g_return_if_fail (GDK_IS_DISPLAY (display));
+
+  if (priv->shadow_width == shadow_width)
+    return;
+
+  priv->shadow_width = shadow_width;
+
+  g_object_notify_by_pspec (G_OBJECT (display), props[PROP_SHADOW_WIDTH]);
 }
 
 static void

gdk/gdkdisplay.h
@@ -63,6 +63,8 @@ GDK_AVAILABLE_IN_ALL
 gboolean    gdk_display_is_composited      (GdkDisplay  *display);
 GDK_AVAILABLE_IN_ALL
 gboolean    gdk_display_is_rgba            (GdkDisplay  *display);
+GDK_AVAILABLE_IN_4_14
+gboolean    gdk_display_supports_shadow_width (GdkDisplay *display);
 GDK_AVAILABLE_IN_ALL
 gboolean    gdk_display_supports_input_shapes (GdkDisplay    *display);
 

gdk/gdkdisplayprivate.h
@@ -257,6 +257,8 @@ void                gdk_display_set_composited        (GdkDisplay       *display
                                                        gboolean          composited);
 void                gdk_display_set_input_shapes      (GdkDisplay       *display,
                                                        gboolean          input_shapes);
+void                gdk_display_set_shadow_width      (GdkDisplay       *display,
+                                                       gboolean          shadow_width);
 
 void                gdk_display_add_seat              (GdkDisplay       *display,
                                                        GdkSeat          *seat);

gdk/gdktoplevelsize.c
@@ -142,6 +142,9 @@ gdk_toplevel_size_set_min_size (GdkToplevelSize *size,
  * The shadow width corresponds to the part of the computed surface size
  * that would consist of the shadow margin surrounding the window, would
  * there be any.
+ * 
+ * Shadow width should only be set if
+ * [method@Gtk.Display.supports_shadow_width] is %TRUE.
  */
 void
 gdk_toplevel_size_set_shadow_width (GdkToplevelSize *size,

gdk/macos/gdkmacosdisplay.c
@@ -625,6 +625,7 @@ gdk_macos_display_init (GdkMacosDisplay *self)
   gdk_display_set_composited (GDK_DISPLAY (self), TRUE);
   gdk_display_set_input_shapes (GDK_DISPLAY (self), FALSE);
   gdk_display_set_rgba (GDK_DISPLAY (self), TRUE);
+  gdk_display_set_shadow_width (GDK_DISPLAY (self), FALSE);
 }
 
 GdkDisplay *

gdk/win32/gdkdisplay-win32.c
@@ -1064,6 +1064,7 @@ gdk_win32_display_check_composited (GdkWin32Display *display)
     }
 
   gdk_display_set_composited (GDK_DISPLAY (display), composited);
+  gdk_display_set_shadow_width (GDK_DISPLAY (display), composited);
 }
 
 static void

gtk/gtkwindow.c
@@ -2965,28 +2965,12 @@ gtk_window_is_composited (GtkWindow *window)
 static gboolean
 gtk_window_supports_client_shadow (GtkWindow *window)
 {
-#ifdef GDK_WINDOWING_X11
   GtkWindowPrivate *priv = gtk_window_get_instance_private (window);
   GdkDisplay *display;
 
   display = priv->display;
-  if (GDK_IS_X11_DISPLAY (display))
-    {
-      if (!gdk_x11_screen_supports_net_wm_hint (gdk_x11_display_get_screen (display),
-                                                g_intern_static_string ("_GTK_FRAME_EXTENTS")))
-        return FALSE;
-    }
-#endif
 
-#ifndef GDK_WINDOWING_MACOS
-
-  /* We should probably be more specific to check if frame extents are
-   * supported. For now this mimics the 4.12 behavior: if we are on a
-   * compositing WM, we assume we can use frame extents. */
-  return gtk_window_is_composited (window);
-#endif
-
-  return FALSE;
+  return gdk_display_supports_shadow_width (display);
 }
 
 static void

commit e6a4cc8ba239bb339ce8c2dec4704153678984a3

x11: Shadows require compositing
We can only do client-side shadows if we have an rgba visual
and a compositing wm.

Fallout from 6e7893d5.

Fixes: #6464

gdk/x11/gdkdisplay-x11.c
@@ -1430,6 +1430,8 @@ gdk_x11_display_open (const char *display_name)
   int maj, min;
   char *cm_name;
   gboolean frame_extents;
+  gboolean rgba;
+  gboolean composited;
 
   XInitThreads ();
 
@@ -1646,7 +1648,10 @@ gdk_x11_display_open (const char *display_name)
 
   frame_extents = gdk_x11_screen_supports_net_wm_hint (gdk_x11_display_get_screen (display),
                                                        g_intern_static_string ("_GTK_FRAME_EXTENTS"));
-  gdk_display_set_shadow_width (display, frame_extents);
+  rgba = gdk_display_is_rgba (display);
+  composited = gdk_display_is_composited (display);
+
+  gdk_display_set_shadow_width (display, frame_extents && rgba && composited);
 
   gdk_display_emit_opened (display);
 