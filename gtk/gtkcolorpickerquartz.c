c835ca41e02697088b6f53a4b1eacd8e54e064f5
Arjan Molenaar <gaphor@gmail.com>
2024-04-03 21:00:17 +0200

    macos: Pick color with NSColorSampler
    NSColorSampler is the easiest way to pick a color.
    It does require macOS 10.15+ to work.

--- gtk/gtkcolorpickerquartz.c
+++ gtk/gtkcolorpickerquartz.c
@@ -17,11 +17,18 @@
 
 #include "config.h"
 
+#include <AppKit/AppKit.h>
+
 #include "gtkcolorpickerquartzprivate.h"
+
+#include <gio/gio.h>
 
 struct _GtkColorPickerQuartz
 {
   GObject parent_instance;
+
+  NSColorSampler *sampler;
+  GTask *task;
 };
 
 struct _GtkColorPickerQuartzClass
@@ -42,7 +49,9 @@ gtk_color_picker_quartz_initable_init (GInitable     *initable,
                                        GCancellable  *cancellable,
                                        GError       **error)
 {
-//   GtkColorPickerQuartz *picker = GTK_COLOR_PICKER_QUARTZ (initable);
+  GtkColorPickerQuartz *picker = GTK_COLOR_PICKER_QUARTZ (initable);
+
+  picker->sampler = [[NSColorSampler alloc] init];
 
   return TRUE;
 }
@@ -62,7 +71,14 @@ gtk_color_picker_quartz_init (GtkColorPickerQuartz *picker)
 static void
 gtk_color_picker_quartz_finalize (GObject *object)
 {
-//   GtkColorPickerQuartz *picker = GTK_COLOR_PICKER_QUARTZ (object);
+  GtkColorPickerQuartz *picker = GTK_COLOR_PICKER_QUARTZ (object);
+
+  g_clear_object (&picker->task);
+  if (picker->sampler != NULL)
+    {
+      [picker->sampler release];
+      picker->sampler = NULL;
+    }
 
   G_OBJECT_CLASS (gtk_color_picker_quartz_parent_class)->finalize (object);
 }
@@ -87,7 +103,37 @@ gtk_color_picker_quartz_pick (GtkColorPicker      *cp,
                               GAsyncReadyCallback  callback,
                               gpointer             user_data)
 {
-//   GtkColorPickerQuartz *picker = GTK_COLOR_PICKER_QUARTZ (cp);
+  GtkColorPickerQuartz *picker = GTK_COLOR_PICKER_QUARTZ (cp);
+
+  if (picker->task)
+    return;
+
+  picker->task = g_task_new (picker, NULL, callback, user_data);
+
+  [picker->sampler showSamplerWithSelectionHandler:^(NSColor *selectedColor) {
+    if (selectedColor == NULL)
+      {
+        g_task_return_new_error (picker->task,
+                                 G_IO_ERROR,
+                                 G_IO_ERROR_FAILED,
+                                 "No color received");
+      }
+    else
+      {
+        GdkRGBA rgba = (GdkRGBA){
+          [selectedColor redComponent],
+          [selectedColor greenComponent],
+          [selectedColor blueComponent],
+          [selectedColor alphaComponent],
+        };
+
+        g_task_return_pointer (picker->task,
+                               gdk_rgba_copy (&rgba),
+                               (GDestroyNotify) gdk_rgba_free);
+      }
+
+    g_clear_object (&picker->task);
+  }];
 }
 
 static GdkRGBA *
