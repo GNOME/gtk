3f05b17d37d19e2309ffd8ad110805dfa689ddf7
Arjan Molenaar <gaphor@gmail.com>
2024-05-23 08:51:45 +0200

    macos: Update default application shortcuts
    Default key should be Meta (a.k.a. Command), and not Control.

--- gtk/gtkapplication.c
+++ gtk/gtkapplication.c
@@ -228,7 +228,11 @@ gtk_application_load_resources (GtkApplication *application)
     path = g_strconcat (base_path, optional_slash, "gtk/help-overlay.ui", NULL);
     if (g_resources_get_info (path, G_RESOURCE_LOOKUP_FLAGS_NONE, NULL, NULL, NULL))
       {
+#ifdef __APPLE__
+        const char * const accels[] = { "<Meta>question", NULL };
+#else
         const char * const accels[] = { "<Control>question", NULL };
+#endif
 
         priv->help_overlay_path = path;
         gtk_application_set_accels_for_action (application, "win.show-help-overlay", accels);
