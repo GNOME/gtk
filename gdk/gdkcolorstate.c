5eaba3b1232fdb9efdeef0a1af97efda1ba6fd17
Matthias Clasen <mclasen@redhat.com>
2024-06-08 14:37:51 -0400

    gdk: Add GdkColorState
    This is mostly an empty shell for now. We only have static instances
    for srgb and srgb-linear, which we will use as markers during our
    node processing.
    
    In the future, this object may grow more instances, as well as the
    ability to create them from and save them to icc profiles or cicp
    data. And a color conversion api.

---
+++ gdk/gdkcolorstate.c
@@ -0,0 +1,183 @@
+/* gdkcolorstate.c
+ *
+ * Copyright 2024 Matthias Clasen
+ *
+ * This library is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Lesser General Public
+ * License as published by the Free Software Foundation; either
+ * version 2 of the License, or (at your option) any later version.
+ *
+ * This library is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public
+ * License along with this library. If not, see <http://www.gnu.org/licenses/>.
+ */
+
+#include "config.h"
+
+#include "gdkcolorstateprivate.h"
+
+#include <glib/gi18n-lib.h>
+
+/**
+ * GdkColorState:
+ *
+ * A `GdkColorState` object provides the information to interpret
+ * colors and pixels in a variety of ways.
+ *
+ * They are also known as
+ * [*color spaces*](https://en.wikipedia.org/wiki/Color_space).
+ *
+ * Crucially, GTK knows how to convert colors from one color
+ * state to another.
+ *
+ * `GdkColorState objects are immutable and therefore threadsafe.
+ *
+ * Since 4.16
+ */
+
+G_DEFINE_BOXED_TYPE (GdkColorState, gdk_color_state,
+                     gdk_color_state_ref, gdk_color_state_unref);
+
+/* {{{ Public API */
+
+/**
+ * gdk_color_state_ref:
+ * @self: a `GdkColorState`
+ *
+ * Increase the reference count of @self.
+ *
+ * Returns: the object that was passed in
+ * 
+ * Since: 4.16
+ */
+GdkColorState *
+(gdk_color_state_ref) (GdkColorState *self)
+{
+  return _gdk_color_state_ref (self);
+}
+
+/**
+ * gdk_color_state_unref:
+ * @self:a `GdkColorState`
+ *
+ * Decrease the reference count of @self.
+ *
+ * Unless @self is static, it will be freed
+ * when the reference count reaches zero.
+ *
+ * Since: 4.16
+ */
+void
+(gdk_color_state_unref) (GdkColorState *self)
+{
+  _gdk_color_state_unref (self);
+}
+
+/**
+ * gdk_color_state_get_srgb:
+ *
+ * Returns the color state object representing the sRGB color space.
+ *
+ * Since: 4.16
+ */
+GdkColorState *
+gdk_color_state_get_srgb (void)
+{
+  return GDK_COLOR_STATE_SRGB;
+}
+
+/**
+ * gdk_color_state_get_srgb_linear:
+ *
+ * Returns the color state object representing the linearized sRGB color space.
+ *
+ * Since: 4.16
+ */
+GdkColorState *
+gdk_color_state_get_srgb_linear (void)
+{
+  return GDK_COLOR_STATE_SRGB_LINEAR;
+}
+
+/**
+ * gdk_color_state_equal:
+ * @self: a `GdkColorState`
+ * @other: another `GdkColorStatee`
+ *
+ * Compares two `GdkColorStates` for equality.
+ *
+ * Note that this function is not guaranteed to be perfect and two objects
+ * describing the same color state may compare not equal. However, different
+ * color states will never compare equal.
+ *
+ * Returns: %TRUE if the two color states compare equal
+ *
+ * Since: 4.16
+ */
+gboolean
+(gdk_color_state_equal) (GdkColorState *self,
+                         GdkColorState *other)
+{
+  return _gdk_color_state_equal (self, other);
+}
+
+/* }}} */
+/* {{{ Default implementation */
+
+const char *
+gdk_color_state_get_name_from_id (GdkColorStateId id)
+{
+  const char *names[] = {
+    "srgb", "srgb-linear",
+  };
+
+  return names[id];
+}
+
+GdkColorState gdk_default_color_states[] = {
+  { NULL, 0 },
+  { NULL, 0 },
+};
+
+/* }}} */
+/* {{{ Private API */
+
+const char *
+gdk_color_state_get_name (GdkColorState *self)
+{
+  if (GDK_IS_DEFAULT_COLOR_STATE (self))
+    {
+      switch (GDK_DEFAULT_COLOR_STATE_ID (self))
+        {
+        case GDK_COLOR_STATE_ID_SRGB: return "srgb";
+        case GDK_COLOR_STATE_ID_SRGB_LINEAR: return "srgb-linear";
+        default: g_assert_not_reached ();
+        }
+    }
+
+  return self->klass->get_name (self);
+}
+
+void
+gdk_color_state_print (GdkColorState *self,
+                       GString       *string)
+{
+  g_string_append (string, gdk_color_state_get_name (self));
+}
+
+GdkMemoryDepth
+gdk_color_state_get_min_depth (GdkColorState *self)
+{
+  if (self == GDK_COLOR_STATE_SRGB || self == GDK_COLOR_STATE_SRGB_LINEAR)
+    return GDK_MEMORY_U8;
+
+  return GDK_MEMORY_FLOAT16;
+}
+
+/* }}} */
+
+/* vim:set foldmethod=marker expandtab: */
