8f9d9a600d6776f6bc6e27f328bf61b29d210d4c
Matthias Clasen <mclasen@redhat.com>
2024-06-08 21:33:56 -0400

    gpu: Pass compositing color states
    Make the node processor and the pattern writer track the current
    compositing color state. Color state nodes change it. We pass
    the surface color state down via the frame apis.
    
    The name of the variable is "ccs" for "compositing color space". It's an
    unused variable name and it's common enough to deserve a short and sweet
    name.

--- gsk/gpu/gskgpuframe.c
+++ gsk/gpu/gskgpuframe.c
@@ -561,6 +561,7 @@ copy_texture (gpointer    user_data,
 static void
 gsk_gpu_frame_record_rect (GskGpuFrame                 *self,
                            GskGpuImage                 *target,
+                           GdkColorState               *target_color_state,
                            const cairo_rectangle_int_t *clip,
                            GskRenderNode               *node,
                            const graphene_rect_t       *viewport)
@@ -572,6 +573,7 @@ gsk_gpu_frame_record_rect (GskGpuFrame                 *self,
 
   gsk_gpu_node_processor_process (self,
                                   target,
+                                  target_color_state,
                                   clip,
                                   node,
                                   viewport);
@@ -585,6 +587,7 @@ static void
 gsk_gpu_frame_record (GskGpuFrame            *self,
                       gint64                  timestamp,
                       GskGpuImage            *target,
+                      GdkColorState          *target_color_state,
                       const cairo_region_t   *clip,
                       GskRenderNode          *node,
                       const graphene_rect_t  *viewport,
@@ -603,13 +606,14 @@ gsk_gpu_frame_record (GskGpuFrame            *self,
           cairo_rectangle_int_t rect;
 
           cairo_region_get_rectangle (clip, i, &rect);
-          gsk_gpu_frame_record_rect (self, target, &rect, node, viewport);
+          gsk_gpu_frame_record_rect (self, target, target_color_state, &rect, node, viewport);
         }
     }
   else
     {
       gsk_gpu_frame_record_rect (self,
                                  target,
+                                 target_color_state,
                                  &(cairo_rectangle_int_t) {
                                      0, 0,
                                      gsk_gpu_image_get_width (target),
@@ -656,6 +660,7 @@ void
 gsk_gpu_frame_render (GskGpuFrame            *self,
                       gint64                  timestamp,
                       GskGpuImage            *target,
+                      GdkColorState          *target_color_state,
                       const cairo_region_t   *region,
                       GskRenderNode          *node,
                       const graphene_rect_t  *viewport,
@@ -663,7 +668,7 @@ gsk_gpu_frame_render (GskGpuFrame            *self,
 {
   gsk_gpu_frame_cleanup (self);
 
-  gsk_gpu_frame_record (self, timestamp, target, region, node, viewport, texture);
+  gsk_gpu_frame_record (self, timestamp, target, target_color_state, region, node, viewport, texture);
 
   gsk_gpu_frame_submit (self);
 }
