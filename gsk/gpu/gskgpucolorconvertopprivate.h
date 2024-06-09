ba78e8c4237053de948ad260d9563ca966def026
Matthias Clasen <mclasen@redhat.com>
2024-06-09 00:42:33 -0400

    gpu: Add a color convert shader
    This shader converts between two color states, by using the
    same functions that we use on the cpu. The conversion to perform
    is passed as an integer encoding the pair of color states.
    
    For now, the only possible color states are srgb and srgb-linear.

---
+++ gsk/gpu/gskgpucolorconvertopprivate.h
@@ -0,0 +1,21 @@
+#pragma once
+
+#include "gskgpushaderopprivate.h"
+
+#include <graphene.h>
+
+G_BEGIN_DECLS
+
+void                    gsk_gpu_color_convert_op                        (GskGpuFrame                    *frame,
+                                                                         GskGpuShaderClip                clip,
+                                                                         GdkColorState                  *from,
+                                                                         GdkColorState                  *to,
+                                                                         GskGpuDescriptors              *desc,
+                                                                         guint32                         descriptor,
+                                                                         const graphene_rect_t          *rect,
+                                                                         const graphene_point_t         *offset,
+                                                                         const graphene_rect_t          *tex_rect);
+
+
+G_END_DECLS
+
--- gsk/gpu/shaders/color.glsl
+++ gsk/gpu/shaders/color.glsl
@@ -19,4 +19,75 @@ luminance (vec3 color)
   return dot (vec3 (0.2126, 0.7152, 0.0722), color);
 }
 
+float
+srgb_transfer_function (float v)
+{
+  if (v >= 0.04045)
+    return pow (((v + 0.055)/(1.0 + 0.055)), 2.4);
+  else
+    return v / 12.92;
+}
+
+float
+srgb_inverse_transfer_function (float v)
+{
+  if (v > 0.0031308)
+    return 1.055 * pow (v, 1.0/2.4) - 0.055;
+  else
+    return 12.92 * v;
+}
+
+vec4
+srgb_to_linear_srgb (vec4 color)
+{
+  return vec4 (srgb_transfer_function (color.r),
+               srgb_transfer_function (color.g),
+               srgb_transfer_function (color.b),
+               color.a);
+}
+
+vec4
+linear_srgb_to_srgb (vec4 color)
+{
+  return vec4 (srgb_inverse_transfer_function (color.r),
+               srgb_inverse_transfer_function (color.g),
+               srgb_inverse_transfer_function (color.b),
+               color.a);
+}
+
+
+#define RGB         0u
+#define LINRGB      1u
+
+#define pair(u,v) ((u)|((v) << 16))
+#define first(p) ((p) & 0xffffu)
+#define second(p) ((p) >> 16)
+
+bool
+do_conversion (in vec4 color, in uint f, out vec4 result)
+{
+  switch (f)
+    {
+    case pair (RGB, LINRGB): result = srgb_to_linear_srgb (color); break;
+    case pair (LINRGB, RGB): result = linear_srgb_to_srgb (color); break;
+
+    default: return false;
+    }
+
+  return true;
+}
+
+vec4
+color_convert (in vec4 color, in uint f)
+{
+  vec4 result;
+
+  if (f == 0u)
+    return color;
+
+  do_conversion (color, f, result);
+
+  return result;
+}
+
 #endif /* _COLOR_ */
