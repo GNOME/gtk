6f5c6108583b951eeb5bc0ccc52eaa42befa2b9b
Matthias Clasen <mclasen@redhat.com>
2024-06-21 18:15:16 -0400

    gsk: Give up automatic font subsetting
    Despite my best effort, it seems impossible to make ci and local
    builds agree on what font subsetter and fonts to use, so make this
    opt-in for now: If you want to produce a node file with embedded
    fonts, set GSK_SUBSET_FONTS=1.

--- gsk/gskrendernodeparser.c
+++ gsk/gskrendernodeparser.c
@@ -2990,8 +2990,13 @@ printer_init_collect_font_info (Printer       *printer,
       info->face = hb_face_reference (hb_font_get_face (pango_font_get_hb_font (font)));
       if (!g_object_get_data (G_OBJECT (pango_font_get_font_map (font)), "font-files"))
         {
-          info->input = hb_subset_input_create_or_fail ();
-          hb_subset_input_set_flags (info->input, HB_SUBSET_FLAGS_RETAIN_GIDS);
+          if (g_strcmp0 (g_getenv ("GSK_SUBSET_FONTS"), "1") == 0)
+            {
+              info->input = hb_subset_input_create_or_fail ();
+              hb_subset_input_set_flags (info->input, HB_SUBSET_FLAGS_RETAIN_GIDS);
+            }
+          else
+            info->serialized = TRUE; /* Don't subset (or serialize) system fonts */
         }
 
       g_hash_table_insert (printer->fonts, info->face, info);
