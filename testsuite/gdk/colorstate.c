e1d693d290f5a881cc71021d01318683bc9c2ad7
Matthias Clasen <mclasen@redhat.com>
2024-06-28 17:40:51 -0400

    Add tests for gdk_color_state_has_srgb_tf

--- testsuite/gdk/colorstate.c
+++ testsuite/gdk/colorstate.c
@@ -1,4 +1,5 @@
-#include <gdk/gdk.h>
+#include <gtk/gtk.h>
+#include <gdk/gdkcolorstateprivate.h>
 
 static void
 test_srgb (void)
@@ -14,12 +15,46 @@ test_srgb (void)
   g_assert_false (gdk_color_state_equal (srgb, srgb_linear));
 }
 
+static void
+test_srgb_tf (void)
+{
+  GdkColorState *srgb;
+  GdkColorState *srgb_linear;
+  GdkColorState *no_srgb;
+  gboolean ret;
+  guint saved_debug_flags = _gdk_debug_flags;
+
+  _gdk_debug_flags |= GDK_DEBUG_LINEAR;
+
+  srgb = gdk_color_state_get_srgb ();
+  srgb_linear = gdk_color_state_get_srgb_linear ();
+
+  ret = gdk_color_state_has_srgb_tf (srgb, &no_srgb);
+  g_assert_true (ret);
+  g_assert_true (gdk_color_state_equal (no_srgb, srgb_linear));
+
+  ret = gdk_color_state_has_srgb_tf (srgb_linear, &no_srgb);
+  g_assert_false (ret);
+
+  _gdk_debug_flags &= ~GDK_DEBUG_LINEAR;
+
+  ret = gdk_color_state_has_srgb_tf (srgb, &no_srgb);
+  g_assert_false (ret);
+
+  ret = gdk_color_state_has_srgb_tf (srgb_linear, &no_srgb);
+  g_assert_false (ret);
+
+  _gdk_debug_flags = saved_debug_flags;
+}
+
 int
 main (int argc, char *argv[])
 {
+  gtk_init ();
   (g_test_init) (&argc, &argv, NULL);
 
   g_test_add_func ("/colorstate/srgb", test_srgb);
+  g_test_add_func ("/colorstate/srgb-tf", test_srgb_tf);
 
   return g_test_run ();
 }
