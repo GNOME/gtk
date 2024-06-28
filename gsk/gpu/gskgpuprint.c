e2ccf69b5016d48f86dab4cbbc4f7004ead41185
Matthias Clasen <mclasen@redhat.com>
2024-06-28 17:14:59 -0400

    gsk: Show srgb information in verbose output
    Show which offscreens are using an srgb format.

--- gsk/gpu/gskgpuprint.c
+++ gsk/gpu/gskgpuprint.c
@@ -140,9 +140,10 @@ void
 gsk_gpu_print_image (GString     *string,
                      GskGpuImage *image)
 {
-  g_string_append_printf (string, "%zux%zu %s ",
+  g_string_append_printf (string, "%zux%zu %s%s ",
                           gsk_gpu_image_get_width (image),
                           gsk_gpu_image_get_height (image),
+                          gsk_gpu_image_get_flags (image) & GSK_GPU_IMAGE_SRGB ? "S" : "",
                           gdk_memory_format_get_name (gsk_gpu_image_get_format (image)));
 }
 
