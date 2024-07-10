ca12e72da23c1851c4e999bf19307105c3e41db7
Matthias Clasen <mclasen@redhat.com>
2024-07-09 22:21:20 -0400

    subsurface: Fix a precondition
    When checking that the texture fits the area, take the transform
    into account.

--- gdk/gdksubsurface.c
+++ gdk/gdksubsurface.c
@@ -156,8 +156,8 @@ gdk_subsurface_attach (GdkSubsurface         *subsurface,
   g_return_val_if_fail (GDK_IS_TEXTURE (texture), FALSE);
   g_return_val_if_fail (source != NULL &&
                         gsk_rect_contains_rect (&GRAPHENE_RECT_INIT (0, 0,
-                                                                     gdk_texture_get_width (texture),
-                                                                     gdk_texture_get_height (texture)),
+                                                                     gdk_dihedral_swaps_xy (transform) ? gdk_texture_get_height (texture) : gdk_texture_get_width (texture),
+                                                                     gdk_dihedral_swaps_xy (transform) ? gdk_texture_get_width (texture) : gdk_texture_get_height (texture)),
                                                 source), FALSE);
   g_return_val_if_fail (dest != NULL, FALSE);
   g_return_val_if_fail (sibling != subsurface, FALSE);
