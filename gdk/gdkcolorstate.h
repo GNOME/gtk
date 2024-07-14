ee7ba54a7f44372c6c938429c5853fde10cc19b6
Matthias Clasen <mclasen@redhat.com>
2024-07-14 12:52:56 -0400

    colorstate: Add autoptr definitions
    A little convenience never hurts.

--- gdk/gdkcolorstate.h
+++ gdk/gdkcolorstate.h
@@ -53,4 +53,6 @@ GDK_AVAILABLE_IN_4_16
 gboolean        gdk_color_state_equal                   (GdkColorState  *self,
                                                          GdkColorState  *other);
 
+G_DEFINE_AUTOPTR_CLEANUP_FUNC(GdkColorState, gdk_color_state_unref);
+
 G_END_DECLS
