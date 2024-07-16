GNOME/gtk

commit 2031ec1b57a891b800b062fb52a8a6f84ce9a9e8

inspector: Show the color management protocol

gtk/inspector/general.c
@@ -67,6 +67,7 @@
 #include <epoxy/egl.h>
 #include <xkbcommon/xkbcommon.h>
 #include "wayland/gdkdisplay-wayland.h"
+#include "wayland/gdkwaylandcolor-private.h"
 #endif
 
 #ifdef GDK_WINDOWING_BROADWAY
@@ -695,6 +696,7 @@ add_wayland_protocols (GdkDisplay          *display,
       append_wayland_protocol_row (gen, (struct wl_proxy *)d->viewporter);
       append_wayland_protocol_row (gen, (struct wl_proxy *)d->presentation);
       append_wayland_protocol_row (gen, (struct wl_proxy *)d->single_pixel_buffer);
+      append_wayland_protocol_row (gen, gdk_wayland_color_get_color_manager (d->color));
     }
 }
 #endif

commit d448e12cf4cc35c8ec57b4ea11af1ef510f262d1

inspector: Fix a crash
gdk_wayland_color_get_color_manager() rightfully is not NULL-safe.

Fixes: 2031ec1b57 ("inspector: Show the color management protocol")

gtk/inspector/general.c
@@ -696,7 +696,8 @@ add_wayland_protocols (GdkDisplay          *display,
       append_wayland_protocol_row (gen, (struct wl_proxy *)d->viewporter);
       append_wayland_protocol_row (gen, (struct wl_proxy *)d->presentation);
       append_wayland_protocol_row (gen, (struct wl_proxy *)d->single_pixel_buffer);
-      append_wayland_protocol_row (gen, gdk_wayland_color_get_color_manager (d->color));
+      if (d->color)
+        append_wayland_protocol_row (gen, gdk_wayland_color_get_color_manager (d->color));
     }
 }
 #endif