8d87ed1a6aa0aae4b6a82852bf90d39877ebbee1
Matthias Clasen <mclasen@redhat.com>
2024-07-16 20:19:39 -0400

    Add tests
    Add some more texture conversion roundtrips. They are currently
    ifdefed out, since they need cicp api.
    
    Also add another test binary for internal tests.

--- testsuite/gdk/colorstate.c
+++ testsuite/gdk/colorstate.c
@@ -66,6 +66,9 @@ image_distance (const guchar *data,
                 gsize         height)
 {
   float dist = 0;
+  gsize imax = 0, jmax = 0;
+  float r1 = 0, g1 = 0, b1 = 0, a1 = 0;
+  float r2 = 0, g2 = 0, b2 = 0, a2 = 0;
 
   for (gsize i = 0; i < height; i++)
     {
@@ -81,8 +84,29 @@ image_distance (const guchar *data,
           db = p[4 * j + 2] - p2[4 * j + 2];
           da = p[4 * j + 3] - p2[4 * j + 3];
 
-          dist = MAX (dist, dr * dr + dg * dg + db * db + da * da);
+          float d = dr * dr + dg * dg + db * db + da * da;
+          if (d > dist)
+            {
+              dist = d;
+              imax = i;
+              jmax = j;
+              r1 = p[4+j+0];
+              g1 = p[4+j+1];
+              b1 = p[4+j+2];
+              a1 = p[4+j+3];
+              r2 = p2[4+j+0];
+              g2 = p2[4+j+1];
+              b2 = p2[4+j+2];
+              a2 = p2[4+j+3];
+            }
         }
+    }
+
+  if (g_test_verbose() &&
+      dist > 0)
+    {
+      g_print ("worst pixel %lu %lu: %f %f %f %f  vs  %f %f %f %f\n",
+               imax, jmax, r1, g1, b1, a1, r2, g2, b2, a2);
     }
 
   return sqrt (dist);
@@ -142,7 +166,8 @@ test_convert (gconstpointer testdata)
   g_bytes_unref (bytes2);
 
   /* Download the data of the new texture in the original texture's colorstate.
-   * This does the reverse conversion. */
+   * This does the reverse conversion.
+   */
   gdk_texture_downloader_set_texture (downloader, texture2);
   gdk_texture_downloader_set_color_state (downloader, gdk_texture_get_color_state (texture));
   bytes2 = gdk_texture_downloader_download_bytes (downloader, &stride2);
@@ -151,7 +176,8 @@ test_convert (gconstpointer testdata)
   /* Check that the conversions produce pixels that are close enough */
   g_assert_cmpfloat (image_distance (data, stride, data2, stride2, width, height), <, 0.001);
 
-  g_test_message ("%g\n", image_distance (data, stride, data2, stride2, width, height));
+  if (g_test_verbose ())
+    g_test_message ("%g\n", image_distance (data, stride, data2, stride2, width, height));
 
   g_bytes_unref (bytes2);
   g_bytes_unref (bytes);
@@ -199,6 +225,47 @@ test_png (gconstpointer testdata)
   g_object_unref (loaded);
 }
 
+static void
+test_cicp (void)
+{
+  GdkCicpParams *params;
+  GdkColorState *cs;
+  GError *error = NULL;
+  GdkCicpParams *params2;
+
+  params = gdk_cicp_params_new ();
+
+  g_assert_true (gdk_cicp_params_get_color_primaries (params) == 2);
+  g_assert_true (gdk_cicp_params_get_transfer_function (params) == 2);
+  g_assert_true (gdk_cicp_params_get_matrix_coefficients (params) == 2);
+  g_assert_true (gdk_cicp_params_get_range (params) == GDK_CICP_RANGE_NARROW);
+
+  cs = gdk_cicp_params_build_color_state (params, &error);
+
+  g_assert_null (cs);
+  g_assert_error (error, G_IO_ERROR, G_IO_ERROR_FAILED);
+
+  g_clear_error (&error);
+
+  gdk_cicp_params_set_color_primaries (params, 5);
+  gdk_cicp_params_set_transfer_function (params, 1);
+  gdk_cicp_params_set_matrix_coefficients (params, 0);
+  gdk_cicp_params_set_range (params, GDK_CICP_RANGE_FULL);
+
+  cs = gdk_cicp_params_build_color_state (params, &error);
+
+  g_assert_nonnull (cs);
+  g_assert_no_error (error);
+
+  params2 = gdk_color_state_create_cicp_params (cs);
+
+  g_assert_true (gdk_cicp_params_get_color_primaries (params) == gdk_cicp_params_get_color_primaries (params2));
+  g_object_unref (params2);
+
+  gdk_color_state_unref (cs);
+  g_object_unref (params);
+}
+
 int
 main (int argc, char *argv[])
 {
@@ -207,6 +274,7 @@ main (int argc, char *argv[])
   (g_test_init) (&argc, &argv, NULL);
 
   g_test_add_func ("/colorstate/equal", test_equal);
+  g_test_add_func ("/colorstate/cicp", test_cicp);
 
   for (i = 0; ; i++)
     {
@@ -219,11 +287,51 @@ main (int argc, char *argv[])
         break;
 
       test_name = g_strdup_printf ("/colorstate/convert/srgb/%s", cs_name);
-      g_test_add_data_func_full (test_name, csi, test_convert, (GDestroyNotify) gdk_color_state_unref);
+      g_test_add_data_func_full (test_name, gdk_color_state_ref (csi), test_convert, (GDestroyNotify) gdk_color_state_unref);
       g_free (test_name);
       test_name = g_strdup_printf ("/colorstate/png/%s", cs_name);
-      g_test_add_data_func_full (test_name, csi, test_png, (GDestroyNotify) gdk_color_state_unref);
+      g_test_add_data_func_full (test_name, gdk_color_state_ref (csi), test_png, (GDestroyNotify) gdk_color_state_unref);
       g_free (test_name);
+
+      gdk_color_state_unref (csi);
+    }
+
+  GdkCicpParams *params = gdk_cicp_params_new ();
+  for (guint primaries = 0; primaries < 32; primaries++)
+    {
+      gdk_cicp_params_set_color_primaries (params, primaries);
+      for (guint tf = 0; tf < 32; tf++)
+        {
+          gdk_cicp_params_set_transfer_function (params, tf);
+          for (guint matrix = 0; matrix < 32; matrix++)
+            {
+              GdkColorState *color_state;
+
+              gdk_cicp_params_set_matrix_coefficients (params, matrix);
+              gdk_cicp_params_set_range (params, GDK_CICP_RANGE_NARROW);
+              color_state = gdk_cicp_params_build_color_state (params, NULL);
+
+              if (color_state)
+                {
+                  char *test_name = g_strdup_printf ("/colorstate/convert/srgb/cicp/%u/%u/%u/0", primaries, tf, matrix);
+                  g_test_add_data_func_full (test_name, gdk_color_state_ref (color_state), test_convert, (GDestroyNotify) gdk_color_state_unref);
+                  g_free (test_name);
+
+                  gdk_color_state_unref (color_state);
+                }
+
+              gdk_cicp_params_set_range (params, GDK_CICP_RANGE_FULL);
+              color_state = gdk_cicp_params_build_color_state (params, NULL);
+              if (color_state)
+                {
+                  char *test_name = g_strdup_printf ("/colorstate/convert/srgb/cicp/%u/%u/%u/1", primaries, tf, matrix);
+                  g_test_add_data_func_full (test_name, gdk_color_state_ref (color_state), test_convert, (GDestroyNotify) gdk_color_state_unref);
+                  g_free (test_name);
+
+                  gdk_color_state_unref (color_state);
+                }
+            }
+        }
     }
 
   return g_test_run ();
