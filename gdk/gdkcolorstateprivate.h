1a188e11713f51456a00df1a087126d10f7169a1
Matthias Clasen <mclasen@redhat.com>
2024-07-25 08:55:29 -0600

    colorstate: Add yuv support
    Add support for some of the yuv-related cicp tuples.
    
    Concretely, this adds bt601, bt709 and bt2020, both in their
    narrow and full-range variants.

--- gdk/gdkcolorstateprivate.h
+++ gdk/gdkcolorstateprivate.h
@@ -211,3 +211,10 @@ gdk_color_state_from_rgba (GdkColorState *self,
                                  out_color);
 }
 
+#ifndef GDK_COLOR_STATE_IMPL
+extern GdkColorState gdk_color_state_bt601_narrow;
+extern GdkColorState gdk_color_state_bt601_full;
+#endif
+
+#define GDK_COLOR_STATE_YUV  ((GdkColorState *) &gdk_color_state_bt601_narrow)
+#define GDK_COLOR_STATE_JPEG ((GdkColorState *) &gdk_color_state_bt601_full)
