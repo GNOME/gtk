ad679187d3f98e91872bf1be928eb33f243a9608
Simon McVittie <smcv@debian.org>
2024-07-27 19:27:13 +0100

    gsk, testsuite: Avoid undefined behaviour in float_to_half_one()
    If, for example, e == 0, it is undefined behaviour to compute an
    expression involving an out-of-range shift by (125 - e), even if the
    result is in fact irrelevant because it's going to be multiplied by 0.
    
    This was already fixed for the memorytexture test in
    commit 5d1b839 "testsuite: Fix another ubsan warning", so use the
    implementation from that test everywhere. It's in the header as an
    inline function to keep the linking of the relevant tests simple:
    its only caller in production code is fp16.c, so there will be no
    duplication outside the test suite.
    
    Detected by running a subset of the test suite with
    -Dsanitize=address,undefined on x86_64.
    
    Signed-off-by: Simon McVittie <smcv@debian.org>

--- gsk/gl/fp16private.h
+++ gsk/gl/fp16private.h
@@ -28,6 +28,26 @@ G_BEGIN_DECLS
 #define FP16_ONE ((guint16)15360)
 #define FP16_MINUS_ONE ((guint16)48128)
 
+static inline guint16
+float_to_half_one (const float x)
+{
+  const guint b = *(guint*)&x+0x00001000; // round-to-nearest-even
+  const guint e = (b&0x7F800000)>>23; // exponent
+  const guint m = b&0x007FFFFF; // mantissa
+  guint n0 = 0;
+  guint n1 = 0;
+  guint n2 = 0;
+
+  if (e > 112)
+    n0 = (((e - 112) << 10) & 0x7C00) | m >> 13;
+  if (e < 113 && e > 101)
+    n1 = (((0x007FF000 + m) >> (125- e)) + 1) >> 1;
+  if (e > 143)
+    n2 = 0x7FFF;
+
+  return (b & 0x80000000) >> 16 | n0 | n1 | n2; // sign : normalized : denormalized : saturate
+}
+
 void float_to_half4 (const float f[4],
                      guint16     h[4]);
 
