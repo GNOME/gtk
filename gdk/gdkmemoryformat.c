30af3effb5092d57af7737414ccdf217edd77657
Benjamin Otte <otte@redhat.com>
2024-09-01 21:39:29 +0200

    memoryformat: Parallelize
    Refactor code so that it uses g_parallel_task_run().

--- gdk/gdkmemoryformat.c
+++ gdk/gdkmemoryformat.c
@@ -24,6 +24,7 @@
 #include "gdkdmabuffourccprivate.h"
 #include "gdkglcontextprivate.h"
 #include "gdkcolorstateprivate.h"
+#include "gdkparalleltaskprivate.h"
 #include "gtk/gtkcolorutilsprivate.h"
 
 #include "gsk/gl/fp16private.h"
