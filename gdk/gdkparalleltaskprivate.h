d4ba57fcc34aa5b4caee370733d2cf56c6a445bb
Benjamin Otte <otte@redhat.com>
2024-09-02 02:35:37 +0200

    gdk: Add gdk_parallel_task_run()
    This is just the API. Users will come later.
    
    I considered putting it into gdkmemoryformat.c because it's likely gonna
    be the only user and this one function is so little code, but it didn't
    fit at all.
    So now it's a new file.

---
+++ gdk/gdkparalleltaskprivate.h
@@ -0,0 +1,32 @@
+/*
+ * Copyright © 2024 Benjamin Otte
+ *
+ * This library is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Lesser General Public
+ * License as published by the Free Software Foundation; either
+ * version 2.1 of the License, or (at your option) any later version.
+ *
+ * This library is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public
+ * License along with this library. If not, see <http://www.gnu.org/licenses/>.
+ *
+ * Authors: Benjamin Otte <otte@gnome.org>
+ */
+
+#pragma once
+
+#include <glib.h>
+
+G_BEGIN_DECLS
+
+typedef void (* GdkTaskFunc) (gpointer user_data);
+
+void                    gdk_parallel_task_run               (GdkTaskFunc                 task_func,
+                                                             gpointer                    task_data);
+
+G_END_DECLS
+
