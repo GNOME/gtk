62723f9f9b89e5ad53ac0be665209fabac2ad706
Arjan Molenaar <gaphor@gmail.com>
2024-09-03 22:42:23 +0200

    macos: Fix input source popup placement in popovers

--- gtk/gtkimcontextquartz.c
+++ gtk/gtkimcontextquartz.c
@@ -318,7 +318,7 @@ quartz_set_cursor_location (GtkIMContext *context, GdkRectangle *area)
   if (!qc->focused)
     return;
 
-  surface_widget = GTK_WIDGET (gdk_surface_get_widget (qc->client_surface));
+  surface_widget = GTK_WIDGET (gtk_widget_get_native (qc->client_widget));
 
   if (!surface_widget)
     return;
