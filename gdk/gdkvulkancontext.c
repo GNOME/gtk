157e60247fed62294f0051ec34c2be599b3cf104
Matthias Clasen <mclasen@redhat.com>
2024-09-09 09:57:35 -0400

    vulkan context: Prefix statics
    This is a step towards enabling unity builds for gdk.

--- gdk/gdkvulkancontext.c
+++ gdk/gdkvulkancontext.c
@@ -88,9 +88,9 @@ struct _GdkVulkanContextPrivate {
 };
 
 enum {
-  IMAGES_UPDATED,
+  GDK_VULKAN_CONTEXT_IMAGES_UPDATED,
 
-  LAST_SIGNAL
+  GDK_VULKAN_CONTEXT_LAST_SIGNAL
 };
 
 /**
@@ -102,7 +102,7 @@ enum {
  **/
 G_DEFINE_QUARK (gdk-vulkan-error-quark, gdk_vulkan_error)
 
-static guint signals[LAST_SIGNAL] = { 0 };
+static guint gdk_vulkan_context_signals[GDK_VULKAN_CONTEXT_LAST_SIGNAL] = { 0 };
 
 static void gdk_vulkan_context_initable_init (GInitableIface *iface);
 
@@ -538,7 +538,7 @@ gdk_vulkan_context_check_swapchain (GdkVulkanContext  *context,
       return FALSE;
     }
 
-  g_signal_emit (context, signals[IMAGES_UPDATED], 0);
+  g_signal_emit (context, gdk_vulkan_context_signals[GDK_VULKAN_CONTEXT_IMAGES_UPDATED], 0);
 
   return res == VK_SUCCESS;
 }
@@ -804,7 +804,7 @@ gdk_vulkan_context_class_init (GdkVulkanContextClass *klass)
    * Usually this means that the swapchain had to be recreated,
    * for example in response to a change of the surface size.
    */
-  signals[IMAGES_UPDATED] =
+  gdk_vulkan_context_signals[GDK_VULKAN_CONTEXT_IMAGES_UPDATED] =
     g_signal_new (g_intern_static_string ("images-updated"),
 		  G_OBJECT_CLASS_TYPE (gobject_class),
                   G_SIGNAL_RUN_LAST,
@@ -2025,7 +2025,7 @@ gdk_display_get_vk_shader_module (GdkDisplay *self,
 static void
 gdk_vulkan_context_class_init (GdkVulkanContextClass *klass)
 {
-  signals[IMAGES_UPDATED] =
+  gdk_vulkan_context_signals[GDK_VULKAN_CONTEXT_IMAGES_UPDATED] =
     g_signal_new (g_intern_static_string ("images-updated"),
 		  G_OBJECT_CLASS_TYPE (klass),
                   G_SIGNAL_RUN_LAST,
