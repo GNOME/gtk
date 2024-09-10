a84fe6bde62e6093c0ecf1a77a0b3df65e814b72
Arjan Molenaar <gaphor@gmail.com>
2024-09-10 22:10:48 +0200

    macos: Put DnD windows on status level
    This will make sure the dragged window will stay visible.

--- gdk/macos/gdkmacosdragsurface.c
+++ gdk/macos/gdkmacosdragsurface.c
@@ -101,6 +101,7 @@ _gdk_macos_drag_surface_constructed (GObject *object)
   [window setOpaque:NO];
   [window setBackgroundColor:[NSColor clearColor]];
   [window setDecorated:NO];
+  [window setLevel:NSStatusWindowLevel];
 
   frame_clock = _gdk_frame_clock_idle_new ();
   gdk_surface_set_frame_clock (surface, frame_clock);
