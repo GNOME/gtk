GNOME/gtk

commit 65c8320a3223ce13dc3e358a50d0c3c738b4963b

gpu: Fix fd leak in GL dmabuf export
The texture ID is not deleted on dmabuf export; a copy is made, the
GskGpuImage retains ownership.

However when doing GL export, the texture *does* take ownership, so we
need the stealing semantics for that case.

gsk/gpu/gskglimage.c
@@ -46,7 +46,7 @@ gsk_gl_image_finalize (GObject *object)
   if (self->texture_id && self->framebuffer_id)
     glDeleteFramebuffers (1, &self->framebuffer_id);
 
-  if (self->owns_texture)
+  if (gsk_gpu_image_get_flags (GSK_GPU_IMAGE (self)) & GSK_GPU_IMAGE_TOGGLE_REF)
     glDeleteTextures (1, &self->texture_id);
 
   G_OBJECT_CLASS (gsk_gl_image_parent_class)->finalize (object);
@@ -336,17 +336,17 @@ gsk_gl_image_get_gl_type (GskGLImage *self)
 }
 
 GLuint
-gsk_gl_image_steal_texture (GskGLImage *self)
+gsk_gl_image_get_texture_id (GskGLImage *self)
 {
-  g_assert (self->owns_texture);
-
-  if (self->framebuffer_id)
-    {
-      glDeleteFramebuffers (1, &self->framebuffer_id);
-      self->framebuffer_id = 0;
-    }
-
-  self->owns_texture = FALSE;
-
   return self->texture_id;
 }
+
+void
+gsk_gl_image_steal_texture_ownership (GskGLImage *self)
+{
+  g_assert (self->texture_id);
+  g_assert (self->owns_texture);
+
+  self->owns_texture = FALSE;
+}
+

gsk/gpu/gskglimageprivate.h
@@ -39,6 +39,7 @@ GLint                   gsk_gl_image_get_gl_internal_format             (GskGLIm
 GLenum                  gsk_gl_image_get_gl_format                      (GskGLImage             *self);
 GLenum                  gsk_gl_image_get_gl_type                        (GskGLImage             *self);
 
-GLuint                  gsk_gl_image_steal_texture                      (GskGLImage             *self);
+GLuint                  gsk_gl_image_get_texture_id                     (GskGLImage             *self);
+void                    gsk_gl_image_steal_texture_ownership            (GskGLImage             *self);
 
 G_END_DECLS

gsk/gpu/gskgpudownloadop.c
@@ -300,7 +300,7 @@ gsk_gpu_download_op_gl_command (GskGpuOp          *op,
   /* Don't use the renderer context, the texture might survive the frame
    * and its surface */
   context = gdk_display_get_gl_context (gsk_gpu_device_get_display (gsk_gpu_frame_get_device (frame)));
-  texture_id = gsk_gl_image_steal_texture (GSK_GL_IMAGE (self->image));
+  texture_id = gsk_gl_image_get_texture_id (GSK_GL_IMAGE (self->image));
 
 #ifdef HAVE_DMABUF
   if (self->allow_dmabuf)
@@ -350,6 +350,9 @@ gsk_gpu_download_op_gl_command (GskGpuOp          *op,
   self->texture = gdk_gl_texture_builder_build (builder,
                                                 gsk_gl_texture_data_free,
                                                 data);
+
+  gsk_gpu_image_toggle_ref_texture (self->image, self->texture);
+  gsk_gl_image_steal_texture_ownership (GSK_GL_IMAGE (self->image));
 
   g_object_unref (builder);
 

commit dc1b2d411787a562cf66f10f6f67068ec2ec980b

gsk: Don't leak gl textures
We need to fix all the textures we own. This broke in 65c8320a.

Fixes: #7013

gsk/gpu/gskglimage.c
@@ -46,7 +46,7 @@ gsk_gl_image_finalize (GObject *object)
   if (self->texture_id && self->framebuffer_id)
     glDeleteFramebuffers (1, &self->framebuffer_id);
 
-  if (gsk_gpu_image_get_flags (GSK_GPU_IMAGE (self)) & GSK_GPU_IMAGE_TOGGLE_REF)
+  if (self->owns_texture)
     glDeleteTextures (1, &self->texture_id);
 
   G_OBJECT_CLASS (gsk_gl_image_parent_class)->finalize (object);