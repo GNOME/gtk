8d5660b4ab90b5e30be2824552f3ffbe84172c69
Benjamin Otte <otte@redhat.com>
2024-09-25 01:55:51 +0200

    demo: Fix argument name

--- demos/gtk-demo/nodewidget.h
+++ demos/gtk-demo/nodewidget.h
@@ -5,4 +5,4 @@
 #define NODE_TYPE_WIDGET (node_widget_get_type ())
 G_DECLARE_FINAL_TYPE (NodeWidget, node_widget, NODE, WIDGET, GtkWidget)
 
-GtkWidget * node_widget_new       (const char *file);
+GtkWidget * node_widget_new       (const char *resource);
