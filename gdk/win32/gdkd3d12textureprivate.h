e3565e0ccd6cb2fb45f2250a6e734628b489ed09
Benjamin Otte <otte@redhat.com>
2024-10-31 14:32:38 +0100

    win32: Add gdk_d3d12_texture_get_handle()
    Makes shared HANDLE handling part of the texture. This handle will be
    needed when importing into GL or Vulkan or using elsewhere.
    Its lifetime is bound to the texture and so is it's use,
    so it's best to maintain it here instead of requiring renderers to
    deal with HANDLEs.

--- gdk/win32/gdkd3d12textureprivate.h
+++ gdk/win32/gdkd3d12textureprivate.h
@@ -6,10 +6,12 @@
 
 G_BEGIN_DECLS
 
-GdkTexture *            gdk_d3d12_texture_new_from_builder  (GdkD3D12TextureBuilder *builder,
-                                                             GDestroyNotify           destroy,
-                                                             gpointer                 data,
-                                                             GError                 **error);
+GdkTexture *            gdk_d3d12_texture_new_from_builder              (GdkD3D12TextureBuilder *builder,
+                                                                         GDestroyNotify          destroy,
+                                                                         gpointer                data,
+                                                                         GError                **error);
+
+HANDLE                  gdk_d3d12_texture_get_resource_handle           (GdkD3D12Texture        *self);
 
 G_END_DECLS
 
