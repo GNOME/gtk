255e677a8c7f7fbcc03efb0821a0a4c4337f5ab1
Benjamin Otte <otte@redhat.com>
2024-11-07 22:00:39 +0100

    win32: Add (private) getters for fences and the HANDLE
    We will need those when using them in Vulkan or GL.

--- gdk/win32/gdkd3d12textureprivate.h
+++ gdk/win32/gdkd3d12textureprivate.h
@@ -13,6 +13,9 @@ GdkTexture *            gdk_d3d12_texture_new_from_builder              (GdkD3D1
 
 ID3D12Resource *        gdk_d3d12_texture_get_resource                  (GdkD3D12Texture        *self);
 HANDLE                  gdk_d3d12_texture_get_resource_handle           (GdkD3D12Texture        *self);
+ID3D12Fence *           gdk_d3d12_texture_get_fence                     (GdkD3D12Texture        *self);
+HANDLE                  gdk_d3d12_texture_get_fence_handle              (GdkD3D12Texture        *self);
+guint64                 gdk_d3d12_texture_get_fence_wait                (GdkD3D12Texture        *self);
 
 guint                   gdk_d3d12_texture_import_gl                     (GdkD3D12Texture        *self,
                                                                          GdkGLContext           *context,
