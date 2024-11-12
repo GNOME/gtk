d84d16018fc93aaad0b0fc90f34017b3bdfdd4a2
correctmost <136447-correctmost@users.noreply.gitlab.gnome.org>
2024-11-12 18:33:10 -0500

    updateiconcache: Fix GOptionContext leak in main

--- gtk/updateiconcache.c
+++ gtk/updateiconcache.c
@@ -1719,6 +1719,7 @@ main (int argc, char **argv)
   g_option_context_add_main_entries (context, args, GETTEXT_PACKAGE);
 
   g_option_context_parse (context, &argc, &argv, NULL);
+  g_option_context_free (context);
 
   path = argv[1];
 #ifdef G_OS_WIN32
