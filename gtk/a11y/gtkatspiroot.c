e188f5ff2033d09f84c48bcda9ec7653ff0d058a
Philip Withnall <pwithnall@gnome.org>
2024-11-23 15:26:45 +0000

    gtkatspiroot: Fix invalid free() call
    `g_variant_get ("as")` gives a heap-allocated `GVariantIter` pointer,
    not a `GStrv`, so the free function being used here was wrong. However,
    the `event_types` variable wasn’t actually read at all, so it’s better
    to just drop it completely.
    
    This fixes heap corruption seen in valgrind when using GTK with
    assistive technologies (such as a screen reader) turned on.
    
    Signed-off-by: Philip Withnall <pwithnall@gnome.org>

--- gtk/a11y/gtkatspiroot.c
+++ gtk/a11y/gtkatspiroot.c
@@ -504,7 +504,6 @@ on_event_listener_registered (GDBusConnection *connection,
     {
       char *sender = NULL;
       char *event_name = NULL;
-      char **event_types = NULL;
       unsigned int *count;
 
       if (self->event_listeners == NULL)
@@ -512,7 +511,7 @@ on_event_listener_registered (GDBusConnection *connection,
                                                        g_free,
                                                        g_free);
 
-      g_variant_get (parameters, "(ssas)", &sender, &event_name, &event_types);
+      g_variant_get (parameters, "(ssas)", &sender, &event_name, NULL);
 
       count = g_hash_table_lookup (self->event_listeners, sender);
       if (count == NULL)
@@ -535,7 +534,6 @@ on_event_listener_registered (GDBusConnection *connection,
         }
 
       g_free (event_name);
-      g_strfreev (event_types);
     }
 }
 
