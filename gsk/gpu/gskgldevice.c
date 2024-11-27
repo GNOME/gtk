GNOME/gtk

commit b154c9caf4c7b93e528f3aef38ff58212c452fef

ngL: Use a single buffer for globals
Instead of using glBuffer(Sub)Data() every time we set new globals, fill
a single buffer with all the globals and use glBindBufferRange() on that
buffer to set the current globals.

gsk/gpu/gskgpuframe.c
@@ -6,6 +6,7 @@
 #include "gskgpucacheprivate.h"
 #include "gskgpudeviceprivate.h"
 #include "gskgpudownloadopprivate.h"
+#include "gskgpuglobalsopprivate.h"
 #include "gskgpuimageprivate.h"
 #include "gskgpunodeprocessorprivate.h"
 #include "gskgpuopprivate.h"
@@ -23,6 +24,8 @@
 
 /* GL_MAX_UNIFORM_BLOCK_SIZE is at 16384 */
 #define DEFAULT_STORAGE_BUFFER_SIZE 16 * 1024 * 64
+
+#define DEFAULT_N_GLOBALS (16384 / sizeof (GskGpuGlobalsInstance))
 
 #define GDK_ARRAY_NAME gsk_gpu_ops
 #define GDK_ARRAY_TYPE_NAME GskGpuOps
@@ -47,6 +50,9 @@ struct _GskGpuFramePrivate
   GskGpuBuffer *vertex_buffer;
   guchar *vertex_buffer_data;
   gsize vertex_buffer_used;
+  GskGpuBuffer *globals_buffer;
+  GskGpuGlobalsInstance *globals_buffer_data;
+  gsize n_globals;
   GskGpuBuffer *storage_buffer;
   guchar *storage_buffer_data;
   gsize storage_buffer_used;
@@ -65,6 +71,8 @@ gsk_gpu_frame_default_cleanup (GskGpuFrame *self)
   GskGpuFramePrivate *priv = gsk_gpu_frame_get_instance_private (self);
   GskGpuOp *op;
   gsize i;
+
+  priv->n_globals = 0;
 
   for (i = 0; i < gsk_gpu_ops_get_size (&priv->ops); i += op->op_class->size)
     {
@@ -139,6 +147,7 @@ gsk_gpu_frame_finalize (GObject *object)
   gsk_gpu_ops_clear (&priv->ops);
 
   g_clear_object (&priv->vertex_buffer);
+  g_clear_object (&priv->globals_buffer);
   g_clear_object (&priv->storage_buffer);
 
   g_object_unref (priv->device);
@@ -506,6 +515,13 @@ gsk_gpu_frame_create_vertex_buffer (GskGpuFrame *self,
 }
 
 static GskGpuBuffer *
+gsk_gpu_frame_create_globals_buffer (GskGpuFrame *self,
+                                     gsize        size)
+{
+  return GSK_GPU_FRAME_GET_CLASS (self)->create_globals_buffer (self, size);
+}
+
+static GskGpuBuffer *
 gsk_gpu_frame_create_storage_buffer (GskGpuFrame *self,
                                      gsize        size)
 {
@@ -558,6 +574,48 @@ gsk_gpu_frame_reserve_vertex_data (GskGpuFrame *self,
   priv->vertex_buffer_used = size_needed;
 
   return size_needed - size;
+}
+
+gsize
+gsk_gpu_frame_add_globals (GskGpuFrame                 *self,
+                           const GskGpuGlobalsInstance *globals)
+{
+  GskGpuFramePrivate *priv = gsk_gpu_frame_get_instance_private (self);
+  gsize size_needed, result;
+
+  if (priv->globals_buffer == NULL)
+    {
+      priv->globals_buffer = gsk_gpu_frame_create_globals_buffer (self, sizeof (GskGpuGlobalsInstance) * DEFAULT_N_GLOBALS);
+      if (priv->globals_buffer == NULL)
+        return 0;
+    }
+  if (priv->globals_buffer_data == NULL)
+    priv->globals_buffer_data = (GskGpuGlobalsInstance *) gsk_gpu_buffer_map (priv->globals_buffer);
+
+  size_needed = sizeof (GskGpuGlobalsInstance) * (priv->n_globals + 1);
+
+  if (gsk_gpu_buffer_get_size (priv->globals_buffer) < size_needed)
+    {
+      gsize old_size = gsk_gpu_buffer_get_size (priv->globals_buffer);
+      GskGpuBuffer *new_buffer = gsk_gpu_frame_create_globals_buffer (self, old_size * 2);
+      GskGpuGlobalsInstance *new_data = (GskGpuGlobalsInstance *) gsk_gpu_buffer_map (new_buffer);
+
+      if (priv->globals_buffer_data)
+        {
+          memcpy (new_data, priv->globals_buffer_data, old_size);
+          gsk_gpu_buffer_unmap (priv->globals_buffer, old_size);
+        }
+      g_object_unref (priv->globals_buffer);
+      priv->globals_buffer = new_buffer;
+      priv->globals_buffer_data = new_data;
+    }
+
+  result = priv->n_globals;
+
+  priv->globals_buffer_data[priv->n_globals] = *globals;
+  priv->n_globals++;
+
+  return result;
 }
 
 guchar *
@@ -691,6 +749,12 @@ gsk_gpu_frame_submit (GskGpuFrame       *self,
       priv->vertex_buffer_used = 0;
     }
 
+  if (priv->globals_buffer)
+    {
+      gsk_gpu_buffer_unmap (priv->globals_buffer, sizeof (GskGpuGlobalsInstance) * priv->n_globals);
+      priv->globals_buffer_data = NULL;
+    }
+
   if (priv->storage_buffer_data)
     {
       gsk_gpu_buffer_unmap (priv->storage_buffer, priv->storage_buffer_used);
@@ -701,6 +765,7 @@ gsk_gpu_frame_submit (GskGpuFrame       *self,
   GSK_GPU_FRAME_GET_CLASS (self)->submit (self,
                                           pass_type,
                                           priv->vertex_buffer,
+                                          priv->globals_buffer,
                                           priv->first_op);
 }
 

gsk/gpu/gskgpuglobalsop.c
@@ -2,7 +2,7 @@
 
 #include "gskgpuglobalsopprivate.h"
 
-#include "gskglframeprivate.h"
+#include "gskglbufferprivate.h"
 #include "gskgpuframeprivate.h"
 #include "gskgpuprintprivate.h"
 #include "gskroundedrectprivate.h"
@@ -17,6 +17,7 @@ struct _GskGpuGlobalsOp
 {
   GskGpuOp op;
 
+  gsize id;
   GskGpuGlobalsInstance instance;
 };
 
@@ -67,13 +68,10 @@ gsk_gpu_globals_op_gl_command (GskGpuOp          *op,
 {
   GskGpuGlobalsOp *self = (GskGpuGlobalsOp *) op;
 
-  gsk_gl_frame_bind_globals (GSK_GL_FRAME (frame));
-
-  /* FIXME: Does it matter if we glBufferData() or glSubBufferData() here? */
-  glBufferSubData (GL_UNIFORM_BUFFER,
-                   0,
-                   sizeof (self->instance),
-                   &self->instance);
+  gsk_gl_buffer_bind_range (GSK_GL_BUFFER (state->globals),
+                            0,
+                            self->id * sizeof (GskGpuGlobalsInstance),
+                            sizeof (GskGpuGlobalsInstance));
 
   return op->next;
 }
@@ -102,4 +100,5 @@ gsk_gpu_globals_op (GskGpuFrame             *frame,
   graphene_matrix_to_float (mvp, self->instance.mvp);
   gsk_rounded_rect_to_float (clip, graphene_point_zero (), self->instance.clip);
   graphene_vec2_to_float (scale, self->instance.scale);
+  self->id = gsk_gpu_frame_add_globals (frame, &self->instance);
 }

gsk/gpu/gskglbuffer.c
@@ -63,10 +63,16 @@ gsk_gl_buffer_bind (GskGLBuffer *self)
 }
 
 void
-gsk_gl_buffer_bind_base (GskGLBuffer *self,
-                         GLuint       index)
+gsk_gl_buffer_bind_range (GskGLBuffer *self,
+                          GLuint       index,
+                          GLintptr     offset,
+                          GLsizeiptr   size)
 {
-  glBindBufferBase (self->target, index, self->buffer_id);
+  glBindBufferRange (self->target,
+                     index,
+                     self->buffer_id,
+                     offset,
+                     size);
 }
 
 static void

gsk/gpu/gskglbufferprivate.h
@@ -20,8 +20,10 @@ GskGpuBuffer *          gsk_gl_copied_buffer_new                        (GLenum 
                                                                          gsize                   size);
 
 void                    gsk_gl_buffer_bind                              (GskGLBuffer            *self);
-void                    gsk_gl_buffer_bind_base                         (GskGLBuffer            *self,
-                                                                         GLuint                  index);
+void                    gsk_gl_buffer_bind_range                        (GskGLBuffer            *self,
+                                                                         GLuint                  index,
+                                                                         GLintptr                offset,
+                                                                         GLsizeiptr              size);
 
 G_END_DECLS
 

gsk/gpu/gskglframe.c
@@ -17,7 +17,6 @@ struct _GskGLFrame
 {
   GskGpuFrame parent_instance;
 
-  GLuint globals_buffer_id;
   guint next_texture_slot;
   GLsync sync;
 
@@ -143,6 +142,17 @@ gsk_gl_frame_create_vertex_buffer (GskGpuFrame *frame,
 }
 
 static GskGpuBuffer *
+gsk_gl_frame_create_globals_buffer (GskGpuFrame *frame,
+                                    gsize        size)
+{
+  if (gdk_gl_context_has_feature (GDK_GL_CONTEXT (gsk_gpu_frame_get_context (frame)),
+                                  GDK_GL_FEATURE_BUFFER_STORAGE))
+    return gsk_gl_mapped_buffer_new (GL_UNIFORM_BUFFER, size);
+  else
+    return gsk_gl_copied_buffer_new (GL_UNIFORM_BUFFER, size);
+}
+
+static GskGpuBuffer *
 gsk_gl_frame_create_storage_buffer (GskGpuFrame *frame,
                                     gsize        size)
 {
@@ -166,12 +176,14 @@ static void
 gsk_gl_frame_submit (GskGpuFrame       *frame,
                      GskRenderPassType  pass_type,
                      GskGpuBuffer      *vertex_buffer,
+                     GskGpuBuffer      *globals_buffer,
                      GskGpuOp          *op)
 {
   GskGLFrame *self = GSK_GL_FRAME (frame);
   GskGLCommandState state = {
     /* rest is 0 */
-    .current_samplers = { GSK_GPU_SAMPLER_N_SAMPLERS, GSK_GPU_SAMPLER_N_SAMPLERS }
+    .current_samplers = { GSK_GPU_SAMPLER_N_SAMPLERS, GSK_GPU_SAMPLER_N_SAMPLERS },
+    .globals = globals_buffer,
   };
 
   glEnable (GL_SCISSOR_TEST);
@@ -181,12 +193,6 @@ gsk_gl_frame_submit (GskGpuFrame       *frame,
 
   if (vertex_buffer)
     gsk_gl_buffer_bind (GSK_GL_BUFFER (vertex_buffer));
-
-  gsk_gl_frame_bind_globals (self);
-  glBufferData (GL_UNIFORM_BUFFER,
-                sizeof (GskGpuGlobalsInstance),
-                NULL,
-                GL_STREAM_DRAW);
 
   while (op)
     {
@@ -202,8 +208,6 @@ gsk_gl_frame_finalize (GObject *object)
   GskGLFrame *self = GSK_GL_FRAME (object);
 
   g_hash_table_unref (self->vaos);
-  if (self->globals_buffer_id != 0)
-    glDeleteBuffers (1, &self->globals_buffer_id);
 
   G_OBJECT_CLASS (gsk_gl_frame_parent_class)->finalize (object);
 }
@@ -219,6 +223,7 @@ gsk_gl_frame_class_init (GskGLFrameClass *klass)
   gpu_frame_class->cleanup = gsk_gl_frame_cleanup;
   gpu_frame_class->upload_texture = gsk_gl_frame_upload_texture;
   gpu_frame_class->create_vertex_buffer = gsk_gl_frame_create_vertex_buffer;
+  gpu_frame_class->create_globals_buffer = gsk_gl_frame_create_globals_buffer;
   gpu_frame_class->create_storage_buffer = gsk_gl_frame_create_storage_buffer;
   gpu_frame_class->write_texture_vertex_data = gsk_gl_frame_write_texture_vertex_data;
   gpu_frame_class->submit = gsk_gl_frame_submit;
@@ -264,14 +269,5 @@ gsk_gl_frame_use_program (GskGLFrame                *self,
   op_class->setup_vao (0);
 
   g_hash_table_insert (self->vaos, (gpointer) op_class, GUINT_TO_POINTER (vao));
-}
-
-void
-gsk_gl_frame_bind_globals (GskGLFrame *self)
-{
-  if (self->globals_buffer_id == 0)
-    glGenBuffers (1, &self->globals_buffer_id);
-
-  glBindBufferBase (GL_UNIFORM_BUFFER, 0, self->globals_buffer_id);
 }
 

gsk/gpu/gskglframeprivate.h
@@ -14,6 +14,4 @@ void                    gsk_gl_frame_use_program                        (GskGLFr
                                                                          GskGpuColorStates       color_states,
                                                                          guint32                 variation);
 
-void                    gsk_gl_frame_bind_globals                       (GskGLFrame             *self);
-
 G_END_DECLS

gsk/gpu/gskgpuframeprivate.h
@@ -39,6 +39,8 @@ struct _GskGpuFrameClass
                                                                          GdkTexture             *texture);
   GskGpuBuffer *        (* create_vertex_buffer)                        (GskGpuFrame            *self,
                                                                          gsize                   size);
+  GskGpuBuffer *        (* create_globals_buffer)                       (GskGpuFrame            *self,
+                                                                         gsize                   size);
   GskGpuBuffer *        (* create_storage_buffer)                       (GskGpuFrame            *self,
                                                                          gsize                   size);
   void                  (* write_texture_vertex_data)                   (GskGpuFrame            *self,
@@ -49,6 +51,7 @@ struct _GskGpuFrameClass
   void                  (* submit)                                      (GskGpuFrame            *self,
                                                                          GskRenderPassType       pass_type,
                                                                          GskGpuBuffer           *vertex_buffer,
+                                                                         GskGpuBuffer           *globals_buffer,
                                                                          GskGpuOp               *op);
 };
 
@@ -92,6 +95,8 @@ void                    gsk_gpu_frame_write_texture_vertex_data         (GskGpuF
                                                                          GskGpuImage           **images,
                                                                          GskGpuSampler          *samplers,
                                                                          gsize                   n_images);
+gsize                   gsk_gpu_frame_add_globals                       (GskGpuFrame            *self,
+                                                                         const GskGpuGlobalsInstance *globals);
 GskGpuBuffer *          gsk_gpu_frame_write_storage_buffer              (GskGpuFrame            *self,
                                                                          const guchar           *data,
                                                                          gsize                   size,

gsk/gpu/gskgpuglobalsopprivate.h
@@ -7,14 +7,16 @@
 
 G_BEGIN_DECLS
 
-typedef struct _GskGpuGlobalsInstance GskGpuGlobalsInstance;
-
 struct _GskGpuGlobalsInstance
 {
   float mvp[16];
   float clip[12];
   float scale[2];
+  float padding[2];
 };
+
+/* GPUs often want 32bit alignment */
+G_STATIC_ASSERT (sizeof (GskGpuGlobalsInstance) % 32 == 0);
 
 void                    gsk_gpu_globals_op                              (GskGpuFrame                    *frame,
                                                                          const graphene_vec2_t          *scale,

commit d8496d66c5a4db86ab75a746b7b16059ddc2985f

ngl: Conform to buffer offset alignment requirements
When using glMapBufferRange() for our globals we need to make sure the offset is a
multiple of GL_UNIFORM_BUFFER_OFFSET_ALIGNMENT.

Since we've only been doing that for globals and GskGlobalsInstace is 128 bytes,
this is usually the case. But it is valid to require 256 bytes
alignment, and in those cases we used to fail.

The case that discovered this was an old card using nouveau, but it may
also affect some Applie hardware.

Fixes: b154c9caf4c7b93e528f3aef38ff58212c452fef
Fixes: #7180
Related: #7053

gsk/gpu/gskgldevice.c
@@ -235,7 +235,7 @@ gsk_gl_device_get_for_display (GdkDisplay  *display,
 {
   GskGLDevice *self;
   GdkGLContext *context;
-  GLint max_texture_size;
+  GLint max_texture_size, globals_alignment;
 
   self = g_object_get_data (G_OBJECT (display), "-gsk-gl-device");
   if (self)
@@ -259,10 +259,13 @@ gsk_gl_device_get_for_display (GdkDisplay  *display,
   gdk_gl_context_make_current (context);
 
   glGetIntegerv (GL_MAX_TEXTURE_SIZE, &max_texture_size);
+  glGetIntegerv (GL_UNIFORM_BUFFER_OFFSET_ALIGNMENT, &globals_alignment);
+
   gsk_gpu_device_setup (GSK_GPU_DEVICE (self),
                         display,
                         max_texture_size,
-                        GSK_GPU_DEVICE_DEFAULT_TILE_SIZE);
+                        GSK_GPU_DEVICE_DEFAULT_TILE_SIZE,
+                        globals_alignment);
 
   self->version_string = gdk_gl_context_get_glsl_version_string (context);
   self->api = gdk_gl_context_get_api (context);

gsk/gpu/gskgpudevice.c
@@ -3,6 +3,7 @@
 #include "gskgpudeviceprivate.h"
 
 #include "gskgpucacheprivate.h"
+#include "gskgpuglobalsopprivate.h"
 
 #include "gdk/gdkprofilerprivate.h"
 
@@ -17,6 +18,7 @@ struct _GskGpuDevicePrivate
   GdkDisplay *display;
   gsize max_image_size;
   gsize tile_size;
+  gsize globals_aligned_size;
 
   GskGpuCache *cache; /* we don't own a ref, but manage the cache */
   guint cache_gc_source;
@@ -143,11 +145,19 @@ static void
 gsk_gpu_device_init (GskGpuDevice *self)
 {
 }
+
+static inline gsize
+round_up (gsize number, gsize divisor)
+{
+  return (number + divisor - 1) / divisor * divisor;
+}
+
 void
 gsk_gpu_device_setup (GskGpuDevice *self,
                       GdkDisplay   *display,
                       gsize         max_image_size,
-                      gsize         tile_size)
+                      gsize         tile_size,
+                      gsize         globals_alignment)
 {
   GskGpuDevicePrivate *priv = gsk_gpu_device_get_instance_private (self);
   const char *str;
@@ -156,6 +166,7 @@ gsk_gpu_device_setup (GskGpuDevice *self,
   priv->max_image_size = max_image_size;
   priv->tile_size = tile_size;
   priv->cache_timeout = CACHE_TIMEOUT;
+  priv->globals_aligned_size = round_up (sizeof (GskGpuGlobalsInstance), globals_alignment);
 
   str = g_getenv ("GSK_CACHE_TIMEOUT");
   if (str != NULL)
@@ -244,6 +255,25 @@ gsk_gpu_device_get_tile_size (GskGpuDevice *self)
   GskGpuDevicePrivate *priv = gsk_gpu_device_get_instance_private (self);
 
   return priv->tile_size;
+}
+
+/*<private>
+ * gsk_gpu_device_get_globals_aligned_size:
+ * @self: a device
+ *
+ * The required size for allocating arrays of globals.
+ * This value will be at least sizeof (GskGpuGlobalsInstance) but due to constraints
+ * of how buffers are mapped, it might be larger to allow a single buffer to hold
+ * all the globals instances.
+ *
+ * Returns: The minimum aligned size for a GskGpuGlobalsInstance
+ **/
+gsize
+gsk_gpu_device_get_globals_aligned_size (GskGpuDevice *self)
+{
+  GskGpuDevicePrivate *priv = gsk_gpu_device_get_instance_private (self);
+
+  return priv->globals_aligned_size;
 }
 
 /**

gsk/gpu/gskgpudeviceprivate.h
@@ -52,13 +52,15 @@ GType                   gsk_gpu_device_get_type                         (void) G
 void                    gsk_gpu_device_setup                            (GskGpuDevice           *self,
                                                                          GdkDisplay             *display,
                                                                          gsize                   max_image_size,
-                                                                         gsize                   tile_size);
+                                                                         gsize                   tile_size,
+                                                                         gsize                   globals_alignment);
 void                    gsk_gpu_device_maybe_gc                         (GskGpuDevice           *self);
 void                    gsk_gpu_device_queue_gc                         (GskGpuDevice           *self);
 GdkDisplay *            gsk_gpu_device_get_display                      (GskGpuDevice           *self);
 GskGpuCache *           gsk_gpu_device_get_cache                        (GskGpuDevice           *self);
 gsize                   gsk_gpu_device_get_max_image_size               (GskGpuDevice           *self);
 gsize                   gsk_gpu_device_get_tile_size                    (GskGpuDevice           *self);
+gsize                   gsk_gpu_device_get_globals_aligned_size         (GskGpuDevice           *self);
 
 GskGpuImage *           gsk_gpu_device_create_offscreen_image           (GskGpuDevice           *self,
                                                                          gboolean                with_mipmap,

gsk/gpu/gskgpuframe.c
@@ -25,8 +25,7 @@
 
 /* GL_MAX_UNIFORM_BLOCK_SIZE is at 16384 */
 #define DEFAULT_STORAGE_BUFFER_SIZE 16 * 1024 * 64
-
-#define DEFAULT_N_GLOBALS (16384 / sizeof (GskGpuGlobalsInstance))
+#define DEFAULT_N_GLOBALS_SIZE 16384
 
 #define GDK_ARRAY_NAME gsk_gpu_ops
 #define GDK_ARRAY_TYPE_NAME GskGpuOps
@@ -52,7 +51,7 @@ struct _GskGpuFramePrivate
   guchar *vertex_buffer_data;
   gsize vertex_buffer_used;
   GskGpuBuffer *globals_buffer;
-  GskGpuGlobalsInstance *globals_buffer_data;
+  guchar *globals_buffer_data;
   gsize n_globals;
   GskGpuBuffer *storage_buffer;
   guchar *storage_buffer_data;
@@ -582,24 +581,26 @@ gsk_gpu_frame_add_globals (GskGpuFrame                 *self,
                            const GskGpuGlobalsInstance *globals)
 {
   GskGpuFramePrivate *priv = gsk_gpu_frame_get_instance_private (self);
-  gsize size_needed, result;
+  gsize size_needed, globals_size, result;
+
+  globals_size = gsk_gpu_device_get_globals_aligned_size (priv->device);
 
   if (priv->globals_buffer == NULL)
     {
-      priv->globals_buffer = gsk_gpu_frame_create_globals_buffer (self, sizeof (GskGpuGlobalsInstance) * DEFAULT_N_GLOBALS);
+      priv->globals_buffer = gsk_gpu_frame_create_globals_buffer (self, DEFAULT_N_GLOBALS_SIZE);
       if (priv->globals_buffer == NULL)
         return 0;
     }
   if (priv->globals_buffer_data == NULL)
-    priv->globals_buffer_data = (GskGpuGlobalsInstance *) gsk_gpu_buffer_map (priv->globals_buffer);
+    priv->globals_buffer_data = gsk_gpu_buffer_map (priv->globals_buffer);
 
-  size_needed = sizeof (GskGpuGlobalsInstance) * (priv->n_globals + 1);
+  size_needed = globals_size * (priv->n_globals + 1);
 
   if (gsk_gpu_buffer_get_size (priv->globals_buffer) < size_needed)
     {
       gsize old_size = gsk_gpu_buffer_get_size (priv->globals_buffer);
       GskGpuBuffer *new_buffer = gsk_gpu_frame_create_globals_buffer (self, old_size * 2);
-      GskGpuGlobalsInstance *new_data = (GskGpuGlobalsInstance *) gsk_gpu_buffer_map (new_buffer);
+      guchar *new_data = gsk_gpu_buffer_map (new_buffer);
 
       if (priv->globals_buffer_data)
         {
@@ -613,7 +614,7 @@ gsk_gpu_frame_add_globals (GskGpuFrame                 *self,
 
   result = priv->n_globals;
 
-  priv->globals_buffer_data[priv->n_globals] = *globals;
+  *((GskGpuGlobalsInstance *) (priv->globals_buffer_data + priv->n_globals * globals_size)) = *globals;
   priv->n_globals++;
 
   return result;

gsk/gpu/gskgpuglobalsop.c
@@ -67,10 +67,13 @@ gsk_gpu_globals_op_gl_command (GskGpuOp          *op,
                                GskGLCommandState *state)
 {
   GskGpuGlobalsOp *self = (GskGpuGlobalsOp *) op;
+  gsize globals_size;
+
+  globals_size = gsk_gpu_device_get_globals_aligned_size (gsk_gpu_frame_get_device (frame));
 
   gsk_gl_buffer_bind_range (GSK_GL_BUFFER (state->globals),
                             0,
-                            self->id * sizeof (GskGpuGlobalsInstance),
+                            self->id * globals_size,
                             sizeof (GskGpuGlobalsInstance));
 
   return op->next;

gsk/gpu/gskvulkandevice.c
@@ -407,7 +407,8 @@ gsk_vulkan_device_setup (GskVulkanDevice *self,
   gsk_gpu_device_setup (GSK_GPU_DEVICE (self),
                         display,
                         vk_props.properties.limits.maxImageDimension2D,
-                        GSK_GPU_DEVICE_DEFAULT_TILE_SIZE);
+                        GSK_GPU_DEVICE_DEFAULT_TILE_SIZE,
+                        vk_props.properties.limits.minUniformBufferOffsetAlignment);
 }
 
 GskGpuDevice *