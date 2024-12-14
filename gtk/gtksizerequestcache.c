GNOME/gtk

commit 3e7ec4f9baf871b4113e0765857a4bb1a5dfa3d6

sizerequestcache: Warn on contradictory measurements
When committing the measurement for a widget to the cache, check if the
measurement contradicts a previous measurement in the opposite
orientation, and print a warning. Unfortunately, we don't have a
reference to the widget itself at this point in the code, so we can't
name the specific widget; nevertheless it should be easy to break on the
wanring and inspect the stack trace to see which measurement has
triggered the warning.

This helps debug complex layout issues by catching the contradictory
measurements in the deepest / most nested widget that reports them,
rather than in a some ancestor GtkBox that happens to break because its
children's measurements are contradictory.

Signed-off-by: Sergey Bugaev <bugaevc@gmail.com>

gtk/gtksizerequestcache.c
@@ -118,6 +118,35 @@ _gtk_size_request_cache_commit (SizeRequestCache *cache,
       SizeRequestX **cached_sizes = cache->requests_x;
       SizeRequestX  *cached_size;
 
+#ifdef G_ENABLE_DEBUG
+      if (cache->flags[GTK_ORIENTATION_HORIZONTAL].cached_size_valid &&
+          minimum_size < cache->cached_size_x.minimum_size)
+        g_warning ("Widget reports min width of %d for height of %d, but overall min width of %d",
+                   minimum_size, for_size, cache->cached_size_x.minimum_size);
+
+      for (i = 0; i < cache->flags[GTK_ORIENTATION_VERTICAL].n_cached_requests; i++)
+        {
+          if (cache->requests_y[i]->upper_for_size >= minimum_size &&
+              cache->requests_y[i]->cached_size.minimum_size > for_size)
+            {
+              g_warning ("Widget reports min width of %d for height of %d, but min height of %d for width of %d",
+                         minimum_size, for_size,
+                         cache->requests_y[i]->cached_size.minimum_size,
+                         cache->requests_y[i]->upper_for_size);
+              break;
+            }
+          else if (cache->requests_y[i]->lower_for_size < minimum_size &&
+                   cache->requests_y[i]->cached_size.minimum_size <= for_size)
+            {
+              g_warning ("Widget reports min width of %d for height of %d, but min height of %d for width of %d",
+                         minimum_size, for_size,
+                         cache->requests_y[i]->cached_size.minimum_size,
+                         cache->requests_y[i]->lower_for_size);
+              break;
+            }
+        }
+#endif
+
       for (i = 0; i < n_sizes; i++)
 	{
 	  if (cached_sizes[i]->cached_size.minimum_size == minimum_size &&
@@ -159,6 +188,35 @@ _gtk_size_request_cache_commit (SizeRequestCache *cache,
     {
       SizeRequestY **cached_sizes = cache->requests_y;
       SizeRequestY  *cached_size;
+
+#ifdef G_ENABLE_DEBUG
+      if (cache->flags[GTK_ORIENTATION_VERTICAL].cached_size_valid &&
+          minimum_size < cache->cached_size_y.minimum_size)
+        g_warning ("Widget reports min height of %d for width of %d, but overall min height of %d",
+                   minimum_size, for_size, cache->cached_size_x.minimum_size);
+
+      for (i = 0; i < cache->flags[GTK_ORIENTATION_HORIZONTAL].n_cached_requests; i++)
+        {
+          if (cache->requests_x[i]->upper_for_size >= minimum_size &&
+              cache->requests_x[i]->cached_size.minimum_size > for_size)
+            {
+              g_warning ("Widget reports min height of %d for width of %d, but min width of %d for height of %d",
+                         minimum_size, for_size,
+                         cache->requests_x[i]->cached_size.minimum_size,
+                         cache->requests_x[i]->upper_for_size);
+              break;
+            }
+          else if (cache->requests_x[i]->lower_for_size < minimum_size &&
+                   cache->requests_x[i]->cached_size.minimum_size <= for_size)
+            {
+              g_warning ("Widget reports min height of %d for width of %d, but min width of %d for height of %d",
+                         minimum_size, for_size,
+                         cache->requests_x[i]->cached_size.minimum_size,
+                         cache->requests_x[i]->lower_for_size);
+              break;
+            }
+        }
+#endif
 
       for (i = 0; i < n_sizes; i++)
 	{

commit 1e45f186144f5cb19e9ac81d1dd3b27d08f19ac8

sizerequestcache: Fix a typo
The warning was benign, but a wrong size was being reported.

Fixes: 3e7ec4f9baf871b4113e0765857a4bb1a5dfa3d6
("sizerequestcache: Warn on contradictory measurements").
Signed-off-by: Sergey Bugaev <bugaevc@gmail.com>

gtk/gtksizerequestcache.c
@@ -193,7 +193,7 @@ _gtk_size_request_cache_commit (SizeRequestCache *cache,
       if (cache->flags[GTK_ORIENTATION_VERTICAL].cached_size_valid &&
           minimum_size < cache->cached_size_y.minimum_size)
         g_warning ("Widget reports min height of %d for width of %d, but overall min height of %d",
-                   minimum_size, for_size, cache->cached_size_x.minimum_size);
+                   minimum_size, for_size, cache->cached_size_y.minimum_size);
 
       for (i = 0; i < cache->flags[GTK_ORIENTATION_HORIZONTAL].n_cached_requests; i++)
         {