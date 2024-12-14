c01d8c272fe697a452fae25053f39d56407c068f
Florian "sp1rit"​ <sp1ritCS@protonmail.com>
2024-12-14 19:57:30 +0100

    gdk: make vulkancontext work with Android armv7
    From vk_platform.h:
      On Android 32-bit ARM targets, Vulkan functions use the "hardfloat"
      calling convention, i.e. float parameters are passed in registers.
      This is true even if the rest of the application passes floats on the
      stack, as it does by default when compiling for the armeabi-v7a NDK
      ABI.

--- gdk/gdkvulkancontext.c
+++ gdk/gdkvulkancontext.c
@@ -1681,7 +1681,7 @@ gdk_display_create_vulkan_device (GdkDisplay  *display,
   return FALSE;
 }
 
-static VkBool32 VKAPI_CALL
+static VKAPI_ATTR VkBool32 VKAPI_CALL
 gdk_vulkan_debug_report (VkDebugReportFlagsEXT      flags,
                          VkDebugReportObjectTypeEXT objectType,
                          uint64_t                   object,
