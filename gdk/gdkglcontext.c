8eff2852bd98c3e3339985e561856ab9f7cddc61
Val Packett <val@packett.cool>
2025-03-08 06:57:22 +0100

    egl: modifers must be an array
    Mesa expects eglExportDMABUFImageQueryMESA() to pass in an array of
    modifiers sized to the number of planes that isn't known yet.
    
    So we call the function twice: First to query the number of planes so we
    can make sure the number of values in the modifier array that we pass is
    actually large enough and we don't get an invalid write.
    
    We ignore all the modifiers but the first though because there is only
    one modifier per dmabuf and Mesa will set all of them to the same value.
    
    This will also ensure things keep working no matter what the resolution of
    the Mesa issue turns out to be.
    
    Related: https://gitlab.freedesktop.org/mesa/mesa/-/issues/12771
    Related: https://social.treehouse.systems/@valpackett/114123260205749024

--- gdk/gdkglcontext.c
+++ gdk/gdkglcontext.c
@@ -2367,7 +2367,7 @@ gdk_gl_context_export_dmabuf (GdkGLContext *self,
   int i;
   int fourcc;
   int n_planes;
-  guint64 modifier;
+  EGLuint64KHR modifiers[GDK_DMABUF_MAX_PLANES];
   int fds[GDK_DMABUF_MAX_PLANES];
   int strides[GDK_DMABUF_MAX_PLANES];
   int offsets[GDK_DMABUF_MAX_PLANES];
@@ -2408,7 +2408,7 @@ gdk_gl_context_export_dmabuf (GdkGLContext *self,
                                       image,
                                       &fourcc,
                                       &n_planes,
-                                      &modifier))
+                                      NULL))
     {
       GDK_DISPLAY_DEBUG (display, DMABUF,
                          "eglExportDMABUFImageQueryMESA failed: %#x", eglGetError ());
@@ -2419,6 +2419,17 @@ gdk_gl_context_export_dmabuf (GdkGLContext *self,
     {
       GDK_DISPLAY_DEBUG (display, DMABUF,
                          "dmabufs with %d planes are not supported", n_planes);
+      goto out;
+    }
+
+  if (!eglExportDMABUFImageQueryMESA (egl_display,
+                                      image,
+                                      &fourcc,
+                                      &n_planes,
+                                      modifiers))
+    {
+      GDK_DISPLAY_DEBUG (display, DMABUF,
+                         "eglExportDMABUFImageQueryMESA for modifiers failed: %#x", eglGetError ());
       goto out;
     }
 
@@ -2442,7 +2453,7 @@ gdk_gl_context_export_dmabuf (GdkGLContext *self,
     }
 
   dmabuf->fourcc = (guint32)fourcc;
-  dmabuf->modifier = modifier;
+  dmabuf->modifier = modifiers[0];
   dmabuf->n_planes = n_planes;
 
   for (i = 0; i < n_planes; i++)
@@ -2454,7 +2465,7 @@ gdk_gl_context_export_dmabuf (GdkGLContext *self,
 
   GDK_DISPLAY_DEBUG (display, DMABUF,
                      "Exported GL texture to dmabuf (format: %.4s:%#" G_GINT64_MODIFIER "x, planes: %d)",
-             (char *)&fourcc, modifier, n_planes);
+                     (char *)&fourcc, modifiers[0], n_planes);
 
   result = TRUE;
 
