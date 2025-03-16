GNOME/gtk

commit ab5b762abb2c0c06808a3b1151ab5ddf20c2fc32

gdk: Change GdkDrawContext.frame_region semantics
Rename it to render_region to make that clear.

Instead of in application units, the render region is now tracked in
device pixels. In details:

* gdk_draw_context_begin_frame() still takes the update_area in
  application units. That's because the whole stack up to that point
  operates in application units (invalid region tracking + rendernode
  diffing).
* gdk_draw_context_begin_frame() then converts the update area to
  device pixels.
* The GdkDrawContext.begin_frame() vfunc takes the frame_region in
  device pixels now. Just like before, this region can be larger than
  the surface bounds and the vfunc unions any damage it tracks (which
  can also be larger) with that region.
* All damage tracking regions (in EGL and Vulkan) now track damage in
  device pixels.
* Cairo surfaces no longer set a device scale. The scaling is done by
  gdk_cairo_context_cairo_create() as the last step. In particular,
  this happens *after* the cairo_clip() for the damage region and that
  means that even for fractional scales, that clip is now aligned to
  the device pixel grid and the Cairo renderer should no longer have
  seams.
* The GPU renderer has been updated to not scale the damage region
  anymore. The Cairo renderer did not need any updates.
* GdkDrawContext.end_frame() now also takes the painted region in device
  pixels. This means in particular that EGL and Vulkan no longer need
  to scale the region when passing it to SwapBuffers().

The benefits of tracking the damage region in device pixels are:

* For hidpi, we have more granularity.
* For fractional scales, the damage region is aligned with the pixel
  grid.
* On Wayland, scaling application damage could lead to a region larger
  than the surface (because buffer size could be rounded down). This
  would cause off-by-one issues everywhere and is now (hopefully)
  fixed.

Fixes #7314

gdk/x11/gdkcairocontext-x11.c
@@ -50,7 +50,6 @@ create_cairo_surface_for_surface (GdkSurface *surface)
                                              visual,
                                              gdk_surface_get_width (surface) * scale,
                                              gdk_surface_get_height (surface) * scale);
-  cairo_surface_set_device_scale (cairo_surface, scale, scale);
 
   return cairo_surface;
 }
@@ -65,7 +64,7 @@ gdk_x11_cairo_context_begin_frame (GdkDrawContext  *draw_context,
   GdkX11CairoContext *self = GDK_X11_CAIRO_CONTEXT (draw_context);
   GdkRectangle clip_box;
   GdkSurface *surface;
-  double sx, sy;
+  int scale;
 
   surface = gdk_draw_context_get_surface (draw_context);
   cairo_region_get_extents (region, &clip_box);
@@ -79,9 +78,9 @@ G_GNUC_BEGIN_IGNORE_DEPRECATIONS
                                                             MAX (clip_box.height, 1));
 G_GNUC_END_IGNORE_DEPRECATIONS
 
-  sx = sy = 1;
-  cairo_surface_get_device_scale (self->paint_surface, &sx, &sy);
-  cairo_surface_set_device_offset (self->paint_surface, -clip_box.x*sx, -clip_box.y*sy);
+  cairo_surface_set_device_scale (self->paint_surface, 1.0, 1.0);
+  scale = gdk_surface_get_scale_factor (surface);
+  cairo_surface_set_device_offset (self->paint_surface, -clip_box.x * scale, -clip_box.y * scale);
 
   *out_color_state = GDK_COLOR_STATE_SRGB;
   *out_depth = gdk_color_state_get_depth (GDK_COLOR_STATE_SRGB);

gdk/android/gdkandroidcairocontext.c
@@ -99,10 +99,10 @@ gdk_android_cairo_context_begin_frame (GdkDrawContext *draw_context,
       cairo_region_get_extents (region, &bounds);
 
       self->surface.bounds = (ARect){
-        .left = floorf (bounds.x * surface_impl->cfg.scale),
-        .top = floorf (bounds.y * surface_impl->cfg.scale),
-        .right = ceilf ((bounds.x + bounds.width) * surface_impl->cfg.scale),
-        .bottom = ceilf ((bounds.y + bounds.height) * surface_impl->cfg.scale)
+        .left = bounds.x,
+        .top = bounds.y,
+        .right = bounds.x + bounds.width,
+        .bottom = bounds.y + bounds.height,
       };
       gint rc = ANativeWindow_lock (self->surface.window,
                                     &self->surface.buffer,
@@ -119,17 +119,16 @@ gdk_android_cairo_context_begin_frame (GdkDrawContext *draw_context,
       g_assert (self->surface.buffer.format == AHARDWAREBUFFER_FORMAT_R8G8B8A8_UNORM);
 
       cairo_rectangle_int_t true_bounds = {
-        .x = floorf (self->surface.bounds.left / surface_impl->cfg.scale),
-        .y = floorf (self->surface.bounds.top / surface_impl->cfg.scale),
-        .width = ceilf (self->surface.bounds.right / surface_impl->cfg.scale) - floorf (self->surface.bounds.left / surface_impl->cfg.scale),
-        .height = ceilf (self->surface.bounds.bottom / surface_impl->cfg.scale) - floorf (self->surface.bounds.top / surface_impl->cfg.scale),
+        .x = self->surface.bounds.left,
+        .y = self->surface.bounds.top,
+        .width = self->surface.bounds.right - self->surface.bounds.left,
+        .height = self->surface.bounds.bottom - self->surface.bounds.top,
       };
       cairo_region_union_rectangle (region, &true_bounds);
 
       self->active_surface = cairo_image_surface_create (CAIRO_FORMAT_ARGB32,
                                                          self->surface.bounds.right - self->surface.bounds.left,
                                                          self->surface.bounds.bottom - self->surface.bounds.top);
-      cairo_surface_set_device_scale (self->active_surface, surface_impl->cfg.scale, surface_impl->cfg.scale);
       cairo_surface_set_device_offset (self->active_surface,
                                        -self->surface.bounds.left,
                                        -self->surface.bounds.top);
@@ -149,13 +148,12 @@ gdk_android_cairo_context_begin_frame (GdkDrawContext *draw_context,
                                                                  CAIRO_FORMAT_ARGB32,
                                                                  scaled_width, scaled_height,
                                                                  scaled_width*sizeof(jint));
-      cairo_surface_set_device_scale (self->active_surface, initiator->cfg.scale, initiator->cfg.scale);
 
       cairo_rectangle_int_t bounds = {
         .x = 0,
         .y = 0,
-        .width = surface->width,
-        .height = surface->height,
+        .width = scaled_width,
+        .height = scaled_height,
       };
       cairo_region_union_rectangle (region, &bounds);
     }

gdk/broadway/gdkcairocontext-broadway.c
@@ -40,17 +40,12 @@ gdk_broadway_cairo_context_begin_frame (GdkDrawContext  *draw_context,
                                         GdkMemoryDepth  *out_depth)
 {
   GdkBroadwayCairoContext *self = GDK_BROADWAY_CAIRO_CONTEXT (draw_context);
-  GdkSurface *surface = gdk_draw_context_get_surface (GDK_DRAW_CONTEXT (self));
   cairo_t *cr;
   cairo_region_t *repaint_region;
-  int width, height, scale;
+  guint width, height;
 
-  width = gdk_surface_get_width (surface);
-  height = gdk_surface_get_height (surface);
-  scale = gdk_surface_get_scale_factor (surface);
-  self->paint_surface = cairo_image_surface_create (CAIRO_FORMAT_ARGB32,
-                                                    width * scale, height * scale);
-  cairo_surface_set_device_scale (self->paint_surface, scale, scale);
+  gdk_draw_context_get_buffer_size (draw_context, &width, &height);
+  self->paint_surface = cairo_image_surface_create (CAIRO_FORMAT_ARGB32, width, height);
 
   repaint_region = cairo_region_create_rectangle (&(cairo_rectangle_int_t) { 0, 0, width, height });
   cairo_region_union (region, repaint_region);

gdk/gdkcairo.c
@@ -303,3 +303,4 @@ gdk_cairo_region_create_from_surface (cairo_surface_t *surface)
 
   return region;
 }
+

gdk/gdkcairocontext.c
@@ -25,6 +25,7 @@
 #include "gdkcairocontextprivate.h"
 
 #include "gdkcairo.h"
+#include "gdksurface.h"
 
 /**
  * GdkCairoContext:
@@ -79,6 +80,7 @@ gdk_cairo_context_cairo_create (GdkCairoContext *self)
 {
   GdkDrawContext *draw_context;
   cairo_t *cr;
+  double scale;
 
   g_return_val_if_fail (GDK_IS_CAIRO_CONTEXT (self), NULL);
 
@@ -91,8 +93,11 @@ G_GNUC_END_IGNORE_DEPRECATIONS
 
   cr = GDK_CAIRO_CONTEXT_GET_CLASS (self)->cairo_create (self);
 
-  gdk_cairo_region (cr, gdk_draw_context_get_frame_region (draw_context));
+  gdk_cairo_region (cr, gdk_draw_context_get_render_region (draw_context));
   cairo_clip (cr);
+
+  scale = gdk_surface_get_scale (gdk_draw_context_get_surface (draw_context));
+  cairo_scale (cr, scale, scale);
 
   return cr;
 }

gdk/gdkcairoprivate.h
@@ -177,3 +177,26 @@ gdk_cairo_surface_convert_color_state (cairo_surface_t *surface,
   cairo_surface_mark_dirty (surface);
 }
 
+static inline cairo_region_t *
+gdk_cairo_region_scale_grow (const cairo_region_t *region,
+                             double                scale_x,
+                             double                scale_y)
+{
+  cairo_region_t *result;
+
+  result = cairo_region_create ();
+  for (int i = 0; i < cairo_region_num_rectangles (region); i++)
+    {
+      cairo_rectangle_int_t rect;
+      cairo_region_get_rectangle (region, i, &rect);
+      cairo_region_union_rectangle (result, &(cairo_rectangle_int_t) {
+                                      .x = (int) floor (rect.x * scale_x),
+                                      .y = (int) floor (rect.y * scale_y),
+                                      .width = (int) ceil ((rect.x + rect.width) * scale_x) - floor (rect.x * scale_x),
+                                      .height = (int) ceil ((rect.y + rect.height) * scale_y) - floor (rect.y * scale_y),
+                                    });
+    }
+
+  return result;
+}
+

gdk/gdkdrawcontext.c
@@ -22,6 +22,7 @@
 
 #include "gdkdrawcontextprivate.h"
 
+#include "gdkcairoprivate.h"
 #include "gdkdebugprivate.h"
 #include <glib/gi18n-lib.h>
 #include "gdkprofilerprivate.h"
@@ -47,7 +48,7 @@ struct _GdkDrawContextPrivate {
   GdkDisplay *display;
   GdkSurface *surface;
 
-  cairo_region_t *frame_region;
+  cairo_region_t *render_region;
   GdkColorState *color_state;
   GdkMemoryDepth depth;
 };
@@ -234,7 +235,7 @@ gdk_draw_context_is_in_frame (GdkDrawContext *context)
 
   g_return_val_if_fail (GDK_IS_DRAW_CONTEXT (context), FALSE);
 
-  return priv->frame_region != NULL;
+  return priv->render_region != NULL;
 }
 
 /*< private >
@@ -364,6 +365,8 @@ gdk_draw_context_begin_frame_full (GdkDrawContext        *context,
                                    const graphene_rect_t *opaque)
 {
   GdkDrawContextPrivate *priv = gdk_draw_context_get_instance_private (context);
+  double scale;
+  guint buffer_width, buffer_height;
 
   if (GDK_SURFACE_DESTROYED (priv->surface))
     return;
@@ -391,14 +394,15 @@ gdk_draw_context_begin_frame_full (GdkDrawContext        *context,
   if (gdk_display_get_debug_flags (priv->display) & GDK_DEBUG_HIGH_DEPTH)
     depth = GDK_MEMORY_FLOAT32;
 
-  priv->frame_region = cairo_region_copy (region);
+  scale = gdk_surface_get_scale (priv->surface);
+  priv->render_region = gdk_cairo_region_scale_grow (region, scale, scale);
   priv->surface->paint_context = g_object_ref (context);
 
   g_assert (priv->color_state == NULL);
 
   GDK_DRAW_CONTEXT_GET_CLASS (context)->begin_frame (context,
                                                      depth,
-                                                     priv->frame_region,
+                                                     priv->render_region,
                                                      &priv->color_state,
                                                      &priv->depth);
 
@@ -406,10 +410,11 @@ gdk_draw_context_begin_frame_full (GdkDrawContext        *context,
   g_assert (priv->color_state != NULL);
   g_assert (priv->depth < GDK_N_DEPTHS);
 
-  cairo_region_intersect_rectangle (priv->frame_region,
+  gdk_draw_context_get_buffer_size (context, &buffer_width, &buffer_height);
+  cairo_region_intersect_rectangle (priv->render_region,
                                     &(cairo_rectangle_int_t) {
                                       0, 0,
-                                      priv->surface->width, priv->surface->height
+                                      buffer_width, buffer_height
                                     });
 }
 
@@ -437,12 +442,12 @@ gdk_draw_context_end_frame_full (GdkDrawContext *context)
 {
   GdkDrawContextPrivate *priv = gdk_draw_context_get_instance_private (context);
 
-  GDK_DRAW_CONTEXT_GET_CLASS (context)->end_frame (context, priv->frame_region);
+  GDK_DRAW_CONTEXT_GET_CLASS (context)->end_frame (context, priv->render_region);
 
-  gdk_profiler_set_int_counter (pixels_counter, region_get_pixels (priv->frame_region));
+  gdk_profiler_set_int_counter (pixels_counter, region_get_pixels (priv->render_region));
 
   priv->color_state = NULL;
-  g_clear_pointer (&priv->frame_region, cairo_region_destroy);
+  g_clear_pointer (&priv->render_region, cairo_region_destroy);
   g_clear_object (&priv->surface->paint_context);
   priv->depth = GDK_N_DEPTHS;
 }
@@ -511,19 +516,39 @@ gdk_draw_context_end_frame (GdkDrawContext *context)
  *   Use `GskRenderNode` and `GskRenderer`.
  */
 const cairo_region_t *
-_gdk_draw_context_get_frame_region (GdkDrawContext *self)
+gdk_draw_context_get_frame_region (GdkDrawContext *self)
+{
+  return NULL;
+}
+
+/*<private>
+ * gdk_draw_context_get_render_region:
+ * @self: a `GdkDrawContext`
+ *
+ * Retrieves the region that is currently being repainted.
+ *
+ * After a call to [method@Gdk.DrawContext.begin_frame] this function will
+ * return the area of the current buffer that the @context determined needs
+ * to be repainted.
+ * This region is created by a union of the region passed to
+ * [method@Gdk.DrawContext.begin_frame] converted to device pixels and any
+ * additional pixels the context has determined need to be repainted.
+ *
+ * The region will never extend the buffer's size.
+ *
+ * If @context is not in between calls to [method@Gdk.DrawContext.begin_frame]
+ * and [method@Gdk.DrawContext.end_frame], %NULL will be returned.
+ *
+ * Returns: (transfer none) (nullable): a Cairo region
+ *
+ * Returns:
+ **/
+const cairo_region_t *
+gdk_draw_context_get_render_region (GdkDrawContext *self)
 {
   GdkDrawContextPrivate *priv = gdk_draw_context_get_instance_private (self);
 
-  return priv->frame_region;
-}
-
-const cairo_region_t *
-(gdk_draw_context_get_frame_region) (GdkDrawContext *context)
-{
-  g_return_val_if_fail (GDK_IS_DRAW_CONTEXT (context), NULL);
-
-  return _gdk_draw_context_get_frame_region (context);
+  return priv->render_region;
 }
 
 /*<private>

gdk/gdkdrawcontextprivate.h
@@ -65,8 +65,7 @@ void                    gdk_draw_context_end_frame_full         (GdkDrawContext 
 
 void                    gdk_draw_context_empty_frame            (GdkDrawContext         *context);
 
-#define gdk_draw_context_get_frame_region(...) _gdk_draw_context_get_frame_region(__VA_ARGS__)
-const cairo_region_t *  _gdk_draw_context_get_frame_region      (GdkDrawContext         *self);
+const cairo_region_t *  gdk_draw_context_get_render_region      (GdkDrawContext         *self);
 GdkColorState *         gdk_draw_context_get_color_state        (GdkDrawContext         *self);
 GdkMemoryDepth          gdk_draw_context_get_depth              (GdkDrawContext         *self);
 void                    gdk_draw_context_get_buffer_size        (GdkDrawContext         *self,

commit e31fdd3fc63b5606f71013ee1be47a3afa3197c9

x11: Don't scale the already scaled offset
That was a thinko in the original commit where I forgot that the
coordinates in the clip_box are scaled now. Nothing more to say really.

Fixes: #7408
Fixes: ab5b762abb2c0c06808a3b1151ab5ddf20c2fc32

gdk/x11/gdkcairocontext-x11.c
@@ -64,7 +64,6 @@ gdk_x11_cairo_context_begin_frame (GdkDrawContext  *draw_context,
   GdkX11CairoContext *self = GDK_X11_CAIRO_CONTEXT (draw_context);
   GdkRectangle clip_box;
   GdkSurface *surface;
-  int scale;
 
   surface = gdk_draw_context_get_surface (draw_context);
   cairo_region_get_extents (region, &clip_box);
@@ -79,8 +78,7 @@ G_GNUC_BEGIN_IGNORE_DEPRECATIONS
 G_GNUC_END_IGNORE_DEPRECATIONS
 
   cairo_surface_set_device_scale (self->paint_surface, 1.0, 1.0);
-  scale = gdk_surface_get_scale_factor (surface);
-  cairo_surface_set_device_offset (self->paint_surface, -clip_box.x * scale, -clip_box.y * scale);
+  cairo_surface_set_device_offset (self->paint_surface, -clip_box.x, -clip_box.y);
 
   *out_color_state = GDK_COLOR_STATE_SRGB;
   *out_depth = gdk_color_state_get_depth (GDK_COLOR_STATE_SRGB);