4487add9c5ce71708db8e796ae9cd78149da7958
Benjamin Otte <otte@redhat.com>
2025-03-27 01:04:34 +0100

    vulkan: Fix CAN_MIPMAP flag handling
    The recent asahi mipmap fixes added the CAN_MIPMAP flag when a format
    could be used for mipmapping. That doesn't mean we want to use
    mipmapping though, both because some additional features may be missing
    to create the mipmaps or because we don't actually want to create mipmaps.
    
    So we need to turn that flag back off in those cases.
    
    Fixes c9a3cdd396c5646382612ed25e93bb5f9664d043

--- gsk/gpu/gskvulkanimage.c
+++ gsk/gpu/gskvulkanimage.c
@@ -344,10 +344,10 @@ gsk_vulkan_image_new (GskVulkanDevice           *device,
   if (gdk_memory_format_alpha (format) == GDK_MEMORY_ALPHA_STRAIGHT)
     flags |= GSK_GPU_IMAGE_STRAIGHT_ALPHA;
 
-  if (((flags & (GSK_GPU_IMAGE_FILTERABLE | GSK_GPU_IMAGE_RENDERABLE | GSK_GPU_IMAGE_NO_BLIT | GSK_GPU_IMAGE_STRAIGHT_ALPHA)) ==
-       (GSK_GPU_IMAGE_FILTERABLE | GSK_GPU_IMAGE_RENDERABLE)) &&
-      (required_flags & GSK_GPU_IMAGE_CAN_MIPMAP))
-    flags |= GSK_GPU_IMAGE_CAN_MIPMAP;
+  if (((flags & (GSK_GPU_IMAGE_FILTERABLE | GSK_GPU_IMAGE_RENDERABLE | GSK_GPU_IMAGE_NO_BLIT | GSK_GPU_IMAGE_CAN_MIPMAP | GSK_GPU_IMAGE_STRAIGHT_ALPHA)) !=
+       (GSK_GPU_IMAGE_FILTERABLE | GSK_GPU_IMAGE_RENDERABLE | GSK_GPU_IMAGE_CAN_MIPMAP)) ||
+      !(required_flags & GSK_GPU_IMAGE_CAN_MIPMAP))
+    flags &= ~GSK_GPU_IMAGE_CAN_MIPMAP;
 
   if (!gsk_component_mapping_is_framebuffer_compatible (&vk_components))
     flags |= GSK_GPU_IMAGE_NO_BLIT;
