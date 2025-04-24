99e48dfc24e6bb5c27daf9ba13fbe77520cd4250
Benjamin Otte <otte@redhat.com>
2025-04-24 03:17:41 +0200

    gsk: Add outline for D3D12 renderer
    The renderer does nothing but fail to realize. It does that on all
    platforms.
    
    And this is what it will always do on anything but Windows.

---
+++ gsk/gpu/gskd3d12deviceprivate.h
@@ -0,0 +1,20 @@
+#pragma once
+
+#include "gskgpudeviceprivate.h"
+
+#include "gskdebugprivate.h"
+
+#include "gdk/gdkdisplayprivate.h"
+
+G_BEGIN_DECLS
+
+#define GSK_TYPE_D3D12_DEVICE (gsk_d3d12_device_get_type ())
+
+G_DECLARE_FINAL_TYPE(GskD3d12Device, gsk_d3d12_device, GSK, D3D12_DEVICE, GskGpuDevice)
+
+GskGpuDevice *          gsk_d3d12_device_get_for_display                (GdkDisplay             *display,
+                                                                         GError                **error);
+
+ID3D12Device *          gsk_d3d12_device_get_d3d12_device               (GskD3d12Device         *self);
+
+G_END_DECLS
