1c910c402413752aa11a17adf77bce8c7d5ae98e
Benjamin Otte <otte@redhat.com>
2025-04-26 04:20:58 +0200

    gpu: Implement barren base classes for the D3D12 renderer
    This dumps in mostly empty implementations of the Device, Frame and
    Image objects.
    
    The code fulfills 2 tasks:
    1. Compile
    2. Don't crash when used.

---
+++ gsk/gpu/gskd3d12imageprivate.h
@@ -0,0 +1,24 @@
+#pragma once
+
+#include "gskgpuimageprivate.h"
+#include "gskd3d12deviceprivate.h"
+
+G_BEGIN_DECLS
+
+#define GSK_TYPE_D3D12_IMAGE (gsk_d3d12_image_get_type ())
+
+G_DECLARE_FINAL_TYPE (GskD3d12Image, gsk_d3d12_image, GSK, D3D12_IMAGE, GskGpuImage)
+
+GskGpuImage *           gsk_d3d12_image_new                             (ID3D12Device           *device,
+                                                                         GdkMemoryFormat         format,
+                                                                         gsize                   width,
+                                                                         gsize                   height,
+                                                                         D3D12_RESOURCE_STATES   initial_state,
+                                                                         D3D12_HEAP_FLAGS        heap_flags,
+                                                                         D3D12_RESOURCE_FLAGS    resource_flags);
+GskGpuImage *           gsk_d3d12_image_new_for_resource                (ID3D12Resource         *resource,
+                                                                         gboolean                premultiplied);
+
+ID3D12Resource *        gsk_d3d12_image_get_resource                    (GskD3d12Image          *self);
+
+G_END_DECLS
