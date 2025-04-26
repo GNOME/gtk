a089c12ed32f03f3b7b303b318577a94109b6e02
Matthias Clasen <mclasen@redhat.com>
2025-04-26 09:02:56 +0200

    Add a debug key to enable session management
    While this protocol is experimental, we should not use it
    without an explicit user opt-in. To opt in, use
    
    GDK_DEBUG=session-mgmt
    
    Part-of: <https://gitlab.gnome.org/GNOME/gtk/-/merge_requests/8502>

--- gdk/gdkdebugprivate.h
+++ gdk/gdkdebugprivate.h
@@ -50,6 +50,7 @@ typedef enum {
   GDK_DEBUG_HIGH_DEPTH      = 1 << 22,
   GDK_DEBUG_NO_VSYNC        = 1 << 23,
   GDK_DEBUG_COLOR_MANAGEMENT= 1 << 24,
+  GDK_DEBUG_SESSION_MANAGEMENT= 1 << 25,
 } GdkDebugFlags;
 
 typedef enum {
