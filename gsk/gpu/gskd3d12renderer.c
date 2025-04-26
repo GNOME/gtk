ae2e0f34d16958ae2cec403bf65fabbefa83d188
Benjamin Otte <otte@redhat.com>
2025-04-26 04:20:58 +0200

    gpu: Implement barren base classes for the D3D12 renderer
    This dumps in mostly empty implementations of the Device, Frame and
    Image objects.
    
    The code fulfills 2 tasks:
    1. Compile
    2. Don't crash when used.

--- gsk/gpu/gskd3d12renderer.c
+++ gsk/gpu/gskd3d12renderer.c
@@ -6,16 +6,14 @@
 
 #include <glib/gi18n-lib.h>
 
-#undef GDK_WINDOWING_WIN32
-
 #ifdef GDK_WINDOWING_WIN32
 
 #include "gskd3d12deviceprivate.h"
 #include "gskd3d12frameprivate.h"
 #include "gskd3d12imageprivate.h"
 
-#include "gdk/gdkd3d12contextprivate.h"
-#include "gdk/gdkdisplayprivate.h"
+#include "gdk/win32/gdkd3d12contextprivate.h"
+#include "gdk/win32/gdkprivate-win32.h"
 #endif
 
 struct _GskD3d12Renderer
@@ -31,70 +29,25 @@ struct _GskD3d12RendererClass
 G_DEFINE_TYPE (GskD3d12Renderer, gsk_d3d12_renderer, GSK_TYPE_GPU_RENDERER)
 
 #ifdef GDK_WINDOWING_WIN32
-static void
-gsk_d3d12_renderer_free_targets (GskD3d12Renderer *self)
-{
-  guint i;
-
-  for (i = 0; i < self->n_targets; i++)
-    {
-      g_object_unref (self->targets[i]);
-    }
-
-  g_clear_pointer (&self->targets, g_free);
-  self->n_targets = 0;
-}
-
-static void
-gsk_d3d12_renderer_update_images_cb (GdkD3d12Context  *context,
-                                      GskD3d12Renderer *self)
-{
-  GskD3d12Device *device;
-  GdkSurface *surface;
-  guint width, height;
-  guint i;
-
-  surface = gsk_renderer_get_surface (GSK_RENDERER (self));
-  if (surface == NULL)
-    return;
-
-  device = GSK_D3D12_DEVICE (gsk_gpu_renderer_get_device (GSK_GPU_RENDERER (self)));
-
-  gsk_d3d12_renderer_free_targets (self);
-
-  self->n_targets = gdk_d3d12_context_get_n_images (context);
-  self->targets = g_new (GskGpuImage *, self->n_targets);
-
-  gdk_draw_context_get_buffer_size (GDK_DRAW_CONTEXT (context), &width, &height);
-
-  for (i = 0; i < self->n_targets; i++)
-    {
-      self->targets[i] = gsk_d3d12_image_new_for_swapchain (device,
-                                                             gdk_d3d12_context_get_image (context, i),
-                                                             gdk_d3d12_context_get_image_format (context),
-                                                             gdk_d3d12_context_get_memory_format (context),
-                                                             width, height);
-    }
-}
-
 static GdkDrawContext *
 gsk_d3d12_renderer_create_context (GskGpuRenderer       *renderer,
-                                    GdkDisplay           *display,
-                                    GdkSurface           *surface,
-                                    GskGpuOptimizations  *supported,
-                                    GError              **error)
+                                   GdkDisplay           *display,
+                                   GdkSurface           *surface,
+                                   GskGpuOptimizations  *supported,
+                                   GError              **error)
 {
   GdkD3d12Context *context;
 
-  context = gdk_display_create_d3d12_context (display, surface, error);
+  if (!GDK_IS_WIN32_DISPLAY (display))
+    {
+      g_set_error (error, G_IO_ERROR, G_IO_ERROR_NOT_SUPPORTED,
+                   "%s does not support Direct3D 12", G_OBJECT_TYPE_NAME (display));
+      return NULL;
+    }
+
+  context = gdk_d3d12_context_new (GDK_WIN32_DISPLAY (display), surface, error);
   if (context == NULL)
     return NULL;
-
-  g_signal_connect (context,
-                    "images-updated",
-                    G_CALLBACK (gsk_d3d12_renderer_update_images_cb),
-                    self);
-  gsk_d3d12_renderer_update_images_cb (context, self);
 
   *supported = -1;
 
@@ -122,10 +75,23 @@ static GskGpuImage *
 gsk_d3d12_renderer_get_backbuffer (GskGpuRenderer *renderer)
 {
   GdkD3d12Context *context;
+  IDXGISwapChain3 *swap_chain;
+  ID3D12Resource *resource;
+  GskGpuImage *result;
 
   context = GDK_D3D12_CONTEXT (gsk_gpu_renderer_get_context (renderer));
+  swap_chain = gdk_d3d12_context_get_swap_chain (context);
 
-  return self->targets[gdk_d3d12_context_get_draw_index (context)];
+  hr_warn (IDXGISwapChain3_GetBuffer (swap_chain,
+                                      IDXGISwapChain3_GetCurrentBackBufferIndex (swap_chain),
+                                      &IID_ID3D12Resource,
+                                      (void **) &resource));
+
+  result = gsk_d3d12_image_new_for_resource (resource, TRUE);
+
+  gdk_win32_com_clear (&resource);
+
+  return result;
 }
 
 #else /* !GDK_WINDOWING_WIN32 */
