e068f9e7ed41bbbb32e1e5b31656899adb2ac44e
Benjamin Otte <otte@redhat.com>
2025-04-26 05:18:17 +0200

    gpu: Add D3D12 root signature
    This is what Vulkan calls a pipeline layout.
    
    We only have one - with our push constant, or as D3D12 calls them:
    constant parameters - for the globals.

--- gsk/gpu/gskd3d12deviceprivate.h
+++ gsk/gpu/gskd3d12deviceprivate.h
@@ -16,5 +16,7 @@ GskGpuDevice *          gsk_d3d12_device_get_for_display                (GdkDisp
                                                                          GError                **error);
 
 ID3D12Device *          gsk_d3d12_device_get_d3d12_device               (GskD3d12Device         *self);
+ID3D12RootSignature *   gsk_d3d12_device_get_d3d12_root_signature       (GskD3d12Device         *self);
+
 
 G_END_DECLS
