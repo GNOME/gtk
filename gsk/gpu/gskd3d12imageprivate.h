9851c0632ec66d2d068bb35bedb61b8e9a3d0330
Benjamin Otte <otte@redhat.com>
2025-04-27 03:49:11 +0200

    gpu: Make GskD3d12Image take a device
    We want to store other information, like RTVs and SRVs in the image,
    so we mirror Vulkan here and keep a reference to the device.

--- gsk/gpu/gskd3d12imageprivate.h
+++ gsk/gpu/gskd3d12imageprivate.h
@@ -9,16 +9,17 @@ G_BEGIN_DECLS
 
 G_DECLARE_FINAL_TYPE (GskD3d12Image, gsk_d3d12_image, GSK, D3D12_IMAGE, GskGpuImage)
 
-GskGpuImage *           gsk_d3d12_image_new                             (ID3D12Device           *device,
-                                                                         GdkMemoryFormat         format,
-                                                                         gsize                   width,
-                                                                         gsize                   height,
-                                                                         D3D12_RESOURCE_STATES   initial_state,
-                                                                         D3D12_HEAP_FLAGS        heap_flags,
-                                                                         D3D12_RESOURCE_FLAGS    resource_flags);
-GskGpuImage *           gsk_d3d12_image_new_for_resource                (ID3D12Resource         *resource,
-                                                                         gboolean                premultiplied);
+GskGpuImage *           gsk_d3d12_image_new                             (GskD3d12Device                 *device,
+                                                                         GdkMemoryFormat                 format,
+                                                                         gsize                           width,
+                                                                         gsize                           height,
+                                                                         D3D12_RESOURCE_STATES           initial_state,
+                                                                         D3D12_HEAP_FLAGS                heap_flags,
+                                                                         D3D12_RESOURCE_FLAGS            resource_flags);
+GskGpuImage *           gsk_d3d12_image_new_for_resource                (GskD3d12Device                 *device,
+                                                                         ID3D12Resource                 *resource,
+                                                                         gboolean                        premultiplied);
 
-ID3D12Resource *        gsk_d3d12_image_get_resource                    (GskD3d12Image          *self);
+ID3D12Resource *        gsk_d3d12_image_get_resource                    (GskD3d12Image                  *self);
 
 G_END_DECLS
