271dba074c1f1bfd538e8ffd31f4fd65d3230da6
Benjamin Otte <otte@redhat.com>
2025-04-29 17:50:23 +0200

    gpu: Implement D3D12 op for all non-texture shaders
    This includes adding the generic gsk_gpu_shader_op_d3d12_command().
    
    This works the same as everywhere else: Merge similar ops into one and
    then call DrawInstanced().

--- gsk/gpu/gskgpushaderopprivate.h
+++ gsk/gpu/gskgpushaderopprivate.h
@@ -67,6 +67,11 @@ GskGpuOp *              gsk_gpu_shader_op_vk_command                    (GskGpuO
 GskGpuOp *              gsk_gpu_shader_op_gl_command                    (GskGpuOp               *op,
                                                                          GskGpuFrame            *frame,
                                                                          GskGLCommandState      *state);
+#ifdef GDK_WINDOWING_WIN32
+GskGpuOp *              gsk_gpu_shader_op_d3d12_command                 (GskGpuOp               *op,
+                                                                         GskGpuFrame            *frame,
+                                                                         GskD3d12CommandState   *state);
+#endif
 
 #define GSK_RGBA_TO_VEC4(_color) (float[4]) { (_color)->red, (_color)->green, (_color)->blue, (_color)->alpha }
 #define GSK_RGBA_TO_VEC4_ALPHA(_color, _alpha) (float[4]) { (_color)->red, (_color)->green, (_color)->blue, (_color)->alpha * (_alpha) }
