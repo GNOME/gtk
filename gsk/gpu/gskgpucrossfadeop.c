27fac88a3ae7cfacd8248d9cf7d2d099cfe9b7a3
Benjamin Otte <otte@redhat.com>
2025-05-01 03:46:58 +0200

    gpu: Implement D3D12 op for all texture shaders
    Now that texture support is implemented, all shader ops with textures
    are supported.

--- gsk/gpu/gskgpucrossfadeop.c
+++ gsk/gpu/gskgpucrossfadeop.c
@@ -37,7 +37,10 @@ static const GskGpuShaderOpClass GSK_GPU_CROSS_FADE_OP_CLASS = {
 #ifdef GDK_RENDERING_VULKAN
     gsk_gpu_shader_op_vk_command,
 #endif
-    gsk_gpu_shader_op_gl_command
+    gsk_gpu_shader_op_gl_command,
+#ifdef GDK_WINDOWING_WIN32
+    gsk_gpu_shader_op_d3d12_command,
+#endif
   },
   "gskgpucrossfade",
   gsk_gpu_crossfade_n_textures,
