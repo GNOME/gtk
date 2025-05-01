875a017c28d3369f2eb128861bc0330d361abb31
Benjamin Otte <otte@redhat.com>
2025-05-01 05:28:25 +0200

    gpu: Move function
    The number of mipmap levels is not Vulkan-specific, so put it into the
    utils header.

--- gsk/gpu/gskgpuutilsprivate.h
+++ gsk/gpu/gskgpuutilsprivate.h
@@ -5,3 +5,10 @@
 GskGpuConversion        gsk_gpu_color_state_get_conversion              (GdkColorState                  *color_state);
 GdkColorState *         gsk_gpu_color_state_apply_conversion            (GdkColorState                  *color_state,
                                                                          GskGpuConversion                conversion);
+
+static inline guint
+gsk_gpu_mipmap_levels (gsize width,
+                       gsize height)
+{
+  return g_bit_nth_msf (MAX (width, height), -1) + 1;
+}
