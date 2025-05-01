ec34c7f767c964305d1d855d82709f464222a137
Matthias Clasen <mclasen@redhat.com>
2025-05-01 13:17:50 -0400

    Drop an unnecessary check
    The can_set_color_state call looks at the alpha anyway.
    
    Part-of: <https://gitlab.gnome.org/GNOME/gtk/-/merge_requests/8481>

--- gdk/wayland/gdksubsurface-wayland.c
+++ gdk/wayland/gdksubsurface-wayland.c
@@ -370,9 +370,6 @@ texture_has_default_color_state (GdkTexture *texture)
         return gdk_color_state_equal (color_state, gdk_color_state_yuv ());
     }
 
-  if (gdk_memory_format_alpha (format) == GDK_MEMORY_ALPHA_STRAIGHT)
-    return FALSE;
-
   return gdk_color_state_equal (color_state, gdk_color_state_get_srgb ());
 }
 
