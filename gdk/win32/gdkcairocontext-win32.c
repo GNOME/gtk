e48d21973e6111f9a8c61405aa6afa2a0d7f78eb
Benjamin Otte <otte@redhat.com>
2025-05-05 21:18:01 +0200

    win32: Make Cairo context use DirectComposition
    Ideas taken from how software rendering is implemented in Chrome, see
    https://github.com/chromium/chromium/commit/285f9aa26646b69369d3a6d2de84cfc1244ba25b

--- gdk/win32/gdkcairocontext-win32.c
+++ gdk/win32/gdkcairocontext-win32.c
@@ -29,10 +29,10 @@ struct _GdkWin32CairoContext
 {
   GdkCairoContext parent_instance;
 
-  IDCompositionSurface *dcomp_surface;
+  IDXGISwapChain3 *swap_chain;
+  ID3D11Texture2D *staging_texture;
 
   /* only set while drawing */
-  IDXGISurface *dxgi_surface;
   cairo_surface_t *cairo_surface;
 };
 struct _GdkWin32CairoContextClass
@@ -44,25 +44,84 @@ struct _GdkWin32CairoContextClass
 G_DEFINE_TYPE (GdkWin32CairoContext, gdk_win32_cairo_context, GDK_TYPE_CAIRO_CONTEXT)
 
 static void
-gdk_win32_cairo_context_ensure_dcomp_surface (GdkWin32CairoContext *self)
+gdk_win32_cairo_context_ensure_staging_texture (GdkWin32CairoContext *self)
 {
   GdkDrawContext *context = GDK_DRAW_CONTEXT (self);
   GdkWin32Display *display;
   guint width, height;
 
-  if (self->dcomp_surface)
+  if (self->staging_texture)
+    return;
+  
+  display = GDK_WIN32_DISPLAY (gdk_draw_context_get_display (context));
+ 
+  gdk_draw_context_get_buffer_size (context, &width, &height);
+  
+  hr_warn (ID3D11Device_CreateTexture2D (gdk_win32_display_get_d3d11_device (display),
+                                         (&(D3D11_TEXTURE2D_DESC) {
+                                             .Width = width,
+                                             .Height = height,
+                                             .MipLevels = 1,
+                                             .ArraySize = 1,
+                                             .Format = DXGI_FORMAT_B8G8R8A8_UNORM,
+                                             .SampleDesc = {
+                                                 .Count = 1,
+                                                 .Quality = 0
+                                             },
+                                             .Usage = D3D11_USAGE_STAGING,
+                                             .BindFlags = 0,
+                                             .CPUAccessFlags = D3D11_CPU_ACCESS_WRITE | D3D11_CPU_ACCESS_READ,
+                                             .MiscFlags = 0,
+                                         }),
+                                         NULL,
+                                         &self->staging_texture));
+}
+
+static void
+gdk_win32_cairo_context_ensure_swap_chain (GdkWin32CairoContext *self)
+{
+  GdkDrawContext *context = GDK_DRAW_CONTEXT (self);
+  GdkWin32Surface *surface;
+  GdkWin32Display *display;
+  ID3D11Device *d3d11_device;
+  IDXGISwapChain1 *swap_chain;
+  guint width, height;
+  HRESULT hr;
+
+  if (self->swap_chain)
     return;
 
   display = GDK_WIN32_DISPLAY (gdk_draw_context_get_display (context));
+  surface = GDK_WIN32_SURFACE (gdk_draw_context_get_surface (context));
+  d3d11_device = gdk_win32_display_get_d3d11_device (display);
+
   gdk_draw_context_get_buffer_size (context, &width, &height);
 
-  hr_warn (IDCompositionDevice_CreateSurface (gdk_win32_display_get_dcomp_device (display),
-                                              width, height,
-                                              DXGI_FORMAT_B8G8R8A8_UNORM,
-                                              DXGI_ALPHA_MODE_PREMULTIPLIED,
-                                              &self->dcomp_surface));
-  gdk_win32_surface_set_dcomp_content (GDK_WIN32_SURFACE (gdk_draw_context_get_surface (context)),
-                                       (IUnknown *) self->dcomp_surface);
+  hr_warn (IDXGIFactory4_CreateSwapChainForComposition (gdk_win32_display_get_dxgi_factory (display),
+                                                        (IUnknown *) d3d11_device,
+                                                        (&(DXGI_SWAP_CHAIN_DESC1) {
+                                                            .Width = width,
+                                                            .Height = height,
+                                                            .Format = DXGI_FORMAT_B8G8R8A8_UNORM,
+                                                            .Stereo = false,
+                                                            .SampleDesc = (DXGI_SAMPLE_DESC) {
+                                                                .Count = 1,
+                                                                .Quality = 0,
+                                                            },
+                                                            .BufferUsage = 0,
+                                                            .BufferCount = 2,
+                                                            .Scaling = DXGI_SCALING_STRETCH,
+                                                            .SwapEffect = DXGI_SWAP_EFFECT_FLIP_SEQUENTIAL,
+                                                            .AlphaMode = DXGI_ALPHA_MODE_PREMULTIPLIED,
+                                                            .Flags = 0,
+                                                        }),
+                                                        NULL,
+                                                        &swap_chain));
+
+  hr_warn (IDXGISwapChain1_QueryInterface (swap_chain, &IID_IDXGISwapChain3, (void **) &self->swap_chain));
+  gdk_win32_com_clear (&swap_chain);
+
+  gdk_win32_surface_set_dcomp_content (GDK_WIN32_SURFACE (surface), (IUnknown *) self->swap_chain);
 }
 
 static void
@@ -74,40 +133,44 @@ gdk_win32_cairo_context_begin_frame (GdkDrawContext  *draw_context,
                                      GdkMemoryDepth  *out_depth)
 {
   GdkWin32CairoContext *self = GDK_WIN32_CAIRO_CONTEXT (draw_context);
-  GdkSurface *surface;
   cairo_t *cr;
   cairo_rectangle_int_t extents;
-  DXGI_MAPPED_RECT mapped_rect;
   POINT offset;
+  GdkWin32Display *display;
+  ID3D11Device *d3d11_device;
+  ID3D11DeviceContext *d3d11_context;
+  D3D11_MAPPED_SUBRESOURCE map;
+  guint width, height;
 
-  gdk_win32_cairo_context_ensure_dcomp_surface (self);
+  gdk_win32_cairo_context_ensure_swap_chain (self);
+  gdk_win32_cairo_context_ensure_staging_texture (self);
 
-  cairo_region_get_extents (region, &extents);
+  display = GDK_WIN32_DISPLAY (gdk_draw_context_get_display (draw_context));
+  d3d11_device = gdk_win32_display_get_d3d11_device (display);
 
-  hr_warn (IDCompositionSurface_BeginDraw (self->dcomp_surface,
-                                           (&(RECT) {
-                                               .left = extents.x,
-                                               .top = extents.y,
-                                               .right = extents.x + extents.width,
-                                               .bottom = extents.y + extents.height
-                                           }),
-                                           &IID_IDXGISurface,
-                                           (void **) &self->dxgi_surface,
-                                           &offset));
-
-  hr_warn (IDXGISurface_Map (self->dxgi_surface, &mapped_rect, DXGI_MAP_READ | DXGI_MAP_WRITE));
+  ID3D11Device_GetImmediateContext (d3d11_device, &d3d11_context);
+  gdk_draw_context_get_buffer_size (draw_context, &width, &height);
   
-  self->cairo_surface = cairo_image_surface_create_for_data (mapped_rect.pBits,
+  hr_warn (ID3D11DeviceContext_Map (d3d11_context,
+                                    (ID3D11Resource *) self->staging_texture,
+                                    0,
+                                    D3D11_MAP_READ_WRITE,
+                                    0,
+                                    &map));
+  
+  self->cairo_surface = cairo_image_surface_create_for_data (map.pData,
                                                              CAIRO_FORMAT_ARGB32,
-                                                             extents.width,
-                                                             extents.height,
-                                                             mapped_rect.Pitch);
+                                                             width,
+                                                             height,
+                                                             map.RowPitch);
 
   cr = cairo_create (self->cairo_surface);
   cairo_set_operator (cr, CAIRO_OPERATOR_CLEAR);
   gdk_cairo_region (cr, region);
   cairo_fill (cr);
   cairo_destroy (cr);
+
+  gdk_win32_com_clear (&d3d11_context);
 
   *out_color_state = GDK_COLOR_STATE_SRGB;
   *out_depth = gdk_color_state_get_depth (GDK_COLOR_STATE_SRGB);
@@ -119,14 +182,94 @@ gdk_win32_cairo_context_end_frame (GdkDrawContext *draw_context,
                                    cairo_region_t *painted)
 {
   GdkWin32CairoContext *self = GDK_WIN32_CAIRO_CONTEXT (draw_context);
+  GdkWin32Display *display;
+  ID3D11Device *d3d11_device;
+  ID3D11DeviceContext *d3d11_context;
+  ID3D11Texture2D *texture;
+  guint width, height;
+  int i, n_rects;
+  RECT *rects;
+
+  display = GDK_WIN32_DISPLAY (gdk_draw_context_get_display (draw_context));
+  d3d11_device = gdk_win32_display_get_d3d11_device (display);
+
+  ID3D11Device_GetImmediateContext (d3d11_device, &d3d11_context);
+  gdk_draw_context_get_buffer_size (draw_context, &width, &height);
 
   cairo_surface_flush (self->cairo_surface);
   g_clear_pointer (&self->cairo_surface, cairo_surface_destroy);
 
-  hr_warn (IDXGISurface_Unmap (self->dxgi_surface));
-  gdk_win32_com_clear (&self->dxgi_surface);
+  ID3D11DeviceContext_Unmap (d3d11_context, (ID3D11Resource *) self->staging_texture, 0);
 
-  hr_warn (IDCompositionSurface_EndDraw (self->dcomp_surface));
+  hr_warn (IDXGISwapChain3_GetBuffer (self->swap_chain,
+                                      IDXGISwapChain3_GetCurrentBackBufferIndex (self->swap_chain),
+                                      &IID_ID3D11Texture2D,
+                                      (void **) &texture));
+
+  if (cairo_region_contains_rectangle (painted,
+                                       &(cairo_rectangle_int_t) {
+                                           .x = 0,
+                                           .y = 0,
+                                           .width = width,
+                                           .height = height,
+                                       }) == CAIRO_REGION_OVERLAP_IN)
+    {
+      n_rects = 0;
+      rects = NULL;
+      ID3D11DeviceContext_CopySubresourceRegion (d3d11_context,
+                                                 (ID3D11Resource *) texture,
+                                                 0,
+                                                 0, 0, 0,
+                                                 (ID3D11Resource *) self->staging_texture,
+                                                 0,
+                                                 (&(D3D11_BOX) {
+                                                     .left = 0,
+                                                     .top = 0,
+                                                     .front = 0,
+                                                     .right = width,
+                                                     .bottom = height,
+                                                     .back = 1
+                                                 }));
+    }
+  else
+    {
+      n_rects = cairo_region_num_rectangles (painted);
+      rects = g_newa(RECT, n_rects);
+      for (i = 0; i < n_rects; i++)
+        {
+          cairo_rectangle_int_t crect;
+
+          cairo_region_get_rectangle (painted, i, &crect);
+          rects[i].left = crect.x;
+          rects[i].top = crect.y;
+          rects[i].right = crect.x + crect.width;
+          rects[i].bottom = crect.y + crect.height;
+          ID3D11DeviceContext_CopySubresourceRegion (d3d11_context,
+                                                     (ID3D11Resource *) texture,
+                                                     0,
+                                                     crect.x, crect.y, 0,
+                                                     (ID3D11Resource *) self->staging_texture,
+                                                     0,
+                                                     (&(D3D11_BOX) {
+                                                         .left = crect.x,
+                                                         .top = crect.y,
+                                                         .front = 0,
+                                                         .right = crect.x + crect.width,
+                                                         .bottom = crect.y + crect.height,
+                                                         .back = 1
+                                                     }));
+        }
+    }
+
+  hr_warn (IDXGISwapChain3_Present1 (self->swap_chain,
+                                     0,
+                                     DXGI_PRESENT_RESTART,
+                                     (&(DXGI_PRESENT_PARAMETERS) {
+                                         .DirtyRectsCount = n_rects,
+                                         .pDirtyRects = rects,
+                                     })));
+
+  gdk_win32_com_clear (&d3d11_context);
 }
 
 static void
