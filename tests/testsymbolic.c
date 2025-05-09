d8613153cf1daa7c66bb856e2290dfd479fe01aa
Matthias Clasen <mclasen@redhat.com>
2025-05-09 09:30:35 -0400

    Make testsymbolic render in different modes
    This allows for easy visual comparison.

--- tests/testsymbolic.c
+++ tests/testsymbolic.c
@@ -1,5 +1,7 @@
 #include <gtk/gtk.h>
 
+#include "gtk/gtkiconthemeprivate.h"
+
 static const char css[] =
 ".testimage {\n"
 "  background: white;\n"
@@ -28,6 +30,9 @@ static const char large_css[] =
 "  -gtk-icon-size: 128px;\n"
 "}\n";
 
+static GtkCssProvider *provider;
+static GListModel *model;
+
 static void
 setup_item (GtkSignalListItemFactory *factory,
             GObject                  *object)
@@ -36,6 +41,33 @@ setup_item (GtkSignalListItemFactory *factory,
   GtkWidget *box, *child;
 
   box = gtk_box_new (GTK_ORIENTATION_HORIZONTAL, 10);
+
+  child = gtk_image_new ();
+  gtk_widget_add_css_class (child, "testimage");
+  gtk_box_append (GTK_BOX (box), child);
+
+  child = gtk_image_new ();
+  gtk_widget_add_css_class (child, "testimage");
+  gtk_widget_add_css_class (child, "dark");
+  gtk_box_append (GTK_BOX (box), child);
+
+  child = gtk_image_new ();
+  gtk_widget_add_css_class (child, "testimage");
+  gtk_box_append (GTK_BOX (box), child);
+
+  child = gtk_image_new ();
+  gtk_widget_add_css_class (child, "testimage");
+  gtk_widget_add_css_class (child, "dark");
+  gtk_box_append (GTK_BOX (box), child);
+
+  child = gtk_image_new ();
+  gtk_widget_add_css_class (child, "testimage");
+  gtk_box_append (GTK_BOX (box), child);
+
+  child = gtk_image_new ();
+  gtk_widget_add_css_class (child, "testimage");
+  gtk_widget_add_css_class (child, "dark");
+  gtk_box_append (GTK_BOX (box), child);
 
   child = gtk_image_new ();
   gtk_widget_add_css_class (child, "testimage");
@@ -58,46 +90,93 @@ bind_item (GtkSignalListItemFactory *factory,
 {
   GtkWidget *box;
   GObject *item;
+  GFile *file;
   const char *name;
+  int icon_size;
+  char *icon_name;
   GtkWidget *child;
+  GtkIconPaintable *icon;
 
   box = gtk_list_item_get_child (GTK_LIST_ITEM (object));
   item = gtk_list_item_get_item (GTK_LIST_ITEM (object));
 
-  name = gtk_string_object_get_string (GTK_STRING_OBJECT (item));
-  for (child = gtk_widget_get_first_child (box);
-       child != NULL;
-       child = gtk_widget_get_next_sibling (child))
-    {
-      if (GTK_IS_IMAGE (child))
-        gtk_image_set_from_icon_name (GTK_IMAGE (child), name);
-      else if (GTK_IS_LABEL (child))
-        gtk_label_set_label (GTK_LABEL (child), name);
-    }
-}
-
-static gpointer
-file_info_to_icon_name (gpointer item,
-                        gpointer user_data)
-{
-  const char *name;
-  char *icon_name;
-  GtkStringObject *obj;
-
+  file = G_FILE (g_file_info_get_attribute_object (G_FILE_INFO (item), "standard::file"));
   name = g_file_info_get_name (G_FILE_INFO (item));
+  icon_size = g_file_info_get_attribute_int32 (G_FILE_INFO (item), "private::icon-size");
 
   if (g_str_has_suffix (name, ".svg"))
     icon_name = g_strndup (name, strlen (name) - strlen (".svg"));
   else
     icon_name = g_strdup (name);
 
-  g_object_unref (item);
+  /* the first two icons allow svg-to-node and recoloring */
+  child = gtk_widget_get_first_child (box);
+  icon = gtk_icon_paintable_new_for_file (file, icon_size, 2);
+  gtk_image_set_from_paintable (GTK_IMAGE (child), GDK_PAINTABLE (icon));
+  g_object_unref (icon);
 
-  obj = gtk_string_object_new (icon_name);
+  child = gtk_widget_get_next_sibling (child);
+  icon = gtk_icon_paintable_new_for_file (file, icon_size, 2);
+  gtk_image_set_from_paintable (GTK_IMAGE (child), GDK_PAINTABLE (icon));
+  g_object_unref (icon);
+
+  /* the next two icons allow svg-to-node, but don't allow recoloring */
+  child = gtk_widget_get_next_sibling (child);
+  icon = gtk_icon_paintable_new_for_file (file, icon_size, 2);
+  gtk_icon_paintable_set_debug (icon, TRUE, FALSE, TRUE);
+  gtk_image_set_from_paintable (GTK_IMAGE (child), GDK_PAINTABLE (icon));
+  g_object_unref (icon);
+
+  child = gtk_widget_get_next_sibling (child);
+  icon = gtk_icon_paintable_new_for_file (file, icon_size, 2);
+  gtk_icon_paintable_set_debug (icon, TRUE, FALSE, TRUE);
+  gtk_image_set_from_paintable (GTK_IMAGE (child), GDK_PAINTABLE (icon));
+  g_object_unref (icon);
+
+  /* the next two icons don't allow svg-to-node, but allow masks */
+  child = gtk_widget_get_next_sibling (child);
+  icon = gtk_icon_paintable_new_for_file (file, icon_size, 2);
+  gtk_icon_paintable_set_debug (icon, FALSE, FALSE, TRUE);
+  gtk_image_set_from_paintable (GTK_IMAGE (child), GDK_PAINTABLE (icon));
+  g_object_unref (icon);
+
+  child = gtk_widget_get_next_sibling (child);
+  icon = gtk_icon_paintable_new_for_file (file, icon_size, 2);
+  gtk_icon_paintable_set_debug (icon, FALSE, FALSE, TRUE);
+  gtk_image_set_from_paintable (GTK_IMAGE (child), GDK_PAINTABLE (icon));
+  g_object_unref (icon);
+
+  /* the next two icons don't allow svg-to-node or masks */
+  child = gtk_widget_get_next_sibling (child);
+  icon = gtk_icon_paintable_new_for_file (file, icon_size, 2);
+  gtk_icon_paintable_set_debug (icon, FALSE, FALSE, FALSE);
+  gtk_image_set_from_paintable (GTK_IMAGE (child), GDK_PAINTABLE (icon));
+  g_object_unref (icon);
+
+  child = gtk_widget_get_next_sibling (child);
+  icon = gtk_icon_paintable_new_for_file (file, icon_size, 2);
+  gtk_icon_paintable_set_debug (icon, FALSE, FALSE, FALSE);
+  gtk_image_set_from_paintable (GTK_IMAGE (child), GDK_PAINTABLE (icon));
+  g_object_unref (icon);
+
+  child = gtk_widget_get_next_sibling (child);
+  gtk_label_set_label (GTK_LABEL (child), icon_name);
 
   g_free (icon_name);
+}
 
-  return obj;
+static gpointer
+add_icon_size_to_file_info (gpointer item,
+                            gpointer user_data)
+{
+  GFileInfo *info;
+
+  info = g_file_info_dup (G_FILE_INFO (item));
+  g_file_info_set_attribute_int32 (info, "private::icon-size", GPOINTER_TO_INT (user_data));
+
+  g_object_unref (item);
+
+  return info;
 }
 
 static void
@@ -105,24 +184,31 @@ large_toggled (GObject    *obj,
                GParamSpec *pspec,
                gpointer    data)
 {
-  GtkCssProvider *provider = data;
+  int icon_size;
 
   if (gtk_toggle_button_get_active (GTK_TOGGLE_BUTTON (obj)))
-    gtk_css_provider_load_from_string (provider, large_css);
+    {
+      icon_size = 128;
+      gtk_css_provider_load_from_string (provider, large_css);
+    }
   else
-    gtk_css_provider_load_from_string (provider, css);
+    {
+      icon_size = 16;
+      gtk_css_provider_load_from_string (provider, css);
+    }
+
+  gtk_map_list_model_set_map_func (GTK_MAP_LIST_MODEL (model),
+                                   add_icon_size_to_file_info, GINT_TO_POINTER (icon_size), NULL);
 }
 
 int
 main (int argc, char *argv[])
 {
   GtkWidget *window;
-  GtkCssProvider *provider;
   GtkListItemFactory *factory;
   GtkWidget *sw;
   GtkWidget *list;
   GFile *file;
-  GListModel *model;
   GtkWidget *headerbar;
   GtkWidget *large_toggle;
 
@@ -134,9 +220,12 @@ main (int argc, char *argv[])
                                               GTK_STYLE_PROVIDER (provider),
                                               800);
 
-  file = g_file_new_for_commandline_arg (argv[1]);
-  model = G_LIST_MODEL (gtk_map_list_model_new (G_LIST_MODEL (gtk_directory_list_new ("standard::name", file)),
-                                                file_info_to_icon_name, NULL, NULL));
+  if (argc > 1)
+    file = g_file_new_for_commandline_arg (argv[1]);
+  else
+    file = g_file_new_for_path ("/usr/share/icons/Adwaita/symbolic/actions");
+
+  model = G_LIST_MODEL (gtk_map_list_model_new (G_LIST_MODEL (gtk_directory_list_new ("standard::name", file)), add_icon_size_to_file_info, GINT_TO_POINTER (16), NULL));
   g_object_unref (file);
 
   factory = gtk_signal_list_item_factory_new ();
