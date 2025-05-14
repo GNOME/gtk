4849f0c69960c8bb9067a3f7ef7782788dd4b632
Benjamin Otte <otte@redhat.com>
2025-05-15 01:39:35 +0200

    gpu: Add gsk_gpu_renderer_clear_frames()
    This API is meant for the D3D12 renderer, which needs to ensure all
    buffers are done executing andmemory associated with it got freed.
    
    Related: https://learn.microsoft.com/en-us/windows/win32/api/dxgi/nf-dxgi-idxgiswapchain-resizebuffers#remarks

--- gsk/gpu/gskgpurenderer.c
+++ gsk/gpu/gskgpurenderer.c
@@ -214,10 +214,18 @@ gsk_gpu_renderer_realize (GskRenderer  *renderer,
   return TRUE;
 }
 
-static void
-gsk_gpu_renderer_unrealize (GskRenderer *renderer)
+/*<private>
+ * gsk_gpu_renderer_clear_frames:
+ * @self: the renderer
+ * 
+ * Waits for all frames to finish executing and then deletes them.
+ * 
+ * This will release all the memory held by the renderer that is not
+ * in the cache.
+ */
+void
+gsk_gpu_renderer_clear_frames (GskGpuRenderer *self)
 {
-  GskGpuRenderer *self = GSK_GPU_RENDERER (renderer);
   GskGpuRendererPrivate *priv = gsk_gpu_renderer_get_instance_private (self);
   gsize i;
 
@@ -231,6 +239,15 @@ gsk_gpu_renderer_unrealize (GskRenderer *renderer)
         gsk_gpu_frame_wait (priv->frames[i]);
       g_clear_object (&priv->frames[i]);
     }
+}
+
+static void
+gsk_gpu_renderer_unrealize (GskRenderer *renderer)
+{
+  GskGpuRenderer *self = GSK_GPU_RENDERER (renderer);
+  GskGpuRendererPrivate *priv = gsk_gpu_renderer_get_instance_private (self);
+
+  gsk_gpu_renderer_clear_frames (self);
 
   gdk_draw_context_detach (priv->context);
 
