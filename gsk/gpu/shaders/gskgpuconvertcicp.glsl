GNOME/gtk

commit 898777c18f71690fd7920917d48c3ecd3b618f0c

gpu: Clamp cicp narrow range inputs
They should end up being in the [0..1] range after scaling to full
range, so ensure that they are.

Also convert the range conversions to fma() calls. They're easier
to read for me (and in theory faster, too!).

Part-of: <https://gitlab.gnome.org/GNOME/gtk/-/merge_requests/8603>

gsk/gpu/shaders/gskgpuconvertcicp.glsl
@@ -410,6 +410,8 @@ apply_cicp_oetf (vec3 color,
     }
 }
 
+const vec3 narrow_range_scale = vec3(224.0, 219.0, 224.0);
+
 vec4
 convert_color_from_cicp (vec4 color,
                          bool from_premul,
@@ -420,11 +422,7 @@ convert_color_from_cicp (vec4 color,
     color = color_unpremultiply (color);
 
   if (_range == 0u)
-    {
-      color.r = (color.r - 16.0/255.0) * 255.0/224.0;
-      color.g = (color.g - 16.0/255.0) * 255.0/219.0;
-      color.b = (color.b - 16.0/255.0) * 255.0/224.0;
-    }
+    color.rgb = clamp (fma (color.rgb, 255.0 / narrow_range_scale, vec3 (-16.0 / narrow_range_scale)), 0.0, 1.0);
 
   color.rgb = _yuv * (color.rgb + _yuv_add);
 
@@ -456,11 +454,7 @@ convert_color_to_cicp (vec4 color,
   color.rgb = clamp (color.rgb, 0.0, 1.0);
 
   if (_range == 0u)
-    {
-      color.r = color.r * 224.0/255.0 + 16.0/255.0;
-      color.g = color.g * 219.0/255.0 + 16.0/255.0;
-      color.b = color.b * 224.0/255.0 + 16.0/255.0;
-    }
+    color.rgb = fma (color.rgb, narrow_range_scale / 255.0, vec3 (16.0 / 255.0));
 
   if (to_premul)
     color = color_premultiply (color);

commit f4003492e210a8a32e00b6764c8cba44cea7aaa7

gpu: Do not use fma() in GLSL shaders
It's only available in GLES 3.1 and GL 4.0, breaking older clients.

Fixes: 898777c18f ("gpu: Clamp cicp narrow range inputs")
Part-of: <https://gitlab.gnome.org/GNOME/gtk/-/merge_requests/8617>

gsk/gpu/shaders/gskgpuconvertcicp.glsl
@@ -411,8 +411,6 @@ apply_cicp_oetf (vec3 color,
     }
 }
 
-const vec3 narrow_range_scale = vec3(224.0, 219.0, 224.0);
-
 vec4
 convert_color_from_cicp (vec4 color,
                          bool from_premul,
@@ -423,7 +421,11 @@ convert_color_from_cicp (vec4 color,
     color = color_unpremultiply (color);
 
   if (_range == 0u)
-    color.rgb = clamp (fma (color.rgb, 255.0 / narrow_range_scale, vec3 (-16.0 / narrow_range_scale)), 0.0, 1.0);
+    {
+      color.r = clamp ((color.r - 16.0/255.0) * 255.0/224.0, 0.0, 1.0);
+      color.g = clamp ((color.g - 16.0/255.0) * 255.0/219.0, 0.0, 1.0);
+      color.b = clamp ((color.b - 16.0/255.0) * 255.0/224.0, 0.0, 1.0);
+    }
 
   color.rgb = _yuv * (color.rgb + _yuv_add);
 
@@ -455,7 +457,11 @@ convert_color_to_cicp (vec4 color,
   color.rgb = clamp (color.rgb, 0.0, 1.0);
 
   if (_range == 0u)
-    color.rgb = fma (color.rgb, narrow_range_scale / 255.0, vec3 (16.0 / 255.0));
+    {
+      color.r = color.r * 224.0/255.0 + 16.0/255.0;
+      color.g = color.g * 219.0/255.0 + 16.0/255.0;
+      color.b = color.b * 224.0/255.0 + 16.0/255.0;
+    }
 
   if (to_premul)
     color = color_premultiply (color);