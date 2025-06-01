GNOME/gtk

commit f3b56a237319b77c3eb31d2f53f65fe4c71b7fb6

gpu: Add GskGpuCachedFill
This is the equivalent of GskGpuCachedGlyph for paths.

Part-of: <https://gitlab.gnome.org/GNOME/gtk/-/merge_requests/8580>

gsk/gpu/gskgpucachedfill.c
@@ -1,0 +1,305 @@
+#include "config.h"
+
+#include "gskgpucachedfillprivate.h"
+
+#include "gskgpucacheprivate.h"
+#include "gskgpucachedprivate.h"
+#include "gskgpudeviceprivate.h"
+#include "gskgpuuploadopprivate.h"
+#include "gskgpuutilsprivate.h"
+#include "gskgpuimageprivate.h"
+#include "gskpathprivate.h"
+#include "gskrectprivate.h"
+
+#include "gdk/gdkcolorstateprivate.h"
+#include "gdk/gdkcolorprivate.h"
+#include "gdk/gdkcairoprivate.h"
+
+#include "gsk/gskprivate.h"
+
+#define SUBPIXEL_SCALE_X 32
+#define SUBPIXEL_SCALE_Y 32
+
+typedef struct _GskGpuCachedFill GskGpuCachedFill;
+
+struct _GskGpuCachedFill
+{
+  GskGpuCached parent;
+
+  GskPath *path;
+  GskFillRule fill_rule;
+  float sx, sy;
+
+  /* Subpixel position of the path bounds wrt to device grid */
+  guint fx, fy;
+
+  GskGpuImage *image;
+  graphene_point_t image_offset;
+};
+
+static void
+gsk_gpu_cached_fill_free (GskGpuCached *cached)
+{
+  GskGpuCachedFill *self = (GskGpuCachedFill *) cached;
+  GskGpuCachePrivate *priv = gsk_gpu_cache_get_private (cached->cache);
+
+  g_hash_table_remove (priv->fill_cache, self);
+
+  gsk_path_unref (self->path);
+  g_object_unref (self->image);
+
+  g_free (self);
+}
+
+static gboolean
+gsk_gpu_cached_fill_should_collect (GskGpuCached *cached,
+                                    gint64        cache_timeout,
+                                    gint64        timestamp)
+{
+  if (gsk_gpu_cached_is_old (cached, cache_timeout, timestamp))
+    {
+      if (cached->atlas)
+        gsk_gpu_cached_set_stale (cached, TRUE);
+      else
+        return TRUE;
+    }
+
+  /* Fills are only collected when their atlas is freed */
+  return FALSE;
+}
+
+static guint
+gsk_gpu_cached_fill_hash (gconstpointer data)
+{
+  const GskGpuCachedFill *self = data;
+
+  return GPOINTER_TO_UINT (self->path) ^
+         (self->fill_rule) << 28 ^
+         (((guint) (self->sx * 16)) << 16) ^
+         ((guint) (self->sy * 16) << 8) ^
+         (self->fx << 4) ^
+         self->fy;
+}
+
+static gboolean
+gsk_gpu_cached_fill_equal (gconstpointer v1,
+                           gconstpointer v2)
+{
+  const GskGpuCachedFill *fill1 = v1;
+  const GskGpuCachedFill *fill2 = v2;
+
+  return fill1->fx == fill2->fx &&
+         fill1->fy == fill2->fy &&
+         fill1->path == fill2->path &&
+         fill1->fill_rule == fill2->fill_rule &&
+         fill1->sx == fill2->sx &&
+         fill1->sy == fill2->sy;
+}
+
+static const GskGpuCachedClass GSK_GPU_CACHED_FILL_CLASS =
+{
+  sizeof (GskGpuCachedFill),
+  "Fill",
+  gsk_gpu_cached_fill_free,
+  gsk_gpu_cached_fill_should_collect
+};
+
+typedef struct _FillData FillData;
+struct _FillData
+{
+  GskPath *path;
+  GskFillRule fill_rule;
+};
+
+static void
+fill_data_free (gpointer data)
+{
+  FillData *fill = data;
+
+  gsk_path_unref (fill->path);
+  g_free (fill);
+}
+
+static void
+fill_path (gpointer  data,
+           cairo_t  *cr)
+{
+  FillData *fill = data;
+
+  cairo_set_source_rgba (cr, 1, 1, 1, 1);
+
+  gsk_cairo_set_fill_rule (cr, fill->fill_rule);
+  gsk_path_to_cairo (fill->path, cr);
+  cairo_fill (cr);
+}
+
+static void
+fill_path_print (gpointer  data,
+                 GString  *string)
+{
+  FillData *fill = data;
+  char *str;
+
+  str = gsk_path_to_string (fill->path);
+  g_string_append_printf (string, "fill %s %.*s%s",
+                          fill->fill_rule == GSK_FILL_RULE_WINDING ? "★" : "✫",
+                          20, str, strlen (str) > 20 ? "…" : "");
+  g_free (str);
+}
+
+static gsize
+mod_subpixel (float  pos,
+              gsize  scale,
+              float *delta)
+{
+  pos = fmod (scale * pos, scale);
+  *delta = (ceil (pos) - pos) / scale;
+  if (pos > 0.0)
+    return scale - (gsize) ceil (pos);
+  else
+    return (gsize) - ceil (pos);
+}
+
+GskGpuImage *
+gsk_gpu_cached_fill_lookup (GskGpuCache           *self,
+                            GskGpuFrame           *frame,
+                            const graphene_vec2_t *scale,
+                            const graphene_rect_t *bounds,
+                            GskPath               *path,
+                            GskFillRule            fill_rule,
+                            graphene_rect_t       *out_rect)
+{
+  GskGpuCachePrivate *priv = gsk_gpu_cache_get_private (self);
+  float sx = graphene_vec2_get_x (scale);
+  float sy = graphene_vec2_get_y (scale);
+  float dx, dy;
+  GskGpuCachedFill *cache;
+  gsize fx, fy, atlas_x, atlas_y, padding, image_width, image_height;
+  GskGpuImage *image = NULL;
+  graphene_rect_t viewport;
+
+  fx = mod_subpixel (sx * bounds->origin.x, SUBPIXEL_SCALE_X, &dx);
+  fy = mod_subpixel (sy * bounds->origin.y, SUBPIXEL_SCALE_Y, &dy);
+
+  cache = g_hash_table_lookup (priv->fill_cache,
+                               &(GskGpuCachedFill) {
+                                 .path = path,
+                                 .fill_rule = fill_rule,
+                                 .sx = sx,
+                                 .sy = sy,
+                                 .fx = fx,
+                                 .fy = fy,
+                               });
+  if (cache)
+    {
+      gsk_gpu_cached_use ((GskGpuCached *) cache);
+
+      graphene_rect_init (out_rect,
+                          cache->image_offset.x - dx,
+                          cache->image_offset.y - dy,
+                          gsk_gpu_image_get_width (cache->image) / sx,
+                          gsk_gpu_image_get_height (cache->image) / sy);
+
+      return g_object_ref (cache->image);
+    }
+
+  if (!gsk_path_get_bounds (path, &viewport) ||
+      !gsk_rect_snap_to_grid (&viewport, scale, &GRAPHENE_POINT_INIT ((float)fx / SUBPIXEL_SCALE_X, (float)fy / SUBPIXEL_SCALE_Y), &viewport))
+    return NULL;
+
+  padding = 1;
+  /* Should already be integers because of snap_to_grid() above, but round just to be sure */
+  image_width = round (sx * viewport.size.width);
+  image_height = round (sy * viewport.size.height);
+
+  image = gsk_gpu_cache_add_atlas_image (self,
+                                         image_width + 2 * padding,
+                                         image_height + 2 * padding,
+                                         &atlas_x,
+                                         &atlas_y);
+
+  if (image)
+    {
+      g_object_ref (image);
+      cache = gsk_gpu_cached_new_from_current_atlas (self, &GSK_GPU_CACHED_FILL_CLASS);
+      cache->path = gsk_path_ref (path);
+      cache->fill_rule = fill_rule;
+      cache->sx = sx;
+      cache->sy = sy;
+      cache->fx = fx;
+      cache->fy = fy;
+      cache->image = g_object_ref (image);
+      graphene_rect_inset (&viewport, padding / -sx, padding / -sy);
+      cache->image_offset = GRAPHENE_POINT_INIT (viewport.origin.x - atlas_x / sx,
+                                                 viewport.origin.y - atlas_y / sy);
+      ((GskGpuCached *) cache)->pixels = (image_width + 2 * padding) * (image_height + 2 * padding);
+
+      g_hash_table_insert (priv->fill_cache, cache, cache);
+      gsk_gpu_cached_use ((GskGpuCached *) cache);
+    }
+  else
+    {
+      /* If the unclipped path is too large to fit in the atlas,
+       * we give up on caching. We still need to return a mask,
+       * but we'll use the clipped bounds that we were given.
+       * We'll also assume those are grid aligned.
+       */
+      viewport = *bounds;
+      padding = 0;
+      atlas_x = 0;
+      atlas_y = 0;
+      image_width = ceil (sx * viewport.size.width);
+      image_height = ceil (sy * viewport.size.height);
+
+      image = gsk_gpu_device_create_upload_image (gsk_gpu_cache_get_device (self),
+                                                  FALSE,
+                                                  GDK_MEMORY_DEFAULT,
+                                                  gsk_gpu_color_state_get_conversion (GDK_COLOR_STATE_SRGB),
+                                                  image_width,
+                                                  image_height);
+      if (image == NULL)
+        return NULL;
+    }
+
+  gsk_gpu_upload_cairo_into_op (frame,
+                                image,
+                                &(cairo_rectangle_int_t) {
+                                  .x = atlas_x,
+                                  .y = atlas_y,
+                                  .width = image_width + 2 * padding,
+                                  .height = image_height + 2 * padding
+                                },
+                                &viewport,
+                                fill_path,
+                                fill_path_print,
+                                g_memdup2 (&(FillData) {
+                                  .path = gsk_path_ref (path),
+                                  .fill_rule = fill_rule,
+                                }, sizeof (FillData)),
+                                (GDestroyNotify) fill_data_free);
+
+  graphene_rect_init (out_rect,
+                      viewport.origin.x - atlas_x / sx - dx,
+                      viewport.origin.y - atlas_y / sy - dy,
+                      gsk_gpu_image_get_width (image) / sx,
+                      gsk_gpu_image_get_height (image) / sy);
+
+  return image;
+}
+
+void
+gsk_gpu_cached_fill_init_cache (GskGpuCache *cache)
+{
+  GskGpuCachePrivate *priv = gsk_gpu_cache_get_private (cache);
+
+  priv->fill_cache = g_hash_table_new (gsk_gpu_cached_fill_hash,
+                                       gsk_gpu_cached_fill_equal);
+}
+
+void
+gsk_gpu_cached_fill_finish_cache (GskGpuCache *cache)
+{
+  GskGpuCachePrivate *priv = gsk_gpu_cache_get_private (cache);
+
+  g_hash_table_unref (priv->fill_cache);
+}

gsk/meson.build
@@ -38,6 +38,7 @@ gsk_private_sources = files([
   'gpu/gskgpuboxshadowop.c',
   'gpu/gskgpubuffer.c',
   'gpu/gskgpucache.c',
+  'gpu/gskgpucachedfill.c',
   'gpu/gskgpucachedglyph.c',
   'gpu/gskgpuclearop.c',
   'gpu/gskgpuclip.c',

gsk/gpu/gskgpucache.c
@@ -3,6 +3,7 @@
 #include "gskgpucacheprivate.h"
 
 #include "gskgpucachedglyphprivate.h"
+#include "gskgpucachedfillprivate.h"
 #include "gskgpucachedprivate.h"
 #include "gskgpudeviceprivate.h"
 #include "gskgpuframeprivate.h"
@@ -879,6 +880,8 @@ gsk_gpu_cache_dispose (GObject *object)
 
   gsk_gpu_cache_clear_cache (self);
 
+  gsk_gpu_cached_fill_finish_cache (self);
+
 #ifdef GDK_RENDERING_VULKAN
   gsk_vulkan_ycbcr_finish_cache (self);
 #endif
@@ -919,6 +922,7 @@ gsk_gpu_cache_init (GskGpuCache *self)
 #ifdef GDK_RENDERING_VULKAN
   gsk_vulkan_ycbcr_init_cache (self);
 #endif
+  gsk_gpu_cached_fill_init_cache (self);
 }
 
 GskGpuImage *

gsk/gpu/gskgpucachedfillprivate.h
@@ -1,0 +1,23 @@
+#pragma once
+
+#include "gskgpucachedprivate.h"
+
+#include "gsk/gskpath.h"
+
+#include <graphene.h>
+
+G_BEGIN_DECLS
+
+void                    gsk_gpu_cached_fill_init_cache                  (GskGpuCache            *cache);
+void                    gsk_gpu_cached_fill_finish_cache                (GskGpuCache            *cache);
+
+GskGpuImage *           gsk_gpu_cached_fill_lookup                      (GskGpuCache            *self,
+                                                                         GskGpuFrame            *frame,
+                                                                         const graphene_vec2_t  *scale,
+                                                                         const graphene_rect_t  *bounds,
+                                                                         GskPath                *path,
+                                                                         GskFillRule             fill_rule,
+                                                                         graphene_rect_t        *rect);
+
+
+G_END_DECLS

gsk/gpu/gskgpucachedprivate.h
@@ -36,6 +36,7 @@ struct _GskGpuCached
 struct _GskGpuCachePrivate
 {
   GHashTable *glyph_cache;
+  GHashTable *fill_cache;
 
   /* Vulkan-specific */
   GHashTable *ycbcr_cache;

commit ff78919d43a51d83b8b9e4157963ba972ac8c8c4

gpu: Include scale when snapping
Tests included.

Fixes: f3b56a237319b77c3eb31d2f53f65fe4c71b7fb6
Fixes: #7572
Related: !8547
Part-of: <https://gitlab.gnome.org/GNOME/gtk/-/merge_requests/8628>

gsk/gpu/gskgpucachedfill.c
@@ -149,13 +149,15 @@ fill_path_print (gpointer  data,
 
 static gsize
 mod_subpixel (float  pos,
-              gsize  scale,
+              float  scale,
+              gsize  subpixel_scale,
               float *delta)
 {
-  pos = fmod (scale * pos, scale);
+  scale *= subpixel_scale;
+  pos = fmod (scale * pos, subpixel_scale);
   *delta = (ceil (pos) - pos) / scale;
   if (pos > 0.0)
-    return scale - (gsize) ceil (pos);
+    return subpixel_scale - (gsize) ceil (pos);
   else
     return (gsize) - ceil (pos);
 }
@@ -178,8 +180,8 @@ gsk_gpu_cached_fill_lookup (GskGpuCache           *self,
   GskGpuImage *image = NULL;
   graphene_rect_t viewport;
 
-  fx = mod_subpixel (sx * bounds->origin.x, SUBPIXEL_SCALE_X, &dx);
-  fy = mod_subpixel (sy * bounds->origin.y, SUBPIXEL_SCALE_Y, &dy);
+  fx = mod_subpixel (bounds->origin.x, sx, SUBPIXEL_SCALE_X, &dx);
+  fy = mod_subpixel (bounds->origin.y, sy, SUBPIXEL_SCALE_Y, &dy);
 
   cache = g_hash_table_lookup (priv->fill_cache,
                                &(GskGpuCachedFill) {
@@ -204,7 +206,11 @@ gsk_gpu_cached_fill_lookup (GskGpuCache           *self,
     }
 
   if (!gsk_path_get_bounds (path, &viewport) ||
-      !gsk_rect_snap_to_grid (&viewport, scale, &GRAPHENE_POINT_INIT ((float)fx / SUBPIXEL_SCALE_X, (float)fy / SUBPIXEL_SCALE_Y), &viewport))
+      !gsk_rect_snap_to_grid (&viewport,
+                              scale,
+                              &GRAPHENE_POINT_INIT ((float) fx / (sx * SUBPIXEL_SCALE_X), 
+                                                    (float) fy / (sy * SUBPIXEL_SCALE_Y)),
+                              &viewport))
     return NULL;
 
   padding = 1;

gsk/gpu/gskgpucachedstroke.c
@@ -162,13 +162,15 @@ stroke_path_print (gpointer  data,
 
 static gsize
 mod_subpixel (float  pos,
-              gsize  scale,
+              float  scale,
+              gsize  subpixel_scale,
               float *delta)
 {
-  pos = fmod (scale * pos, scale);
+  scale *= subpixel_scale;
+  pos = fmod (scale * pos, subpixel_scale);
   *delta = (ceil (pos) - pos) / scale;
   if (pos > 0.0)
-    return scale - (gsize) ceil (pos);
+    return subpixel_scale - (gsize) ceil (pos);
   else
     return (gsize) - ceil (pos);
 }
@@ -191,8 +193,8 @@ gsk_gpu_cached_stroke_lookup (GskGpuCache           *self,
   GskGpuImage *image = NULL;
   graphene_rect_t viewport;
 
-  fx = mod_subpixel (sx * bounds->origin.x, SUBPIXEL_SCALE_X, &dx);
-  fy = mod_subpixel (sy * bounds->origin.y, SUBPIXEL_SCALE_Y, &dy);
+  fx = mod_subpixel (bounds->origin.x, sx, SUBPIXEL_SCALE_X, &dx);
+  fy = mod_subpixel (bounds->origin.y, sy, SUBPIXEL_SCALE_Y, &dy);
 
   cache = g_hash_table_lookup (priv->stroke_cache,
                                &(GskGpuCachedStroke) {
@@ -217,7 +219,11 @@ gsk_gpu_cached_stroke_lookup (GskGpuCache           *self,
     }
 
   if (!gsk_path_get_stroke_bounds (path, stroke, &viewport) ||
-      !gsk_rect_snap_to_grid (&viewport, scale, &GRAPHENE_POINT_INIT ((float) fx / SUBPIXEL_SCALE_X, (float) fy / SUBPIXEL_SCALE_Y), &viewport))
+      !gsk_rect_snap_to_grid (&viewport,
+                              scale,
+                              &GRAPHENE_POINT_INIT ((float) fx / (sx * SUBPIXEL_SCALE_X), 
+                                                    (float) fy / (sy * SUBPIXEL_SCALE_Y)),
+                              &viewport))
     return NULL;
 
   padding = 1;

testsuite/gsk/compare/fill-scale-alignment-nocairo.node
@@ -1,0 +1,17 @@
+color {
+  bounds: 0 0 16 16;
+  color: black;
+}
+
+transform {
+  /* 0.0009765625 = 1/1024 */
+  transform: scale(16) translate(0.0009765625, 0.0009765625);
+  child: fill {
+    child: color {
+      bounds: 0 0 1 1;
+      color: red;
+    }
+    path: "M 0.25 0.25 h 0.5 v 0.5 h -0.5 z";
+    fill-rule: winding;
+  }
+}

testsuite/gsk/compare/stroke-scale-alignment-nocairo.node
@@ -1,0 +1,19 @@
+color {
+  bounds: 0 0 16 16;
+  color: black;
+}
+
+transform {
+  /* 0.0009765625 = 1/1024 */
+  transform: scale(16) translate(0.0009765625, 0.0009765625);
+  child: stroke {
+    child: color {
+      bounds: 0 0 1 1;
+      color: red;
+    }
+    path: "M 0.3125 0.3125 h 0.375 v 0.375 h -0.375 z";
+    line-width: 0.125;
+    line-cap: square;
+    line-join: miter;
+  }
+}

testsuite/gsk/meson.build
@@ -91,6 +91,7 @@ compare_render_tests = [
   'fill-huge-path',
   'fill-node-without-path',
   'fill-opacity',
+  'fill-scale-alignment-nocairo',
   'fill-scaled-up',
   'fill-scale-up-with-bad-clip',
   'fill-subpixel-offsets-horizontal-nocairo',
@@ -199,6 +200,7 @@ compare_render_tests = [
   'stroke-fractional-translate',
   'stroke-huge-path',
   'stroke-opacity',
+  'stroke-scale-alignment-nocairo',
   'stroke-with-3d-contents-nocairo',
   'subpixel-positioning',
   'subpixel-positioning-hidpi-nocairo',