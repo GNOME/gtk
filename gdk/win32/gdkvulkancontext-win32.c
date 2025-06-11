GNOME/gtk

commit f278261ade0ae0accbf0961af3cfa95c75f4bd84

win32: Update VulkanContext to use DirectComposition
We create a custom HWND now that we use eclusively for rendering.
In fact, we don't even listen for evevnts and just use DefWindowProc.

The Window is set to use WS_EX_LAYERED so it can support transparency,
however many drivers seem to ignore this and only advertise OPAQUE
swapchains.

Part-of: <https://gitlab.gnome.org/GNOME/gtk/-/merge_requests/8559>

gdk/win32/gdkvulkancontext-win32.c
@@ -27,32 +27,63 @@
 
 #include "gdkvulkancontext-win32.h"
 
+#include "gdkdisplay-win32.h"
 #include "gdkprivate-win32.h"
 #include "gdkwin32misc.h"
 
+#include <dwmapi.h>
+
+struct _GdkWin32VulkanContext
+{
+  GdkVulkanContext parent_instance;
+
+  HWND handle;
+};
+
+struct _GdkWin32VulkanContextClass
+{
+  GdkVulkanContextClass parent_class;
+};
+
 G_DEFINE_TYPE (GdkWin32VulkanContext, gdk_win32_vulkan_context, GDK_TYPE_VULKAN_CONTEXT)
+
+static ATOM
+gdk_win32_vulkan_context_get_class (void)
+{
+  static ATOM class_atom = 0;
+
+  if (class_atom)
+    return class_atom;
+
+  class_atom = RegisterClassExW (&(WNDCLASSEX) {
+                                     .cbSize = sizeof (WNDCLASSEX),
+                                     .style = CS_HREDRAW | CS_VREDRAW,
+                                     .lpfnWndProc = DefWindowProc,
+                                     .hInstance = this_module (),
+                                     .lpszClassName  = L"GdkWin32Vulkan",
+                                 });
+  if (class_atom == 0)
+    WIN32_API_FAILED ("RegisterClassExW");
+
+  return class_atom;
+}
 
 static VkResult
 gdk_win32_vulkan_context_create_surface (GdkVulkanContext *context,
-                                         VkSurfaceKHR     *surface)
+                                         VkSurfaceKHR     *vk_surface)
 {
-  GdkSurface *gdk_surface = gdk_draw_context_get_surface (GDK_DRAW_CONTEXT (context));
-  VkWin32SurfaceCreateInfoKHR info;
-  VkResult result;
+  GdkWin32VulkanContext *self = GDK_WIN32_VULKAN_CONTEXT (context);
 
-  info.sType = VK_STRUCTURE_TYPE_WIN32_SURFACE_CREATE_INFO_KHR;
-  info.pNext = NULL;
-  info.flags = 0;
-  info.hinstance = this_module ();
-  info.hwnd = GDK_SURFACE_HWND (gdk_surface);
-
-  result = GDK_VK_CHECK (vkCreateWin32SurfaceKHR,
-                         gdk_vulkan_context_get_instance (context),
-                         &info,
-                         NULL,
-                         surface);
-
-  return result;
+  return GDK_VK_CHECK (vkCreateWin32SurfaceKHR,
+                       gdk_vulkan_context_get_instance (context),
+                       (&(VkWin32SurfaceCreateInfoKHR) {
+                           .pNext = NULL,
+                           .sType = VK_STRUCTURE_TYPE_WIN32_SURFACE_CREATE_INFO_KHR,
+                           .hinstance = this_module (),
+                           .hwnd = self->handle
+                       }),
+                       NULL,
+                       vk_surface);
 }
 
 static void
@@ -60,15 +91,139 @@ gdk_win32_vulkan_context_empty_frame (GdkDrawContext *draw_context)
 {
 }
 
+static gboolean
+gdk_win32_vulkan_context_surface_attach (GdkDrawContext  *context,
+                                         GError         **error)
+{
+  GdkWin32VulkanContext *self = GDK_WIN32_VULKAN_CONTEXT (context);
+  GdkDrawContext *draw_context = GDK_DRAW_CONTEXT (context);
+  GdkSurface *surface;
+  GdkWin32Display *display;
+  VkResult result;
+  IUnknown *dcomp_surface;
+  guint width, height;
+
+  surface = gdk_draw_context_get_surface (draw_context);
+  display = GDK_WIN32_DISPLAY (gdk_draw_context_get_display (draw_context));
+
+  if (!gdk_win32_display_get_dcomp_device (display))
+    {
+      g_set_error (error, GDK_VULKAN_ERROR, GDK_VULKAN_ERROR_NOT_AVAILABLE, "Vulkan requires Direct Composition");
+      return FALSE;
+    }
+  
+  gdk_draw_context_get_buffer_size (draw_context, &width, &height);
+  
+  self->handle = CreateWindowExW (0,
+                                  MAKEINTRESOURCEW (gdk_win32_vulkan_context_get_class ()),
+                                  NULL,
+                                  WS_POPUP,
+                                  0, 0,
+                                  width, height,
+                                  GDK_SURFACE_HWND (surface),
+                                  NULL,
+                                  this_module (),
+                                  self);
+  if (self->handle == NULL)
+    {
+      DWORD err = GetLastError ();
+      gdk_win32_check_hresult (HRESULT_FROM_WIN32 (err), error, "Failed to create rendering window");
+      return FALSE;
+    }
+  
+  hr_warn (DwmSetWindowAttribute (self->handle, DWMWA_CLOAK, (BOOL[1]) { true }, sizeof (BOOL)));
+
+  if (!GDK_DRAW_CONTEXT_CLASS (gdk_win32_vulkan_context_parent_class)->surface_attach (context, error))
+    {
+      if (!DestroyWindow (self->handle))
+        WIN32_API_FAILED ("DestroyWindow");
+      return FALSE;
+    }
+
+  ShowWindow (self->handle, SW_SHOWNOACTIVATE);
+
+  hr_warn (IDCompositionDevice_CreateSurfaceFromHwnd (gdk_win32_display_get_dcomp_device (display),
+                                                      self->handle,
+                                                      &dcomp_surface));
+  gdk_win32_surface_set_dcomp_content (GDK_WIN32_SURFACE (surface), dcomp_surface);
+
+  return TRUE;
+}
+
+static void
+gdk_win32_vulkan_context_surface_detach (GdkDrawContext *context)
+{
+  GdkWin32VulkanContext *self = GDK_WIN32_VULKAN_CONTEXT (context);
+  GdkSurface *surface = gdk_draw_context_get_surface (context);
+
+  GDK_DRAW_CONTEXT_CLASS (gdk_win32_vulkan_context_parent_class)->surface_detach (context);
+
+  if (!GDK_SURFACE_DESTROYED (surface))
+    {
+      gdk_win32_surface_set_dcomp_content (GDK_WIN32_SURFACE (surface), NULL);
+
+      if (!DestroyWindow (self->handle))
+        WIN32_API_FAILED ("DestroyWindow");
+    }
+}
+
+static void
+gdk_win32_vulkan_context_surface_resized (GdkDrawContext *draw_context)
+{
+  GdkWin32VulkanContext *self = GDK_WIN32_VULKAN_CONTEXT (draw_context);
+  guint width, height;
+
+  if (self->handle)
+    {
+      gdk_draw_context_get_buffer_size (draw_context, &width, &height);
+
+      API_CALL (SetWindowPos, (self->handle,
+                              SWP_NOZORDER_SPECIFIED,
+                              0,
+                              0,
+                              width, height,
+                              SWP_NOACTIVATE | SWP_NOMOVE | SWP_NOCOPYBITS | SWP_NOZORDER));
+    }
+
+  GDK_DRAW_CONTEXT_CLASS (gdk_win32_vulkan_context_parent_class)->surface_resized (draw_context);
+}
+
+static void
+gdk_win32_vulkan_context_dispose (GObject *object)
+{
+  GdkWin32VulkanContext *self = GDK_WIN32_VULKAN_CONTEXT (object);
+
+  if (self->handle)
+    {
+      GdkSurface *surface = gdk_draw_context_get_surface (GDK_DRAW_CONTEXT (self));
+
+      if (!GDK_SURFACE_DESTROYED (surface))
+        {
+          gdk_win32_surface_set_dcomp_content (GDK_WIN32_SURFACE (surface), NULL);
+
+          if (!DestroyWindow (self->handle))
+            WIN32_API_FAILED ("DestroyWindow");
+        }
+    }
+
+  G_OBJECT_CLASS (gdk_win32_vulkan_context_parent_class)->dispose (object);
+}
+
 static void
 gdk_win32_vulkan_context_class_init (GdkWin32VulkanContextClass *klass)
 {
   GdkVulkanContextClass *context_class = GDK_VULKAN_CONTEXT_CLASS (klass);
   GdkDrawContextClass *draw_context_class = GDK_DRAW_CONTEXT_CLASS (klass);
+  GObjectClass *object_class = G_OBJECT_CLASS (klass);
+
+  object_class->dispose = gdk_win32_vulkan_context_dispose;
 
   context_class->create_surface = gdk_win32_vulkan_context_create_surface;
 
   draw_context_class->empty_frame = gdk_win32_vulkan_context_empty_frame;
+  draw_context_class->surface_attach = gdk_win32_vulkan_context_surface_attach;
+  draw_context_class->surface_detach = gdk_win32_vulkan_context_surface_detach;
+  draw_context_class->surface_resized = gdk_win32_vulkan_context_surface_resized;
 }
 
 static void

gdk/win32/gdkvulkancontext-win32.h
@@ -41,16 +41,6 @@ G_BEGIN_DECLS
 typedef struct _GdkWin32VulkanContext      GdkWin32VulkanContext;
 typedef struct _GdkWin32VulkanContextClass GdkWin32VulkanContextClass;
 
-struct _GdkWin32VulkanContext
-{
-  GdkVulkanContext parent_instance;
-};
-
-struct _GdkWin32VulkanContextClass
-{
-  GdkVulkanContextClass parent_class;
-};
-
 GDK_AVAILABLE_IN_ALL
 GType gdk_win32_vulkan_context_get_type (void) G_GNUC_CONST;
 

commit 061bb7da381eefebae9621a2951dad6a2a6a2e16

win32: Remove leftover code
The dispose() call cleanup is now done in detach().

Apparently I missed deleting the dispose function while refactoring.

Fixes: f278261ade0ae0accbf0961af3cfa95c75f4bd84
Fixes: #7589
Part-of: <https://gitlab.gnome.org/GNOME/gtk/-/merge_requests/8675>

gdk/win32/gdkvulkancontext-win32.c
@@ -189,34 +189,10 @@ gdk_win32_vulkan_context_surface_resized (GdkDrawContext *draw_context)
 }
 
 static void
-gdk_win32_vulkan_context_dispose (GObject *object)
-{
-  GdkWin32VulkanContext *self = GDK_WIN32_VULKAN_CONTEXT (object);
-
-  if (self->handle)
-    {
-      GdkSurface *surface = gdk_draw_context_get_surface (GDK_DRAW_CONTEXT (self));
-
-      if (!GDK_SURFACE_DESTROYED (surface))
-        {
-          gdk_win32_surface_set_dcomp_content (GDK_WIN32_SURFACE (surface), NULL);
-
-          if (!DestroyWindow (self->handle))
-            WIN32_API_FAILED ("DestroyWindow");
-        }
-    }
-
-  G_OBJECT_CLASS (gdk_win32_vulkan_context_parent_class)->dispose (object);
-}
-
-static void
 gdk_win32_vulkan_context_class_init (GdkWin32VulkanContextClass *klass)
 {
   GdkVulkanContextClass *context_class = GDK_VULKAN_CONTEXT_CLASS (klass);
   GdkDrawContextClass *draw_context_class = GDK_DRAW_CONTEXT_CLASS (klass);
-  GObjectClass *object_class = G_OBJECT_CLASS (klass);
-
-  object_class->dispose = gdk_win32_vulkan_context_dispose;
 
   context_class->create_surface = gdk_win32_vulkan_context_create_surface;
 