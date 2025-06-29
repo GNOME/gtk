GNOME/gtk

commit 0b03c105a8f45aadf5e2f872317ab13e58e98e62

wayland: More debug spew for color
Report some details if we decide not to attach a texture due
to its color state.

Part-of: <https://gitlab.gnome.org/GNOME/gtk/-/merge_requests/8645>

gdk/wayland/gdksubsurface-wayland.c
@@ -519,6 +519,7 @@ gdk_wayland_subsurface_attach (GdkSubsurface         *sub,
   gboolean premultiplied = TRUE;
   gboolean was_transparent;
   gboolean is_transparent;
+  GError *error = NULL;
 
   if (sub->parent == NULL)
     {
@@ -615,14 +616,17 @@ gdk_wayland_subsurface_attach (GdkSubsurface         *sub,
            (!get_texture_info (self, texture, &fourcc, &premultiplied, &dmabuf) ||
            !gdk_wayland_color_surface_can_set_color_state (self->color,
                                                            gdk_texture_get_color_state (texture),
-                                                           fourcc, premultiplied)))
+                                                           fourcc, premultiplied,
+                                                           &error)))
     {
       gdk_dmabuf_close_fds (&dmabuf);
       GDK_DISPLAY_DEBUG (gdk_surface_get_display (sub->parent), OFFLOAD,
-                         "[%p] 🗙 Texture colorstate %s (%.4s, %s) not supported",
+                         "[%p] 🗙 Texture colorstate %s (%.4s, %s): %s",
                          self,
                          gdk_color_state_get_name (gdk_texture_get_color_state (texture)),
-                         (char *) &fourcc, premultiplied ? "premultiplied" : "straight");
+                         (char *) &fourcc, premultiplied ? "premultiplied" : "straight",
+                         error->message);
+      g_error_free (error);
     }
   else
     {

gdk/wayland/gdksurface-wayland.c
@@ -738,7 +738,8 @@ gdk_wayland_surface_sync_color_state (GdkSurface *surface)
   if (gdk_wayland_color_surface_can_set_color_state (self->display_server.color,
                                                      gdk_surface_get_color_state (surface),
                                                      DRM_FORMAT_RGBA8888,
-                                                     TRUE))
+                                                     TRUE,
+                                                     NULL))
     {
       gdk_wayland_color_surface_set_color_state (self->display_server.color,
                                                  gdk_surface_get_color_state (surface),

gdk/wayland/gdkwaylandcolor-private.h
@@ -40,10 +40,11 @@ GdkWaylandColorSurface *
 
 void              gdk_wayland_color_surface_free                (GdkWaylandColorSurface *self);
 
-gboolean          gdk_wayland_color_surface_can_set_color_state (GdkWaylandColorSurface *self,
-                                                                 GdkColorState          *cs,
-                                                                 guint32                 fourcc,
-                                                                 gboolean                premultiplied);
+gboolean          gdk_wayland_color_surface_can_set_color_state (GdkWaylandColorSurface  *self,
+                                                                 GdkColorState           *cs,
+                                                                 guint32                  fourcc,
+                                                                 gboolean                 premultiplied,
+                                                                 GError                 **error);
 void              gdk_wayland_color_surface_set_color_state     (GdkWaylandColorSurface *self,
                                                                  GdkColorState          *cs,
                                                                  guint32                 fourcc,

gdk/wayland/gdkwaylandcolor.c
@@ -1273,45 +1273,65 @@ gdk_wayland_color_surface_unset_color_state (GdkWaylandColorSurface *self)
 }
 
 gboolean
-gdk_wayland_color_surface_can_set_color_state (GdkWaylandColorSurface *self,
-                                               GdkColorState          *cs,
-                                               guint32                 fourcc,
-                                               gboolean                premultiplied)
+gdk_wayland_color_surface_can_set_color_state (GdkWaylandColorSurface  *self,
+                                               GdkColorState           *cs,
+                                               guint32                  fourcc,
+                                               gboolean                 premultiplied,
+                                               GError                 **error)
 {
   GdkMemoryFormat format;
   gboolean is_yuv;
   GdkColorState *default_cs;
 
   if (!gdk_memory_format_find_by_dmabuf_fourcc (fourcc, premultiplied, &format, &is_yuv))
-    return FALSE;
+    {
+      g_set_error_literal (error, G_IO_ERROR, G_IO_ERROR_FAILED, "No memory format for fourcc");
+      return FALSE;
+    }
 
   default_cs = is_yuv ? GDK_COLOR_STATE_YUV : GDK_COLOR_STATE_SRGB;
 
   if (!self->mgmt_surface)
     {
       if (!gdk_color_state_equivalent (cs, default_cs))
-        return FALSE;
+        {
+          g_set_error_literal (error, G_IO_ERROR, G_IO_ERROR_FAILED, "No color-management, non-default color state");
+          return FALSE;
+        }
     }
   else
     {
       if (!gdk_wayland_color_get_image_description (self->color, cs))
-        return FALSE;
+        {
+          g_set_error_literal (error, G_IO_ERROR, G_IO_ERROR_FAILED, "No image description");
+          return FALSE;
+        }
     }
 
   if (!self->color->color_representation_manager)
     {
       if (!gdk_color_state_equivalent (cs, default_cs))
-        return FALSE;
+        {
+          g_set_error_literal (error, G_IO_ERROR, G_IO_ERROR_FAILED, "No color-representation, non-default color state");
+          return FALSE;
+        }
 
       if (gdk_memory_format_alpha (format) == GDK_MEMORY_ALPHA_STRAIGHT)
-        return FALSE;
+        {
+          g_set_error_literal (error, G_IO_ERROR, G_IO_ERROR_FAILED, "No color-representation, straight alpha");
+          return FALSE;
+        }
     }
   else
     {
       uint32_t coefficients, range, alpha;
 
       if (!gdk_wayland_color_get_color_representation (self->color, cs, format, &coefficients, &range, &alpha))
-        return FALSE;
+        {
+          g_set_error_literal (error, G_IO_ERROR, G_IO_ERROR_FAILED, coefficients == 0 ? "Coefficients not supported"
+                                                                                       : "Alpha not supported");
+          return FALSE;
+        }
     }
 
   return TRUE;

commit 66268164aa17b657e903392bee28ae1a5fba04e8

wayland: Split up some debug spew
get_texture_info() notably fails for textures build with
GdkMemoryTextureBuilder, causing assertions in the following
g_error_free().

Fixes: 0b03c105a8 ("wayland: More debug spew for color")
Part-of: <https://gitlab.gnome.org/GNOME/gtk/-/merge_requests/8738>

gdk/wayland/gdksubsurface-wayland.c
@@ -613,11 +613,17 @@ gdk_wayland_subsurface_attach (GdkSubsurface         *sub,
                          self);
     }
   else if (texture && texture != self->texture &&
-           (!get_texture_info (self, texture, &fourcc, &premultiplied, &dmabuf) ||
+           !get_texture_info (self, texture, &fourcc, &premultiplied, &dmabuf))
+    {
+      GDK_DISPLAY_DEBUG (gdk_surface_get_display (sub->parent), OFFLOAD,
+                         "[%p] 🗙 Texture type not supported or export failed",
+                         self);
+    }
+  else if (texture && texture != self->texture &&
            !gdk_wayland_color_surface_can_set_color_state (self->color,
                                                            gdk_texture_get_color_state (texture),
                                                            fourcc, premultiplied,
-                                                           &error)))
+                                                           &error))
     {
       gdk_dmabuf_close_fds (&dmabuf);
       GDK_DISPLAY_DEBUG (gdk_surface_get_display (sub->parent), OFFLOAD,