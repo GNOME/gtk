5754924e51c2fc9eb5264e470d4784793c6b25af
Doublonmousse <e0949142@u.nus.edu>
2025-08-02 17:37:44 +0200

    `gdksurface-win32: compute_toplevel_size` test `monitor` first in if statements
    Part-of: <https://gitlab.gnome.org/GNOME/gtk/-/merge_requests/8791>

--- gdk/win32/gdksurface-win32.c
+++ gdk/win32/gdksurface-win32.c
@@ -2784,12 +2784,16 @@ compute_toplevel_size (GdkSurface *surface,
       gboolean is_fullscreened;
       GdkRectangle monitor_geometry;
 
-      if (gdk_toplevel_layout_get_maximized (layout, &is_maximized) && is_maximized && monitor) 
+      if (monitor && 
+          gdk_toplevel_layout_get_maximized (layout, &is_maximized) &&
+          is_maximized) 
         {
           geometry.min_width = bounds_width;
           geometry.min_height = bounds_height;
           mask = GDK_HINT_MIN_SIZE;
-      } else if (gdk_toplevel_layout_get_fullscreen (layout, &is_fullscreened) && is_fullscreened && monitor) 
+      } else if (monitor && 
+        gdk_toplevel_layout_get_fullscreen (layout, &is_fullscreened) && 
+        is_fullscreened) 
         {
           gdk_monitor_get_geometry (monitor, &monitor_geometry);
           geometry.min_width = monitor_geometry.width;
