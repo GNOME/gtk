6c1d5bffc97e0b81fbab50b831f5350d0f674463
Matthias Clasen <mclasen@redhat.com>
2025-10-13 19:20:45 -0400

    icon editor: Add a state editor
    This is a little easier than clicking through the states
    one by one.
    
    Part-of: <https://gitlab.gnome.org/GNOME/gtk/-/merge_requests/9034>

---
+++ demos/icon-editor/state-editor.h
@@ -0,0 +1,37 @@
+/*
+ * Copyright © 2025 Red Hat, Inc
+ *
+ * This library is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Lesser General Public
+ * License as published by the Free Software Foundation; either
+ * version 2.1 of the License, or (at your option) any later version.
+ *
+ * This library is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public
+ * License along with this library. If not, see <http://www.gnu.org/licenses/>.
+ *
+ * SPDX-License-Identifier: LGPL-2.1-or-later
+ *
+ * Authors: Matthias Clasen <mclasen@redhat.com>
+ */
+
+#pragma once
+
+#include <gtk/gtk.h>
+
+#include "path-paintable.h"
+
+
+#define STATE_EDITOR_TYPE (state_editor_get_type ())
+G_DECLARE_FINAL_TYPE (StateEditor, state_editor, STATE, EDITOR, GtkWindow)
+
+
+StateEditor *   state_editor_new                (void);
+
+void            state_editor_set_paintable      (StateEditor   *self,
+                                                 PathPaintable *paintable);
+PathPaintable * state_editor_get_paintable      (StateEditor   *self);
