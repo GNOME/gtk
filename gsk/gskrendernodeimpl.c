GNOME/gtk

commit 42d0ddef76c7e4c8d3f2f2418733347ab7ea5bc3

gsk: Add a component transfer node
This implements the necessary plumbing for per-component filtering
inspired by
https://www.w3.org/TR/filter-effects-1/#feComponentTransferElement

Some tests included. They don't cover all modes, since gamma and
table cause hard-to-avoid rounding differences with software rendering.

Part-of: <https://gitlab.gnome.org/GNOME/gtk/-/merge_requests/8785>

gsk/gskrendernodeimpl.c
@@ -30,6 +30,7 @@
 #include "gskroundedrectprivate.h"
 #include "gskstrokeprivate.h"
 #include "gsktransformprivate.h"
+#include "gskcomponenttransferprivate.h"
 #include "gskprivate.h"
 #include "gpu/gskglrenderer.h"
 
@@ -8633,6 +8634,228 @@ gsk_subsurface_node_get_subsurface (const GskRenderNode *node)
 }
 
 /* }}} */
+/* {{{ GSK_COMPONENT_TRANSFER_NODE */
+
+struct _GskComponentTransferNode
+{
+  GskRenderNode render_node;
+
+  GskRenderNode *child;
+  GskComponentTransfer transfer[4];
+};
+
+static void
+gsk_component_transfer_node_finalize (GskRenderNode *node)
+{
+  GskComponentTransferNode *self = (GskComponentTransferNode *) node;
+  GskRenderNodeClass *parent_class = g_type_class_peek (g_type_parent (GSK_TYPE_COMPONENT_TRANSFER_NODE));
+
+  gsk_render_node_unref (self->child);
+
+  parent_class->finalize (node);
+}
+
+static void
+gsk_component_transfer_node_draw (GskRenderNode *node,
+                                  cairo_t       *cr,
+                                  GdkColorState *ccs)
+{
+  GskComponentTransferNode *self = (GskComponentTransferNode *) node;
+  int width, height;
+  cairo_surface_t *surface;
+  cairo_t *cr2;
+  guchar *data;
+  gsize stride;
+  guint32 pixel;
+  float r, g, b, a;
+  cairo_pattern_t *pattern;
+
+  width = ceil (node->bounds.size.width);
+  height = ceil (node->bounds.size.height);
+
+  surface = cairo_image_surface_create (CAIRO_FORMAT_ARGB32, width, height);
+
+  cr2 = cairo_create (surface);
+  gsk_render_node_draw (self->child, cr2);
+  cairo_destroy (cr2);
+
+  data = cairo_image_surface_get_data (surface);
+  stride = cairo_image_surface_get_stride (surface);
+
+  for (guint y = 0; y < height; y++)
+    {
+      for (guint x = 0; x < width; x++)
+        {
+          pixel = *(guint32 *)(data + y * stride + 4 * x);
+
+          a = ((pixel >> 24) & 0xff) / 255.;
+          r = ((pixel >> 16) & 0xff) / 255.;
+          g = ((pixel >> 8) & 0xff) / 255.;
+          b = ((pixel >> 0) & 0xff) / 255.;
+
+          r /= a;
+          g /= a;
+          b /= a;
+
+          r = gsk_component_transfer_apply (&self->transfer[0], r);
+          g = gsk_component_transfer_apply (&self->transfer[1], g);
+          b = gsk_component_transfer_apply (&self->transfer[2], b);
+          a = gsk_component_transfer_apply (&self->transfer[3], a);
+
+          r *= a;
+          g *= a;
+          b *= a;
+
+          pixel = CLAMP ((int) roundf (a * 255), 0, 255) << 24 |
+                  CLAMP ((int) roundf (r * 255), 0, 255) << 16 |
+                  CLAMP ((int) roundf (g * 255), 0, 255) << 8 |
+                  CLAMP ((int) roundf (b * 255), 0, 255) << 0;
+
+          *(guint32 *)(data + y * stride + 4 * x) = pixel;
+        }
+    }
+
+  pattern = cairo_pattern_create_for_surface (surface);
+  cairo_pattern_set_extend (pattern, CAIRO_EXTEND_PAD);
+
+  cairo_set_source (cr, pattern);
+  cairo_pattern_destroy (pattern);
+  cairo_surface_destroy (surface);
+
+  gdk_cairo_rect (cr, &node->bounds);
+  cairo_fill (cr);
+}
+
+static gboolean
+gsk_component_transfer_node_can_diff (const GskRenderNode *node1,
+                                      const GskRenderNode *node2)
+{
+  GskComponentTransferNode *self1 = (GskComponentTransferNode *) node1;
+  GskComponentTransferNode *self2 = (GskComponentTransferNode *) node2;
+
+  return gsk_component_transfer_equal (&self1->transfer[0], &self2->transfer[0]) &&
+         gsk_component_transfer_equal (&self1->transfer[1], &self2->transfer[1]) &&
+         gsk_component_transfer_equal (&self1->transfer[2], &self2->transfer[2]) &&
+         gsk_component_transfer_equal (&self1->transfer[3], &self2->transfer[3]);
+}
+
+static void
+gsk_component_transfer_node_diff (GskRenderNode *node1,
+                                  GskRenderNode *node2,
+                                  GskDiffData   *data)
+{
+  GskComponentTransferNode *self1 = (GskComponentTransferNode *) node1;
+  GskComponentTransferNode *self2 = (GskComponentTransferNode *) node2;
+
+  if (gsk_component_transfer_node_can_diff (node1, node2))
+    gsk_render_node_diff (self1->child, self2->child, data);
+
+  gsk_render_node_diff_impossible (node1, node2, data);
+}
+
+static void
+gsk_component_transfer_node_class_init (gpointer g_class,
+                                        gpointer class_data)
+{
+  GskRenderNodeClass *node_class = g_class;
+
+  node_class->node_type = GSK_COMPONENT_TRANSFER_NODE;
+
+  node_class->finalize = gsk_component_transfer_node_finalize;
+  node_class->draw = gsk_component_transfer_node_draw;
+  node_class->can_diff = gsk_component_transfer_node_can_diff;
+  node_class->diff = gsk_component_transfer_node_diff;
+}
+
+/**
+ * gsk_component_transfer_node_new:
+ * @child: The child to apply component transfers to
+ * @r: transfer for the red component
+ * @g: transfer for the green component
+ * @b: transfer for the blue component
+ * @a: transfer for the alpha component
+ *
+ * Creates a render node that will apply component
+ * transfers to a child node.
+ *
+ * Returns: (transfer full) (type GskComponentTransferNode): A new `GskRenderNode`
+ *
+ * Since: 4.20
+ */
+GskRenderNode *
+gsk_component_transfer_node_new (GskRenderNode              *child,
+                                 const GskComponentTransfer *r,
+                                 const GskComponentTransfer *g,
+                                 const GskComponentTransfer *b,
+                                 const GskComponentTransfer *a)
+{
+  GskComponentTransferNode *self;
+  GskRenderNode *node;
+
+  g_return_val_if_fail (GSK_IS_RENDER_NODE (child), NULL);
+
+  self = gsk_render_node_alloc (GSK_COMPONENT_TRANSFER_NODE);
+  node = (GskRenderNode *) self;
+  node->offscreen_for_opacity = child->offscreen_for_opacity;
+  node->fully_opaque = FALSE;
+
+  self->child = gsk_render_node_ref (child);
+
+  gsk_component_transfer_init_copy (&self->transfer[0], r);
+  gsk_component_transfer_init_copy (&self->transfer[1], g);
+  gsk_component_transfer_init_copy (&self->transfer[2], b);
+  gsk_component_transfer_init_copy (&self->transfer[3], a);
+
+  gsk_rect_init_from_rect (&node->bounds, &child->bounds);
+
+  node->preferred_depth = gsk_render_node_get_preferred_depth (child);
+  node->is_hdr = gsk_render_node_is_hdr (child);
+
+  return node;
+}
+
+/**
+ * gsk_component_transfer_node_get_child:
+ * @node: (type GskComponentTransferNode): a debug `GskRenderNode`
+ *
+ * Gets the child node that is getting drawn by the given @node.
+ *
+ * Returns: (transfer none): the child `GskRenderNode`
+ *
+ * Since: 4.20
+ */
+GskRenderNode *
+gsk_component_transfer_node_get_child (const GskRenderNode *node)
+{
+  const GskComponentTransferNode *self = (const GskComponentTransferNode *) node;
+
+  return self->child;
+}
+
+/**
+ * gsk_component_transfer_node_get_transfer:
+ * @node: (type GskComponentTransferNode): a debug `GskRenderNode`
+ * @component: a value between 0 and 3 to indicate the red, green, blue
+ *   or alpha component
+ *
+ * Gets the component transfer for one of the components.
+ *
+ * Returns: (transfer none): the `GskComponentTransfer`
+ *
+ * Since: 4.20
+ */
+const GskComponentTransfer *
+gsk_component_transfer_node_get_transfer (const GskRenderNode *node,
+                                          guint                component)
+{
+  const GskComponentTransferNode *self = (const GskComponentTransferNode *) node;
+
+  g_return_val_if_fail (component < 4, NULL);
+
+  return &self->transfer[component];
+}
+
+/* }}} */
 /* {{{ Serialization */
 
 static void
@@ -8897,6 +9120,7 @@ GSK_DEFINE_RENDER_NODE_TYPE (gsk_mask_node, GSK_MASK_NODE)
 GSK_DEFINE_RENDER_NODE_TYPE (gsk_gl_shader_node, GSK_GL_SHADER_NODE)
 GSK_DEFINE_RENDER_NODE_TYPE (gsk_debug_node, GSK_DEBUG_NODE)
 GSK_DEFINE_RENDER_NODE_TYPE (gsk_subsurface_node, GSK_SUBSURFACE_NODE)
+GSK_DEFINE_RENDER_NODE_TYPE (gsk_component_transfer_node, GSK_COMPONENT_TRANSFER_NODE)
 
 #define GSK_INIT_NODE_TYPE_WITH_STRUCT(TYPE, T_N, S_N, t_n) \
   gsk_render_node_types[TYPE] = gsk_render_node_type_register_static (I_(#T_N), \
@@ -8940,6 +9164,7 @@ G_GNUC_END_IGNORE_DEPRECATIONS
   GSK_INIT_NODE_TYPE (GSK_FILL_NODE, GskFillNode, gsk_fill_node)
   GSK_INIT_NODE_TYPE (GSK_STROKE_NODE, GskStrokeNode, gsk_stroke_node)
   GSK_INIT_NODE_TYPE (GSK_SUBSURFACE_NODE, GskSubsurfaceNode, gsk_subsurface_node)
+  GSK_INIT_NODE_TYPE (GSK_COMPONENT_TRANSFER_NODE, GskComponentTransferNode, gsk_component_transfer_node)
 }
 
 /*< private >

gsk/broadway/gskbroadwayrenderer.c
@@ -292,6 +292,7 @@ collect_reused_child_nodes (GskRenderer *renderer,
     case GSK_FILL_NODE:
     case GSK_STROKE_NODE:
     case GSK_SUBSURFACE_NODE:
+    case GSK_COMPONENT_TRANSFER_NODE:
 
     default:
 
@@ -890,6 +891,7 @@ gsk_broadway_renderer_add_node (GskRenderer *renderer,
     case GSK_GL_SHADER_NODE:
     case GSK_FILL_NODE:
     case GSK_STROKE_NODE:
+    case GSK_COMPONENT_TRANSFER_NODE:
     default:
       break; /* Fallback */
     }

gsk/gpu/gskgpucomponenttransferop.c
@@ -1,0 +1,170 @@
+#include "config.h"
+
+#include "gskgpucomponenttransferopprivate.h"
+
+#include "gskgpuframeprivate.h"
+#include "gskgpuprintprivate.h"
+#include "gskrectprivate.h"
+#include "gskcomponenttransferprivate.h"
+
+#include "gpu/shaders/gskgpucomponenttransferinstance.h"
+
+typedef struct _GskGpuComponentTransferOp GskGpuComponentTransferOp;
+
+struct _GskGpuComponentTransferOp
+{
+  GskGpuShaderOp op;
+};
+
+static void
+gsk_gpu_component_transfer_op_print_instance (GskGpuShaderOp *shader,
+                                              gpointer        instance_,
+                                              GString        *string)
+{
+  GskGpuComponenttransferInstance *instance = (GskGpuComponenttransferInstance *) instance_;
+
+  gsk_gpu_print_rect (string, instance->rect);
+  gsk_gpu_print_image (string, shader->images[0]);
+}
+
+static const GskGpuShaderOpClass GSK_GPU_COMPONENT_TRANSFER_OP_CLASS = {
+  {
+    GSK_GPU_OP_SIZE (GskGpuComponentTransferOp),
+    GSK_GPU_STAGE_SHADER,
+    gsk_gpu_shader_op_finish,
+    gsk_gpu_shader_op_print,
+#ifdef GDK_RENDERING_VULKAN
+    gsk_gpu_shader_op_vk_command,
+#endif
+    gsk_gpu_shader_op_gl_command
+  },
+  "gskgpucomponenttransfer",
+  gsk_gpu_componenttransfer_n_textures,
+  sizeof (GskGpuComponenttransferInstance),
+#ifdef GDK_RENDERING_VULKAN
+  &gsk_gpu_componenttransfer_info,
+#endif
+  gsk_gpu_component_transfer_op_print_instance,
+  gsk_gpu_componenttransfer_setup_attrib_locations,
+  gsk_gpu_componenttransfer_setup_vao
+};
+
+static void
+set_param (GskGpuComponenttransferInstance *instance,
+           guint                            coord,
+           guint                            idx,
+           float                            value)
+{
+  switch (coord)
+    {
+    case 0:
+      instance->params_r[idx] = value;
+      break;
+    case 1:
+      instance->params_g[idx] = value;
+      break;
+    case 2:
+      instance->params_b[idx] = value;
+      break;
+    case 3:
+      instance->params_a[idx] = value;
+      break;
+    default:
+      g_assert_not_reached ();
+    }
+}
+
+static void
+set_table_value (GskGpuComponenttransferInstance *instance,
+                 guint                            idx,
+                 float                            value)
+{
+  if (idx < 4)
+    instance->table0[idx] = value;
+  else if (idx < 8)
+    instance->table1[idx - 4] = value;
+  else if (idx < 12)
+    instance->table2[idx - 8] = value;
+  else if (idx < 16)
+    instance->table3[idx - 12] = value;
+  else if (idx < 20)
+    instance->table4[idx - 16] = value;
+  else if (idx < 24)
+    instance->table5[idx - 20] = value;
+  else if (idx < 28)
+    instance->table6[idx - 24] = value;
+  else if (idx < 32)
+    instance->table7[idx - 28] = value;
+}
+
+static void
+copy_component_transfer (const GskComponentTransfer      *transfer,
+                         guint                            coord,
+                         GskGpuComponenttransferInstance *instance,
+                         guint                           *n)
+{
+  set_param (instance, coord, 0, transfer->kind);
+  switch (transfer->kind)
+    {
+    case GSK_COMPONENT_TRANSFER_IDENTITY:
+      break;
+    case GSK_COMPONENT_TRANSFER_LEVELS:
+      set_param (instance, coord, 1, transfer->levels.n);
+      break;
+    case GSK_COMPONENT_TRANSFER_LINEAR:
+      set_param (instance, coord, 1, transfer->linear.m);
+      set_param (instance, coord, 2, transfer->linear.b);
+      break;
+    case GSK_COMPONENT_TRANSFER_GAMMA:
+      set_param (instance, coord, 1, transfer->gamma.amp);
+      set_param (instance, coord, 2, transfer->gamma.exp);
+      set_param (instance, coord, 3, transfer->gamma.ofs);
+      break;
+    case GSK_COMPONENT_TRANSFER_DISCRETE:
+    case GSK_COMPONENT_TRANSFER_TABLE:
+      if (*n + transfer->table.n >= 32)
+        g_warning ("tables too big in component transfer");
+      set_param (instance, coord, 1, transfer->table.n);
+      set_param (instance, coord, 2, *n);
+      for (guint i = 0; i < transfer->table.n; i++)
+        set_table_value (instance, *n + i, transfer->table.values[i]);
+      *n += transfer->table.n;
+      break;
+    default:
+      g_assert_not_reached ();
+    }
+}
+
+void
+gsk_gpu_component_transfer_op (GskGpuFrame                *frame,
+                               GskGpuShaderClip            clip,
+                               GskGpuColorStates           color_states,
+                               const graphene_point_t     *offset,
+                               const GskGpuShaderImage    *image,
+                               const GskComponentTransfer *red,
+                               const GskComponentTransfer *green,
+                               const GskComponentTransfer *blue,
+                               const GskComponentTransfer *alpha)
+{
+  GskGpuComponenttransferInstance *instance;
+  guint n;
+
+  gsk_gpu_shader_op_alloc (frame,
+                           &GSK_GPU_COMPONENT_TRANSFER_OP_CLASS,
+                           color_states,
+                           0,
+                           clip,
+                           (GskGpuImage *[1]) { image->image },
+                           (GskGpuSampler[1]) { image->sampler },
+                           &instance);
+
+  gsk_gpu_rect_to_float (image->coverage, offset, instance->rect);
+  gsk_gpu_rect_to_float (image->bounds, offset, instance->tex_rect);
+
+  n = 0;
+  copy_component_transfer (red, 0, instance, &n);
+  copy_component_transfer (green, 1, instance, &n);
+  copy_component_transfer (blue, 2, instance, &n);
+  copy_component_transfer (alpha, 3, instance, &n);
+}
+

gsk/gpu/gskgpucomponenttransferopprivate.h
@@ -1,0 +1,21 @@
+#pragma once
+
+#include "gskgpushaderopprivate.h"
+#include "gskcomponenttransfer.h"
+
+#include <graphene.h>
+
+G_BEGIN_DECLS
+
+void                    gsk_gpu_component_transfer_op                   (GskGpuFrame                    *frame,
+                                                                         GskGpuShaderClip                clip,
+                                                                         GskGpuColorStates               color_states,
+                                                                         const graphene_point_t         *offset,
+                                                                         const GskGpuShaderImage        *image,
+                                                                         const GskComponentTransfer     *red,
+                                                                         const GskComponentTransfer     *green,
+                                                                         const GskComponentTransfer     *blue,
+                                                                         const GskComponentTransfer     *alpha);
+
+G_END_DECLS
+

gsk/gpu/gskgpunodeprocessor.c
@@ -16,6 +16,7 @@
 #include "gskgpuclipprivate.h"
 #include "gskgpucolorizeopprivate.h"
 #include "gskgpucolormatrixopprivate.h"
+#include "gskgpucomponenttransferopprivate.h"
 #include "gskgpucoloropprivate.h"
 #include "gskgpuconicgradientopprivate.h"
 #include "gskgpuconvertbuiltinopprivate.h"
@@ -3296,6 +3297,42 @@ gsk_gpu_node_processor_add_color_matrix_node (GskGpuNodeProcessor *self,
 }
 
 static void
+gsk_gpu_node_processor_add_component_transfer_node (GskGpuNodeProcessor *self,
+                                                    GskRenderNode       *node)
+{
+  GskGpuImage *image;
+  GskRenderNode *child;
+  graphene_rect_t tex_rect;
+
+  child = gsk_component_transfer_node_get_child (node);
+
+  image = gsk_gpu_node_processor_get_node_as_image (self,
+                                                    0,
+                                                    NULL,
+                                                    child,
+                                                    &tex_rect);
+  if (image == NULL)
+    return;
+
+  gsk_gpu_component_transfer_op (self->frame,
+                                 gsk_gpu_clip_get_shader_clip (&self->clip, &self->offset, &node->bounds),
+                                 gsk_gpu_node_processor_color_states_explicit (self, self->ccs, FALSE),
+                                 &self->offset,
+                                 &(GskGpuShaderImage) {
+                                   image,
+                                   GSK_GPU_SAMPLER_DEFAULT,
+                                   &node->bounds,
+                                   &tex_rect,
+                                 },
+                                 gsk_component_transfer_node_get_transfer (node, 0),
+                                 gsk_component_transfer_node_get_transfer (node, 1),
+                                 gsk_component_transfer_node_get_transfer (node, 2),
+                                 gsk_component_transfer_node_get_transfer (node, 3));
+
+  g_object_unref (image);
+}
+
+static void
 gsk_gpu_node_processor_repeat_tile (GskGpuNodeProcessor    *self,
                                     const graphene_rect_t  *rect,
                                     float                   x,
@@ -4077,6 +4114,13 @@ static const struct
     gsk_gpu_node_processor_add_subsurface_node,
     gsk_gpu_node_processor_add_first_subsurface_node,
     gsk_gpu_get_subsurface_node_as_image,
+  },
+  [GSK_COMPONENT_TRANSFER_NODE] = {
+    0,
+    0,
+    gsk_gpu_node_processor_add_component_transfer_node,
+    NULL,
+    NULL,
   },
 };
 

gsk/gpu/shaders/gskgpucomponenttransfer.glsl
@@ -1,0 +1,189 @@
+#define GSK_N_TEXTURES 1
+
+#define GSK_COMPONENT_TRANSFER_IDENTITY 0u
+#define GSK_COMPONENT_TRANSFER_LEVELS 1u
+#define GSK_COMPONENT_TRANSFER_LINEAR 2u
+#define GSK_COMPONENT_TRANSFER_GAMMA 3u
+#define GSK_COMPONENT_TRANSFER_DISCRETE 4u
+#define GSK_COMPONENT_TRANSFER_TABLE 5u
+
+#include "common.glsl"
+
+PASS_FLAT(0) vec4 _params_r;
+PASS_FLAT(1) vec4 _params_g;
+PASS_FLAT(2) vec4 _params_b;
+PASS_FLAT(3) vec4 _params_a;
+PASS_FLAT(4) vec4 _table0;
+PASS_FLAT(5) vec4 _table1;
+PASS_FLAT(6) vec4 _table2;
+PASS_FLAT(7) vec4 _table3;
+PASS_FLAT(8) vec4 _table4;
+PASS_FLAT(9) vec4 _table5;
+PASS_FLAT(10) vec4 _table6;
+PASS_FLAT(11) vec4 _table7;
+PASS(12) vec2 _pos;
+PASS_FLAT(13) Rect _rect;
+PASS(14) vec2 _tex_coord;
+
+
+#ifdef GSK_VERTEX_SHADER
+
+IN(0) vec4 in_params_r;
+IN(1) vec4 in_params_g;
+IN(2) vec4 in_params_b;
+IN(3) vec4 in_params_a;
+IN(4) vec4 in_table0;
+IN(5) vec4 in_table1;
+IN(6) vec4 in_table2;
+IN(7) vec4 in_table3;
+IN(8) vec4 in_table4;
+IN(9) vec4 in_table5;
+IN(10) vec4 in_table6;
+IN(11) vec4 in_table7;
+IN(12) vec4 in_rect;
+IN(13) vec4 in_tex_rect;
+
+void
+run (out vec2 pos)
+{
+  Rect r = rect_from_gsk (in_rect);
+
+  pos = rect_get_position (r);
+
+  _pos = pos;
+  _rect = r;
+  _tex_coord = rect_get_coord (rect_from_gsk (in_tex_rect), pos);
+  _params_r = in_params_r;
+  _params_g = in_params_g;
+  _params_b = in_params_b;
+  _params_a = in_params_a;
+  _table0 = in_table0;
+  _table1 = in_table1;
+  _table2 = in_table2;
+  _table3 = in_table3;
+  _table4 = in_table4;
+  _table5 = in_table5;
+  _table6 = in_table6;
+  _table7 = in_table7;
+}
+
+#endif
+
+
+
+#ifdef GSK_FRAGMENT_SHADER
+
+float
+get_table_value (uint k)
+{
+  if (k < 4u)
+    return _table0[k];
+  else if (k < 8u)
+    return _table1[k-4u];
+  else if (k < 12u)
+    return _table2[k-8u];
+  else if (k < 16u)
+    return _table3[k-12u];
+  else if (k < 20u)
+    return _table4[k-16u];
+  else if (k < 24u)
+    return _table5[k-20u];
+  else if (k < 28u)
+    return _table6[k-24u];
+  else if (k < 32u)
+    return _table7[k-28u];
+  return 0.;
+}
+
+float
+get_param(uint coord, uint k)
+{
+  if (coord == 0u)
+    return _params_r[k];
+  else if (coord == 1u)
+    return _params_g[k];
+  else if (coord == 2u)
+    return _params_b[k];
+  else
+    return _params_a[k];
+}
+
+float
+apply_component_transfer (uint coord, float c)
+{
+  uint kind = uint(get_param(coord, 0u));
+
+  if (kind == GSK_COMPONENT_TRANSFER_IDENTITY)
+    {
+      return c;
+    }
+  else if (kind == GSK_COMPONENT_TRANSFER_LEVELS)
+    {
+      float n = get_param(coord, 1u);
+      return floor (c * n) / n;
+    }
+  else if (kind == GSK_COMPONENT_TRANSFER_LINEAR)
+    {
+      float m = get_param(coord, 1u);
+      float b = get_param(coord, 2u);
+      return c * m + b;
+    }
+  else if (kind == GSK_COMPONENT_TRANSFER_GAMMA)
+    {
+      float a = get_param(coord, 1u);
+      float e = get_param(coord, 2u);
+      float o = get_param(coord, 3u);
+      return a * pow(c, e) + o;
+    }
+  else if (kind == GSK_COMPONENT_TRANSFER_DISCRETE)
+    {
+      float n = get_param(coord, 1u);
+      uint base = uint(get_param(coord, 2u));
+      uint k;
+      for (k = 0u; float(k) < n; k++)
+        {
+          if (float(k) <= c*n && c*n < float(k+1u))
+            return get_table_value (base + k);
+        }
+      return get_table_value (base + k - 1u);
+    }
+  else if (kind == GSK_COMPONENT_TRANSFER_TABLE)
+    {
+      float n = get_param(coord, 1u) - 1.;
+      uint base = uint(get_param(coord, 2u));
+      uint k;
+      for (k = 0u; float(k) < n; k++)
+        {
+          if (float(k) <= c*n && c*n < float(k+1u))
+            {
+              float vk = get_table_value (base + k);
+              float vk2 = get_table_value (base + k + 1u);
+              return vk + (c - float(k) / n) * n * (vk2 - vk);
+            }
+        }
+      return get_table_value (base + k);
+    }
+}
+
+void
+run (out vec4 color,
+     out vec2 position)
+{
+  vec4 pixel = texture (GSK_TEXTURE0, _tex_coord);
+  pixel = alt_color_from_output (pixel);
+
+  pixel.r = apply_component_transfer (0u, pixel.r);
+  pixel.g = apply_component_transfer (1u, pixel.g);
+  pixel.b = apply_component_transfer (2u, pixel.b);
+  pixel.a = apply_component_transfer (3u, pixel.a);
+
+  pixel = clamp (pixel, 0.0, 1.0);
+
+  pixel = output_color_from_alt (pixel);
+
+  color = output_color_alpha (pixel, rect_coverage (_rect, _pos));
+  position = _pos;
+}
+
+#endif
+

gsk/gpu/shaders/meson.build
@@ -18,6 +18,7 @@ gsk_private_gpu_shaders = files([
   'gskgpucolor.glsl',
   'gskgpucolorize.glsl',
   'gskgpucolormatrix.glsl',
+  'gskgpucomponenttransfer.glsl',
   'gskgpuconicgradient.glsl',
   'gskgpuconvert.glsl',
   'gskgpuconvertbuiltin.glsl',

gsk/gsk.h
@@ -30,6 +30,7 @@
 #include <gsk/gskstroke.h>
 #include <gsk/gsktransform.h>
 #include <gsk/gskglshader.h>
+#include <gsk/gskcomponenttransfer.h>
 
 #include <gsk/gskcairorenderer.h>
 #include <gsk/gpu/gskvulkanrenderer.h>

commit 6a2e95e3204095061ae282d76fbdc51d5a77698e

gsk: Plug a memory leak
While we embed the component transfers in the rendernode,
we still need to clear them.

Fixes: 42d0ddef76c7e4c8d3f2
Part-of: <https://gitlab.gnome.org/GNOME/gtk/-/merge_requests/9054>

gsk/gskrendernodeimpl.c
@@ -8629,6 +8629,11 @@ gsk_component_transfer_node_finalize (GskRenderNode *node)
 
   gsk_render_node_unref (self->child);
 
+  gsk_component_transfer_clear (&self->transfer[0]);
+  gsk_component_transfer_clear (&self->transfer[1]);
+  gsk_component_transfer_clear (&self->transfer[2]);
+  gsk_component_transfer_clear (&self->transfer[3]);
+
   parent_class->finalize (node);
 }
 