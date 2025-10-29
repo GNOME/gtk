ef0dd009b642fcaa250b2d998e83a9399b9cf3cd
Matthias Clasen <mclasen@redhat.com>
2025-10-29 12:15:08 -0400

    Make it possible to replay animation tests
    This is handy to see the expected output. Use it by
    pointing test-svg-animation --replay at one of the
    test files.
    
    Part-of: <https://gitlab.gnome.org/GNOME/gtk/-/merge_requests/9094>

--- testsuite/svg/animation/test-svg-animation.c
+++ testsuite/svg/animation/test-svg-animation.c
@@ -37,6 +37,7 @@ get_sibling (const char *file,
 
 typedef enum
 {
+  INPUT,
   TIME,
   STATE,
   COLORS,
@@ -46,6 +47,7 @@ typedef enum
 typedef struct
 {
   StepType type;
+  char *input;
   int64_t time;
   unsigned int state;
   char *output;
@@ -58,21 +60,238 @@ clear_step (gpointer data)
 {
   Step *step = data;
 
+  if (step->type == INPUT)
+    g_free (step->input);
+
   if (step->type == OUTPUT)
     g_free (step->output);
+}
+
+static GArray *
+parse_test_file (const char *filename)
+{
+  GArray *steps;
+  char *contents;
+  size_t length;
+  GStrv strv;
+  GError *error = NULL;
+  unsigned int i = 0;
+  Step step;
+
+  steps = g_array_new (FALSE, FALSE, sizeof (Step));
+  g_array_set_clear_func (steps, clear_step);
+
+  if (!g_file_get_contents (filename, &contents, &length, &error))
+    g_error ("%s", error->message);
+
+  strv = g_strsplit (contents, "\n", 0);
+
+  /* Allow for a comment at the top */
+  while (strv[i] && strv[i][0] == '#') i++;
+
+  if (!g_str_has_prefix (strv[i], "input: "))
+    g_error ("Can't parse %s.%u (expected 'input: ')\n", filename, i);
+
+  memset (&step, 0, sizeof (Step));
+  step.type = INPUT;
+  step.input = get_sibling (filename, strv[i] + strlen ("input: "));
+  g_array_append_val (steps, step);
+
+  for (i++; strv[i]; i++)
+    {
+      memset (&step, 0, sizeof (Step));
+
+      if (g_str_has_prefix (strv[i], "state: "))
+        {
+          char *end;
+          step.type = STATE;
+          if (strcmp (strv[i] + strlen ("state: "), "empty") == 0)
+            step.state = GTK_SVG_STATE_EMPTY;
+          else
+            {
+              step.state = g_ascii_strtoull (strv[i] + strlen ("state: "), &end, 10);
+              if ((end && *end != '\0') || step.state > 63)
+                g_error ("Can't parse %s.%u (expected a state)\n", filename, i);
+            }
+          g_array_append_val (steps, step);
+        }
+      else if (g_str_has_prefix (strv[i], "time: "))
+        {
+          char *end;
+          step.type = TIME;
+          step.time = g_ascii_strtoull (strv[i] + strlen ("time: "), &end, 10);
+          if (end && *end != '\0')
+            g_error ("Can't parse %s.%u (expected a time)\n", filename, i);
+          g_array_append_val (steps, step);
+        }
+      else if (g_str_has_prefix (strv[i], "colors: "))
+        {
+          GStrv cols = g_strsplit (strv[i] + strlen ("colors: "), ";", 0);
+
+          step.type = COLORS;
+          step.n_colors = MIN (g_strv_length (cols), 5);
+          for (unsigned int j = 0; j < step.n_colors; j++)
+            {
+              if (!gdk_rgba_parse (&step.colors[j], cols[j]))
+                g_error ("Can't parse %s.%u (expected colors)\n", filename, i);
+            }
+          g_strfreev (cols);
+
+          g_array_append_val (steps, step);
+        }
+      else if (g_str_has_prefix (strv[i], "output: "))
+        {
+          step.type = OUTPUT;
+          step.output = get_sibling (filename, strv[i] + strlen ("output: "));
+          g_array_append_val (steps, step);
+        }
+    }
+
+  g_strfreev (strv);
+  g_free (contents);
+
+  return steps;
+}
+
+typedef struct
+{
+  unsigned int step;
+  GtkSvg *svg;
+  unsigned int state;
+  char *output;
+} StepData;
+
+static gboolean
+set_state (gpointer data)
+{
+  StepData *sd = data;
+
+  g_print ("Step %u: Setting state to %u\n", sd->step, sd->state);
+  gtk_svg_set_state (sd->svg, sd->state);
+  g_free (sd);
+
+  return G_SOURCE_REMOVE;
+}
+
+static gboolean
+snapshot_here (gpointer data)
+{
+  StepData *sd = data;
+
+  g_print ("Step %u: Snapshot %s\n", sd->step, sd->output);
+  g_free (sd);
+
+  return G_SOURCE_REMOVE;
+}
+
+static gboolean
+end_it_all (gpointer data)
+{
+  gtk_window_close (GTK_WINDOW (data));
+  return G_SOURCE_REMOVE;
+}
+
+static void
+play_svg_test (GFile *file)
+{
+  const char *filename;
+  GArray *steps;
+  char *contents;
+  size_t length;
+  GBytes *bytes;
+  GtkSvg *svg = NULL;
+  int64_t load_time;
+  GError *error = NULL;
+  GtkWidget *window, *picture;
+  int64_t time = 0;
+  StepData *data;
+
+  filename = g_file_peek_path (file);
+
+  if (!g_str_has_suffix (filename, ".test"))
+    g_error ("Not a test file: %s", filename);
+
+  steps = parse_test_file (filename);
+
+  for (unsigned int i = 0; i < steps->len; i++)
+    {
+      Step *step = &g_array_index (steps, Step, i);
+      switch (step->type)
+        {
+        case INPUT:
+          g_assert (i == 0);
+          if (!g_file_get_contents (step->input, &contents, &length, &error))
+          g_error ("%s", error->message);
+
+          bytes = g_bytes_new_take (contents, length);
+          svg = gtk_svg_new_from_bytes (bytes);
+          g_bytes_unref (bytes);
+
+          load_time = g_get_monotonic_time ();
+          gtk_svg_set_load_time (svg, load_time);
+          break;
+
+        case TIME:
+          time = step->time;
+          break;
+
+        case STATE:
+          data = g_new (StepData, 1);
+          data->step = i;
+          data->svg = svg;
+          data->state = step->state;
+          g_timeout_add (time, set_state, data);
+          break;
+
+        case COLORS:
+          g_print ("FIXME: apply colors\n");
+          break;
+
+        case OUTPUT:
+          data = g_new (StepData, 1);
+          data->step = i;
+          data->output = g_path_get_basename (step->output);
+          g_timeout_add (time, snapshot_here, data);
+          break;
+
+        default:
+          g_assert_not_reached ();
+        }
+    }
+
+  if (!svg)
+    g_error ("No input?!\n");
+
+  window = gtk_window_new ();
+  picture = gtk_picture_new_for_paintable (GDK_PAINTABLE (svg));
+  gtk_window_set_child (GTK_WINDOW (window), picture);
+
+  g_timeout_add (time + 1000, end_it_all, window);
+
+  g_print ("Starting replay\n");
+  gtk_svg_play (svg);
+  gtk_window_present (GTK_WINDOW (window));
+
+  while (g_list_model_get_n_items (gtk_window_get_toplevels ()) > 0)
+    g_main_context_iteration (NULL, TRUE);
+
+  gtk_svg_play (svg);
+
+  g_array_unref (steps);
+  g_object_unref (svg);
 }
 
 static void
 render_svg_file (GFile *file, gboolean generate)
 {
   const char *filename;
-  GtkSvg *svg;
+  GtkSvg *svg = NULL;
   char *svg_file;
   char *contents;
   size_t length;
   GBytes *bytes;
   GError *error = NULL;
-  int64_t load_time;
+  int64_t load_time = 0;
   GArray *steps;
   const GdkRGBA *colors = NULL;
   size_t n_colors = 0;
@@ -84,86 +303,17 @@ render_svg_file (GFile *file, gboolean generate)
 
   if (g_str_has_suffix (filename, ".test"))
     {
-      GStrv strv;
-      unsigned int i = 0;
-
-      if (!g_file_get_contents (filename, &contents, &length, &error))
-        g_error ("%s", error->message);
-
-      strv = g_strsplit (contents, "\n", 0);
-
-      /* Allow for a comment at the top */
-      while (strv[i] && strv[i][0] == '#') i++;
-
-      if (!g_str_has_prefix (strv[i], "input: "))
-        g_error ("Can't parse %s.%u (expected 'input: ')\n", filename, i);
-
-      svg_file = get_sibling (filename, strv[i] + strlen ("input: "));
-
-      for (i++; strv[i]; i++)
-        {
-          Step step;
-
-          memset (&step, 0, sizeof (Step));
-
-          if (g_str_has_prefix (strv[i], "state: "))
-            {
-              char *end;
-              step.type = STATE;
-              if (strcmp (strv[i] + strlen ("state: "), "empty") == 0)
-                step.state = GTK_SVG_STATE_EMPTY;
-              else
-                {
-                  step.state = g_ascii_strtoull (strv[i] + strlen ("state: "), &end, 10);
-                  if ((end && *end != '\0') || step.state > 63)
-                    g_error ("Can't parse %s.%u (expected a state)\n", filename, i);
-                }
-              g_array_append_val (steps, step);
-            }
-          else if (g_str_has_prefix (strv[i], "time: "))
-            {
-              char *end;
-              step.type = TIME;
-              step.time = g_ascii_strtoull (strv[i] + strlen ("time: "), &end, 10);
-              if (end && *end != '\0')
-                g_error ("Can't parse %s.%u (expected a time)\n", filename, i);
-              g_array_append_val (steps, step);
-            }
-          else if (g_str_has_prefix (strv[i], "colors: "))
-            {
-              GStrv cols = g_strsplit (strv[i] + strlen ("colors: "), ";", 0);
-
-              step.type = COLORS;
-              step.n_colors = MIN (g_strv_length (cols), 5);
-              for (unsigned int j = 0; j < step.n_colors; j++)
-                {
-                  if (!gdk_rgba_parse (&step.colors[j], cols[j]))
-                    g_error ("Can't parse %s.%u (expected colors)\n", filename, i);
-                }
-              g_strfreev (cols);
-
-              g_array_append_val (steps, step);
-            }
-          else if (g_str_has_prefix (strv[i], "output: "))
-            {
-              step.type = OUTPUT;
-              step.output = get_sibling (filename, strv[i] + strlen ("output: "));
-              g_array_append_val (steps, step);
-            }
-        }
-
-      g_strfreev (strv);
-      g_free (contents);
+      steps = parse_test_file (filename);
     }
   else
     {
       Step step;
       char *p, *end = NULL;
-      int64_t time;
+      unsigned int time;
 
       svg_file = g_file_get_path (file);
       p = strrchr (svg_file, '.');
-      time = g_ascii_strtoull (&p[1], &end, 10);
+      time = (unsigned int) g_ascii_strtoull (&p[1], &end, 10);
       *p = '\0';
 
       step.type = TIME;
@@ -175,21 +325,23 @@ render_svg_file (GFile *file, gboolean generate)
       g_array_append_val (steps, step);
     }
 
-  if (!g_file_get_contents (svg_file, &contents, &length, &error))
-    g_error ("%s", error->message);
-
-  bytes = g_bytes_new_take (contents, length);
-  svg = gtk_svg_new_from_bytes (bytes);
-  g_bytes_unref (bytes);
-
-  load_time = g_get_monotonic_time ();
-  gtk_svg_set_load_time (svg, load_time);
-
   for (unsigned int i = 0; i < steps->len; i++)
     {
       Step *step = &g_array_index (steps, Step, i);
       switch (step->type)
         {
+        case INPUT:
+          g_assert (i == 0);
+          if (!g_file_get_contents (step->input, &contents, &length, &error))
+          g_error ("%s", error->message);
+
+          bytes = g_bytes_new_take (contents, length);
+          svg = gtk_svg_new_from_bytes (bytes);
+          g_bytes_unref (bytes);
+
+          load_time = g_get_monotonic_time ();
+          gtk_svg_set_load_time (svg, load_time);
+          break;
         case TIME:
           gtk_svg_advance (svg, load_time + step->time * G_TIME_SPAN_MILLISECOND);
           break;
@@ -240,7 +392,6 @@ render_svg_file (GFile *file, gboolean generate)
     }
 
   g_array_unref (steps);
-  g_free (svg_file);
   g_object_unref (svg);
 }
 
@@ -331,6 +482,18 @@ main (int argc, char **argv)
 
       file = g_file_new_for_commandline_arg (argv[2]);
       render_svg_file (file, TRUE);
+      g_object_unref (file);
+
+      return 0;
+    }
+  else if (argc >= 3 && strcmp (argv[1], "--replay") == 0)
+    {
+      GFile *file;
+
+      gtk_init ();
+
+      file = g_file_new_for_commandline_arg (argv[2]);
+      play_svg_test (file);
       g_object_unref (file);
 
       return 0;
