9e9fcdeffd72cc3448126bb659e6c1c250838f63
Benjamin Otte <otte@redhat.com>
2025-11-11 12:47:00 +0100

    gsk: Add a method to replace copy/paste nodes
    ... by replaying the parts of the node tree that the paste node copies
    and using that instead of the paste node.
    
    Use this method in GskRenderer to allow all renderers to render
    copy/paste nodes.
    
    It's absolutely not performant, but it works flawlessly.
    
    Part-of: <https://gitlab.gnome.org/GNOME/gtk/-/merge_requests/9126>

---
+++ gsk/gskcopypasteutilsprivate.h
@@ -0,0 +1,28 @@
+/* GSK - The GTK Scene Kit
+ *
+ * Copyright 2025  Benjamin Otte
+ *
+ * This library is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Lesser General Public
+ * License as published by the Free Software Foundation; either
+ * version 2 of the License, or (at your option) any later version.
+ *
+ * This library is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public
+ * License along with this library. If not, see <http://www.gnu.org/licenses/>.
+ */
+
+#pragma once
+
+#include <gsk/gsktypes.h>
+
+G_BEGIN_DECLS
+
+GskRenderNode *         gsk_render_node_replace_copy_paste      (GskRenderNode            *node);
+
+G_END_DECLS
+
