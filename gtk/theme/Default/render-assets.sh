ba409ffa44fdff82e5bd09fa5bd4d7f88fccf14c
Berk Elyesa Yıldırım <beryesa@sayafe.org>
2025-11-11 03:47:18 +0100

    theme: Update merged assets rendering script
    It will now export all under assets/
    And prefix them with hc- accordingly.
    
    Part-of: <https://gitlab.gnome.org/GNOME/gtk/-/merge_requests/9124>

--- gtk/theme/Default/render-assets.sh
+++ gtk/theme/Default/render-assets.sh
@@ -4,38 +4,38 @@ INKSCAPE="/usr/bin/inkscape"
 OPTIPNG="/usr/bin/optipng"
 
 INDEX="assets.txt"
+SRC_FILE="assets.svg"
+ASSETS_DIR="assets"
 
-for s in "" "-hc";
+for contrast in "" "hc-";
 do
-  SRC_FILE="assets$s.svg"
-  ASSETS_DIR="assets$s"
 
   for i in `cat $INDEX`
   do
-    if [ -f $ASSETS_DIR/$i.png ]; then
-      echo $ASSETS_DIR/$i.png exists.
+    if [ -f $ASSETS_DIR/$contrast$i.png ]; then
+      echo $ASSETS_DIR/$contrast$i.png exists.
     else
       echo
-      echo Rendering $ASSETS_DIR/$i.png
-      $INKSCAPE --export-id=$i \
+      echo Rendering $contrast$i.png
+      $INKSCAPE --export-id=$contrast$i \
                 --export-id-only \
                 --export-type="png" \
-                --export-filename=$ASSETS_DIR/$i.png \
-                $SRC_FILE >/dev/null #\
-      # && $OPTIPNG -o7 --quiet $ASSETS_DIR/$i.png
+                --export-filename=$ASSETS_DIR/$contrast$i.png \
+                $SRC_FILE >/dev/null \
+       && $OPTIPNG -o7 --quiet $ASSETS_DIR/$contrast$i.png
     fi
-    if [ -f $ASSETS_DIR/$i@2.png ]; then
-      echo $ASSETS_DIR/$i@2.png exists.
+    if [ -f $ASSETS_DIR/$contrast$i@2.png ]; then
+      echo $ASSETS_DIR/$contrast$i@2.png exists.
     else
       echo
-      echo Rendering $ASSETS_DIR/$i@2.png
-      $INKSCAPE --export-id=$i \
+      echo Rendering $contrast$i@2.png
+      $INKSCAPE --export-id=$contrast$i \
                 --export-dpi=180 \
                 --export-id-only \
                 --export-type="png" \
-                --export-filename=$ASSETS_DIR/$i@2.png \
-                $SRC_FILE >/dev/null #\
-      # && $OPTIPNG -o7 --quiet $ASSETS_DIR/$i@2.png
+                --export-filename=$ASSETS_DIR/$contrast$i@2.png \
+                $SRC_FILE >/dev/null \
+       && $OPTIPNG -o7 --quiet $ASSETS_DIR/$contrast$i@2.png
     fi
   done
 done
