GNOME/gtk

commit 9d200c0002bf48a77fe1ce052f7e26c25796244d

colorstate: Don't make oklab and oklch default colorstates
We don't use them for compositing so it's not necessary to force every
renderer to be able to natively convert to/from them.

Instead, introduce GdkBuiltinColorstate which is meant to carry the
built in colorspaces that aren't defaults.

gdk/gdkcolorstate.c
@@ -412,9 +412,6 @@ oklab_to_oklch (GdkColorState *self,
   values[2] = H;
 }
 
-CONVERT_FUNC (oklch_to_oklab)
-CONVERT_FUNC (oklab_to_oklch)
-
 TRANSFORM_PAIR (srgb_to_oklch,           srgb_to_oklab,           oklab_to_oklch)
 TRANSFORM_PAIR (srgb_linear_to_oklch,    srgb_linear_to_oklab,    oklab_to_oklch)
 TRANSFORM_PAIR (rec2100_pq_to_oklch,     rec2100_pq_to_oklab,     oklab_to_oklch)
@@ -575,8 +572,6 @@ GdkDefaultColorState gdk_default_color_states[] = {
       [GDK_COLOR_STATE_ID_SRGB_LINEAR] = gdk_convert_srgb_to_srgb_linear,
       [GDK_COLOR_STATE_ID_REC2100_PQ] = gdk_convert_srgb_to_rec2100_pq,
       [GDK_COLOR_STATE_ID_REC2100_LINEAR] = gdk_convert_srgb_to_rec2100_linear,
-      [GDK_COLOR_STATE_ID_OKLAB] = gdk_convert_srgb_to_oklab,
-      [GDK_COLOR_STATE_ID_OKLCH] = gdk_convert_srgb_to_oklch,
     },
     .clamp = gdk_color_state_clamp_0_1,
     .cicp = { 1, 13, 0, 1 },
@@ -595,8 +590,6 @@ GdkDefaultColorState gdk_default_color_states[] = {
       [GDK_COLOR_STATE_ID_SRGB] = gdk_convert_srgb_linear_to_srgb,
       [GDK_COLOR_STATE_ID_REC2100_PQ] = gdk_convert_srgb_linear_to_rec2100_pq,
       [GDK_COLOR_STATE_ID_REC2100_LINEAR] = gdk_convert_srgb_linear_to_rec2100_linear,
-      [GDK_COLOR_STATE_ID_OKLAB] = gdk_convert_srgb_linear_to_oklab,
-      [GDK_COLOR_STATE_ID_OKLCH] = gdk_convert_srgb_linear_to_oklch,
     },
     .clamp = gdk_color_state_clamp_0_1,
     .cicp = { 1, 8, 0, 1 },
@@ -615,8 +608,6 @@ GdkDefaultColorState gdk_default_color_states[] = {
       [GDK_COLOR_STATE_ID_SRGB] = gdk_convert_rec2100_pq_to_srgb,
       [GDK_COLOR_STATE_ID_SRGB_LINEAR] = gdk_convert_rec2100_pq_to_srgb_linear,
       [GDK_COLOR_STATE_ID_REC2100_LINEAR] = gdk_convert_rec2100_pq_to_rec2100_linear,
-      [GDK_COLOR_STATE_ID_OKLAB] = gdk_convert_rec2100_pq_to_oklab,
-      [GDK_COLOR_STATE_ID_OKLCH] = gdk_convert_rec2100_pq_to_oklch,
     },
     .clamp = gdk_color_state_clamp_0_1,
     .cicp = { 9, 16, 0, 1 },
@@ -635,47 +626,113 @@ GdkDefaultColorState gdk_default_color_states[] = {
       [GDK_COLOR_STATE_ID_SRGB] = gdk_convert_rec2100_linear_to_srgb,
       [GDK_COLOR_STATE_ID_SRGB_LINEAR] = gdk_convert_rec2100_linear_to_srgb_linear,
       [GDK_COLOR_STATE_ID_REC2100_PQ] = gdk_convert_rec2100_linear_to_rec2100_pq,
-      [GDK_COLOR_STATE_ID_OKLAB] = gdk_convert_rec2100_linear_to_oklab,
-      [GDK_COLOR_STATE_ID_OKLCH] = gdk_convert_rec2100_linear_to_oklch,
     },
     .clamp = gdk_color_state_clamp_unbounded,
     .cicp = { 9, 8, 0, 1 },
   },
-  [GDK_COLOR_STATE_ID_OKLAB] = {
+};
+
+static gboolean
+gdk_builtin_color_state_equal (GdkColorState *self,
+                               GdkColorState *other)
+{
+  return self == other;
+}
+
+static const char *
+gdk_builtin_color_state_get_name (GdkColorState *color_state)
+{
+  GdkBuiltinColorState *self = (GdkBuiltinColorState *) color_state;
+
+  return self->name;
+}
+
+static GdkColorState *
+gdk_builtin_color_state_get_no_srgb_tf (GdkColorState *color_state)
+{
+  return NULL;
+}
+
+static GdkFloatColorConvert
+gdk_builtin_color_state_get_convert_to (GdkColorState  *color_state,
+                                        GdkColorState  *target)
+{
+  GdkBuiltinColorState *self = (GdkBuiltinColorState *) color_state;
+
+  return self->convert_to[GDK_DEFAULT_COLOR_STATE_ID (target)];
+}
+
+static GdkFloatColorConvert
+gdk_builtin_color_state_get_convert_from (GdkColorState  *color_state,
+                                          GdkColorState  *source)
+{
+  GdkBuiltinColorState *self = (GdkBuiltinColorState *) color_state;
+
+  return self->convert_from[GDK_DEFAULT_COLOR_STATE_ID (source)];
+}
+
+static const GdkCicp *
+gdk_builtin_color_state_get_cicp (GdkColorState *color_state)
+{
+  return NULL;
+}
+
+/* }}} */
+
+static const
+GdkColorStateClass GDK_BUILTIN_COLOR_STATE_CLASS = {
+  .free = NULL, /* crash here if this ever happens */
+  .equal = gdk_builtin_color_state_equal,
+  .get_name = gdk_builtin_color_state_get_name,
+  .get_no_srgb_tf = gdk_builtin_color_state_get_no_srgb_tf,
+  .get_convert_to = gdk_builtin_color_state_get_convert_to,
+  .get_convert_from = gdk_builtin_color_state_get_convert_from,
+  .get_cicp = gdk_builtin_color_state_get_cicp,
+  .clamp = gdk_color_state_clamp_unbounded,
+};
+
+GdkBuiltinColorState gdk_builtin_color_states[] = {
+  [GDK_BUILTIN_COLOR_STATE_ID_OKLAB] = {
     .parent = {
-      .klass = &GDK_DEFAULT_COLOR_STATE_CLASS,
+      .klass = &GDK_BUILTIN_COLOR_STATE_CLASS,
       .ref_count = 0,
       .depth = GDK_MEMORY_FLOAT16,
       .rendering_color_state = GDK_COLOR_STATE_SRGB,
     },
     .name = "oklab",
-    .no_srgb = NULL,
     .convert_to = {
       [GDK_COLOR_STATE_ID_SRGB] = gdk_convert_oklab_to_srgb,
       [GDK_COLOR_STATE_ID_SRGB_LINEAR] = gdk_convert_oklab_to_srgb_linear,
       [GDK_COLOR_STATE_ID_REC2100_PQ] = gdk_convert_oklab_to_rec2100_pq,
       [GDK_COLOR_STATE_ID_REC2100_LINEAR] = gdk_convert_oklab_to_rec2100_linear,
-      [GDK_COLOR_STATE_ID_OKLCH] = gdk_convert_oklab_to_oklch,
     },
-    .cicp = { 0, 0, 0, 0 },
+    .convert_from = {
+      [GDK_COLOR_STATE_ID_SRGB] = gdk_convert_srgb_to_oklab,
+      [GDK_COLOR_STATE_ID_SRGB_LINEAR] = gdk_convert_srgb_linear_to_oklab,
+      [GDK_COLOR_STATE_ID_REC2100_PQ] = gdk_convert_rec2100_pq_to_oklab,
+      [GDK_COLOR_STATE_ID_REC2100_LINEAR] = gdk_convert_rec2100_linear_to_oklab,
+    },
   },
-  [GDK_COLOR_STATE_ID_OKLCH] = {
+  [GDK_BUILTIN_COLOR_STATE_ID_OKLCH] = {
     .parent = {
-      .klass = &GDK_DEFAULT_COLOR_STATE_CLASS,
+      .klass = &GDK_BUILTIN_COLOR_STATE_CLASS,
       .ref_count = 0,
       .depth = GDK_MEMORY_FLOAT16,
       .rendering_color_state = GDK_COLOR_STATE_SRGB,
     },
     .name = "oklch",
-    .no_srgb = NULL,
     .convert_to = {
       [GDK_COLOR_STATE_ID_SRGB] = gdk_convert_oklch_to_srgb,
       [GDK_COLOR_STATE_ID_SRGB_LINEAR] = gdk_convert_oklch_to_srgb_linear,
       [GDK_COLOR_STATE_ID_REC2100_PQ] = gdk_convert_oklch_to_rec2100_pq,
       [GDK_COLOR_STATE_ID_REC2100_LINEAR] = gdk_convert_oklch_to_rec2100_linear,
-      [GDK_COLOR_STATE_ID_OKLAB] = gdk_convert_oklch_to_oklab,
     },
-    .cicp = { 0, 0, 0, 0 },
+    .convert_from = {
+      [GDK_COLOR_STATE_ID_SRGB] = gdk_convert_srgb_to_oklch,
+      [GDK_COLOR_STATE_ID_SRGB_LINEAR] = gdk_convert_srgb_linear_to_oklch,
+      [GDK_COLOR_STATE_ID_REC2100_PQ] = gdk_convert_rec2100_pq_to_oklch,
+      [GDK_COLOR_STATE_ID_REC2100_LINEAR] = gdk_convert_rec2100_linear_to_oklch,
+    },
   },
 };
 
@@ -716,11 +773,6 @@ TRANSFORM(cicp_from_rec2100_pq,     pq_eotf,     cicp->from_rec2020, NONE, IDENT
 TRANSFORM(cicp_from_rec2100_linear, NONE,        cicp->from_rec2020, NONE, IDENTITY, cicp->oetf)
 
 #undef cicp
-
-TRANSFORM_PAIR (cicp_to_oklab,   cicp_to_srgb_linear,  srgb_linear_to_oklab)
-TRANSFORM_PAIR (cicp_from_oklab, oklab_to_srgb_linear, cicp_from_srgb_linear)
-TRANSFORM_PAIR (cicp_to_oklch,   cicp_to_srgb_linear,  srgb_linear_to_oklch)
-TRANSFORM_PAIR (cicp_from_oklch, oklch_to_srgb_linear, cicp_from_srgb_linear)
 
 /* }}} */
 /* {{{ Vfuncs */
@@ -781,10 +833,6 @@ gdk_cicp_color_state_get_convert_to (GdkColorState *self,
       return gdk_convert_cicp_to_rec2100_pq;
     case GDK_COLOR_STATE_ID_REC2100_LINEAR:
       return gdk_convert_cicp_to_rec2100_linear;
-    case GDK_COLOR_STATE_ID_OKLAB:
-      return gdk_convert_cicp_to_oklab;
-    case GDK_COLOR_STATE_ID_OKLCH:
-      return gdk_convert_cicp_to_oklch;
 
     case GDK_COLOR_STATE_N_IDS:
     default:
@@ -811,10 +859,6 @@ gdk_cicp_color_state_get_convert_from (GdkColorState *self,
       return gdk_convert_cicp_from_rec2100_pq;
     case GDK_COLOR_STATE_ID_REC2100_LINEAR:
       return gdk_convert_cicp_from_rec2100_linear;
-    case GDK_COLOR_STATE_ID_OKLAB:
-      return gdk_convert_cicp_from_oklab;
-    case GDK_COLOR_STATE_ID_OKLCH:
-      return gdk_convert_cicp_from_oklch;
 
     case GDK_COLOR_STATE_N_IDS:
     default:

gdk/gdkcolorstateprivate.h
@@ -13,11 +13,17 @@ typedef enum
   GDK_COLOR_STATE_ID_SRGB_LINEAR,
   GDK_COLOR_STATE_ID_REC2100_PQ,
   GDK_COLOR_STATE_ID_REC2100_LINEAR,
-  GDK_COLOR_STATE_ID_OKLAB,
-  GDK_COLOR_STATE_ID_OKLCH,
 
   GDK_COLOR_STATE_N_IDS
 } GdkColorStateId;
+
+typedef enum
+{
+  GDK_BUILTIN_COLOR_STATE_ID_OKLAB,
+  GDK_BUILTIN_COLOR_STATE_ID_OKLCH,
+
+  GDK_BUILTIN_COLOR_STATE_N_IDS
+} GdkBuiltinColorStateId;
 
 typedef struct _GdkColorStateClass GdkColorStateClass;
 
@@ -69,18 +75,33 @@ struct _GdkDefaultColorState
   GdkCicp cicp;
 };
 
+typedef struct _GdkBuiltinColorState GdkBuiltinColorState;
+
+struct _GdkBuiltinColorState
+{
+  GdkColorState parent;
+
+  const char *name;
+  GdkFloatColorConvert convert_to[GDK_COLOR_STATE_N_IDS];
+  GdkFloatColorConvert convert_from[GDK_COLOR_STATE_N_IDS];
+};
+
 extern GdkDefaultColorState gdk_default_color_states[GDK_COLOR_STATE_N_IDS];
+extern GdkBuiltinColorState gdk_builtin_color_states[GDK_BUILTIN_COLOR_STATE_N_IDS];
 
 #define GDK_COLOR_STATE_SRGB           ((GdkColorState *) &gdk_default_color_states[GDK_COLOR_STATE_ID_SRGB])
 #define GDK_COLOR_STATE_SRGB_LINEAR    ((GdkColorState *) &gdk_default_color_states[GDK_COLOR_STATE_ID_SRGB_LINEAR])
 #define GDK_COLOR_STATE_REC2100_PQ     ((GdkColorState *) &gdk_default_color_states[GDK_COLOR_STATE_ID_REC2100_PQ])
 #define GDK_COLOR_STATE_REC2100_LINEAR ((GdkColorState *) &gdk_default_color_states[GDK_COLOR_STATE_ID_REC2100_LINEAR])
-#define GDK_COLOR_STATE_OKLAB          ((GdkColorState *) &gdk_default_color_states[GDK_COLOR_STATE_ID_OKLAB])
-#define GDK_COLOR_STATE_OKLCH          ((GdkColorState *) &gdk_default_color_states[GDK_COLOR_STATE_ID_OKLCH])
+#define GDK_COLOR_STATE_OKLAB          ((GdkColorState *) &gdk_builtin_color_states[GDK_BUILTIN_COLOR_STATE_ID_OKLAB])
+#define GDK_COLOR_STATE_OKLCH          ((GdkColorState *) &gdk_builtin_color_states[GDK_BUILTIN_COLOR_STATE_ID_OKLCH])
 
 #define GDK_IS_DEFAULT_COLOR_STATE(c) ((GdkDefaultColorState *) (c) >= &gdk_default_color_states[0] && \
                                        (GdkDefaultColorState *) (c) < &gdk_default_color_states[GDK_COLOR_STATE_N_IDS])
 #define GDK_DEFAULT_COLOR_STATE_ID(c) ((GdkColorStateId) (((GdkDefaultColorState *) c) - gdk_default_color_states))
+#define GDK_IS_BUILTIN_COLOR_STATE(c) ((GdkBuiltinColorState *) (c) >= &gdk_builtin_color_states[0] && \
+                                       (GdkBuiltinColorState *) (c) < &gdk_builtin_color_states[GDK_BUILTIN_COLOR_STATE_N_IDS])
+#define GDK_BUILTIN_COLOR_STATE_ID(c) ((GdkBuiltinColorStateId) (((GdkBuiltinColorState *) c) - gdk_builtin_color_states))
 
 const char *    gdk_color_state_get_name                (GdkColorState          *self);
 GdkColorState * gdk_color_state_get_no_srgb_tf          (GdkColorState          *self);
@@ -123,7 +144,8 @@ gdk_color_state_get_by_id (GdkColorStateId id)
 static inline GdkColorState *
 _gdk_color_state_ref (GdkColorState *self)
 {
-  if (GDK_IS_DEFAULT_COLOR_STATE (self))
+  if (GDK_IS_DEFAULT_COLOR_STATE (self) ||
+      GDK_IS_BUILTIN_COLOR_STATE (self))
     return self;
 
   g_atomic_ref_count_inc (&self->ref_count);
@@ -135,7 +157,8 @@ _gdk_color_state_ref (GdkColorState *self)
 static inline void
 _gdk_color_state_unref (GdkColorState *self)
 {
-  if (GDK_IS_DEFAULT_COLOR_STATE (self))
+  if (GDK_IS_DEFAULT_COLOR_STATE (self) ||
+      GDK_IS_BUILTIN_COLOR_STATE (self))
     return;
 
   if (g_atomic_ref_count_dec (&self->ref_count))

gsk/gpu/gskgpuconvertbuiltinop.c
@@ -1,0 +1,140 @@
+#include "config.h"
+
+#include "gskgpuconvertbuiltinopprivate.h"
+
+#include "gskgpuframeprivate.h"
+#include "gskgpuimageprivate.h"
+#include "gskgpuprintprivate.h"
+#include "gskrectprivate.h"
+#include "gdk/gdkcolorstateprivate.h"
+
+#include "gpu/shaders/gskgpuconvertbuiltininstance.h"
+
+typedef struct _GskGpuConvertBuiltinOp GskGpuConvertBuiltinOp;
+
+struct _GskGpuConvertBuiltinOp
+{
+  GskGpuShaderOp op;
+};
+
+#define VARIATION_COLOR_SPACE_MASK     (0xFF)
+#define VARIATION_OPACITY              (1u << 8)
+#define VARIATION_STRAIGHT_ALPHA       (1u << 9)
+#define VARIATION_PREMULTIPLY          (1u << 10)
+#define VARIATION_REVERSE              (1u << 11)
+
+static void
+gsk_gpu_convert_builtin_op_print_instance (GskGpuShaderOp *shader,
+                                           gpointer        instance_,
+                                           GString        *string)
+{
+  GskGpuConvertbuiltinInstance *instance = (GskGpuConvertbuiltinInstance *) instance_;
+  GdkColorState *builtin = (GdkColorState *) &gdk_builtin_color_states [shader->variation & VARIATION_COLOR_SPACE_MASK];
+
+  gsk_gpu_print_rect (string, instance->rect);
+  gsk_gpu_print_image (string, shader->images[0]);
+  if (shader->variation & VARIATION_STRAIGHT_ALPHA)
+    gsk_gpu_print_string (string, "straight");
+  if (shader->variation & VARIATION_REVERSE)
+    gsk_gpu_print_string (string, "reverse");
+  gsk_gpu_print_string (string, gdk_color_state_get_name (builtin));
+}
+
+static const GskGpuShaderOpClass GSK_GPU_CONVERT_OP_CLASS = {
+  {
+    GSK_GPU_OP_SIZE (GskGpuConvertBuiltinOp),
+    GSK_GPU_STAGE_SHADER,
+    gsk_gpu_shader_op_finish,
+    gsk_gpu_shader_op_print,
+#ifdef GDK_RENDERING_VULKAN
+    gsk_gpu_shader_op_vk_command,
+#endif
+    gsk_gpu_shader_op_gl_command
+  },
+  "gskgpuconvertbuiltin",
+  gsk_gpu_convertbuiltin_n_textures,
+  sizeof (GskGpuConvertbuiltinInstance),
+#ifdef GDK_RENDERING_VULKAN
+  &gsk_gpu_convertbuiltin_info,
+#endif
+  gsk_gpu_convert_builtin_op_print_instance,
+  gsk_gpu_convertbuiltin_setup_attrib_locations,
+  gsk_gpu_convertbuiltin_setup_vao
+};
+
+static GdkColorState *
+gsk_gpu_get_shader_color_state (GdkColorState *builtin)
+{
+  switch (GDK_BUILTIN_COLOR_STATE_ID (builtin))
+    {
+    case GDK_BUILTIN_COLOR_STATE_ID_OKLAB:
+    case GDK_BUILTIN_COLOR_STATE_ID_OKLCH:
+      return GDK_COLOR_STATE_SRGB_LINEAR;
+
+    case GDK_BUILTIN_COLOR_STATE_N_IDS:
+    default:
+      g_assert_not_reached ();
+      return NULL;
+    }
+}
+
+void
+gsk_gpu_convert_from_builtin_op (GskGpuFrame             *frame,
+                                 GskGpuShaderClip         clip,
+                                 GdkColorState           *ccs,
+                                 GdkColorState           *builtin,
+                                 float                    opacity,
+                                 const graphene_point_t  *offset,
+                                 const GskGpuShaderImage *image)
+{
+  GskGpuConvertbuiltinInstance *instance;
+
+  g_assert (GDK_IS_BUILTIN_COLOR_STATE (builtin));
+
+  gsk_gpu_shader_op_alloc (frame,
+                           &GSK_GPU_CONVERT_OP_CLASS,
+                           gsk_gpu_color_states_create (ccs, TRUE,
+                                                        gsk_gpu_get_shader_color_state (builtin), FALSE),
+                           GDK_BUILTIN_COLOR_STATE_ID (builtin) |
+                           (opacity < 1.0 ? VARIATION_OPACITY : 0) |
+                           ((gsk_gpu_image_get_flags (image->image) & GSK_GPU_IMAGE_STRAIGHT_ALPHA) ? VARIATION_STRAIGHT_ALPHA : 0),
+                           clip,
+                           (GskGpuImage *[1]) { image->image },
+                           (GskGpuSampler[1]) { image->sampler },
+                           &instance);
+
+  gsk_gpu_rect_to_float (image->coverage, offset, instance->rect);
+  gsk_gpu_rect_to_float (image->bounds, offset, instance->tex_rect);
+  instance->opacity = opacity;
+}
+
+void
+gsk_gpu_convert_to_builtin_op (GskGpuFrame             *frame,
+                               GskGpuShaderClip         clip,
+                               GdkColorState           *builtin,
+                               gboolean                 builtin_premultiplied,
+                               GdkColorState           *source_cs,
+                               float                    opacity,
+                               const graphene_point_t  *offset,
+                               const GskGpuShaderImage *image)
+{
+  GskGpuConvertbuiltinInstance *instance;
+
+  gsk_gpu_shader_op_alloc (frame,
+                           &GSK_GPU_CONVERT_OP_CLASS,
+                           gsk_gpu_color_states_create (source_cs, TRUE,
+                                                        gsk_gpu_get_shader_color_state (builtin), FALSE),
+                           GDK_BUILTIN_COLOR_STATE_ID (builtin) |
+                           (opacity < 1.0 ? VARIATION_OPACITY : 0) |
+                           ((gsk_gpu_image_get_flags (image->image) & GSK_GPU_IMAGE_STRAIGHT_ALPHA) ? VARIATION_STRAIGHT_ALPHA : 0) |
+                           (builtin_premultiplied ? VARIATION_PREMULTIPLY : 0) |
+                           VARIATION_REVERSE,
+                           clip,
+                           (GskGpuImage *[1]) { image->image },
+                           (GskGpuSampler[1]) { image->sampler },
+                           &instance);
+
+  gsk_gpu_rect_to_float (image->coverage, offset, instance->rect);
+  gsk_gpu_rect_to_float (image->bounds, offset, instance->tex_rect);
+  instance->opacity = opacity;
+}

gsk/gpu/gskgpuconvertbuiltinopprivate.h
@@ -1,0 +1,27 @@
+#pragma once
+
+#include "gskgpushaderopprivate.h"
+
+#include <graphene.h>
+
+G_BEGIN_DECLS
+
+void                    gsk_gpu_convert_from_builtin_op                 (GskGpuFrame                    *frame,
+                                                                         GskGpuShaderClip                clip,
+                                                                         GdkColorState                  *ccs,
+                                                                         GdkColorState                  *builtin,
+                                                                         float                           opacity,
+                                                                         const graphene_point_t         *offset,
+                                                                         const GskGpuShaderImage        *image);
+
+void                    gsk_gpu_convert_to_builtin_op                   (GskGpuFrame                    *frame,
+                                                                         GskGpuShaderClip                clip,
+                                                                         GdkColorState                  *builtin,
+                                                                         gboolean                        builtin_premultiplied,
+                                                                         GdkColorState                  *source_cs,
+                                                                         float                           opacity,
+                                                                         const graphene_point_t         *offset,
+                                                                         const GskGpuShaderImage        *image);
+
+
+G_END_DECLS

gsk/gpu/gskgpunodeprocessor.c
@@ -15,8 +15,9 @@
 #include "gskgpucolormatrixopprivate.h"
 #include "gskgpucoloropprivate.h"
 #include "gskgpuconicgradientopprivate.h"
-#include "gskgpuconvertopprivate.h"
+#include "gskgpuconvertbuiltinopprivate.h"
 #include "gskgpuconvertcicpopprivate.h"
+#include "gskgpuconvertopprivate.h"
 #include "gskgpucrossfadeopprivate.h"
 #include "gskgpudeviceprivate.h"
 #include "gskgpuframeprivate.h"
@@ -574,7 +575,22 @@ gsk_gpu_node_processor_image_op (GskGpuNodeProcessor   *self,
 
   straight_alpha = gsk_gpu_image_get_flags (image) & GSK_GPU_IMAGE_STRAIGHT_ALPHA;
 
-  if (!GDK_IS_DEFAULT_COLOR_STATE (image_color_state))
+  if (GDK_IS_BUILTIN_COLOR_STATE (image_color_state))
+    {
+      gsk_gpu_convert_from_builtin_op (self->frame,
+                                       gsk_gpu_clip_get_shader_clip (&self->clip, &self->offset, rect),
+                                       self->ccs,
+                                       image_color_state,
+                                       self->opacity,
+                                       &self->offset,
+                                       &(GskGpuShaderImage) {
+                                         image,
+                                         sampler,
+                                         rect,
+                                         tex_rect
+                                       });
+    }
+  else if (!GDK_IS_DEFAULT_COLOR_STATE (image_color_state))
     {
       const GdkCicp *cicp = gdk_color_state_get_cicp (image_color_state);
 
@@ -4304,7 +4320,23 @@ gsk_gpu_node_processor_convert_to (GskGpuNodeProcessor   *self,
 {
   gsk_gpu_node_processor_sync_globals (self, 0);
 
-  if (!GDK_IS_DEFAULT_COLOR_STATE (self->ccs))
+  if (GDK_IS_BUILTIN_COLOR_STATE (self->ccs))
+    {
+      gsk_gpu_convert_to_builtin_op (self->frame,
+                                     gsk_gpu_clip_get_shader_clip (&self->clip, &self->offset, rect),
+                                     self->ccs,
+                                     TRUE,
+                                     image_color_state,
+                                     self->opacity,
+                                     &self->offset,
+                                     &(GskGpuShaderImage) {
+                                         image,
+                                         GSK_GPU_SAMPLER_DEFAULT,
+                                         rect,
+                                         tex_rect
+                                     });
+    }
+  else if (!GDK_IS_DEFAULT_COLOR_STATE (self->ccs))
     {
       const GdkCicp *cicp = gdk_color_state_get_cicp (self->ccs);
 

gsk/gpu/shaders/color.glsl
@@ -111,54 +111,6 @@ const mat3 rec2020_from_srgb = mat3(
   0.043303, 0.011360, 0.895380
 );
 
-const mat3 oklab_to_lms = mat3(
-  1.0,           1.0,           1.0,
-  0.3963377774, -0.1055613458, -0.0894841775,
-  0.2158037573, -0.0638541728, -1.2914855480
-);
-
-const mat3 lms_to_srgb = mat3(
-  4.0767416621, -1.2684380046, -0.0041960863,
- -3.3077115913,  2.6097574011, -0.7034186147,
-  0.2309699292, -0.3413193965,  1.7076147010
-);
-
-const mat3 srgb_to_lms = mat3(
-  0.4122214708, 0.2119034982, 0.0883024619,
-  0.5363325363, 0.6806995451, 0.2817188376,
-  0.0514459929, 0.1073969566, 0.6299787005
-);
-
-const mat3 lms_to_oklab = mat3(
-  0.2104542553,  1.9779984951,  0.0259040371,
-  0.7936177850, -2.4285922050,  0.7827717662,
- -0.0040720468,  0.4505937099, -0.8086757660
-);
-
-vec3
-oklab_to_srgb_linear (vec3 color)
-{
-  vec3 lms = oklab_to_lms * color;
-
-  lms = vec3 (pow (lms.r, 3.0),
-              pow (lms.g, 3.0),
-              pow (lms.b, 3.0));
-
-  return lms_to_srgb * lms;
-}
-
-vec3
-srgb_linear_to_oklab (vec3 color)
-{
-  vec3 lms = srgb_to_lms * color;
-
-  lms = vec3 (pow (lms.r, 1.0/3.0),
-              pow (lms.g, 1.0/3.0),
-              pow (lms.b, 1.0/3.0));
-
-  return lms_to_oklab * lms;
-}
-
 vec3
 apply_eotf (vec3 color,
             uint cs)
@@ -178,9 +130,6 @@ apply_eotf (vec3 color,
     case GDK_COLOR_STATE_ID_SRGB_LINEAR:
     case GDK_COLOR_STATE_ID_REC2100_LINEAR:
       return color;
-
-    case GDK_COLOR_STATE_ID_OKLAB:
-      return oklab_to_srgb_linear (color);
 
     default:
       return vec3(1.0, 0.0, 0.8);
@@ -206,9 +155,6 @@ apply_oetf (vec3 color,
     case GDK_COLOR_STATE_ID_SRGB_LINEAR:
     case GDK_COLOR_STATE_ID_REC2100_LINEAR:
       return color;
-
-    case GDK_COLOR_STATE_ID_OKLAB:
-      return srgb_linear_to_oklab (color);
 
     default:
       return vec3(0.0, 1.0, 0.8);
@@ -237,69 +183,8 @@ linear_color_space (uint cs)
     case GDK_COLOR_STATE_ID_SRGB_LINEAR:    return GDK_COLOR_STATE_ID_SRGB_LINEAR;
     case GDK_COLOR_STATE_ID_REC2100_PQ:     return GDK_COLOR_STATE_ID_REC2100_LINEAR;
     case GDK_COLOR_STATE_ID_REC2100_LINEAR: return GDK_COLOR_STATE_ID_REC2100_LINEAR;
-    case GDK_COLOR_STATE_ID_OKLAB:          return GDK_COLOR_STATE_ID_SRGB_LINEAR;
-    case GDK_COLOR_STATE_ID_OKLCH:          return GDK_COLOR_STATE_ID_SRGB_LINEAR;
     default:                                return 0u;
   };
-}
-
-uint
-rectangular_color_space (uint cs)
-{
-  if (cs == GDK_COLOR_STATE_ID_OKLCH)
-    return GDK_COLOR_STATE_ID_OKLAB;
-  else
-    return cs;
-}
-
-#define M_PI 3.14159265358979323846
-#define RAD_TO_DEG(x) ((x)*180.0/M_PI)
-#define DEG_TO_RAD(x) ((x)*M_PI/180.0)
-
-float
-normalize_hue (float h)
-{
-  while (h < 0.0)
-    h += 360.0;
-  while (h > 360.0)
-    h -= 360.0;
-  return h;
-}
-
-vec3
-oklch_to_oklab (vec3 color)
-{
-  color.z = normalize_hue (color.z);
-
-  return vec3 (color.x,
-               color.y * cos (DEG_TO_RAD (color.z)),
-               color.y * sin (DEG_TO_RAD (color.z)));
-}
-
-vec3
-oklab_to_oklch (vec3 color)
-{
-  return vec3 (color.x,
-               length (color.yz),
-               RAD_TO_DEG (atan (color.z, color.y)));
-}
-
-vec3
-to_rect (vec3 color, uint from)
-{
-  if (from == GDK_COLOR_STATE_ID_OKLCH)
-    return oklch_to_oklab (color);
-  else
-    return vec3(1, 0, 0.5);
-}
-
-vec3
-to_polar (vec3 color, uint to)
-{
-  if (to == GDK_COLOR_STATE_ID_OKLCH)
-    return oklab_to_oklch (color);
-  else
-    return vec3(1, 0, 0.5);
 }
 
 vec4
@@ -314,25 +199,17 @@ convert_color (vec4 color,
 
   if (from != to)
     {
-      uint from_rectangular = rectangular_color_space (from);
-      uint to_rectangular = rectangular_color_space (to);
-      uint from_linear = linear_color_space (from_rectangular);
-      uint to_linear = linear_color_space (to_rectangular);
+      uint from_linear = linear_color_space (from);
+      uint to_linear = linear_color_space (to);
 
-      if (from_rectangular != from)
-        color.rgb = to_rect (color.rgb, from);
-
-      if (from_linear != from_rectangular)
-        color.rgb = apply_eotf (color.rgb, from_rectangular);
+      if (from_linear != from)
+        color.rgb = apply_eotf (color.rgb, from);
 
       if (from_linear != to_linear)
         color.rgb = convert_linear (color.rgb, from_linear, to_linear);
 
-      if (to_linear != to_rectangular)
-        color.rgb = apply_oetf (color.rgb, to_rectangular);
-
-      if (to_rectangular != to)
-        color.rgb = to_polar (color.rgb, to);
+      if (to_linear != to)
+        color.rgb = apply_oetf (color.rgb, to);
     }
 
   if (to_premul && (!from_premul || from != to))

gsk/gpu/shaders/enums.glsl
@@ -56,8 +56,9 @@
 #define GDK_COLOR_STATE_ID_SRGB_LINEAR 1u
 #define GDK_COLOR_STATE_ID_REC2100_PQ 2u
 #define GDK_COLOR_STATE_ID_REC2100_LINEAR 3u
-#define GDK_COLOR_STATE_ID_OKLAB 4u
-#define GDK_COLOR_STATE_ID_OKLCH 5u
+
+#define GDK_BUILTIN_COLOR_STATE_ID_OKLAB 0u
+#define GDK_BUILTIN_COLOR_STATE_ID_OKLCH 1u
 
 #define TOP 0u
 #define RIGHT 1u

commit 6fa5576fbe294d415a2b579fea470d743251c7bd

colorstate: Make conversion to/from oklab/oklch not crash
... when the colorstate is not a default colorstate.

Fixes: 9d200c0002bf48a77fe1ce052f7e26c25796244d
Part-of: <https://gitlab.gnome.org/GNOME/gtk/-/merge_requests/9122>

gdk/gdkcolorstate.c
@@ -682,7 +682,10 @@ gdk_builtin_color_state_get_convert_to (GdkColorState  *color_state,
 {
   GdkBuiltinColorState *self = (GdkBuiltinColorState *) color_state;
 
-  return self->convert_to[GDK_DEFAULT_COLOR_STATE_ID (target)];
+  if (GDK_IS_DEFAULT_COLOR_STATE (target))
+    return self->convert_to[GDK_DEFAULT_COLOR_STATE_ID (target)];
+
+  return NULL;
 }
 
 static GdkFloatColorConvert
@@ -691,7 +694,10 @@ gdk_builtin_color_state_get_convert_from (GdkColorState  *color_state,
 {
   GdkBuiltinColorState *self = (GdkBuiltinColorState *) color_state;
 
-  return self->convert_from[GDK_DEFAULT_COLOR_STATE_ID (source)];
+  if (GDK_IS_DEFAULT_COLOR_STATE (source))
+    return self->convert_from[GDK_DEFAULT_COLOR_STATE_ID (source)];
+
+  return NULL;
 }
 
 static const GdkCicp *