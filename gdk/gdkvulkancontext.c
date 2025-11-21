ca58e397dc3cc96027ea724be61de002f36064cb
Benjamin Otte <otte@redhat.com>
2025-11-21 10:07:31 +0100

    vulkan: Don't treat Vulkan objects as pointers or 32bit breaks
    Fixes: #7883
    Part-of: <https://gitlab.gnome.org/GNOME/gtk/-/merge_requests/9170>

--- gdk/gdkvulkancontext.c
+++ gdk/gdkvulkancontext.c
@@ -692,7 +692,7 @@ gdk_vulkan_present_is_busy (GdkVulkanContext *self,
 {
   VkResult res;
 
-  if (present->vk_swapchain == NULL)
+  if (!present->vk_swapchain)
     return FALSE;
 
   if (!present->vk_fence)
@@ -1200,7 +1200,7 @@ gdk_vulkan_context_surface_detach (GdkDrawContext *context)
       if (priv->presents[i].vk_swapchain)
         {
           gdk_vulkan_context_unref_swapchain (self, priv->presents[i].vk_swapchain);
-          priv->presents[i].vk_swapchain = NULL;
+          priv->presents[i].vk_swapchain = VK_NULL_HANDLE;
         }
       vkDestroySemaphore (vk_device,
                           priv->presents[i].vk_semaphore,
