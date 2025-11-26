GNOME/gtk

commit 7c31c645931f585b756c75a6e769d89ac50c1ec2

gl: Enable GL_blend_func_extended in shaders
... if the GL driver supports it and it is required (ie only on GLES,
not on normal GL).

Part-of: <https://gitlab.gnome.org/GNOME/gtk/-/merge_requests/9159>

gsk/gpu/gskgldevice.c
@@ -400,6 +400,8 @@ gsk_gl_device_load_shader (GskGLDevice       *self,
   g_string_append (preamble, "\n");
   if (self->api == GDK_GL_API_GLES)
     {
+      if (gdk_display_get_gl_context (gsk_gpu_device_get_display (GSK_GPU_DEVICE (self))))
+        g_string_append (preamble, "#extension GL_EXT_blend_func_extended : require\n");
       if (gsk_gpu_shader_flags_has_external_textures (flags))
         {
           g_string_append (preamble, "#extension GL_OES_EGL_image_external_essl3 : require\n");

commit e0ed24180e74daff45900c2b17aaf9d782b49005

gl: Fix copy/paste error
Instead of just querying the context, actually check that the context
has the required feature.

Fixes: 7c31c645931f585b756c75a6e769d89ac50c1ec2
Fixes: #7894
Part-of: <https://gitlab.gnome.org/GNOME/gtk/-/merge_requests/9196>

gsk/gpu/gskgldevice.c
@@ -380,6 +380,16 @@ print_shader_info (const char *prefix,
     }
 }
 
+static gboolean
+gsk_gl_device_has_gl_feature (GskGLDevice   *self,
+                              GdkGLFeatures  feature)
+{
+  GdkDisplay *display = gsk_gpu_device_get_display (GSK_GPU_DEVICE (self));
+  GdkGLContext *context = gdk_display_get_gl_context (display);
+
+  return gdk_gl_context_has_feature (context, feature);
+}
+
 static GLuint
 gsk_gl_device_load_shader (GskGLDevice       *self,
                            const char        *program_name,
@@ -400,7 +410,7 @@ gsk_gl_device_load_shader (GskGLDevice       *self,
   g_string_append (preamble, "\n");
   if (self->api == GDK_GL_API_GLES)
     {
-      if (gdk_display_get_gl_context (gsk_gpu_device_get_display (GSK_GPU_DEVICE (self))))
+      if (gsk_gl_device_has_gl_feature (self, GDK_GL_FEATURE_BLEND_FUNC_EXTENDED))
         g_string_append (preamble, "#extension GL_EXT_blend_func_extended : require\n");
       if (gsk_gpu_shader_flags_has_external_textures (flags))
         {