f028e5a45d59e2ef412075197cb089b1cd1f9040
Christoph Reiter <reiter.christoph@gmail.com>
2025-11-30 20:46:38 +0100

    CI: restore -Werror with MSYS2
    there was a typo
    
    Part-of: <https://gitlab.gnome.org/GNOME/gtk/-/merge_requests/9208>

--- .gitlab-ci/test-msys2.sh
+++ .gitlab-ci/test-msys2.sh
@@ -42,7 +42,7 @@ ccache --zero-stats
 ccache --show-stats
 export CCACHE_DISABLE=true
 meson setup \
-    ${COMMON_MESON_CFLAGS} \
+    ${COMMON_MESON_FLAGS} \
     -Dx11-backend=false \
     -Dwayland-backend=false \
     -Dwin32-backend=true \
