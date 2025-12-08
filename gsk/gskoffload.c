f4848cdf975f485261dc32125edd2d6286588c51
Benjamin Otte <otte@redhat.com>
2025-12-08 05:06:06 +0100

    offload: Ignore isolation nodes when finding textures
    Isolation nodes have no visual effect on texure rendering, so they can
    safely be ignored.
    
    And it is important to handle because external content - like video
    frames - is now isolated.
    
    Part-of: <https://gitlab.gnome.org/GNOME/gtk/-/merge_requests/9235>

--- gsk/gskoffload.c
+++ gsk/gskoffload.c
@@ -27,6 +27,7 @@
 #include "gskcontainernodeprivate.h"
 #include "gskdebugnode.h"
 #include "gskdebugprivate.h"
+#include "gskisolationnode.h"
 #include "gskopacitynode.h"
 #include "gskrendernode.h"
 #include "gskrectprivate.h"
@@ -124,6 +125,10 @@ find_texture_to_attach (GskOffload          *self,
           if (gsk_opacity_node_get_opacity (node) < 1.0)
             goto out;
           node = gsk_opacity_node_get_child (node);
+          break;
+
+        case GSK_ISOLATION_NODE:
+          node = gsk_isolation_node_get_child (node);
           break;
 
         case GSK_CONTAINER_NODE:
