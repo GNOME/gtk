GNOME/gtk

commit b8c95f03f4ff4dcc88685c09488b9e66f18c5702

API: Add GskIsolationNode
This is a node that isolates its child from the surrounding nodes.

A flags parameter allows controlling which things get passed through.

It includes an implementation for the GPU renderer and a GtkSnapshot
function.

Testcase attached.

Part-of: <https://gitlab.gnome.org/GNOME/gtk/-/merge_requests/9235>

gtk/gtksnapshot.c
@@ -153,6 +153,9 @@ G_GNUC_END_IGNORE_DEPRECATIONS
       GskPorterDuff op;
       GskRenderNode *mask;
     } composite;
+    struct {
+      GskIsolation allowed;
+    } isolation;
   } data;
 };
 
@@ -524,6 +527,56 @@ gtk_snapshot_push_opacity (GtkSnapshot *snapshot,
                                    gtk_snapshot_collect_opacity,
                                    NULL);
   state->data.opacity.opacity = CLAMP (opacity, 0.0, 1.0);
+}
+
+static GskRenderNode *
+gtk_snapshot_collect_isolation (GtkSnapshot      *snapshot,
+                                GtkSnapshotState *state,
+                                GskRenderNode   **nodes,
+                                guint             n_nodes)
+{
+  GskRenderNode *node, *isolation_node;
+
+  node = gtk_snapshot_collect_default (snapshot, state, nodes, n_nodes);
+  if (node == NULL)
+    return NULL;
+
+  isolation_node = gsk_isolation_node_new (node, state->data.isolation.allowed);
+  gsk_render_node_unref (node);
+
+  return isolation_node;
+}
+
+/**
+ * gtk_snapshot_push_isolation:
+ * @snapshot: a `GtkSnapshot`
+ * @features: features that are isolated
+ *
+ * Isolates the following drawing operations from previous ones.
+ *
+ * You can express "everything but these flags" in a forward compatible
+ * way by using bit math:
+ * `GSK_ISOLATION_ALL & ~(GSK_ISOLATION_BACKGROUND | GSK_ISOLATION_COPY_PASTE)`
+ * will isolate everything but background and copy/paste.
+ *
+ * For what isolation features exist, see [flags@Gsk.Isolation].
+ *
+ * Content is isolated until the next call to [method@Gtk.Snapshot.pop].
+ *
+ * Since: 4.22
+ */
+void
+gtk_snapshot_push_isolation (GtkSnapshot  *snapshot,
+                             GskIsolation  allowed)
+{
+  GtkSnapshotState *current_state = gtk_snapshot_get_current_state (snapshot);
+  GtkSnapshotState *state;
+
+  state = gtk_snapshot_push_state (snapshot,
+                                   current_state->transform,
+                                   gtk_snapshot_collect_isolation,
+                                   NULL);
+  state->data.isolation.allowed = allowed;
 }
 
 static GskRenderNode *

gtk/gtksnapshot.h
@@ -70,6 +70,9 @@ void            gtk_snapshot_push_debug                 (GtkSnapshot            
 GDK_AVAILABLE_IN_ALL
 void            gtk_snapshot_push_opacity               (GtkSnapshot            *snapshot,
                                                          double                  opacity);
+GDK_AVAILABLE_IN_4_22
+void            gtk_snapshot_push_isolation             (GtkSnapshot            *snapshot,
+                                                         GskIsolation            allowed);
 GDK_AVAILABLE_IN_ALL
 void            gtk_snapshot_push_blur                  (GtkSnapshot            *snapshot,
                                                          double                  radius);

gsk/broadway/gskbroadwayrenderer.c
@@ -309,6 +309,7 @@ collect_reused_child_nodes (GskRenderer *renderer,
     case GSK_COPY_NODE:
     case GSK_PASTE_NODE:
     case GSK_COMPOSITE_NODE:
+    case GSK_ISOLATION_NODE:
 
     default:
 
@@ -911,6 +912,7 @@ gsk_broadway_renderer_add_node (GskRenderer *renderer,
     case GSK_COPY_NODE:
     case GSK_PASTE_NODE:
     case GSK_COMPOSITE_NODE:
+    case GSK_ISOLATION_NODE:
     default:
       break; /* Fallback */
     }

gsk/gpu/gskgpunodeprocessor.c
@@ -53,6 +53,7 @@
 #include "gskdebugprivate.h"
 #include "gskdebugnode.h"
 #include "gskfillnode.h"
+#include "gskisolationnode.h"
 #include "gskmasknode.h"
 #include "gskopacitynode.h"
 #include "gskpath.h"
@@ -4077,6 +4078,26 @@ gsk_gpu_node_processor_add_composite_node (GskGpuNodeProcessor *self,
 }
 
 static void
+gsk_gpu_node_processor_add_isolation_node (GskGpuNodeProcessor *self,
+                                           GskRenderNode       *node)
+{
+  GskRenderNode *child = gsk_isolation_node_get_child (node);
+  GskIsolation isolations = gsk_isolation_node_get_isolations (node);
+
+  if (isolations & GSK_ISOLATION_BACKGROUND)
+    {
+      if (gsk_render_node_get_copy_mode (child) != GSK_COPY_NONE ||
+          gsk_render_node_clears_background (child))
+        {
+          gsk_gpu_node_processor_add_without_opacity (self, child);
+          return;
+        }
+    }
+  
+  gsk_gpu_node_processor_add_node (self, child);
+}
+
+static void
 gsk_gpu_node_processor_add_container_node (GskGpuNodeProcessor *self,
                                            GskRenderNode       *node)
 {
@@ -4441,6 +4462,13 @@ static const struct
     0,
     GSK_GPU_HANDLE_OPACITY,
     gsk_gpu_node_processor_add_composite_node,
+    NULL,
+    NULL,
+  },
+  [GSK_ISOLATION_NODE] = {
+    GSK_GPU_GLOBAL_MATRIX | GSK_GPU_GLOBAL_SCALE | GSK_GPU_GLOBAL_CLIP | GSK_GPU_GLOBAL_SCISSOR | GSK_GPU_GLOBAL_BLEND,
+    GSK_GPU_HANDLE_OPACITY,
+    gsk_gpu_node_processor_add_isolation_node,
     NULL,
     NULL,
   },

gsk/gsk.h
@@ -36,6 +36,7 @@
 #include <gsk/gskenums.h>
 #include <gsk/gskfillnode.h>
 #include <gsk/gskglshadernode.h>
+#include <gsk/gskisolationnode.h>
 #include <gsk/gskmasknode.h>
 #include <gsk/gskopacitynode.h>
 #include <gsk/gskpastenode.h>

gsk/gskcopypasteutils.c
@@ -24,6 +24,7 @@
 #include "gskcolornode.h"
 #include "gskcontainernode.h"
 #include "gskcopynode.h"
+#include "gskisolationnode.h"
 #include "gskopacitynode.h"
 #include "gskpastenode.h"
 #include "gskrendernodeprivate.h"
@@ -125,6 +126,7 @@ replay_partial_node (const PartialNode *replay)
         case GSK_COPY_NODE:
         case GSK_PASTE_NODE:
         case GSK_COMPOSITE_NODE:
+        case GSK_ISOLATION_NODE:
           /* These all don't record anything, so we never
            * encounter them */
         case GSK_NOT_A_RENDER_NODE:
@@ -217,6 +219,20 @@ replace_copy_paste_node_record (GskRenderReplay *replay,
         recording->nodes = NULL;
         result = gsk_render_replay_default (replay, node);
         recording->nodes = saved;
+      }
+      break;
+
+    case GSK_ISOLATION_NODE:
+      /* Do either of the 2 above, depending on flags */
+      {
+        GskIsolation isolations = gsk_isolation_node_get_isolations (node);
+        Recording saved = *recording;
+        if (isolations & GSK_ISOLATION_BACKGROUND)
+          recording->nodes = NULL;
+        if (isolations & GSK_ISOLATION_COPY_PASTE)
+          recording->copies = NULL;
+        result = gsk_render_replay_default (replay, node);
+        *recording = saved;
       }
       break;
 

gsk/gskenums.h
@@ -126,6 +126,14 @@
  * Since: 4.22
  */
 
+/**
+ * GSK_ISOLATION_NODE:
+ *
+ * A node that isolated content of its child from previous content.
+ *
+ * Since: 4.22
+ */
+
 typedef enum {
   GSK_NOT_A_RENDER_NODE = 0,
   GSK_CONTAINER_NODE,
@@ -162,6 +170,7 @@ typedef enum {
   GSK_COPY_NODE,
   GSK_PASTE_NODE,
   GSK_COMPOSITE_NODE,
+  GSK_ISOLATION_NODE,
 } GskRenderNodeType;
 
 /**
@@ -318,6 +327,32 @@ typedef enum {
   GSK_FILL_RULE_WINDING,
   GSK_FILL_RULE_EVEN_ODD
 } GskFillRule;
+
+/**
+ * GskIsolation:
+ * @GSK_ISOLATION_NONE: No isolation is defined.
+ * @GSK_ISOLATION_BACKGROUND: If the background should be made available.
+ *   If the background is not available, future operations will be rendered
+ *   to a transparent background and added to the existing background later.
+ * @GSK_ISOLATION_COPY_PASTE: If copies should be available to paste nodes.
+ *   If copies are not available, paste nodes can only paste from copies that
+ *   are made inside the isolated contents.
+ * @GSK_ISOLATION_ALL: Isolate everything. This will include features that
+ *   are added in the future.
+ *
+ * These flags describe the types of isolations possible with a
+ * [class@Gsk.IsolationNode].
+ *
+ * More isolation options may be added in the future.
+ *
+ * Since: 4.22
+ */
+typedef enum {
+  GSK_ISOLATION_NONE       = 0,
+  GSK_ISOLATION_BACKGROUND = 1 << 0,
+  GSK_ISOLATION_COPY_PASTE = 1 << 1,
+  GSK_ISOLATION_ALL        = -1
+} GskIsolation;
 
 /**
  * GskLineCap:

gsk/gskisolationnode.c
@@ -1,0 +1,267 @@
+/* GSK - The GTK Scene Kit
+ *
+ * Copyright 2025  Benjamin Otte
+ *
+ * This library is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Lesser General Public
+ * License as published by the Free Software Foundation; either
+ * version 2 of the License, or (at your option) any later version.
+ *
+ * This library is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public
+ * License along with this library. If not, see <http://www.gnu.org/licenses/>.
+ */
+
+#include "config.h"
+
+#include "gskisolationnode.h"
+
+#include "gskrectprivate.h"
+#include "gskrendernodeprivate.h"
+#include "gskrenderreplay.h"
+
+#include "gdk/gdkcairoprivate.h"
+
+/**
+ * GskIsolationNode:
+ *
+ * A render node that isolates its child from surrounding rendernodes.
+ *
+ * Since: 4.22
+ */
+struct _GskIsolationNode
+{
+  GskRenderNode render_node;
+
+  GskRenderNode *child;
+  GskIsolation isolations;
+};
+
+static gboolean
+gsk_isolation_node_is_isolating (GskIsolationNode *self,
+                                 GskIsolation      feature)
+{
+  return (self->isolations & feature) == feature;
+}
+
+static void
+gsk_isolation_node_finalize (GskRenderNode *node)
+{
+  GskIsolationNode *self = (GskIsolationNode *) node;
+  GskRenderNodeClass *parent_class = g_type_class_peek (g_type_parent (GSK_TYPE_ISOLATION_NODE));
+
+  gsk_render_node_unref (self->child);
+
+  parent_class->finalize (node);
+}
+
+static void
+gsk_isolation_node_draw (GskRenderNode *node,
+                         cairo_t       *cr,
+                         GskCairoData  *data)
+{
+  GskIsolationNode *self = (GskIsolationNode *) node;
+
+  if (gsk_isolation_node_is_isolating (self, GSK_ISOLATION_BACKGROUND))
+    {
+      gdk_cairo_rectangle_snap_to_grid (cr, &node->bounds);
+      cairo_clip (cr);
+      cairo_push_group (cr);
+    }
+
+  gsk_render_node_draw_full (self->child, cr, data);
+
+  if (gsk_isolation_node_is_isolating (self, GSK_ISOLATION_BACKGROUND))
+    {
+      cairo_pop_group_to_source (cr);
+      cairo_paint (cr);
+    }
+}
+
+static void
+gsk_isolation_node_diff (GskRenderNode *node1,
+                         GskRenderNode *node2,
+                         GskDiffData   *data)
+{
+  GskIsolationNode *self1 = (GskIsolationNode *) node1;
+  GskIsolationNode *self2 = (GskIsolationNode *) node2;
+  GskDiffData child_data = {
+    .surface = data->surface
+  };
+
+  if (self1->isolations != self2->isolations)
+    {
+      gsk_render_node_diff_impossible (node1, node2, data);
+      return;
+    }
+
+  if (gsk_isolation_node_is_isolating (self1, GSK_ISOLATION_BACKGROUND))
+    child_data.region = cairo_region_create ();
+  else
+    child_data.region = data->region;
+
+  if (gsk_isolation_node_is_isolating (self1, GSK_ISOLATION_COPY_PASTE))
+    child_data.copies = NULL;
+  else
+    child_data.copies = data->copies;
+
+  gsk_render_node_diff (self1->child, self2->child, &child_data);
+
+  if (gsk_isolation_node_is_isolating (self1, GSK_ISOLATION_BACKGROUND))
+    {
+      cairo_region_union (data->region, child_data.region);
+      cairo_region_destroy (child_data.region);
+    }
+}
+
+static gboolean
+gsk_isolation_node_get_opaque_rect (GskRenderNode   *node,
+                                    graphene_rect_t *out_opaque)
+{
+  GskIsolationNode *self = (GskIsolationNode *) node;
+
+  return gsk_render_node_get_opaque_rect (self->child, out_opaque);
+}
+
+static GskRenderNode **
+gsk_isolation_node_get_children (GskRenderNode *node,
+                                 gsize         *n_children)
+{
+  GskIsolationNode *self = (GskIsolationNode *) node;
+
+  *n_children = 1;
+
+  return &self->child;
+}
+
+static GskRenderNode *
+gsk_isolation_node_replay (GskRenderNode   *node,
+                           GskRenderReplay *replay)
+{
+  GskIsolationNode *self = (GskIsolationNode *) node;
+  GskRenderNode *result, *child;
+
+  child = gsk_render_replay_filter_node (replay, self->child);
+
+  if (child == NULL)
+    return NULL;
+
+  if (child == self->child)
+    result = gsk_render_node_ref (node);
+  else
+    result = gsk_isolation_node_new (child, self->isolations);
+
+  gsk_render_node_unref (child);
+
+  return result;
+}
+
+static void
+gsk_isolation_node_class_init (gpointer g_class,
+                               gpointer class_data)
+{
+  GskRenderNodeClass *node_class = g_class;
+
+  node_class->node_type = GSK_ISOLATION_NODE;
+
+  node_class->finalize = gsk_isolation_node_finalize;
+  node_class->draw = gsk_isolation_node_draw;
+  node_class->diff = gsk_isolation_node_diff;
+  node_class->get_children = gsk_isolation_node_get_children;
+  node_class->replay = gsk_isolation_node_replay;
+  node_class->get_opaque_rect = gsk_isolation_node_get_opaque_rect;
+}
+
+GSK_DEFINE_RENDER_NODE_TYPE (GskIsolationNode, gsk_isolation_node)
+
+/**
+ * gsk_isolation_node_new:
+ * @child: The child
+ * @isolations: features to isolate
+ *
+ * Creates a `GskRenderNode` that isolates the drawing operations of
+ * the child from surrounding ones.
+ *
+ * You can express "everything but these flags" in a forward compatible
+ * way by using bit math:
+ * `GSK_ISOLATION_ALL & ~(GSK_ISOLATION_BACKGROUND | GSK_ISOLATION_COPY_PASTE)`
+ * will isolate everything but background and copy/paste.
+ *
+ * For the available isolations, see [flags@Gsk.Isolation].
+ *
+ * Returns: (transfer full) (type GskIsolationNode): A new `GskRenderNode`
+ *
+ * Since: 4.22
+ */
+GskRenderNode *
+gsk_isolation_node_new (GskRenderNode *child,
+                        GskIsolation   isolations)
+{
+  GskIsolationNode *self;
+  GskRenderNode *node;
+
+  g_return_val_if_fail (GSK_IS_RENDER_NODE (child), NULL);
+
+  self = gsk_render_node_alloc (GSK_TYPE_ISOLATION_NODE);
+  node = (GskRenderNode *) self;
+  node->fully_opaque = child->fully_opaque;
+
+  self->child = gsk_render_node_ref (child);
+  self->isolations = isolations;
+
+  gsk_rect_init_from_rect (&node->bounds, &child->bounds);
+
+  node->preferred_depth = gsk_render_node_get_preferred_depth (child);
+  node->is_hdr = gsk_render_node_is_hdr (child);
+  if (!gsk_isolation_node_is_isolating (self, GSK_ISOLATION_BACKGROUND))
+    {
+      node->clears_background = gsk_render_node_clears_background (child);
+      node->copy_mode = gsk_render_node_get_copy_mode (child);
+    }
+  if (!gsk_isolation_node_is_isolating (self, GSK_ISOLATION_COPY_PASTE))
+    node->contains_paste_node = gsk_render_node_contains_paste_node (child);
+  node->contains_subsurface_node = gsk_render_node_contains_subsurface_node (child);
+
+  return node;
+}
+
+/**
+ * gsk_isolation_node_get_child:
+ * @node: (type GskIsolationNode): an isolation `GskRenderNode`
+ *
+ * Gets the child node that is getting drawn by the given @node.
+ *
+ * Returns: (transfer none): the child `GskRenderNode`
+ *
+ * Since: 4.22
+ **/
+GskRenderNode *
+gsk_isolation_node_get_child (const GskRenderNode *node)
+{
+  const GskIsolationNode *self = (const GskIsolationNode *) node;
+
+  return self->child;
+}
+
+/**
+ * gsk_isolation_node_get_isolations:
+ * @node: (type GskIsolationNode): an isolation `GskRenderNode`
+ *
+ * Gets the isolation features that are enforced by this node.
+ *
+ * Returns: the isolation features
+ *
+ * Since: 4.22
+ **/
+GskIsolation
+gsk_isolation_node_get_isolations (const GskRenderNode *node)
+{
+  const GskIsolationNode *self = (const GskIsolationNode *) node;
+
+  return self->isolations;
+}
+

commit 6c41ba42c2293a986a6c0f0b9aa6d255a203818d

snapshot: Rename variables
They weren't renamed from a pre-merge refactoring.

Fixes warnings from gi-docgen.

Fixes: b8c95f03f4ff4dcc88685c09488b9e66f18c5702

gtk/gtksnapshot.c
@@ -154,7 +154,7 @@ G_GNUC_END_IGNORE_DEPRECATIONS
       GskRenderNode *mask;
     } composite;
     struct {
-      GskIsolation allowed;
+      GskIsolation features;
     } isolation;
   } data;
 };
@@ -541,7 +541,7 @@ gtk_snapshot_collect_isolation (GtkSnapshot      *snapshot,
   if (node == NULL)
     return NULL;
 
-  isolation_node = gsk_isolation_node_new (node, state->data.isolation.allowed);
+  isolation_node = gsk_isolation_node_new (node, state->data.isolation.features);
   gsk_render_node_unref (node);
 
   return isolation_node;
@@ -567,7 +567,7 @@ gtk_snapshot_collect_isolation (GtkSnapshot      *snapshot,
  */
 void
 gtk_snapshot_push_isolation (GtkSnapshot  *snapshot,
-                             GskIsolation  allowed)
+                             GskIsolation  features)
 {
   GtkSnapshotState *current_state = gtk_snapshot_get_current_state (snapshot);
   GtkSnapshotState *state;
@@ -576,7 +576,7 @@ gtk_snapshot_push_isolation (GtkSnapshot  *snapshot,
                                    current_state->transform,
                                    gtk_snapshot_collect_isolation,
                                    NULL);
-  state->data.isolation.allowed = allowed;
+  state->data.isolation.features = features;
 }
 
 static GskRenderNode *

gtk/gtksnapshot.h
@@ -72,7 +72,7 @@ void            gtk_snapshot_push_opacity               (GtkSnapshot            
                                                          double                  opacity);
 GDK_AVAILABLE_IN_4_22
 void            gtk_snapshot_push_isolation             (GtkSnapshot            *snapshot,
-                                                         GskIsolation            allowed);
+                                                         GskIsolation            features);
 GDK_AVAILABLE_IN_ALL
 void            gtk_snapshot_push_blur                  (GtkSnapshot            *snapshot,
                                                          double                  radius);