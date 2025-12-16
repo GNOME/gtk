GNOME/gtk

commit 67fb129ed7db377bd179757a77514c9ce0d3557f

gsk: gsk_render_node_set_transform() => GskTransformNode
Instead of having a setter for the transform, have a GskTransformNode.

Most of the oprations that GTK does do not require a transform, so it
doesn't make sense to have it as a primary attribute.

Also, changing the transform requires updating the uniforms of the GL
renderer, so we're happy if we can avoid that.

gsk/gskrendernodeimpl.c
@@ -424,3 +424,112 @@ gsk_container_node_get_child (GskRenderNode *node,
   return g_ptr_array_index (container->children, idx);
 }
 
+/*** GSK_TRANSFORM_NODE ***/
+
+typedef struct _GskTransformNode GskTransformNode;
+
+struct _GskTransformNode
+{
+  GskRenderNode render_node;
+
+  GskRenderNode *child;
+  graphene_matrix_t transform;
+};
+
+static void
+gsk_transform_node_finalize (GskRenderNode *node)
+{
+  GskTransformNode *self = (GskTransformNode *) node;
+
+  gsk_render_node_unref (self->child);
+}
+
+static void
+gsk_transform_node_make_immutable (GskRenderNode *node)
+{
+  GskTransformNode *self = (GskTransformNode *) node;
+
+  gsk_render_node_make_immutable (self->child);
+}
+
+static void
+gsk_transform_node_get_bounds (GskRenderNode   *node,
+                           graphene_rect_t *bounds)
+{
+  GskTransformNode *self = (GskTransformNode *) node;
+  graphene_rect_t child_bounds;
+
+  gsk_render_node_get_bounds (self->child, &child_bounds);
+
+  graphene_matrix_transform_bounds (&self->transform,
+                                    &child_bounds,
+                                    bounds);
+}
+
+static const GskRenderNodeClass GSK_TRANSFORM_NODE_CLASS = {
+  GSK_TRANSFORM_NODE,
+  sizeof (GskTransformNode),
+  "GskTransformNode",
+  gsk_transform_node_finalize,
+  gsk_transform_node_make_immutable,
+  gsk_transform_node_get_bounds
+};
+
+/**
+ * gsk_transform_node_new:
+ * @child: The node to transform
+ * @transform: The transform to apply
+ *
+ * Creates a #GskRenderNode that will transform the given @child
+ * with the given @transform.
+ *
+ * Returns: A new #GskRenderNode
+ *
+ * Since: 3.90
+ */
+GskRenderNode *
+gsk_transform_node_new (GskRenderNode           *child,
+                        const graphene_matrix_t *transform)
+{
+  GskTransformNode *self;
+
+  g_return_val_if_fail (GSK_IS_RENDER_NODE (child), NULL);
+  g_return_val_if_fail (transform != NULL, NULL);
+
+  self = (GskTransformNode *) gsk_render_node_new (&GSK_TRANSFORM_NODE_CLASS);
+
+  self->child = gsk_render_node_ref (child);
+  graphene_matrix_init_from_matrix (&self->transform, transform);
+
+  return &self->render_node;
+}
+
+/**
+ * gsk_transform_node_get_child:
+ * @node: a transform @GskRenderNode
+ *
+ * Gets the child node that is getting transformed by the given @node.
+ *
+ * Returns: (transfer none): The child that is getting transformed
+ **/
+GskRenderNode *
+gsk_transform_node_get_child (GskRenderNode *node)
+{
+  GskTransformNode *self = (GskTransformNode *) node;
+
+  g_return_val_if_fail (GSK_IS_RENDER_NODE_TYPE (node, GSK_TRANSFORM_NODE), NULL);
+
+  return self->child;
+}
+
+void
+gsk_transform_node_get_transform (GskRenderNode     *node,
+                                  graphene_matrix_t *transform)
+{
+  GskTransformNode *self = (GskTransformNode *) node;
+
+  g_return_if_fail (GSK_IS_RENDER_NODE_TYPE (node, GSK_TRANSFORM_NODE));
+
+  graphene_matrix_init_from_matrix (transform, &self->transform);
+}
+

gsk/gskcairorenderer.c
@@ -52,23 +52,9 @@ gsk_cairo_renderer_render_node (GskCairoRenderer *self,
                                 cairo_t          *cr)
 {
   gboolean pop_group = FALSE;
-  graphene_matrix_t mat;
-  cairo_matrix_t ctm;
   graphene_rect_t frame;
 
   cairo_save (cr);
-
-  gsk_render_node_get_transform (node, &mat);
-  if (graphene_matrix_to_2d (&mat, &ctm.xx, &ctm.yx, &ctm.xy, &ctm.yy, &ctm.x0, &ctm.y0))
-    {
-      GSK_NOTE (CAIRO, g_print ("CTM = { .xx = %g, .yx = %g, .xy = %g, .yy = %g, .x0 = %g, .y0 = %g }\n",
-                                ctm.xx, ctm.yx,
-                                ctm.xy, ctm.yy,
-                                ctm.x0, ctm.y0));
-      cairo_transform (cr, &ctm);
-    }
-  else
-    g_critical ("Invalid non-affine transformation for node %p", node);
 
   gsk_render_node_get_bounds (node, &frame);
   GSK_NOTE (CAIRO, g_print ("CLIP = { .x = %g, .y = %g, .width = %g, .height = %g }\n",
@@ -129,6 +115,27 @@ gsk_cairo_renderer_render_node (GskCairoRenderer *self,
       {
         cairo_set_source_surface (cr, gsk_cairo_node_get_surface (node), frame.origin.x, frame.origin.y); 
         cairo_paint (cr);
+      }
+      break;
+
+    case GSK_TRANSFORM_NODE:
+      {
+        graphene_matrix_t mat;
+        cairo_matrix_t ctm;
+
+        gsk_transform_node_get_transform (node, &mat);
+        if (graphene_matrix_to_2d (&mat, &ctm.xx, &ctm.yx, &ctm.xy, &ctm.yy, &ctm.x0, &ctm.y0))
+          {
+            GSK_NOTE (CAIRO, g_print ("CTM = { .xx = %g, .yx = %g, .xy = %g, .yy = %g, .x0 = %g, .y0 = %g }\n",
+                                      ctm.xx, ctm.yx,
+                                      ctm.xy, ctm.yy,
+                                      ctm.x0, ctm.y0));
+            cairo_transform (cr, &ctm);
+          }
+        else
+          g_critical ("Invalid non-affine transformation for node %p", node);
+
+        gsk_cairo_renderer_render_node (self, gsk_transform_node_get_child (node), cr);
       }
       break;
     }

gsk/gskenums.h
@@ -28,6 +28,8 @@
  * @GSK_CONTAINER_NODE: A node containing a stack of children
  * @GSK_CAIRO_NODE: A node drawing a #cairo_surface_t
  * @GSK_TEXTURE_NODE: A node drawing a #GskTexture
+ * @GSK_TRANSFORM_NODE: A node that renders its child after applying a
+ *     matrix transform
  *
  * The type of a node determines what the node is rendering.
  *
@@ -37,7 +39,8 @@ typedef enum {
   GSK_NOT_A_RENDER_NODE = 0,
   GSK_CONTAINER_NODE,
   GSK_CAIRO_NODE,
-  GSK_TEXTURE_NODE
+  GSK_TEXTURE_NODE,
+  GSK_TRANSFORM_NODE
 } GskRenderNodeType;
 
 /**

gsk/gskglrenderer.c
@@ -603,13 +603,12 @@ render_node_needs_render_target (GskRenderNode *node)
 static void
 gsk_gl_renderer_add_render_item (GskGLRenderer           *self,
                                  const graphene_matrix_t *projection,
-                                 const graphene_matrix_t *parent_modelview,
+                                 const graphene_matrix_t *modelview,
                                  GArray                  *render_items,
                                  GskRenderNode           *node,
                                  RenderItem              *parent)
 {
   graphene_rect_t viewport;
-  graphene_matrix_t mv, transform;
   graphene_rect_t bounds;
   RenderItem item;
   RenderItem *ritem = NULL;
@@ -645,10 +644,8 @@ gsk_gl_renderer_add_render_item (GskGLRenderer           *self,
   item.max.z = 0.f;
 
   /* The location of the item, in normalized world coordinates */
-  gsk_render_node_get_transform (node, &transform);
-  graphene_matrix_multiply (&transform, parent_modelview, &mv);
-  graphene_matrix_multiply (&mv, &self->mvp, &item.mvp);
-  item.z = project_item (projection, &mv);
+  graphene_matrix_multiply (modelview, &self->mvp, &item.mvp);
+  item.z = project_item (projection, modelview);
 
   item.opacity = gsk_render_node_get_opacity (node);
 
@@ -743,8 +740,22 @@ gsk_gl_renderer_add_render_item (GskGLRenderer           *self,
         for (i = 0; i < gsk_container_node_get_n_children (node); i++)
           {
             GskRenderNode *child = gsk_container_node_get_child (node, i);
-            gsk_gl_renderer_add_render_item (self, projection, &mv, render_items, child, ritem);
+            gsk_gl_renderer_add_render_item (self, projection, modelview, render_items, child, ritem);
           }
+      }
+      return;
+
+    case GSK_TRANSFORM_NODE:
+      {
+        graphene_matrix_t transform, transformed_mv;
+
+        gsk_transform_node_get_transform (node, &transform);
+        graphene_matrix_multiply (&transform, modelview, &transformed_mv);
+        gsk_gl_renderer_add_render_item (self,
+                                         projection, &transformed_mv,
+                                         render_items,
+                                         gsk_transform_node_get_child (node),
+                                         ritem);
       }
       return;
 

gsk/gskrendernode.c
@@ -97,8 +97,6 @@ gsk_render_node_new (const GskRenderNodeClass *node_class)
 
   self->ref_count = 1;
 
-  graphene_matrix_init_identity (&self->transform);
-
   self->opacity = 1.0;
 
   self->min_filter = GSK_SCALING_FILTER_NEAREST;
@@ -185,47 +183,6 @@ gsk_render_node_get_bounds (GskRenderNode   *node,
   g_return_if_fail (bounds != NULL);
 
   node->node_class->get_bounds (node, bounds);
-}
-
-/**
- * gsk_render_node_set_transform:
- * @node: a #GskRenderNode
- * @transform: (nullable): a transformation matrix
- *
- * Sets the transformation matrix used when rendering the @node.
- *
- * Since: 3.90
- */
-void
-gsk_render_node_set_transform (GskRenderNode           *node,
-                               const graphene_matrix_t *transform)
-{
-  g_return_if_fail (GSK_IS_RENDER_NODE (node));
-  g_return_if_fail (node->is_mutable);
-
-  if (transform == NULL)
-    graphene_matrix_init_identity (&node->transform);
-  else
-    graphene_matrix_init_from_matrix (&node->transform, transform);
-}
-
-/**
- * gsk_render_node_get_transform:
- * @node: a #GskRenderNode
- * @mv: (out caller-allocates): return location for the transform matrix
- *
- * Retrieves the transform matrix set using gsk_render_node_set_transform().
- *
- * Since: 3.90
- */
-void
-gsk_render_node_get_transform (GskRenderNode     *node,
-                               graphene_matrix_t *mv)
-{
-  g_return_if_fail (GSK_IS_RENDER_NODE (node));
-  g_return_if_fail (mv != NULL);
-
-  graphene_matrix_init_from_matrix (mv, &node->transform);
 }
 
 /**

gsk/gskrendernode.h
@@ -66,8 +66,11 @@ GskRenderNode *         gsk_container_node_get_child            (GskRenderNode  
                                                                  guint                    idx);
 
 GDK_AVAILABLE_IN_3_90
-void                    gsk_render_node_set_transform           (GskRenderNode            *node,
+GskRenderNode *         gsk_transform_node_new                  (GskRenderNode            *child,
                                                                  const graphene_matrix_t  *transform);
+GDK_AVAILABLE_IN_3_90
+GskRenderNode *         gsk_transform_node_get_child            (GskRenderNode            *node);
+
 GDK_AVAILABLE_IN_3_90
 void                    gsk_render_node_set_opacity             (GskRenderNode *node,
                                                                  double         opacity);

gsk/gskrendernodeprivate.h
@@ -29,9 +29,6 @@ struct _GskRenderNode
   GskScalingFilter min_filter;
   GskScalingFilter mag_filter;
 
-  /* Transformations applied to the node */
-  graphene_matrix_t transform;
-
   /* Bit fields; leave at the end */
   gboolean is_mutable : 1;
 };
@@ -53,13 +50,13 @@ void gsk_render_node_make_immutable (GskRenderNode *node);
 
 void gsk_render_node_get_bounds (GskRenderNode   *node,
                                  graphene_rect_t *frame);
-void gsk_render_node_get_transform (GskRenderNode     *node,
-                                    graphene_matrix_t *mv);
 double gsk_render_node_get_opacity (GskRenderNode *node);
 
 cairo_surface_t *gsk_cairo_node_get_surface (GskRenderNode *node);
 
 GskTexture *gsk_texture_node_get_texture (GskRenderNode *node);
+
+void gsk_transform_node_get_transform (GskRenderNode *node, graphene_matrix_t *transform);
 
 GskBlendMode gsk_render_node_get_blend_mode (GskRenderNode *node);
 

gsk/gskvulkanrender.c
@@ -233,7 +233,7 @@ gsk_vulkan_render_add_node (GskVulkanRender *self,
 
   self->render_passes = g_slist_prepend (self->render_passes, pass);
 
-  gsk_vulkan_render_pass_add_node (pass, self, node);
+  gsk_vulkan_render_pass_add (pass, self, &self->mvp, node);
 }
 
 void
@@ -466,7 +466,7 @@ gsk_vulkan_render_draw (GskVulkanRender   *self,
 
   for (l = self->render_passes; l; l = l->next)
     {
-      gsk_vulkan_render_pass_draw (l->data, self, &self->mvp, self->pipeline, self->command_buffer);
+      gsk_vulkan_render_pass_draw (l->data, self, self->pipeline, self->command_buffer);
     }
 
   vkCmdEndRenderPass (self->command_buffer);

commit 82a533a38337c515b186839e2068f7528fc6085c

transformnode: Allow NULL transform in constructor
NULL is a valid transform.

This is a leftover from 2016 when transform nodes still took
graphene_matrix_t and NULL wasn't a valid transform.

Fixes crashes in SVG code that creates transform nodes unconditionally
during animations and sometimes ends up with the identity transform.

Fixes: 67fb129ed7db377bd179757a77514c9ce0d3557f
Part-of: <https://gitlab.gnome.org/GNOME/gtk/-/merge_requests/9279>

gsk/gskrendernodeimpl.c
@@ -3490,7 +3490,7 @@ gsk_transform_node_class_init (gpointer g_class,
 /**
  * gsk_transform_node_new:
  * @child: The node to transform
- * @transform: (transfer none): The transform to apply
+ * @transform: (transfer none) (nullable): The transform to apply
  *
  * Creates a `GskRenderNode` that will transform the given @child
  * with the given @transform.
@@ -3506,7 +3506,6 @@ gsk_transform_node_new (GskRenderNode *child,
   GskTransformCategory category;
 
   g_return_val_if_fail (GSK_IS_RENDER_NODE (child), NULL);
-  g_return_val_if_fail (transform != NULL, NULL);
 
   category = gsk_transform_get_category (transform);
 