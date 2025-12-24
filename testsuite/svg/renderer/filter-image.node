GNOME/gtk

commit d11bb00b9600c404fb6ada428887dfe552a8e403

svg: Fixes for bounding boxes
Take the transform into account for all shapes,
and implement bounding boxes for text and use.

Part-of: <https://gitlab.gnome.org/GNOME/gtk/-/merge_requests/9296>

gtk/gtksvg.c
@@ -9330,11 +9330,23 @@ shape_get_path (Shape                 *shape,
         }
       break;
 
+    case SHAPE_USE:
+      {
+        Shape *use_shape = ((SvgHref *) shape->current[SHAPE_ATTR_HREF])->shape;
+        if (use_shape)
+          {
+            return shape_get_path (use_shape, viewport, current);
+          }
+        else
+          {
+            builder = gsk_path_builder_new ();
+            return gsk_path_builder_free_to_path (builder);
+          }
+      }
     case SHAPE_GROUP:
     case SHAPE_CLIP_PATH:
     case SHAPE_MASK:
     case SHAPE_DEFS:
-    case SHAPE_USE:
     case SHAPE_LINEAR_GRADIENT:
     case SHAPE_RADIAL_GRADIENT:
     case SHAPE_PATTERN:
@@ -9421,11 +9433,14 @@ shape_get_current_path (Shape                 *shape,
             }
           break;
 
+        case SHAPE_USE:
+          g_clear_pointer (&shape->path, gsk_path_unref);
+          break;
+
         case SHAPE_GROUP:
         case SHAPE_CLIP_PATH:
         case SHAPE_MASK:
         case SHAPE_DEFS:
-        case SHAPE_USE:
         case SHAPE_LINEAR_GRADIENT:
         case SHAPE_RADIAL_GRADIENT:
         case SHAPE_PATTERN:
@@ -9445,6 +9460,7 @@ shape_get_current_path (Shape                 *shape,
   if (!shape->path)
     {
       shape->path = shape_get_path (shape, viewport, TRUE);
+      shape->valid_bounds = FALSE;
 
       switch (shape->type)
         {
@@ -9484,13 +9500,13 @@ shape_get_current_path (Shape                 *shape,
           break;
 
         case SHAPE_PATH:
+        case SHAPE_USE:
           break;
 
         case SHAPE_GROUP:
         case SHAPE_CLIP_PATH:
         case SHAPE_MASK:
         case SHAPE_DEFS:
-        case SHAPE_USE:
         case SHAPE_LINEAR_GRADIENT:
         case SHAPE_RADIAL_GRADIENT:
         case SHAPE_PATTERN:
@@ -9527,9 +9543,11 @@ shape_get_current_bounds (Shape                 *shape,
                           const graphene_rect_t *viewport,
                           graphene_rect_t       *bounds)
 {
+  GskTransform *transform;
+  graphene_rect_t b;
   gboolean ret = FALSE;
 
-  graphene_rect_init_from_rect (bounds, graphene_rect_zero ());
+  graphene_rect_init_from_rect (&b, graphene_rect_zero ());
 
   switch (shape->type)
     {
@@ -9540,17 +9558,23 @@ shape_get_current_bounds (Shape                 *shape,
     case SHAPE_CIRCLE:
     case SHAPE_ELLIPSE:
     case SHAPE_PATH:
-      {
-        GskPath *path = shape_get_current_path (shape, viewport);
-        ret = gsk_path_get_tight_bounds (path, bounds);
-        gsk_path_unref (path);
-      }
+      if (!shape->valid_bounds)
+        {
+          GskPath *path = shape_get_current_path (shape, viewport);
+          if (!gsk_path_get_tight_bounds (shape->path, &shape->bounds))
+            graphene_rect_init (&shape->bounds, 0, 0, 0, 0);
+          shape->valid_bounds = TRUE;
+          gsk_path_unref (path);
+        }
+      graphene_rect_init_from_rect (&b, &shape->bounds);
+      ret = TRUE;
       break;
     case SHAPE_USE:
       {
         Shape *use_shape = ((SvgHref *) shape->current[SHAPE_ATTR_HREF])->shape;
         if (use_shape)
-          ret = shape_get_current_bounds (use_shape, viewport, bounds);
+          ret = shape_get_current_bounds (use_shape, viewport, &b);
+        ret = TRUE;
       }
       break;
     case SHAPE_GROUP:
@@ -9559,36 +9583,26 @@ shape_get_current_bounds (Shape                 *shape,
     case SHAPE_PATTERN:
     case SHAPE_MARKER:
     case SHAPE_SVG:
-      {
-        for (unsigned int i = 0; i < shape->shapes->len; i++)
-          {
-            Shape *sh = g_ptr_array_index (shape->shapes, i);
-            GskTransform *transform;
-            graphene_rect_t b;
+      for (unsigned int i = 0; i < shape->shapes->len; i++)
+        {
+          Shape *sh = g_ptr_array_index (shape->shapes, i);
+          graphene_rect_t b2;
 
-            if (shape_get_current_bounds (sh, viewport, &b))
-              {
-                transform = svg_transform_get_gsk ((SvgTransform *) shape->current[SHAPE_ATTR_TRANSFORM]);
-                gsk_transform_transform_bounds (transform, &b, &b);
-                if (i == 0)
-                  graphene_rect_init_from_rect (bounds, &b);
-                else
-                  graphene_rect_union (bounds, &b, bounds);
-                gsk_transform_unref (transform);
-              }
-          }
-        ret = TRUE;
-      }
-      break;
-    case SHAPE_DEFS:
-    case SHAPE_LINEAR_GRADIENT:
-    case SHAPE_RADIAL_GRADIENT:
-    case SHAPE_FILTER:
-      g_error ("Attempt to get the bounds of a %s", shape_types[shape->type].name);
+          if (shape_get_current_bounds (sh, viewport, &b2))
+            {
+              if (i == 0)
+                graphene_rect_init_from_rect (&b, &b2);
+              else
+                graphene_rect_union (&b, &b2, &b);
+            }
+        }
+      ret = TRUE;
       break;
     case SHAPE_TEXT:
     case SHAPE_TSPAN:
-      g_critical ("TODO");
+      if (!shape->valid_bounds)
+        g_critical ("TODO");
+      graphene_rect_init_from_rect (bounds, &shape->bounds);
       break;
     case SHAPE_IMAGE:
       {
@@ -9603,9 +9617,19 @@ shape_get_current_bounds (Shape                 *shape,
         ret = TRUE;
       }
       break;
+    case SHAPE_DEFS:
+    case SHAPE_LINEAR_GRADIENT:
+    case SHAPE_RADIAL_GRADIENT:
+    case SHAPE_FILTER:
+      g_error ("Attempt to get the bounds of a %s", shape_types[shape->type].name);
+      break;
     default:
       g_assert_not_reached ();
     }
+
+  transform = svg_transform_get_gsk ((SvgTransform *) shape->current[SHAPE_ATTR_TRANSFORM]);
+  gsk_transform_transform_bounds (transform, &b, bounds);
+  gsk_transform_unref (transform);
 
   return ret;
 }
@@ -18595,6 +18619,7 @@ paint_shape (Shape        *shape,
       WritingMode wmode;
       graphene_rect_t bounds;
       GskStroke *stroke;
+      float dx, dy;
 
       if (svg_enum_get (shape->current[SHAPE_ATTR_VISIBILITY]) == VISIBILITY_HIDDEN)
         return;
@@ -18604,26 +18629,34 @@ paint_shape (Shape        *shape,
       if (!generate_layouts (shape, NULL, NULL, NULL, &bounds))
         return;
 
-      gtk_snapshot_save (context->snapshot);
+      dx = dy = 0;
       switch (anchor)
         {
         case TEXT_ANCHOR_START:
           break;
         case TEXT_ANCHOR_MIDDLE:
           if (is_vertical_writing_mode[wmode])
-            gtk_snapshot_translate (context->snapshot, &GRAPHENE_POINT_INIT (0., -bounds.size.height / 2.));
+            dy = - bounds.size.height / 2;
           else
-            gtk_snapshot_translate (context->snapshot, &GRAPHENE_POINT_INIT (-bounds.size.width / 2., 0.));
+            dx = - bounds.size.width / 2;
           break;
         case TEXT_ANCHOR_END:
           if (is_vertical_writing_mode[wmode])
-            gtk_snapshot_translate (context->snapshot, &GRAPHENE_POINT_INIT (0., -bounds.size.height));
+            dy = - bounds.size.height;
           else
-            gtk_snapshot_translate (context->snapshot, &GRAPHENE_POINT_INIT (-bounds.size.width, 0.));
+            dx = - bounds.size.width;
           break;
         default:
           g_assert_not_reached ();
         }
+
+      graphene_rect_init (&shape->bounds,
+                          bounds.origin.x + dx, bounds.origin.y + dy,
+                          bounds.size.width, bounds.size.height);
+      shape->valid_bounds = TRUE;
+
+      gtk_snapshot_save (context->snapshot);
+      gtk_snapshot_translate (context->snapshot, &GRAPHENE_POINT_INIT (dx, dy));
 
       stroke = shape_create_stroke (shape, context);
 

gtk/gtksvgprivate.h
@@ -300,6 +300,7 @@ struct _Shape
   Shape *next;
 
   gboolean computed_for_use;
+  gboolean valid_bounds;
 
   SvgValue *base[N_SHAPE_ATTRS];
   SvgValue *current[N_SHAPE_ATTRS];
@@ -312,6 +313,7 @@ struct _Shape
 
   GskPath *path;
   GskPathMeasure *measure;
+  graphene_rect_t bounds;
   union {
     struct {
       double cx, cy, r;

commit 88a0bebd02672ff80b9188415ab9d313f309bc79

svg: Fix bounding boxes for images
Box confusion. This broke in d11bb00b9600c40.

Test added.

Part-of: <https://gitlab.gnome.org/GNOME/gtk/-/merge_requests/9303>

gtk/gtksvg.c
@@ -9926,7 +9926,7 @@ shape_get_current_bounds (Shape                 *shape,
     case SHAPE_TSPAN:
       if (!shape->valid_bounds)
         g_critical ("TODO");
-      graphene_rect_init_from_rect (bounds, &shape->bounds);
+      graphene_rect_init_from_rect (&b, &shape->bounds);
       break;
     case SHAPE_IMAGE:
       {
@@ -9937,7 +9937,7 @@ shape_get_current_bounds (Shape                 *shape,
         width = svg_number_get (shape->current[SHAPE_ATTR_WIDTH], viewport->size.width);
         height = svg_number_get (shape->current[SHAPE_ATTR_HEIGHT], viewport->size.height);
 
-        graphene_rect_init (bounds, x, y, width, height);
+        graphene_rect_init (&b, x, y, width, height);
         ret = TRUE;
       }
       break;

testsuite/svg/renderer/filter-image.node
@@ -1,0 +1,76 @@
+isolation {
+  child: isolation {
+    child: clip {
+      clip: -10 -10 120 120;
+      child: clip {
+        clip: 0 0 100 100;
+        child: transform {
+          transform: scale(3.125);
+          child: texture {
+            bounds: 0 0 32 32;
+            texture: memory {
+              format: RGBA8;
+              width: 32;
+              height: 32;
+              offset: 0;
+              stride: 128;
+              data: url("data:application/gzip;base64,\
+H4sIAAAAAAACA61WZ1SUVxqmM8BQlCZVEBApKoIFKYpYjooail3UKIqSMKioFAHFLkVdGwajEoOx\
+o4BiMESNmrUDARNFkN57G8owMzz7fp/jWdbVTfbs/njOPfd7732et94ZAFL4U0hJA9EywH25ItxW\
+BF4qAwWqLXipDjxVa8B9LlDKoe/ydJ7OQRp/ifeva99M9NdK2T3X78Zej8z0mHlF6fE+LTcPLuxL\
+OzS/7Wa897vUA1/8nBLj/VVmor8e3ZP9f/mQHOZmfnnb5LRL0TN6nl/kdTfkJaOn5jGEbYUQd1dD\
+2P4OPXXP0Pz6Cn67vq0nZe+c3gsRU7K+3+Zm+7/4cCXaRuGHUJeEC+GTuouu80R9dc8hankFYdMz\
+CFtzIOp8A3FXMaEEoo7XELXmQdT+luz5KM4IEyeHuXcnBTufT/R3UP5vtc+Gu2onhzjlZO1z6+7I\
+Poa+qgfofXuW1lsQteVC3FOK/r569AubCU0QC6oh5hdA2PgIgvJ09NU/B/9VErL2T+9J3Dix4Chv\
+guFfzvdGV73krU51uacWCrtfX0RX7lHCfgjKrlBsjyDi/0F6ZRCLatAvqiPUki+V5FMh+ZYNQU0m\
+egpOE86jpygNuaeXio4Hjm8+HDBu2J9pJ0W7cZK3OL3KSfAU8p8fRevNxej8ZT268+NR/uI4prnZ\
+o6o4AyLB76RP9Re/IxRBLCxAVVkWpk0Zi4rc0+gtOAn+kwh0ZAWgK/skck76iI+scyiJWe2s+p/0\
+z212PHs7Ynx356P9aPrOCa0pM9B5z484QrHM0wFxcbFYsdQdQgHlQfQCon7qA3E2RMInWL50Omv3\
+9RmP7rwd4P/KQ9vN+Wi5MBX8v8cjLcS+99Aau9TPaZ/a5GxzdsOErqarq9F8yRMNCaZovuyE9h89\
+wH+4CKNHaMPX1xdj7YeR/k+kTz70P6X4H0PUdx92o01Zu8NIPfAfr0DHHU+0pUxB45mRaL4wF83X\
+1+Gg3yj+ni/tHT+pHzT+XnaMu7g1dT1qYwej4YQRmpOt0XrdgfLoDmWOHLhcLtTVlCDqTqOY75IP\
+Dwn30dd7G8pKCqxdjasA/i8eaEtzRPN5GzR+Y4LaOG20XPdDdpx7/76Vo3I/1mb6M5E3oav52jrU\
+xhgQBqHh2BA0JZmi9YoNWjIcIS8nAykpKShx5NHXngRRbwrVneZBkI6etvOQl5d9b1eURfvPbhT7\
+KDSdM6c49Nl4ag/ooyUlADGrx3RGLRlt8y/6XzsG3Yp06Wo8+wWqdyhL9HXRdMYYLZct0ZHhgBEm\
+Kiz/SKqDsDGG5j8B/d1naB5O0btwCFbDtVi7rbkaOjId0XrVGo1JJmg4rkv6g1AdrUK18MCNECdB\
+5OJR0QP1/+Zv//Rl3HTUxo9E9XbSP6CB+qM6aDpthJaLFpTLUfCcrAk9PT34zBwKYTkPovpwiJq2\
+Q9QQSe9CMLxnmrB2LzdttN+0Y+8x/jNxMPEwcdXEWiGHdCIW2hQM1D+4dkzdH0dnoypKh6CE2v3q\
+qD+ijcZTBmg5Pwxt163w/PxkjBtjgezUOeh9sxB9xcshLF0FQfFKCAoWI/uWF2t/kTyZ/LVFyw9m\
+aPzWkHh0iE+D4lJCVeQgFB7zQMgCa/5A/QN+9oKCY7NQGaKMqggOavaqoe6QFvWOPprPDaUeGI7u\
+33noaf4efdX70PvbXMJMCF7NRm/+TPS8nA5BSQjZk9FbEo22dAe6Z8Lerz+sxfJVRXJQsUUB7xLm\
+YpO3VV/0AhsFRjua3ud9q+x7/jgyC+UbpFAZroCaXVzUxdEMHKcePGPExtLx40R0PVsA/lNmHh3R\
+9dgZ3U9d0PXECV205//qzto7s6bSzJuz9xpODEFd/GDU7OYSryLKN0rj9ZGZ2OJt1RE4z1KfjV9K\
+Snr3Cru+l7FTUBgkRzmQQ3GEMvIj1VF7mGqQKMnBJeqDVBvqxVHovGOHzrsO4N8bS++TA2na0ffR\
+rJ05x8aeaIDSOC3khKuhPEqZeOVRFCSD3Hh3BM617PWf+8/fpe2+tk13dzgib4MKKoJl2Bq8CuPi\
+u1VcZG4ZjLJjBmj+3oS4zakXRqA93Rrtt2xJcyQ6aGX2zPfWy6SdbIqSY/pI4Q3GhTVqKI1WZfkq\
+NssgfwMHD3Y5Yv1si56B9Y9cYpN/ZYsdngZroySIahAqz/ZrYbQaEnxVwZuhgv1LNHBhkw6eHzRG\
+E5OLa5Y045bsmn/CFJdD9RC7QhOb56gheCYXaUHqqNyrzvIwfGVU22ebNHF1sx38Z5mVD9QPW2i7\
+4/Aam977W4ch7yup9znYpoiqnVyaXU3cDdVHlJcG1rpzsdRFBfMnqmCBMxdLXFWxyIULH9ovc+Vi\
+3TQuTvrp4k3sMNTEaaKa+qgqQpHlyyfeByGm5KOlcPUMs6MD9UN9rG3CF1l33g61xkN/WRTyyIct\
+sjSLHLZ3auN1UP/dJLz+xg2p26yQ4G+AvYsHYddCDcQu10ZSkBnu7RuHih/moT7JCTV0vnqPGnu/\
+gvrpHfE9WieLzDBrBMw2a1s53cz54zc42NvydcJac/y0UR/P10uz+aoMpX6k96iS4TpAb2gCvedX\
+vdF6Lwztv+5F6+MYtD+Jp304Gq76oPrECFTsH4QqOl9J7whzn5mpF8T300Y9JPiZYvlU00p6J//t\
+f1mg5wi3TV7DO68GmSIrgIvsACn2bgX1LfMmle9URcluNbzbo46i/ToojDdD0SELFB00R8GewSjc\
+pYHi3eoo28loK6OCal6+UQq5xJMVoIxrG0yxappx50I3E8/P/QYHzh1+N2q+oeAGzxBZa2RZH0qJ\
+o2KrHMqoH4ojlfF2OxevIlXxW4QqcrZxaeUij/Amiot3kSrsOSbnZXQvh9FeI4MbQYYI99IVLp5s\
+nP05bSYnvq5D9fxnWzTELNPvv7ZeC5l+MmwtCgKlUEY9VLqVZjhMEW9ClfB7KAd5W5WRF8LBa9oX\
+hSqihOxlm2TwlmaIuXeHtFPWa+LAEl0scDVu8xhnPIzR+Tj/km+yBAUvF2OX1TOGdR1YSjMcaIAM\
+fw4erpVmc8H4UbpBGsWkUbSJ+jRYDkXBsrSXRQl9Z/qWiZk5f3utIq4GaIPh8XEx7nYfM2QW8SsR\
+5D6jz3xXIei42OrOWTHVtC3Ec4joYqAJ0ni6uE25uE+8z9ZJsxrMnOZ9/X5l9sz3B2T/kc6l+qvh\
+Em8oIhcYi72djfj25lrLiFePoE5QJMgM9EGiL09QJQwhjNDW4Hzp5WRYs8LdqC92uSFSttgglaeH\
+9LXKuLVKGpmrCX7v1wzap66Sw7W1qriywQxH/a3g624s9Bin36SprhRGfPYEI8IgAkeSa+lPxK9M\
+0CQMJbgSNlsYqKV7TjTsXOpm1Be1xKr/2yB73Ihywq1oF9za7oSMnZOQut0Z57Y6YY/f2P5VsywF\
+8yYYdFnoq92h+9GEL5h4CLqS+BSY+D/RfzKSHDA+DJb44ERgchduqKWSPNFKK3eeo36n50QjMeVG\
+tMjVWECr0Iv2pMl3ttZ+ZaStclnmva4/YTrBcoC24sexf5SDD3ngSM5rEZietSNMIswj+BLWqCjK\
+BetocHaqKMmFSLRWEnwI0wjjJbofaq4kiU3mU9qf8EPmwzxI7nIJGpI4jCQ+mUs0LAhmknzpSWqs\
+KsmjgiQe2U/NHaP3D4l8Yu8AEAAA\
+");
+            }
+          }
+        }
+      }
+    }
+    isolations: background;
+  }
+  isolations: background;
+}

testsuite/svg/renderer/filter-image.svg
@@ -1,0 +1,7 @@
+<svg width='100' height='100' viewBox='0 0 100 100'>
+  <filter id='filter'>
+  <feImage href='data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAACAAAAAgCAYAAABzenr0AAAABHNCSVQICAgIfAhkiAAAAAlwSFlzAAAN1wAADdcBQiibeAAAABl0RVh0U29mdHdhcmUAd3d3Lmlua3NjYXBlLm9yZ5vuPBoAAAAXdEVYdEF1dGhvcgBMYXBvIENhbGFtYW5kcmVp35EaKgAAACl0RVh0RGVzY3JpcHRpb24AQmFzZWQgb2YgSmFrdWIgU3RlaW5lciBkZXNpZ26ghAVzAAAJhElEQVRYw5WXa2xl11XHf3ufc1++9tge25NxxpmXPTPJjDOdEGYSgmkaqSI8pKIQUqqKSqCKBCECFPjSQS1EInyoqISoVJTwoUiNChWltDQIUqVtmhaSJinpDMlMxvHEM7bj8evavtf33vPYe6/Fh3untQNhypGWzkvn/H/rv/bZex2jqtx4Mwb+1MC9doYkmmBPBL3RBk07iNdVEhnhoIdagDsDoN248ZtvDNARf/rJxd35yrVfslYeiuJonKgwZEzUK0baVmQt+HAlmOjL1YHhf7r/4SdWAPlxIG4I8NTH75soWv20Fks/e/jYMT14/Eylb2ScuGcYW6wiLsEnNdrrV5l/4wfp5YvnTdZufcerfuwjj3/r9RtBvCvAPzw2WXTJ4F8aa3/99On3FA/c85HIFiuotCEqYOIKxhYBg4YUggNbRl3G3Pe+IP/xnZcyn2dfzpvpww8/8Ur7/wXwubPvHSmE8MzegeKtd93/YKV800mkdRlb3YPt3YcpDWCiHjAxoKhk4LaQZBVpb2DKe8lXXueFp7+QvrXcvJoFef+jf/Xiwo8F8NTH3jtKIbw6eWRs6NjUA7Fmq0CLeHAc2zuKKe/GFKoQFTHYjsPi0ZCg2RahvYo0FoAyRH288e0vhn//wUzdBT39+5996a13AtjtJ3/72H1l4vD1E4f2DB05NRVnl7+K1C9gogJLqyv8/K/8HkvLV1CaQBM116PFteWr/NwDj7C8vIDBETYu4q78G0fv+JnonhO3DFiRZz/10am+/xPAbqWfHSr58SO33xWnF/4e8nUQh7pVzj7+Oe7/xQ9x9hOfRtlAqaOmgdJA2eDjZz/F/b/wQc7+2ZNItgShgaSrZJe+xNGTP2UP9uvNBdv6/LsC/M0fTJ0IXn/1zKnJSv7295D2NcQ3kWwJbV3htTfmOHfuPBfeuAIkQArkGDLQlP96fYZz585zcXoeac0irXnIa4Rkhezqt5m6+3RJVd7/+G/cefd2gPiHR8F95j37estRoUg6+zxRtQr5JpJkmLifN69scHnhK0Q2YEICNgJRlIBIwvT028xc/gqWHPIaktYIyRa4Fvn6i5TGP8D7bh3ueea1tb8G7tjhwGd+9+4xVXPXwfEjNr30NVDtWO8T8Ck+b+O80Gw2yXOPuHUImxhtQNgipOs4H2g2mzgXCL4Fvo2GDA05aCCb/lcOHT5moiia+OSHT53YWQIxv7yvv4A0lpD2RveWggRUPTGO8bEKABMHBzD5CrhljFsGt0RBVpg4NAjA+C1VjE9R8ah40M7MLGmdsDnH0eFKUVUf2gEgLv/w3v5Kj19/qzuLK6radUIQ77j1QJnR0VGOHuyB9gK055H2VWjPIe0Fbjvcy+joKEfGSqhLUe8wIj9aFVRxtbc4MFItqoQP7QAIqgcrpQKhvvwjYVVUBEKA4Dj7m5OM7e3l7G/dTmgvI80FdGuesDWPNhf549+5o3P/o8c7mYugIqgoSOd90liit1zEqY7tAHBiBiOjaNbsDCxRNHQe1BBQH5g8dZLnn/0Et5++B7IE2VpFmstoaxlp1pg8foTnn/0kt//0fWBKqA8QZAeIpC1sZHFBi499cLIIED/2yE/2FAUJApK1sVGx82AQ1HfDBfz8K0StJYK2Ia9h4ghjDaoCTgjpK5jVWaSxjjTrqA9oEPQ6RFDUO3LniCBdy/wwsGj/5MnvJyFIlGQpmcYQhCQTVuqB4DrZq/eE+hru2gWkNo8mKZKkSDtFkgzJUqRew127gK/Noc6jXmi1PYs1j8s7CeViyFxO6qSYa+9mpwSq6sQ36q2ExJRQL5QQjA88cz7h+2+mbDU8kjs0zZAs70SSIUmGtjMkyZEsQ7MccZ56I+f51xJeuJQzWFIiETQEElOk0W7jg+gT/9xZIWMACbpYa6VD1aiHPt+i4AO7y5Z7DsQ8O+P4l/MZNw8VOLC3yMRYmcP7SsSxRU1nhM8t51ycS7l6LWd53aFeuPdoxJlDBpt7xAfEC4mtslJPEZWVHTOhD+Yf3950R0cG+0pb6SqDLqCRpadkeeBkiWO1Es9Nt/nWuTZPv9wiD2CsIbYGUcUHpRwbektw6kCV9x2r0m/qaJJ1SugCDQdp7y5mlxLvA1/dAaBBvrSyxR8lQ+VS3UdUXKBsPTYyaAbHRyImbztJLbVMzy2zuNZgo9EmiFIuFRgZ3MWBvbuZODBKKV8j1GbQ1KHOIy6QO6EeIhJbYXYtawXli/+jH/jDB2+9MDEUbpuothlIr7GnpETlGEpFKMaYOCbaNUo8cox46Ci22IfYGGsiJKnhatO4ldcJjWVsCGjmIHdI6lnJDBvlvcxslXnhCguf/8bsfu0K/3AxysX89ptr9msjlVKvMVWivMkwHqMGq0oISl6bR9YX0Oi7aKEPYy2oEpJ1rAiRUSJRjPdo7tHMs57DpqnQ0DIvXw3NLNhHdVsXtKMjevQDx74xWGpP/cQoxd7kGrsLgd0liAoxUogINsJbSy6GzjylWAMGKFooqBKLx/qA5J71DGrO0uy5mZfmnZ9dL53/u+eu3vm/9gPGGLO5mf3acrtSn14XrceDrOaW1dTQTjykDps5bO6InSf2jsgJxnkKPhDnDpvlkDiS1LOSGladpREPcmnVMbNWam21ech0tx0OdC9YIHpgav+ZwZ74maPDac+R3YZyXqNPM6qxUo2gFBnEGgSDGoNBsQpGBC9K00PTG5oUacW7uFwv8PJcnGy08ge/+erSc4DrLD8d67cDREAJqE5N3nTm8E09T+3tS3rvGCtHPSah0F6lYpUeCwWrRIaO9wpBwYkhFWiLIYt6SUuDvLakcmFRkyvLySP/ObP2TaDdbaUcoKqq2wHiTitLFRgYGSjfPXV8+M/7ymZ4cp8Wxm/qJ8rWsWkd6xPidwIQ4eMK0rOHpaTISzMtv7Hl6y9Ob/xFrZ58HVgFmt1+zgHyToAIKAIVoBfYD5w5sm/XvSf277qvp2RK46O98f6RihnuL1OILIhgohgXAvVEmV/P9fLilqutN/3F+a3vvrnYeAF4FbgEbHQdyACvqrLjKzDG2C5EoetEH7APOATsHxuuHr9lpDI50l+csCbqMUa1GNmQB7GosUEkqW3ls3Or7Qtvr7YuCiwCs8AcsNkVzwF/Pft3AlwfmVG3HIXumNi1LQa6+3K1FO+qVuL+VuZbrcRvdjNrAnWgsW2/XThcr/27/hl1Qa5HtC3iLlCxC3fdMemG7wql28T8tr9koau+Xe+/AYGL7Pzya+IwAAAAAElFTkSuQmCC
+' x='0' y='0' width='100' height='100'/>
+  </filter>
+  <rect x='0' y='0' width='100' height='100' filter='url(#filter)'/>
+</svg>

testsuite/svg/renderer/meson.build
@@ -29,6 +29,7 @@ test_data = [
   'filter-fill-paint.svg',
   'filter-flood.svg',
   'filter-gaussian-blur.svg',
+  'filter-image.svg',
   'filter-merge.svg',
   'filter-offset.svg',
   'filter-offset2.svg',