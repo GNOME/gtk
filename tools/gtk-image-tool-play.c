GNOME/gtk

commit 3264d811261b2327829404c5b6bf76ea3339907c

image-tool: Add some comfort
Make Ctrl-R restart the animation, and
Ctrl-P toggle playing. This reveals that
a) we should have api to restart the animation
without reloading things entirely
b) pausing doesn't stop time as it should

Part-of: <https://gitlab.gnome.org/GNOME/gtk/-/merge_requests/9297>

tools/gtk-image-tool-play.c
@@ -95,6 +95,42 @@ clicked (GtkGestureClick *click,
   update_tooltip (widget, state);
 }
 
+static gboolean
+toggle_playing (GtkWidget *widget,
+                GVariant  *args,
+                gpointer   user_data)
+{
+  GtkSvg *svg = user_data;
+  gboolean playing;
+
+  g_object_get (svg, "playing", &playing, NULL);
+  g_object_set (svg, "playing", !playing, NULL);
+  return TRUE;
+}
+
+static GtkSvg * load_animation_file (const char *filename);
+
+static gboolean
+restart (GtkWidget *widget,
+         GVariant  *args,
+         gpointer   user_data)
+{
+  const char *filename;
+  GtkSvg *svg;
+
+  filename = (const char *) g_object_get_data (G_OBJECT (widget), "filename");
+
+  svg = load_animation_file (filename);
+
+  gtk_svg_set_frame_clock (svg, gtk_widget_get_frame_clock (widget));
+
+  gtk_svg_play (svg);
+  gtk_picture_set_paintable (GTK_PICTURE (widget), GDK_PAINTABLE (svg));
+  g_object_unref (svg);
+
+  return TRUE;
+}
+
 static void
 error_cb (GtkSvg *svg, GError *error)
 {
@@ -134,7 +170,10 @@ load_animation_file (const char *filename)
   GtkSvg *svg;
 
   if (!g_file_get_contents (filename, &contents, &length, &error))
-    g_error ("%s", error->message);
+    {
+      g_printerr ("%s\n", error->message);
+      exit (1);
+    }
 
   bytes = g_bytes_new_take (contents, length);
 
@@ -160,6 +199,7 @@ show_files (char     **filenames,
 
   window = gtk_window_new ();
   g_signal_connect (window, "destroy", G_CALLBACK (quit_cb), &done);
+
   gtk_widget_realize (window);
 
   title = g_string_new ("");
@@ -195,6 +235,9 @@ show_files (char     **filenames,
       GtkSvg *svg;
       GtkWidget *picture;
       GtkEventController *click;
+      GtkEventController *shortcuts;
+      GtkShortcutTrigger *trigger;
+      GtkShortcutAction *action;
 
       svg = load_animation_file (filenames[i]);
 
@@ -216,10 +259,27 @@ show_files (char     **filenames,
           gtk_image_set_pixel_size (GTK_IMAGE (picture), size);
         }
 
+      g_object_set_data_full (G_OBJECT (picture), "filename", g_strdup (filenames[i]), g_free);
+
       click = GTK_EVENT_CONTROLLER (gtk_gesture_click_new ());
       gtk_gesture_single_set_button (GTK_GESTURE_SINGLE (click), 0);
       g_signal_connect (click, "pressed", G_CALLBACK (clicked), svg);
       gtk_widget_add_controller (picture, click);
+
+      shortcuts = gtk_shortcut_controller_new ();
+      gtk_shortcut_controller_set_scope (GTK_SHORTCUT_CONTROLLER (shortcuts), GTK_SHORTCUT_SCOPE_GLOBAL);
+
+      trigger = gtk_alternative_trigger_new (
+                  gtk_keyval_trigger_new (GDK_KEY_AudioPlay, 0),
+                  gtk_keyval_trigger_new (GDK_KEY_P, GDK_CONTROL_MASK));
+      action = gtk_callback_action_new (toggle_playing, svg, NULL);
+      gtk_shortcut_controller_add_shortcut (GTK_SHORTCUT_CONTROLLER (shortcuts), gtk_shortcut_new (trigger, action));
+      trigger = gtk_alternative_trigger_new (
+                  gtk_keyval_trigger_new (GDK_KEY_AudioRewind, 0),
+                  gtk_keyval_trigger_new (GDK_KEY_R, GDK_CONTROL_MASK));
+      action = gtk_callback_action_new (restart, svg, NULL);
+      gtk_shortcut_controller_add_shortcut (GTK_SHORTCUT_CONTROLLER (shortcuts), gtk_shortcut_new (trigger, action));
+      gtk_widget_add_controller (picture, shortcuts);
 
       if (i > 0)
         gtk_box_append (GTK_BOX (box), gtk_separator_new (GTK_ORIENTATION_VERTICAL));

commit 5ab517ae875db1c53fdd160e6d5af82ab44d115d

image tool: Fix a crash
This was introduced when restarting was introduced
in 3264d811261b2327829.

Part-of: <https://gitlab.gnome.org/GNOME/gtk/-/merge_requests/9309>

tools/gtk-image-tool-play.c
@@ -100,9 +100,10 @@ toggle_playing (GtkWidget *widget,
                 GVariant  *args,
                 gpointer   user_data)
 {
-  GtkSvg *svg = user_data;
+  GtkSvg *svg;
   gboolean playing;
 
+  svg = GTK_SVG (gtk_picture_get_paintable (GTK_PICTURE (widget)));
   g_object_get (svg, "playing", &playing, NULL);
   g_object_set (svg, "playing", !playing, NULL);
   return TRUE;
@@ -116,8 +117,8 @@ restart (GtkWidget *widget,
          GVariant  *args,
          gpointer   user_data)
 {
-  const char *filename;
   GtkSvg *svg;
+  const char *filename;
   GtkSvgFeatures features;
 
   svg = GTK_SVG (gtk_picture_get_paintable (GTK_PICTURE (widget)));
@@ -280,12 +281,12 @@ show_files (char           **filenames,
       trigger = gtk_alternative_trigger_new (
                   gtk_keyval_trigger_new (GDK_KEY_AudioPlay, 0),
                   gtk_keyval_trigger_new (GDK_KEY_P, GDK_CONTROL_MASK));
-      action = gtk_callback_action_new (toggle_playing, svg, NULL);
+      action = gtk_callback_action_new (toggle_playing, NULL, NULL);
       gtk_shortcut_controller_add_shortcut (GTK_SHORTCUT_CONTROLLER (shortcuts), gtk_shortcut_new (trigger, action));
       trigger = gtk_alternative_trigger_new (
                   gtk_keyval_trigger_new (GDK_KEY_AudioRewind, 0),
                   gtk_keyval_trigger_new (GDK_KEY_R, GDK_CONTROL_MASK));
-      action = gtk_callback_action_new (restart, svg, NULL);
+      action = gtk_callback_action_new (restart, NULL, NULL);
       gtk_shortcut_controller_add_shortcut (GTK_SHORTCUT_CONTROLLER (shortcuts), gtk_shortcut_new (trigger, action));
       gtk_widget_add_controller (picture, shortcuts);
 