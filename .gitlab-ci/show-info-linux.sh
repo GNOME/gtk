b4e580111b65628bde16e47c448ee5523d52ea14
Jordan Petridis <jpetridis@gnome.org>
2026-01-31 20:12:19 +0200

    .gitlab-ci/show-info-linux.sh: Print useful gnomeos metadata
    Part-of: <https://gitlab.gnome.org/GNOME/gtk/-/merge_requests/9417>

--- .gitlab-ci/show-info-linux.sh
+++ .gitlab-ci/show-info-linux.sh
@@ -10,4 +10,15 @@ if [ "$ID" = "fedora" ]; then
   rpm -q mesa-dri-drivers vulkan-loader
 fi
 
+if [ "$ID" = "org.gnome.os" ]; then
+  # Image metadata
+  echo "$IMAGE_VERSION"
+  echo "$GNOMEOS_COMMIT"
+  echo "$GNOMEOS_COMMIT_DATE"
+  echo "$GNOMEOS_COMMITS_URL"
+
+  # Print the mesa sources
+  jq '.modules[] | select(.name=="extensions/mesa/mesa.bst")' /usr/manifest.json
+fi
+
 ls -l /dev/udmabuf
