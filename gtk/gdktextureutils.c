GNOME/gtk

commit 30e88108a401b6f6fb798c7a867a6d4db004346f

icon paintable: Support visibility
We use visibility when serializing stateful icons.
Therefore, it is better if GtkIconPaintable supports
this attribute too.

Part-of: <https://gitlab.gnome.org/GNOME/gtk/-/merge_requests/9436>

gtk/gdktextureutils.c
@@ -837,6 +837,7 @@ start_element_cb (GMarkupParseContext  *context,
   const char *stroke_dashoffset_attr = NULL;
   const char *opacity_attr = NULL;
   const char *class_attr = NULL;
+  const char *visibility_attr = NULL;
   GskPath *path = NULL;
   GskStroke *stroke = NULL;
   GskFillRule fill_rule;
@@ -848,6 +849,7 @@ start_element_cb (GMarkupParseContext  *context,
   char *end;
   gboolean do_fill = FALSE;
   gboolean do_stroke = FALSE;
+  gboolean visibility = TRUE;
 
   if (strcmp (element_name, "svg") == 0)
     {
@@ -1089,6 +1091,7 @@ start_element_cb (GMarkupParseContext  *context,
                                  error,
                                  "class", &class_attr,
                                  "opacity", &opacity_attr,
+                                 "visibility", &visibility_attr,
                                  "fill-rule", &fill_rule_attr,
                                  "fill-opacity", &fill_opacity_attr,
                                  "stroke-width", &stroke_width_attr,
@@ -1233,6 +1236,13 @@ start_element_cb (GMarkupParseContext  *context,
         }
     }
 
+  visibility = TRUE;
+  if (visibility_attr)
+    {
+      if (strcmp (visibility_attr, "hidden") == 0)
+        visibility = FALSE;
+    }
+
   if (fill_rule_attr && strcmp (fill_rule_attr, "evenodd") == 0)
     fill_rule = GSK_FILL_RULE_EVEN_ODD;
   else
@@ -1338,20 +1348,23 @@ start_element_cb (GMarkupParseContext  *context,
       gsk_stroke_set_dash_offset (stroke, offset);
     }
 
-  if (opacity != 1)
-    gtk_snapshot_push_opacity (data->snapshot, opacity);
-
-  if (do_fill)
+  if (visibility)
     {
-      data->n_paths++;
-      gtk_snapshot_append_fill (data->snapshot, path, fill_rule, &fill_color);
-    }
+      if (opacity != 1)
+        gtk_snapshot_push_opacity (data->snapshot, opacity);
 
-  if (do_stroke)
-    {
-      data->n_paths++;
-      data->has_strokes = TRUE;
-      gtk_snapshot_append_stroke (data->snapshot, path, stroke, &stroke_color);
+      if (do_fill)
+        {
+          data->n_paths++;
+          gtk_snapshot_append_fill (data->snapshot, path, fill_rule, &fill_color);
+        }
+
+      if (do_stroke)
+        {
+          data->n_paths++;
+          data->has_strokes = TRUE;
+          gtk_snapshot_append_stroke (data->snapshot, path, stroke, &stroke_color);
+        }
     }
 
   if (opacity != 1)

commit 3e5719426edd4a18b44d1d8463e353a296478d1b

icon paintable: Fix up visibility handling
When I added support for visibility handling, I messed up
the push/pop matching for opacity.

Fixes: 30e88108a401b6f6f
Part-of: <https://gitlab.gnome.org/GNOME/gtk/-/merge_requests/9442>

gtk/gdktextureutils.c
@@ -1365,10 +1365,10 @@ start_element_cb (GMarkupParseContext  *context,
           data->has_strokes = TRUE;
           gtk_snapshot_append_stroke (data->snapshot, path, stroke, &stroke_color);
         }
-    }
 
-  if (opacity != 1)
-    gtk_snapshot_pop (data->snapshot);
+      if (opacity != 1)
+        gtk_snapshot_pop (data->snapshot);
+    }
 
 cleanup:
   g_clear_pointer (&path, gsk_path_unref);