382108d6f3ad7e3ee0f308effeebfce45563b3b6
Benjamin Otte <otte@redhat.com>
2026-02-12 04:35:43 +0100

    tools: Rename image-tool-utils.c to tool-utils.
    And put its stuff into its own header.
    
    I want to share it across image-tool and rendernode-tool
    
    Part-of: <https://gitlab.gnome.org/GNOME/gtk/-/merge_requests/9465>

--- tools/gtk-image-tool.h
+++ tools/gtk-image-tool.h
@@ -9,17 +9,3 @@ void do_relabel     (int *argc, const char ***argv);
 void do_show        (int *argc, const char ***argv);
 void do_play        (int *argc, const char ***argv);
 
-GdkTexture * load_image_file (const char *filename);
-
-
-gboolean        find_format_by_name      (const char      *name,
-                                          GdkMemoryFormat *format);
-char **         get_format_names         (void);
-GdkColorState * find_color_state_by_name (const char      *name);
-
-char **         get_color_state_names    (void);
-char *          get_color_state_name     (GdkColorState   *color_state);
-char *          get_color_state_cicp     (GdkColorState   *color_state);
-
-GdkColorState  *parse_cicp_tuple         (const char      *cicp_tuple,
-                                          GError         **error);
