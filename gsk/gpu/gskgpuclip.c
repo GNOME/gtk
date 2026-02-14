b8fbad92c708f83513a3e7c89b7ac46128c0a81f
Benjamin Otte <otte@redhat.com>
2026-02-14 13:43:51 +0100

    gpuclip: Make init_empty() take an offset
    I've decided that all clip functions now take an offset and they do the
    conversions from user coordinates to clip coordinates.
    
    This simplifies the node handling code because it only has to deal with
    user and device coordinates, but not with clip.
    
    Part-of: <https://gitlab.gnome.org/GNOME/gtk/-/merge_requests/9437>

--- gsk/gpu/gskgpuclip.c
+++ gsk/gpu/gskgpuclip.c
@@ -7,11 +7,14 @@
 #include "gsktransform.h"
 
 void
-gsk_gpu_clip_init_empty (GskGpuClip            *clip,
-                         const graphene_rect_t *rect)
+gsk_gpu_clip_init_empty (GskGpuClip             *clip,
+                         const graphene_point_t *offset,
+                         const graphene_rect_t  *rect)
 {
   clip->type = GSK_GPU_CLIP_NONE;
   gsk_rounded_rect_init_from_rect (&clip->rect, rect, 0);
+  clip->rect.bounds.origin.x += offset->x;
+  clip->rect.bounds.origin.y += offset->y;
 }
 
 void
