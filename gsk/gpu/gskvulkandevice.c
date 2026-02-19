c55ced9e5e9b664574c663fe45b80c40810ae2c3
Benjamin Otte <otte@redhat.com>
2026-02-19 17:05:20 +0100

    gpu: Add a new blend method
    This is the blend method needed to combine 2 masks, ie it multiplies the
    2 alpha values.

--- gsk/gpu/gskvulkandevice.c
+++ gsk/gpu/gskvulkandevice.c
@@ -821,6 +821,19 @@ static VkPipelineColorBlendAttachmentState blend_attachment_states[] = {
                     | VK_COLOR_COMPONENT_G_BIT
                     | VK_COLOR_COMPONENT_B_BIT
   },
+  [GSK_GPU_BLEND_MASK] = {
+    .blendEnable = VK_TRUE,
+    .colorBlendOp = VK_BLEND_OP_ADD,
+    .srcColorBlendFactor = VK_BLEND_FACTOR_ZERO,
+    .dstColorBlendFactor = VK_BLEND_FACTOR_SRC_ALPHA,
+    .alphaBlendOp = VK_BLEND_OP_ADD,
+    .srcAlphaBlendFactor = VK_BLEND_FACTOR_ZERO,
+    .dstAlphaBlendFactor = VK_BLEND_FACTOR_SRC_ALPHA,
+    .colorWriteMask = VK_COLOR_COMPONENT_A_BIT
+                    | VK_COLOR_COMPONENT_R_BIT
+                    | VK_COLOR_COMPONENT_G_BIT
+                    | VK_COLOR_COMPONENT_B_BIT
+  },
   [GSK_GPU_BLEND_MASK_ONE] = {
     .blendEnable = VK_TRUE,
     .colorBlendOp = VK_BLEND_OP_ADD,
