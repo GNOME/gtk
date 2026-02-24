bd586b901242acc5a7a7306130b477859032ab60
Benjamin Otte <otte@redhat.com>
2026-02-24 21:20:07 +0100

    gpu: Turn cache stats into a table
    The info printed with GSK_DEBUG=cache is much more useful as a table.

--- gsk/gpu/gskgpucache.c
+++ gsk/gpu/gskgpucache.c
@@ -594,18 +594,25 @@ print_cache_stats (GskGpuCache *self)
         }
     }
 
-  message = g_string_new ("Cached items");
+  message = g_string_new ("Cached items:");
+  if (g_hash_table_size (classes) == 0)
+    g_string_append (message, "\n  none");
+  else
+    g_string_append (message, "\n  Class        Items Stale      Pixels       Stale");
   g_hash_table_iter_init (&iter, classes);
   while (g_hash_table_iter_next (&iter, &key, &value))
     {
       const GskGpuCachedClass *class = key;
       const GskGpuCacheData *cache_data = value;
 
-      g_string_append_printf (message, "\n  %s:%*s%5u (%u stale)", class->name, 12 - MIN (12, (int) strlen (class->name)), "", cache_data->n_items, cache_data->n_stale_items);
+      g_string_append_printf (message, "\n  %-12s %5u %5u %'11zu %'11zu %3zu%%", 
+                              class->name,
+                              cache_data->n_items, cache_data->n_stale_items,
+                              cache_data->n_pixels, cache_data->n_stale_pixels,
+                              cache_data->n_stale_pixels * 100 / cache_data->n_pixels);
 
       if (class == &GSK_GPU_CACHED_TEXTURE_CLASS)
         g_string_append_printf (message, " (%u in hash)", g_hash_table_size (self->texture_cache));
-      g_string_append_printf (message, " %zu pixels (%zu stale)", cache_data->n_pixels, cache_data->n_stale_pixels);
     }
 
   gdk_debug_message ("%s", message->str);
