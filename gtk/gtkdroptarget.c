GNOME/gtk

commit 62dd8fc002447940656546b5e63a6c462318bcab

drop target: Make sure enter/leave are paired
We were no longer emitting the leave signal, after
some recent changes.

Closes: #8037
Part-of: <https://gitlab.gnome.org/GNOME/gtk/-/merge_requests/9568>

gtk/gtkdroptarget.c
@@ -117,8 +117,8 @@ struct _GtkDropTarget
 
   GdkContentFormats *formats;
   GdkDragAction actions;
-  guint preload : 1;
-
+  guint preload  : 1;
+  guint entered  : 1;
   guint dropping : 1;
   graphene_point_t coords;
   GdkDrop *drop;
@@ -516,7 +516,10 @@ gtk_drop_target_handle_crossing (GtkEventController    *controller,
 
       /* start_drop ends w/ thaw_notify, where handler may reject, so recheck */
       if (self->drop != NULL)
-        g_signal_emit (self, signals[ENTER], 0, x, y, &preferred);
+        {
+          self->entered = TRUE;
+          g_signal_emit (self, signals[ENTER], 0, x, y, &preferred);
+        }
       else
         preferred = 0;
 
@@ -539,12 +542,17 @@ gtk_drop_target_handle_crossing (GtkEventController    *controller,
     }
   else
     {
+      if (self->entered)
+        {
+          self->entered = FALSE;
+          g_signal_emit (self, signals[LEAVE], 0);
+        }
+
       if (crossing->new_descendent != NULL ||
           crossing->new_target == widget ||
           self->drop == NULL)
         return;
 
-      g_signal_emit (self, signals[LEAVE], 0);
       if (!self->dropping)
         gtk_drop_target_end_drop (self);
 
@@ -557,8 +565,12 @@ gtk_drop_target_reset (GtkEventController *controller)
 {
   GtkDropTarget *self = GTK_DROP_TARGET (controller);
 
-  if (self->drop)
-    g_signal_emit (self, signals[LEAVE], 0);
+  if (self->entered)
+    {
+      self->entered = FALSE;
+      g_signal_emit (self, signals[LEAVE], 0);
+    }
+
   gtk_drop_target_end_drop (self);
 }
 

commit 12c7cb825a0737bf42cd3cce717422f9d09d9ac9

droptarget: Fix unconditional leave on cross out
Fixes a regression introduced by 62dd8fc002447940656546b5e63a6c462318bcab.

Before emitting the ::leave signal we need to check if the target
actually moved out of the `DropTarget`'s hierarchy. Otherwise ::leave
is emitted everytime the target changes between any descendents of
the `DropTarget`'s widget, which is not the correct behavior.

Part-of: <https://gitlab.gnome.org/GNOME/gtk/-/merge_requests/9572>

gtk/gtkdroptarget.c
@@ -542,15 +542,22 @@ gtk_drop_target_handle_crossing (GtkEventController    *controller,
     }
   else
     {
+      /*
+       * @self is attached to the common ancestor of new_target and old_target.
+       * I.e. not actually crossing out of the drop target's area, so there is
+       * nothing to do.
+       */
+      if (crossing->new_descendent != NULL ||
+          crossing->new_target == widget)
+        return;
+
       if (self->entered)
         {
           self->entered = FALSE;
           g_signal_emit (self, signals[LEAVE], 0);
         }
 
-      if (crossing->new_descendent != NULL ||
-          crossing->new_target == widget ||
-          self->drop == NULL)
+      if (self->drop == NULL)
         return;
 
       if (!self->dropping)
@@ -1169,4 +1176,3 @@ gtk_drop_target_reject (GtkDropTarget *self)
 
   gtk_drop_target_end_drop (self);
 }
-