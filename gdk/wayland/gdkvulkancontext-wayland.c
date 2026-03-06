GNOME/gtk

commit bbfd8e0e8925409e38f7ca643bb1f7f4b7cb73fd

vulkan: Use wl_surface_offset
Call wl_surface_offset in end_frame to apply the offset for drag
surfaces. This matches what the GL draw context already does, and
it fixes drag surfaces jumping at the beginning of the drag.

Fixes: #6972

gdk/wayland/gdkvulkancontext-wayland.c
@@ -64,9 +64,15 @@ gdk_vulkan_context_wayland_end_frame (GdkDrawContext *context,
                                       cairo_region_t *painted)
 {
   GdkSurface *surface = gdk_draw_context_get_surface (GDK_DRAW_CONTEXT (context));
+  GdkWaylandSurface *impl = GDK_WAYLAND_SURFACE (surface);
+  int dx = impl->pending_buffer_offset_x;
+  int dy = impl->pending_buffer_offset_y;
 
   gdk_wayland_surface_sync (surface);
   gdk_wayland_surface_request_frame (surface);
+
+  if (wl_surface_get_version (impl->display_server.wl_surface) >= WL_SURFACE_OFFSET_SINCE_VERSION)
+    wl_surface_offset (impl->display_server.wl_surface, dx, dy);
 
   GDK_DRAW_CONTEXT_CLASS (gdk_wayland_vulkan_context_parent_class)->end_frame (context, painted);
 

commit 398c1f9a452d6b7eb7c2ebc9022c10f21a203e96

wayland: Reset pending offset in Vulkan path
Otherwise drag surfaces that redraw without changing
the hotspot have it applied on top every frame and
quickly slide away.

This is the same fix that applied for OpenGL in 54e36f6b735.

Fixes: bbfd8e0e89
Closes: #8079
Part-of: <https://gitlab.gnome.org/GNOME/gtk/-/merge_requests/9577>

gdk/wayland/gdkvulkancontext-wayland.c
@@ -61,6 +61,9 @@ gdk_vulkan_context_wayland_end_frame (GdkDrawContext *context,
   int dx = impl->pending_buffer_offset_x;
   int dy = impl->pending_buffer_offset_y;
 
+  impl->pending_buffer_offset_x = 0;
+  impl->pending_buffer_offset_y = 0;
+
   gdk_wayland_surface_sync (surface);
   gdk_wayland_surface_request_frame (surface);
 