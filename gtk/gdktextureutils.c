edce58663c6b070657d4ef59529b572f56865130
Matthias Clasen <mclasen@redhat.com>
2026-03-09 19:40:27 -0400

    svg: Fix up fallback for symbolic
    n 73c58ad9a3fd, I was optimistic and move the symbolic
    png code away, since we were no longer generating recolorable
    textures on the fly. But then, in 73c58ad9a3 we had to go
    back to allow loading svgs that GtkSvg can't handle properly
    as textures, including symbolic ones. So we still need
    the symbolic png code. Bring it back for now.
    
    Closes: #8090

--- gtk/gdktextureutils.c
+++ gtk/gdktextureutils.c
@@ -100,6 +100,240 @@ svg_from_bytes (GBytes     *bytes,
 }
 
 /* }}} */
+/* {{{ Symbolic processing */
+
+static GdkTexture *
+load_symbolic_svg (GtkSvg      *svg,
+                   int          width,
+                   int          height,
+                   const char  *fg_color,
+                   const char  *success_color,
+                   const char  *warning_color,
+                   const char  *error_color,
+                   GError     **error)
+{
+  GdkRGBA colors[4];
+
+  gdk_rgba_parse (&colors[GTK_SYMBOLIC_COLOR_FOREGROUND], fg_color);
+  gdk_rgba_parse (&colors[GTK_SYMBOLIC_COLOR_SUCCESS], success_color);
+  gdk_rgba_parse (&colors[GTK_SYMBOLIC_COLOR_WARNING], warning_color);
+  gdk_rgba_parse (&colors[GTK_SYMBOLIC_COLOR_ERROR], error_color);
+
+  return svg_to_texture (svg, width, height, colors, 4);
+}
+
+static gboolean
+extract_plane (GdkTexture *src,
+               guchar     *dst_data,
+               gsize       dst_width,
+               gsize       dst_height,
+               int         from_plane,
+               int         to_plane)
+{
+  const guchar *src_data, *src_row;
+  int width, height;
+  gsize src_stride, dst_stride;
+  guchar *dst_row;
+  int x, y;
+  gboolean all_clear = TRUE;
+  GdkTextureDownloader *downloader;
+  GBytes *bytes;
+
+  width = gdk_texture_get_width (src);
+  height = gdk_texture_get_height (src);
+
+  g_assert (width <= dst_width);
+  g_assert (height <= dst_height);
+
+  downloader = gdk_texture_downloader_new (src);
+  gdk_texture_downloader_set_format (downloader, GDK_MEMORY_R8G8B8A8);
+  bytes = gdk_texture_downloader_download_bytes (downloader, &src_stride);
+
+  src_data = g_bytes_get_data (bytes, NULL);
+
+  dst_stride = dst_width * 4;
+
+  for (y = 0; y < height; y++)
+    {
+      src_row = src_data + src_stride * y;
+      dst_row = dst_data + dst_stride * y;
+      for (x = 0; x < width; x++)
+        {
+          if (src_row[from_plane] != 0)
+            all_clear = FALSE;
+
+          dst_row[to_plane] = src_row[from_plane];
+          src_row += 4;
+          dst_row += 4;
+        }
+    }
+
+  gdk_texture_downloader_free (downloader);
+  g_bytes_unref (bytes);
+
+  return all_clear;
+}
+
+static GdkTexture *
+keep_alpha (GdkTexture *src)
+{
+  GdkTextureDownloader *downloader;
+  GBytes *bytes;
+  gsize stride;
+  gsize width, height;
+  guchar *data;
+  gsize size;
+  GdkTexture *res;
+
+  width = gdk_texture_get_width (src);
+  height = gdk_texture_get_height (src);
+
+  downloader = gdk_texture_downloader_new (src);
+  gdk_texture_downloader_set_format (downloader, GDK_MEMORY_R8G8B8A8);
+  data = g_bytes_unref_to_data (gdk_texture_downloader_download_bytes (downloader, &stride), &size);
+
+  for (gsize y = 0; y < height; y++)
+    {
+      guchar *row = data + stride * y;
+      for (gsize x = 0; x < width; x++)
+        {
+          row[0] = row[1] = row[2] = 0;
+          row += 4;
+        }
+    }
+
+  bytes = g_bytes_new_take (data, size);
+  res = gdk_memory_texture_new (width, height, GDK_MEMORY_R8G8B8A8, bytes, stride);
+  g_bytes_unref (bytes);
+  g_object_unref (src);
+
+  gdk_texture_downloader_free (downloader);
+
+  return res;
+}
+
+static gboolean
+svg_has_symbolic_classes (GBytes *bytes)
+{
+#ifdef HAVE_MEMMEM
+  const char *data;
+  gsize len;
+
+  data = g_bytes_get_data (bytes, &len);
+
+  /* Not super precise, but good enough */
+  return memmem (data, len, "class=\"", strlen ("class=\"")) != NULL;
+#else
+  return TRUE;
+#endif
+}
+
+static GdkTexture *
+gdk_texture_new_from_bytes_symbolic (GBytes    *bytes,
+                                     int        width,
+                                     int        height,
+                                     gboolean  *out_only_fg,
+                                     GError   **error)
+
+{
+  GtkSvg *svg;
+  const char *r_string = "rgb(255,0,0)";
+  const char *g_string = "rgb(0,255,0)";
+  gboolean only_fg;
+  guchar *data;
+  GBytes *data_bytes;
+  GdkTexture *texture;
+
+  svg = gtk_svg_new_from_bytes (bytes);
+
+  if (width == 0 && height == 0)
+    {
+      width = gdk_paintable_get_intrinsic_width (GDK_PAINTABLE (svg));
+      height = gdk_paintable_get_intrinsic_height (GDK_PAINTABLE (svg));
+      if (width == 0 || height == 0)
+        {
+          g_set_error (error, G_IO_ERROR, G_IO_ERROR_FAILED,
+                       "Symbolic icon has no intrinsic size; please set one in its SVG");
+          g_object_unref (svg);
+          return NULL;
+        }
+    }
+
+  if (!svg_has_symbolic_classes (bytes))
+    {
+      texture = svg_to_texture (svg, width, height, NULL, 0);
+
+      if (texture)
+        texture = keep_alpha (texture);
+
+      if (out_only_fg)
+        *out_only_fg = TRUE;
+
+      g_object_unref (svg);
+
+      return texture;
+    }
+
+  only_fg = TRUE;
+  texture = NULL;
+
+  data = NULL;
+  data_bytes = NULL;
+
+  for (int plane = 0; plane < 3; plane++)
+    {
+      GdkTexture *loaded;
+
+      /* Here we render the svg with all colors solid, this should
+       * always make the alpha channel the same and it should match
+       * the final alpha channel for all possible renderings. We
+       * Just use it as-is for final alpha.
+       *
+       * For the 3 non-fg colors, we render once each with that
+       * color as red, and every other color as green. The resulting
+       * red will describe the amount of that color is in the
+       * opaque part of the color. We store these as the rgb
+       * channels, with the color of the fg being implicitly
+       * the "rest", as all color fractions should add up to 1.
+       */
+      loaded = load_symbolic_svg (svg, width, height,
+                                  g_string,
+                                  plane == 0 ? r_string : g_string,
+                                  plane == 1 ? r_string : g_string,
+                                  plane == 2 ? r_string : g_string,
+                                  error);
+      if (loaded == NULL)
+        goto out;
+
+      if (plane == 0)
+        {
+          data = g_new0 (guchar, 4 * width * height);
+          data_bytes = g_bytes_new_take (data, 4 * width * height);
+
+          extract_plane (loaded, data, width, height, 3, 3);
+        }
+
+      only_fg &= extract_plane (loaded, data, width, height, 0, plane);
+
+      g_object_unref (loaded);
+    }
+
+  texture = gdk_memory_texture_new (width, height,
+                                    GDK_MEMORY_R8G8B8A8,
+                                    data_bytes,
+                                    4 * width);
+
+out:
+  g_bytes_unref (data_bytes);
+  g_object_unref (svg);
+
+  if (out_only_fg)
+    *out_only_fg = only_fg;
+
+  return texture;
+}
+
+/* }}} */
 /* {{{ Texture-from-stream API */
 
 static GBytes *
@@ -192,7 +426,12 @@ gdk_paintable_new_from_bytes (GBytes     *bytes,
       svg = svg_from_bytes (bytes, is_symbolic, &unsupported);
       if (unsupported)
         {
-          paintable = GDK_PAINTABLE (gdk_texture_new_from_bytes (bytes, NULL));
+          gboolean only_fg;
+
+          if (is_symbolic)
+            paintable = GDK_PAINTABLE (gdk_texture_new_from_bytes_symbolic (bytes, 0, 0, &only_fg, NULL));
+          else
+            paintable = GDK_PAINTABLE (gdk_texture_new_from_bytes (bytes, NULL));
           if (paintable)
             {
               GTK_DEBUG (ICONFALLBACK, "Falling back to a texture for %s: %s", path, unsupported);
