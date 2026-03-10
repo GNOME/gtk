7c6faa88baca731cc799107d29188a4bb6a62541
Adrian Vovk <adrianvovk@gmail.com>
2026-03-09 22:16:04 -0400

    Revert save/restore API removal
    This brings back the session save/restore API, now that we've branched
    GTK 4.22. Also reverts some subsequent fixes that had to do with the
    save/restore API removal.
    
    This will of course need to be reworked before 4.24, but bringing back
    what we had would make a useful starting point
    
    This reverts commit e5f8484be0d011e2a000662c7852e684def1db6d.
    This reverts commit 5552a5d092d5ac029418c40347b9b19bc99045b5.
    This reverts commit da7e58813e0fd4e4857526da17d5b7887bb33d8f.
    This reverts commit 031658a00650cda816ea3637304634efabde9f53.

--- gtk/gtkapplicationwindow.h
+++ gtk/gtkapplicationwindow.h
@@ -56,8 +56,21 @@ struct _GtkApplicationWindowClass
 {
   GtkWindowClass parent_class;
 
+  /**
+   * GtkApplicationWindowClass::save_state:
+   * @dict: a dictionary where to store the window's state
+   *
+   * Class closure for the [signal@ApplicationWindow::save-state] signal.
+   *
+   * Returns: true to stop stop further handlers from running
+   *
+   * Since: 4.22
+   */
+  gboolean     (* save_state)           (GtkApplicationWindow   *window,
+                                         GVariantDict           *dict);
+
   /*< private >*/
-  gpointer padding[8];
+  gpointer padding[7];
 };
 
 GDK_AVAILABLE_IN_ALL
