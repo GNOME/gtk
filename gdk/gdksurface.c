GNOME/gtk

commit 5c00dc7ef4646455b1007701fcfee982b773a76b

GdkSurface: prevent popups from appearing offscreen in edge cases
This commit adds a single additional condition to the maybe_flip_position
function in gdksurface.c. If a popup's unflipped position is below the
bounds of its containing area, the popup uses its flipped position
instead. This prevents tooltips from appearing below the bounds of the
screen when a small widget is positioned very close to the bottom edge of
the screen, such as in Budgie and XFCE panel applets.

gdk/gdksurface.c
@@ -256,7 +256,7 @@ maybe_flip_position (int       bounds_pos,
   *flipped = TRUE;
   secondary = rect_pos + (1 - rect_sign) * rect_size / 2 - offset - (1 - surface_sign) * surface_size / 2;
 
-  if (secondary >= bounds_pos && secondary + surface_size <= bounds_pos + bounds_size)
+  if ((secondary >= bounds_pos && secondary + surface_size <= bounds_pos + bounds_size) || primary > bounds_pos + bounds_size)
     return secondary;
 
   *flipped = FALSE;

commit b041999af0e386f2470a88f9b181f50d9a7611ed

surface: Adjust how we decide whether to flip popups
If neither the unflipped position nor the flipped position allow the
popup to receive its full desired size, see which one is less bad, and
pick that one.

This helps both in case the popup can not be resized (in which case
we'll at least position it so it fits as best as possible), and when it
can be resized down (GDK_ANCHOR_RESIZE_* is set) to actually fit.
Without this patch, we'd see that the popup's natural size doesn't fully
fit either way, decide not to flip it, and then fail to present it at
all, even though it could be presented just fine if we flipped and then
shrunk it down.

Also, reshuffle and comment the code to make it easier to follow and
debug.

Fixes: 5c00dc7ef4646455b1007701fcfee982b773a76b
"GdkSurface: prevent popups from appearing offscreen in edge cases".

Signed-off-by: Sergey Bugaev <bugaevc@gmail.com>
Part-of: <https://gitlab.gnome.org/GNOME/gtk/-/merge_requests/9786>

gdk/gdksurface.c
@@ -248,21 +248,42 @@ maybe_flip_position (int       bounds_pos,
 {
   int primary;
   int secondary;
+  int badness = 0;
+  int limit;
+
+  /* Try to fit without flipping */
 
   *flipped = FALSE;
   primary = rect_pos + (1 + rect_sign) * rect_size / 2 + offset - (1 + surface_sign) * surface_size / 2;
 
-  if (!flip || (primary >= bounds_pos && primary + surface_size <= bounds_pos + bounds_size))
+  if (!flip)
     return primary;
 
-  *flipped = TRUE;
+  /* If the position gets larger than this limit,
+   * the other (right/bottom) edge is not going to fit.
+   */
+  limit = bounds_pos + bounds_size - surface_size;
+
+  if (primary < bounds_pos)
+    badness = bounds_pos - primary;
+  if (primary > limit)
+    badness = MAX (badness, primary - limit);
+  /* If it fit, we're done */
+  if (badness == 0)
+    return primary;
+
+  /* See if flipping helps */
+
   secondary = rect_pos + (1 - rect_sign) * rect_size / 2 - offset - (1 - surface_sign) * surface_size / 2;
 
-  if ((secondary >= bounds_pos && secondary + surface_size <= bounds_pos + bounds_size) || primary > bounds_pos + bounds_size)
-    return secondary;
+  if (secondary < bounds_pos && bounds_pos - secondary > badness)
+    return primary;
+  else if (secondary > limit && secondary - limit > badness)
+    return primary;
 
-  *flipped = FALSE;
-  return primary;
+  /* It does help */
+  *flipped = TRUE;
+  return secondary;
 }
 
 GdkMonitor *