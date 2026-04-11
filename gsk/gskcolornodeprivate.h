1065acb6681ee73821473ccb8a791d6f32aee6e0
Benjamin Otte <otte@redhat.com>
2026-04-11 06:51:00 +0200

    colornode: Add support for snapping
    The constructor is private, the only way to create this with public API
    is via GtkSnapshot.
    
    Part-of: <https://gitlab.gnome.org/GNOME/gtk/-/merge_requests/9844>

--- gsk/gskcolornodeprivate.h
+++ gsk/gskcolornodeprivate.h
@@ -7,7 +7,8 @@
 G_BEGIN_DECLS
 
 GskRenderNode *         gsk_color_node_new2                     (const GdkColor         *color,
-                                                                 const graphene_rect_t  *bounds);
+                                                                 const graphene_rect_t  *bounds,
+                                                                 GskRectSnap             snap);
 const GdkColor *        gsk_color_node_get_gdk_color            (const GskRenderNode    *node);
 
 G_END_DECLS
