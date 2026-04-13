GNOME/gtk

commit 8cc6e31e018bc620eae81c1751509770e7322b09

svg: Clean up current time handling
When doing state changes for animations that are triggered
by events, we want to use the current time for the event,
but we should not advance the animation as a side-effect.

Part-of: <https://gitlab.gnome.org/GNOME/gtk/-/merge_requests/9796>

gtk/svg/gtksvg.c
@@ -249,7 +249,6 @@
 #undef DEBUG
 #endif /* _MSC_VER */
 
-
 /* {{{ Some debug tools */
 
 #ifdef DEBUG
@@ -789,11 +788,12 @@ svg_writing_mode_is_vertical (WritingMode mode)
   return is_vertical[mode];
 }
 
-static void
-ensure_current_time (GtkSvg *self)
+static int64_t
+get_current_time (GtkSvg *self)
 {
-  if (self->clock && self->playing)
-    self->current_time = MAX (self->current_time, gdk_frame_clock_get_frame_time (self->clock));
+  if (self->clock && self->playing && self->load_time != INDEFINITE)
+    return MAX (self->current_time, gdk_frame_clock_get_frame_time (self->clock));
+  return self->current_time;
 }
 
 /* }}} */
@@ -9839,14 +9839,14 @@ gtk_svg_activate_element (GtkSvg     *self,
   SvgValue *value = svg_element_get_current_value (link, SVG_PROPERTY_HREF);
   SvgAnimation *animation = svg_href_get_animation (value);
   SvgElement *target = svg_href_get_shape (value);
+  int64_t current_time = get_current_time (self);
 
   g_assert (svg_element_get_type (link) == SVG_ELEMENT_LINK);
 
   if (animation)
     {
-      ensure_current_time (self);
-      animation_set_begin (animation, self->current_time);
-      animation_update_state (animation, self->current_time);
+      animation_set_begin (animation, current_time);
+      animation_update_state (animation, current_time);
       collect_next_update (self);
       invalidate_for_next_update (self);
       schedule_next_update (self);
@@ -9873,25 +9873,27 @@ static void
 gtk_svg_set_hover (GtkSvg     *self,
                    SvgElement *target)
 {
+  int64_t current_time = get_current_time (self);
+
   if (self->hover == target)
     return;
 
   if (self->hover)
     {
-      timeline_update_for_event (self->timeline, self->hover, EVENT_TYPE_MOUSE_LEAVE, self->current_time);
+      timeline_update_for_event (self->timeline, self->hover, EVENT_TYPE_MOUSE_LEAVE, current_time);
       svg_element_set_hover (self->hover, FALSE);
     }
 
   if (target)
     {
-      timeline_update_for_event (self->timeline, target, EVENT_TYPE_MOUSE_ENTER, self->current_time);
+      timeline_update_for_event (self->timeline, target, EVENT_TYPE_MOUSE_ENTER, current_time);
       svg_element_set_hover (target, TRUE);
     }
 
   self->hover = target;
   self->style_changed = TRUE;
 
-  update_animation_state (self);
+  gtk_svg_advance (self, current_time);
 
   self->style_changed = TRUE;
   gdk_paintable_invalidate_contents (GDK_PAINTABLE (self));
@@ -9935,8 +9937,6 @@ gtk_svg_handle_event (GtkSvg   *self,
   /* Don't handle events before we're loaded */
   if (self->load_time == INDEFINITE)
     return FALSE;
-
-  ensure_current_time (self);
 
   switch ((unsigned int) gdk_event_get_event_type (event))
     {
@@ -10042,7 +10042,7 @@ gtk_svg_grab_focus (GtkSvg *self)
   svg_element_set_focus (focus, TRUE);
 
   timeline_update_for_event (self->timeline, focus, EVENT_TYPE_FOCUS, current_time);
-  update_animation_state (self);
+  gtk_svg_advance (self, current_time);
 
   self->focus = focus;
 
@@ -10067,7 +10067,7 @@ gtk_svg_lose_focus (GtkSvg *self)
   svg_element_set_focus (self->focus, FALSE);
 
   timeline_update_for_event (self->timeline, self->focus, EVENT_TYPE_BLUR, current_time);
-  update_animation_state (self);
+  gtk_svg_advance (self, current_time);
 
   self->focus = NULL;
 
@@ -10082,6 +10082,7 @@ gtk_svg_move_focus (GtkSvg           *self,
                     GtkDirectionType  direction)
 {
   SvgElement *focus = NULL;
+  int64_t current_time;
 
   /* Don't handle events before we're loaded */
   if (self->load_time == INDEFINITE)
@@ -10129,32 +10130,28 @@ gtk_svg_move_focus (GtkSvg           *self,
       focus = self->focus;
     }
 
-  if (self->focus != focus)
+  if (self->focus == focus)
+    return FALSE;
+
+  current_time = get_current_time (self);
+
+  if (self->focus)
     {
-      ensure_current_time (self);
-
-      if (self->focus)
-        {
-          timeline_update_for_event (self->timeline, self->focus, EVENT_TYPE_BLUR, self->current_time);
-
-          svg_element_set_focus (self->focus, FALSE);
-        }
-      if (focus)
-        {
-          timeline_update_for_event (self->timeline, focus, EVENT_TYPE_FOCUS, self->current_time);
-
-          svg_element_set_focus (focus, TRUE);
-        }
-
-      if (self->load_time != INDEFINITE)
-        {
-          update_animation_state (self);
-        }
-
-      self->focus = focus;
-      self->style_changed = TRUE;
-      gdk_paintable_invalidate_contents (GDK_PAINTABLE (self));
+      timeline_update_for_event (self->timeline, self->focus, EVENT_TYPE_BLUR, current_time);
+      svg_element_set_focus (self->focus, FALSE);
     }
+
+  if (focus)
+    {
+      timeline_update_for_event (self->timeline, focus, EVENT_TYPE_FOCUS, current_time);
+      svg_element_set_focus (focus, TRUE);
+    }
+
+  gtk_svg_advance (self, current_time);
+
+  self->focus = focus;
+  self->style_changed = TRUE;
+  gdk_paintable_invalidate_contents (GDK_PAINTABLE (self));
 
   return self->focus != NULL;
 }
@@ -10382,8 +10379,6 @@ gtk_svg_set_state (GtkSvg       *self,
   previous_state = self->state;
   self->state = state;
 
-  ensure_current_time (self);
-
   if ((self->features & GTK_SVG_EXTENSIONS) == 0)
     {
       g_object_notify_by_pspec (G_OBJECT (self), properties[PROP_STATE]);
@@ -10403,11 +10398,12 @@ gtk_svg_set_state (GtkSvg       *self,
   /* Don't jiggle things while we're still loading */
   if (self->load_time != INDEFINITE)
     {
+      int64_t current_time = get_current_time (self);
       dbg_print ("state", "renderer state %u -> %u", previous_state, state);
 
       timeline_update_for_state (self->timeline,
                                  previous_state, self->state,
-                                 self->current_time + self->state_change_delay);
+                                 current_time + self->state_change_delay);
 
       update_animation_state (self);
 

commit 36ce4d8f10ab598702545100e6645c7907d7e429

svg: Fix an oversight
We need to advance the animation in gtk_svg_set_state().

Fixes: 8cc6e31e018bc620
Part-of: <https://gitlab.gnome.org/GNOME/gtk/-/merge_requests/9016>

gtk/svg/gtksvg.c
@@ -10412,7 +10412,7 @@ gtk_svg_set_state (GtkSvg       *self,
                                  previous_state, self->state,
                                  current_time + self->state_change_delay);
 
-      update_animation_state (self);
+      gtk_svg_advance (self, current_time);
 
 #ifdef DEBUG
       animation_state_dump (self);