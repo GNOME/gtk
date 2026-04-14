GNOME/gtk

commit 286073d5c1f1f61ef4de770caf3a15f99180d495

gpu: Handle scale being too small
If the scale gets too small we end up with issues, so reject transforms
when they come out with scales that are too small.

Fixes various rare crashes in user/animation-initiated transforms.

Testcase added.

Fixes #8139

Part-of: <https://gitlab.gnome.org/GNOME/gtk/-/merge_requests/9801>

gsk/gpu/gskgpurenderpass.c
@@ -26,6 +26,15 @@
  */
 #define EPSILON 0.001
 
+/* the minimum scale required. If it's smaller than this, we treat it like 0 and
+ * skip it.
+ *
+ * We set this to 1/100,000 because then 100,000 units would make up 1 pixel. And
+ * it's unlikely that something useful results from scaling this far, considering
+ * max texture size is usually 16,384 and FLOAT16 can't represent numbers this big.
+ */
+#define GSK_GPU_MIN_SCALE 0.00001
+
 gboolean
 gsk_gpu_render_pass_device_to_user (GskGpuRenderPass            *self,
                                     const cairo_rectangle_int_t *device,
@@ -410,6 +419,25 @@ extract_scale_from_transform (GskTransform *transform,
     }
 }
 
+static gboolean
+gsk_gpu_render_pass_push_transform_check_scale (GskGpuRenderPass                 *self,
+                                                float                             scale_x,
+                                                float                             scale_y,
+                                                GskGpuRenderPassTransformStorage *storage)
+{
+  if (scale_x >= GSK_GPU_MIN_SCALE &&
+      scale_y >= GSK_GPU_MIN_SCALE)
+    return TRUE;
+
+  gsk_gpu_clip_init_copy (&storage->clip.clip, &self->clip);
+  gsk_gpu_clip_init_all_clipped (&self->clip);
+  self->modelview = gsk_transform_ref (storage->modelview);
+  storage->clip.modified = GSK_GPU_GLOBAL_CLIP;
+  self->pending_globals |= storage->clip.modified;
+
+  return FALSE;
+}
+
 static void
 gsk_gpu_render_pass_draw_clip_mask (GskGpuRenderPass            *self,
                                     const graphene_rect_t       *new_clip_rect,
@@ -443,6 +471,12 @@ gsk_gpu_render_pass_push_transform (GskGpuRenderPass                 *self,
         float dx, dy, scale_x, scale_y;
 
         gsk_transform_to_affine (transform, &scale_x, &scale_y, &dx, &dy);
+        if (!gsk_gpu_render_pass_push_transform_check_scale (self,
+                                                             scale_x * storage->scale.width,
+                                                             scale_y * storage->scale.height,
+                                                             storage))
+          return;
+
         gsk_gpu_clip_scale (&self->clip, &storage->clip.clip, GDK_DIHEDRAL_NORMAL, scale_x, scale_y);
         self->offset.x = (self->offset.x + dx) / scale_x;
         self->offset.y = (self->offset.y + dy) / scale_y;
@@ -462,13 +496,18 @@ gsk_gpu_render_pass_push_transform (GskGpuRenderPass                 *self,
         gsk_transform_to_dihedral (transform, &dihedral, &scale_x, &scale_y, &dx, &dy);
         inverted = gdk_dihedral_invert (dihedral);
         gdk_dihedral_get_mat2 (inverted, &xx, &xy, &yx, &yy);
+        old_scale_x = storage->scale.width;
+        old_scale_y = storage->scale.height;
+        if (!gsk_gpu_render_pass_push_transform_check_scale (self,
+                                                             fabs (scale_x * (old_scale_x * xx + old_scale_y * yx)),
+                                                             fabs (scale_y * (old_scale_x * xy + old_scale_y * yy)),
+                                                             storage))
+          return;
         gsk_gpu_clip_scale (&self->clip, &storage->clip.clip, inverted, scale_x, scale_y);
         self->offset.x = (self->offset.x + dx) / scale_x;
         self->offset.y = (self->offset.y + dy) / scale_y;
         self->offset = GRAPHENE_POINT_INIT (xx * self->offset.x + xy * self->offset.y,
                                             yx * self->offset.x + yy * self->offset.y);
-        old_scale_x = storage->scale.width;
-        old_scale_y = storage->scale.height;
         self->scale = GRAPHENE_SIZE_INIT (fabs (scale_x * (old_scale_x * xx + old_scale_y * yx)),
                                           fabs (scale_y * (old_scale_x * xy + old_scale_y * yy)));
         self->modelview = gsk_transform_dihedral (gsk_transform_ref (storage->modelview), dihedral);
@@ -526,6 +565,11 @@ gsk_gpu_render_pass_push_transform (GskGpuRenderPass                 *self,
             gsk_transform_to_dihedral (self->modelview, &dihedral, &scale_x, &scale_y, &dx, &dy);
             inverted = gdk_dihedral_invert (dihedral);
             gdk_dihedral_get_mat2 (inverted, &xx, &xy, &yx, &yy);
+            if (!gsk_gpu_render_pass_push_transform_check_scale (self,
+                                                                 fabs (scale_x * xx + scale_y * yx),
+                                                                 fabs (scale_x * xy + scale_y * yy),
+                                                                 storage))
+              return;
             dx /= scale_x;
             dy /= scale_y;
             self->offset = GRAPHENE_POINT_INIT (xx * dx + xy * dy,
@@ -559,7 +603,6 @@ gsk_gpu_render_pass_push_transform (GskGpuRenderPass                 *self,
                               storage->scale.height * storage->clip.clip.rect.bounds.size.height);
             new_pixels = MAX (scale_x * self->clip.rect.bounds.size.width,
                               scale_y * self->clip.rect.bounds.size.height);
-
             /* Check that our offscreen doesn't get too big.  1.5 ~ sqrt(2) */
             if (new_pixels > 1.5 * old_pixels)
               {
@@ -567,6 +610,12 @@ gsk_gpu_render_pass_push_transform (GskGpuRenderPass                 *self,
                 scale_x *= forced_downscale;
                 scale_y *= forced_downscale;
               }
+
+            if (!gsk_gpu_render_pass_push_transform_check_scale (self,
+                                                                 scale_x,
+                                                                 scale_y,
+                                                                 storage))
+              return;
 
             self->modelview = gsk_transform_scale (self->modelview, 1 / scale_x, 1 / scale_y);
             self->scale = GRAPHENE_SIZE_INIT (scale_x, scale_y);

testsuite/gsk/compare/transform-scale-too-small-nocairo.node
@@ -1,0 +1,76 @@
+/* first, ensure the bounds are guaranteed,
+ * so changes in the bounds logic can't change
+ * this test
+ */
+clip {
+  clip: -50 -50 100 100;
+  child: container {
+    color {
+      color: black;
+      bounds: -50 -50 100 100;
+    }
+
+    /* too small scale */
+    transform {
+      transform: scale(0.000001);
+      child: cairo { }
+    }
+
+    /* too small scale, but via 2 transforms */
+    transform {
+      transform: scale(0.001);
+      child: transform {
+        transform: scale(0.001);
+        child: cairo { }
+      }
+    }
+
+    /* too small scale with dihedral matrix */
+    transform {
+      transform: rotate(90) scale(0.000001);
+      child: cairo { }
+    }
+
+    /* too small scale with dihedral matrix, but via 2 transforms */
+    transform {
+      transform: scale(0.001);
+      child: transform {
+        transform: rotate(90) scale(0.001);
+        child: cairo { }
+      }
+    }
+
+    /* too small scale with dihedral matrix, but via intermediate 3d matrix */
+    transform {
+      transform: scale(0.001) rotate(45);
+      child: transform {
+        transform: rotate(45) scale(0.001);
+        child: cairo { }
+      }
+    }
+
+    /* too small scale for 3d matrix */
+    transform {
+      transform: scale(0.000001) perspective(800);
+      child: cairo { }
+    }
+
+    /* too small scale for 3d matrix, but via 2 transforms */
+    transform {
+      transform: scale(0.001);
+      child: transform {
+        transform: scale(0.001) perspective(800);
+        child: cairo { }
+      }
+    }
+
+    /* non-invertible matrix */
+    transform {
+      transform: matrix3d(0,0,0,0,
+                          1,1,0,0,
+                          0,0,1,0,
+                          0,0,0,1);
+      child: cairo { } 
+    }
+  }
+}

testsuite/gsk/meson.build
@@ -266,6 +266,7 @@ compare_render_tests = [
   'transform-huge-child-3d-nocairo',
   'transform-in-transform',
   'transform-in-transform-in-transform',
+  'transform-scale-too-small-nocairo',
   'yuv-matrix-d3d12',
   'yuv-matrix-dmabuf-nv12',
   'yuv-matrix-dmabuf-rgba',

commit aec107a64e3451d11daf32e216c6e26c7763a777

renderpass: Fix an oversight in recent fix
The code was saving the wrong clip, so after a pop_transform() the clip
was different then before the push_transform().

We did not check that in the testcase, so adapt the testcase by drawing
something afterwards.

Fixes: 286073d5c1f1f61ef4de770caf3a15f99180d495
Part-of: <https://gitlab.gnome.org/GNOME/gtk/-/merge_requests/9803>

gsk/gpu/gskgpurenderpass.c
@@ -429,7 +429,6 @@ gsk_gpu_render_pass_push_transform_check_scale (GskGpuRenderPass                
       scale_y >= GSK_GPU_MIN_SCALE)
     return TRUE;
 
-  gsk_gpu_clip_init_copy (&storage->clip.clip, &self->clip);
   gsk_gpu_clip_init_all_clipped (&self->clip);
   self->modelview = gsk_transform_ref (storage->modelview);
   storage->clip.modified = GSK_GPU_GLOBAL_CLIP;

testsuite/gsk/compare/transform-scale-too-small-nocairo.node
@@ -16,6 +16,13 @@ clip {
       child: cairo { }
     }
 
+    /* draw a small rectangle after every transform to check
+       that we properly unset everything in our error handling */
+    color {
+      color: red;
+      bounds: -40 -20 10 10;
+    }
+
     /* too small scale, but via 2 transforms */
     transform {
       transform: scale(0.001);
@@ -25,10 +32,20 @@ clip {
       }
     }
 
+    color {
+      color: red;
+      bounds: -20 -40 10 10;
+    }
+
     /* too small scale with dihedral matrix */
     transform {
       transform: rotate(90) scale(0.000001);
       child: cairo { }
+    }
+
+    color {
+      color: lime;
+      bounds: 10 -40 10 10;
     }
 
     /* too small scale with dihedral matrix, but via 2 transforms */
@@ -40,6 +57,11 @@ clip {
       }
     }
 
+    color {
+      color: lime;
+      bounds: 30 -20 10 10;
+    }
+
     /* too small scale with dihedral matrix, but via intermediate 3d matrix */
     transform {
       transform: scale(0.001) rotate(45);
@@ -49,10 +71,20 @@ clip {
       }
     }
 
+    color {
+      color: blue;
+      bounds: -40 10 10 10;
+    }
+
     /* too small scale for 3d matrix */
     transform {
       transform: scale(0.000001) perspective(800);
       child: cairo { }
+    }
+
+    color {
+      color: blue;
+      bounds: -20 30 10 10;
     }
 
     /* too small scale for 3d matrix, but via 2 transforms */
@@ -64,6 +96,11 @@ clip {
       }
     }
 
+    color {
+      color: yellow;
+      bounds: 10 30 10 10;
+    }
+
     /* non-invertible matrix */
     transform {
       transform: matrix3d(0,0,0,0,
@@ -71,6 +108,11 @@ clip {
                           0,0,1,0,
                           0,0,0,1);
       child: cairo { } 
+    }
+
+    color {
+      color: yellow;
+      bounds: 30 10 10 10;
     }
   }
 }