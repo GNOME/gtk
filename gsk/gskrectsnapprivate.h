6400db6273b96ff290a5a12d90c9a8e2e7956628
Benjamin Otte <otte@redhat.com>
2026-04-19 15:29:12 +0200

    gsk: Add (private) gsk_rect_snap_can_shrink/grow()
    Shrink checks are necessary to check if nodes can be marked as fully
    opaque - because if snapping shrinks the rect, then it does no longer
    cover the whole bounds. And then it won't be fully opaque.
    
    Grow checks are necessary when intersecting rects - if one of them can
    grow, the intersection might not be empty,
    The interesting part with intersections is that a non-empty intersection
    is valid as long as the snapping is retained, but an empty intersection
    is not - it needs to be reduced to a point or line.

--- gsk/gskrectsnapprivate.h
+++ gsk/gskrectsnapprivate.h
@@ -20,3 +20,53 @@ _gsk_rect_snap_get_direction (GskRectSnap snap,
   return (GskSnapDirection) ((snap >> (8 * dir)) & 0xFF);
 }
 
+#define gsk_rect_snap_can_shrink(...) _gsk_rect_snap_can_shrink(__VA_ARGS__)
+static inline gboolean
+_gsk_rect_snap_can_shrink (GskRectSnap snap)
+{
+  GskSnapDirection dir;
+
+  dir = _gsk_rect_snap_get_direction (snap, 0);
+  if (dir == GSK_SNAP_CEIL || dir == GSK_SNAP_ROUND)
+    return TRUE;
+
+  dir = _gsk_rect_snap_get_direction (snap, 1);
+  if (dir == GSK_SNAP_FLOOR)
+    return TRUE;
+
+  dir = _gsk_rect_snap_get_direction (snap, 2);
+  if (dir == GSK_SNAP_FLOOR)
+    return TRUE;
+
+  dir = _gsk_rect_snap_get_direction (snap, 3);
+  if (dir == GSK_SNAP_CEIL || dir == GSK_SNAP_ROUND)
+    return TRUE;
+
+  return FALSE;
+}
+
+#define gsk_rect_snap_can_grow(...) _gsk_rect_snap_can_grow(__VA_ARGS__)
+static inline gboolean
+_gsk_rect_snap_can_grow (GskRectSnap snap)
+{
+  GskSnapDirection dir;
+
+  dir = _gsk_rect_snap_get_direction (snap, 0);
+  if (dir == GSK_SNAP_FLOOR)
+    return TRUE;
+
+  dir = _gsk_rect_snap_get_direction (snap, 1);
+  if (dir == GSK_SNAP_CEIL || dir == GSK_SNAP_ROUND)
+    return TRUE;
+
+  dir = _gsk_rect_snap_get_direction (snap, 2);
+  if (dir == GSK_SNAP_CEIL || dir == GSK_SNAP_ROUND)
+    return TRUE;
+
+  dir = _gsk_rect_snap_get_direction (snap, 3);
+  if (dir == GSK_SNAP_FLOOR)
+    return TRUE;
+
+  return FALSE;
+}
+
