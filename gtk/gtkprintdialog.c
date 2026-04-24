GNOME/gtk

commit ed6acbcc813b9a03ff6df4a5763b4e196bb93012

print dialog: Fix GTask lifecycle management
We unref the task after we returned from it, and we are consistent
about that. But there is a case where we receive the Response
signal after we've already returned from the task, and we need
to make sure the task is still alive in the signal handler.

Related: !9631
Part-of: <https://gitlab.gnome.org/GNOME/gtk/-/merge_requests/9825>

gtk/gtkprintdialog.c
@@ -963,7 +963,6 @@ prepare_print_response (GDBusConnection *connection,
     }
 
   g_variant_unref (options);
-
   g_object_unref (task);
 }
 
@@ -1007,7 +1006,7 @@ prepare_print_called (GObject      *source,
                                             NULL,
                                             G_DBUS_SIGNAL_FLAGS_NO_MATCH_RULE,
                                             prepare_print_response,
-                                            self, NULL);
+                                            g_object_ref (task), g_object_unref);
 
     }
 
@@ -1049,7 +1048,7 @@ setup_window_handle_exported (GtkWindow  *window,
                                         NULL,
                                         G_DBUS_SIGNAL_FLAGS_NO_MATCH_RULE,
                                         prepare_print_response,
-                                        task, NULL);
+                                        g_object_ref (task), g_object_unref);
 
   g_variant_builder_init (&opt_builder, G_VARIANT_TYPE_VARDICT);
   g_variant_builder_add (&opt_builder, "{sv}", "handle_token", g_variant_new_string (handle_token));
@@ -1160,7 +1159,6 @@ print_response (GDBusConnection *connection,
           break;
         }
     }
-
   g_object_unref (task);
 }
 
@@ -1204,7 +1202,7 @@ print_called (GObject      *source,
                                             NULL,
                                             G_DBUS_SIGNAL_FLAGS_NO_MATCH_RULE,
                                             print_response,
-                                            task, NULL);
+                                            g_object_ref (task), g_object_unref);
 
     }
 
@@ -1255,7 +1253,7 @@ print_window_handle_exported (GtkWindow  *window,
                                         NULL,
                                         G_DBUS_SIGNAL_FLAGS_NO_MATCH_RULE,
                                         print_response,
-                                        task, NULL);
+                                        g_object_ref (task), g_object_unref);
 
   fd_list = g_unix_fd_list_new ();
   idx = g_unix_fd_list_append (fd_list, ptd->fds[0], NULL);

commit 91c91f73d883747218180d8259c2bf4759042ecb

printdialog: Fix a crash
Commit ed6acbcc813b9a slipped up in its attempt to couple the
unref of the task with its returning. In one case, we were unreffing
the task another time after it had already returned, causing a
later crash in gdbus cleanup.

Fixes: ed6acbcc813b9a
Part-of: <https://gitlab.gnome.org/GNOME/gtk/-/merge_requests/9864>

gtk/gtkprintdialog.c
@@ -990,7 +990,7 @@ prepare_print_called (GObject      *source,
 
   g_variant_get (ret, "(o)", &path);
   if (strcmp (path, ptd->portal_handle) != 0)
-   {
+    {
       g_free (ptd->portal_handle);
       ptd->portal_handle = g_steal_pointer (&path);
 
@@ -1141,6 +1141,7 @@ print_response (GDBusConnection *connection,
         {
         case 0:
           g_task_return_boolean (task, TRUE);
+          g_object_unref (task);
           break;
 
         case 1:
@@ -1148,6 +1149,7 @@ print_response (GDBusConnection *connection,
                                    GTK_DIALOG_ERROR,
                                    GTK_DIALOG_ERROR_DISMISSED,
                                    "Dismissed by user");
+          g_object_unref (task);
           break;
 
         case 2:
@@ -1156,10 +1158,10 @@ print_response (GDBusConnection *connection,
                                    GTK_DIALOG_ERROR,
                                    GTK_DIALOG_ERROR_FAILED,
                                    "Operation failed");
+          g_object_unref (task);
           break;
         }
     }
-  g_object_unref (task);
 }
 
 static void