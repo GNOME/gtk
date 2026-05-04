ElectronSpark/v6-gtk

snapping: Add a fudge factor
When there are rounding errors, the math can often come out as 0.49999
when round()ing (or 0.00001 when ceil()ing or 0.99999 when floor()ing)
even though the desired value after snapping is 0.5 (or 0.0 or 1.0
respectively).

Applying a fudge factor eliminates that problem.

Of course, it means that if that was the desired value then we are now
making a misjudgement and snapping wrong. But rounding errors happen
all the time and offsets this small are very rare, especially because
snapping is an operation on the device pixel grid, not some artificially
scaled coordinate system.

gsk/gskrectprivate.h
@@ -9,6 +9,11 @@
 #include <graphene.h>
 #include <math.h>
 
+/* the epsilon we allow pixels to be off due to rounding errors.
+ * Chosen rather randomly.
+ */
+#define GSK_SNAP_EPSILON 0.001
+
 #define GSK_RECT_INIT_CAIRO(cairo_rect) GRAPHENE_RECT_INIT((cairo_rect)->x, (cairo_rect)->y, (cairo_rect)->width, (cairo_rect)->height)
 
 static inline void
@@ -185,11 +190,11 @@ gsk_rect_snap_direction (float            value,
   switch (snap)
     {
     case GSK_SNAP_FLOOR:
-      return floorf (value);
+      return floorf (value + GSK_SNAP_EPSILON);
     case GSK_SNAP_CEIL:
-      return ceilf (value);
+      return ceilf (value - GSK_SNAP_EPSILON);
     case GSK_SNAP_ROUND:
-      return round (value);
+      return round (value + GSK_SNAP_EPSILON);
     case GSK_SNAP_NONE:
     default:
       return value;

GNOME/gtk

gsk/gskrectprivate.h
@@ -9,6 +9,11 @@
 #include <graphene.h>
 #include <math.h>
 
+/* the epsilon we allow pixels to be off due to rounding errors.
+ * Chosen rather randomly.
+ */
+#define GSK_SNAP_EPSILON 0.001
+
 #define GSK_RECT_INIT_CAIRO(cairo_rect) GRAPHENE_RECT_INIT((cairo_rect)->x, (cairo_rect)->y, (cairo_rect)->width, (cairo_rect)->height)
 
 static inline void
@@ -185,11 +190,11 @@ gsk_rect_snap_direction (float            value,
   switch (snap)
     {
     case GSK_SNAP_FLOOR:
-      return floorf (value);
+      return floorf (value + GSK_SNAP_EPSILON);
     case GSK_SNAP_CEIL:
-      return ceilf (value);
+      return ceilf (value - GSK_SNAP_EPSILON);
     case GSK_SNAP_ROUND:
-      return round (value);
+      return roundf (value + GSK_SNAP_EPSILON);
     case GSK_SNAP_NONE:
     default:
       return value;