GNOME/gtk

commit d2c95a1b13ee710fcfc69a592be669bcea4b26e8

gdk: Replace 'WITHDRAWN' state with async 'is-mapped' boolean
It was used by all surfaces to track 'is-mapped', but still part of the
GdkToplevelState, and is now replaced with a separate boolean in the
GdkSurface structure.

It also caused issues when a widget was unmapped, and due to that
unmapped a popover which hid its corresponding surface. When this
surface was hidden, it emitted a state change event, which would then go
back into GTK and queue a resize on popover widget, which would travel
back down to the widget that was originally unmapped, causing confusino
when doing future allocations.

To summarize, one should not hide widgets during allocation, and to
avoid this, make this new is-mapped boolean asynchronous when hiding a
surface, meaning the notification event for the changed mapped state
will be emitted in an idle callback. This avoids the above described
reentry issue.

gdk/gdksurface.c
@@ -114,6 +114,9 @@ static void update_cursor               (GdkDisplay *display,
 static void gdk_surface_set_frame_clock (GdkSurface      *surface,
                                          GdkFrameClock  *clock);
 
+static void gdk_surface_queue_set_is_mapped (GdkSurface *surface,
+                                             gboolean    is_mapped);
+
 
 static guint signals[LAST_SIGNAL] = { 0 };
 static GParamSpec *properties[LAST_PROP] = { NULL, };
@@ -472,7 +475,7 @@ gdk_surface_init (GdkSurface *surface)
 {
   /* 0-initialization is good for all other fields. */
 
-  surface->state = GDK_TOPLEVEL_STATE_WITHDRAWN;
+  surface->state = 0;
   surface->fullscreen_mode = GDK_FULLSCREEN_ON_CURRENT_MONITOR;
   surface->width = 1;
   surface->height = 1;
@@ -923,7 +926,10 @@ _gdk_surface_destroy_hierarchy (GdkSurface *surface,
 
   _gdk_surface_clear_update_area (surface);
 
-  surface->state |= GDK_TOPLEVEL_STATE_WITHDRAWN;
+  g_clear_handle_id (&surface->set_is_mapped_source_id, g_source_remove);
+  surface->is_mapped = FALSE;
+  surface->pending_is_mapped = FALSE;
+
   surface->destroyed = TRUE;
 
   surface_remove_from_pointer_info (surface, surface->display);
@@ -1696,14 +1702,7 @@ gdk_surface_hide (GdkSurface *surface)
 
   was_mapped = GDK_SURFACE_IS_MAPPED (surface);
 
-  if (GDK_SURFACE_IS_MAPPED (surface))
-    {
-      gdk_synthesize_surface_state (surface,
-                                    surface->state & ~GDK_TOPLEVEL_STATE_WITHDRAWN,
-                                    GDK_TOPLEVEL_STATE_WITHDRAWN);
-      surface->pending_unset_flags = 0;
-      surface->pending_set_flags = 0;
-    }
+  gdk_surface_queue_set_is_mapped (surface, FALSE);
 
   if (was_mapped)
     {
@@ -2614,7 +2613,6 @@ void
 gdk_surface_set_state (GdkSurface      *surface,
                        GdkToplevelState new_state)
 {
-  gboolean was_mapped, mapped;
   gboolean was_sticky, sticky;
   g_return_if_fail (GDK_IS_SURFACE (surface));
 
@@ -2626,19 +2624,14 @@ gdk_surface_set_state (GdkSurface      *surface,
    * inconsistent state to the user.
    */
 
-  was_mapped = GDK_SURFACE_IS_MAPPED (surface);
   was_sticky = GDK_SURFACE_IS_STICKY (surface);
 
   surface->state = new_state;
 
-  mapped = GDK_SURFACE_IS_MAPPED (surface);
   sticky = GDK_SURFACE_IS_STICKY (surface);
 
   if (GDK_IS_TOPLEVEL (surface))
     g_object_notify (G_OBJECT (surface), "state");
-
-  if (was_mapped != mapped)
-    g_object_notify_by_pspec (G_OBJECT (surface), properties[PROP_MAPPED]);
 
   if (was_sticky != sticky)
     g_object_notify (G_OBJECT (surface), "sticky");
@@ -2673,10 +2666,71 @@ gdk_surface_apply_state_change (GdkSurface *surface)
   gdk_synthesize_surface_state (surface,
                                 surface->pending_unset_flags,
                                 surface->pending_set_flags);
-  if (surface->pending_unset_flags & GDK_TOPLEVEL_STATE_WITHDRAWN)
-    gdk_surface_invalidate_rect (surface, NULL);
   surface->pending_unset_flags = 0;
   surface->pending_set_flags = 0;
+}
+
+static gboolean
+set_is_mapped_idle (gpointer user_data)
+{
+  GdkSurface *surface = GDK_SURFACE (user_data);
+
+  surface->set_is_mapped_source_id = 0;
+
+  g_return_val_if_fail (surface->pending_is_mapped != surface->is_mapped,
+                        G_SOURCE_REMOVE);
+
+  surface->is_mapped = surface->pending_is_mapped;
+  if (surface->is_mapped)
+    gdk_surface_invalidate_rect (surface, NULL);
+
+  g_object_notify (G_OBJECT (surface), "mapped");
+
+  return G_SOURCE_REMOVE;
+}
+
+void
+gdk_surface_set_is_mapped (GdkSurface *surface,
+                           gboolean    is_mapped)
+{
+  gboolean was_mapped;
+
+  if (surface->pending_is_mapped != surface->is_mapped)
+    g_clear_handle_id (&surface->set_is_mapped_source_id, g_source_remove);
+
+  surface->pending_is_mapped = is_mapped;
+
+  was_mapped = surface->is_mapped;
+  surface->is_mapped = is_mapped;
+  if (surface->is_mapped)
+    gdk_surface_invalidate_rect (surface, NULL);
+
+  if (was_mapped != is_mapped)
+    g_object_notify (G_OBJECT (surface), "mapped");
+}
+
+static void
+gdk_surface_queue_set_is_mapped (GdkSurface *surface,
+                                 gboolean    is_mapped)
+{
+  if (surface->pending_is_mapped == is_mapped)
+    return;
+
+  surface->pending_is_mapped = is_mapped;
+
+  if (surface->is_mapped == surface->pending_is_mapped)
+    {
+      g_clear_handle_id (&surface->set_is_mapped_source_id, g_source_remove);
+    }
+  else
+    {
+      g_return_if_fail (!surface->set_is_mapped_source_id);
+
+      surface->set_is_mapped_source_id =
+        g_idle_add_full (G_PRIORITY_HIGH - 10,
+                         set_is_mapped_idle,
+                         surface, NULL);
+    }
 }
 
 static gboolean

gdk/broadway/gdksurface-broadway.c
@@ -601,7 +601,7 @@ gdk_broadway_surface_layout_popup (GdkSurface     *surface,
 static void
 show_popup (GdkSurface *surface)
 {
-  gdk_synthesize_surface_state (surface, GDK_TOPLEVEL_STATE_WITHDRAWN, 0);
+  gdk_surface_set_is_mapped (surface, TRUE);
   gdk_broadway_surface_show (surface, FALSE);
   gdk_surface_invalidate_rect (surface, NULL);
 }
@@ -1521,7 +1521,7 @@ show_surface (GdkSurface *surface)
   was_mapped = GDK_SURFACE_IS_MAPPED (surface);
 
   if (!was_mapped)
-    gdk_synthesize_surface_state (surface, GDK_TOPLEVEL_STATE_WITHDRAWN, 0);
+    gdk_surface_set_is_mapped (surface, TRUE);
 
   gdk_broadway_surface_show (surface, FALSE);
 

gdk/gdksurfaceprivate.h
@@ -44,6 +44,10 @@ struct _GdkSurface
   GdkSurface *parent;        /* for popups */
   GList *children;           /* popups */
 
+  guint set_is_mapped_source_id;
+  gboolean pending_is_mapped;
+  gboolean is_mapped;
+
   gpointer widget;
 
   int x;
@@ -167,11 +171,13 @@ struct _GdkSurfaceClass
 
 #define GDK_SURFACE_DESTROYED(d) (((GdkSurface *)(d))->destroyed)
 
-#define GDK_SURFACE_IS_MAPPED(surface) (((surface)->state & GDK_TOPLEVEL_STATE_WITHDRAWN) == 0)
-
+#define GDK_SURFACE_IS_MAPPED(surface) ((surface)->pending_is_mapped)
 
 void gdk_surface_set_state (GdkSurface      *surface,
                             GdkToplevelState  new_state);
+
+void gdk_surface_set_is_mapped (GdkSurface *surface,
+                                gboolean    is_mapped);
 
 GdkMonitor * gdk_surface_get_layout_monitor (GdkSurface      *surface,
                                              GdkPopupLayout  *layout,

gdk/gdktoplevel.c
@@ -123,7 +123,7 @@ gdk_toplevel_default_init (GdkToplevelInterface *iface)
       g_param_spec_flags ("state",
                           P_("State"),
                           P_("State"),
-                          GDK_TYPE_TOPLEVEL_STATE, GDK_TOPLEVEL_STATE_WITHDRAWN,
+                          GDK_TYPE_TOPLEVEL_STATE, 0,
                           G_PARAM_READABLE | G_PARAM_STATIC_STRINGS));
   g_object_interface_install_property (iface,
       g_param_spec_string ("title",

gdk/gdktoplevel.h
@@ -71,7 +71,6 @@ typedef enum
 
 /**
  * GdkToplevelState:
- * @GDK_TOPLEVEL_STATE_WITHDRAWN: the surface is not shown
  * @GDK_TOPLEVEL_STATE_MINIMIZED: the surface is minimized
  * @GDK_TOPLEVEL_STATE_MAXIMIZED: the surface is maximized
  * @GDK_TOPLEVEL_STATE_STICKY: the surface is sticky
@@ -98,23 +97,22 @@ typedef enum
  */
 typedef enum
 {
-  GDK_TOPLEVEL_STATE_WITHDRAWN        = 1 << 0,
-  GDK_TOPLEVEL_STATE_MINIMIZED        = 1 << 1,
-  GDK_TOPLEVEL_STATE_MAXIMIZED        = 1 << 2,
-  GDK_TOPLEVEL_STATE_STICKY           = 1 << 3,
-  GDK_TOPLEVEL_STATE_FULLSCREEN       = 1 << 4,
-  GDK_TOPLEVEL_STATE_ABOVE            = 1 << 5,
-  GDK_TOPLEVEL_STATE_BELOW            = 1 << 6,
-  GDK_TOPLEVEL_STATE_FOCUSED          = 1 << 7,
-  GDK_TOPLEVEL_STATE_TILED            = 1 << 8,
-  GDK_TOPLEVEL_STATE_TOP_TILED        = 1 << 9,
-  GDK_TOPLEVEL_STATE_TOP_RESIZABLE    = 1 << 10,
-  GDK_TOPLEVEL_STATE_RIGHT_TILED      = 1 << 11,
-  GDK_TOPLEVEL_STATE_RIGHT_RESIZABLE  = 1 << 12,
-  GDK_TOPLEVEL_STATE_BOTTOM_TILED     = 1 << 13,
-  GDK_TOPLEVEL_STATE_BOTTOM_RESIZABLE = 1 << 14,
-  GDK_TOPLEVEL_STATE_LEFT_TILED       = 1 << 15,
-  GDK_TOPLEVEL_STATE_LEFT_RESIZABLE   = 1 << 16
+  GDK_TOPLEVEL_STATE_MINIMIZED        = 1 << 0,
+  GDK_TOPLEVEL_STATE_MAXIMIZED        = 1 << 1,
+  GDK_TOPLEVEL_STATE_STICKY           = 1 << 2,
+  GDK_TOPLEVEL_STATE_FULLSCREEN       = 1 << 3,
+  GDK_TOPLEVEL_STATE_ABOVE            = 1 << 4,
+  GDK_TOPLEVEL_STATE_BELOW            = 1 << 5,
+  GDK_TOPLEVEL_STATE_FOCUSED          = 1 << 6,
+  GDK_TOPLEVEL_STATE_TILED            = 1 << 7,
+  GDK_TOPLEVEL_STATE_TOP_TILED        = 1 << 8,
+  GDK_TOPLEVEL_STATE_TOP_RESIZABLE    = 1 << 9,
+  GDK_TOPLEVEL_STATE_RIGHT_TILED      = 1 << 10,
+  GDK_TOPLEVEL_STATE_RIGHT_RESIZABLE  = 1 << 11,
+  GDK_TOPLEVEL_STATE_BOTTOM_TILED     = 1 << 12,
+  GDK_TOPLEVEL_STATE_BOTTOM_RESIZABLE = 1 << 13,
+  GDK_TOPLEVEL_STATE_LEFT_TILED       = 1 << 14,
+  GDK_TOPLEVEL_STATE_LEFT_RESIZABLE   = 1 << 15
 } GdkToplevelState;
 
 

gdk/macos/gdkmacossurface.c
@@ -762,7 +762,7 @@ _gdk_macos_surface_show (GdkMacosSurface *self)
   was_mapped = GDK_SURFACE_IS_MAPPED (GDK_SURFACE (self));
 
   if (!was_mapped)
-    gdk_synthesize_surface_state (GDK_SURFACE (self), GDK_TOPLEVEL_STATE_WITHDRAWN, 0);
+    gdk_surface_set_is_mapped (GDK_SURFACE (self), TRUE);
 
   _gdk_macos_display_clear_sorting (GDK_MACOS_DISPLAY (GDK_SURFACE (self)->display));
 

gdk/wayland/gdksurface-wayland.c
@@ -1602,7 +1602,7 @@ maybe_notify_mapped (GdkSurface *surface)
     return;
 
   if (!GDK_SURFACE_IS_MAPPED (surface))
-    gdk_surface_queue_state_change (surface, GDK_TOPLEVEL_STATE_WITHDRAWN, 0);
+    gdk_surface_set_is_mapped (surface, TRUE);
 }
 
 static void

gdk/win32/gdkevents-win32.c
@@ -2825,11 +2825,6 @@ gdk_event_translate (MSG *msg,
 	  set_bits = 0;
 	  unset_bits = 0;
 
-	  if (IsWindowVisible (msg->hwnd))
-	    unset_bits |= GDK_TOPLEVEL_STATE_WITHDRAWN;
-	  else
-	    set_bits |= GDK_TOPLEVEL_STATE_WITHDRAWN;
-
 	  if (IsIconic (msg->hwnd))
 	    set_bits |= GDK_TOPLEVEL_STATE_MINIMIZED;
 	  else
@@ -2840,6 +2835,7 @@ gdk_event_translate (MSG *msg,
 	  else
 	    unset_bits |= GDK_TOPLEVEL_STATE_MAXIMIZED;
 
+	  gdk_surface_set_is_mapped (window, !!IsWindowVisible (msg->hwnd));
 	  gdk_synthesize_surface_state (window, unset_bits, set_bits);
 
 	  new_state = window->state;

commit 3dc2ab517f3adc667acad1a22f10f4c7b2798431

surface: Remove stray g_object_notify()
This was previously used to notify about the WITHDRAWN state but that
state doesn't exist since 4.0.0

Fixes: d2c95a1b13ee710fcfc69a592be669bcea4b26e8
Part-of: <https://gitlab.gnome.org/GNOME/gtk/-/merge_requests/9943>

gdk/gdksurface.c
@@ -1043,8 +1043,6 @@ _gdk_surface_destroy_hierarchy (GdkSurface *surface,
 
   g_object_ref (surface);
 
-  if (GDK_IS_TOPLEVEL (surface))
-    g_object_notify (G_OBJECT (surface), "state");
   g_object_notify_by_pspec (G_OBJECT (surface), properties[PROP_MAPPED]);
 
   if (surface->gl_paint_context)