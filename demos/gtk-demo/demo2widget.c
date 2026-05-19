GNOME/gtk

commit 52c00c65c34b12515b9536a30fb01a806f548c3e

Use named enum values instead of 0
Some of these enums were introduced in later versions of glib/gtk.

Part-of: <https://gitlab.gnome.org/GNOME/gtk/-/merge_requests/9790>

demos/gtk-demo/demo2widget.c
@@ -174,19 +174,19 @@ demo2_widget_class_init (Demo2WidgetClass *class)
   gtk_widget_class_install_action (widget_class, "stop", NULL, stop_animation);
 
   gtk_widget_class_add_binding_action (widget_class,
-                                       GDK_KEY_Left, 0,
+                                       GDK_KEY_Left, GDK_NO_MODIFIER_MASK,
                                        "rotate",
                                        "(ii)", GTK_ORIENTATION_HORIZONTAL, -1);
   gtk_widget_class_add_binding_action (widget_class,
-                                       GDK_KEY_Right, 0,
+                                       GDK_KEY_Right, GDK_NO_MODIFIER_MASK,
                                        "rotate",
                                        "(ii)", GTK_ORIENTATION_HORIZONTAL, 1);
   gtk_widget_class_add_binding_action (widget_class,
-                                       GDK_KEY_Up, 0,
+                                       GDK_KEY_Up, GDK_NO_MODIFIER_MASK,
                                        "rotate",
                                        "(ii)", GTK_ORIENTATION_VERTICAL, 1);
   gtk_widget_class_add_binding_action (widget_class,
-                                       GDK_KEY_Down, 0,
+                                       GDK_KEY_Down, GDK_NO_MODIFIER_MASK,
                                        "rotate",
                                        "(ii)", GTK_ORIENTATION_VERTICAL, -1);
   gtk_widget_class_add_binding_action (widget_class, GDK_KEY_Escape, 0, "stop", NULL);

demos/gtk-demo/demoimage.c
@@ -36,7 +36,7 @@ get_image_paintable (GtkImage *image)
                                          NULL,
                                          48, 1,
                                          gtk_widget_get_direction (GTK_WIDGET (image)),
-                                         0);
+                                         GTK_ICON_LOOKUP_NONE);
       if (icon == NULL)
         return NULL;
       return GDK_PAINTABLE (icon);

demos/gtk-demo/hypertext.c
@@ -105,7 +105,7 @@ show_page (GtkTextView *text_view,
                                          16,
                                          1,
                                          GTK_TEXT_DIR_LTR,
-                                         0);
+                                         GTK_ICON_LOOKUP_NONE);
       gtk_text_buffer_insert_paintable (buffer, &iter, GDK_PAINTABLE (icon));
       g_object_unref (icon);
       gtk_text_buffer_insert (buffer, &iter, ", or even widgets ", -1);

demos/gtk-demo/paintable_emblem.c
@@ -105,7 +105,7 @@ demo_icon_new_with_paintable (const char   *icon_name,
   icon = gtk_icon_theme_lookup_icon (theme,
                                      icon_name, NULL,
                                      128, 1,
-                                     GTK_TEXT_DIR_LTR, 0);
+                                     GTK_TEXT_DIR_LTR, GTK_ICON_LOOKUP_NONE);
 
   self = g_object_new (DEMO_TYPE_ICON, NULL);
 
@@ -135,7 +135,7 @@ demo_icon_new (const char *icon_name,
   emblem = gtk_icon_theme_lookup_icon (theme,
                                        emblem_name, NULL,
                                        128, 1,
-                                       GTK_TEXT_DIR_LTR, 0);
+                                       GTK_TEXT_DIR_LTR, GTK_ICON_LOOKUP_NONE);
 
   return GDK_PAINTABLE (demo_icon_new_with_paintable (icon_name,
                                                       GDK_PAINTABLE (emblem)));

demos/gtk-demo/pickers.c
@@ -40,7 +40,10 @@ set_file (GFile    *file,
   gtk_widget_set_sensitive (open_folder_button, TRUE);
   g_object_set_data_full (G_OBJECT (open_folder_button), "file", g_object_ref (file), g_object_unref);
 
-  info = g_file_query_info (file, "standard::content-type", 0, NULL, NULL);
+  info = g_file_query_info (file, "standard::content-type",
+                            G_FILE_QUERY_INFO_NONE,
+                            NULL,
+                            NULL);
   if (strcmp (g_file_info_get_content_type (info), "application/pdf") == 0)
     {
       gtk_widget_set_sensitive (print_button, TRUE);

demos/gtk-demo/textview.c
@@ -142,7 +142,7 @@ insert_text (GtkTextView *view)
                                      NULL,
                                      32, 1,
                                      gtk_widget_get_direction (widget),
-                                     0);
+                                     GTK_ICON_LOOKUP_NONE);
   nuclear = gtk_nuclear_animation_new (TRUE);
 
   /* get start of buffer; each insertion will revalidate the

demos/print-editor/print-editor.c
@@ -114,7 +114,10 @@ load_file (GFile *open_filename)
 	}
       else
 	{
-          GFileInfo *info = g_file_query_info (open_filename, "standard::display-name", 0, NULL, &error);
+          GFileInfo *info = g_file_query_info (open_filename, "standard::display-name",
+                                               G_FILE_QUERY_INFO_NONE,
+                                               NULL,
+                                               &error);
           const char *display_name = g_file_info_get_display_name (info);
           GtkAlertDialog *alert;
 
@@ -127,7 +130,10 @@ load_file (GFile *open_filename)
     }
   else
     {
-      GFileInfo *info = g_file_query_info (open_filename, "standard::display-name", 0, NULL, &error);
+      GFileInfo *info = g_file_query_info (open_filename, "standard::display-name",
+                                           G_FILE_QUERY_INFO_NONE,
+                                           NULL,
+                                           &error);
       const char *display_name = g_file_info_get_display_name (info);
       GtkAlertDialog *alert;
 
@@ -168,7 +174,10 @@ save_file (GFile *save_filename)
     }
   else
     {
-      GFileInfo *info = g_file_query_info (save_filename, "standard::display-name", 0, NULL, NULL);
+      GFileInfo *info = g_file_query_info (save_filename, "standard::display-name",
+                                           G_FILE_QUERY_INFO_NONE,
+                                           NULL,
+                                           NULL);
       const char *display_name = g_file_info_get_display_name (info);
       GtkAlertDialog *alert;
 

examples/action-namespace.c
@@ -119,7 +119,7 @@ main(int    argc,
 {
   GtkApplication *app;
 
-  app = gtk_application_new ("org.gtk.Example", 0);
+  app = gtk_application_new ("org.gtk.Example", G_APPLICATION_DEFAULT_FLAGS);
   g_signal_connect (app, "activate", G_CALLBACK (activate), NULL);
 
   return g_application_run (G_APPLICATION (app), argc, argv);

commit c73b646c3b1e338c908fcf2a7e9ec9ee41f427aa

gtk-demo: Use named enum instead of 0
Fixes: 52c00c65c34b12515b9536a30fb01a806f548c3e

demos/gtk-demo/demo2widget.c
@@ -189,7 +189,9 @@ demo2_widget_class_init (Demo2WidgetClass *class)
                                        GDK_KEY_Down, GDK_NO_MODIFIER_MASK,
                                        "rotate",
                                        "(ii)", GTK_ORIENTATION_VERTICAL, -1);
-  gtk_widget_class_add_binding_action (widget_class, GDK_KEY_Escape, 0, "stop", NULL);
+  gtk_widget_class_add_binding_action (widget_class,
+                                       GDK_KEY_Escape, GDK_NO_MODIFIER_MASK,
+                                       "stop", NULL);
 
   /* here is where we use our custom layout manager */
   gtk_widget_class_set_layout_manager_type (widget_class, DEMO2_TYPE_LAYOUT);