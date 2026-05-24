eb16a3acfc516348238899d56bbef041ae442b70
Benjamin Otte <otte@redhat.com>
2026-05-24 18:08:02 +0200

    testsuite: Stop random dmabuf conversion in compare tests
    While this was a good idea in the past when we didn't have a way to
    create dmabuf textures otherwise, we now can and do do that in our
    tests, so the disadvantages of this approach now outweigh the benefits.
    
    One of the largest disadvantages (larger than unexpected behavior of
    the replay tool) is that we now have autogenerated tests with tons
    of texture nodes that reuse the same few textures (like the soon to
    be added texture-all-snaps.node which draws the same texture over 20,000
    times) and randomly turning them into dmabufs is not just slowing things
    down a lot, it's also exhausting fds quite quickly.
    
    This reverts commit 107ef2017c9bea6a1b1ff4d067a49e8e95332475.
    
    Part-of: <https://gitlab.gnome.org/GNOME/gtk/-/merge_requests/9971>

--- testsuite/gsk/replay-node.c
+++ testsuite/gsk/replay-node.c
@@ -12,7 +12,6 @@
 #include <gsk/gskrendernodeprivate.h>
 #include <gsk/gskrepeatnodeprivate.h>
 #include <gtk/gtksnapshotprivate.h>
-#include "../gdk/udmabuf.h"
 
 void
 replay_node (GskRenderNode *node, GtkSnapshot *snapshot);
@@ -109,23 +108,7 @@ replay_texture_node (GskRenderNode *node, GtkSnapshot *snapshot)
   graphene_rect_t bounds;
   gsk_render_node_get_bounds (node, &bounds);
 
-  g_object_ref (texture);
-
-  if (g_test_rand_bit ())
-    {
-      GdkTexture *t;
-
-      t = udmabuf_texture_from_texture (texture, NULL);
-      if (t)
-        {
-          g_object_unref (texture);
-          texture = t;
-        }
-    }
-
   gtk_snapshot_append_texture (snapshot, texture, &bounds);
-
-  g_object_unref (texture);
 }
 
 static void
@@ -349,30 +332,7 @@ replay_texture_scale_node (GskRenderNode *node, GtkSnapshot *snapshot)
   graphene_rect_t bounds;
   gsk_render_node_get_bounds (node, &bounds);
 
-  g_object_ref (texture);
-
-  if (g_test_rand_bit ())
-    {
-      GdkTexture *t;
-      GError *error = NULL;
-
-      t = udmabuf_texture_from_texture (texture, &error);
-      if (t)
-        {
-          g_test_message ("Using dmabuf texture");
-          g_object_unref (texture);
-          texture = t;
-        }
-      else
-        {
-          g_test_message ("Creating dmabuf texture failed: %s", error->message);
-          g_error_free (error);
-        }
-    }
-
   gtk_snapshot_append_scaled_texture (snapshot, texture, filter, &bounds);
-
-  g_object_unref (texture);
 }
 
 static void
