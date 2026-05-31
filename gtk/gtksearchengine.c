GNOME/gtk

commit 8cfbab2344881ef98f7fbeded6d4949d93da0f02

gtk: Add missing G_DEFINE_AUTOPTR_CLEANUP_FUNC()
Thanks gobject-linter

Part-of: <https://gitlab.gnome.org/GNOME/gtk/-/merge_requests/9952>

gtk/gtksearchengineprivate.h
@@ -39,8 +39,6 @@ typedef struct _GtkSearchEngineClass GtkSearchEngineClass;
 typedef struct _GtkSearchEnginePrivate GtkSearchEnginePrivate;
 typedef struct _GtkSearchHit GtkSearchHit;
 
-G_DEFINE_AUTOPTR_CLEANUP_FUNC (GtkSearchEngine, g_object_unref)
-
 struct _GtkSearchHit
 {
   GFile *file;

gtk/css/gtkcsssection.h
@@ -75,5 +75,7 @@ GDK_AVAILABLE_IN_ALL
 const GtkCssLocation *
                    gtk_css_section_get_end_location    (const GtkCssSection  *section);
 
+G_DEFINE_AUTOPTR_CLEANUP_FUNC(GtkCssSection, gtk_css_section_unref)
+
 G_END_DECLS
 

gtk/deprecated/gtkassistant.h
@@ -182,6 +182,7 @@ GDK_DEPRECATED_IN_4_10
 GListModel *          gtk_assistant_get_pages (GtkAssistant *assistant);
 
 G_DEFINE_AUTOPTR_CLEANUP_FUNC(GtkAssistant, g_object_unref)
+G_DEFINE_AUTOPTR_CLEANUP_FUNC(GtkAssistantPage, g_object_unref)
 
 G_END_DECLS
 

gtk/deprecated/gtkfilechooser.h
@@ -186,5 +186,7 @@ GDK_DEPRECATED_IN_4_10
 const char *         gtk_file_chooser_get_choice           (GtkFileChooser      *chooser,
                                                             const char          *id);
 
+G_DEFINE_AUTOPTR_CLEANUP_FUNC(GtkFileChooser, g_object_unref)
+
 G_END_DECLS
 

gtk/deprecated/gtkshortcutlabel.h
@@ -50,5 +50,7 @@ GDK_AVAILABLE_IN_ALL
 void         gtk_shortcut_label_set_disabled_text (GtkShortcutLabel *self,
                                                    const char       *disabled_text);
 
+G_DEFINE_AUTOPTR_CLEANUP_FUNC(GtkShortcutLabel, g_object_unref)
+
 G_END_DECLS
 

gtk/deprecated/gtkshortcutsgroup.h
@@ -41,5 +41,7 @@ GDK_DEPRECATED_IN_4_18
 void gtk_shortcuts_group_add_shortcut (GtkShortcutsGroup    *self,
                                        GtkShortcutsShortcut *shortcut);
 
+G_DEFINE_AUTOPTR_CLEANUP_FUNC(GtkShortcutsGroup, g_object_unref)
+
 G_END_DECLS
 

gtk/deprecated/gtkshortcutssection.h
@@ -41,5 +41,7 @@ GDK_DEPRECATED_IN_4_18
 void gtk_shortcuts_section_add_group (GtkShortcutsSection *self,
                                       GtkShortcutsGroup   *group);
 
+G_DEFINE_AUTOPTR_CLEANUP_FUNC(GtkShortcutsSection, g_object_unref)
+
 G_END_DECLS
 

gtk/deprecated/gtkshortcutsshortcut.h
@@ -79,5 +79,7 @@ typedef enum {
 GDK_AVAILABLE_IN_ALL
 GType        gtk_shortcuts_shortcut_get_type (void) G_GNUC_CONST;
 
+G_DEFINE_AUTOPTR_CLEANUP_FUNC(GtkShortcutsShortcut, g_object_unref)
+
 G_END_DECLS
 

commit 09433950d6c4f7d7c906af29cd47e9e6cf19a2dc

searchengine: Fix the build
The build with tracker broke during the recent goblint cleanups,
and nobody noticed. Take this opportunity to clean up the
GObject boilerplate a bit and use a DECLARE macro.

Fixes: 8cfbab2344881ef98f
Part-of: <https://gitlab.gnome.org/GNOME/gtk/-/merge_requests/9995>

gtk/gtksearchengine.c
@@ -59,7 +59,7 @@ enum
 
 static guint signals[LAST_SIGNAL];
 
-G_DEFINE_TYPE_WITH_PRIVATE (GtkSearchEngine, _gtk_search_engine, G_TYPE_OBJECT);
+G_DEFINE_TYPE_WITH_PRIVATE (GtkSearchEngine, gtk_search_engine, G_TYPE_OBJECT);
 
 static void
 set_query (GtkSearchEngine *engine,
@@ -131,11 +131,11 @@ finalize (GObject *object)
 
   g_clear_object (&engine->priv->query);
 
-  G_OBJECT_CLASS (_gtk_search_engine_parent_class)->finalize (object);
+  G_OBJECT_CLASS (gtk_search_engine_parent_class)->finalize (object);
 }
 
 static void
-_gtk_search_engine_class_init (GtkSearchEngineClass *class)
+gtk_search_engine_class_init (GtkSearchEngineClass *class)
 {
   GObjectClass *object_class = G_OBJECT_CLASS (class);
 
@@ -176,9 +176,9 @@ _gtk_search_engine_class_init (GtkSearchEngineClass *class)
 }
 
 static void
-_gtk_search_engine_init (GtkSearchEngine *engine)
+gtk_search_engine_init (GtkSearchEngine *engine)
 {
-  engine->priv = _gtk_search_engine_get_instance_private (engine);
+  engine->priv = gtk_search_engine_get_instance_private (engine);
 }
 
 static void

gtk/gtksearchengineprivate.h
@@ -23,19 +23,15 @@
 
 #include "gtkqueryprivate.h"
 #include "gtkfilesystemmodelprivate.h"
+#include "gdktypes.h"
 #include <gio/gio.h>
 
 G_BEGIN_DECLS
 
-#define GTK_TYPE_SEARCH_ENGINE		(_gtk_search_engine_get_type ())
-#define GTK_SEARCH_ENGINE(obj)		(G_TYPE_CHECK_INSTANCE_CAST ((obj), GTK_TYPE_SEARCH_ENGINE, GtkSearchEngine))
-#define GTK_SEARCH_ENGINE_CLASS(klass)	(G_TYPE_CHECK_CLASS_CAST ((klass), GTK_TYPE_SEARCH_ENGINE, GtkSearchEngineClass))
-#define GTK_IS_SEARCH_ENGINE(obj)		(G_TYPE_CHECK_INSTANCE_TYPE ((obj), GTK_TYPE_SEARCH_ENGINE))
-#define GTK_IS_SEARCH_ENGINE_CLASS(klass)	(G_TYPE_CHECK_CLASS_TYPE ((klass), GTK_TYPE_SEARCH_ENGINE))
-#define GTK_SEARCH_ENGINE_GET_CLASS(obj)    (G_TYPE_INSTANCE_GET_CLASS ((obj), GTK_TYPE_SEARCH_ENGINE, GtkSearchEngineClass))
+#define GTK_TYPE_SEARCH_ENGINE (gtk_search_engine_get_type ())
 
-typedef struct _GtkSearchEngine GtkSearchEngine;
-typedef struct _GtkSearchEngineClass GtkSearchEngineClass;
+GDK_DECLARE_INTERNAL_TYPE (GtkSearchEngine, gtk_search_engine, GTK, SEARCH_ENGINE, GObject)
+
 typedef struct _GtkSearchEnginePrivate GtkSearchEnginePrivate;
 typedef struct _GtkSearchHit GtkSearchHit;
 
@@ -52,10 +48,10 @@ struct _GtkSearchEngine
   GtkSearchEnginePrivate *priv;
 };
 
-struct _GtkSearchEngineClass 
+struct _GtkSearchEngineClass
 {
   GObjectClass parent_class;
-  
+
   /* VTable */
   void     (*set_query)       (GtkSearchEngine *engine, 
 			       GtkQuery        *query);
@@ -69,8 +65,6 @@ struct _GtkSearchEngineClass
   void     (*error)           (GtkSearchEngine *engine, 
 			       const char      *error_message);
 };
-
-GType            _gtk_search_engine_get_type        (void);
 
 GtkSearchEngine* _gtk_search_engine_new             (void);
 
@@ -93,4 +87,3 @@ void             _gtk_search_engine_set_model       (GtkSearchEngine    *engine,
                                                      GtkFileSystemModel *model);
 
 G_END_DECLS
-