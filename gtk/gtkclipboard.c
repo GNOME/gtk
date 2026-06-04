9043fcd60b2f62917c8d86fdda748f7d988d854a
Vlad Zahorodnii <vlad.zahorodnii@kde.org>
2026-06-04 14:39:24 +0300

    Add SAVE_TARGETS clipboard target only on X11 and Win32
    This prevents GDK from setting the selection twice per Ctrl-C on
    Wayland. One without the SAVE_TARGETS target, another with the
    SAVE_TARGETS target.
    
    Also the relevant code in gtk_clipboard_real_store() is already guarded
    by gdk_display_supports_clipboard_persistence().
    
    Part-of: <https://gitlab.gnome.org/GNOME/gtk/-/merge_requests/10008>

--- gtk/gtkclipboard.c
+++ gtk/gtkclipboard.c
@@ -2088,6 +2088,9 @@ gtk_clipboard_real_set_can_store (GtkClipboard         *clipboard,
   if (clipboard->selection != GDK_SELECTION_CLIPBOARD)
     return;
   
+  if (!gdk_display_supports_clipboard_persistence (gtk_clipboard_get_display (clipboard)))
+    return;
+
   g_free (clipboard->storable_targets);
   
   clipboard_widget = get_clipboard_widget (clipboard->display);
