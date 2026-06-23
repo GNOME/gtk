GNOME/gtk

commit f8f70094ef7386ac1142141d2ed3c2e6f590d66c

revealer: Respect the reduced motion setting
Turn every animation into a crossfade if reduced motion
is requested.

Part-of: <https://gitlab.gnome.org/GNOME/gtk/-/merge_requests/10070>

gtk/gtkrevealer.c
@@ -42,7 +42,7 @@
  * [method@Gtk.Revealer.set_transition_type].
  *
  * These animations respect the [property@Gtk.Settings:gtk-enable-animations]
- * setting.
+ * and [property@GTk.Settings:gtk-interface-reduced-motion] settings.
  *
  * # CSS nodes
  *
@@ -121,6 +121,18 @@ static GtkBuildableIface *parent_buildable_iface;
 static GtkRevealerTransitionType
 effective_transition (GtkRevealer *revealer)
 {
+  if (revealer->transition_type != GTK_REVEALER_TRANSITION_TYPE_NONE)
+    {
+      GtkReducedMotion reduced_motion;
+
+      g_object_get (gtk_widget_get_settings (GTK_WIDGET (revealer)),
+                    "gtk-interface-reduced-motion", &reduced_motion,
+                    NULL);
+
+      if (reduced_motion == GTK_REDUCED_MOTION_REDUCE)
+        return GTK_REVEALER_TRANSITION_TYPE_CROSSFADE;
+    }
+
   if (gtk_widget_get_direction (GTK_WIDGET (revealer)) == GTK_TEXT_DIR_RTL)
     {
       if (revealer->transition_type == GTK_REVEALER_TRANSITION_TYPE_SLIDE_LEFT)
@@ -640,7 +652,7 @@ gtk_revealer_snapshot (GtkWidget   *widget,
   gboolean is_fade;
 
   if (animation_running)
-    is_fade = get_is_fading_type (revealer->transition_type);
+    is_fade = get_is_fading_type (effective_transition (revealer));
   else
     is_fade = FALSE;
 

commit 988f6b46741246c035a03f7abcf6fa9d1407ae33

revealer: Better reduced motion behavior
The first attempt in f8f70094ef7386ac1 had a thinko:
crossfading does not make the revealer give up its space,
which is what we want in almost all cases. So instead
of forcing cross-fade, force a none transition if
reduced motion is requested, while leaving cross-fade
unchanged.

Fixes: f8f70094ef7386ac1
Closes: #8278
Part-of: <https://gitlab.gnome.org/GNOME/gtk/-/merge_requests/10077>

gtk/gtkrevealer.c
@@ -121,7 +121,8 @@ static GtkBuildableIface *parent_buildable_iface;
 static GtkRevealerTransitionType
 effective_transition (GtkRevealer *revealer)
 {
-  if (revealer->transition_type != GTK_REVEALER_TRANSITION_TYPE_NONE)
+  if (revealer->transition_type != GTK_REVEALER_TRANSITION_TYPE_NONE &&
+      revealer->transition_type != GTK_REVEALER_TRANSITION_TYPE_CROSSFADE)
     {
       GtkReducedMotion reduced_motion;
 
@@ -130,7 +131,7 @@ effective_transition (GtkRevealer *revealer)
                     NULL);
 
       if (reduced_motion == GTK_REDUCED_MOTION_REDUCE)
-        return GTK_REVEALER_TRANSITION_TYPE_CROSSFADE;
+        return GTK_REVEALER_TRANSITION_TYPE_NONE;
     }
 
   if (gtk_widget_get_direction (GTK_WIDGET (revealer)) == GTK_TEXT_DIR_RTL)