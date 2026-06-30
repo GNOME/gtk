GNOME/gtk

commit 05a4921ccf650b7e399a8433c4cb9a1cf24153c7

gtk/imcontextquartz: Track widget position changes
Connect to the widget frame clock's after-paint phase to
update the cursor position relative to the surface, if the
widget coordinates were to change. The last effective call
to gtk_im_context_set_cursor_location() will apply for the
widget-local coordinates.

This allows the IM candidates window to properly follow
e.g. emerging entries in a search bar.

Part-of: <https://gitlab.gnome.org/GNOME/gtk/-/merge_requests/9958>

gtk/gtkimcontextquartz.c
@@ -46,7 +46,9 @@ typedef struct _GtkIMContextQuartz
   unsigned int cursor_index;
   unsigned int selected_len;
   GdkRectangle *cursor_rect;
+  GdkRectangle *surface_cursor_rect;
   gboolean focused;
+  gulong after_layout_id;
 } GtkIMContextQuartz;
 
 typedef struct _GtkIMContextQuartzClass
@@ -75,6 +77,73 @@ surface_from_widget (GtkIMContext *context)
     return NULL;
 
   return gtk_native_get_surface (native);
+}
+
+static void
+after_layout_cb (GtkIMContext *context)
+{
+  GtkIMContextQuartz *qc = GTK_IM_CONTEXT_QUARTZ (context);
+  cairo_rectangle_int_t rect;
+  GdkSurface *surface;
+  GtkNative *native;
+  graphene_point_t p;
+  double nx, ny;
+
+  surface = surface_from_widget (context);
+  if (!GDK_IS_MACOS_SURFACE (surface))
+    return;
+
+  native = GTK_NATIVE (gtk_widget_get_root (qc->client_widget));
+  if (!native)
+    return;
+
+  /* Compute surface-local coordinates */
+  rect = *qc->cursor_rect;
+
+  if (!gtk_widget_compute_point (qc->client_widget,
+                                 GTK_WIDGET (native),
+                                 &GRAPHENE_POINT_INIT (rect.x, rect.y),
+                                 &p))
+    graphene_point_init (&p, rect.x, rect.y);
+
+  gtk_native_get_surface_transform (native, &nx, &ny);
+  rect.x = p.x + nx;
+  rect.y = p.y + ny;
+
+  if (qc->surface_cursor_rect->x != rect.x ||
+      qc->surface_cursor_rect->y != rect.y ||
+      qc->surface_cursor_rect->width != rect.width ||
+      qc->surface_cursor_rect->height != rect.height)
+    {
+      *qc->surface_cursor_rect = rect;
+      g_object_set_data (G_OBJECT (surface), GIC_CURSOR_RECT, qc->cursor_rect);
+    }
+}
+
+static void
+on_widget_realize (GtkWidget          *widget,
+                   GtkIMContextQuartz *qc)
+{
+  GdkFrameClock *frame_clock;
+
+  frame_clock = gtk_widget_get_frame_clock (widget);
+  if (frame_clock)
+    {
+      qc->after_layout_id =
+        g_signal_connect_object (frame_clock, "layout",
+                                 G_CALLBACK (after_layout_cb), qc,
+                                 G_CONNECT_AFTER | G_CONNECT_SWAPPED);
+    }
+}
+
+static void
+on_widget_unrealize (GtkWidget          *widget,
+                     GtkIMContextQuartz *qc)
+{
+  GdkFrameClock *frame_clock;
+
+  frame_clock = gtk_widget_get_frame_clock (qc->client_widget);
+  g_clear_signal_handler (&qc->after_layout_id, frame_clock);
 }
 
 static void
@@ -287,7 +356,32 @@ quartz_set_client_widget (GtkIMContext *context,
 
   GTK_DEBUG (MODULES, "quartz_set_client_widget: %p", widget);
 
+  if (qc->client_widget)
+    {
+      on_widget_unrealize (widget, qc);
+      g_signal_handlers_disconnect_by_func (qc->client_widget,
+                                            on_widget_realize,
+                                            context);
+      g_signal_handlers_disconnect_by_func (qc->client_widget,
+                                            on_widget_unrealize,
+                                            context);
+
+      g_clear_weak_pointer (&qc->client_widget);
+    }
+
   qc->client_widget = widget;
+
+  if (widget)
+    {
+      g_object_add_weak_pointer (G_OBJECT (qc->client_widget),
+                                 (gpointer*) &qc->client_widget);
+
+      g_signal_connect (qc->client_widget, "realize",
+                        G_CALLBACK (on_widget_realize), context);
+      g_signal_connect (qc->client_widget, "unrealize",
+                        G_CALLBACK (on_widget_unrealize), context);
+      on_widget_realize (widget, qc);
+    }
 }
 
 static void
@@ -315,42 +409,19 @@ static void
 quartz_set_cursor_location (GtkIMContext *context, GdkRectangle *area)
 {
   GtkIMContextQuartz *qc = GTK_IM_CONTEXT_QUARTZ (context);
-  GtkWidget* surface_widget;
-  GdkSurface *client_surface;
-  int sx, sy;
-  graphene_point_t p;
 
   GTK_DEBUG (MODULES, "quartz_set_cursor_location");
 
-  if (!qc->client_widget)
-    return;
+  *qc->cursor_rect = *area;
 
-  client_surface = surface_from_widget (context);
-  if (!client_surface)
-    return;
+  if (qc->client_widget)
+    {
+      GdkFrameClock *frame_clock;
 
-  if (!qc->focused)
-    return;
-
-  surface_widget = GTK_WIDGET (gtk_widget_get_native (qc->client_widget));
-
-  if (!surface_widget)
-    return;
-
-  gdk_surface_get_origin (client_surface, &sx, &sy);
-  if (!gtk_widget_compute_point (qc->client_widget, surface_widget,
-                                 &GRAPHENE_POINT_INIT (area->x, area->y), &p))
-    graphene_point_init (&p, area->x, area->y);
-
-  qc->cursor_rect->x = sx + (int) p.x;
-  qc->cursor_rect->y = sy + (int) p.y;
-  qc->cursor_rect->width = area->width;
-  qc->cursor_rect->height = area->height;
-
-  if (!GDK_IS_MACOS_SURFACE (client_surface))
-    return;
-
-  g_object_set_data (G_OBJECT (client_surface), GIC_CURSOR_RECT, qc->cursor_rect);
+      frame_clock = gtk_widget_get_frame_clock (qc->client_widget);
+      if (frame_clock)
+        gdk_frame_clock_request_phase (frame_clock, GDK_FRAME_CLOCK_PHASE_LAYOUT);
+    }
 }
 
 static void
@@ -373,6 +444,9 @@ imquartz_finalize (GObject *obj)
   GtkIMContextQuartz *qc = GTK_IM_CONTEXT_QUARTZ (obj);
   g_clear_pointer (&qc->preedit_str, g_free);
   g_clear_pointer (&qc->cursor_rect, g_free);
+  g_clear_pointer (&qc->surface_cursor_rect, g_free);
+
+  quartz_set_client_widget (GTK_IM_CONTEXT (qc), NULL);
 
   g_signal_handlers_disconnect_by_func (qc->helper, (gpointer)commit_cb, qc);
   g_object_unref (qc->helper);
@@ -409,6 +483,7 @@ gtk_im_context_quartz_init (GtkIMContextQuartz *qc)
   qc->cursor_index = 0;
   qc->selected_len = 0;
   qc->cursor_rect = g_new (GdkRectangle, 1);
+  qc->surface_cursor_rect = g_new (GdkRectangle, 1);
   qc->focused = FALSE;
 
   qc->helper = g_object_new (GTK_TYPE_IM_CONTEXT_SIMPLE, NULL);

commit 22ee2c01abd92727b651b2faa6512c26610cffa6

gtkimcontextquartz: Convert (again) IM cursor location to global coords
The changes to push IM cursor location updates following the frame clock
missed the conversion to global coordinate space through
gdk_surface_get_origin(), add it back to fix IM cursor location.

Fixes: 05a4921ccf ("gtk/imcontextquartz: Track widget position changes")
Part-of: <https://gitlab.gnome.org/GNOME/gtk/-/merge_requests/10103>

gtk/gtkimcontextquartz.c
@@ -87,7 +87,8 @@ after_layout_cb (GtkIMContext *context)
   GdkSurface *surface;
   GtkNative *native;
   graphene_point_t p;
-  double nx, ny;
+  double nx = 0, ny = 0;
+  int sx, sy;
 
   surface = surface_from_widget (context);
   if (!GDK_IS_MACOS_SURFACE (surface))
@@ -106,9 +107,10 @@ after_layout_cb (GtkIMContext *context)
                                  &p))
     graphene_point_init (&p, rect.x, rect.y);
 
+  gdk_surface_get_origin (surface, &sx, &sy);
   gtk_native_get_surface_transform (native, &nx, &ny);
-  rect.x = p.x + nx;
-  rect.y = p.y + ny;
+  rect.x = sx + p.x + nx;
+  rect.y = sy + p.y + ny;
 
   if (qc->surface_cursor_rect->x != rect.x ||
       qc->surface_cursor_rect->y != rect.y ||
@@ -116,7 +118,7 @@ after_layout_cb (GtkIMContext *context)
       qc->surface_cursor_rect->height != rect.height)
     {
       *qc->surface_cursor_rect = rect;
-      g_object_set_data (G_OBJECT (surface), GIC_CURSOR_RECT, qc->cursor_rect);
+      g_object_set_data (G_OBJECT (surface), GIC_CURSOR_RECT, qc->surface_cursor_rect);
     }
 }
 