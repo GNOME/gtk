GNOME/gtk

commit 7461e2e8b38fc2fe5d048367cb9d5159be35148a

gsk: Avoid cache explosion in 3D
When rendering paths with in 3D transformed context,
give up on trying to produce perfectly scaled and subpixel
aligned masks, since the difference is not going to be
noticeable and prefer cache reuse over precision.

This keeps the fill cache of the 'icons on a sphere' demo
under 2000 items (before it was blowing up to 90000 items),
and gets this demo back to 60fps.

This commit covers the fill and stroke cache.
Other caches may or may not have similar issues.

gsk/gpu/gskgpucachedfill.c
@@ -10,15 +10,13 @@
 #include "gskgpuimageprivate.h"
 #include "gskpathprivate.h"
 #include "gskrectprivate.h"
+#include "gsktransformprivate.h"
 
 #include "gdk/gdkcolorstateprivate.h"
 #include "gdk/gdkcolorprivate.h"
 #include "gdk/gdkcairoprivate.h"
 
 #include "gsk/gskprivate.h"
-
-#define SUBPIXEL_SCALE_X 32
-#define SUBPIXEL_SCALE_Y 32
 
 typedef struct _GskGpuCachedFill GskGpuCachedFill;
 
@@ -162,27 +160,49 @@ mod_subpixel (float  pos,
     return (gsize) - ceil (pos);
 }
 
+static void
+determine_scale_and_subpixel_grid (const graphene_size_t *scale,
+                                   GskTransform           *modelview,
+                                   float                  *sx,
+                                   float                  *sy,
+                                   size_t                 *subpixel_scale)
+{
+  if (gsk_transform_get_fine_category (modelview) <= GSK_FINE_TRANSFORM_CATEGORY_3D)
+    {
+      *sx = *sy = ceilf (MAX (scale->width, scale->height) + 0.5);
+      *subpixel_scale = 1;
+    }
+  else
+    {
+      *sx = scale->width;
+      *sy = scale->height;
+      *subpixel_scale = 32;
+    }
+}
+
 GskGpuImage *
 gsk_gpu_cached_fill_lookup (GskGpuCache           *self,
                             GskGpuFrame           *frame,
                             const graphene_size_t *scale,
                             const graphene_rect_t *bounds,
+                            GskTransform          *modelview,
                             GskPath               *path,
                             GskFillRule            fill_rule,
                             graphene_rect_t       *out_rect)
 {
   GskGpuCachePrivate *priv = gsk_gpu_cache_get_private (self);
-  float sx = scale->width;
-  float sy = scale->height;
-  float dx, dy;
+  float sx, sy, dx, dy;
   GskGpuCachedFill *cached;
   gsize fx, fy, padding;
   cairo_rectangle_int_t area;
   GskGpuImage *image = NULL;
   graphene_rect_t viewport;
+  size_t subpixel_scale;
 
-  fx = mod_subpixel (bounds->origin.x, sx, SUBPIXEL_SCALE_X, &dx);
-  fy = mod_subpixel (bounds->origin.y, sy, SUBPIXEL_SCALE_Y, &dy);
+  determine_scale_and_subpixel_grid (scale, modelview, &sx, &sy, &subpixel_scale);
+
+  fx = mod_subpixel (bounds->origin.x, sx, subpixel_scale, &dx);
+  fy = mod_subpixel (bounds->origin.y, sy, subpixel_scale, &dy);
 
   cached = g_hash_table_lookup (priv->fill_cache,
                                 &(GskGpuCachedFill) {
@@ -209,8 +229,8 @@ gsk_gpu_cached_fill_lookup (GskGpuCache           *self,
   if (!gsk_path_get_bounds (path, &viewport) ||
       !gsk_rect_snap_to_grid_grow (&viewport,
                                    scale,
-                                   &GRAPHENE_POINT_INIT ((float) fx / (sx * SUBPIXEL_SCALE_X), 
-                                                         (float) fy / (sy * SUBPIXEL_SCALE_Y)),
+                                   &GRAPHENE_POINT_INIT ((float) fx / (sx * subpixel_scale),
+                                                         (float) fy / (sy * subpixel_scale)),
                                    &viewport))
     return NULL;
 

gsk/gpu/gskgpucachedstroke.c
@@ -13,15 +13,13 @@
 #include "gskrectprivate.h"
 #include "gskstrokeprivate.h"
 #include "gskpathprivate.h"
+#include "gsktransformprivate.h"
 
 #include "gdk/gdkcolorstateprivate.h"
 #include "gdk/gdkcolorprivate.h"
 #include "gdk/gdkcairoprivate.h"
 
 #include "gsk/gskprivate.h"
-
-#define SUBPIXEL_SCALE_X 32
-#define SUBPIXEL_SCALE_Y 32
 
 typedef struct _GskGpuCachedStroke GskGpuCachedStroke;
 
@@ -173,27 +171,49 @@ mod_subpixel (float  pos,
     return (gsize) - ceil (pos);
 }
 
+static void
+determine_scale_and_subpixel_grid (const graphene_size_t *scale,
+                                   GskTransform           *modelview,
+                                   float                  *sx,
+                                   float                  *sy,
+                                   size_t                 *subpixel_scale)
+{
+  if (gsk_transform_get_fine_category (modelview) <= GSK_FINE_TRANSFORM_CATEGORY_3D)
+    {
+      *sx = *sy = ceilf (MAX (scale->width, scale->height) + 0.5);
+      *subpixel_scale = 1;
+    }
+  else
+    {
+      *sx = scale->width;
+      *sy = scale->height;
+      *subpixel_scale = 32;
+    }
+}
+
 GskGpuImage *
 gsk_gpu_cached_stroke_lookup (GskGpuCache           *self,
                               GskGpuFrame           *frame,
                               const graphene_size_t *scale,
                               const graphene_rect_t *bounds,
+                              GskTransform          *modelview,
                               GskPath               *path,
                               const GskStroke       *stroke,
                               graphene_rect_t       *out_rect)
 {
   GskGpuCachePrivate *priv = gsk_gpu_cache_get_private (self);
-  float sx = scale->width;
-  float sy = scale->height;
-  float dx, dy;
+  float sx, sy, dx, dy;
   GskGpuCachedStroke *cached;
   gsize fx, fy, padding;
   cairo_rectangle_int_t area;
   GskGpuImage *image = NULL;
   graphene_rect_t viewport;
+  size_t subpixel_scale;
 
-  fx = mod_subpixel (bounds->origin.x, sx, SUBPIXEL_SCALE_X, &dx);
-  fy = mod_subpixel (bounds->origin.y, sy, SUBPIXEL_SCALE_Y, &dy);
+  determine_scale_and_subpixel_grid (scale, modelview, &sx, &sy, &subpixel_scale);
+
+  fx = mod_subpixel (bounds->origin.x, sx, subpixel_scale, &dx);
+  fy = mod_subpixel (bounds->origin.y, sy, subpixel_scale, &dy);
 
   cached = g_hash_table_lookup (priv->stroke_cache,
                                 &(GskGpuCachedStroke) {
@@ -220,8 +240,8 @@ gsk_gpu_cached_stroke_lookup (GskGpuCache           *self,
   if (!gsk_path_get_stroke_bounds (path, stroke, &viewport) ||
       !gsk_rect_snap_to_grid_grow (&viewport,
                                    scale,
-                                   &GRAPHENE_POINT_INIT ((float) fx / (sx * SUBPIXEL_SCALE_X), 
-                                                         (float) fy / (sy * SUBPIXEL_SCALE_Y)),
+                                   &GRAPHENE_POINT_INIT ((float) fx / (sx * subpixel_scale),
+                                                         (float) fy / (sy * subpixel_scale)),
                                    &viewport))
     return NULL;
 

gsk/gpu/gskgpunodeprocessor.c
@@ -2931,6 +2931,7 @@ gsk_gpu_node_processor_add_fill_node (GskGpuRenderPass *self,
                                            self->frame,
                                            &self->scale,
                                            &clip_bounds,
+                                           self->modelview,
                                            gsk_fill_node_get_path (node),
                                            gsk_fill_node_get_fill_rule (node),
                                            &mask_rect);
@@ -2983,6 +2984,7 @@ gsk_gpu_node_processor_add_stroke_node (GskGpuRenderPass *self,
                                              self->frame,
                                              &self->scale,
                                              &clip_bounds,
+                                             self->modelview,
                                              gsk_stroke_node_get_path (node),
                                              gsk_stroke_node_get_stroke (node),
                                              &mask_rect);

gsk/gpu/gskgpucachedfillprivate.h
@@ -3,6 +3,7 @@
 #include "gskgpucachedprivate.h"
 
 #include "gsk/gskpath.h"
+#include "gsk/gsktransform.h"
 
 #include <graphene.h>
 
@@ -15,6 +16,7 @@ GskGpuImage *           gsk_gpu_cached_fill_lookup                      (GskGpuC
                                                                          GskGpuFrame            *frame,
                                                                          const graphene_size_t  *scale,
                                                                          const graphene_rect_t  *bounds,
+                                                                         GskTransform           *modelview,
                                                                          GskPath                *path,
                                                                          GskFillRule             fill_rule,
                                                                          graphene_rect_t        *rect);

gsk/gpu/gskgpucachedstrokeprivate.h
@@ -4,6 +4,7 @@
 
 #include "gsk/gskpath.h"
 #include "gsk/gskstroke.h"
+#include "gsk/gsktransform.h"
 
 #include <graphene.h>
 
@@ -16,6 +17,7 @@ GskGpuImage *           gsk_gpu_cached_stroke_lookup                    (GskGpuC
                                                                          GskGpuFrame            *frame,
                                                                          const graphene_size_t  *scale,
                                                                          const graphene_rect_t  *bounds,
+                                                                         GskTransform           *modelview,
                                                                          GskPath                *path,
                                                                          const GskStroke        *stroke,
                                                                          graphene_rect_t        *rect);

commit 5a4ebab458b9b7196b65ee7f81156cade58241a0

gpu: Don't scale caches too far
We would round scale factors up to the next integer. However, when
the scale factor was very small - like 1/100 in the case of the
fontforge icon - this could result in very large textures.

Instead, round up the log2 to the next integer. That way, scales
grow at most 2x in each direction.

This should still avoid many similar texture entries for similar
scales, which was the original goal of this code.

Tests included.

Fixes: 7461e2e8b38fc2fe5d048367cb9d5159be35148a

gsk/gpu/gskgpucachedfill.c
@@ -169,7 +169,8 @@ determine_scale_and_subpixel_grid (const graphene_size_t *scale,
 {
   if (gsk_transform_get_fine_category (modelview) <= GSK_FINE_TRANSFORM_CATEGORY_2D)
     {
-      *sx = *sy = ceilf (MAX (scale->width, scale->height) + 0.5);
+      *sx = exp2f (ceilf (log2f (scale->width)));
+      *sy = exp2f (ceilf (log2f (scale->height)));
       *subpixel_scale = 1;
     }
   else

gsk/gpu/gskgpucachedstroke.c
@@ -180,7 +180,8 @@ determine_scale_and_subpixel_grid (const graphene_size_t *scale,
 {
   if (gsk_transform_get_fine_category (modelview) <= GSK_FINE_TRANSFORM_CATEGORY_2D)
     {
-      *sx = *sy = ceilf (MAX (scale->width, scale->height) + 0.5);
+      *sx = exp2f (ceilf (log2f (scale->width)));
+      *sy = exp2f (ceilf (log2f (scale->height)));
       *subpixel_scale = 1;
     }
   else

gsk/gpu/gskgpunodeprocessor.c
@@ -2639,7 +2639,7 @@ gsk_gpu_node_processor_add_glyph_node (GskGpuRenderPass *self,
 
   if (gsk_transform_get_fine_category (self->modelview) <= GSK_FINE_TRANSFORM_CATEGORY_2D)
     {
-      scale = ceilf (scale + 0.5);
+      scale = exp2f (ceilf (log2f (scale)));
       align_scale_x = align_scale_y = 1;
       flags_mask = 0;
     }

testsuite/gsk/compare/fill-tiny-3d-scale-nocairo.node
@@ -1,0 +1,6 @@
+transform {
+  transform: scale3d(0.0009765625,0.0009765625,-10);
+  child: fill {
+    path: "M0 0 h40960 v40960 h-40960 z";
+  }
+}

testsuite/gsk/compare/stroke-tiny-3d-scale-nocairo.node
@@ -1,0 +1,7 @@
+transform {
+  transform: scale3d(0.0009765625,0.0009765625,-10);
+  child: stroke {
+    path: "M0 0 h40960 v40960 h-40960 z";
+    line-width: 4096;
+  }
+}

testsuite/gsk/compare/text-tiny-3d-scale-nocairo.node
@@ -1,0 +1,14 @@
+transform {
+  transform: scale3d(0.0006103515625,0.0006103515625,-10);
+  transform: scale3d(0.0009765625,0.0009765625,-10);
+  child: text {
+    font: "Font 23040" url("data://;base64,AAEAAAAIAIAAAwAAY21hcAAIAHYAAAEMAAAALGdseWYVIkEkAAABPAAAABhoZWFkwQ7zSgAAAIwA\
+AAA2aGhlYQwBBAIAAADEAAAAJGhtdHgEAAAAAAABCAAAAARsb2NhAAAADAAAATgAAAAEbWF4cAAD\
+AAUAAADoAAAAIG5hbWVuh0Z1AAABVAAAABYAAQAAAAEZmrvUSClfDzz1AAIIAAAAAAB8JbCAAAAA\
+AOHT3DoAAAAABAAIAAAAAAEAAgAAAAAAAAABAAAIAAAAAAAEAAAAAAAEAAABAAAAAAAAAAAAAAAA\
+AAAAAQABAAAAAQAEAAEAAAAAAAEAAAAAAAAAAAAAAAAAAAAABAAAAAAAAAEAAAADAAAADAAEACAA\
+AAAEAAQAAQAAAEH//wAAAEH///+/AAEAAAAAAAAADAABAAAAAAQACAAAAwAAMSERIQQA/AAIAAAA\
+AAEAEgABAAAAAAABAAQAAEZvbnQAAA==");
+    glyphs: 0 0;
+  }
+}

testsuite/gsk/meson.build
@@ -131,6 +131,7 @@ compare_render_tests = [
   'fill-subpixel-offsets-horizontal-nocairo',
   'fill-subpixel-offsets-vertical-nocairo',
   'fill-subpixel-shift-nocairo',
+  'fill-tiny-3d-scale-nocairo',
   'fill-with-3d-contents-nocairo',
   'globals-stress',
   'glyph-cache-overflow',
@@ -259,6 +260,7 @@ compare_render_tests = [
   'stroke-huge-path',
   'stroke-opacity',
   'stroke-scale-alignment-nocairo',
+  'stroke-tiny-3d-scale-nocairo',
   'stroke-with-3d-contents-nocairo',
   'subpixel-positioning',
   'subpixel-positioning-hidpi-nocairo',
@@ -267,6 +269,7 @@ compare_render_tests = [
   'text-glyph-lsb',
   'text-mixed-color-nocairo',
   'text-mixed-color-colrv1',
+  'text-tiny-3d-scale-nocairo',
   'texture-all-snaps',
   'texture-coords',
   'texture-offscreen-mipmap',