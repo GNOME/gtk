GNOME/gtk

commit 604a9c7cb385ae24fe74bebff5531ad3cecbd884

This should help with managing the shape tree.
Part-of: <https://gitlab.gnome.org/GNOME/gtk/-/merge_requests/10157>

gtk/svg/gtksvgelement.c
@@ -49,6 +49,37 @@
 
 #define CLONE_LIMIT 15000
 
+struct _SvgElementClass
+{
+  GObjectClass parent_class;
+};
+
+enum
+{
+  SVG_ELEMENT_PROP_ELEMENT_TYPE = 1,
+  SVG_ELEMENT_PROP_ID,
+  SVG_ELEMENT_NUM_PROPERTIES,
+};
+
+static GParamSpec *svg_element_prop[NUM_PROPERTIES];
+
+G_DEFINE_TYPE (SvgElement, svg_element, G_TYPE_OBJECT)
+
+static void
+svg_element_init (SvgElement *self)
+{
+}
+
+static void svg_element_finalize (GObject *object);
+
+static void
+svg_element_class_init (SvgElementClass *class)
+{
+  GObjectClass *object_class = G_OBJECT_CLASS (class);
+
+  object_class->finalize = svg_element_finalize;
+}
+
 static void
 text_node_clear (TextNode *self)
 {
@@ -96,9 +127,11 @@ lang_string_clear (LangString *l)
   l->type = NULL;
 }
 
-void
-svg_element_free (SvgElement *element)
+static void
+svg_element_finalize (GObject *object)
 {
+  SvgElement *element = SVG_ELEMENT (object);
+
   g_clear_pointer (&element->id, g_free);
   g_clear_pointer (&element->style, g_free);
   g_clear_pointer (&element->classes, g_strfreev);
@@ -138,15 +171,14 @@ svg_element_free (SvgElement *element)
 
   _gtk_bitmask_free (element->attrs);
 
-  g_free (element);
+  G_OBJECT_CLASS (svg_element_parent_class)->finalize (object);
 }
 
-SvgElement *
-svg_element_new (SvgElement     *parent,
-                 SvgElementType  type)
+static SvgElement *
+svg_element_setup (SvgElement     *element,
+                   SvgElement     *parent,
+                   SvgElementType  type)
 {
-  SvgElement *element = g_new0 (SvgElement, 1);
-
   element->parent = parent;
   element->type = type;
 
@@ -166,7 +198,7 @@ svg_element_new (SvgElement     *parent,
     }
 
   if (svg_element_type_is_container (type) || element->type == SVG_ELEMENT_USE)
-    element->shapes = g_ptr_array_new_with_free_func ((GDestroyNotify) svg_element_free);
+    element->shapes = g_ptr_array_new_with_free_func ((GDestroyNotify) g_object_unref);
 
   if (svg_element_type_is_gradient (type))
     element->color_stops = g_ptr_array_new_with_free_func ((GDestroyNotify) svg_color_stop_free);
@@ -195,6 +227,17 @@ svg_element_new (SvgElement     *parent,
     gtk_css_node_set_state (element->css_node, GTK_STATE_FLAG_LINK);
 
   return element;
+}
+
+SvgElement *
+svg_element_new (SvgElement     *parent,
+                 SvgElementType  type)
+{
+  SvgElement *self = g_object_new (SVG_TYPE_ELEMENT, NULL);
+
+  svg_element_setup (self, parent, type);
+
+  return self;
 }
 
 /* {{{ Conditional exclusion */
@@ -1768,13 +1811,13 @@ SvgElement *
 svg_element_duplicate (SvgElement *element,
                        SvgElement *parent)
 {
-  SvgElement *copy = g_new0 (SvgElement, 1);
+  SvgElement *copy = g_object_new (SVG_TYPE_ELEMENT, NULL);
 
-  copy->type = element->type;
   copy->parent = parent;
+  copy->type = element->type;
+
   copy->attrs = _gtk_bitmask_copy (element->attrs);
   copy->important = _gtk_bitmask_copy (element->important);
-  copy->id = NULL;
   copy->style = g_strdup (element->style);
   copy->classes = g_strdupv (element->classes);
   copy->title = g_strdup (element->title);
@@ -1791,7 +1834,14 @@ svg_element_duplicate (SvgElement *element,
     gtk_css_node_set_state (copy->css_node, GTK_STATE_FLAG_LINK);
 
   copy->specified = g_array_ref (element->specified);
+  copy->inline_styles = g_array_ref (element->inline_styles);
   copy->styles = g_ptr_array_ref (element->styles);
+
+  for (SvgProperty attr = FIRST_SVG_PROPERTY; attr <= LAST_SVG_PROPERTY; attr++)
+    {
+      element->base[attr] = svg_property_ref_initial_value (attr, copy->type, parent != NULL);
+      element->current[attr] = svg_value_ref (element->base[attr]);
+    }
 
   copy->shapes = g_ptr_array_new ();
   copy->animations = g_ptr_array_new ();
@@ -1845,7 +1895,7 @@ svg_element_set_element_type (SvgElement     *element,
     g_clear_pointer (&element->shapes, g_ptr_array_unref);
   if (!svg_element_type_is_container (old_type) &&
       svg_element_type_is_container (type))
-    element->shapes = g_ptr_array_new_with_free_func ((GDestroyNotify) svg_element_free);
+    element->shapes = g_ptr_array_new_with_free_func ((GDestroyNotify) g_object_unref);
 
   if (svg_element_type_is_text (old_type) &&
       !svg_element_type_is_text (type))
@@ -2352,7 +2402,7 @@ svg_element_clone (SvgElement *element,
                    GtkSvg     *svg,
                    ShadowData *data)
 {
-  SvgElement *clone = g_new0 (SvgElement, 1);
+  SvgElement *clone = g_object_new (SVG_TYPE_ELEMENT, NULL);
 
   if (data->count++ > CLONE_LIMIT)
     {
@@ -2411,14 +2461,14 @@ svg_element_clone (SvgElement *element,
       SvgElement *p, *q;
       unsigned int idx;
 
-      clone->shapes = g_ptr_array_new_with_free_func ((GDestroyNotify) svg_element_free);
+      clone->shapes = g_ptr_array_new_with_free_func ((GDestroyNotify) g_object_unref);
       for (unsigned int i = 0; i < element->shapes->len; i++)
         {
           SvgElement *child = g_ptr_array_index (element->shapes, i);
           SvgElement *child_clone = svg_element_clone (child, clone, svg, data);
           if (!child_clone)
             {
-              svg_element_free (clone);
+              g_object_unref (clone);
               return NULL;
             }
           g_ptr_array_add (clone->shapes, child_clone);

gtk/svg/gtksvg.c
@@ -1925,7 +1925,7 @@ gtk_svg_dispose (GObject *object)
   g_clear_pointer (&self->stylesheet, g_bytes_unref);
 
   /* clear_content recreates these */
-  g_clear_pointer (&self->content, svg_element_free);
+  g_clear_object (&self->content);
   g_clear_pointer (&self->timeline, timeline_free);
   g_clear_pointer (&self->images, g_hash_table_unref);
   g_clear_pointer (&self->user_styles, g_array_unref);
@@ -2512,7 +2512,7 @@ gtk_svg_clear_content (GtkSvg *self)
 
   g_clear_pointer (&self->resource, g_free);
   g_clear_pointer (&self->timeline, timeline_free);
-  g_clear_pointer (&self->content, svg_element_free);
+  g_clear_object (&self->content);
   g_clear_pointer (&self->images, g_hash_table_unref);
   g_clear_object (&self->fontmap);
   g_clear_pointer (&self->font_files, g_ptr_array_unref);

gtk/svg/gtksvgelementinternal.h
@@ -92,6 +92,8 @@ typedef struct
 
 struct _SvgElement
 {
+  GObject parent_instance;
+
   SvgElementType type;
   SvgElement *parent;
   char *id;

gtk/svg/gtksvgelementprivate.h
@@ -40,7 +40,10 @@
 
 G_BEGIN_DECLS
 
-void         svg_element_free                (SvgElement            *element);
+#define SVG_TYPE_ELEMENT (svg_element_get_type ())
+
+GDK_DECLARE_INTERNAL_TYPE (SvgElement, svg_element, SVG, ELEMENT, GObject)
+
 SvgElement * svg_element_new                 (SvgElement            *parent,
                                               SvgElementType         type);
 

gtk/svg/gtksvgparser.c
@@ -4664,7 +4664,7 @@ gtk_svg_init_from_bytes (GtkSvg *self,
     error_cb,
   };
 
-  g_clear_pointer (&self->content, svg_element_free);
+  g_clear_object (&self->content);
 
   if ((self->features & GTK_SVG_SYSTEM_RESOURCES) == 0)
     {

gtk/svg/gtksvgrenderer.c
@@ -5097,7 +5097,7 @@ gtk_svg_apply_filter (GtkSvg                *svg,
 
   result = apply_filter_tree (shape, filter, &paint_context, source);
 
-  svg_element_free (shape);
+  g_object_unref (shape);
 
   node = gtk_snapshot_free_to_node (paint_context.snapshot);
   g_assert (node == NULL);

commit b11016e0c7cd3407ccafbd24f9a6868a025f9e76

svg: Fix an oversight
When converting svg_element_copy to GObject, I introduced
a bug that was resetting the base and current attributes
of the copied element. The effect of this is only seen
in shaper where elements are frequently copied, and it causes
svg_element_get_path to always return empty paths, breaking
various things.

Fixes: 604a9c7cb385ae24f
Part-of: <https://gitlab.gnome.org/GNOME/gtk/-/merge_requests/10168>

gtk/svg/gtksvgelement.c
@@ -2036,8 +2036,8 @@ svg_element_duplicate (SvgElement *element,
 
   for (SvgProperty attr = FIRST_SVG_PROPERTY; attr <= LAST_SVG_PROPERTY; attr++)
     {
-      element->base[attr] = svg_property_ref_initial_value (attr, copy->type, parent != NULL);
-      element->current[attr] = svg_value_ref (element->base[attr]);
+      copy->base[attr] = svg_property_ref_initial_value (attr, copy->type, parent != NULL);
+      copy->current[attr] = svg_value_ref (copy->base[attr]);
     }
 
   copy->animations = g_ptr_array_new ();