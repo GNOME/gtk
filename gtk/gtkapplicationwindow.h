eabaa008d554a99fbfd0176836a91a79497db917
Adrian Vovk <adrianvovk@gmail.com>
2026-07-29 22:30:34 -0400

    application: Drop public save/restore API
    The async save/restore API was unable to land in GTK 4.24, so to avoid
    stabilizing the sync API we need to hide it again.
    
    You can find more complete context in my GUADEC 2026 talk about Session
    Save/Restore.
    
    Reverts 15ed5c2fea8ff8568110d5b86c148c02968166e4
    
    See also 031658a00650cda816ea3637304634efabde9f53
    
    Part-of: <https://gitlab.gnome.org/GNOME/gtk/-/merge_requests/10196>

--- gtk/gtkapplicationwindow.h
+++ gtk/gtkapplicationwindow.h
@@ -56,21 +56,8 @@ struct _GtkApplicationWindowClass
 {
   GtkWindowClass parent_class;
 
-  /**
-   * GtkApplicationWindowClass::save_state:
-   * @dict: a dictionary to populate with application window state
-   *
-   * Class closure for the [signal@ApplicationWindow::save-state] signal.
-   *
-   * Returns: true to stop stop further handlers from running
-   *
-   * Since: 4.24
-   */
-  gboolean     (* save_state)           (GtkApplicationWindow   *window,
-                                         GVariantDict           *dict);
-
   /*< private >*/
-  gpointer padding[7];
+  gpointer padding[8];
 };
 
 GDK_AVAILABLE_IN_ALL
