bb265f22ac7af8e259011b9c4fc0f982f573ddd2
Benjamin Otte <otte@redhat.com>
2026-08-04 19:05:23 +0200

    drawcontext: introduce GdkDrawContextFrame
    This is the new object that we create for each rendered frame and that
    tracks all the relevant per-frame information.
    
    This is just plumbing that adds the argument to each begin/end_frame
    vfunc.

--- gdk/macos/gdkmacosglcontext.c
+++ gdk/macos/gdkmacosglcontext.c
@@ -480,11 +480,12 @@ gdk_macos_gl_context_real_realize (GdkGLContext  *context,
 }
 
 static void
-gdk_macos_gl_context_begin_frame (GdkDrawContext  *context,
-                                  gpointer         context_data,
-                                  cairo_region_t  *region,
-                                  GdkColorState  **out_color_state,
-                                  GdkMemoryDepth  *out_depth)
+gdk_macos_gl_context_begin_frame (GdkDrawContext      *context,
+                                  GdkDrawContextFrame *frame,
+                                  gpointer             context_data,
+                                  cairo_region_t      *region,
+                                  GdkColorState      **out_color_state,
+                                  GdkMemoryDepth      *out_depth)
 {
   GdkMacosGLContext *self = (GdkMacosGLContext *)context;
   GdkMacosBuffer *buffer;
@@ -502,16 +503,17 @@ gdk_macos_gl_context_begin_frame (GdkDrawContext  *context,
   gdk_gl_context_make_current (GDK_GL_CONTEXT (self));
   gdk_macos_gl_context_allocate (self);
 
-  GDK_DRAW_CONTEXT_CLASS (gdk_macos_gl_context_parent_class)->begin_frame (context, context_data, region, out_color_state, out_depth);
+  GDK_DRAW_CONTEXT_CLASS (gdk_macos_gl_context_parent_class)->begin_frame (context, frame, context_data, region, out_color_state, out_depth);
 
   gdk_gl_context_make_current (GDK_GL_CONTEXT (self));
   CHECK_GL (NULL, glBindFramebuffer (GL_FRAMEBUFFER, self->fbo));
 }
 
 static void
-gdk_macos_gl_context_end_frame (GdkDrawContext *context,
-                                gpointer        context_data,
-                                cairo_region_t *painted)
+gdk_macos_gl_context_end_frame (GdkDrawContext      *context,
+                                GdkDrawContextFrame *frame,
+                                gpointer             context_data,
+                                cairo_region_t      *painted)
 {
   GdkMacosGLContext *self = GDK_MACOS_GL_CONTEXT (context);
   GdkSurface *surface;
@@ -521,7 +523,7 @@ gdk_macos_gl_context_end_frame (GdkDrawContext *context,
   g_assert (GDK_IS_MACOS_GL_CONTEXT (self));
   g_assert (self->cgl_context != nil);
 
-  GDK_DRAW_CONTEXT_CLASS (gdk_macos_gl_context_parent_class)->end_frame (context, context_data, painted);
+  GDK_DRAW_CONTEXT_CLASS (gdk_macos_gl_context_parent_class)->end_frame (context, frame, context_data, painted);
 
   surface = gdk_draw_context_get_surface (context);
   gdk_gl_context_make_current (GDK_GL_CONTEXT (self));
