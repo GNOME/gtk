ee34f9e041a06dea29a3fd80aa070d1beefccf32
Benjamin Otte <otte@redhat.com>
2026-08-06 23:02:08 +0200

    cairo: Add GdkCairoContextFrame
    This frame carries the surface to be rendered to.
    
    There's currently no implementation of it, but this is the plumbing.

--- gdk/gdkcairocontext.c
+++ gdk/gdkcairocontext.c
@@ -47,8 +47,44 @@ G_DEFINE_ABSTRACT_TYPE_WITH_CODE (GdkCairoContext, gdk_cairo_context, GDK_TYPE_D
                                   G_ADD_PRIVATE (GdkCairoContext))
 
 static void
+gdk_cairo_context_finalize_frame (GdkDrawContext      *context,
+                                  GdkDrawContextFrame *draw_frame)
+{
+  GdkCairoContextFrame *frame = (GdkCairoContextFrame *) draw_frame;
+
+  g_clear_pointer (&frame->surface, cairo_surface_destroy);
+
+  GDK_DRAW_CONTEXT_CLASS (gdk_cairo_context_parent_class)->finalize_frame (context, draw_frame);
+}
+
+static cairo_t *
+gdk_cairo_context_default_cairo_create (GdkCairoContext *self)
+{
+  GdkCairoContextFrame *frame;
+  cairo_t *cr;
+
+  frame = (GdkCairoContextFrame *) gdk_draw_context_get_current_frame (GDK_DRAW_CONTEXT (self));
+  if (frame == NULL || frame->surface == NULL)
+    return NULL;
+
+  cr = cairo_create (frame->surface);
+
+  gdk_cairo_region (cr, gdk_draw_context_frame_get_damage ((GdkDrawContextFrame *) frame));
+  cairo_clip (cr);
+
+  return cr;
+}
+
+static void
 gdk_cairo_context_class_init (GdkCairoContextClass *klass)
 {
+  GdkDrawContextClass *context_class = GDK_DRAW_CONTEXT_CLASS (klass);
+
+  context_class->frame_size = sizeof (GdkCairoContextFrame);
+
+  context_class->finalize_frame = gdk_cairo_context_finalize_frame;
+
+  klass->cairo_create = gdk_cairo_context_default_cairo_create;
 }
 
 static void
@@ -100,5 +136,43 @@ G_GNUC_END_IGNORE_DEPRECATIONS
   cairo_scale (cr, scale, scale);
 
   return cr;
+}
+
+/**
+ * gdk_cairo_context_frame_set_surface:
+ * @frame: the frame
+ * @surface: (transfer full) (nullable): the surface to use for rendering to this frame
+ *
+ * This API is called by backends to set the surface that belongs to this frame.
+ *
+ * The surface must have been set after begin_frame() and can be unset again in
+ * end_frame(), but it must exist inbetween.
+ *
+ * Backends tend to use one of two methods here: Either they create and assign a
+ * surface during initialize_frame() and keep reusing the surface or they recreate
+ * a new surfaces every frame in begin_frame() and unset them again in end_frame().
+ **/
+void
+gdk_cairo_context_frame_set_surface (GdkCairoContextFrame *frame,
+                                     cairo_surface_t     *surface)
+{
+  g_clear_pointer (&frame->surface, cairo_surface_destroy);
+  frame->surface = surface;
+}
+
+/*
+ * gdk_cairo_context_frame_get_surface:
+ * @frame: the frame
+ *
+ * Queries the surface that's used for this frame. A surface may only exist
+ * between begin/end_frame() calls, backends are free to release the surface
+ * afterwards.
+ *
+ * Returns: the surface to render to or NULL if none exists yet
+ **/
+cairo_surface_t *
+gdk_cairo_context_frame_get_surface (GdkCairoContextFrame *frame)
+{
+  return frame->surface;
 }
 
