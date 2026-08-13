GNOME/gtk

commit 0f237ffaee5757a0e085593c848e705eae0fe7a8

imcontextwayland: Handle conversion of pre-edit style hints to pango
This is hooked up to the CSS machinery, so that a "preedit" node with
a set of style classes for the (combined) hints is used. These CSS nodes
are cached by the Wayland GtkIMContext implementation as long as the
object has a client widget. Optionally it can be made to use a different
parent CSS node than the widget's own.

When obtaining the pre-edit text, these CSS nodes will be looked up
and converted to a PangoAttrList ready to use for the widget.

Part-of: <https://gitlab.gnome.org/GNOME/gtk/-/merge_requests/6922>

gtk/gtkimcontextwayland.c
@@ -21,8 +21,12 @@
 #include <string.h>
 #include <wayland-client-protocol.h>
 
+#include "gtk/gtkcsscolorvalueprivate.h"
+#include "gtk/gtkcssstyleprivate.h"
+#include "gtk/gtkimcontextprivate.h"
 #include "gtk/gtkimcontextwaylandprivate.h"
 #include "gtk/gtkimmoduleprivate.h"
+#include "gtk/gtkwidgetprivate.h"
 
 #include "gdk/wayland/gdkwayland.h"
 #include "text-input-unstable-v3-client-protocol.h"
@@ -81,6 +85,8 @@ struct _GtkIMContextWayland
 {
   GtkIMContext parent_instance;
   GtkWidget *widget;
+
+  GHashTable *style_css_nodes;
 
   struct {
     char *text;
@@ -566,7 +572,29 @@ gtk_im_context_wayland_finalize (GObject *object)
   g_clear_pointer (&context->pending_preedit.style_hints, g_array_unref);
   g_free (context->pending_commit);
 
+  g_clear_pointer (&context->style_css_nodes, g_hash_table_unref);
+
   G_OBJECT_CLASS (gtk_im_context_wayland_parent_class)->finalize (object);
+}
+
+static void
+update_css_node_state (gpointer key,
+                       gpointer value,
+                       gpointer user_data)
+{
+  GtkCssNode *css_node = value;
+  GtkStateFlags state_flags = GPOINTER_TO_UINT (user_data);
+
+  gtk_css_node_set_state (css_node, state_flags & ~GTK_STATE_FLAG_DROP_ACTIVE);
+}
+
+static void
+on_widget_state_changed (GtkWidget            *widget,
+                         GtkStateFlags         flags,
+                         GtkIMContextWayland  *context)
+{
+  g_hash_table_foreach (context->style_css_nodes,
+                        update_css_node_state, GUINT_TO_POINTER (flags));
 }
 
 static void
@@ -579,9 +607,195 @@ gtk_im_context_wayland_set_client_widget (GtkIMContext *context,
     return;
 
   if (context_wayland->widget)
-    gtk_im_context_wayland_focus_out (context);
+    {
+      g_hash_table_remove_all (context_wayland->style_css_nodes);
+      g_signal_handlers_disconnect_by_func (context_wayland->widget,
+                                            on_widget_state_changed,
+                                            context);
+      gtk_im_context_wayland_focus_out (context);
+    }
 
   g_set_object (&context_wayland->widget, widget);
+
+  if (context_wayland->widget)
+    {
+      g_signal_connect (context_wayland->widget, "state-flags-changed",
+                        G_CALLBACK (on_widget_state_changed), context);
+    }
+}
+
+static void
+on_css_node_style_changed (GtkCssNode          *node,
+                           GtkCssStyleChange   *change,
+                           GtkIMContextWayland *context)
+{
+  if (context->widget)
+    return;
+
+  if (gtk_css_style_change_affects (change, GTK_CSS_AFFECTS_SIZE))
+    gtk_widget_queue_resize (context->widget);
+  else
+    gtk_widget_queue_draw (context->widget);
+}
+
+static GtkCssNode *
+lookup_css_node (GtkIMContextWayland *context_wayland,
+                 unsigned int         hint_flags)
+{
+  GtkCssNode *css_node, *parent_node;
+
+  css_node = g_hash_table_lookup (context_wayland->style_css_nodes,
+                                  GUINT_TO_POINTER (hint_flags));
+
+  if (!css_node)
+    {
+
+      parent_node = gtk_im_context_get_parent_node (GTK_IM_CONTEXT (context_wayland));
+      if (!parent_node)
+        parent_node = gtk_widget_get_css_node (context_wayland->widget);
+
+      if (!parent_node)
+        return NULL;
+
+      css_node = gtk_css_node_new ();
+      gtk_css_node_set_name (css_node, g_quark_from_static_string ("preedit"));
+      gtk_css_node_set_parent (css_node, parent_node);
+      gtk_css_node_set_state (css_node, gtk_css_node_get_state (parent_node));
+
+      if ((hint_flags & (1 << ZWP_TEXT_INPUT_V3_PREEDIT_HINT_WHOLE)) != 0)
+        gtk_css_node_add_class (css_node, g_quark_from_static_string ("whole"));
+      if ((hint_flags & (1 << ZWP_TEXT_INPUT_V3_PREEDIT_HINT_SELECTION)) != 0)
+        gtk_css_node_add_class (css_node, g_quark_from_static_string ("selection"));
+      if ((hint_flags & (1 << ZWP_TEXT_INPUT_V3_PREEDIT_HINT_PREDICTION)) != 0)
+        gtk_css_node_add_class (css_node, g_quark_from_static_string ("prediction"));
+      if ((hint_flags & (1 << ZWP_TEXT_INPUT_V3_PREEDIT_HINT_PREFIX)) != 0)
+        gtk_css_node_add_class (css_node, g_quark_from_static_string ("prefix"));
+      if ((hint_flags & (1 << ZWP_TEXT_INPUT_V3_PREEDIT_HINT_SUFFIX)) != 0)
+        gtk_css_node_add_class (css_node, g_quark_from_static_string ("suffix"));
+      if ((hint_flags & (1 << ZWP_TEXT_INPUT_V3_PREEDIT_HINT_SPELLING_ERROR)) != 0)
+        gtk_css_node_add_class (css_node, g_quark_from_static_string ("spelling-error"));
+      if ((hint_flags & (1 << ZWP_TEXT_INPUT_V3_PREEDIT_HINT_COMPOSE_ERROR)) != 0)
+        gtk_css_node_add_class (css_node, g_quark_from_static_string ("compose-error"));
+
+      g_signal_connect (css_node, "style-changed",
+                        G_CALLBACK (on_css_node_style_changed),
+                        context_wayland);
+
+      g_hash_table_insert (context_wayland->style_css_nodes, GUINT_TO_POINTER (hint_flags), css_node);
+    }
+
+  return css_node;
+}
+
+static void
+node_to_pango_attrs (GtkIMContextWayland        *context_wayland,
+                     GtkIMContextPreeditSegment *segment,
+                     GtkCssNode                 *node,
+                     PangoAttrList              *attrs)
+{
+  PangoAttrList *node_attrs;
+  PangoAttribute *attr;
+  GtkCssStyle *style;
+  const GdkRGBA *color;
+
+  node_attrs = gtk_css_style_get_pango_attributes (gtk_css_node_get_style (node));
+  if (node_attrs)
+    {
+      GSList *attr_list, *l;
+
+      attr_list = pango_attr_list_get_attributes (node_attrs);
+
+      /* Change start/end indexes and move to the overall list */
+      for (l = attr_list; l; l = l->next)
+        {
+          attr = l->data;
+          attr->start_index = segment->start_idx;
+          attr->end_index = segment->end_idx;
+          pango_attr_list_insert (attrs, attr);
+        }
+
+      pango_attr_list_unref (node_attrs);
+      g_slist_free (attr_list);
+    }
+
+  style = gtk_css_node_get_style (node);
+
+  /* Foreground */
+  color = gtk_css_color_value_get_rgba (style->used->color);
+
+  if (color->alpha > 0)
+    {
+      attr = pango_attr_foreground_new (CLAMP (color->red * 65535. + 0.5, 0, 65535),
+                                        CLAMP (color->green * 65535. + 0.5, 0, 65535),
+                                        CLAMP (color->blue * 65535. + 0.5, 0, 65535));
+      attr->start_index = segment->start_idx;
+      attr->end_index = segment->end_idx;
+      pango_attr_list_insert (attrs, attr);
+
+      if (color->alpha < 1)
+        {
+          attr = pango_attr_foreground_alpha_new (CLAMP (color->alpha * 65535. + 0.5, 0, 65535));
+          attr->start_index = segment->start_idx;
+          attr->end_index = segment->end_idx;
+          pango_attr_list_insert (attrs, attr);
+        }
+    }
+
+  /* Background */
+  color = gtk_css_color_value_get_rgba (style->used->background_color);
+
+  if (color->alpha > 0)
+    {
+      attr = pango_attr_background_new (CLAMP (color->red * 65535. + 0.5, 0, 65535),
+                                        CLAMP (color->green * 65535. + 0.5, 0, 65535),
+                                        CLAMP (color->blue * 65535. + 0.5, 0, 65535));
+      attr->start_index = segment->start_idx;
+      attr->end_index = segment->end_idx;
+      pango_attr_list_insert (attrs, attr);
+
+      if (color->alpha < 1)
+        {
+          attr = pango_attr_background_alpha_new (CLAMP (color->alpha * 65535. + 0.5, 0, 65535));
+          attr->start_index = segment->start_idx;
+          attr->end_index = segment->end_idx;
+          pango_attr_list_insert (attrs, attr);
+        }
+    }
+}
+
+static PangoAttrList *
+convert_style_hints_to_pango (GtkIMContextWayland *context_wayland,
+                              const char          *text)
+{
+  PangoAttrList *attrs;
+  PangoAttribute *attr;
+  unsigned int i;
+
+  attrs = pango_attr_list_new ();
+
+  /* enable fallback, since IBus will send us things like ⎄ */
+  attr = pango_attr_fallback_new (TRUE);
+  attr->start_index = 0;
+  attr->end_index = strlen (text);
+  pango_attr_list_insert (attrs, attr);
+
+  if (!context_wayland->current_preedit.style_hints)
+    return attrs;
+
+  for (i = 0; i < context_wayland->current_preedit.style_hints->len; i++)
+    {
+      GtkIMContextPreeditSegment *segment;
+      GtkCssNode *node;
+
+      segment = &g_array_index (context_wayland->current_preedit.style_hints,
+                                GtkIMContextPreeditSegment, i);
+      node = lookup_css_node (context_wayland, segment->hint_flags);
+
+      if (node)
+        node_to_pango_attrs (context_wayland, segment, node, attrs);
+    }
+
+  return attrs;
 }
 
 static void
@@ -593,9 +807,6 @@ gtk_im_context_wayland_get_preedit_string (GtkIMContext   *context,
   GtkIMContextWayland *context_wayland = GTK_IM_CONTEXT_WAYLAND (context);
   const char *preedit_str;
 
-  if (attrs)
-    *attrs = NULL;
-
   preedit_str =
     context_wayland->current_preedit.text ? context_wayland->current_preedit.text : "";
 
@@ -605,35 +816,9 @@ gtk_im_context_wayland_get_preedit_string (GtkIMContext   *context,
 
   if (str)
     *str = g_strdup (preedit_str);
+
   if (attrs)
-    {
-      PangoAttribute *attr;
-      guint len = strlen (preedit_str);
-
-      if (!*attrs)
-        *attrs = pango_attr_list_new ();
-
-      attr = pango_attr_underline_new (PANGO_UNDERLINE_SINGLE);
-      attr->start_index = 0;
-      attr->end_index = len;
-      pango_attr_list_insert (*attrs, attr);
-
-      /* enable fallback, since IBus will send us things like ⎄ */
-      attr = pango_attr_fallback_new (TRUE);
-      attr->start_index = 0;
-      attr->end_index = len;
-      pango_attr_list_insert (*attrs, attr);
-
-      if (context_wayland->current_preedit.cursor_begin
-          != context_wayland->current_preedit.cursor_end)
-        {
-          /* FIXME: Oh noes, how to highlight while taking into account user preferences? */
-          PangoAttribute *cursor = pango_attr_weight_new (PANGO_WEIGHT_BOLD);
-          cursor->start_index = context_wayland->current_preedit.cursor_begin;
-          cursor->end_index = context_wayland->current_preedit.cursor_end;
-          pango_attr_list_insert (*attrs, cursor);
-        }
-    }
+    *attrs = convert_style_hints_to_pango (context_wayland, preedit_str);
 }
 
 static void
@@ -1117,6 +1302,13 @@ on_content_type_changed (GtkIMContextWayland *context)
 }
 
 static void
+unset_css_node (GtkCssNode *node)
+{
+  gtk_css_node_set_parent (node, NULL);
+  g_object_unref (node);
+}
+
+static void
 gtk_im_context_wayland_init (GtkIMContextWayland *context)
 {
   context->use_preedit = TRUE;
@@ -1124,4 +1316,6 @@ gtk_im_context_wayland_init (GtkIMContextWayland *context)
                             G_CALLBACK (on_content_type_changed), context);
   g_signal_connect_swapped (context, "notify::input-hints",
                             G_CALLBACK (on_content_type_changed), context);
+  context->style_css_nodes =
+    g_hash_table_new_full (NULL, NULL, NULL, (GDestroyNotify) unset_css_node);
 }

commit 32cecfefb775fe9ef7db163eac182d2e47fc1e27

imcontextwayland: Fix a typo
We want obviously to exit early if there is no widget.

Fixes: 0f237ffaee5757a0e085
Closes: #8350
Part-of: <https://gitlab.gnome.org/GNOME/gtk/-/merge_requests/10246>

gtk/gtkimcontextwayland.c
@@ -731,7 +731,7 @@ on_css_node_style_changed (GtkCssNode          *node,
                            GtkCssStyleChange   *change,
                            GtkIMContextWayland *context)
 {
-  if (context->widget)
+  if (!context->widget)
     return;
 
   if (gtk_css_style_change_affects (change, GTK_CSS_AFFECTS_SIZE))