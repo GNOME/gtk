25bac6f058f2dcf898aa21829b39865752676953
Benjamin Otte <otte@redhat.com>
2026-08-14 03:50:11 +0200

    egl: Add fd-based GPU completion tracking

--- gdk/gdkdisplayprivate.h
+++ gdk/gdkdisplayprivate.h
@@ -129,6 +129,7 @@ struct _GdkDisplay
   guint have_egl_dma_buf_import : 1;
   guint have_egl_dma_buf_export : 1;
   guint have_egl_gl_colorspace : 1;
+  guint have_egl_sync_fd : 1;
 
   /* atomic */ GdkDmabufFormats *dmabuf_formats;
   GdkDmabufDownloader *egl_downloader;
