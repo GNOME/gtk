GNOME/gtk

commit b4d9da8c0d334537ff50af4412cdd5f8caa3835e

gdk: Add default implementation of GdkDrawContext::empty_frame()
... instead of adding a custom no-op in all backends.

Now that all backends have reviewed this vfunc and the conclusion is
that it's almost never needed, it makes sense to add a default
implementation that does nothing.

Part-of: <https://gitlab.gnome.org/GNOME/gtk/-/merge_requests/10271>

gdk/macos/gdkmacosvulkancontext.c
@@ -70,11 +70,6 @@ gdk_macos_vulkan_context_create_surface (GdkVulkanContext *context,
 }
 
 static void
-gdk_macos_vulkan_context_empty_frame (GdkDrawContext *context)
-{
-}
-
-static void
 gdk_macos_vulkan_context_surface_resized (GdkDrawContext *context)
 {
   GdkSurface *surface = gdk_draw_context_get_surface (context);

gdk/android/gdkandroidglcontext.c
@@ -68,10 +68,6 @@ gdk_android_gl_context_end_frame (GdkDrawContext *draw_context,
   GDK_DRAW_CONTEXT_CLASS (gdk_android_gl_context_parent_class)->end_frame (draw_context, context_data, painted);
 }
 
-static void
-gdk_android_gl_context_empty_frame (GdkDrawContext *draw_context)
-{}
-
 static gboolean
 gdk_android_gl_context_surface_attach (GdkDrawContext  *context,
                                        GError         **error)
@@ -138,7 +134,6 @@ gdk_android_gl_context_class_init (GdkAndroidGLContextClass *class)
 
   draw_context_class->begin_frame = gdk_android_gl_context_begin_frame;
   draw_context_class->end_frame = gdk_android_gl_context_end_frame;
-  draw_context_class->empty_frame = gdk_android_gl_context_empty_frame;
   draw_context_class->surface_attach = gdk_android_gl_context_surface_attach;
   draw_context_class->surface_resized = gdk_android_gl_context_surface_resized;
   draw_context_class->surface_detach = gdk_android_gl_context_surface_detach;

gdk/gdkdrawcontext.c
@@ -101,7 +101,6 @@ gdk_draw_context_default_surface_detach (GdkDrawContext *context)
 static void
 gdk_draw_context_default_empty_frame (GdkDrawContext *context)
 {
-  g_warning ("FIXME: Implement GdkDrawContext.empty_frame in %s", G_OBJECT_TYPE_NAME (context));
 }
 
 static void

gdk/macos/gdkmacoscairocontext.c
@@ -234,11 +234,6 @@ _gdk_macos_cairo_context_end_frame (GdkDrawContext *draw_context,
 }
 
 static void
-_gdk_macos_cairo_context_empty_frame (GdkDrawContext *draw_context)
-{
-}
-
-static void
 _gdk_macos_cairo_context_surface_resized (GdkDrawContext *draw_context)
 {
   g_assert (GDK_IS_MACOS_CAIRO_CONTEXT (draw_context));
@@ -268,7 +263,6 @@ _gdk_macos_cairo_context_class_init (GdkMacosCairoContextClass *klass)
 
   draw_context_class->begin_frame = _gdk_macos_cairo_context_begin_frame;
   draw_context_class->end_frame = _gdk_macos_cairo_context_end_frame;
-  draw_context_class->empty_frame = _gdk_macos_cairo_context_empty_frame;
   draw_context_class->surface_resized = _gdk_macos_cairo_context_surface_resized;
   draw_context_class->surface_attach = _gdk_macos_cairo_context_surface_attach;
 

gdk/macos/gdkmacosglcontext.c
@@ -551,11 +551,6 @@ gdk_macos_gl_context_end_frame (GdkDrawContext *context,
 }
 
 static void
-gdk_macos_gl_context_empty_frame (GdkDrawContext *draw_context)
-{
-}
-
-static void
 gdk_macos_gl_context_surface_resized (GdkDrawContext *draw_context)
 {
   GdkMacosGLContext *self = (GdkMacosGLContext *)draw_context;
@@ -695,7 +690,6 @@ gdk_macos_gl_context_class_init (GdkMacosGLContextClass *klass)
 
   draw_context_class->begin_frame = gdk_macos_gl_context_begin_frame;
   draw_context_class->end_frame = gdk_macos_gl_context_end_frame;
-  draw_context_class->empty_frame = gdk_macos_gl_context_empty_frame;
   draw_context_class->surface_resized = gdk_macos_gl_context_surface_resized;
   draw_context_class->surface_attach = gdk_macos_gl_context_surface_attach;
 

gdk/win32/gdkcairocontext-win32.c
@@ -372,11 +372,6 @@ gdk_win32_cairo_context_end_frame (GdkDrawContext *draw_context,
 }
 
 static void
-gdk_win32_cairo_context_empty_frame (GdkDrawContext *draw_context)
-{
-}
-
-static void
 gdk_win32_cairo_context_surface_resized (GdkDrawContext *draw_context)
 {
   GdkWin32CairoContext *self = GDK_WIN32_CAIRO_CONTEXT (draw_context);
@@ -424,7 +419,6 @@ gdk_win32_cairo_context_class_init (GdkWin32CairoContextClass *klass)
 
   draw_context_class->begin_frame = gdk_win32_cairo_context_begin_frame;
   draw_context_class->end_frame = gdk_win32_cairo_context_end_frame;
-  draw_context_class->empty_frame = gdk_win32_cairo_context_empty_frame;
   draw_context_class->surface_attach = gdk_win32_cairo_context_surface_attach;
   draw_context_class->surface_detach = gdk_win32_cairo_context_surface_detach;
   draw_context_class->surface_resized = gdk_win32_cairo_context_surface_resized;

gdk/win32/gdkglcontext-win32-egl.c
@@ -49,19 +49,11 @@ typedef struct _GdkWin32GLContextClass    GdkWin32GLContextEGLClass;
 G_DEFINE_TYPE (GdkWin32GLContextEGL, gdk_win32_gl_context_egl, GDK_TYPE_WIN32_GL_CONTEXT)
 
 static void
-gdk_win32_gl_context_egl_empty_frame (GdkDrawContext *draw_context)
-{
-}
-
-static void
 gdk_win32_gl_context_egl_class_init (GdkWin32GLContextClass *klass)
 {
   GdkGLContextClass *context_class = GDK_GL_CONTEXT_CLASS(klass);
-  GdkDrawContextClass *draw_context_class = GDK_DRAW_CONTEXT_CLASS(klass);
 
   context_class->backend_type = GDK_GL_EGL;
-
-  draw_context_class->empty_frame = gdk_win32_gl_context_egl_empty_frame;
 }
 
 static void

gdk/win32/gdkglcontext-win32-wgl.c
@@ -132,11 +132,6 @@ gdk_win32_gl_context_wgl_end_frame (GdkDrawContext *draw_context,
   SwapBuffers (GetDC (GDK_WIN32_GL_CONTEXT (context)->handle));
 }
 
-static void
-gdk_win32_gl_context_wgl_empty_frame (GdkDrawContext *draw_context)
-{
-}
-
 static cairo_region_t *
 gdk_win32_gl_context_wgl_get_damage (GdkGLContext *gl_context)
 {
@@ -1092,7 +1087,6 @@ gdk_win32_gl_context_wgl_class_init (GdkWin32GLContextWGLClass *klass)
   context_class->get_damage = gdk_win32_gl_context_wgl_get_damage;
 
   draw_context_class->end_frame = gdk_win32_gl_context_wgl_end_frame;
-  draw_context_class->empty_frame = gdk_win32_gl_context_wgl_empty_frame;
   draw_context_class->surface_attach = gdk_win32_gl_context_wgl_surface_attach;
   draw_context_class->surface_detach = gdk_win32_gl_context_wgl_surface_detach;
 

commit 45ffe9566a7279fea722772e26574ca1120b31ae

macos: Remove leftover function assignment
Apparently CI doesn't compile this code so it didn't complain about this
error being left in the code.

Fixes: b4d9da8c0d334537ff50af4412cdd5f8caa3835e
Part-of: <https://gitlab.gnome.org/GNOME/gtk/-/merge_requests/10315>

gdk/macos/gdkmacosvulkancontext.c
@@ -116,7 +116,6 @@ gdk_macos_vulkan_context_class_init (GdkMacosVulkanContextClass *klass)
 
   vulkan_context_class->create_surface = gdk_macos_vulkan_context_create_surface;
 
-  draw_context_class->empty_frame = gdk_macos_vulkan_context_empty_frame;
   draw_context_class->surface_resized = gdk_macos_vulkan_context_surface_resized;
   draw_context_class->surface_attach = gdk_macos_vulkan_context_surface_attach;
 }