GNOME/gtk

commit 32625381fa2bbc5a63d6fecab19ecc6d43222221

gsk: Deprecate GskGLShader and the rendernode
The new renderers don't support them due to the required complexity of
integrating them with Vulkan and the assumptions those nodes make about
the renderer (the GL renderer exports its internal APIs into the
GLShader).

There haven't been any complaints that I'm aware of since 4.14 was
released where the default renderer does not support the nodes, so usage
in public seems to be close to nonexistant.

The 2 uses I know of were workarounds about missing features in GTK that
have stopped since GTK now supports them:

1. GStreamer used in to do premultiplication when the old GL renderer
   did not do so in hardware but on the CPU.
2. Adwaita used it for masking before the mask node wa added in 4.10.

gtk/gtksnapshot.c
@@ -91,6 +91,7 @@ struct _GtkSnapshotState {
     struct {
       graphene_rect_t bounds;
     } clip;
+G_GNUC_BEGIN_IGNORE_DEPRECATIONS
     struct {
       GskGLShader *shader;
       GBytes *args;
@@ -98,6 +99,7 @@ struct _GtkSnapshotState {
       GskRenderNode **nodes;
       GskRenderNode *internal_nodes[4];
     } glshader;
+G_GNUC_END_IGNORE_DEPRECATIONS
     struct {
       graphene_rect_t bounds;
       int node_idx;
@@ -917,6 +919,8 @@ gtk_snapshot_push_clip (GtkSnapshot           *snapshot,
   gtk_graphene_rect_scale_affine (bounds, scale_x, scale_y, dx, dy, &state->data.clip.bounds);
 }
 
+G_GNUC_BEGIN_IGNORE_DEPRECATIONS
+
 static GskRenderNode *
 gtk_snapshot_collect_gl_shader (GtkSnapshot      *snapshot,
                                 GtkSnapshotState *state,
@@ -1045,6 +1049,10 @@ gtk_snapshot_collect_gl_shader_texture (GtkSnapshot      *snapshot,
  * re-rendered.
  *
  * For details on how to write shaders, see [class@Gsk.GLShader].
+ *
+ * Deprecated: 4.16: GTK's new Vulkan-focused rendering
+ *   does not support this feature. Use [class@Gtk.GLArea] for
+ *   OpenGL rendering.
  */
 void
 gtk_snapshot_push_gl_shader (GtkSnapshot           *snapshot,
@@ -1125,6 +1133,8 @@ gtk_snapshot_collect_rounded_clip (GtkSnapshot      *snapshot,
 
   return clip_node;
 }
+
+G_GNUC_END_IGNORE_DEPRECATIONS
 
 /**
  * gtk_snapshot_push_rounded_clip:
@@ -1956,6 +1966,10 @@ gtk_snapshot_pop (GtkSnapshot *snapshot)
  * This must be called the same number of times as the number
  * of textures is needed for the shader in
  * [method@Gtk.Snapshot.push_gl_shader].
+ *
+ * Deprecated: 4.16: GTK's new Vulkan-focused rendering
+ *   does not support this feature. Use [class@Gtk.GLArea] for
+ *   OpenGL rendering.
  */
 void
 gtk_snapshot_gl_shader_pop_texture (GtkSnapshot *snapshot)

demos/gtk-demo/fishbowl.c
@@ -170,6 +170,7 @@ update_paintable (GtkWidget     *widget,
 static GtkWidget *
 create_cogs (void)
 {
+G_GNUC_BEGIN_IGNORE_DEPRECATIONS
   GtkWidget *picture;
   static GskGLShader *cog_shader = NULL;
   GdkPaintable *paintable;
@@ -182,6 +183,7 @@ create_cogs (void)
   gtk_widget_add_tick_callback (picture, update_paintable, NULL, NULL);
 
   return picture;
+G_GNUC_END_IGNORE_DEPRECATIONS
 }
 
 static gboolean

demos/gtk-demo/gltransition.c
@@ -20,6 +20,8 @@
 #include "gtkshadertoy.h"
 #include "gskshaderpaintable.h"
 
+G_GNUC_BEGIN_IGNORE_DEPRECATIONS
+
 static GtkWidget *demo_window = NULL;
 
 static void
@@ -360,3 +362,5 @@ do_gltransition (GtkWidget *do_widget)
 
   return demo_window;
 }
+
+G_GNUC_END_IGNORE_DEPRECATIONS

demos/gtk-demo/gskshaderpaintable.c
@@ -22,6 +22,8 @@
 #include <gtk/gtk.h>
 #include "gskshaderpaintable.h"
 
+G_GNUC_BEGIN_IGNORE_DEPRECATIONS
+
 /**
  * GskShaderPaintable:
  *
@@ -332,3 +334,5 @@ gsk_shader_paintable_update_time (GskShaderPaintable *self,
 
   g_bytes_unref (args);
 }
+
+G_GNUC_END_IGNORE_DEPRECATIONS

demos/gtk-demo/gskshaderpaintable.h
@@ -22,6 +22,8 @@
 #include <gdk/gdk.h>
 #include <gsk/gsk.h>
 
+G_GNUC_BEGIN_IGNORE_DEPRECATIONS
+
 G_BEGIN_DECLS
 
 #define GSK_TYPE_SHADER_PAINTABLE (gsk_shader_paintable_get_type ())
@@ -41,3 +43,5 @@ void             gsk_shader_paintable_update_time      (GskShaderPaintable *self
                                                         int                 time_idx,
                                                         gint64              frame_time);
 G_END_DECLS
+
+G_GNUC_END_IGNORE_DEPRECATIONS

demos/gtk-demo/gtkshaderbin.c
@@ -1,5 +1,7 @@
 #include "gtkshaderbin.h"
 
+G_GNUC_BEGIN_IGNORE_DEPRECATIONS
+
 typedef struct {
   GskGLShader *shader;
   GtkStateFlags state;
@@ -262,3 +264,5 @@ gtk_shader_bin_new (void)
 
   return GTK_WIDGET (self);
 }
+
+G_GNUC_END_IGNORE_DEPRECATIONS

demos/gtk-demo/gtkshaderbin.h
@@ -2,6 +2,8 @@
 
 #include <gtk/gtk.h>
 
+G_GNUC_BEGIN_IGNORE_DEPRECATIONS
+
 G_BEGIN_DECLS
 
 #define GTK_TYPE_SHADER_BIN      (gtk_shader_bin_get_type ())
@@ -18,3 +20,5 @@ void       gtk_shader_bin_set_child   (GtkShaderBin *self,
 GtkWidget *gtk_shader_bin_get_child   (GtkShaderBin *self);
 
 G_END_DECLS
+
+G_GNUC_END_IGNORE_DEPRECATIONS

demos/gtk-demo/gtkshaderstack.c
@@ -1,5 +1,7 @@
 #include "gtkshaderstack.h"
 
+G_GNUC_BEGIN_IGNORE_DEPRECATIONS
+
 struct _GtkShaderStack
 {
   GtkWidget parent_instance;
@@ -359,3 +361,5 @@ gtk_shader_stack_set_active (GtkShaderStack *self,
   self->current = MIN (index, self->children->len);
   update_child_visible (self);
 }
+
+G_GNUC_END_IGNORE_DEPRECATIONS

commit 872e0e4b1ec1aa242bcb9a17552c9fc89085b9be

snapshot: Move END_DEPRECATIONS line
It was meant for the glshader, but included the rounded clip
implementation.

Fixes: 32625381fa2bbc5a63d6fecab19ecc6d43222221

gtk/gtksnapshot.c
@@ -1256,6 +1256,8 @@ gtk_snapshot_push_gl_shader (GtkSnapshot           *snapshot,
     }
 }
 
+G_GNUC_END_IGNORE_DEPRECATIONS
+
 static GskRenderNode *
 gtk_snapshot_collect_rounded_clip (GtkSnapshot      *snapshot,
                                    GtkSnapshotState *state,
@@ -1301,8 +1303,6 @@ gtk_snapshot_collect_rounded_clip (GtkSnapshot      *snapshot,
 
   return clip_node;
 }
-
-G_GNUC_END_IGNORE_DEPRECATIONS
 
 /**
  * gtk_snapshot_push_rounded_clip: